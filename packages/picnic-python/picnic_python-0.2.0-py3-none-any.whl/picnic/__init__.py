618625125cacb98eb39d784fec11f3a5edba2f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package picnic-python/0.2.0/picnic_python-0.2.0-py3-none-any.whl

---
+++ packages/picnic-python/picnic_python-0.2.0-py3-none-any.whl/picnic/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+__version__ = '0.2.0'
+
+api_key = None
+api_base = 'https://picnic.sh/api'
+
+from picnic.picnic import get_price, get_website, list_websites, create_website
+
