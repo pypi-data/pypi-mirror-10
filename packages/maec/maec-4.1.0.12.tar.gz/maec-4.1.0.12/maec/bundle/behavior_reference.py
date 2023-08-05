c1c12d810a53021c9cda7439128c2ac292381e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package maec/4.1.0.12/maec-4.1.0.12.tar.gz

---
+++ packages/maec/maec-4.1.0.12.tar.gz/maec-4.1.0.12/maec/bundle/behavior_reference.py
@@ -0,0 +1,20 @@
+# MAEC Behavior Reference Class
+
+# Copyright (c) 2015, The MITRE Corporation
+# All rights reserved
+
+
+import maec
+from . import _namespace
+import maec.bindings.maec_bundle as bundle_binding
+       
+class BehaviorReference(maec.Entity):
+    _binding = bundle_binding
+    _binding_class = bundle_binding.BehaviorReferenceType    
+    _namespace = _namespace
+
+    behavior_idref = maec.TypedField("behavior_idref")
+
+    def __init__(self, behavior_idref = None):
+        super(BehaviorReference, self).__init__()
+        self.behavior_idref = behavior_idref
