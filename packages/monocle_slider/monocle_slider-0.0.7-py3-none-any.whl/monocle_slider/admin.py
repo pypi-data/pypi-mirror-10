3ce0ae4a17bfd9f7d462e3194e9c6ca9c222bb9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:30 +0000

    Add package monocle_slider/0.0.7/monocle_slider-0.0.7-py3-none-any.whl

---
+++ packages/monocle_slider/monocle_slider-0.0.7-py3-none-any.whl/monocle_slider/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import *
+from grappelli.forms import GrappelliSortableHiddenMixin
+
+from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteInlineModelAdmin
+
+class SlideInline(GrappelliSortableHiddenMixin, admin.TabularInline, SummernoteInlineModelAdmin):
+    model = Slide
+    sortable_field_name = "position"
+    extra = 0
+
+    fieldsets = [
+        ('Настройки слайда', {'fields': ['name', 'image', 'text'], 'classes': ['collapse']}),
+        ('Дополнительно', {'fields': ['position'], 'classes': ['collapse']}),
+    ]
+    list_display = ('name', 'text', 'position',)
+
+class SliderAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'elem_number', 'isShown',)
+    list_editable = ('elem_number', 'isShown',)
+    inlines = [SlideInline]
+
+admin.site.register(Slider, SliderAdmin)
