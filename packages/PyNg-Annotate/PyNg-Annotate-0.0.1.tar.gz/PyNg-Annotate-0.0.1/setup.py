706d5bfd4189d8dd89f82579255c90dd27126537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:59 +0000

    Add package PyNg-Annotate/0.0.1/PyNg-Annotate-0.0.1.tar.gz

---
+++ packages/PyNg-Annotate/PyNg-Annotate-0.0.1.tar.gz/PyNg-Annotate-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='PyNg-Annotate',
+    modules=['ng_annotate'],
+    version='0.0.1',
+    author='kyle roux',
+    author_email='kyle@level2designs.com',
+)
