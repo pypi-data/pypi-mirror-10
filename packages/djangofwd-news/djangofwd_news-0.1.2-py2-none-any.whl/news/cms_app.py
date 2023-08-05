7bbc542951e43d44cb1a0908c422e07c34e350ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:15 +0000

    Add package djangofwd-news/0.1.2/djangofwd_news-0.1.2-py2-none-any.whl

---
+++ packages/djangofwd-news/djangofwd_news-0.1.2-py2-none-any.whl/news/cms_app.py
@@ -0,0 +1,10 @@
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
+
+class NewsApphook(CMSApp):
+    name = _("News")
+    urls = ["news.app.news.urls"]
+
+apphook_pool.register(NewsApphook)
