c254a1298761f121b49db320ad0818158b9ffdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:55 +0000

    Add package doxypypy/0.8.8.5/doxypypy-0.8.8.5.tar.gz

---
+++ packages/doxypypy/doxypypy-0.8.8.5.tar.gz/doxypypy-0.8.8.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from os.path import dirname, join
+from os import chdir
+
+if dirname(__file__):
+    chdir(dirname(__file__))
+
+setup(
+    name='doxypypy',
+    version='0.8.8.5',
+    description='A Doxygen filter for Python',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    keywords='Doxygen filter Python documentation',
+    author='Eric W. Brown',
+    url='https://github.com/Feneric/doxypypy',
+    packages=find_packages(),
+    test_suite='doxypypy.test.test_doxypypy',
+    entry_points={
+        'console_scripts': [
+            'doxypypy = doxypypy.doxypypy:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Documentation'
+    ]
+)
