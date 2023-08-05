ac3745daacf2d815a838426d15862389f0d5a5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package gobble/0.1.1/gobble-0.1.1-py3-none-any.whl

---
+++ packages/gobble/gobble-0.1.1-py3-none-any.whl/gobble/location.py
@@ -0,0 +1,12 @@
+def compute_location(source, index):
+    if not source:
+        return (1, 1)
+    if not isinstance(source, str):
+        return (1, index)
+    if index >= len(source):
+        return source.count('\n') + 2, 1
+    preceding_newlines = source.count('\n', 0, index + 1)
+    previous_newline_index = source.rfind('\n', 0, index + 1)
+    if previous_newline_index == -1:
+        previous_newline_index = 0
+    return preceding_newlines + 1, index - previous_newline_index + 1
