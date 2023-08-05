c4e370973809520d8f648c3959ac91949954c2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package urbanairship/0.7.2/urbanairship-0.7.2.tar.gz

---
+++ packages/urbanairship/urbanairship-0.7.2.tar.gz/urbanairship-0.7.2/urbanairship/devices/__init__.py
@@ -0,0 +1,25 @@
+from .devicelist import (
+    ChannelList,
+    ChannelInfo,
+    DevicePINInfo,
+    DeviceTokenList,
+    DevicePINList,
+    APIDList,
+    Feedback,
+)
+
+from .tag import (
+    TagList,
+    Tag,
+    DeleteTag,
+    BatchTag,
+)
+
+from .segment import (
+    Segment,
+    SegmentList
+)
+
+from .channel_uninstall import (
+    ChannelUninstall
+)
