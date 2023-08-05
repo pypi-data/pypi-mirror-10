9a49d05785429f1c33d02caa468fd739e9e094df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package schematec/0.3.2/schematec-0.3.2-py2.py3-none-any.whl

---
+++ packages/schematec/schematec-0.3.2-py2.py3-none-any.whl/schematec/__init__.py
@@ -0,0 +1,40 @@
+from .converters import (
+    integer,
+    number,
+    string,
+    boolean,
+)
+
+from .validators import (
+    required,
+    length,
+)
+
+from .schema import (
+    array,
+    dictionary,
+)
+
+from .exc import (
+    SchematecError,
+    ConvertationError,
+    ValidationError,
+    SchemaError,
+)
+
+__all__ = [
+    'integer',
+    'number',
+    'string',
+    'boolean',
+    'required',
+    'length',
+    'array',
+    'dictionary',
+    'SchematecError',
+    'ConvertationError',
+    'ValidationError',
+    'SchemaError',
+]
+
+__version__ = '0.3.2'
