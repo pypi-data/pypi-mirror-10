0d89b38811d5958f8e35bdcfec06d54520711149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:54 +0000

    Add package bu-cascade/1.81/bu_cascade-1.81.zip

---
+++ packages/bu-cascade/bu_cascade-1.81.zip/bu_cascade-1.81/bu_cascade/assets/block.py
@@ -0,0 +1,14 @@
+__author__ = 'ces55739'
+
+from asset import Asset
+from structured_list import List
+
+class Block(Asset):
+
+    def __init__(self, ws_connector, identifier):
+        super(self.__class__, self).__init__(ws_connector)
+        self.asset_type = "block"
+        self.identifier = identifier
+
+    def structured_data(self):
+        return List(self.asset['asset']['xhtmlDataDefinitionBlock'])##['structuredData']['structuredDataNodes']['structuredDataNode'])
