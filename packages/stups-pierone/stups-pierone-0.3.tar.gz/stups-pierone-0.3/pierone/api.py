58e9f1f3bfabcd40dc3c2068f6b299c36ee94a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:53 +0000

    Add package stups-pierone/0.3/stups-pierone-0.3.tar.gz

---
+++ packages/stups-pierone/stups-pierone-0.3.tar.gz/stups-pierone-0.3/pierone/api.py
@@ -0,0 +1,29 @@
+import codecs
+import json
+import os
+from clickclick import Action
+import requests
+import yaml
+from zign.api import get_named_token
+
+
+def docker_login(url, realm, name, user, password, token_url=None, use_keyring=True):
+    with Action('Getting OAuth2 token "{}"..'.format(name)):
+        token = get_named_token(['uid'], realm, name, user, password, url=token_url, use_keyring=use_keyring)
+    access_token = token.get('access_token')
+    path = os.path.expanduser('~/.dockercfg')
+    try:
+        with open(path) as fd:
+            dockercfg = yaml.safe_load(fd)
+    except:
+        dockercfg = {}
+    basic_auth = codecs.encode('oauth2:{}'.format(access_token).encode('utf-8'), 'base64').strip().decode('utf-8')
+    dockercfg[url] = {'auth': basic_auth,
+                      'email': 'no-mail-required@example.org'}
+    with Action('Storing Docker client configuration in {}..'.format(path)):
+        with open(path, 'w') as fd:
+            json.dump(dockercfg, fd)
+
+
+def request(url, path, access_token):
+    return requests.get(url + path, headers={'Authorization': 'Bearer {}'.format(access_token)}, timeout=10)
