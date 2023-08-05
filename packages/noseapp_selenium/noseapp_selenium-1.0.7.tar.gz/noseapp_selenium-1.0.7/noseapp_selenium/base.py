6de04d601d1bf749783b27d64505e04d71811c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:49 +0000

    Add package noseapp_selenium/1.0.7/noseapp_selenium-1.0.7.tar.gz

---
+++ packages/noseapp_selenium/noseapp_selenium-1.0.7.tar.gz/noseapp_selenium-1.0.7/noseapp_selenium/base.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from functools import wraps
+from urllib2 import URLError
+
+from noseapp.core import ExtensionInstaller
+from noseapp.utils.common import waiting_for
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from noseapp_selenium import drivers
+from noseapp_selenium.proxy import ProxyObject
+from noseapp_selenium.query import QueryProcessor
+
+
+logger = logging.getLogger(__name__)
+
+
+_DRIVER_TO_CAPABILITIES = {
+    drivers.OPERA: DesiredCapabilities.OPERA,
+    drivers.CHROME: DesiredCapabilities.CHROME,
+    drivers.FIREFOX: DesiredCapabilities.FIREFOX,
+    drivers.PHANTOMJS: DesiredCapabilities.PHANTOMJS,
+    drivers.IE: DesiredCapabilities.INTERNETEXPLORER,
+}
+
+DEFAULT_IMPLICITLY_WAIT = 30
+DEFAULT_MAXIMIZE_WINDOW = True
+DEFAULT_POLLING_TIMEOUT = None
+DEFAULT_DRIVER = drivers.CHROME
+
+GET_DRIVER_TIMEOUT = 10
+GET_DRIVER_SLEEP = 0.5
+
+
+class SeleniumExError(BaseException):
+    pass
+
+
+def get_capabilities(driver_name):
+    try:
+        return _DRIVER_TO_CAPABILITIES[driver_name]
+    except KeyError:
+        raise SeleniumExError(
+            'capabilities for driver "{}" is not found'.find(driver_name),
+        )
+
+
+def setup_config(f):
+    """
+    Setup config to driver and apply settings
+    """
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        driver = f(self, *args, **kwargs)
+
+        driver.config = DriverConfig(
+            driver,
+            implicitly_wait=self._implicitly_wait,
+            maximize_window=self._maximize_window,
+            polling_timeout=self._polling_timeout,
+        )
+        driver.config.apply()
+
+        return ProxyObject(driver)
+
+    return wrapper
+
+
+class DriverConfig(object):
+
+    def __init__(self, driver,
+                 implicitly_wait=DEFAULT_IMPLICITLY_WAIT,
+                 maximize_window=DEFAULT_MAXIMIZE_WINDOW,
+                 polling_timeout=DEFAULT_POLLING_TIMEOUT):
+        self.__driver = driver
+
+        self.IMPLICITLY_WAIT = implicitly_wait
+        self.MAXIMIZE_WINDOW = maximize_window
+        self.POLLING_TIMEOUT = polling_timeout
+
+    def apply(self):
+        self.init_query()
+
+        self.apply_implicitly_wait()
+        self.apply_maximize_window()
+
+    def apply_implicitly_wait(self):
+        if self.IMPLICITLY_WAIT is not None:
+            self.__driver.implicitly_wait(self.IMPLICITLY_WAIT)
+        else:
+            self.__driver.IMPLICITLY_WAIT = 0
+
+    def apply_maximize_window(self):
+        if self.MAXIMIZE_WINDOW:
+            self.__driver.maximize_window()
+
+    def init_query(self):
+        self.__driver.query = QueryProcessor(self.__driver)
+
+
+class SeleniumEx(object):
+    """
+    Initialize selenium extension for noseapp
+    """
+
+    name = 'selenium'
+    config_key = 'SELENIUM_EX'
+
+    def __init__(
+            self,
+            config,
+            use_remote=False,
+            driver_name=DEFAULT_DRIVER,
+            maximize_window=DEFAULT_MAXIMIZE_WINDOW,
+            implicitly_wait=DEFAULT_IMPLICITLY_WAIT,
+            polling_timeout=DEFAULT_POLLING_TIMEOUT):
+        self._config = config
+        self._use_remote = use_remote
+        self._driver_name = driver_name.lower()
+        self._maximize_window = maximize_window
+        self._implicitly_wait = implicitly_wait
+        self._polling_timeout= polling_timeout
+
+        logger.debug(
+            'Selenium-EX initialize. Config: {}, Use Remote: {}, Driver name: {}'.format(
+                config,
+                'Yes' if use_remote else 'No',
+                driver_name,
+            ),
+        )
+
+    @classmethod
+    def install(cls, app):
+        """
+        Automatic installation from app instance
+
+        :param app: instance of application
+        """
+        config = app.config.get(cls.config_key, {})
+        options = config.pop('OPTIONS', {})
+        installer = ExtensionInstaller(cls, (config, ), options)
+        app.shared_extension(cls=installer)
+        return installer
+
+    @property
+    def config(self):
+        return self._config
+
+    @setup_config
+    def remote(self):
+        remote_config = self._config.get('REMOTE_WEBDRIVER')
+
+        if not remote_config:
+            raise SeleniumExError('remote web driver settings not found')
+
+        logger.debug('Remote config: {}'.format(str(remote_config)))
+
+        options = remote_config.get('options', {})
+        capabilities = get_capabilities(self._driver_name)
+        capabilities.update(
+            remote_config['capabilities'][self._driver_name],
+        )
+
+        return drivers.RemoteWebDriver(
+            desired_capabilities=capabilities,
+            **options
+        )
+
+    @setup_config
+    def ie(self):
+        ie_config = self._config.get('IE_WEBDRIVER')
+
+        if not ie_config:
+            raise SeleniumExError('ie web driver settings not found')
+
+        logger.debug('Ie config: {}'.format(str(ie_config)))
+
+        return drivers.IeWebDriver(**ie_config)
+
+    @setup_config
+    def chrome(self):
+        chrome_config = self._config.get('CHROME_WEBDRIVER')
+
+        if not chrome_config:
+            raise SeleniumExError('google chrome web driver settings not found')
+
+        logger.debug('Chrome config: {}'.format(str(chrome_config)))
+
+        return drivers.ChromeWebDriver(**chrome_config)
+
+    @setup_config
+    def firefox(self):
+        firefox_config = self._config.get('FIREFOX_WEBDRIVER', {})
+
+        logger.debug('Firefox config: {}'.format(str(firefox_config)))
+
+        return drivers.FirefoxWebDriver(**firefox_config)
+
+    @setup_config
+    def phantom(self):
+        phantom_config = self._config.get('PHANTOMJS_WEBDRIVER')
+
+        if not phantom_config:
+            raise SeleniumExError('phantom js web driver settings not found')
+
+        logger.debug('PhantomJS config: {}'.format(str(phantom_config)))
+
+        return drivers.PhantomJSWebDriver(**phantom_config)
+
+    @setup_config
+    def opera(self):
+        opera_config = self._config.get('OPERA_WEBDRIVER')
+
+        if not opera_config:
+            raise SeleniumExError('opera web driver settings not found')
+
+        logger.debug('Opera config: {}'.format(str(opera_config)))
+
+        return drivers.OperaWebDriver(**opera_config)
+
+    def _get_local_driver(self):
+        driver = getattr(self, self._driver_name, None)
+
+        if driver:
+            return driver()
+
+        raise SeleniumExError(
+            'incorrect driver name "{}"'.format(self._driver_name),
+        )
+
+    def get_driver(self,
+                   driver_name=None,
+                   timeout=GET_DRIVER_TIMEOUT,
+                   sleep=GET_DRIVER_SLEEP):
+        if driver_name is not None:
+            self._driver_name = driver_name
+
+        def get_driver(func):
+            try:
+                return func()
+            except URLError:
+                return None
+
+        if self._use_remote:
+            driver = waiting_for(
+                lambda: get_driver(self.remote),
+                timeout=timeout,
+                sleep=sleep,
+            )
+        else:
+            driver = waiting_for(
+                lambda: get_driver(self._get_local_driver),
+                timeout=timeout,
+                sleep=sleep,
+            )
+
+        return driver
---
+++ packages/noseapp_selenium/noseapp_selenium-1.0.7.tar.gz/noseapp_selenium-1.0.7/noseapp_selenium/tools.py
@@ -0,0 +1,155 @@
+# -*- coding: utf8 -*-
+
+import time
+from functools import wraps
+
+from noseapp.utils.common import TimeoutException
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def make_object(web_element):
+    """
+    Convert web element to object.
+
+    Example:
+
+        input = driver.find_element...
+        value = make_object(input).value
+        css_value = make_object(input).css.background_image
+
+    :type web_element: selenium.webdriver.remote.webdriver.WebElement
+    """
+    # TODO: __setattr__
+    class WebElementToObject(object):
+
+        def __init__(self, web_element):
+            self.__web_element = web_element
+
+        @property
+        def css(self):
+            return WebElementCssToObject(self.__web_element)
+
+        def __getattr__(self, item):
+            atr = self.__web_element.get_attribute(item)
+
+            if atr:
+                return atr
+
+            raise AttributeError('{} "{}"'.format(repr(self.__web_element), item))
+
+    class WebElementCssToObject(object):
+
+        def __init__(self, web_element):
+            self.__web_element = web_element
+
+        def __getattr__(self, item):
+            return self.__web_element.value_of_css_property(
+                item.replace('_', '-'),
+            )
+
+    return WebElementToObject(web_element)
+
+
+def get_driver_from_web_element(web_element):
+    """
+    :type web_element: selenium.webdriver.remote.webdriver.WebElement
+    """
+    if hasattr(web_element, 'orig'):
+        web_element = web_element.orig()
+
+    if isinstance(web_element, WebElement):
+        driver = web_element._parent
+    else:
+        driver = web_element
+
+    if isinstance(driver, WebDriver):
+        return driver
+
+    return get_driver_from_web_element(driver)
+
+
+def get_driver_from_query(query):
+    """
+    :param query: instance of QueryProcessor
+    """
+    return get_driver_from_web_element(query._client)
+
+
+def get_config(client):
+    """
+    Get config from WebDriver instance
+
+    :param client: WebDriver or WebElement
+    """
+    driver = get_driver_from_web_element(client)
+
+    return driver.config
+
+
+def polling(callback=None, timeout=30, sleep=0.01):
+    """
+    Do sleep while wrapped function will be raised
+    exception of WebDriverException class.
+
+    Use timeout param for setting max seconds to waiting.
+    This function will be used like decorator if callback is None.
+    """
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            t_start = time.time()
+
+            while time.time() <= t_start + timeout:
+                try:
+                    return f(*args, **kwargs)
+                except WebDriverException:
+                    time.sleep(sleep)
+                    continue
+            else:
+                raise
+
+        return wrapped
+
+    if callback is not None:
+        return wrapper(callback)
+
+    return wrapper
+
+
+class ReRaiseWebDriverException(BaseException):
+    pass
+
+
+def re_raise_wd_exc(callback=None, exc_cls=ReRaiseWebDriverException, message=None):
+    """
+    Except WebDriverException and re raising to custom exception.
+    Also, TimeoutException will be excepting for re raise polling.
+    """
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except (TimeoutException, WebDriverException) as e:
+                raise exc_cls(
+                    u"""
+    Re raise web driver exception:
+
+    * Message: {}
+    * Original exception class: {}
+    * Original message: {}
+                    """.format(
+                        message or '',
+                        e.__class__.__name__,
+                        e.message,
+                    ),
+                )
+
+        return wrapped
+
+    if callable(callback):
+        return wrapper(callback)
+
+    return wrapper
