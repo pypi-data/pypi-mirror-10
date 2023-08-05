d4360ea35756a071d9400955e914e4fce36d5802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:19 +0000

    Add package join/0.0.1/join-0.0.1.tar.gz

---
+++ packages/join/join-0.0.1.tar.gz/join-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+from distutils.core import setup
+
+setup(name='join',
+      packages=['join'],
+      version='0.0.1',
+      description='SQL-style joins for iterables.',
+      author='Stuart Owen',
+      author_email='stuart.owen@gmail.com',
+      url='https://github.com/StuartAxelOwen/join',
+      download_url='https://github.com/StuartAxelOwen/join/archive/0.1.zip',
+      keywords=['join', 'joins', 'merge', 'merges', 'list join', 'iterable join'],
+      )
