cd431aa6e3db6104559af27bd3f131020f9932b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:10 +0000

    Add package noseapp/1.1.3/noseapp-1.1.3.tar.gz

---
+++ packages/noseapp/noseapp-1.1.3.tar.gz/noseapp-1.1.3/noseapp/plugins/configure.py
@@ -0,0 +1,70 @@
+
+import os
+
+from noseapp.plugins.base import AppPlugin
+
+
+class AppConfigurePlugin(AppPlugin):
+
+    name = 'app-configure'
+
+    def options(self, parser, env=os.environ):
+        super(AppConfigurePlugin, self).options(parser, env=env)
+
+        parser.add_option(
+            '--app-processes',
+            dest='app_processes',
+            default=0,
+            type=int,
+            help='Num of processes for running. one process == one suite.'
+        )
+        parser.add_option(
+            '--gevent',
+            dest='gevent_pool',
+            default=0,
+            type=int,
+            help='Num of greenlets for running. One greenlet == one suite.'
+        )
+        parser.add_option(
+            '--greenlets',
+            dest='gevent_greenlets',
+            default=0,
+            type=int,
+            help='Num of greenlets for running. One greenlet == one test case.'
+        )
+        parser.add_option(
+            '--threads',
+            dest='thread_pool',
+            default=0,
+            type=int,
+            help='Num of threads for running. If app processes is set, one thread == one test case, '
+                 'else one thread == one suite.'
+        )
+        parser.add_option(
+            '--ls',
+            dest='ls',
+            action='store_true',
+            default=False,
+            help='Show suites tree.'
+        )
+        parser.add_option(
+            '--doc',
+            dest='doc',
+            action='store_true',
+            default=False,
+            help='Show docstring of test cases.'
+        )
+        parser.add_option(
+            '-t', '--test',
+            dest='run_test',
+            default='',
+            type=str,
+            help='Test case or suite name for running.'
+        )
+
+        try:  # if app instance does not pushed to plugin,
+            # AttributeError will be passed.
+            self.app.parser = parser
+            self.app.add_options()
+        except AttributeError:
+            pass
