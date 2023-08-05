673b033fc753328a71119d17f043b817f02a0695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:57 +0000

    Add package partd/0.2.1/partd-0.2.1.tar.gz

---
+++ packages/partd/partd-0.2.1.tar.gz/partd-0.2.1/partd/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from .file import File
+from .encode import Encode
+from .pickle import Pickle
+from .python import Python
+from .compressed import *
+from .utils import ignoring
+with ignoring(ImportError):
+    from .numpy import Numpy
+with ignoring(ImportError):
+    from .pandas import PandasColumns, PandasBlocks
+with ignoring(ImportError):
+    from .zmq import Shared, Server
