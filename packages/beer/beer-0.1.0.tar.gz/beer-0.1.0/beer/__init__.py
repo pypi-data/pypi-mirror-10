c03b27e7d559730ecd165fc09eacb116e2671ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:11 +0000

    Add package beer/0.1.0/beer-0.1.0.tar.gz

---
+++ packages/beer/beer-0.1.0.tar.gz/beer-0.1.0/beer/__init__.py
@@ -0,0 +1,9 @@
+import time
+
+class SinTaxError(Exception):
+	def __init__(self,message,errors=None):
+		super().__init__(message)
+		self.errors = errors
+
+if time.localtime().tm_hour < 7:
+	raise SinTaxError("It is not even 5 o'clock")
