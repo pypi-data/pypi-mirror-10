2866cfc051561cea99ad982d213dcce489e3cb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package python-jose/0.1.2/python-jose-0.1.2.tar.gz

---
+++ packages/python-jose/python-jose-0.1.2.tar.gz/python-jose-0.1.2/jose/utils.py
@@ -0,0 +1,18 @@
+import base64
+
+
+def base64url_decode(input):
+    rem = len(input) % 4
+
+    if rem > 0:
+        input += b'=' * (4 - rem)
+
+    return base64.urlsafe_b64decode(input)
+
+
+def base64url_encode(input):
+    return base64.urlsafe_b64encode(input).replace(b'=', b'')
+
+
+def timedelta_total_seconds(delta):
+    return delta.days * 24 * 60 * 60 + delta.seconds
