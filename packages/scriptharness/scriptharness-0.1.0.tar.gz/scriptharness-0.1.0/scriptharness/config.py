8261ff31b4884bcb2c9079671de9610484e8499b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:34 +0000

    Add package scriptharness/0.1.0/scriptharness-0.1.0.tar.gz

---
+++ packages/scriptharness/scriptharness-0.1.0.tar.gz/scriptharness-0.1.0/scriptharness/config.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The goal of `flexible configuration` is to make each script useful in a
+variety of contexts and environments.
+
+Attributes:
+  LOGGER_NAME (str): logging.getLogger name
+
+  SCRIPTHARNESS_INITIAL_CONFIG (dict): These key/value pairs are available
+    in all scriptharness scripts.
+"""
+from __future__ import absolute_import, division, print_function, \
+                       unicode_literals
+import argparse
+from copy import deepcopy
+import json
+import logging
+import os
+import requests
+from scriptharness.actions import Action
+from scriptharness.exceptions import ScriptHarnessException, to_unicode
+from scriptharness.structures import iterate_pairs
+import six
+import six.moves.urllib as urllib
+import sys
+
+
+LOGGER_NAME = "scriptharness.config"
+SCRIPTHARNESS_INITIAL_CONFIG = {
+    "scriptharness_base_dir": six.text_type(os.getcwd()),
+    "scriptharness_work_dir":
+        "%(scriptharness_base_dir)s{}build".format(os.sep),
+    "scriptharness_artifact_dir":
+        "%(scriptharness_base_dir)s{}artifacts".format(os.sep),
+}
+
+
+# parse_config_file() {{{1
+def parse_config_file(path):
+    """Read a config file and return a dictionary.
+    For now, only support json.
+
+    Args:
+      path (str): path or url to config file.
+
+    Returns:
+      config (dict): the parsed json dict.
+
+    Raises:
+      scriptharness.exceptions.ScriptHarnessException: if the path is
+        unreadable or not valid json.
+    """
+    if is_url(path):
+        path = download_url(path)
+    # py3 may throw FileNotFoundError or IOError; both inherit OSError.
+    # py2 throws IOError, which doesn't inherit OSError.
+    if six.PY3:
+        exception = OSError
+    else:
+        exception = IOError
+    try:
+        with open(path) as filehandle:
+            config = dict(json.load(filehandle))
+    except exception as exc_info:
+        raise ScriptHarnessException(
+            "Can't open path %s!" % path, exc_info
+        )
+    except ValueError as exc_info:
+        raise ScriptHarnessException(
+            "Can't parse json in %s!" % path, exc_info
+        )
+    return config
+
+
+def get_filename_from_url(url):
+    """Determine the filename of a file from its url.
+
+    Args:
+      url (str): the url to parse
+
+    Returns:
+      name (str): the name of the file
+    """
+    parsed = urllib.parse.urlparse(url)
+    if parsed.path != '':
+        return parsed.path.rstrip('/').rsplit('/', 1)[-1]
+    else:
+        return parsed.netloc
+
+
+def is_url(resource):
+    """Is it a url?
+
+    .. note:: This function will return False for `file://` strings
+
+    Args:
+      resource (str): possible url
+
+    Returns:
+      bool: True if it's a url, False otherwise.
+    """
+    parsed = urllib.parse.urlparse(resource)
+    if parsed.scheme and parsed.netloc:
+        return True
+    return False
+
+
+def download_url(url, path=None, timeout=None):
+    """Download a url to a path.
+
+    Args:
+      url (str): the url to download
+      path (str, optional): the path to write the contents to.
+      timeout (float, optional): how long to wait before timing out.
+
+    Returns:
+      path (str): the path to the downloaded file.
+
+    Raises:
+      scriptharness.exceptions.ScriptHarnessException: if there are download
+        issues, or if we can't write to path.
+    """
+    if path is None:
+        path = get_filename_from_url(url)
+    if timeout is None:
+        timeout = 10
+    try:
+        session = requests.Session()
+        session.mount(url, requests.adapters.HTTPAdapter(max_retries=5))
+        response = session.get(url, timeout=timeout, stream=True)
+        with open(path, 'wb') as filehandle:
+            for chunk in response.iter_content(  # pragma: no branch
+                    chunk_size=1024):
+                if chunk:  # pragma: no branch
+                    filehandle.write(chunk)
+                    filehandle.flush()
+        return path
+    except requests.exceptions.RequestException as exc_info:
+        raise ScriptHarnessException("Error downloading from url %s" % url,
+                                     exc_info)
+    except IOError as exc_info:
+        raise ScriptHarnessException(
+            "Error writing downloaded contents to path %s" % path,
+            exc_info
+        )
+
+
+# get_parser() {{{1
+def get_list_actions_string(action_name, enabled):
+    """Build a string for --list-actions output.
+
+    Args:
+      action_name (str):  name of the action
+      enabled (bool): whether the action is enabled by default
+
+    Returns:
+      string (str): a line of --list-actions output.
+    """
+    string = "  "
+    if enabled:
+        string = "* "
+    string += action_name
+    return string
+
+def get_action_parser(all_actions):
+    """Create an action option parser from the action list.
+
+    Actions to run are specified as the argparse.REMAINDER options.
+
+    Args:
+      all_actions (iterable): this is either all Action objects for the
+        script, or a data structure of pairs of action_name:enabled to pass
+        to iterate_pairs().
+
+      **kwargs: additional kwargs for ArgumentParser
+
+    Returns:
+      ArgumentParser with action options
+    """
+    parser = argparse.ArgumentParser(add_help=False)
+    message = []
+    choices = []
+    for action in all_actions:
+        if isinstance(action, Action):
+            choices.append(action.name)
+            message.append(
+                get_list_actions_string(action.name, action.enabled)
+            )
+        else:
+            message = []
+            choices = []
+            for name, enabled in iterate_pairs(all_actions):
+                message.append(get_list_actions_string(name, enabled))
+                choices.append(name)
+            break
+    def list_actions():
+        """Helper function to list all actions (enabled shown with a '*')"""
+        print(os.linesep.join(message))
+        sys.exit(0)
+    parser.add_argument(
+        "--list-actions", action='store_const', const=list_actions,
+        dest="scriptharness_volatile_list_actions",
+        help="List all actions (default prepended with '*') and exit."
+    )
+    parser.add_argument(
+        "--actions", nargs='+', choices=choices, metavar="ACTION",
+        dest="scriptharness_volatile_actions",
+        help="Specify the actions to run."
+    )
+    return parser
+
+def get_config_parser():
+    """Create a config option parser.
+
+    Args:
+      kwargs: additional kwargs for ArgumentParser
+
+    Returns:
+      ArgumentParser with config options
+    """
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        '--config-file', '--cfg', '-c', action='append', dest='config_files',
+        metavar="CONFIG_FILE", help="Specify required config files/urls"
+    )
+    parser.add_argument(
+        '--opt-config-file', '--opt-cfg', action='append',
+        dest='opt_config_files', metavar="CONFIG_FILE",
+        help="Specify optional config files/urls"
+    )
+    parser.add_argument(
+        '--dump-config', action='store_true',
+        dest="scriptharness_volatile_dump_config",
+        help="Log the built configuration and exit."
+    )
+    return parser
+
+
+def get_parser(all_actions=None, parents=None, **kwargs):
+    """Create a script option parser.
+
+    Args:
+      parents (list, optional): ArgumentParsers to set as parents of the parser
+      **kwargs: additional kwargs for ArgumentParser
+
+    Returns:
+      ArgumentParser with config options
+    """
+    if parents is None:
+        parents = []
+        if all_actions:
+            parents.append(get_action_parser(all_actions))
+        parents.append(get_config_parser())
+    parser = argparse.ArgumentParser(parents=parents, **kwargs)
+    return parser
+
+
+def parse_args(parser, cmdln_args=None):
+    """Build the parser and parse the commandline args.
+
+    Args:
+      parser (ArgumentParser): specify the parser to use
+      cmdln_args (optional): override the commandline args with these
+
+    Returns:
+      tuple(ArgumentParser, parsed_args)
+    """
+    args = []
+    if cmdln_args is not None:  # pragma: no branch
+        args.append(cmdln_args)
+    parsed_args = parser.parse_args(*args)
+    if hasattr(parsed_args, 'scriptharness_volatile_list_actions') and \
+            callable(parsed_args.scriptharness_volatile_list_actions):
+        parsed_args.scriptharness_volatile_list_actions()
+    return parsed_args
+
+
+# build_config {{{1
+
+def update_dirs(config, max_depth=2):
+    """Directory paths for the script are defined in config.
+    Absolute paths help avoid chdir issues.
+
+    `scriptharness_base_dir` (or any other directory path, or any config value)
+    can be overridden during build_config().  Defining the directory paths as
+    formattable strings is configurable but not overly complex.
+
+    Any key in `config` named scriptharness_SOMETHING_dir will be % formatted
+    with the other dirs as the replacement dictionary.
+
+    Args:
+      config (dict): the config to parse for scriptharness_SOMETHING_dir keys.
+    """
+    repl_dict = {}
+    for key, value in config.items():
+        if key.startswith("scriptharness_") and key.endswith("_dir"):
+            repl_dict[key] = value
+    # Make a couple expansion passes, in case a dir is based on another dir
+    # with a % formatting string.
+    for _ in range(max_depth):
+        for key in repl_dict:
+            repl_dict[key] = repl_dict[key] % repl_dict
+    config.update(repl_dict)
+
+def build_config(parser, parsed_args, initial_config=None):
+    """Build a configuration dict from the parser and initial config.
+
+    The configuration is built in this order:
+
+      * parser defaults
+      * initial_config
+      * parsed_args.config_files, in order
+      * parsed_args.opt_config_files, in order, if they exist
+      * non-default parser args (cmdln_args)
+
+    So the commandline args can override everything else, as long as there are
+    options to do so. (Commandline args will need to be a subset of the parser
+    args).  The final configuration file can override everything but the
+    commandline args, and its config isn't restricted as a subset of the
+    parser options.
+
+    Args:
+      parser (ArgumentParser): the parser used to parse_args()
+      parsed_args (argparse Namespace): the results of parse_args()
+      initial_config (dict, optional): initial configuration to set before
+        commandline args
+    """
+    config = deepcopy(SCRIPTHARNESS_INITIAL_CONFIG)
+    cmdln_config = {}
+    resources = {}
+    initial_config = initial_config or {}
+    logger = logging.getLogger(LOGGER_NAME)
+    for key, value in parsed_args.__dict__.items():
+        if key.startswith('scriptharness_') and '_volatile_' in key:
+            continue
+        if key in ('config_files', 'opt_config_files'):
+            resources.setdefault(key, value or [])
+            continue
+        if parser.get_default(key) == value:
+            config[to_unicode(key)] = to_unicode(value)
+        else:
+            cmdln_config[key] = value
+    config.update(initial_config)
+    for resource in resources.get('config_files', []):
+        config.update(parse_config_file(resource))
+    for resource in resources.get('opt_config_files', []):
+        try:
+            config.update(parse_config_file(resource))
+        except ScriptHarnessException:
+            logger.info("Can't read optional config file %s; skipping.",
+                        resource)
+    if cmdln_config:
+        config.update(cmdln_config)
+    update_dirs(config)
+    return config
---
+++ packages/scriptharness/scriptharness-0.1.0.tar.gz/scriptharness-0.1.0/scriptharness/log.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The goal of `full logging` is to be able to debug problems purely through
+the log.
+
+Attributes:
+  DEFAULT_DATEFMT (str): default logging date format
+  DEFAULT_FMT (str): default logging format
+  DEFAULT_LEVEL (int): default logging level
+"""
+
+from __future__ import absolute_import, division, print_function, \
+                       unicode_literals
+from copy import deepcopy
+import logging
+import os
+from scriptharness.commands import make_parent_dir
+from scriptharness.exceptions import ScriptHarnessException
+import six
+
+
+DEFAULT_DATEFMT = '%H:%M:%S'
+DEFAULT_FMT = '%(asctime)s %(levelname)8s - %(message)s'
+DEFAULT_LEVEL = logging.INFO
+
+
+# UnicodeFormatter {{{1
+class UnicodeFormatter(logging.Formatter):
+    """Subclass logging.Formatter to handle unicode strings in py2.
+
+    Attributes:
+      encoding (str): defaults to utf-8.
+    """
+    encoding = 'utf-8'
+
+    def format(self, record):
+        string = super(UnicodeFormatter, self).format(record)
+        if six.PY2 and isinstance(string, six.text_type):
+            string = string.encode(self.encoding, 'replace')
+        return string
+
+
+# logging helper methods {{{1
+def get_formatter(fmt=DEFAULT_FMT, datefmt=DEFAULT_DATEFMT):
+    """Create a unicode-friendly formatter to add to logging handlers.
+
+    Args:
+      fmt (str, optional): logging message format.
+      datefmt (str, optional): date format for the log message.
+
+    Returns:
+      UnicodeFormatter to add to a handler - handler.setFormatter(formatter)
+    """
+    formatter = UnicodeFormatter(fmt=fmt, datefmt=datefmt)
+    return formatter
+
+
+def prepare_simple_logging(path, mode='w', logger_name='', level=DEFAULT_LEVEL,
+                           formatter=None):
+    """Create a unicode-friendly logger.
+
+    By default it'll create the root logger with a console handler; if passed
+    a path it'll also create a file handler.  Both handlers will have a
+    unicode-friendly formatter.
+
+    This function is intended to be called a single time.  If called
+    a second time, beware creating multiple console handlers or multiple
+    file handlers writing to the same file.
+
+    Args:
+      path (str, optional): path to the file log.  If this isn't set,
+        don't create a file handler.  Default ''
+      mode (char, optional): the mode to open the file log.  Default 'w'
+      logger_name (str, optional): the name of the logger to use. Default ''
+      level (int, optional): the level to log.  Default DEFAULT_LEVEL
+      formatter (Formatter, optional): a logging Formatter to use; to handle
+        unicode, subclass UnicodeFormatter.
+
+    Returns:
+        logger (Logger object).  This is also easily retrievable via
+            logging.getLogger(logger_name).
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    get_console_handler(logger=logger, level=level, formatter=formatter)
+    get_file_handler(path, logger=logger, mode=mode, level=level,
+                     formatter=formatter)
+    return logger
+
+
+def get_file_handler(path, level=logging.INFO, formatter=None,
+                     logger=None, mode='w'):
+    """Create a file handler to add to a logger.
+
+    Args:
+      path (str): the path to the logfile.
+      level (int, optional): logging level for the file.
+      formatter (logging.Formatter, optional): formatter to use for logs.
+      logger (logging logger, optional): logger to add the file handler to.
+      mode (str, optional): mode to open the file
+
+    Returns:
+      handler (logging.FileHandler):  This can be added to a logger
+      via logger.addHandler(handler)
+    """
+    make_parent_dir(path, level=logging.DEBUG)
+    if not formatter:
+        formatter = get_formatter()
+    handler = logging.FileHandler(path, mode)
+    handler.setLevel(level)
+    handler.setFormatter(formatter)
+    if logger:
+        logger.addHandler(handler)
+    return handler
+
+
+def get_console_handler(formatter=None, logger=None, level=logging.INFO):
+    """Create a stream handler to add to a logger.
+
+    Args:
+      formatter (logging.Formatter, optional): formatter to use for logs.
+      logger (logging logger, optional): logger to add the file handler to.
+      level (int, optional): logging level for the file.
+
+    Returns:
+      logging.StreamHandler handler.  This can be added to a logger
+      via logger.addHandler(handler)
+    """
+    if not formatter:
+        formatter = get_formatter()
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    handler.setFormatter(formatter)
+    if logger:
+        logger.addHandler(handler)
+    return handler
+
+
+# LogMethod decorator {{{1
+class LogMethod(object):
+    # pylint: disable=anomalous-backslash-in-string
+    """Wrapper decorator object for logging and error detection.
+    This is here as a shortcut to wrap functions with basic logging.
+
+    Attributes:
+
+      default_config (dict): contains the config defaults that can be
+        overridden via __init__ \**kwargs.  Changing default_config directly
+        may carry over to other decorated LogMethod functions!
+    """
+    default_config = {
+        'level': logging.INFO,
+        'error_level': logging.ERROR,
+        'logger_name': 'scriptharness.{func_name}',
+        'pre_msg': '%(func_name)s arguments were: %(args)s %(kwargs)s',
+        'post_success_msg': '%(func_name)s completed.',
+        'post_failure_msg': '%(func_name)s failed.',
+        'exception': None,
+        'detect_error_cb': None,
+    }
+
+    def __init__(self, func=None, **kwargs):
+        """Set instance attributes from the decorator.
+
+        Usage::
+
+          # with arguments
+          @LogMethod(foo='bar')
+          def decorated_function(...):
+              ...
+
+          # without arguments
+          @LogMethod()
+          def decorated_function2(...):
+              ...
+
+        In the first example, func will be decorated_function, and kwargs
+        will be {'foo': 'bar'}.  (Which will raise an exception, because
+        'foo' isn't in self.defaults.)
+
+        All of the self.defaults are overrideable via **kwargs or
+        subclassing and changing self.defaults.
+
+        func is optional because decorators are called differently when
+        they have arguments or not.  As long as the decorator-without-args
+        has the empty (), the function will be sent to LogMethod.__call__()
+
+        Args:
+          func (function, optional): This is the decorated function.
+          **kwargs: Contains any config options to override default_config
+        """
+        self.func = func
+        self.return_value = None
+        self.args = None
+        self.kwargs = None
+        self.repl_dict = {}
+        self.detected_errors = False
+        self.config = deepcopy(self.default_config)
+        messages = []
+        for key, value in kwargs.items():
+            if key not in self.config:
+                messages.append('Unknown key {} in kwargs!'.format(key))
+            self.config[key] = value
+        if kwargs.get('detect_error_cb') is not None and \
+                not callable(kwargs['detect_error_cb']):
+            messages.append('detect_error_cb not callable!')
+        if messages:
+            raise ScriptHarnessException(os.linesep.join(messages))
+
+    def __call__(self, func, *args, **kwargs):
+        # pylint: disable=anomalous-backslash-in-string
+        """Wrap the function call as a decorator.
+
+        When there are decorator arguments, \__call__ is only called once, at
+        decorator time.  args and kwargs only show up when func is called,
+        so we need to create and return a wrapping function.
+
+        Args:
+          func (function): this is the decorated function.
+          *args: the args from the wrapped function call.
+          **kwargs: the kwargs from the wrapped function call.
+        """
+        self.func = func
+        def wrapped_func(*args, **kwargs):
+            """This function replaces the decorated function.
+            """
+            self.args = args
+            self.kwargs = kwargs
+            self.set_repl_dict()
+            self.pre_func()
+            self.return_value = self.func(*self.args, **self.kwargs)
+            self.repl_dict['return_value'] = self.return_value
+            if self.config['detect_error_cb'] is not None:
+                self.detected_errors = self.config['detect_error_cb'].__call__(self)
+            self.post_func()
+            return self.return_value
+        return wrapped_func
+
+    def set_repl_dict(self):
+        """Create a replacement dictionary to format strings.
+
+        The log messages in pre_func() and post_func() require some additional
+        info.  Specify that info in the replacement dictionary.
+
+        Currently, set the following::
+
+          func_name: self.func.__name__
+          *args: the args passed to self.func()
+          **kwargs: the kwargs passed to self.func()
+
+        After running self.func, we'll also set return_value.
+        """
+        self.repl_dict = {
+            'args': self.args,
+            'kwargs': self.kwargs,
+        }
+        for name_var in ('__qualname__', '__name__'):
+            if hasattr(self.func, name_var):
+                self.repl_dict['func_name'] = getattr(self.func, name_var)
+
+    def pre_func(self):
+        """Log the function call before proceeding.
+
+        This method is split out for easier subclassing.
+        """
+        log = logging.getLogger(
+            self.config['logger_name'].format(**self.repl_dict)
+        )
+        log.log(self.config['level'], self.config['pre_msg'], self.repl_dict)
+
+    def post_func(self):
+        """Log the success message until we get an error detection callback.
+
+        This method is split out for easier subclassing.
+        """
+        log = logging.getLogger(
+            self.config['logger_name'].format(**self.repl_dict)
+        )
+        if self.detected_errors:
+            msg = self.config['post_failure_msg']
+            level = self.config['error_level']
+        else:
+            msg = self.config['post_success_msg']
+            level = self.config['level']
+        log.log(level, msg, self.repl_dict)
+        if self.detected_errors and self.config['exception']:
+            raise self.config['exception'](
+                self.config['post_failure_msg'].format(**self.repl_dict)
+            )
