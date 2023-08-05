d55ea6036c82871a6ce18d3a5cf3cf9517e28302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:24 +0000

    Add package yellow-sdk/0.2.6/yellow-sdk-0.2.6.tar.gz

---
+++ packages/yellow-sdk/yellow-sdk-0.2.6.tar.gz/yellow-sdk-0.2.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='yellow-sdk',
+      version="0.2.6",
+      description='Yellow Python SDK. A python module to easily integrate with the Yellow bitcoin payments API.',
+      url='http://yellowpay.co',
+      author='Eslam A. Hefnawy',
+      author_email='eslam@yellowpay.co',
+      license='MIT',
+      packages=['yellow'],
+      install_requires=[
+        'requests==2.4.3'
+      ],
+      zip_safe=False)
