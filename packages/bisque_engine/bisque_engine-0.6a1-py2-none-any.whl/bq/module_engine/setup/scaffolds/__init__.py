541b6fc12a33acf3aca57e8a483358c1f5a3aab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:33 +0000

    Add package bisque_engine/0.6a1/bisque_engine-0.6a1-py2-none-any.whl

---
+++ packages/bisque_engine/bisque_engine-0.6a1-py2-none-any.whl/bq/module_engine/setup/scaffolds/__init__.py
@@ -0,0 +1,15 @@
+from pyramid.scaffolds import PyramidTemplate
+from textwrap import dedent
+
+class PythonModuleTemplate(PyramidTemplate):
+    _template_dir = 'python_module'
+    summary = 'Create a basic python bisque module '
+    quiet = True
+
+    def post(self, command, output_dir, vars): # pragma: no cover
+        pass
+
+    def out(self, msg):
+        pass
+
+
