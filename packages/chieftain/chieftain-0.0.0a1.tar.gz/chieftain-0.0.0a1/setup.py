7fd9ec5e98866904a94f7bb668ebed8ebd4b2e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package chieftain/0.0.0a1/chieftain-0.0.0a1.tar.gz

---
+++ packages/chieftain/chieftain-0.0.0a1.tar.gz/chieftain-0.0.0a1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+]
+
+setup(
+    name='chieftain',
+    version='0.0.0a1',
+    description='',
+    author='Chive',
+    author_email='kim@smuzey.ch',
+    packages=find_packages(),
+    platforms=['OS Independent'],
+    install_requires=[],
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False,
+)
