bdb3be69de9f401af0b27b5a907da6c225751ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package whl.setup/0.2/whl.setup-0.2.tar.gz

---
+++ packages/whl.setup/whl.setup-0.2.tar.gz/whl.setup-0.2/setup.py
@@ -0,0 +1,38 @@
+"""
+
+
+
+"""
+
+
+from whl_setup import setup, Lazy
+
+@Lazy
+def long_description():
+    with open('README.rst') as fp:
+        return fp.read()
+
+
+if __name__ == '__main__':
+    setup(
+        name='whl.setup',
+        description='a pip based replacement for ez_setup and setup_requires',
+        long_description=long_description,
+        author='Ronny Pfannschmidt',
+        author_email='opensource+whl.setup@ronnypfannschmidt.de',
+        url='https://github.com/RonnyPfannschmidt/whl_setup',
+        use_scm_version=True,
+        setup_requires=[
+            'setuptools',
+            'setuptools_scm',
+        ],
+        entry_points={
+            'console_scripts': {
+                'whl_setup_script_install = whl_setup:install',
+            }
+
+        },
+        py_modules=[
+            'whl_setup',
+        ],
+    )
