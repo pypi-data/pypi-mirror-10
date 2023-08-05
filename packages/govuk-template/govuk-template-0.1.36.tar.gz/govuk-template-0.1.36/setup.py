bff63dc821be0511f1f8872e71d8d643a75ffd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:35 +0000

    Add package govuk-template/0.1.36/govuk-template-0.1.36.tar.gz

---
+++ packages/govuk-template/govuk-template-0.1.36.tar.gz/govuk-template-0.1.36/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+
+build_number = os.getenv('BUILD_NUMBER', '0')
+
+
+def read(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='govuk-template',
+    version='0.1.{}'.format(build_number),
+    description='The GOV.UK template for python',
+    url='https://github.com/LandRegistry/python-govuk-template',
+    license='MIT',
+    author='Ramin Vazir <ramin.vazir@digital.landregistry.gov.uk>,\
+            Michael Allen <michael@michaelallen.io>',
+    packages=[
+        'govuk_template.flask.assets',
+        'govuk_template.flask.mustache'
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+    install_requires=['flask', 'pystache'],
+)
