3cc9dc681a35f2899ad175cf155542f32e8c06b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package modoboa-admin-relaydomains/1.0.1/modoboa-admin-relaydomains-1.0.1.tar.gz

---
+++ packages/modoboa-admin-relaydomains/modoboa-admin-relaydomains-1.0.1.tar.gz/modoboa-admin-relaydomains-1.0.1/modoboa_admin_relaydomains/factories.py
@@ -0,0 +1,41 @@
+"""Factories."""
+
+import factory
+
+from modoboa.core.factories import PermissionFactory
+
+from . import models
+
+
+class ServiceFactory(factory.DjangoModelFactory):
+
+    """Factory to create Service instances."""
+
+    class Meta:
+        model = models.Service
+        django_get_or_create = ('name', )
+
+    name = 'dummy'
+
+
+class RelayDomainFactory(PermissionFactory):
+
+    """Factory to create RelayDomain instances."""
+
+    class Meta:
+        model = models.RelayDomain
+
+    target_host = 'external.host.tld'
+    enabled = True
+    verify_recipients = True
+    service = factory.SubFactory(ServiceFactory)
+
+
+class RelayDomainAliasFactory(PermissionFactory):
+
+    """Factory to create RelayDomainAlias instances."""
+
+    class Meta:
+        model = models.RelayDomainAlias
+
+    enabled = True
