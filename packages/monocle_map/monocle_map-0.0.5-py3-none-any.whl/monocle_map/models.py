0ed2d853e904fe680733109d0ec6e07253c0326f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package monocle_map/0.0.5/monocle_map-0.0.5-py3-none-any.whl

---
+++ packages/monocle_map/monocle_map-0.0.5-py3-none-any.whl/monocle_map/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from . import widgets
+
+CHOICES = (('YM', 'Яндекс Карта',), ('GM', 'Google Карта',))
+
+class Map(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=255, verbose_name='Заголовок')
+    map = widgets.LocationField(blank=False, null=False, verbose_name='Карта')
+    zoom = models.IntegerField(blank=False, null=False, default='14', verbose_name='Зум')
+    baloon = models.TextField(blank=True, verbose_name='Текст балуна')
+    markerImg = models.ImageField(upload_to='marker', blank=False, verbose_name='Картинка для метки')
+    mapType = models.CharField(max_length=2, choices=CHOICES, default='YM', verbose_name='Тип карты')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = u'Карты'
+        verbose_name = u'Карта'
