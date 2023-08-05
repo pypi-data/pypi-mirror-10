de5d596b939c0791cdbef8bb3c3b56bdc9b64c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package marshmallow-sqlalchemy/0.2.0/marshmallow-sqlalchemy-0.2.0.tar.gz

---
+++ packages/marshmallow-sqlalchemy/marshmallow-sqlalchemy-0.2.0.tar.gz/marshmallow-sqlalchemy-0.2.0/CHANGELOG.rst
@@ -0,0 +1,25 @@
+Changelog
+---------
+
+0.2.0 (2015-05-03)
+++++++++++++++++++
+
+Features:
+
+- Add ``field_for`` function for generating marshmallow Fields from SQLAlchemy mapped class properties.
+
+Support:
+
+- Docs: Add "Overriding generated fields" section to "Recipes".
+
+0.1.1 (2015-05-02)
+++++++++++++++++++
+
+Bug fixes:
+
+- Fix ``keygetter`` class Meta option.
+
+0.1.0 (2015-04-28)
+++++++++++++++++++
+
+- First release.
---
+++ packages/marshmallow-sqlalchemy/marshmallow-sqlalchemy-0.2.0.tar.gz/marshmallow-sqlalchemy-0.2.0/marshmallow_sqlalchemy/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from .schema import (
+    SchemaOpts,
+    ModelSchema,
+)
+
+from .convert import (
+    ModelConverter,
+    fields_for_model,
+    get_pk_from_identity,
+    property2field,
+    column2field,
+    field_for,
+)
+from .exceptions import ModelConversionError
+
+__version__ = '0.2.0'
+__license__ = 'MIT'
+
+__all__ = [
+    'ModelSchema',
+    'SchemaOpts',
+    'ModelConverter',
+    'fields_for_model',
+    'property2field',
+    'column2field',
+    'get_pk_from_identity',
+    'ModelConversionError',
+    'field_for',
+]
