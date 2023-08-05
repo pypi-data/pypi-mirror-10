8b5bc81467db6edaa99720fee0f7a09163ef9637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package configman/1.2.13/configman-1.2.13.tar.gz

---
+++ packages/configman/configman-1.2.13.tar.gz/configman-1.2.13/configman/commandline.py
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# this will be expanded in the future when additional command line libraries
+# are supported
+
+# at which point we want to make argparse the default, we will eliminate
+# this line
+import getopt as command_line
