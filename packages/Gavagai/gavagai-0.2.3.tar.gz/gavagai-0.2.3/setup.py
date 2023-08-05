8fb8df8e8e5db4bad9bc2bfb890e441191c03e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:40 +0000

    Add package Gavagai/0.2.3/gavagai-0.2.3.tar.gz

---
+++ packages/Gavagai/gavagai-0.2.3.tar.gz/gavagai-0.2.3/setup.py
@@ -0,0 +1,24 @@
+import io
+import re
+from setuptools import setup
+
+init_py = io.open('gavagai/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
+metadata['doc'] = re.findall('"""(.+)"""', init_py)[0]
+
+setup(
+    name='gavagai',
+    packages=['gavagai'],
+    version=metadata['version'],
+    description=metadata['doc'],
+    author=metadata['author'],
+    author_email=metadata['email'],
+    url=metadata['url'],
+    keywords = ['text-analysis', 'api', 'nlp'], 
+    license='The MIT License (MIT)',
+    install_requires=[
+        'requests>=2.7.0,<3.0',
+        'urllib3',
+        'ndg-httpsclient',
+    ]
+)
