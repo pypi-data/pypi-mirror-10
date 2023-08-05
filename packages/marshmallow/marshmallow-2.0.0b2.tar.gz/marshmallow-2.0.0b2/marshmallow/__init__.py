442c52ab0912d5321f653d81270ae655589b46e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package marshmallow/2.0.0b2/marshmallow-2.0.0b2.tar.gz

---
+++ packages/marshmallow/marshmallow-2.0.0b2.tar.gz/marshmallow-2.0.0b2/marshmallow/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from marshmallow.schema import (
+    Schema,
+    SchemaOpts,
+    MarshalResult,
+    UnmarshalResult,
+)
+from marshmallow.decorators import pre_dump, post_dump, pre_load, post_load
+from marshmallow.utils import pprint, missing
+from marshmallow.exceptions import MarshallingError, UnmarshallingError, ValidationError
+
+__version__ = '2.0.0b2'
+__author__ = 'Steven Loria'
+__license__ = 'MIT'
+
+__all__ = [
+    'Schema',
+    'SchemaOpts',
+    'pre_dump',
+    'post_dump',
+    'pre_load',
+    'post_load',
+    'pprint',
+    'MarshalResult',
+    'UnmarshalResult',
+    'MarshallingError',
+    'UnmarshallingError',
+    'ValidationError',
+    'missing',
+]
