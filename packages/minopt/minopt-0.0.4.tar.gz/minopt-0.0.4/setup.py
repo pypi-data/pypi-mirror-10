2e3533d9c5f63ecf84c9e772ce021582adb74c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package minopt/0.0.4/minopt-0.0.4.tar.gz

---
+++ packages/minopt/minopt-0.0.4.tar.gz/minopt-0.0.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='minopt',
+    version='0.0.4',
+    description='minimalistic option parser',
+    author='Ajay MT',
+    author_email='ajaymt@icloud.com',
+    url='http://github.com/ajaymt/minopt',
+    download_url='https://github.com/AjayMT/minopt/tarball/v0.0.4',
+    py_modules=['minopt'],
+    keywords='option-parser option parser optparse argparse command line',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ]
+)
