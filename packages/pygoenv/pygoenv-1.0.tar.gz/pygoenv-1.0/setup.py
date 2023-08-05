9db4527e969240c976d1c7e33468edaac2d7e326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:11 +0000

    Add package pygoenv/1.0/pygoenv-1.0.tar.gz

---
+++ packages/pygoenv/pygoenv-1.0.tar.gz/pygoenv-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name = "pygoenv",
+    version = "1.0",
+    packages = find_packages(),
+
+    install_requires = ['requests>=2.7.0'],
+
+    entry_points = {
+        'console_scripts': [ 'goenv = goenv:main' ],
+    },
+
+    # metadata for upload to PyPI
+    author = "Paul Woolcock",
+    author_email = "paul@woolcock.us",
+    description = "Simple environment manager for the Go programming language",
+    license = "MIT",
+    # keywords = "hello world example examples",
+    url = "https://github.com/pwoolcoc/goenv",
+)
+
+
