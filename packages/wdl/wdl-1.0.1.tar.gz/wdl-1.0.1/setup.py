283efc6b2306b32ba913d0b5fbd7addd7ddd55be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package wdl/1.0.1/wdl-1.0.1.tar.gz

---
+++ packages/wdl/wdl-1.0.1.tar.gz/wdl-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+version = '1.0.1'
+long_description = 'Runs WDL tasks and workflows locally'
+
+setup(
+  name='wdl',
+  version=version,
+  description=long_description,
+  author='Scott Frazer',
+  author_email='scott.d.frazer@gmail.com',
+  packages=['wdl'],
+  install_requires=[
+      "xtermcolor",
+      "nose"
+  ],
+  scripts={
+      'scripts/wdl',
+  },
+  license = 'MIT',
+  keywords = "Workflow, Task",
+  url = "http://github.com/broadinstitute/wdl",
+  classifiers=[
+      'License :: OSI Approved :: MIT License',
+      "Programming Language :: Python",
+      "Development Status :: 4 - Beta",
+      "Intended Audience :: Developers",
+      "Natural Language :: English",
+      "Topic :: Software Development :: Compilers"
+  ]
+)
