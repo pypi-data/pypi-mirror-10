a1c05468d94c50cfaa55b6a92effaad7adefe266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package f1/1.0.1/f1-1.0.1.zip

---
+++ packages/f1/f1-1.0.1.zip/f1-1.0.1/f1.py
@@ -0,0 +1,7 @@
+"""The first func"""
+def print_lol(the_list):
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item)
+        else:
+            print(item)
---
+++ packages/f1/f1-1.0.1.zip/f1-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+      name  = 'f1',
+      version = '1.0.1',
+      py_modules = ['f1'],
+      author = 'zz',
+      author_email = 'sdf@jd.com',
+      url = 'http://www.163.com',
+      description = 'a simple ',
+      
+      )
