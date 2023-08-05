657dec4d8df1f68110cea789ae2615f74f0f04bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package webhdfspy/0.2/webhdfspy-0.2.tar.gz

---
+++ packages/webhdfspy/webhdfspy-0.2.tar.gz/webhdfspy-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='webhdfspy',
+      version='0.2',
+      description='A wrapper library to access Hadoop HTTP REST API',
+      url='https://github.com/fsouto/webhdfspy',
+      author='Fabio Souto',
+      author_email='fabio@fabiosouto.me',
+      license='MIT',
+      packages=['webhdfspy'],
+      zip_safe=False)
