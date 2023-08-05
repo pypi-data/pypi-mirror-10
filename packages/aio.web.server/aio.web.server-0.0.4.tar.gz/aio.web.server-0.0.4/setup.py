46567cfd77c21ea3bcf6fc415468e3a45bebf6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:50 +0000

    Add package aio.web.server/0.0.4/aio.web.server-0.0.4.tar.gz

---
+++ packages/aio.web.server/aio.web.server-0.0.4.tar.gz/aio.web.server-0.0.4/setup.py
@@ -0,0 +1,68 @@
+"""
+aio.web.server
+"""
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = "0.0.4"
+
+install_requires = [
+    'setuptools',
+    'aio.core',
+    'aio.app',
+    'aio.http.server',
+    'aiohttp',
+    'aiohttp_jinja2']
+
+if sys.version_info < (3, 4):
+    install_requires += [
+        'asyncio']
+
+tests_require = install_requires + ['aio.testing']
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    'Detailed documentation\n'
+    + '**********************\n'
+    + '\n'
+    + read("README.rst")
+    + '\n')
+
+try:
+    long_description += (
+        '\n'
+        + read("aio", "web", "server", "README.rst")
+        + '\n')
+except FileNotFoundError:
+    pass
+
+
+setup(
+    name='aio.web.server',
+    version=version,
+    description="Aio web server",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='',
+    author='Ryan Northey',
+    author_email='ryan@3ca.org.uk',
+    url='http://github.com/phlax/aio.web.server',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['aio', "aio.web"],
+    include_package_data=True,
+    package_data={'': ['templates/*.html', 'templates/fragments/*.html', '*.rst']},
+    test_suite="aio.app.tests",
+    zip_safe=False,
+    tests_require=tests_require,
+    install_requires=install_requires,
+    entry_points={})
