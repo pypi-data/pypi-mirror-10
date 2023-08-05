beb1067529e3b2475b92a6d56ec0aa735289fda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:56 +0000

    Add package bravado/1.0.0-rc1/bravado-1.0.0-rc1.tar.gz

---
+++ packages/bravado/bravado-1.0.0-rc1.tar.gz/bravado-1.0.0-rc1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Copyright (c) 2013, Digium, Inc.
+# Copyright (c) 2014-2015, Yelp, Inc.
+
+import os
+
+from setuptools import setup
+
+import bravado
+
+setup(
+    name="bravado",
+    version=bravado.version,
+    license="BSD 3-Clause License",
+    description="Library for accessing Swagger-enabled API's",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       "README.rst")).read(),
+    author="Digium, Inc. and Yelp, Inc.",
+    author_email="opensource+bravado@yelp.com",
+    url="https://github.com/Yelp/bravado",
+    packages=["bravado"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        "bravado-core",
+        "crochet >= 1.4.0",
+        "fido >= 1.0.1",
+        "python-dateutil",
+        "requests",
+        "twisted >= 14.0.0",
+        "yelp_uri >= 1.0.1",
+    ],
+)
