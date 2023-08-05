a10c217767633a4b616e201749d5044e1e926f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package mongoext/0.2.2/mongoext-0.2.2.tar.gz

---
+++ packages/mongoext/mongoext-0.2.2.tar.gz/mongoext-0.2.2/mongoext/document.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+
+import mongoext.collection
+import mongoext.fields
+
+
+class MetaDocument(type):
+    def __new__(cls, name, bases, attrs):
+        fields = {}
+        for base in bases:
+            for attr, obj in vars(base).iteritems():
+                if issubclass(type(obj), mongoext.fields.Field):
+                    fields[attr] = obj
+        for attr, obj in attrs.iteritems():
+            if issubclass(type(obj), mongoext.fields.Field):
+                fields[attr] = obj
+        attrs['FIELDS'] = fields
+        return super(MetaDocument, cls).__new__(cls, name, bases, attrs)
+
+    def __init__(cls, name, bases, attrs):
+        for attr, obj in vars(cls).iteritems():
+            if issubclass(type(obj), mongoext.collection.Collection):
+                obj._model = cls
+        super(MetaDocument, cls).__init__(name, bases, attrs)
+
+
+class Document(object):
+    __metaclass__ = MetaDocument
+    FIELDS = None
+
+    _id = mongoext.fields.Field()
+
+    def __init__(self, **kw):
+        for name, obj in self.FIELDS.iteritems():
+            if name in kw:
+                try:
+                    setattr(self, name, obj(kw[name]))
+                except ValueError as e:
+                    raise ValueError('{}: {}'.format(e.message, name))
+            else:
+                setattr(self, name, None)
+
+    def save(self):
+        self.__init__(**vars(self))
+
+    def to_dict(self):
+        return {f: getattr(self, f, None) for f in self.FIELDS}
+
+    def __repr__(self):
+        return '<{}: {}>'.format(type(self).__name__, self._id)
