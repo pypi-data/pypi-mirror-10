6c00a4b458af0896bfede0352a008e11382edb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:54 +0000

    Add package Flask-LiveScript/0.1/Flask-LiveScript-0.1.tar.gz

---
+++ packages/Flask-LiveScript/Flask-LiveScript-0.1.tar.gz/Flask-LiveScript-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/urb/bin/env python
+
+from distutils.core import setup
+
+setup(name='Flask-LiveScript',
+    version='0.1',
+    description='implements the webassets filter for livescript',
+    author='Miguel Angel Gordian',
+    author_email="miguel.angel@codeandomexico.org",
+    url="https://github.com/zoek1/flask-filter-livescript",
+    packages=['livescript']
+    )
