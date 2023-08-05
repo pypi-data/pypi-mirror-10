a5695facdc27373425bf9b47dba0219bdb2b738a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:21 +0000

    Add package hypothesis/1.6.1/hypothesis-1.6.1.tar.gz

---
+++ packages/hypothesis/hypothesis-1.6.1.tar.gz/hypothesis-1.6.1/src/hypothesis/internal/compat.py
@@ -0,0 +1,104 @@
+# coding=utf-8
+
+# Copyright (C) 2013-2015 David R. MacIver (david@drmaciver.com)
+
+# This file is part of Hypothesis (https://github.com/DRMacIver/hypothesis)
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+# END HEADER
+
+# pylint: skip-file
+from __future__ import division, print_function, absolute_import, \
+    unicode_literals
+
+import sys
+import platform
+import importlib
+
+PY3 = sys.version_info[0] == 3
+BAD_PY3 = PY3 and (sys.version_info[1] <= 2)
+PYPY = platform.python_implementation() == 'PyPy'
+
+if PY3:
+    text_type = str
+    binary_type = bytes
+    hrange = range
+    ARG_NAME_ATTRIBUTE = 'arg'
+    integer_types = (int,)
+    hunichr = chr
+    from functools import reduce
+else:
+    text_type = unicode
+    binary_type = str
+
+    def hrange(start_or_finish, finish=None, step=None):
+        try:
+            if step is None:
+                if finish is None:
+                    return xrange(start_or_finish)
+                else:
+                    return xrange(start_or_finish, finish)
+            else:
+                return xrange(start_or_finish, finish, step)
+        except OverflowError:
+            if step == 0:
+                raise ValueError('step argument may not be zero')
+            if step is None:
+                step = 1
+            if finish is not None:
+                start = start_or_finish
+            else:
+                start = 0
+                finish = start_or_finish
+            assert step != 0
+            if step > 0:
+                def shimrange():
+                    i = start
+                    while i < finish:
+                        yield i
+                        i += step
+            else:
+                def shimrange():
+                    i = start
+                    while i > finish:
+                        yield i
+                        i += step
+            return shimrange()
+
+    ARG_NAME_ATTRIBUTE = 'id'
+    integer_types = (int, long)
+    hunichr = unichr
+    reduce = reduce
+
+
+def a_good_encoding():
+    result = sys.getdefaultencoding()
+    if result == 'ascii':
+        return 'utf-8'
+    else:
+        return result
+
+
+def to_unicode(x):
+    if isinstance(x, text_type):
+        return x
+    else:
+        return x.decode(a_good_encoding())
+
+
+def qualname(f):
+    try:
+        return f.__qualname__
+    except AttributeError:
+        pass
+    try:
+        return f.im_class.__name__ + '.' + f.__name__
+    except AttributeError:
+        return f.__name__
+
+
+importlib_invalidate_caches = getattr(
+    importlib, 'invalidate_caches', lambda: ())
