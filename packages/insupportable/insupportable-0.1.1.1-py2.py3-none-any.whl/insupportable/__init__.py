102376bae6e530abd43d8c447e9416f86ae9fcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package insupportable/0.1.1.1/insupportable-0.1.1.1-py2.py3-none-any.whl

---
+++ packages/insupportable/insupportable-0.1.1.1-py2.py3-none-any.whl/insupportable/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+a simple way to remember to remove dead code due to notanymore supported
+library of python versions.
+"""
+
+from .insupportable import *
+
+__author__ = 'Matthias Bussonnier'
+__email__ = 'bussonniermatthias@gmail.com'
+__version__ = '0.1.1.1'
