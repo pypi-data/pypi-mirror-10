6ef893e3b4b99c5589b9ca407670e50862720fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package pyyaxml/0.5.3/pyyaxml-0.5.3.zip

---
+++ packages/pyyaxml/pyyaxml-0.5.3.zip/pyyaxml-0.5.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyyaxml',
+      version='0.5.3',
+      description='Python API to Yandex.XML',
+      url='https://github.com/dbf256/py-ya-xml',
+      author='Alexey Moskvin',
+      author_email='dbf256@gmail.com',
+      license='MIT',
+      packages=['pyyaxml'],
+      zip_safe=False)
