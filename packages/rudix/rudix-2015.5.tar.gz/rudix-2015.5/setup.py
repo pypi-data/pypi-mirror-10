0b878f68d4c084611101499b623d25a69bdf8789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:47 +0000

    Add package rudix/2015.5/rudix-2015.5.tar.gz

---
+++ packages/rudix/rudix-2015.5.tar.gz/rudix-2015.5/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+with open('README') as f:
+    readme = f.read()
+
+setup(name='rudix',
+      version='2015.5',
+      license='BSD',
+      description='Rudix Package Manager',
+      long_description=readme,
+      author='Rudá Moura',
+      author_email='ruda.moura@gmail.com',
+      url='http://rudix.org/',
+      keywords='package manager',
+      classifiers = ["Programming Language :: Python", 
+                     "License :: OSI Approved :: BSD License",
+                     "Operating System :: MacOS :: MacOS X",
+                     "Topic :: System :: Installation/Setup",
+                     "Topic :: System :: Software Distribution"],
+      scripts=["rudix"],
+)
