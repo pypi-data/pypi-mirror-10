e1512a91806a9bf41309a74a82be91ac2834eb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package flup-py3/1.0.2-1/flup-py3-1.0.2-1.tar.gz

---
+++ packages/flup-py3/flup-py3-1.0.2-1.tar.gz/flup-py3-1.0.2-1/setup.py
@@ -0,0 +1,39 @@
+# Bootstrap setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name='flup-py3',
+    version='1.0.2-1',
+    packages=find_packages(),
+    zip_safe=True,
+
+    entry_points="""
+    [paste.server_runner]
+    ajp = flup.server.paste_factory:run_ajp_thread
+    fcgi = flup.server.paste_factory:run_fcgi_thread
+    scgi = flup.server.paste_factory:run_scgi_thread
+    ajp_thread = flup.server.paste_factory:run_ajp_thread
+    fcgi_thread = flup.server.paste_factory:run_fcgi_thread
+    scgi_thread = flup.server.paste_factory:run_scgi_thread
+    ajp_fork = flup.server.paste_factory:run_ajp_fork
+    fcgi_fork = flup.server.paste_factory:run_fcgi_fork
+    scgi_fork = flup.server.paste_factory:run_scgi_fork
+    """,
+
+    author='Allan Saddi',
+    author_email='allan@saddi.com',
+    description='Random assortment of WSGI servers, port to py3 based on Anton Surashov work',
+    license='BSD',
+    url='http://www.saddi.com/software/flup/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
