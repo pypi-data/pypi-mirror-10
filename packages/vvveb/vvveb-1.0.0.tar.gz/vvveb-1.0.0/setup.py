18ea3f3272d615978c0404de93fd62eb69961c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:57 +0000

    Add package vvveb/1.0.0/vvveb-1.0.0.tar.gz

---
+++ packages/vvveb/vvveb-1.0.0.tar.gz/vvveb-1.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+
+setup(
+    name='vvveb',
+    version='1.0.0',
+    install_requires=__requires,
+    description='An automatic website hosting configurator',
+    author='Eugene Pankov',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org/',
+    scripts=['veb'],
+    packages=find_packages(),
+)
