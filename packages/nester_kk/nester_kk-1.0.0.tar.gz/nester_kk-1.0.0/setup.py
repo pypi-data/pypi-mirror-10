534f7a9bdda2ab18538b8139b9c5dc0392c58b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package nester_kk/1.0.0/nester_kk-1.0.0.tar.gz

---
+++ packages/nester_kk/nester_kk-1.0.0.tar.gz/nester_kk-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_kk',
+	version = '1.0.0',
+	py_modules = ['nester_kk'],
+	author = 'kaustubh',
+	author_email = 'kaustubhkalla1994@gmail.com',
+	url = 'http://www.kaustubhkalla.wordpress.com',
+	description = 'Nested list printer',
+     )	
