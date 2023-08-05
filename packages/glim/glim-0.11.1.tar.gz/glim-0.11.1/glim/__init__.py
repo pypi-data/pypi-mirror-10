c0437196c754ad4063e08d9bd3873ddf469939d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package glim/0.11.1/glim-0.11.1.tar.gz

---
+++ packages/glim/glim-0.11.1.tar.gz/glim-0.11.1/glim/__init__.py
@@ -0,0 +1,22 @@
+from glim import config
+from glim import core
+from glim import log
+from glim import controller
+from glim import ext
+from glim import service
+from glim import command
+
+# package shortcuts for easy import
+Config = config.ConfigFacade
+
+Log = log.LogFacade
+GlimLog = log.GlimLogFacade
+
+Command = command.Command
+GlimCommand = command.GlimCommand
+
+Controller = controller.Controller
+
+Extension = ext.Extension
+
+Service = service.Service
