97fcb83f805309229472e4701d0032436af44ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:41 +0000

    Add package commandify/0.0.4.0/commandify-0.0.4.0.tar.gz

---
+++ packages/commandify/commandify-0.0.4.0.tar.gz/commandify-0.0.4.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+from commandify.version import get_version
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='commandify',
+    version=get_version(),
+    description='Simple command line commands through decorators',
+    long_description=read('README.rst'),
+    author='Mark Muetzelfeldt',
+    author_email='markmuetz@gmail.com',
+    maintainer='Mark Muetzelfeldt',
+    maintainer_email='markmuetz@gmail.com',
+    packages=['commandify'],
+    scripts=['commandify/commandify_examples'],
+    install_requires=[],
+    data_files=[],
+    url='https://github.com/markmuetz/commandify',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        ],
+    keywords=['command line argument arguments'],
+    )
