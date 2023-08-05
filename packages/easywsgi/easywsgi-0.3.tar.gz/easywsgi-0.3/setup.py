720ca9029c0c6b230d4a07764ca5cf0924be878a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:43 +0000

    Add package easywsgi/0.3/easywsgi-0.3.tar.gz

---
+++ packages/easywsgi/easywsgi-0.3.tar.gz/easywsgi-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='easywsgi',
+            version='0.3',
+            description='Small module to make wsgi apps easy to write, do error checking an publishe this to the client side.',
+            url='https://github.com/jorants/easywsgi',
+            author='Control-K',
+            author_email='jorants@gmail.com',
+            license='BEER',
+            packages=['easywsgi'],
+            zip_safe=False)
