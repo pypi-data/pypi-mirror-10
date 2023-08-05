946dd73937699e5ad11a6688c7f1ee6576afb638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package pixhostuploader/1.0.8/pixhostuploader-1.0.8.tar.gz

---
+++ packages/pixhostuploader/pixhostuploader-1.0.8.tar.gz/pixhostuploader-1.0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='pixhostuploader',
+    version='1.0.8',
+    description='Unofficial uploader to pixhost.org image sharing site.',
+    long_description=readme(),
+    url='https://github.com/martinsik/pixhost-uploader',
+    author='Martin Sikora',
+    license='MIT',
+    packages=find_packages(exclude=['tests*']),
+    classifiers=[
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'requests',
+        'lxml'
+    ],
+    test_suite='tests',
+)
