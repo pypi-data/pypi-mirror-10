3b19327eb0bb199b035e05b0356b8a839958b157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:12 +0000

    Add package pubcontrol/2.2.3/pubcontrol-2.2.3.tar.gz

---
+++ packages/pubcontrol/pubcontrol-2.2.3.tar.gz/pubcontrol-2.2.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+if sys.version_info >= (3,):
+	install_requires = ['PyJWT>=0,<1','requests>=2,<3','tnetstring3>=0,<1']
+else:
+	install_requires = ['PyJWT>=0,<1','requests>=2,<3','tnetstring>=0,<1']
+
+setup(
+	name='pubcontrol',
+	version='2.2.3',
+	description='EPCP library',
+	author='Justin Karneges',
+	author_email='justin@affinix.com',
+	url='https://github.com/fanout/pypubcontrol',
+	license='MIT',
+	package_dir={'pubcontrol': 'src'},
+	packages=['pubcontrol'],
+	install_requires=install_requires,
+	classifiers=[
+		'Topic :: Utilities',
+		'License :: OSI Approved :: MIT License'
+	]
+)
