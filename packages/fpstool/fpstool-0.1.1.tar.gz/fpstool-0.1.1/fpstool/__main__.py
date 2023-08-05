c73168bec6113c3ca6aaf9a3fa691c6a42841c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:20 +0000

    Add package fpstool/0.1.1/fpstool-0.1.1.tar.gz

---
+++ packages/fpstool/fpstool-0.1.1.tar.gz/fpstool-0.1.1/fpstool/__main__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from . import *
+
+if __name__ == '__main__':
+    init()
+
+    import signal
+    def handler(signum, frame):
+        print 'Signal handler called with signal', signum
+        stop()
+        exit(0)
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGQUIT, handler)
+
+    def printer(n):
+        print 'Recv fps:', n
+
+    recvfps(printer)
+
+    time.sleep(5.0)
