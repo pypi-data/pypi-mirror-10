f0f80617b3289f75ab7ac040759f06c25194b942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package wildcard.cloudflare/2.0b1/wildcard.cloudflare-2.0b1.zip

---
+++ packages/wildcard.cloudflare/wildcard.cloudflare-2.0b1.zip/wildcard.cloudflare-2.0b1/wildcard/cloudflare/upgrades.py
@@ -0,0 +1,9 @@
+from Products.CMFCore.utils import getToolByName
+
+
+PROFILE_ID = 'profile-wildcard.cloudflare:default'
+
+
+def upgrade_20(context, logger=None):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
