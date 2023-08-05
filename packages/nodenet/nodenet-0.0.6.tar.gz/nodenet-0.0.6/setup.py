f8581ca7fe9839376730fcc0dd5e3bb0037b4998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:35 +0000

    Add package nodenet/0.0.6/nodenet-0.0.6.tar.gz

---
+++ packages/nodenet/nodenet-0.0.6.tar.gz/nodenet-0.0.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='nodenet',
+    version='0.0.6',
+    description='an asynchronous node-based UDP networking library',
+    author='Ajay MT',
+    author_email='ajaymt@icloud.com',
+    url='http://github.com/AjayMT/nodenet',
+    download_url='https://github.com/AjayMT/nodenet/tarball/v0.0.6',
+    keywords='node network UDP asynchronous',
+    py_modules=['nodenet'],
+    requires=[
+        'pyuv (>1.0.0, <2.0.0)',
+        'emitter (>=0.0.6)'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ]
+)
