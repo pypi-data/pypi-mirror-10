5055492791969b4bdedb90c0b773a3ea49b7edb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:50 +0000

    Add package nativeconfig/2.0.3/nativeconfig-2.0.3.tar.gz

---
+++ packages/nativeconfig/nativeconfig-2.0.3.tar.gz/nativeconfig-2.0.3/nativeconfig/options/int_option.py
@@ -0,0 +1,40 @@
+import json
+from nativeconfig.exceptions import DeserializationError, ValidationError
+from nativeconfig.options.base_option import BaseOption
+
+
+class IntOption(BaseOption):
+    """
+    FloatOption represents Python float in config.
+
+    """
+
+    def __init__(self, name, **kwargs):
+        """
+        Accepts all the arguments of BaseConfig except choices.
+        """
+        super().__init__(name, **kwargs)
+
+    def serialize(self, python_value):
+        return str(python_value)
+
+    def deserialize(self, raw_value):
+        try:
+            return int(raw_value)
+        except ValueError:
+            raise DeserializationError("Unable to deserialize \"{}\" into int value!".format(raw_value), raw_value)
+
+    def deserialize_json(self, json_value):
+        try:
+            value = json.loads(json_value)
+        except ValueError:
+            raise DeserializationError("Invalid json: \"{}\"".format(json_value), json_value)
+        else:
+            return value
+
+    def validate(self, python_value):
+        super().validate(python_value)
+        try:
+            valid_val = int(python_value)
+        except ValueError as e:
+            raise ValidationError("Invalid int value \"{}\"!".format(python_value), python_value)
