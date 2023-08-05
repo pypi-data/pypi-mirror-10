b15faa91e109a552bcdbed47997e6f058d2878ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package hpelm/0.5.15/hpelm-0.5.15.tar.gz

---
+++ packages/hpelm/hpelm-0.5.15.tar.gz/hpelm-0.5.15/README.rst
@@ -0,0 +1,4 @@
+High Performance ELM
+--------
+
+I am sorry to announce that the toolbox is temporary suspended due to the copyright concenrs with the university. It will be re-published as soon as possible.
---
+++ packages/hpelm/hpelm-0.5.15.tar.gz/hpelm-0.5.15/dummy/__inti__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 21:49:30 2015
+
+@author: akusok
+"""
+
+ELM = "I am sorry to announce that the toolbox is temporary suspended due to the copyright concenrs with the university. It will be re-published as soon as possible."
+HPELM = "I am sorry to announce that the toolbox is temporary suspended due to the copyright concenrs with the university. It will be re-published as soon as possible."
