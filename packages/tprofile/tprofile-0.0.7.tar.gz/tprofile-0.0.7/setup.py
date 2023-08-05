7b0b593cecfe78fa6396eb95948c8e3184abb0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:17 +0000

    Add package tprofile/0.0.7/tprofile-0.0.7.tar.gz

---
+++ packages/tprofile/tprofile-0.0.7.tar.gz/tprofile-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='tprofile',
+    version='0.0.7',
+    packages=['tprofile'],
+    author='timchow',
+    author_email='jingjiang@staff.sina.com.cn',
+    license='LGPL',
+    install_requires=["tornado"],
+    description="a profile tool for tornado web requesthandler",
+    keywords='tornado profile',
+    url='http://tieba.baidu.com/f?ie=utf-8&kw=%E5%91%A8%E4%BA%95%E6%B1%9F',
+    entry_points={
+        "console_scripts":[
+            "test_tprofile_server=tprofile.test_server:main",
+        ],
+    },
+    package_data={
+        'tprofile': ['usage.txt'],
+    }
+)
+
