bd822b10348e6cef3c39ecacbfa07061f2ec0650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package configgen/0.2.1/configgen-0.2.1.tar.gz

---
+++ packages/configgen/configgen-0.2.1.tar.gz/configgen-0.2.1/configgen/__init__.py
@@ -0,0 +1,1 @@
+from configgen.config import ConfigError, ConfigInitError, LookupError, MultiValue, KeyValue, Config, import_config_module, get_configs_from_module, get_config_from_module, make_multi_key
