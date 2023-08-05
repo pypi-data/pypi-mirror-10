714dc2b9df49fe20c3502bfcb6faea23cb218b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package trimfilename/0.5.1/trimfilename-0.5.1.tar.gz

---
+++ packages/trimfilename/trimfilename-0.5.1.tar.gz/trimfilename-0.5.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+add_keywords = dict(
+    entry_points = {
+        'console_scripts': ['trimfilename = trimfilename.main:main'],
+        },
+)
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    fhan = open('README.txt')
+    long_description = fhan.read()
+    fhan.close()
+
+setup(
+
+    name="trimfilename",
+    version="0.5.1",
+    license='GPLv3',
+    description="Remove string patterns from filenames",
+    url="https://github.com/tanjot/trimfilename",
+    author="Tanjot Kaur",
+    author_email="tanjot28@gmail.com",
+    packages=['trimfilename'],
+    **add_keywords
+)
