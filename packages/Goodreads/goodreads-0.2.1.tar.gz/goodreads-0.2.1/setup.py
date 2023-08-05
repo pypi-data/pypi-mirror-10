24d8f8682edf878546681ef68df4f62e1b7ae278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:45 +0000

    Add package Goodreads/0.2.1/goodreads-0.2.1.tar.gz

---
+++ packages/Goodreads/goodreads-0.2.1.tar.gz/goodreads-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import pypandoc
+
+setup(
+    name = 'goodreads',
+    description = "Python wrapper for Goodreads API",
+    long_description = pypandoc.convert('README.md', 'rst'),
+    author = 'Sefa Kilic',
+    author_email = 'sefakilic@gmail.com',
+    url = 'https://github.com/sefakilic/goodreads/',
+    version = '0.2.1',
+    install_requires = ['nose', 'xmltodict', 'requests', 'rauth'],
+    packages = ['goodreads'],
+    scripts = [],
+    license = 'MIT',
+)
