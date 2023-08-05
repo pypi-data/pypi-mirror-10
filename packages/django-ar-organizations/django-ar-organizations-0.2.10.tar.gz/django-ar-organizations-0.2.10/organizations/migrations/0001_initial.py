2bea8dfea3505e5423bbdeb64c3c0d640acc9e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:22 +0000

    Add package django-ar-organizations/0.2.10/django-ar-organizations-0.2.10.tar.gz

---
+++ packages/django-ar-organizations/django-ar-organizations-0.2.10.tar.gz/django-ar-organizations-0.2.10/organizations/migrations/0001_initial.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Organization',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('name', models.CharField(help_text='The name of the organization', max_length=200)),
+                ('slug', django_extensions.db.fields.AutoSlugField(populate_from=b'name', editable=False, max_length=200, blank=True, help_text='The name in all lowercase, suitable for URL identification', unique=True)),
+                ('is_active', models.BooleanField(default=True)),
+            ],
+            options={
+                'ordering': ['name'],
+                'verbose_name': 'organization',
+                'verbose_name_plural': 'organizations',
+            },
+        ),
+        migrations.CreateModel(
+            name='OrganizationOwner',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('organization', models.OneToOneField(related_name='owner', to='organizations.Organization')),
+            ],
+            options={
+                'verbose_name': 'organization owner',
+                'verbose_name_plural': 'organization owners',
+            },
+        ),
+        migrations.CreateModel(
+            name='OrganizationUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('is_admin', models.BooleanField(default=False)),
+                ('organization', models.ForeignKey(related_name='organization_users', to='organizations.Organization')),
+                ('user', models.ForeignKey(related_name='organization_users', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['organization', 'user'],
+                'verbose_name': 'organization user',
+                'verbose_name_plural': 'organization users',
+            },
+        ),
+        migrations.AddField(
+            model_name='organizationowner',
+            name='organization_user',
+            field=models.OneToOneField(related_name='owned_organization', to='organizations.OrganizationUser'),
+        ),
+        migrations.AddField(
+            model_name='organization',
+            name='users',
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, through='organizations.OrganizationUser'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='organizationuser',
+            unique_together=set([('user', 'organization')]),
+        ),
+    ]
