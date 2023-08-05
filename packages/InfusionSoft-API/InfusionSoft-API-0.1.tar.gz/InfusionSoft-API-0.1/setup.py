a801a7f08789882035d1917091fc804e846e233a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:35 +0000

    Add package InfusionSoft-API/0.1/InfusionSoft-API-0.1.tar.gz

---
+++ packages/InfusionSoft-API/InfusionSoft-API-0.1.tar.gz/InfusionSoft-API-0.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+dependencies = [
+
+]
+
+links = [
+    
+]
+
+setup(name='InfusionSoft-API',
+      version='0.1',
+      description='python wrapper for InfusionSoft API',
+      maintainer='Nick Sloan',
+      maintainer_email='spam+pypi@nasloan.com',
+      scripts=[],
+      url='https://github.com/infusionsoft/Official-API-Python-Library',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=dependencies,
+      dependency_links = links,
+     )
+
+
+
