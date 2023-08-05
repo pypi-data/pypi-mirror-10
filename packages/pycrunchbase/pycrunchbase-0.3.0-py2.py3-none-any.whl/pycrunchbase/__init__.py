a7a8aa88b29e78e2a5642cf7f23b2a8b161b5df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package pycrunchbase/0.3.0/pycrunchbase-0.3.0-py2.py3-none-any.whl

---
+++ packages/pycrunchbase/pycrunchbase-0.3.0-py2.py3-none-any.whl/pycrunchbase/__init__.py
@@ -0,0 +1,52 @@
+from .pycrunchbase import (
+    CrunchBase,
+)
+from .resource import (
+    Acquisition,
+    Address,
+    Category,
+    Degree,
+    FundingRound,
+    Fund,
+    Image,
+    Investment,
+    IPO,
+    Job,
+    Location,
+    News,
+    Organization,
+    Page,
+    PageItem,
+    Person,
+    Product,
+    Relationship,
+    Video,
+    Website,
+)
+
+
+__version__ = "0.3.0"
+
+__all__ = [
+    'Acquisition',
+    'Address',
+    'Category',
+    'Degree',
+    'FundingRound',
+    'Fund',
+    'Image',
+    'Investment',
+    'IPO',
+    'Job',
+    'Location',
+    'News',
+    'Organization',
+    'Page',
+    'PageItem',
+    'Person',
+    'Product',
+    'Relationship',
+    'Video',
+    'Website',
+    'CrunchBase'
+]
