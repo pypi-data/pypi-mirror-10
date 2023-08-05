f1fbc174b53cd8e4f068c9781fc84aa0e5322b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package hello-greeting-test/0.0.0/hello-greeting-test-0.0.0.tar.gz

---
+++ packages/hello-greeting-test/hello-greeting-test-0.0.0.tar.gz/hello-greeting-test-0.0.0/hello-greeting-test/hello.py
@@ -0,0 +1,3 @@
+class Greeting:
+    def say_hi(self, name):
+        print "Hello " + str(name)
---
+++ packages/hello-greeting-test/hello-greeting-test-0.0.0.tar.gz/hello-greeting-test-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="hello-greeting-test",
+    packages=["hello-greeting-test"],
+    version="0.0.0",
+    description="A module to say hi to people", licence="MIT", classifiers=[
+    "Programming Language :: Python :: 2.7",
+    "Development Status :: 1 - Planning"
+])
+
