c1d417984c7067a3229ca0f39ade677fbecb5626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package pyrdio/1.1.0/pyrdio-1.1.0.tar.gz

---
+++ packages/pyrdio/pyrdio-1.1.0.tar.gz/pyrdio-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'oauth2'
+]
+
+setup(
+    name='pyrdio',
+    version='1.1.0',
+    description='A simple Python wrapper for Rdio\'s API',
+    url='https://github.com/rcrdclub/pyrdio',
+    author='Hery Ratsimihah',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ),
+    keywords='rdio library',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=requires
+)
