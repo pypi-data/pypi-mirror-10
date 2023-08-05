773dc1dde5e2744d4e0d56e2b37e4d1c79e4836d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:43 +0000

    Add package maxclient/5.0/maxclient-5.0.zip

---
+++ packages/maxclient/maxclient-5.0.zip/maxclient-5.0/maxclient/defaults.py
@@ -0,0 +1,10 @@
+ENDPOINT_METHOD_DEFAULTS = {
+    '/people/{username}/activities_post': {'object': {'objectType': 'note'}},
+    '/people/{username}/conversations/{id}/messages_post': {'object': {'objectType': 'note'}},
+    '/people/{username}/subscriptions': {'object': {'objectType': 'context'}},
+    '/conversations/{id}/messages_post': {'object': {'objectType': 'note'}},
+    '/contexts/{hash}/activities_post': {'object': {'objectType': 'note'}},
+    '/contexts_post': {'objectType': 'context'},
+    '/people/{username}/subscriptions_post': {'object': {'objectType': 'context'}},
+    '/conversations_post': {"object": {'objectType': 'note'}}
+}
