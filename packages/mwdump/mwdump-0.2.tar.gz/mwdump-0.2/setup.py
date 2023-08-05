76e80bfffeb206c224c4a4ffe690356f8c6f7ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package mwdump/0.2/mwdump-0.2.tar.gz

---
+++ packages/mwdump/mwdump-0.2.tar.gz/mwdump-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mwdump',
+      version='0.2',
+      description='MediaWiki dump file reader',
+      url='https://github.com/cllu/mwdump',
+      author='Chunliang Lyu',
+      author_email='hi@chunlianglyu.com',
+      license='MIT',
+      packages=['mwdump'],
+      install_requires=['lxml'],
+      zip_safe=False
+)
