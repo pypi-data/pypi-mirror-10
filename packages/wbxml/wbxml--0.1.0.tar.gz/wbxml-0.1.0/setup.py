e4c6ef1b2f8e097aced99f120b640becf3c5b515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package wbxml/0.1.0/wbxml--0.1.0.tar.gz

---
+++ packages/wbxml/wbxml--0.1.0.tar.gz/wbxml-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+
+with open('README.rst', 'r') as file:
+    long_description = file.read()
+
+wbxml = Extension('wbxml',
+                  libraries=['wbxml2'],
+                  sources=['src/wbxml.c'])
+
+setup(name='wbxml',
+      version='0.1.0',
+      author='Jezeniel Zapanta',
+      author_email='jezeniel@infoshiftinc.com',
+      description='Python wrapper for libwbxml',
+      long_description=long_description,
+      license='MIT',
+      keywords=['wbxml', 'xml', 'mobile'],
+      url='https://github.com/infoshift/python-wbxml',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7'
+      ],
+      ext_modules=[wbxml])
