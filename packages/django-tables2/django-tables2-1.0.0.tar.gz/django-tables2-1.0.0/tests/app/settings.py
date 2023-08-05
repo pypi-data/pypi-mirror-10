c48f94b49102a3a38a4250b817c9b94ee0a99c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:43 +0000

    Add package django-tables2/1.0.0/django-tables2-1.0.0.tar.gz

---
+++ packages/django-tables2/django-tables2-1.0.0.tar.gz/django-tables2-1.0.0/tests/app/settings.py
@@ -0,0 +1,37 @@
+from django.conf import global_settings
+import six
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'tests.app',
+    'django_tables2',
+]
+
+ROOT_URLCONF = 'tests.app.urls'
+
+SECRET_KEY = "this is super secret"
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.core.context_processors.request'
+] + list(global_settings.TEMPLATE_CONTEXT_PROCESSORS)
+
+TIME_ZONE = "Australia/Brisbane"
+
+USE_TZ = True
+
+if not six.PY3:  # Haystack isn't compatible with Python 3
+    INSTALLED_APPS += [
+        'haystack',
+    ]
+    HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+        }
+    }
