7e1c29f4722a7b74883ec887286bbf4b5ce1b7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:26 +0000

    Add package aio.web.server/0.0.8/aio.web.server-0.0.8.tar.gz

---
+++ packages/aio.web.server/aio.web.server-0.0.8.tar.gz/aio.web.server-0.0.8/aio/web/server/tests/test_factories.py
@@ -0,0 +1,218 @@
+import os
+import asyncio
+import tempfile
+from configparser import ConfigParser
+
+import aiohttp
+import aiohttp_jinja2
+
+from aio.testing import aiotest
+from aio.web.server.testing import AioWebAppTestCase
+from aio.app.runner import runner
+from aio.web.server import factories
+from aio.core.exceptions import BadConfiguration
+import aio.web.server.tests.test_factories
+
+
+TEST_DIR = os.path.basename(
+    os.path.abspath(__file__))
+
+
+class WebFactoryTestCase(AioWebAppTestCase):
+
+    @asyncio.coroutine
+    def _get_web_app(self):
+        loop = asyncio.get_event_loop()
+        web_app = aiohttp.web.Application(loop=loop)
+        web_app['name'] = "TEST"
+        return web_app
+
+
+def example_filter():
+    pass
+
+
+@asyncio.coroutine
+def test_route():
+    pass
+
+
+class WebSetupFactoryTestCase(WebFactoryTestCase):
+
+    @asyncio.coroutine
+    def _get_web_app(self):
+        web_app = yield from WebFactoryTestCase._get_web_app(self)
+
+        # setup jinja
+        yield from factories.templates_factory(web_app, {})
+        return web_app
+
+    
+class WebRoutesFactoryTestCase(WebSetupFactoryTestCase):
+
+    @aiotest
+    def test_routes_factory_no_config(self):    
+        web_app = yield from self._get_web_app()
+        yield from factories.routes_factory(
+            web_app,
+            {})        
+
+    @aiotest
+    def test_routes_factory(self):
+        config = ConfigParser()
+        config.read_dict(
+            {'web/TEST/route' : {
+                'match': '/',
+                'route': "aio.web.server.tests.test_factories.test_route"}})
+        aio.app.config = config
+        web_app = yield from self._get_web_app()
+        yield from factories.routes_factory(
+            web_app,
+            {})
+        self.assertEqual(
+            web_app.router['web/TEST/route'].url(), "/")
+        
+
+class WebStaticFactoryTestCase(WebSetupFactoryTestCase):
+
+    @aiotest
+    def test_static_factory_no_config(self):
+        web_app = yield from self._get_web_app()
+        yield from factories.static_factory(
+            web_app,
+            {})
+        self.assertEqual(
+            web_app.router._urls, [])
+
+    @aiotest
+    def test_static_factory(self):
+        """
+        [web/TEST]
+        static_dir = TMP
+        static_url = /test
+        """
+        with tempfile.TemporaryDirectory() as tmp:
+            web_app = yield from self._get_web_app()
+            yield from factories.static_factory(
+                web_app,
+                dict(
+                    static_dir=tmp,
+                    static_url="/test_static"))
+            urls = web_app.router._urls
+            self.assertEqual(
+                urls[0].url(filename=""), "/test_static/")
+            self.assertEqual(
+                urls[0]._directory, tmp)
+        
+
+class WebTemplatesFactoryTestCase(WebFactoryTestCase):
+
+    @aiotest
+    def test_templates_factory_no_config(self):
+        """
+        [web/TEST]
+        """
+        web_app = yield from self._get_web_app()
+        yield from factories.templates_factory(
+            web_app,
+            {})
+        env = aiohttp_jinja2.get_env(web_app)
+        with self.assertRaises(AttributeError):
+            env.list_templates()
+
+    @aiotest
+    def test_templates_factory_web_conf(self):
+        """
+        [web/TEST]
+        modules = aio.web.server.tests
+        """
+        web_app = yield from self._get_web_app()
+        yield from factories.templates_factory(
+            web_app,
+            {'modules': 'aio.web.server.tests'})
+        env = aiohttp_jinja2.get_env(web_app)
+        self.assertEqual(
+            [x for x in env.list_templates(extensions=["html"])],
+            ['fragments/test_fragment.html', 'test_template.html'])
+
+    @aiotest
+    def test_templates_factory_web_app_conf(self):
+        """
+        [aio/web]
+        modules = aio.web.server.tests
+        """
+        aio.app.config = {
+            'aio/web' :{'modules': 'aio.web.server.tests'}}
+        web_app = yield from self._get_web_app()
+        yield from factories.templates_factory(
+            web_app,
+            {})
+        env = aiohttp_jinja2.get_env(web_app)
+        tl = env.list_templates(extensions=[".html"])
+        self.assertEqual(
+            [x for x in env.list_templates(extensions=["html"])],
+            ['fragments/test_fragment.html', 'test_template.html'])
+
+    @aiotest
+    def test_templates_factory_aio_conf(self):
+        """
+        [aio]
+        modules = aio.web.server.tests
+        """
+        aio.app.config = {
+            'aio/web' :{'modules': 'aio.web.server.tests'}}
+        web_app = yield from self._get_web_app()
+        yield from factories.templates_factory(
+            web_app,
+            {})
+        env = aiohttp_jinja2.get_env(web_app)
+        tl = env.list_templates(extensions=[".html"])
+        self.assertEqual(
+            [x for x in env.list_templates(extensions=["html"])],
+            ['fragments/test_fragment.html', 'test_template.html'])
+
+    @aiotest
+    def test_templates_factory_multiple_modules(self):
+        """
+        [web/TEST]
+        modules = aio.web.server.tests
+        """
+        web_app = yield from self._get_web_app()
+        yield from factories.templates_factory(
+            web_app,
+            {'modules': 'aio.web.server.tests\naio.web.server'})
+        env = aiohttp_jinja2.get_env(web_app)
+        templ_list = [
+            x for x in env.list_templates(extensions=["html"])]
+        self.assertTrue(
+            "fragments/test_fragment.html" in templ_list)
+        self.assertTrue(
+            "base.html" in templ_list)
+
+
+class WebFiltersFactoryTestCase(WebSetupFactoryTestCase):
+
+    @aiotest
+    def test_filters_factory_no_config(self):
+        yield from factories.filters_factory(
+            (yield from self._get_web_app()),
+            {})
+
+    @aiotest
+    def test_filters_factory(self):
+        web_app = yield from self._get_web_app()
+        yield from factories.filters_factory(
+            web_app,
+            {'filters': "my_filter aio.web.server.tests.test_factories.example_filter"})
+        env = aiohttp_jinja2.get_env(web_app)
+        self.assertEqual(
+            env.filters['my_filter'],
+            aio.web.server.tests.test_factories.example_filter)
+
+    @aiotest
+    def test_filters_factory_bad_handler(self):
+        web_app = yield from self._get_web_app()
+        with self.assertRaises(BadConfiguration):
+            yield from factories.filters_factory(
+                web_app,
+                {'filters': "my_filter aio.web.server.tests.test_factories._BAD_example_filter"})
---
+++ packages/aio.web.server/aio.web.server-0.0.8.tar.gz/aio.web.server-0.0.8/aio/web/server/tests/test_web_server.py
@@ -0,0 +1,119 @@
+import asyncio
+from configparser import ConfigParser
+
+import aiohttp.web
+
+from aio.testing import aiofuturetest, aiotest
+from aio.web.server.testing import AioWebAppTestCase
+from aio.app.runner import runner
+import aio.web.server
+from aio.core.exceptions import MissingConfiguration
+
+CONFIG = """
+[server/test]
+factory = aio.web.server.factory
+port = 7070
+
+[web/test/route]
+match = /
+route = aio.web.server.tests.handle_hello_web_world
+"""
+
+@asyncio.coroutine
+def example_factory(web_app, conf):
+    web_app["foo"] = conf["foo"]
+
+
+class WebServerFactoryTestCase(AioWebAppTestCase):
+
+    @aiotest
+    def test_server_factory_no_config(self):
+        with self.assertRaises(MissingConfiguration):
+            yield from aio.web.server.factory(
+                "TEST", None, None, "8080")
+
+    @aiofuturetest
+    def test_web_factory_no_port(self):
+        aio.app.config = ConfigParser()
+        aio.app.config.read_dict({})
+        with self.assertRaises(ValueError):
+            srv = yield from aio.web.server.factory(
+                "TEST", None, None, None)
+
+    @aiofuturetest
+    def test_web_factory_empty_config(self):
+        aio.app.config = ConfigParser()
+        aio.app.config.read_dict({})
+        srv = yield from aio.web.server.factory(
+            "TEST", None, None, "8080")
+        self.assertIsInstance(
+            srv, asyncio.base_events.Server)
+
+    @aiofuturetest
+    def test_web_factory(self):
+        aio.app.config = ConfigParser()
+        aio.app.config.read_dict(
+            {"aio/web": {
+                'factory_types': 'example',
+                "example_factory": "aio.web.server.tests.test_web_server.example_factory"},
+             "web/TEST": {
+                 "foo": "bar"}})
+        srv = yield from aio.web.server.factory(
+            "TEST", None, None, "8080")
+        self.assertIsInstance(
+            srv, asyncio.base_events.Server)
+        self.assertEqual(
+            aio.web.server.apps['TEST']['foo'],
+            "bar")
+        
+    @aiofuturetest
+    def test_web_factory_custom_protocol(self):
+        aio.app.config = ConfigParser()
+        aio.app.config.read_dict({})        
+
+        @asyncio.coroutine
+        def custom_protocol(name):
+            loop = asyncio.get_event_loop()
+            http_app = aiohttp.web.Application(loop=loop)
+            http_app['name'] = name
+            http_app['foo'] = 'baz'
+            aio.web.server.apps[name] = http_app
+            return http_app.make_handler()
+        
+        srv = yield from aio.web.server.factory(
+            "TEST", custom_protocol, None, "8080")
+        self.assertIsInstance(
+            srv, asyncio.base_events.Server)
+        self.assertEqual(
+            aio.web.server.apps['TEST']['foo'],
+            "baz")
+        
+
+class WebServerProtocolTestCase(AioWebAppTestCase):
+
+    @aiotest
+    def test_web_protocol_no_config(self):
+        with self.assertRaises(MissingConfiguration):
+            yield from aio.web.server.protocol("TEST")
+
+    @aiotest
+    def test_web_protocol_empty_config(self):
+        aio.app.config = ConfigParser()
+        aio.app.config.read_dict({})
+        protocol = yield from aio.web.server.protocol("TEST")
+        self.assertIsInstance(
+            protocol, aiohttp.web.RequestHandlerFactory)
+
+    @aiotest
+    def test_web_protocol_factory(self):
+        aio.app.config = ConfigParser()
+        aio.app.config.read_dict(
+            {"aio/web": {
+                'factory_types': 'example',
+                "example_factory": "aio.web.server.tests.test_web_server.example_factory"},
+             "web/TEST": {
+                 "foo": "bar"}})
+        protocol = yield from aio.web.server.protocol("TEST")
+        self.assertEqual(
+            protocol._app['foo'],
+            "bar")
