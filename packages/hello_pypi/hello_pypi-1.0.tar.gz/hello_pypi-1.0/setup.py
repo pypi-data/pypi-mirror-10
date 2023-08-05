5982072153fbb693a4f07f8fd9ba1241858d1c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package hello_pypi/1.0/hello_pypi-1.0.tar.gz

---
+++ packages/hello_pypi/hello_pypi-1.0.tar.gz/hello_pypi-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hello_pypi',
+      version='1.0',
+      description='An example pypi project',
+      url='https://github.com/smelnicki/hello_pypi',
+      author='Stephen Melnicki',
+      author_email='smelnicki@tutamail.com',
+      license='MIT',
+      packages=['hello_pypi'],
+      zip_safe=False)
