524bb0abda38404ea65ffdbd082bbbe07eba11e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:49 +0000

    Add package nester_test1/1.0.0/nester_test1-1.0.0.tar.gz

---
+++ packages/nester_test1/nester_test1-1.0.0.tar.gz/nester_test1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup( name = 'nester_test1',
+       version = '1.0.0',
+       py_modules = ['nester'],
+       author = 'cai',
+       author_email = '1980869288@qq.com',
+       url = 'blank',
+       description = 'a simple test'
+       )
