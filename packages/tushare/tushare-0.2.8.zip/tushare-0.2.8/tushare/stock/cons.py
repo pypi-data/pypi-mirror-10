cd421f17050d5e18f0b61729c9b07c8aec263789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package tushare/0.2.8/tushare-0.2.8.zip

---
+++ packages/tushare/tushare-0.2.8.zip/tushare-0.2.8/tushare/stock/cons.py
@@ -0,0 +1,124 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2014/07/31
+@author: Jimmy Liu
+@group : waditu
+@contact: jimmysoa@sina.cn
+"""
+
+VERSION = '0.2.8'
+K_LABELS = ['D', 'W', 'M']
+K_MIN_LABELS = ['5', '15', '30', '60']
+K_TYPE = {'D': 'akdaily', 'W': 'akweekly', 'M': 'akmonthly'}
+INDEX_LABELS = ['sh', 'sz', 'hs300', 'sz50', 'cyb', 'zxb']
+INDEX_LIST = {'sh': 'sh000001', 'sz': 'sz399001', 'hs300': 'sz399300',
+              'sz50': 'sh000016', 'zxb': 'sz399005', 'cyb': 'sz399006'}
+P_TYPE = {'http': 'http://', 'ftp': 'ftp://'}
+PAGE_NUM = [38, 60, 80, 100]
+FORMAT = lambda x: '%.2f' % x
+DOMAINS = {'sina': 'sina.com.cn', 'sinahq': 'sinajs.cn',
+           'ifeng': 'ifeng.com', 'sf': 'finance.sina.com.cn',
+           'vsf': 'vip.stock.finance.sina.com.cn', 
+           'idx':'www.csindex.com.cn', '163':'money.163.com',
+           'em':'eastmoney.com', 'sseq':'query.sse.com.cn',
+           'sse':'www.sse.com.cn', 'szse':'www.szse.cn',
+           'oss':'tudata.oss-cn-beijing.aliyuncs.com'}
+PAGES = {'fd': 'index.phtml', 'dl': 'downxls.php', 'jv': 'json_v2.php',
+         'cpt': 'newFLJK.php', 'ids': 'newSinaHy.php', 'lnews':'rollnews_ch_out_interface.php',
+         'ntinfo':'vCB_BulletinGather.php', 'hs300b':'000300cons.xls',
+         'hs300w':'000300closeweight.xls','sz50b':'000016cons.xls',
+         'dp':'all_fpya.php', '163dp':'fpyg.html',
+         'emxsg':'JS.aspx', '163fh':'jjcgph.php',
+         'newstock':'vRPD_NewStockIssue.php', 'zz500b':'000905cons.xls',
+         't_ticks':'vMS_tradedetail.php',
+         'qmd':'queryMargin.do', 'szsefc':'FrontController.szse',
+         'ssecq':'commonQuery.do'}
+TICK_COLUMNS = ['time', 'price', 'change', 'volume', 'amount', 'type']
+TODAY_TICK_COLUMNS = ['time', 'price', 'pchange', 'change', 'volume', 'amount', 'type']
+DAY_TRADING_COLUMNS = ['code', 'symbol', 'name', 'changepercent',
+                       'trade', 'open', 'high', 'low', 'settlement', 'volume', 'turnoverratio']
+REPORT_COLS = ['code', 'name', 'eps', 'eps_yoy', 'bvps', 'roe',
+               'epcf', 'net_profits', 'profits_yoy', 'distrib', 'report_date']
+FORECAST_COLS = ['code', 'name', 'type', 'report_date', 'pre_eps', 'range']
+PROFIT_COLS = ['code', 'name', 'roe', 'net_profit_ratio',
+               'gross_profit_rate', 'net_profits', 'eps', 'business_income', 'bips']
+OPERATION_COLS = ['code', 'name', 'arturnover', 'arturndays', 'inventory_turnover',
+                  'inventory_days', 'currentasset_turnover', 'currentasset_days']
+GROWTH_COLS = ['code', 'name', 'mbrg', 'nprg', 'nav', 'targ', 'epsg', 'seg']
+DEBTPAYING_COLS = ['code', 'name', 'currentratio',
+                   'quickratio', 'cashratio', 'icratio', 'sheqratio', 'adratio']
+CASHFLOW_COLS = ['code', 'name', 'cf_sales', 'rateofreturn',
+                 'cf_nm', 'cf_liabilities', 'cashflowratio']
+DAY_PRICE_COLUMNS = ['date', 'open', 'high', 'close', 'low', 'volume', 'price_change', 'p_change',
+                     'ma5', 'ma10', 'ma20', 'v_ma5', 'v_ma10', 'v_ma20', 'turnover']
+INX_DAY_PRICE_COLUMNS = ['date', 'open', 'high', 'close', 'low', 'volume', 'price_change', 'p_change',
+                         'ma5', 'ma10', 'ma20', 'v_ma5', 'v_ma10', 'v_ma20']
+LIVE_DATA_COLS = ['name', 'open', 'pre_close', 'price', 'high', 'low', 'bid', 'ask', 'volume', 'amount',
+                  'b1_v', 'b1_p', 'b2_v', 'b2_p', 'b3_v', 'b3_p', 'b4_v', 'b4_p', 'b5_v', 'b5_p',
+                  'a1_v', 'a1_p', 'a2_v', 'a2_p', 'a3_v', 'a3_p', 'a4_v', 'a4_p', 'a5_v', 'a5_p', 'date', 'time', 's']
+FOR_CLASSIFY_B_COLS = ['code','name']
+FOR_CLASSIFY_W_COLS = ['date','code','weight']
+THE_FIELDS = ['code','symbol','name','changepercent','trade','open','high','low','settlement','volume','turnoverratio']
+TICK_PRICE_URL = '%smarket.%s/%s?date=%s&symbol=%s'
+TODAY_TICKS_PAGE_URL = '%s%s/quotes_service/api/%s/CN_Transactions.getAllPageTime?date=%s&symbol=%s'
+TODAY_TICKS_URL = '%s%s/quotes_service/view/%s?symbol=%s&date=%s&page=%s'
+DAY_PRICE_URL = '%sapi.finance.%s/%s/?code=%s&type=last'
+LIVE_DATA_URL = '%shq.%s/rn=%s&list=%s'
+DAY_PRICE_MIN_URL = '%sapi.finance.%s/akmin?scode=%s&type=%s'
+SINA_DAY_PRICE_URL = '%s%s/quotes_service/api/%s/Market_Center.getHQNodeData?num=80&sort=changepercent&asc=0&node=hs_a&symbol=&_s_r_a=page&page=%s'
+REPORT_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/mainindex/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+FORECAST_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/performance/%s?s_i=&s_a=&s_c=&s_type=&reportdate=%s&quarter=%s&p=%s&num=%s'
+PROFIT_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/profit/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+OPERATION_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/operation/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+GROWTH_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/grow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+DEBTPAYING_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/debtpaying/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+CASHFLOW_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/cashflow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+ALL_STOCK_BASICS_FILE = '%s/tushare/data/all.csv'
+SINA_CONCEPTS_INDEX_URL = '%smoney.%s/q/view/%s?param=class'
+SINA_INDUSTRY_INDEX_URL = '%s%s/q/view/%s'
+SINA_DATA_DETAIL_URL = '%s%s/quotes_service/api/%s/Market_Center.getHQNodeData?page=1&num=400&sort=symbol&asc=1&node=%s&symbol=&_s_r_a=page'
+INDEX_C_COMM = 'sseportal/ps/zhs/hqjt/csi'
+HS300_CLASSIFY_URL = '%s%s/%s/%s'
+HIST_FQ_URL = '%s%s/corp/go.php/vMS_FuQuanMarketHistory/stockid/%s.phtml?year=%s&jidu=%s'
+HIST_INDEX_URL = '%s%s/corp/go.php/vMS_MarketHistory/stockid/%s/type/S.phtml?year=%s&jidu=%s'
+HIST_FQ_FACTOR_URL = '%s%s/api/json.php/BasicStockSrv.getStockFuQuanData?symbol=%s&type=qfq'
+INDEX_HQ_URL = '''%shq.%s/rn=xppzh&list=sh000001,sh000002,sh000003,sh000008,sh000009,sh000010,sh000011,sh000012,sh000016,sh000017,sh000300,sz399001,sz399002,sz399003,sz399004,sz399005,sz399006,sz399100,sz399101,sz399106,sz399107,sz399108,sz399333,sz399606'''
+SSEQ_CQ_REF_URL = '%s%s/assortment/stock/list/name'
+ALL_STK_URL = '%s%s/all.csv'
+INDEX_HEADER = 'code,name,open,preclose,close,high,low,0,0,volume,amount,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,d,c,3\n'
+INDEX_COLS = ['code', 'name', 'change', 'preclose', 'close', 'high', 'low', 'volume', 'amount']
+HIST_FQ_COLS = ['date', 'open', 'high', 'close', 'low', 'volume', 'amount', 'factor']
+HIST_FQ_FACTOR_COLS = ['code','value']
+DATA_GETTING_TIPS = '[Getting data:]'
+DATA_GETTING_FLAG = '#'
+DATA_ROWS_TIPS = '%s rows data found.Please wait for a moment.'
+DATA_INPUT_ERROR_MSG = 'date input error.'
+NETWORK_URL_ERROR_MSG = '获取失败，请检查网络和URL'
+DATE_CHK_MSG = '年度输入错误：请输入1989年以后的年份数字，格式：YYYY'
+DATE_CHK_Q_MSG = '季度输入错误：请输入1、2、3或4数字'
+
+import sys
+PY3 = (sys.version_info[0] >= 3)
+def _write_head():
+    sys.stdout.write(DATA_GETTING_TIPS)
+    sys.stdout.flush()
+
+def _write_console():
+    sys.stdout.write(DATA_GETTING_FLAG)
+    sys.stdout.flush()
+    
+def _write_tips(tip):
+    sys.stdout.write(DATA_ROWS_TIPS%tip)
+    sys.stdout.flush()
+
+def _write_msg(msg):
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    
+def _check_input(year, quarter):
+    if type(year) is str or year < 1989 :
+        raise TypeError(DATE_CHK_MSG)
+    elif quarter is None or type(quarter) is str or quarter not in [1, 2, 3, 4]:
+        raise TypeError(DATE_CHK_Q_MSG)
+    else:
+        return True
