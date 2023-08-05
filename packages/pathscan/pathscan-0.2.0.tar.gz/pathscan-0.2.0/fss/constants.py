f4b041a88c0ba24ea56f375a1a049889d9d676b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:13 +0000

    Add package pathscan/0.2.0/pathscan-0.2.0.tar.gz

---
+++ packages/pathscan/pathscan-0.2.0.tar.gz/pathscan-0.2.0/fss/constants.py
@@ -0,0 +1,20 @@
+# File-types.
+
+FT_DIR = 'dir'
+FT_FILE = 'file'
+
+# Pipeline components.
+
+PC_GENERATOR = 'generator'
+
+# Pipeline component states.
+
+PCS_INITIAL = 0
+PCS_RUNNING = 1
+PCS_FINISHED = 2
+PCS_STOPPED = 3
+
+# Filter types.
+
+FILTER_INCLUDE = 'include'
+FILTER_EXCLUDE = 'exclude'
