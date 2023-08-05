1fef6549dc8a3da50505e80777bd52a4922529f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:39 +0000

    Add package leonardo-module-analytics/2015.0.2/leonardo-module-analytics-2015.0.2.tar.gz

---
+++ packages/leonardo-module-analytics/leonardo-module-analytics-2015.0.2.tar.gz/leonardo-module-analytics-2015.0.2/leonardo_module_analytics/__init__.py
@@ -0,0 +1,27 @@
+
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+default_app_config = 'leonardo_module_analytics.AnalyticsConfig'
+
+
+class Default(object):
+
+    apps = [
+        'analytical',
+        'leonardo_module_analytics',
+    ]
+
+    config = {
+        'GOOGLE_ANALYTICS_PROPERTY_ID': ('UA-62809705-1', _('Google Site identificator')),
+        'GOOGLE_ANALYTICS_SITE_SPEED': (False, _('analyze page speed')),
+        'GOOGLE_ANALYTICS_ANONYMIZE_IP': (False, _('anonymize ip')),
+    }
+
+
+class AnalyticsConfig(AppConfig, Default):
+    name = 'leonardo_module_analytics'
+    verbose_name = ("Leonardo Analytics")
+
+default = Default()
