efab03dead5310d793b6770a735f354e5da2449f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:34 +0000

    Add package westergaard_species/0.3/westergaard_species-0.3.tar.gz

---
+++ packages/westergaard_species/westergaard_species-0.3.tar.gz/westergaard_species-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='westergaard_species',
+      version='0.3',
+      description=("Utilities typeset Westergaard's Species "
+                   "Counterpoint analysis"),
+      url='https://github.com/cryptonomicon314/westergaard-species.git',
+      author='Cryptonomicon',
+      author_email='cryptonomicon.314@gmail.com',
+      license='MIT',
+      packages=['westergaard_species'],
+      install_requires=[
+          'abjad',
+          'PyYAML',
+          'iabjad'
+      ],
+      zip_safe=False)
