3b1b924e2b855c2a1c9d791e4e4281090cf2e355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:44 +0000

    Add package Integer-Tuple-Generator/0.0.1/Integer-Tuple-Generator-0.0.1.tar.gz

---
+++ packages/Integer-Tuple-Generator/Integer-Tuple-Generator-0.0.1.tar.gz/Integer-Tuple-Generator-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='Integer-Tuple-Generator',
+    version='0.0.1',
+    author='Andrew Plummer',
+    author_email='plummer574@gmail.com',
+    url='https://github.com/plumdog/python-integer-tuple-generator/',
+    description='Generation of tuples of integers',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+    ])
