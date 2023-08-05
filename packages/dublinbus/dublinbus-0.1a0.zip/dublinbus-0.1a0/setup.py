bfe7456874d1105caa212caf9892f4d8bd43a3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package dublinbus/0.1a0/dublinbus-0.1a0.zip

---
+++ packages/dublinbus/dublinbus-0.1a0.zip/dublinbus-0.1a0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'dublinbus',
+    version = '0.1alpha0',
+    url = 'http://github.com/vasyl-purchel/dublinbus',
+    author = 'Vasyl Purchel',
+    author_email = 'vasyl.purchel@gmail.com',
+    license = 'MIT',
+    packages = ['dublinbus'],
+    zip_safe = False)
