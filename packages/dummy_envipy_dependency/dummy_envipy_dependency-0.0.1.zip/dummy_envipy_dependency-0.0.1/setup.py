35cd1c76b1da297c49588f49fbe6def734be9af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:36 +0000

    Add package dummy_envipy_dependency/0.0.1/dummy_envipy_dependency-0.0.1.zip

---
+++ packages/dummy_envipy_dependency/dummy_envipy_dependency-0.0.1.zip/dummy_envipy_dependency-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+version = "0.0.1"
+
+setup(name="dummy_envipy_dependency",
+      version=version,
+      author="Uwe Schmitt",
+      author_email="uwe.schmitt@id.ethz.ch",
+      description="we need this package to test envipy updates",
+      license="BSD",
+      packages=["dummy_envipy_dependency"],
+      )
