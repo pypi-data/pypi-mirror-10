cd0e21f94a3fcea31c67f3df88ce7de021a36f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package django-webpack-loader/0.0.4.2/django-webpack-loader-0.0.4.2.tar.gz

---
+++ packages/django-webpack-loader/django-webpack-loader-0.0.4.2.tar.gz/django-webpack-loader-0.0.4.2/webpack_loader/templatetags/webpack_loader.py
@@ -0,0 +1,21 @@
+import datetime
+from django import template
+
+from ..utils import get_bundle
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def render_bundle(bundle_name):
+    bundle = get_bundle(bundle_name)
+    tags = []
+    for chunk in bundle:
+
+        if chunk['name'].endswith('.js'):
+            url = chunk.get('publicPath') or chunk['url']
+            tags.append('<script type="text/javascript" src="{}"/>'.format(url))
+        elif chunk['name'].endwith('.css'):
+            tags.append('<link type="text/css" href="{}" rel="stylesheet"/>'.format(url))
+    return '\n'.join(tags)
