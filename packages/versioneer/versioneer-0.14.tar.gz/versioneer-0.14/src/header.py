1f9da3648387f654ac2d994c85924abe406db9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:04 +0000

    Add package versioneer/0.14/versioneer-0.14.tar.gz

---
+++ packages/versioneer/versioneer-0.14.tar.gz/versioneer-0.14/src/header.py
@@ -0,0 +1,26 @@
+
+# Version: @VERSIONEER-VERSION@
+
+"""
+@README@
+"""
+
+import errno
+import os
+import re
+import subprocess
+import sys
+from distutils.command.build import build as _build
+from distutils.command.sdist import sdist as _sdist
+from distutils.core import Command
+
+# these configuration settings will be overridden by setup.py after it
+# imports us
+versionfile_source = None
+versionfile_build = None
+tag_prefix = None
+parentdir_prefix = None
+VCS = None
+
+# these dictionaries contain VCS-specific tools
+LONG_VERSION_PY = {}
