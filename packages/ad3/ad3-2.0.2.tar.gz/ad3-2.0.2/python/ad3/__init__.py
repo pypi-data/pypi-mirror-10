054ae651af25d1fd36952b0e8d27cbf075cbb2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package ad3/2.0.2/ad3-2.0.2.tar.gz

---
+++ packages/ad3/ad3-2.0.2.tar.gz/ad3-2.0.2/python/ad3/__init__.py
@@ -0,0 +1,5 @@
+from .factor_graph import PBinaryVariable, PFactorGraph, PMultiVariable
+from .simple_inference import simple_grid, general_graph
+
+__all__ = ['PBinaryVariable', 'PFactorGraph', 'PMultiVariable', 'simple_grid',
+           'general_graph']
