a95d104e226b7ee5f5db0b1d58237f613dc80a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package ga/0.1.1.dev/ga-0.1.1.dev.zip

---
+++ packages/ga/ga-0.1.1.dev.zip/ga-0.1.1.dev/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ga',
+      version='0.1.1.dev',
+      license='GNU GPLv3',
+      description='A lightweight genetic algorithm library written in pure Python',
+      author='Matt Scruggs',
+      url='https://github.com/mdscruggs/ga',
+      packages=['ga', 'ga.examples'],
+     )
