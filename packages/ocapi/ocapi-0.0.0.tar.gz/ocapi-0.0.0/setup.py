c9666e4b177bdaa24eddc5409cb4345649ebb3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package ocapi/0.0.0/ocapi-0.0.0.tar.gz

---
+++ packages/ocapi/ocapi-0.0.0.tar.gz/ocapi-0.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import ocapi.info as pkg
+
+
+if __name__ == "__main__":
+    name = pkg.__name__.split(".")[0]
+    setup(
+      name=name,
+      packages=[name],
+      author=pkg.__author__,
+      author_email=pkg.__email__,
+      long_description=pkg.__doc__,
+      url=pkg.__web__,
+      keywords=[name, 'automatization', 'api'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+      test_suite="test")
