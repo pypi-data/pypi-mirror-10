d3d121aa24b511f5d56dfe01e7ca8df2d30a916c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package AutoHDL/3.0.1196/AutoHDL-3.0.1196.win32.zip

---
+++ packages/AutoHDL/AutoHDL-3.0.1196.win32.zip/Python34/Scripts/hdl.py
@@ -0,0 +1,39 @@
+import argparse
+import sys
+import subprocess
+import logging
+
+import autohdl.structure as structure
+import autohdl.pkg_info as pkg_info
+import autohdl.documentation as documentation
+import autohdl.configuration as configuration
+
+alog = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Helper to create designs')
+    parser.add_argument('-doc', action='store_true', help='extended documentation in browser')
+    parser.add_argument('-name', default='',
+                        help='set design name and create structure [default - current directory name]')
+    parser.add_argument('-version', action='store_true', help='display package version')
+    parser.add_argument('-edit', choices=['default_build', 'toolchain'], help='edit default build.yaml file')
+    args = parser.parse_args()
+
+    if args.version:
+        print('AutoHDL version: ' + pkg_info.version())
+    elif args.doc:
+        documentation.handler('index')
+    elif args.edit:
+        if args.edit == 'default_build':
+            subprocess.Popen('notepad {}/Lib/site-packages/autohdl/data/build.yaml'.format(sys.prefix))
+        elif args.edit == 'toolchain':
+            subprocess.Popen('notepad {}/Lib/site-packages/autohdl_cfg/toolchain.yaml'.format(sys.prefix))
+    else:
+        dsn = structure.generate(path=args.name)
+        print(dsn)
+
+
+if __name__ == '__main__':
+    configuration.copy()
+    main()
