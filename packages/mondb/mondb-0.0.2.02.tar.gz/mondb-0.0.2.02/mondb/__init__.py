f99f1790d898bc524c9c0be892d0cc436ce8f6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package mondb/0.0.2.02/mondb-0.0.2.02.tar.gz

---
+++ packages/mondb/mondb-0.0.2.02.tar.gz/mondb-0.0.2.02/mondb/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2015
+
+# author: plasmashadow.
+#  company: StrawHatPirates.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software
+"""
+  MongoDB Models are just like ndb models where the user can persist
+  the records with extending it with mondb.Model class.
+
+  class Person(mondb.Model):
+       name = mondb.StringProperty()
+       age  = mondb.IntegerProperty()
+       person_id = mondb.StringProperty(key = True)
+
+  Note: that the primary key is set to custom key
+"""
+from Connection import *
+from Property import *
+from Document import *
+
+AUTO_CREATE_FIELDS = False
+
+class EngineException(Exception):
+    pass
+
