b04093ea6dcab3d012935006c9928039992fd6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package google_translator/0.1/google_translator-0.1.tar.gz

---
+++ packages/google_translator/google_translator-0.1.tar.gz/google_translator-0.1/google_translator/Lang.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 语言
+
+
+class Lang(object):
+    # @AttributeType string
+    # 南非语
+    afrikaans = "af"
+    # @AttributeType string
+    # 阿尔巴尼亚语
+    albanian = "sq"
+    # @AttributeType string
+    # 中文简体
+    chinese_simplified = "zh-CN"
+    # @AttributeType string
+    # 台湾
+    chinese_traditional = "zh-TW"
+    # @AttributeType string
+    # 英文
+    english = "en"
+    # @AttributeType string
+    # 自动检测(只适用于源语言)
+    auto = "auto"
+
