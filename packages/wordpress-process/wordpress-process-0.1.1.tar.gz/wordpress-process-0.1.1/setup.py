e56261b00b463c0e004de830326e6a20073eb471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package wordpress-process/0.1.1/wordpress-process-0.1.1.tar.gz

---
+++ packages/wordpress-process/wordpress-process-0.1.1.tar.gz/wordpress-process-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name='wordpress-process',    # This is the name of your PyPI-package.
+      version='0.1.1',                # Update the version number for new releases
+      scripts=['wordpress'],   # The name of your scipt, and also the command you'll be using for calling it
+      author='Morfat Mosoti Ogega',
+      author_email='morfatmosoti@gmail.com',
+      license='MIT'
+    )
