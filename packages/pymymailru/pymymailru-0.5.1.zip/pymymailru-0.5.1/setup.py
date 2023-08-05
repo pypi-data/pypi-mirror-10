114b8e362cb25897b976b1cd559b2a7906e349a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package pymymailru/0.5.1/pymymailru-0.5.1.zip

---
+++ packages/pymymailru/pymymailru-0.5.1.zip/pymymailru-0.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymymailru',
+      version='0.5.1',
+      description='Python API to MyMailRu API',
+      url='https://github.com/dbf256/py-mymailru',
+      author='Alexey Moskvin',
+      author_email='dbf256@gmail.com',
+      license='MIT',
+      packages=['pymymailru'],
+      zip_safe=False)
