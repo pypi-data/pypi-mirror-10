18db4f8e9e27638f09569a3f49ab8c3af6849abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package appengine-sdk/1.9.19.post1/appengine_sdk-1.9.19.post1-py2-none-any.whl

---
+++ packages/appengine-sdk/appengine_sdk-1.9.19.post1-py2-none-any.whl/appengine_sdk/commands.py
@@ -0,0 +1,18 @@
+def make_shim(script):
+    def main():
+        import os
+        global __name__
+        global __file__
+        global __path__
+
+        sdk_root = os.environ['GAE_SDK_ROOT'] = os.path.join(os.path.dirname(__file__), "google_appengine")
+        __file__ = os.path.join(sdk_root, script)
+        __name__ = "__main__"
+        del os, sdk_root
+
+        execfile(__file__, globals(), globals())
+    return main
+
+remote_api_shell = make_shim('remote_api_shell.py')
+dev_appserver = make_shim('dev_appserver.py')
+appcfg = make_shim('appcfg.py')
