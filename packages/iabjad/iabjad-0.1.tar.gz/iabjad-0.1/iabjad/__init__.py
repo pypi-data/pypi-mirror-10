25c871c1f9d6446472b2c80abdbed79af4530ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package iabjad/0.1/iabjad-0.1.tar.gz

---
+++ packages/iabjad/iabjad-0.1.tar.gz/iabjad-0.1/iabjad/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['ishow', 'ishow_preview_', 'ishow_',
+           'export', 'export_preview', 'export_']
+
+from iabjad import ishow, ishow_preview, ishow_, \
+              export, export_preview, export_
---
+++ packages/iabjad/iabjad-0.1.tar.gz/iabjad-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='iabjad',
+      version='0.1',
+      description='Utilities to work with Abjad from IPython',
+      url='https://github.com/cryptonomicon314/iabjad.git',
+      author='Cryptonomicon',
+      author_email='cryptonomicon.314@gmail.com',
+      license='MIT',
+      packages=['iabjad'],
+      install_requires=[
+          'abjad'
+      ],
+      zip_safe=False)
