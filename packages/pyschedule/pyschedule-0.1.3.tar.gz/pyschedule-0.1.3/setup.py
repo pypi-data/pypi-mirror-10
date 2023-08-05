4c0ace98ff05d22b42b7b3c9308c7298b9735367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:08 +0000

    Add package pyschedule/0.1.3/pyschedule-0.1.3.tar.gz

---
+++ packages/pyschedule/pyschedule-0.1.3.tar.gz/pyschedule-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyschedule',
+      version='0.1.3',
+      description='A python package to formulate and solve resource-constrained scheduling problems: flow- and job-shop, travelling salesman, vehicle routing and all kind of combinations',
+      url='https://github.com/timnon/pyschedule',
+      author='Tim Nonner',
+      author_email='tim@nonner.de',
+      license='Apache 2.0',
+      packages=['pyschedule'],
+      install_requires=['pulp'])
+
