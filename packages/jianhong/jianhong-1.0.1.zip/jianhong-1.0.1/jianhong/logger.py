ff772599f148504bc1e9b5f93c7f720064690462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:51 +0000

    Add package jianhong/1.0.1/jianhong-1.0.1.zip

---
+++ packages/jianhong/jianhong-1.0.1.zip/jianhong-1.0.1/jianhong/logger.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+
+import os
+import sys
+import logging
+from logging.handlers import MemoryHandler, RotatingFileHandler
+
+import fileutil
+from cm_client_info.config import Path
+
+class Logger:
+
+    def __init__(self, name, size = 10, backupCount = 50, cacheRecords = 1, print_to_console=True): 
+        '''  size : 单个日志文件的大小，单位是M。
+             backupCount :备份的最大日志文件数'
+             cacheRecords :日志缓存数。达到该数字才会写硬盘。flushLevel以上级别的除外'''
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        logdir = os.path.join(Path.LOG, name)
+        fileutil.ensure_dir_exists(logdir)
+        logfile = os.path.join(logdir, '%s.log' % name)
+        hdlr = RotatingFileHandler(logfile, 'a', maxBytes=1024 * 1024 *size, backupCount=backupCount)
+        hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+        mh = MemoryHandler(cacheRecords,flushLevel=logging.INFO, target=hdlr) #flushLevel（包含）以上级别的立刻写硬盘
+        self.logger.addHandler(mh)
+        if print_to_console:
+            #将大于或等于DEBUG级别的信息输出到控件台
+            hdlr = logging.StreamHandler(sys.stdout)
+            hdlr.setFormatter(logging.Formatter("%(message)s", ""))
+            hdlr.setLevel(logging.DEBUG)
+            self.logger.addHandler(hdlr)
+
+    def getLogger(self):
+        return self.logger
