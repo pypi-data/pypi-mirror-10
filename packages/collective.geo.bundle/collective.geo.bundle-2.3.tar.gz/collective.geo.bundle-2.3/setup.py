8c7bcc4e7575aa09bd5d5ebb69b1ee22c4ee8c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:29 +0000

    Add package collective.geo.bundle/2.3/collective.geo.bundle-2.3.tar.gz

---
+++ packages/collective.geo.bundle/collective.geo.bundle-2.3.tar.gz/collective.geo.bundle-2.3/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+version = '2.3'
+
+setup(
+    name='collective.geo.bundle',
+    version=version,
+    description="Plone Maps (collective.geo)",
+    long_description=(
+        open("OVERVIEW.rst").read() + "\n" +
+        open("README.rst").read() + "\n" +
+        open("HISTORY.rst").read()
+    ),
+    classifiers=[
+        "Framework :: Plone",
+        "Topic :: Internet",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Programming Language :: Python",
+    ],
+    keywords='Zope Plone GIS KML Google Maps Bing Yahoo OpenLayers',
+    author='Giorgio Borelli',
+    author_email='giorgio@giorgioborelli.it',
+    url='http://plone.org/products/collective.geo',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['collective', 'collective.geo'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'BeautifulSoup',
+        'collective.geo.mapwidget >= 2.2',
+        'collective.geo.contentlocations >= 3.1',
+        'collective.geo.kml >= 3.2',
+        'plone.app.dexterity',
+        'collective.geo.behaviour >= 1.2'
+    ],
+    extras_require={
+        'dexterity': [
+            # not used
+        ],
+        "test": [
+            "plone.app.testing",
+            "plone.app.robotframework"
+        ]
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
