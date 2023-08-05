781e9564d1b9d8cae2c7dc37cd30659d36bf60e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:22 +0000

    Add package Thanatos/0.1.0/Thanatos-0.1.0-py2-none-any.whl

---
+++ packages/Thanatos/Thanatos-0.1.0-py2-none-any.whl/thanatos/__init__.py
@@ -0,0 +1,11 @@
+
+import logging
+
+from thanatos import database
+from thanatos import questions
+from thanatos import categories
+from thanatos import ccp_image_server
+
+__version__ = '0.1.0'
+
+_log = logging.getLogger('thanatos')
