5076e538fd4791f8f17c73ade5a1edf6cf09096f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:20 +0000

    Add package charm-benchmark/0.0.1/charm-benchmark-0.0.1.tar.gz

---
+++ packages/charm-benchmark/charm-benchmark-0.0.1.tar.gz/charm-benchmark-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+install_requires = [
+    'PyYAML',
+    'charmhelpers'
+]
+
+tests_require = [
+    'coverage',
+    'nose',
+    'pep8',
+]
+
+
+setup(
+    name='charm-benchmark',
+    version='0.0.1',
+    description='Library to aid in the creation of benchmark actions in Juju',
+    install_requires=install_requires,
+    url="https://launchpad.net/~cabs-team",
+    packages=['charmbenchmark'],
+    entry_points={
+        'console_scripts': [
+            'benchmark-start=charmbenchmark.Benchmark.cli:start',
+            'benchmark-finish=charmbenchmark.Benchmark:finish',
+            # benchmarks <actions>
+            # composite <>
+        ]
+    }
+)
