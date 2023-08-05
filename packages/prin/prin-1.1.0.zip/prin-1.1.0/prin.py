814063c8a89ea36fc5a76eb3fb95b2d9bb3dbe29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package prin/1.1.0/prin-1.1.0.zip

---
+++ packages/prin/prin-1.1.0.zip/prin-1.1.0/prin.py
@@ -0,0 +1,9 @@
+def prin(num,str="jack"):
+    while num != 0:
+        print str
+        num -= 1
+        
+
+
+
+"""This is how you print"""
---
+++ packages/prin/prin-1.1.0.zip/prin-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name       = 'prin',
+    version    = '1.1.0',
+    py_modules = ['prin'],
+    author     = 'JackY',
+    description= 'simplePrint'
+)
