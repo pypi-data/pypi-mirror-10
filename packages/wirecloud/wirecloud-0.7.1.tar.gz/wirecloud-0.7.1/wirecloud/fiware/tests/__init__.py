3855f4672b370bd903aa6c2bf0237a46699870ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package wirecloud/0.7.1/wirecloud-0.7.1.tar.gz

---
+++ packages/wirecloud/wirecloud-0.7.1.tar.gz/wirecloud-0.7.1/wirecloud/fiware/tests/__init__.py
@@ -0,0 +1,6 @@
+from wirecloud.fiware.tests.marketplace import MarketplaceTestCase
+from wirecloud.fiware.tests.store import StoreTestCase
+from wirecloud.fiware.tests.proxy import ProxyTestCase
+from wirecloud.fiware.tests.south_migrations import FIWARESouthMigrationsTestCase
+from wirecloud.fiware.tests.views import FIWAREViewsTestCase
+from wirecloud.fiware.tests.selenium import *
