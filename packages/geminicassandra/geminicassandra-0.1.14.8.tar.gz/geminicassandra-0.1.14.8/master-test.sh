49c5590dece8319db5d0e41bc0c44e7aaec61077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:24 +0000

    Add package geminicassandra/0.1.13.1/geminicassandra-0.1.14.8.tar.gz

---
+++ packages/geminicassandra/geminicassandra-0.1.14.8.tar.gz/geminicassandra-0.1.14.8/master-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+check()
+{
+        if diff $1 $2; then
+        echo ok
+        else
+        echo fail
+        fi
+}
+export -f check
+
+export cassandra_ips="127.0.0.1"
+
+cd test
+
+bash cassandra-test-setup.sh
+
+bash cassandra-test-query.sh
+
+bash test-query-multicore.sh
+
+bash test-load.sh
+
+bash test-wildcards.sh
+
+bash test-wildcards-multicore.sh
+
+bash test-genotypes.sh
+
+bash test-exac.sh
+
+bash test-genome.sh
+
+bash test-columns.sh
+
+bash test-clinvar.sh
