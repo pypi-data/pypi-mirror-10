bdd6c0b37160fdcb0c4ce7aad912071924a46bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package mabs/0.1.2/mabs-0.1.2.tar.gz

---
+++ packages/mabs/mabs-0.1.2.tar.gz/mabs-0.1.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mabs',
+    version = '0.1.2',
+    keywords = ('mabs', 'egg'),
+    description = 'used to control server by group with password,系统集中管理',
+    license = 'mabsvs License',
+
+    url = 'https://github.com/targetoyes/mabs',
+    author = 'targetoyes',
+    author_email = 'targetoyes@163.com',
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = ["paramiko>=1.15.2", "sh>=1.11", "argparse>=1.2.1"],
+
+    scripts=['bin/mabs'],
+
+    package_data={'/etc/ansible/': ['mabs/hosts.ini', 'mabs/secret.ini']},
+)
