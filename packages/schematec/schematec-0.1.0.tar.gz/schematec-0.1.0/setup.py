4a1d91f8b393367df11e5412543cf9110946a147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:49 +0000

    Add package schematec/0.1.0/schematec-0.1.0.tar.gz

---
+++ packages/schematec/schematec-0.1.0.tar.gz/schematec-0.1.0/setup.py
@@ -0,0 +1,40 @@
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = ''
+
+with open('schematec/__init__.py', 'r') as fd:
+    regex = re.compile(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]')
+    for line in fd:
+        m = regex.match(line)
+        if m:
+            version = m.group(1)
+            break
+
+setup(
+    name='schematec',
+    packages=['schematec'],
+    package_data={'': ['LICENSE']},
+    version=version,
+    description='Data models for Mongo',
+    author='Andrey Gubarev',
+    author_email='mylokin@me.com',
+    url='https://github.com/mylokin/schematec',
+    keywords=['schema'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Database',
+        'Topic :: Database :: Database Engines/Servers',
+    ),
+)
