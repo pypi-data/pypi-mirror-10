07981bf00563da7ec78b08ddd70202324a485b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package vlermv/1.2.0/vlermv-1.2.0.tar.gz

---
+++ packages/vlermv/vlermv-1.2.0.tar.gz/vlermv-1.2.0/vlermv/_cache.py
@@ -0,0 +1,44 @@
+from functools import partial
+
+from . import transformers as t
+from ._vlermv import Vlermv
+
+def cache(*args, key_transformer = t.tuple, **kwargs):
+    '''
+    Cache a function with a :py:class:`~vlermv.Vlermv`. ::
+
+        @cache('~/.http')
+        def get(url):
+            return requests.get(url, auth = ('username', 'password'))
+
+    The args and kwargs get passed to the Vlermv with some slight changes.
+    Here are the changes.
+
+    First, the default ``key_transformer`` is the tuple transformer
+    rather than the simple transformer.
+
+    Second, it is valid for cache to be called without arguments.
+    Vlermv would ordinarily fail if no arguments were passed to it.
+    If you pass no arguments to cache, the Vlermv directory argument
+    (the one required argument) will be set to the name of the function.
+
+    Third, you are more likely to use the ``cache_exceptions`` keyword
+    argument; see :py:class:`~vlermv.Vlermv` for documentation on that.
+    '''
+    def decorator(func):
+        if len(args) == 0:
+            _args = (func.__name__,)
+        else:
+            _args = args
+        v = Vlermv(*_args, key_transformer = key_transformer,
+            parent_directory = kwargs.pop('parent_directory', ''), **kwargs)
+        v.func = func
+        return v
+    return decorator
+
+def archive(*args, key_transformer = t.magic, **kwargs):
+    '''
+    A thin wrapper with reasonable defaults archival caches based on
+    :py:func:`vlermv.cache` and :py:func:`vlermv.serializers.archive`
+    '''
+    return cache(*args, key_transformer = t.archive(t.magic), **kwargs)
