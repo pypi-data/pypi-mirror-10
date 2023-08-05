1bfd6fd0e7908605f29a2624c5be848642e50432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:39 +0000

    Add package tdigest/0.1.0/tdigest-0.1.0.tar.gz

---
+++ packages/tdigest/tdigest-0.1.0.tar.gz/tdigest-0.1.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='tdigest',
+      version='0.1.0',
+      description='T-Digest data structure',
+      author='Cam Davidson-pilon',
+      author_email='cam.davidson.pilon@gmail.com',
+      url='https://github.com/CamDavidsonPilon/tdigest',
+      packages=['tdigest'],
+      long_description=read('README.md'),
+      install_requires=[
+          "bintrees",
+      ],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        ],
+      license="MIT",
+      keywords='percentile, median, data structure, quantitle, distributed, qdigest',
+
+      )
