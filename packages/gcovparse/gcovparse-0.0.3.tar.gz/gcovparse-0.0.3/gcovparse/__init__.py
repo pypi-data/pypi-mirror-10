becfd7e2175db9f39af83edca4992d9807fbdc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:53 +0000

    Add package gcovparse/0.0.3/gcovparse-0.0.3.tar.gz

---
+++ packages/gcovparse/gcovparse-0.0.3.tar.gz/gcovparse-0.0.3/gcovparse/__init__.py
@@ -0,0 +1,32 @@
+version = VERSION = __version__ = "0.0.3"
+
+
+def gcovparse(combined):
+    # clean and strip lines
+    assert ':Source:' in combined, 'gcov file is missing ":Source:" line(s)'
+    files = filter(lambda f: f != '', combined.strip().split("0:Source:"))
+    reports = map(_part, files[1:])
+    return reports
+
+
+def _part(chunk):
+    report = {
+        "file": chunk.split('\n', 1)[0],
+        "lines": []
+    }
+    map(lambda l: _line(l, report), chunk.strip().split('\n')[1:])
+
+    return report
+
+
+def _line(l, report):
+    line = l.split(':', 2)
+    if len(line) == 3:
+        hit, line, data = tuple(line)
+        if '#' in hit and data.strip() == '}':
+            # ignore lines #####:   33:}
+            return None
+        elif '-' in hit:
+            return None
+        elif int(line) > 0:
+            report['lines'].append(dict(line=int(line.strip()), hit=0 if '#' in hit else int(hit.strip())))
