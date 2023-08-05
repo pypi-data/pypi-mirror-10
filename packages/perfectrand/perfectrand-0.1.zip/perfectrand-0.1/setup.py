6e1f6c0d11775ee9a40b5064ec1074c3d6bf0753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:47 +0000

    Add package perfectrand/0.1/perfectrand-0.1.zip

---
+++ packages/perfectrand/perfectrand-0.1.zip/perfectrand-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='perfectrand',
+      version='0.1',
+      description='The perfect random number generator',
+      url='https://github.com/maral/perfectrand',
+      author='Marek Lisy',
+      author_email='marek.lisy@centrum.cz',
+      license='MIT',
+      packages=['perfectrand'],
+      zip_safe=False)
