71beca193730df144c81d2fbfc83f3b7b9161035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:23 +0000

    Add package littleleafirst/1.2.0/littleleafirst-1.2.0.zip

---
+++ packages/littleleafirst/littleleafirst-1.2.0.zip/littleleafirst-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name          ='littleleafirst',
+    version       ='1.2.0',
+    py_modules    =['nester'],
+    author        ='hfpython',
+    author_email  ='hfpython@headfors',
+    url           ='http://www.headfirstlabs.com',
+    dercription   ='A simple printer of nested listed',
+    )
+    
