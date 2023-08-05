b27ab95084de84f02f1df5e635834fa633424dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package django-common-helpers/0.7.0/django-common-helpers-0.7.0.tar.gz

---
+++ packages/django-common-helpers/django-common-helpers-0.7.0.tar.gz/django-common-helpers-0.7.0/django_common/tests.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, unicode_literals, with_statement, division
+
+from django.utils import unittest
+
+
+class SimpleTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_example(self):
+        """Some test example"""
+        pass
