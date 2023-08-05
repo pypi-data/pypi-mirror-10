76890bce304317a21ea381ef10346b2ce82a53c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:50 +0000

    Add package hxx_nester1/1.0.0/hxx_nester1-1.0.0.zip

---
+++ packages/hxx_nester1/hxx_nester1-1.0.0.zip/hxx_nester1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name='hxx_nester1',
+		version='1.0.0',
+		py_modules=['nester1'],
+		author='xiaoxiahu',
+		author_email='hcc_2014@163.com',
+		url='http://www.emily_hxx@163',
+		description='This is a simple print list',
+)
