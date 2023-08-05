9ed573c703a4fb7bfc248726c0397dc614d9b2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package thefuck/1.20/thefuck-1.20.tar.gz

---
+++ packages/thefuck/thefuck-1.20.tar.gz/thefuck-1.20/thefuck/rules/sudo.py
@@ -0,0 +1,17 @@
+patterns = ['permission denied',
+            'EACCES',
+            'pkg: Insufficient privileges',
+            'you cannot perform this operation unless you are root',
+            'non-root users cannot',
+            'Operation not permitted']
+
+
+def match(command, settings):
+    for pattern in patterns:
+        if pattern.lower() in command.stderr.lower():
+            return True
+    return False
+
+
+def get_new_command(command, settings):
+    return u'sudo {}'.format(command.script)
