17a7c59ca6957881a7002179bbb472b213f9485d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:26 +0000

    Add package aio.app/0.0.24/aio.app-0.0.24.tar.gz

---
+++ packages/aio.app/aio.app-0.0.24.tar.gz/aio.app-0.0.24/aio/app/tests/test_run_schedulers.py
@@ -0,0 +1,42 @@
+import os
+import asyncio
+
+import aio.app
+from aio.testing import aiofuturetest
+from aio.app.testing import AioAppTestCase
+from aio.app.runner import runner
+
+test_dir = os.path.dirname(__file__)
+
+SCHEDULER_CONFIG = """
+[schedule/test]
+every: 2
+func: aio.app.tests._test_scheduler
+"""
+
+
+class RunCommandSchedulersTestCase(AioAppTestCase):
+
+    @aiofuturetest(timeout=5)
+    def test_run_schedulers(self):
+
+        class Counter:
+            hit_count = 0
+        counter = Counter()
+
+        @asyncio.coroutine
+        def scheduler(name):
+            counter.hit_count += 1
+
+        aio.app.tests._test_scheduler = scheduler
+
+        yield from runner(
+            ['run'],
+            config_string=SCHEDULER_CONFIG)
+
+        @asyncio.coroutine
+        def test_complete():
+            # this is called 5 seconds after the server has started
+            self.assertTrue(counter.hit_count == 3)
+
+        return test_complete
