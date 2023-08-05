3307f19b180b0605a6da87f4d90b1786f13110bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:49 +0000

    Add package govuk-template/0.1.40/govuk-template-0.1.40.tar.gz

---
+++ packages/govuk-template/govuk-template-0.1.40.tar.gz/govuk-template-0.1.40/govuk_template/flask/assets/routes.py
@@ -0,0 +1,12 @@
+from flask import send_from_directory
+from os import path
+
+
+__dot = path.dirname(path.realpath(__file__))
+__asset_dir = path.join(__dot, '../../assets')
+
+
+def register_routes(blueprint):
+    @blueprint.route('/assets/<path:path>')
+    def serve_assets(path):
+        return send_from_directory(__asset_dir, path)
