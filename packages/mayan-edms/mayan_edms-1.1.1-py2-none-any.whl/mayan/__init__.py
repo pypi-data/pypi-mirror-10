3bc02fe4ec63ba63251b8d1c8d25e3b23d59e010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package mayan-edms/1.1.1/mayan_edms-1.1.1-py2-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-1.1.1-py2-none-any.whl/mayan/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+__title__ = 'Mayan EDMS'
+__version__ = '1.1.1'
+__build__ = 0x010101
+__author__ = 'Roberto Rosario'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2011-2015 Roberto Rosario'
