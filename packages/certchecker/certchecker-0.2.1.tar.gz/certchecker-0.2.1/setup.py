93c42e25b615890abc6896511b610c3712036354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:25 +0000

    Add package certchecker/0.2.1/certchecker-0.2.1.tar.gz

---
+++ packages/certchecker/certchecker-0.2.1.tar.gz/certchecker-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import os
+
+requirements = [
+    'boto>=2.37.0',
+    'click>=4.0'
+]
+
+setup(
+    name='certchecker',
+    description="A tool to check SSL Certificates in AWS",
+    version='0.2.1',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    author='Steven Tsiang',
+    author_email='steven@scopely.com',
+    url='https://github.com/tsiang/certchecker',
+    license=open("LICENSE").read(),
+    install_requires=requirements,
+    entry_points="""
+        [console_scripts]
+        certchecker=certchecker.__main__:main
+    """
+)
