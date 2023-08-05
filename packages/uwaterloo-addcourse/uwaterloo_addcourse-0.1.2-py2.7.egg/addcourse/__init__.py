8e65fce64e428c5e8f4d2ad369dfd17e4780a8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package uwaterloo-addcourse/0.1.2/uwaterloo_addcourse-0.1.2-py2.7.egg

---
+++ packages/uwaterloo-addcourse/uwaterloo_addcourse-0.1.2-py2.7.egg/addcourse/__init__.py
@@ -0,0 +1,11 @@
+"""Repeatedly ask QUEST to add you into a particular course."""
+
+# Expose useful imports.
+from .course_adder import addcourse
+from .class_numbers import numbers
+from .version import (
+    __author__,
+    __license__,
+    __copyright__,
+    __version__,
+)
