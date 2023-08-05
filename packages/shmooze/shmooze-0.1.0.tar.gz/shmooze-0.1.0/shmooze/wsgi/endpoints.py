b4a4b30a6092fbd4db18d7e699267a320f5fcd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:58 +0000

    Add package shmooze/0.1.0/shmooze-0.1.0.tar.gz

---
+++ packages/shmooze/shmooze-0.1.0.tar.gz/shmooze-0.1.0/shmooze/wsgi/endpoints.py
@@ -0,0 +1,12 @@
+import util
+import pkg_resources
+import shmooze.settings as settings
+
+static_endpoints = {
+    '/': settings.static_path
+}
+
+wsgi_endpoints = {}
+
+for service, portnum in settings.ports.items():
+    wsgi_endpoints["/{}".format(service)] = util.wsgi_control("localhost", portnum)
