2ef1c30c3125bfb6306c41c925ec7217aadad723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package unnest.py/1.1.0/unnest.py-1.1.0.tar.gz

---
+++ packages/unnest.py/unnest.py-1.1.0.tar.gz/unnest.py-1.1.0/unnest.py
@@ -0,0 +1,15 @@
+"""This is the unnest.py module, it provides one function called print_lil()
+which prints lists that may or may not include nested lists."""
+
+def print_lil(a_list, level=0):
+    """This function takes a positional arg called "a_list",
+    which is a python list, which may contain nested lists.
+    Each list is printed recursively printed to the screen."""
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lil(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+        
