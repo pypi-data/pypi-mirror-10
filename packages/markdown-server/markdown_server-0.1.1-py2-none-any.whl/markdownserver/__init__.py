acc1a28e554592441299ab83407e25de554788fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package markdown-server/0.1.1/markdown_server-0.1.1-py2-none-any.whl

---
+++ packages/markdown-server/markdown_server-0.1.1-py2-none-any.whl/markdownserver/__init__.py
@@ -0,0 +1,17 @@
+from bottle import route,run,template,static_file
+from markdown_converter import MarkdownConverter
+from env import *
+
+converter = MarkdownConverter()
+
+@route('/<resource>')
+def gfmize(resource):
+
+    html_file_name = converter.convert(resource)
+    return static_file(html_file_name, root=ms_root)
+
+def main():
+    run(host=ms_host,port=ms_port,debug=ms_debug,reloader=ms_reloader)
+
+if __name__ == '__main__':
+    main()
