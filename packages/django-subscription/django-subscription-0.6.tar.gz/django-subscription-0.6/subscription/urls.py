80565ae4d9e5f3ecc73e6a9008cc6bd0a14ebf72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:52 +0000

    Add package django-subscription/0.6/django-subscription-0.6.tar.gz

---
+++ packages/django-subscription/django-subscription-0.6.tar.gz/django-subscription-0.6/subscription/urls.py
@@ -0,0 +1,12 @@
+try:
+   from django.conf.urls.defaults import patterns, url  # Django 1.4
+except ImportError:
+   from django.conf.urls import patterns, url  # Django >= 1.6
+from subscription.views import SubscriptionView
+
+urlpatterns = patterns('subscription.views',
+   url(r'^$', SubscriptionView.as_view(), name="subscriptions"),
+   url(r'^actstream/ping/$', 'actstream_ping', name="actstream_ping"),
+   url('unsubscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', 'unsubscribe', name="subscription_unsubscribe"),
+   url('subscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', 'subscribe', name="subscription_subscribe"),
+)
