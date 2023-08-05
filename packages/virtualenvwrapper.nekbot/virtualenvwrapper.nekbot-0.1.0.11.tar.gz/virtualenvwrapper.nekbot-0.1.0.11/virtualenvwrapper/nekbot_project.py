da700ee56cc72df5c531670536b1b3c7cb97efbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package virtualenvwrapper.nekbot/0.1.0.11/virtualenvwrapper.nekbot-0.1.0.11.tar.gz

---
+++ packages/virtualenvwrapper.nekbot/virtualenvwrapper.nekbot-0.1.0.11.tar.gz/virtualenvwrapper.nekbot-0.1.0.11/virtualenvwrapper/nekbot_project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""Create bots automatically with virtualenvwrapper.
+"""
+__author__ = 'nekmo'
+
+import logging
+import os
+import subprocess
+
+log = logging.getLogger('virtualenvwrapper.nekbot')
+
+
+def template(args):
+    """Installs NekBot and runs nekbot-admin to create a new bot.
+    """
+    project, project_dir = args
+    os.chdir(project_dir)
+    subprocess.check_call(['pip', 'install', 'nekbot'])
+    log.info('Running "nekbot-admin createbot %s ."', project)
+    subprocess.check_call(['nekbot-admin', 'createbot', project, '.'])
+    return
