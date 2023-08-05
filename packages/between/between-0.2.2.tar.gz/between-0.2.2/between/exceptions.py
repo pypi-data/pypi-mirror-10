ca40682295038adea92398a75ebe04061d3a9142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:01 +0000

    Add package between/0.2.2/between-0.2.2.tar.gz

---
+++ packages/between/between-0.2.2.tar.gz/between-0.2.2/between/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+requests.exceptions
+~~~~~~~~~~~~~~~~~~~
+
+This module contains the set of Between's exceptions.
+"""
+
+class LoginError(IOError):
+    """An Login error occurred."""
+
+class AuthenticateError(IOError):
+    """An Login error occurred."""
+
+class MessageError(IOError):
+    """An Login error occurred."""
