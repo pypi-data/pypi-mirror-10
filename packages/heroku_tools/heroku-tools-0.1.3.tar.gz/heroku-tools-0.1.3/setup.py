053d3411860863d261551d8c6bd2fdec346bc840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package heroku_tools/0.1.3/heroku-tools-0.1.3.tar.gz

---
+++ packages/heroku_tools/heroku-tools-0.1.3.tar.gz/heroku-tools-0.1.3/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Package setup for heroku-tools CLI application."""
+import os
+
+from setuptools import setup
+
+dependencies = ['click', 'envoy', 'yaml', 'requests']
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='heroku-tools',
+    version='0.1.3',
+    url='https://github.com/yunojuno/heroku-tools',
+    license='MIT',
+    author='Hugo Rodger-Brown',
+    author_email='hugo@yunojuno.com',
+    description=(
+        "Command line application for managing Heroku applications."
+    ),
+    long_description=README,
+    include_package_data=True,
+    packages=[
+        'heroku_tools',
+    ],
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'deploy = heroku_tools.deploy:deploy',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ]
+)
