feade8a26fb7ff055b0cc0876848b58d8d17067e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package planemo/0.9.0/planemo-0.9.0-py2.7.egg

---
+++ packages/planemo/planemo-0.9.0-py2.7.egg/planemo_ext/galaxy/tools/linters/command.py
@@ -0,0 +1,29 @@
+
+
+def lint_command(tool_xml, lint_ctx):
+    root = tool_xml.getroot()
+    commands = root.findall("command")
+    if len(commands) > 1:
+        lint_ctx.error("More than one command tag found, behavior undefined.")
+        return
+
+    if len(commands) == 0:
+        lint_ctx.error("No command tag found, must specify a command template to execute.")
+        return
+
+    command = commands[0]
+    if "TODO" in command:
+        lint_ctx.warn("Command template contains TODO text.")
+
+    command_attrib = command.attrib
+    interpreter_type = None
+    for key, value in command_attrib.items():
+        if key == "interpreter":
+            interpreter_type = value
+        else:
+            lint_ctx.warn("Unknown attribute [%s] encountered on command tag." % key)
+
+    interpreter_info = ""
+    if interpreter_type:
+        interpreter_info = " with interpreter of type [%s]" % interpreter_type
+    lint_ctx.info("Tool contains a command%s." % interpreter_info)
