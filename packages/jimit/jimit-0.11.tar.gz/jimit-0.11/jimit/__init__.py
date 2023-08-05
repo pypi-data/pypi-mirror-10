a7e5cd90e9ffe237ec610e19c5464e338b3e23b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:12 +0000

    Add package jimit/0.11/jimit-0.11.tar.gz

---
+++ packages/jimit/jimit-0.11.tar.gz/jimit-0.11/jimit/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/4/20'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+
+from utils import (
+    Commons,
+    Check,
+    Convert
+)
+
+__version__ = "0.11"
+
+__all__ = [
+    'Commons', 'Check', 'Convert'
+]
