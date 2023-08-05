5d8a786b7669238fb14db6e59b359dc67087a7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package mongofiles/1.1/mongofiles-1.1.tar.gz

---
+++ packages/mongofiles/mongofiles-1.1.tar.gz/mongofiles-1.1/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='mongofiles',
+    version='1.1',
+    url='https://github.com/shuge/mongofiles',
+    license='MIT License',
+    author='Shuge Lee',
+    author_email='shuge.lee@gmail.com',
+    description='mongofiles for Humans',
+    scripts = [
+        "mongofiles.py",
+    ],
+
+    requires = [
+        'pymongo',
+    ]
+)
