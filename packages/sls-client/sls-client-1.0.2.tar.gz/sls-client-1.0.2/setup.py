a09011de876c743022eb28221d7b07ce65a5e32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:08 +0000

    Add package sls-client/1.0.2/sls-client-1.0.2.tar.gz

---
+++ packages/sls-client/sls-client-1.0.2.tar.gz/sls-client-1.0.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="sls-client",
+    version="1.0.2",
+    description="A python client for the Simple Lookup Service",
+    long_description=open("README.rst").read(),
+    author=["Andrew Sides","Sowmya Balasubramanian"],
+    author_email=["asides@es.net","sowmya@es.net"],
+    url="https://github.com/esnet/python-sls-client",
+    license="3-clause BSD License",
+    package_data={
+        "sls-client":["sls-client/LICENSE"]
+    },
+    packages=find_packages(),
+    install_requires=[
+        "IPy",
+        "YURL",
+        "isodate",
+        "futures",
+        "requests",
+        "voluptuous"
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Networking"
+    ],
+)
---
+++ packages/sls-client/sls-client-1.0.2.tar.gz/sls-client-1.0.2/sls_client/find_ps_ma.py
@@ -0,0 +1,78 @@
+__author__ = 'sowmya'
+
+from sls_client.records import *
+from sls_client.query import *
+import sys
+import json
+import socket
+
+from optparse import OptionParser
+
+def get_ma_for_host(hostname):
+
+    ipaddresses={}
+    if(not hostname):
+        raise Exception("Null Parameter", "Hostname parameter is empty.")
+    ipaddr = socket.getaddrinfo(hostname, None)
+
+    #ipaddr is a 5-tuple and the ipv4 or ipv6 is the last element in the tuple.
+    for ip in ipaddr:
+        ipaddresses[ip[4][0]]=1
+
+    response=[]
+    result={}
+    queryKeys = ['psmetadata-dst-address','psmetadata-src-address']
+    for queryKey in queryKeys:
+        queryString=""
+        for ip in ipaddresses:
+            queryString += queryKey+"="+ip+"&"
+        response += query(queryString)
+
+
+    for record in response:
+        malocators = record[u'psmetadata-ma-locator']
+        for malocator in malocators:
+            malocatorString = malocator.encode("ascii")
+            result[malocatorString] = 1
+
+    return result.keys()
+
+def get_json(query,hostlist):
+    result={}
+    result["ma-hosts"] = []
+    result["search-query"] = query
+    if(hostlist):
+        result["ma-hosts"] = hostlist
+
+    json_output = json.dumps(result)
+    return json_output
+
+
+def main():
+
+    parser = OptionParser()
+
+    parser.add_option("-n", "--hostname",
+                      dest="hostname",
+                      help="specify hostname or IP")
+    parser.add_option("-o", "--output",
+                      dest="output_type",
+                      help="output type - json or console",
+                      choices=["console","json"],
+                      default="console"
+                      )
+    (options, args) = parser.parse_args()
+
+    if (not options.hostname):
+        print "Please specify hostname"
+        sys.exit(1)
+
+    result = get_ma_for_host(options.hostname)
+    if(options.output_type == "json"):
+        output=get_json(options.hostname,result)
+        print output
+    else:
+        for host in result:
+            print host
+if __name__=='__main__':
+    main()
