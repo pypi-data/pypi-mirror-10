179290069440ec66fea852fb86128bb101171bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:07 +0000

    Add package django-rest-signature/0.0.2/django-rest-signature-0.0.2.tar.gz

---
+++ packages/django-rest-signature/django-rest-signature-0.0.2.tar.gz/django-rest-signature-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-rest-signature',
+    version='0.0.2',
+    include_package_data=True,
+    packages=['rest_signature'],
+    license='BSD License',  # example license
+    description='django-rest-signature for multisite',
+    long_description=README,
+    url='http://www.example.com/',
+    author='c',
+    author_email='zombiecong@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
