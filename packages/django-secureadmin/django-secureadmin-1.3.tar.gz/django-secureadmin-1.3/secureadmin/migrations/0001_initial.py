10225fcf1fd80c79e958c5d665f72492aa645a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:49 +0000

    Add package django-secureadmin/1.3/django-secureadmin-1.3.tar.gz

---
+++ packages/django-secureadmin/django-secureadmin-1.3.tar.gz/django-secureadmin-1.3/secureadmin/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SecureAdmin',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('ip', models.CharField(max_length=10000, blank=True)),
+                ('ipmail', models.CharField(max_length=100, blank=True)),
+                ('ipcode', models.CharField(max_length=100, blank=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
