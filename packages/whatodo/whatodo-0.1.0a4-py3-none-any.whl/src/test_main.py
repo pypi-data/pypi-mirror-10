60333734b0b84769edf3b84fd1e87e18bd69a07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package whatodo/0.1.0a4/whatodo-0.1.0a4-py3-none-any.whl

---
+++ packages/whatodo/whatodo-0.1.0a4-py3-none-any.whl/src/test_main.py
@@ -0,0 +1,116 @@
+import pytest
+from src.main import *
+
+test_files = [  "examples/C/filenames/script", "examples/Clojure/index.cljs.hl", "examples/Forth/core.fth", 
+                "examples/GAP/Magic.gd", "examples/JavaScript/steelseries-min.js",
+                "examples/Matlab/FTLE_reg.m", "examples/Perl6/for.t",
+                "examples/VimL/solarized.vim", "examples/C/cpu.c",
+                "examples/D/mpq.d", "examples/Go/api.pb.go", 
+                "examples/HTML+ERB/index.html.erb"]
+
+number_of_comments = [
+    195,    # examples/C/filenames/script
+    13,     # examples/Clojure/index.cljs.hl
+    0,      # examples/Forth/core.fth
+    303,    # examples/GAP/Magic.gd 
+    2,      # examples/JavaScript/steelseries-min.js
+    0,      # examples/Matlab/FTLE_reg.m
+    76,    # examples/Perl6/for.t
+    420,   # examples/VimL/solarized.vim
+    80,     # examples/C/cpu.c
+    36,     # examples/D/mpq.d 
+    184,    # examples/Go/api.pb.go
+    10      # examples/HTML+ERB/index.html.erb
+]
+
+def test_get_comment_tokens():
+    from pygments.lexers.c_cpp import CLexer
+
+    file_text_test = "int main(int argc, char[] argv){\n//This is a comment\n}\n"
+    c_lexer = CLexer()
+
+    results = []
+    for comment in get_comment_tokens(file_text_test, c_lexer):
+        results.append(comment)
+
+    assert len(results) == 1
+    assert results[0] == "//This is a comment\n"
+
+def test_get_tokens_from_file():
+    for index,file in enumerate(test_files, 0):
+        result = get_tokens_from_file(file)
+        #print(index)
+        print(file)
+        assert number_of_comments[index] == len(result.keys())
+
+test_A = "// TODO This is a example todo with #TAGS #TODOAWESOME"
+     
+test_B = "#todo This is a example todo with #TAGS #TODOAWESOME"
+
+test_C = """/**TODO This is a todo 
+         #HASHTAGGALORE 
+         #TWITTERSUCKS 
+         */"""
+
+test_D = """Comments here
+
+
+         TODO This is a todo
+         This is a body #HASHTAGGALORE 
+         #TWITTERSUCKS 
+
+         TODO This is a second todo
+         #TASKSKSKSK"""
+
+test_E = """/* TODO This is a todo
+        *
+        * TODO  This is a new todo
+        *       With a body too!
+        *
+        * TODO This is the third one
+        *      lets keep it going... #BRICKHACKS
+        *
+        * TODO And a final lonely one...
+        */"""
+
+def test_find_Keywords():
+    test_A_findKey = find_Keywords(test_A, ["TODO"])
+
+    assert len(test_A_findKey) == 1
+
+    test_B_findKey = find_Keywords(test_B, ["TODO"])
+
+    assert len(test_B_findKey) == 0
+
+    test_C_findKey = find_Keywords(test_C, ["TODO"])
+
+    assert len(test_C_findKey) == 1
+
+    test_D_findKey = find_Keywords(test_D, ["TODO"])
+
+    assert len(test_D_findKey) == 2
+
+    test_E_findKey = find_Keywords(test_E, ["TODO"])
+
+    assert len(test_E_findKey) == 4
+
+def test_merge_single_line_comments():
+    token_to_line_mapping = {}
+    token_to_line_mapping[1] = "# Line 1"
+    token_to_line_mapping[2] = "# Line 2"
+    token_to_line_mapping[3] = "# Line 3"
+    token_to_line_mapping[4] = "# Line 4"
+    token_to_line_mapping[6] = "# Line 6"
+    token_to_line_mapping[7] = "# Line 7"
+
+    merged_result = merge_single_line_comments(token_to_line_mapping)
+
+    # Check that the lines we expect to merge were merged
+    assert 1 in merged_result.keys() and 6 in merged_result.keys()
+
+def test_expand_file_paths():
+    path = ["./examples/C/"]
+
+    test_expand = expand_file_paths(path)
+
+    assert len(test_expand) == 54
---
+++ packages/whatodo/whatodo-0.1.0a4-py3-none-any.whl/whatodo-0.1.0a4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,88 @@
+Whatodo
+=======
+
+| |Build Status|
+| |Coverage Status| |Latest Version| |Downloads|
+
+Description
+===========
+
+In software development and any other area of coding it is a common
+occerance to use comments for TODO and FIXME for two examples. It can be
+difficult with large files and/or large numbers of files to locate these
+comments.
+
+whatodo is created to make locating theses comments easier. This app can
+read through any number of files and locate these comments returning the
+line number of the contents of the actual comment.
+
+The application is written in pure Python with the help of the pygments
+library. With this library we are able to parse through more than 100+
+languages, for a more detailed description see `pygments
+docs <http://pygments.org/languages/>`__
+
+Usage
+=====
+
+| The most common use case would be scanning a whole source directory
+  for tags, to achieve this you can do the following:
+| ``whatodo src/``
+
+For more info and additional options see ``whatodo -h``
+
+Options
+-------
+
+::
+
+    - json
+    - keyword
+    - files
+
+JSON
+~~~~
+
+Changes the output format to json, useful for automating with other
+tools.
+
+Keyword
+~~~~~~~
+
+The Keywords are the words searched for in the beginning of a comment to
+determine if this is a comment to extract.
+
+Defaults to TODO, todo, FIXME
+
+Files
+~~~~~
+
+The Files are the files searched through to locate comments with the
+above mentioned Keywords.
+
+A list of files or directories to analyze and look for TODO's. At least
+1 file must be provided.
+
+Installing
+==========
+
+To install simply do:
+
+::
+
+    pip install whatodo
+
+License
+=======
+
+MIT License for more details see /LICENSE
+
+.. |Build Status| image:: https://travis-ci.org/masterkoppa/whatodo.svg?branch=master
+   :target: https://travis-ci.org/masterkoppa/whatodo
+.. |Coverage Status| image:: https://coveralls.io/repos/masterkoppa/whatodo/badge.svg?branch=master
+   :target: https://coveralls.io/r/masterkoppa/whatodo?branch=master
+.. |Latest Version| image:: https://pypip.in/version/whatodo/badge.svg
+   :target: https://pypi.python.org/pypi/whatodo/
+.. |Downloads| image:: https://pypip.in/download/whatodo/badge.svg
+   :target: https://pypi.python.org/pypi/whatodo/
+
+
