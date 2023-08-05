aa70dbc4d68265d4632fb528ea0dc1ffa3e0d47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package ecdsa_recover/0.1.4/ecdsa_recover-0.1.4.tar.gz

---
+++ packages/ecdsa_recover/ecdsa_recover-0.1.4.tar.gz/ecdsa_recover-0.1.4/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+extensions = [
+    Extension("ecdsa_recover", ["ecdsa_recover/ecdsa_recover.pyx"],
+              libraries=['gmp', 'm'],
+              extra_compile_args=['-O3', '-I/usr/include', '-I/usr/local/include']
+              )
+]
+compiler_directives = {}
+install_requires = ['bitcoin']
+
+version = '0.1.4'
+
+setup(
+    #ext_modules=cythonize(extensions, compiler_directives=compiler_directives),
+    ext_modules=extensions,
+    cmdclass={'build_ext': build_ext},
+    name='ecdsa_recover',
+    version=version,
+    description="faster implementation of ecdsa recover using cython + gmp",
+    author="HeikoHeiko",
+    author_email='heiko@ethdev.com',
+    url='https://github.com/heikoheiko/cy_ecdsa_recover',
+    license="BSD",
+    keywords='ecdsa recover ethereum cython gmp',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=install_requires,
+)
