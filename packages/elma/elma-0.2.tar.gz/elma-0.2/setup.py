25c8a57761dfe0664aa861251b3c25af146e5707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package elma/0.2/elma-0.2.tar.gz

---
+++ packages/elma/elma-0.2.tar.gz/elma-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='elma',
+    version='0.2',
+    url='http://github.com/sigvef/elma/',
+    license='MIT',
+    maintainer='Sigve Sebastian Farstad',
+    maintainer_email='sigvefarstad@gmail.com',
+    description='Elma Python Library.',
+    test_suite="tests"
+)
