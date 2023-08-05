4d3931f7a248c7aba2c2662c7cb15c7968dde7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package pipreqs/0.1.9/pipreqs-0.1.9.tar.gz

---
+++ packages/pipreqs/pipreqs-0.1.9.tar.gz/pipreqs-0.1.9/HISTORY.rst
@@ -0,0 +1,48 @@
+.. :changelog:
+
+History
+-------
+
+0.1.9 (2015-05-01)
+---------------------
+
+* Output tuning (Harri Berglund)
+* Use str.partition() to simplify the logic (cclaus)
+
+0.1.8 (2015-04-26)
+---------------------
+
+* Fixed problems with local imports (Dongwon Shin)
+* Fixed problems with imports with 'as' (Dongwon Shin)
+* Fix indentation, pep8 Styling. (Michael Borisov)
+* Optimize imports and adding missing import for sys module. (Michael Borisov)
+
+0.1.7 (2015-04-24)
+---------------------
+
+* Add more assertions in tests
+* Add more verbose output
+* Add recursive delete to Makefile clean
+* Update Readme
+
+0.1.6 (2015-04-22)
+---------------------
+
+* py3 print function
+
+0.1.5 (2015-04-22)
+---------------------
+
+* Add Readme, Add Examples
+* Add Stdlib into package
+
+0.1.1 (2015-04-22)
+---------------------
+
+* Fix regex matching for imports
+* Release on Pypi
+
+0.1.0 (2015-04-22)
+---------------------
+
+* First release on Github.
---
+++ packages/pipreqs/pipreqs-0.1.9.tar.gz/pipreqs-0.1.9/tests/test_pipreqs.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pipreqs
+----------------------------------
+
+Tests for `pipreqs` module.
+"""
+
+import unittest, os
+
+from pipreqs import pipreqs
+
+
+class TestPipreqs(unittest.TestCase):
+    def setUp(self):
+        self.modules = ['flask', 'requests', 'sqlalchemy', 'docopt', 'ujson', 'nonexistendmodule']
+        self.project = os.path.join(os.path.dirname(__file__), "_data")
+        self.requirements_path = os.path.join(self.project, "requirements.txt")
+
+    def test_get_all_imports(self):
+        imports = pipreqs.get_all_imports(self.project)
+        self.assertEqual(len(imports), 6, "Incorrect Imports array length")
+        for item in imports:
+            self.assertTrue(item in self.modules, "Import is missing")
+        self.assertFalse("time" in imports)
+        self.assertFalse("logging" in imports)
+        self.assertFalse("curses" in imports)
+        self.assertFalse("__future__" in imports)
+        self.assertFalse("django" in imports)
+        self.assertFalse("models" in imports)
+
+    def test_get_imports_info(self):
+        imports = pipreqs.get_all_imports(self.project)
+        with_info = pipreqs.get_imports_info(imports)
+        # Should contain only 5 Elements without the "nonexistendmodule"
+        self.assertEqual(len(with_info), 5, "Length of imports array with info is wrong")
+        for item in with_info:
+            self.assertTrue(item['name'] in self.modules, "Import item appears to be missing")
+
+    def test_init(self):
+        pipreqs.init({'<path>': self.project, '--savepath': None})
+        assert os.path.exists(self.requirements_path) == 1
+        with open(self.requirements_path, "r") as f:
+            data = f.read()
+            for item in self.modules[:-1]:
+                self.assertTrue(item in data)
+
+    def tearDown(self):
+        try:
+            os.remove(self.requirements_path)
+        except OSError:
+            pass
+
+
+if __name__ == '__main__':
+    unittest.main()
