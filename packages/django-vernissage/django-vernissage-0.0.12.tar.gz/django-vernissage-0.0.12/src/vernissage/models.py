83413147c170e91c4213c91444f1dd0ed5b02c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package django-vernissage/0.0.12/django-vernissage-0.0.12.tar.gz

---
+++ packages/django-vernissage/django-vernissage-0.0.12.tar.gz/django-vernissage-0.0.12/src/vernissage/models.py
@@ -0,0 +1,60 @@
+"""
+Models for gallery
+"""
+from __future__ import unicode_literals
+
+import json
+
+from django.db import models
+from filer.fields.image import FilerImageField
+
+try:
+    from cms.models import CMSPlugin
+    USE_CMS = True
+except ImportError:
+    USE_CMS = False
+
+from vernissage import settings as vsets
+
+
+class Image(models.Model):
+    """
+    Image objects for the galleries
+    """
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048, blank=True, null=True)
+    image = FilerImageField(related_name="gallery_image")
+    alt = models.CharField(max_length=128, blank=True, null=True)
+    gallery = models.ForeignKey('vernissage.Gallery', related_name="images")
+
+    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+
+    class Meta:
+        ordering = ('order',)
+
+
+class Gallery(models.Model):
+    """
+    Gallery model
+    """
+    title = models.CharField(max_length=64)
+    attributes = models.TextField(default=json.dumps(
+        vsets.VERNISSAGE_GALLERY_ATTRIBUTES))
+
+    @property
+    def template(self):
+        return "vernissage/carousel.html"
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Galleries'
+
+
+if USE_CMS:
+    class GalleryPlugin(CMSPlugin, Gallery):
+
+        def __unicode__(self):
+            return u"Gallery '{}'".format(
+                super(GalleryPlugin, self).__unicode__())
