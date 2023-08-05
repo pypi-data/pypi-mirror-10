e88f4f486c7ad6108fc473f2cb1175ba4fdcfeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:49 +0000

    Add package printlistoop/0.1/printlistoop-0.1.zip

---
+++ packages/printlistoop/printlistoop-0.1.zip/printlistoop-0.1/listprint/stack.py
@@ -0,0 +1,14 @@
+'''
+Created on Apr 21, 2015
+
+@author: nishant.nawarkhede
+'''
+
+
+class Xyz:
+    
+    def __init__(self,):
+        pass
+		
+	def printList(self):
+		print [i for i in range(0,10)]  
---
+++ packages/printlistoop/printlistoop-0.1.zip/printlistoop-0.1/setup.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 27, 2015
+
+@author: nishant.nawarkhede
+'''
+from setuptools import setup
+
+setup(name='printlistoop',
+      version='0.1',
+      description='The funniest joke in the world- Nishant',
+      url='http://github.com/nawarkhede',
+      author='Nishant',
+      author_email='nishant.nawarkhede@gmail.com',
+      license='MIT',
+      packages=['listprint'],
+      zip_safe=False)
