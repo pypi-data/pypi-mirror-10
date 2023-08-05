8674ce689cd5b21da7da8761a1b87e407571a209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:17 +0000

    Add package onegov.form/0.1.0/onegov.form-0.1.0-py2-none-any.whl

---
+++ packages/onegov.form/onegov.form-0.1.0-py2-none-any.whl/onegov/form/collection.py
@@ -0,0 +1,100 @@
+from onegov.core.utils import normalize_for_url
+from onegov.form.models import FormDefinition, FormSubmission
+
+
+class FormCollection(object):
+    """ Manages a collection of forms and form-submissions. """
+
+    def __init__(self, session):
+        self.session = session
+
+    @property
+    def definitions(self):
+        return FormDefinitionCollection(self.session)
+
+    @property
+    def submissions(self):
+        return FormSubmissionCollection(self.session)
+
+
+class FormDefinitionCollection(object):
+    """ Manages a collection of forms. """
+
+    def __init__(self, session):
+        self.session = session
+
+    def query(self):
+        return self.session.query(FormDefinition)
+
+    def add(self, title, definition, type=None, meta=None, content=None):
+        """ Add the given form to the database. """
+        form = FormDefinition()
+
+        form.name = normalize_for_url(title)
+        form.title = title
+        form.definition = definition
+        form.type = type
+        form.meta = meta or {}
+        form.content = content or {}
+
+        # try to parse the form (which will throw errors if there are problems)
+        assert form.form_class
+
+        self.session.add(form)
+        self.session.flush()
+
+        return form
+
+    def delete(self, name, with_submissions=False):
+        """ Delete the given form. Only possible if there are no submissions
+        associated with it, or if ``with_submissions`` is True.
+
+        """
+        if with_submissions:
+            submissions = self.session.query(FormSubmission)
+            submissions = submissions.filter(FormSubmission.name == name)
+            submissions.delete()
+
+        self.query().filter(FormDefinition.name == name).delete('fetch')
+        self.session.flush()
+
+    def by_name(self, name):
+        """ Returns the given form by name or None. """
+        return self.query().filter(FormDefinition.name == name).first()
+
+
+class FormSubmissionCollection(object):
+    """ Manages a collection of submissions. """
+
+    def __init__(self, session):
+        self.session = session
+
+    def query(self):
+        return self.session.query(FormSubmission)
+
+    def add(self, form_name, form):
+        """ Takes a form filled-out form instance and stores the submission
+        in the database. The form instance is expected to have a ``_source``
+        parameter, which contains the source used to build the form (as only
+        forms with this source may be stored).
+
+        """
+
+        assert hasattr(form, '_source')
+
+        # this should happen way earlier, we just double check here
+        assert form.validate()
+
+        submission = FormSubmission()
+        submission.name = form_name
+        submission.definition = form._source
+        submission.data = form.data
+
+        self.session.add(submission)
+        self.session.flush()
+
+        return submission
+
+    def by_form_name(self, name):
+        """ Return all submissions for the given form-name. """
+        return self.query().filter(FormSubmission.name == name).all()
---
+++ packages/onegov.form/onegov.form-0.1.0-py2-none-any.whl/onegov/form/parser/core.py
@@ -0,0 +1,221 @@
+from onegov.form.core import (
+    Form,
+    with_options
+)
+from onegov.form.fields import TimeField, MultiCheckboxField
+from onegov.form.parser.grammar import document
+from onegov.form.utils import label_to_field_id
+from onegov.form.validators import Stdnum
+from wtforms import (
+    PasswordField,
+    RadioField,
+    StringField,
+    TextAreaField
+)
+from wtforms.fields.html5 import DateField, DateTimeLocalField, EmailField
+from wtforms.validators import InputRequired, Length
+from wtforms.widgets import TextArea
+from wtforms_components import Email, If
+
+
+# cache the parser
+doc = document()
+
+
+def parse_form(text):
+    """ Takes the given form text, parses it and returns a WTForms form
+    class (not an instance of it).
+
+    """
+
+    builder = WTFormsClassBuilder(Form)
+
+    for block in (i[0] for i in doc.scanString(text)):
+        handle_block(builder, block)
+
+    form_class = builder.form_class
+    form_class._source = text
+
+    return form_class
+
+
+def handle_block(builder, block, dependency=None):
+    """ Takes a parsed block and instructs the builder to add a field based
+    on it.
+
+    """
+    if block.type == 'fieldset':
+        builder.set_current_fieldset(block.label or None)
+    elif block.type == 'text':
+        if block.length:
+            validators = [Length(max=block.length)]
+        else:
+            validators = []
+
+        field_id = builder.add_field(
+            field_class=StringField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+            validators=validators,
+        )
+    elif block.type == 'textarea':
+        field_id = builder.add_field(
+            field_class=TextAreaField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+            widget=with_options(TextArea, rows=block.rows or None)
+        )
+    elif block.type == 'password':
+        field_id = builder.add_field(
+            field_class=PasswordField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required
+        )
+    elif block.type == 'email':
+        field_id = builder.add_field(
+            field_class=EmailField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+            validators=[Email()]
+        )
+    elif block.type == 'stdnum':
+        field_id = builder.add_field(
+            field_class=StringField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+            validators=[Stdnum(block.format)]
+        )
+    elif block.type == 'date':
+        field_id = builder.add_field(
+            field_class=DateField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+        )
+    elif block.type == 'datetime':
+        field_id = builder.add_field(
+            field_class=DateTimeLocalField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+        )
+    elif block.type == 'time':
+        field_id = builder.add_field(
+            field_class=TimeField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required
+        )
+    elif block.type == 'radio':
+        choices = [
+            (c.label, c.label) for c in block.parts
+        ]
+        checked = [c.label for c in block.parts if c.checked]
+        default = checked and checked[0] or None
+
+        field_id = builder.add_field(
+            field_class=RadioField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+            choices=choices,
+            default=default
+        )
+    elif block.type == 'checkbox':
+        choices = [
+            (c.label, c.label) for c in block.parts
+        ]
+        default = [c.label for c in block.parts if c.checked]
+        field_id = builder.add_field(
+            field_class=MultiCheckboxField,
+            label=block.label,
+            dependency=dependency,
+            required=block.required,
+            choices=choices,
+            default=default
+        )
+    else:
+        raise NotImplementedError
+
+    # go through nested blocks
+    if block.type in {'radio', 'checkbox'}:
+        for part in block.parts:
+            dependency = FieldDependency(field_id, part.label)
+
+            for child in part.dependencies:
+                handle_block(builder, child, dependency)
+
+
+class FieldDependency(object):
+    """ Defines a dependency to a field. The given field must have the given
+    choice for this dependency to be fulfilled.
+
+    """
+
+    def __init__(self, field_id, choice):
+        self.field_id = field_id
+        self.choice = choice
+
+    def fulfilled(self, form, field):
+        return getattr(form, self.field_id).data == self.choice
+
+    @property
+    def html_data(self):
+        return {'data-depends-on': '/'.join((self.field_id, self.choice))}
+
+
+class WTFormsClassBuilder(object):
+    """ Helps dynamically build a wtforms class from parsed blocks.
+
+    For example::
+
+        builder = WTFormsClassBuilder(BaseClass)
+        builder.add_field(StringField, label='Name', required=True)
+
+        MyForm = builder.form_class
+    """
+
+    def __init__(self, form_class):
+
+        class DynamicForm(form_class):
+            pass
+
+        self.form_class = DynamicForm
+        self.current_fieldset = None
+
+    def set_current_fieldset(self, label):
+        self.current_fieldset = label
+
+    def add_field(self, field_class, label, required,
+                  dependency=None, **kwargs):
+        validators = kwargs.pop('validators', [])
+
+        if required:
+            if dependency is None:
+                validators.insert(0, InputRequired())
+            else:
+                validators.insert(0, If(dependency.fulfilled, InputRequired()))
+
+        field_id = label_to_field_id(label)
+
+        setattr(self.form_class, field_id, field_class(
+            label=label,
+            validators=validators,
+            fieldset=self.current_fieldset,
+            **kwargs
+        ))
+
+        if dependency:
+            field = getattr(self.form_class, field_id)
+            widget = field.kwargs.get('widget', field.field_class.widget)
+
+            field.kwargs['widget'] = with_options(
+                widget, **dependency.html_data
+            )
+
+        return field_id
