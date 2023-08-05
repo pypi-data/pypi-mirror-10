a0aa04558815ba4827cfbf29315e05c7b632ab5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:48 +0000

    Add package lmadd/1.1.0/lmadd-1.1.0.tar.gz

---
+++ packages/lmadd/lmadd-1.1.0.tar.gz/lmadd-1.1.0/lmadd.py
@@ -0,0 +1,9 @@
+def add(a, b):
+''' add function '''
+	return a + b
+
+def menus(a, b):
+''' menus function '''
+    return a - b
+
+
---
+++ packages/lmadd/lmadd-1.1.0.tar.gz/lmadd-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'lmadd',
+	version = '1.1.0',
+	py_modules = ['lmadd'],
+	author = 'lumin',
+	author_email = 'lumin.vis@gmail.com',
+	description = 'A Sum-up, Menus Function',
+	)
