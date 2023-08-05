64a7b2cce8247dbbc7191b6c8621c6d60dc55e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package pyqueued/0.1.3/pyqueued-0.1.3.tar.gz

---
+++ packages/pyqueued/pyqueued-0.1.3.tar.gz/pyqueued-0.1.3/pyqueued.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import requests
+
+__version__ = '0.1.3'
+
+class Client:
+    """
+    A Queued client object.
+    """
+
+    def __init__(self, host='localhost', port=5353):
+        self.host = host
+        self.port = port
+
+    def queue_url(self, queue):
+        """
+        Get the URL to a queue.
+        """
+        return "http://{0}:{1}/{2}".format(self.host, self.port, queue)
+
+    def enqueue(self, queue, msg):
+        """
+        Enqueue a message on a given queue.
+        """
+        r = requests.post(self.queue_url(queue), data=msg)
+        if not r.status_code == 201:
+            raise RuntimeError("enqueue failed: %s, %s" % (r, r.text))
+        return r.headers['Location']
+
+    def dequeue(self, queue, wait=None, timeout=None):
+        """
+        Dequeue a message on a given queue.
+        """
+        params = dict([(k, v) for k, v in (('wait', wait), ('timeout', timeout)) if v is not None])
+        r = requests.post(os.path.join(self.queue_url(queue), 'dequeue'), params=params)
+        if not r.status_code == 200:
+            raise RuntimeError("dequeue failed: %s" % r)
+        return r.text, r.headers['Location']
+
+    def complete(self, queue, id):
+        """
+        Remove an item from a queue, given its id.
+        """
+        r = requests.delete(os.path.join(self.queue_url(queue), str(id)))
+        if not r.status_code == 204:
+            raise RuntimeError("complete failed: %s, %s" % (r, r.text))
+
+    def complete_by_url(self, url):
+        """
+        Remove an item from a queue, given the items full url.
+        """
+        queue, id = url.split('/')[-2:]
+        hostport = url.split('/')[-3]
+        host, port = hostport.split(':')
+        if self.host != host or self.port != port:
+            raise RuntimeError('client hostport mistmatch')
+        r = requests.delete(os.path.join(self.queue_url(queue), id))
+        if not r.status_code == 204:
+            raise RuntimeError("complete failed: %s, %s" % (r, r.text))
+
+    def get(self, queue, id):
+        """
+        Get an item by queue and id.
+        """
+        r = requests.get(os.path.join(self.queue_url(queue), id))
+        if not r.status_code == 200:
+            raise RuntimeError("get failed: %s, %s" % (r, r.text))
+        return r.text
+
+    def stats(self, queue):
+        """
+        Return stats for a queue.
+        """
+        r = requests.get(self.queue_url(queue))
+        if not r.status_code == 200:
+            raise RuntimeError("stats failed: %s, %s" % (r, r.text))
+        return json.loads(r.text)
---
+++ packages/pyqueued/pyqueued-0.1.3.tar.gz/pyqueued-0.1.3/setup.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+"""
+Python client for HTTP message queue [Queued](https://github.com/scttnlsn/queued).
+
+Usage
+-----
+
+    >>> import pyqueued
+
+Get a client.
+
+    >>> client = pyqueued.Client(host='localhost', port=5353)
+
+Enqueue a message:
+
+    >>> loc = client.enqueue("q", "my message")
+    >>> loc
+    'http://localhost:5353/q/155'
+
+Get the head of the queue. Without any timeout, the item is marked as completed when dequeued.
+
+    >>> msg, loc = client.dequeue("q")
+    >>> msg
+    'my message'
+
+THe queue is empty now, so another dequeue will fail:
+
+    >>> msg, loc = client.dequeue("q")
+    ...
+    RuntimeError: dequeue failed: <Response [404]>
+
+Enqueue another message:
+
+    >>> loc = client.enqueue("q", "another message")
+
+But dequeue with timeout (given in seconds). If the message is not completed within `timeout`,
+it will be enqueued again.
+
+    >>> client.dequeue("q", timeout=2)
+    ('another message', 'http://localhost:5353/q/160')
+
+The has message timed out and has been enqueued again. So we can actually dequeue it once more. But now we
+mark it completed in time.
+
+    >>> client.dequeue("q", timeout=20)
+    ('my message with timeout', 'http://localhost:5353/q/160')
+
+    >>> client.complete("q", "160")
+
+Alternatively, item can be completed by URL. To the last line have been written also as:
+
+    >>> client.complete_by_url('http://localhost:5353/q/160')
+
+----
+
+Stats about the queue:
+
+    >>> client.stats("q")
+    {'depth': 1, 'dequeued': 3, 'enqueued': 3, 'timeouts': 1}
+
+"""
+
+from setuptools import setup
+
+setup(name='pyqueued',
+      version='0.1.3',
+      description='A client for queued.',
+      long_description=__doc__,
+      url='https://github.com/miku/pyqueued',
+      author='Martin Czygan',
+      author_email='martin.czygan@gmail.com',
+      py_modules=['pyqueued'],
+      install_requires=['requests>=2'],
+)
