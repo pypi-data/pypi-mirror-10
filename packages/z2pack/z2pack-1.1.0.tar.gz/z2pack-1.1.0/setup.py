ed496a4e7e125c02ae5411ef89b262c73b27d919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:50 +0000

    Add package z2pack/1.1.0/z2pack-1.1.0.tar.gz

---
+++ packages/z2pack/z2pack-1.1.0.tar.gz/z2pack-1.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@gmx.ch>
+# Date:    20.10.2014 11:27:40 CEST
+# File:    setup.py
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+readme = r"""Z2Pack is a tool for calculating topological invariants on
+first-principles (z2pack.fp) and tight-binding (z2pack.tb) systems.
+
+It uses the method described in http://journals.aps.org/prb/abstract/10.1103/PhysRevB.83.235401
+to calculate the Z2 topological invariant.
+
+Overlap matrices are calculated either directly (for tb) or via
+the Wannier90 code package (fp).
+
+- Documentation: http://z2pack.ethz.ch/doc
+- Online interface: http://z2pack.ethz.ch/online
+"""
+
+setup(
+    name='z2pack',
+    version='1.1.0',
+    url='http://z2pack.ethz.ch',
+    author='Dominik Gresch',
+    author_email='greschd@gmx.ch',
+    description='A tool for computing topological invariants',
+    install_requires=['numpy', 'scipy', 'decorator'],
+    extras_require = {'plot':  ['matplotlib']},
+    long_description=readme,
+    license='LICENSE.txt',
+    packages=['z2pack',
+              'z2pack.ptools',
+              'z2pack.tb',
+              'z2pack.fp'])
---
+++ packages/z2pack/z2pack-1.1.0.tar.gz/z2pack-1.1.0/z2pack/fp/kpts.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Authors:  Dominik Gresch <greschd@ethz.ch>, Gabriel Autes
+# Date:    27.09.2014 21:27:27 CEST
+# File:    kpts.py
+r"""
+A collection of functions for creating k-points input for different 
+first-principles codes.
+
+All functions have the same calling structure as :func:`prototype`.
+"""
+
+
+def prototype(kpt):
+    r"""
+    Specifies the interface
+
+    :param kpt:     The list of k-points in the string INCLUDING the
+        final point which should not be in the calculation
+    :type kpt:      list
+    """
+    raise NotImplementedError('This is only the prototype for kpts')
+
+
+def abinit(kpt):
+    """
+    Creates a k-point input for **ABINIT**. It uses ``kptopt -1`` and
+    specifies the k-points string using ``ndivk`` and ``kptbounds``.
+    """
+    start_point = kpt[0]
+    end_point = kpt[-1]
+    last_point = kpt[-2]
+    N = len(kpt) - 1
+    for point in kpt:
+        if len(point) != 3:
+            raise ValueError('dimension of point != 3')
+
+    string = "\nkptopt -1\nndivk " + str(int(N - 1)) + '\nkptbounds '
+    for coord in start_point:
+        string += str(coord).replace('e', 'd') + ' '
+    string += '\n'
+    for coord in last_point:
+        string += str(coord).replace('e', 'd') + ' '
+    string += '\n'
+    return string
+
+
+def qe(kpt):
+    """
+    Creates a k-point input for  **Quantum Espresso**.
+    """
+    start_point = kpt[0]
+    end_point = kpt[-1]
+    last_point = kpt[-2]
+    for point in kpt:
+        if len(point) != 3:
+            raise ValueError('dimension of point != 3')
+
+    string = "\nK_POINTS crystal_b\n 2 \n"
+    for coord in start_point:
+        string += str(coord).replace('e', 'd') + ' '
+    string += str(N-1)+'\n'
+    for coord in last_point:
+        string += str(coord).replace('e', 'd') + ' '
+    string += str(1)+'\n'
+    return string
+
+def wannier90(kpt):
+    """
+    Creates a k-point input for **Wannier90**. It can be useful when the
+    first-principles code does not generate the k-points in
+    ``wannier90.win`` (e.g. with Quantum Espresso).
+    """
+    for point in kpt:
+        if len(point) != 3:
+            raise ValueError('dimension of point != 3')
+
+    string = "mp_grid: " + str(int(N)) + " 1 1 \nbegin kpoints"
+    for k in kpt[:-1]:
+        string += '\n'
+        for coord in k:
+            string += str(coord).replace('e', 'd') + ' '
+    string += '\nend kpoints\n'
+    return string
+
+def vasp(kpt):
+    """
+    Creates a k-point input for  **VASP**, using explicit points
+    """
+    N = len(kpt) - 1
+    string = 'Explicit k-points\n' + str(N) + '\nReciprocal\n'
+    for k in kpt[:-1]:
+        string += '{0} {1} {2} 1.'.format(*list(k))
+    string += '\n'
+    return string
