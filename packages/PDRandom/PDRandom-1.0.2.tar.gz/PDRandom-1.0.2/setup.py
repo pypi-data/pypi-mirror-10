230095d5db2674df9851a228b887caf565ec5560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:51 +0000

    Add package PDRandom/1.0.2/PDRandom-1.0.2.tar.gz

---
+++ packages/PDRandom/PDRandom-1.0.2.tar.gz/PDRandom-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='PDRandom',
+      version='1.0.2',
+      py_modules=['PDRandom'],
+      author = 'KenLeung',
+	author_email = 'devyat192002@gmail.com',
+	url = 'https://github.com/d2fdydx/PDRandom',
+		description = 'python module for generating random numbers according to a given probability density function ',
+		keywords=['probability density', 'random']
+
+      )
