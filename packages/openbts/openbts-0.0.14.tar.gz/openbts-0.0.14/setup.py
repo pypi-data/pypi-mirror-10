d2e773b4fc90a9914cc2c98dfe61b4603c40501a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:55 +0000

    Add package openbts/0.0.14/openbts-0.0.14.tar.gz

---
+++ packages/openbts/openbts-0.0.14.tar.gz/openbts-0.0.14/setup.py
@@ -0,0 +1,26 @@
+"""setup
+openbts-python package definition
+"""
+from setuptools import setup
+
+# load the readme
+with open('readme.md') as f:
+  readme = f.read()
+
+version = '0.0.14'
+
+setup(
+    name='openbts',
+    version=version,
+    description='OpenBTS NodeManager client',
+    long_description=readme,
+    url='http://github.com/endaga/openbts-python',
+    download_url=('https://github.com/endaga/openbts-python/tarball/%s' %
+                  version),
+    author='Matt Ball',
+    author_email='matt@endaga.com',
+    license='MIT',
+    packages=['openbts'],
+    install_requires=["enum34==1.0.4", "pyzmq==14.5.0"],
+    zip_safe=False
+)
