6ecaeb658701dcf4ac1af126ebaad622c7093990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:45 +0000

    Add package abduct/2.0.0/abduct-2.0.0-py2.py3-none-any.whl

---
+++ packages/abduct/abduct-2.0.0-py2.py3-none-any.whl/abduct/__init__.py
@@ -0,0 +1,22 @@
+from contextlib2 import contextmanager, ExitStack
+from abduct.stream import stdout as out
+from abduct.stream import stderr as err
+
+capture_stdout = out  # pylint: disable=invalid-name
+capture_stderr = err  # pylint: disable=invalid-name
+
+
+@contextmanager
+def captured(*stream_context_managers):
+    """
+    Temporarily capture output from the specified streams and optionally
+    release it when an exception occurs.
+    """
+    with __nested(stream_context_managers) as v:
+        yield v[0] if len(v) == 1 else v
+
+
+@contextmanager
+def __nested(context_managers):
+    with ExitStack() as stack:
+        yield tuple(stack.enter_context(c) for c in context_managers)
