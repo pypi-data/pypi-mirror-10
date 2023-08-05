53404def2dc50c316027749f605c9c0118e89821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:20 +0000

    Add package fino/0.2.0/fino-0.2.0.tar.gz

---
+++ packages/fino/fino-0.2.0.tar.gz/fino-0.2.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="fino",
+    version="0.2.0",
+    description="Output the Finnish word for a given integer.",
+    url="https://github.com/hugovk/fino",
+    author="hugovk",
+    keywords=["Finnish", "suomi", "numbers", "integers"],
+    py_modules=("fino",),
+    zip_safe=False,
+    )
+
+# End of file
