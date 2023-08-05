5058eb42af343599af88564cc78484145adf6a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:53 +0000

    Add package db_url/0.3.2/db_url-0.3.2.tar.gz

---
+++ packages/db_url/db_url-0.3.2.tar.gz/db_url-0.3.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='db_url',
+    version='0.3.2',
+    license='Public Domain',
+    description='connect to the DATABASE_URL environment variable',
+    url='https://github.com/razzius/db_url/',
+    long_description=open('README.md').read(),
+    py_modules=['db_url'],
+    entry_points={
+        'console_scripts': [
+            'db=db_url:connect_to_database'
+        ]
+    }
+)
