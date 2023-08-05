f98c0be7e810850a81ee74e65f7cb1f3bc453782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package testdist/1.0.0/testdist-1.0.0.tar.gz

---
+++ packages/testdist/testdist-1.0.0.tar.gz/testdist-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'beta'
+
+from distutils.core import setup
+
+setup(
+    name='testdist',
+    version='1.0.0',
+    py_modules=['testdist'],
+    author='beta',
+    author_email='atbeta@live.com',
+    url='http://betas.me',
+    description='just test dist'
+)
