0b8c6a6907a3b783bd9d453a6e68ec90a21ef832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package unnest.py/1.3.0/unnest.py-1.3.0.tar.gz

---
+++ packages/unnest.py/unnest.py-1.3.0.tar.gz/unnest.py-1.3.0/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+        name            = 'unnest.py',
+        version         = '1.3.0',
+        py_modules      = ['unnest'],
+        author          = 'bgw',
+        author_email    = 'brandonwallace@fastmail.us',
+        url             = '',
+        description     = 'Print items from a nested list.'
+)
+
