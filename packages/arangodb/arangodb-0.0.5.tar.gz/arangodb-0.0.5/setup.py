4e70a0d95d5aac7921e7c7c4fd0615ed759202cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package arangodb/0.0.5/arangodb-0.0.5.tar.gz

---
+++ packages/arangodb/arangodb-0.0.5.tar.gz/arangodb-0.0.5/setup.py
@@ -0,0 +1,67 @@
+"""package setup"""
+
+__version__ = "0.0.5"
+
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    """Our test runner."""
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ["tests"]
+
+    def finalize_options(self):
+        # pylint: disable=W0201
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name="arangodb",
+    version=__version__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+    ],
+    license='ApacheV2',
+
+    keyword="ArangoDB REST API Requests graph nosql database AQL",
+
+    package_dir={'': 'src'},
+    namespace_packages=['arangodb'],
+    packages=find_packages(
+        'src',
+        exclude=["tests*"]
+    ),
+    entry_points="",
+
+    install_requires=[
+        'requests',
+    ],
+
+    cmdclass={'test': PyTest},
+    tests_require=[
+        # tests
+        'pytest',
+        'pytest-pep8',
+        'pytest-cache',
+        'pytest-random',
+        'mock',
+    ]
+)
