90c9e93a98e72cc31856ed78b312899bfbbf1e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package simplebus-mail/0.7.2/simplebus-mail-0.7.2.tar.gz

---
+++ packages/simplebus-mail/simplebus-mail-0.7.2.tar.gz/simplebus-mail-0.7.2/simplebus_mail/message.py
@@ -0,0 +1,39 @@
+# Copyright 2015 Vinicius Chiele. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Implements the message related objects."""
+
+
+class Message(object):
+    """Encapsulates an email message."""
+
+    def __init__(self, sender=None,
+                 recipients=None,
+                 subject=None,
+                 body=None,
+                 is_body_html=None,
+                 cc=None,
+                 bcc=None,
+                 reply_to=None,
+                 headers=None):
+
+        self.sender = sender
+        self.recipients = recipients or []
+        self.subject = subject
+        self.body = body
+        self.is_body_html = is_body_html or False
+        self.cc = cc or []
+        self.bcc = bcc or []
+        self.reply_to = reply_to
+        self.headers = headers or {}
