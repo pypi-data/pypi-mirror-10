1bf3af4ac973a5e89848204aa4c96a739b59ee28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:37 +0000

    Add package django-test-plus/1.0.2/django-test-plus-1.0.2.tar.gz

---
+++ packages/django-test-plus/django-test-plus-1.0.2.tar.gz/django-test-plus-1.0.2/test_project/test_app/views.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Data
+
+
+def view_200(request):
+    return HttpResponse('', status=200)
+
+
+def view_201(request):
+    return HttpResponse('', status=201)
+
+
+def view_302(request):
+    return HttpResponse('', status=302)
+
+
+def view_404(request):
+    return HttpResponse('', status=404)
+
+
+@login_required
+def needs_login(request):
+    return HttpResponse('', status=200)
+
+
+def data_1(request):
+    list(Data.objects.all())
+    return HttpResponse('', status=200)
+
+
+def data_5(request):
+    list(Data.objects.all())
+    list(Data.objects.all())
+    list(Data.objects.all())
+    list(Data.objects.all())
+    list(Data.objects.all())
+    return HttpResponse('', status=200)
+
+
+def view_context_with(request):
+    return render(request, 'base.html', {'testvalue': True})
+
+
+def view_context_without(request):
+    return render(request, 'base.html', {})
