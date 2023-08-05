051a1f1e0d9bae6258e1f0d48e4ff1b034f40b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package buildout.bootstrap/1.4.7/buildout.bootstrap-1.4.7.zip

---
+++ packages/buildout.bootstrap/buildout.bootstrap-1.4.7.zip/buildout.bootstrap-1.4.7/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+
+VERSION = '1.4.7'
+
+
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    description='A buildout extension that adds a buildout\
+        bootstrap.py file to your buildout.',
+    entry_points={
+        "zc.buildout.extension": [
+            "default = buildout.bootstrap:install"
+        ]
+    },
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+    ],
+    keywords='buildout bootstrap',
+    license='ZPL',
+    long_description=(
+        open('README.rst').read() + '\n' +
+        open('CHANGES.rst').read() + '\n'
+    ),
+    name='buildout.bootstrap',
+    namespace_packages=[
+        'buildout'
+    ],
+    packages=find_packages(),
+    test_suite='buildout.bootstrap.tests.TestBuildoutBootstrap',
+    url='http://collective.github.com/buildout.bootstrap',
+    version=VERSION,
+    zip_safe=False,
+)
