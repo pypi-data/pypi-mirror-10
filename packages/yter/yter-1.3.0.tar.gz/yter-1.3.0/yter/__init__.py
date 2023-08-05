452e1641aae2be1d6880873b94407d675da95527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package yter/1.3.0/yter-1.3.0.tar.gz

---
+++ packages/yter/yter-1.3.0.tar.gz/yter-1.3.0/yter/__init__.py
@@ -0,0 +1,25 @@
+"""Clever, quick iterators that make your smile whiter."""
+
+from ._fun import *
+from ._ytr import *
+from ._key import *
+
+__all__ = [
+    "yany",
+    "yall",
+    "last",
+    "head",
+    "tail",
+    "finish",
+    "minmax",
+    "minmedmax",
+    "isiter",
+    "contain",
+    "call",
+    "percent",
+    "flat",
+    "chunk",
+    "key",
+    "formatter",
+    "numeric",
+]
