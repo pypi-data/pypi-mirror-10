ac531c427aba6185159f2ee7f575f5f5329be294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package diastema/0.0.1/diastema-0.0.1.tar.gz

---
+++ packages/diastema/diastema-0.0.1.tar.gz/diastema-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='diastema',
+      version='0.0.1',
+      description='A tool for modal music analysis',
+      url='https://github.com/AnasGhrab/diastema',
+      author='Anas Ghrab',
+      author_email='anas.ghrab@gmail.com',
+      license='MIT',
+      packages=['diastema'],
+      zip_safe=False)
