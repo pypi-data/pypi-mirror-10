6fbfc52d3ea94dc6d48ec6541f263faf634238b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:48 +0000

    Add package silverdawn1992/1.1.1/silverdawn1992-1.1.1.zip

---
+++ packages/silverdawn1992/silverdawn1992-1.1.1.zip/silverdawn1992-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'silverdawn1992',
+    version = '1.1.1',
+    py_modules = ['silverdawn1992'],
+    author = 'YuChang',
+    author_email = 'yuchang1992@126.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested listd',
+    )
