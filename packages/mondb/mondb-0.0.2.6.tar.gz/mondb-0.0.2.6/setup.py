47cde2551990f1e410608435bb43d0480cb4545d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:58 +0000

    Add package mondb/0.0.2.6/mondb-0.0.2.6.tar.gz

---
+++ packages/mondb/mondb-0.0.2.6.tar.gz/mondb-0.0.2.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    return open("README.md", "r").read()
+
+setup(
+    name = 'mondb',
+    packages = ['mondb', 'mondb/urltools'],
+    version = '0.0.2.6',
+    long_description= "MODELS for MONGODB",
+    description = 'MongoDB Models',
+    author='plasmashadow',
+    author_email='plasmashadowx@gmail.com',
+    url='https://github.com/RevelutionWind/MDB.git',
+    license="BSD 2-Claus",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires=['pymongo==2.8', 'six >= 1.9.0'],
+    test_suite="test"
+)
