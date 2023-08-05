067da4c51712be5c049b5a35bdff9910b3dfe603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package dsat/0.6.4/dsat-0.6.4.tar.gz

---
+++ packages/dsat/dsat-0.6.4.tar.gz/dsat-0.6.4/dsat/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Temp changlelog for me to remember
+
+0.6.2
+=====
+    adding serialized queue an asbtraction for a serialized queue
+
+0.6.3
+=====
+    better carbon handling
+
+0.6.4
+=====
+    I should clean my home dir
+"""
+
+__short_version__ = "0.6"
+__version__ = __short_version__ + ".4"
+
+
+
+
+
+
---
+++ packages/dsat/dsat-0.6.4.tar.gz/dsat-0.6.4/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+import unittest
+import sys
+import os
+sys.path += [ "./dsat" ]
+
+import dsat
+#TODO call make text with sphinx (please don't use system) before
+#building/install
+
+fcontent = lambda f: open(f).read()
+
+def test():
+    print "TEST SKIPPED"
+    return True
+    loader= unittest.TestLoader()
+    suite=loader.discover("")
+    runner=unittest.TextTestRunner()
+    result=runner.run(suite)
+    if not result.wasSuccessful():
+        raise Exception( "Test Failed: Aborting install")
+
+long_desc = fcontent('README.rst')
+setup(
+        name = "dsat",
+        version = dsat.__version__,
+        description = long_desc,
+        packages = [ 'dsat', ],
+        author_email = "julien@tayon.net",
+        url='http://dsat.readthedocs.org/',
+        install_requires = map(
+            str.strip,
+            open("requirements.txt").readlines()
+        ),
+        ### doc writer don't be stupid
+        ### if there is an order for the file then use alphabetical
+        license = fcontent('LICENSE.txt'),
+        include_package_data = True,
+        long_description = long_desc,
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 2.7',
+            'License :: OSI Approved :: BSD License',
+            'Environment :: Console',
+            'Operating System :: POSIX :: Linux',
+            'Topic :: System :: Distributed Computing'
+        ],
+#        test_suite="tests",
+)
+
+if 'install' in sys.argv or 'develop' in sys.argv:
+    test()
+    print long_desc
+
