57a165f54f2a34ed2cbcca06f2c72816e3cf897f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package python_da/0.0.3/python_da-0.0.3.tar.gz

---
+++ packages/python_da/python_da-0.0.3.tar.gz/python_da-0.0.3/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, Extension
+
+version = '0.0.3'
+name = 'python_da'
+short_description = "`python_da` is Python Double Array Library."
+long_description = """\
+`python_da` is Python Double Array Library.
+
+Requirements
+------------
+* Python 2.7 or later (not support 3.x)
+
+Features
+--------
+* nothing
+
+Setup
+-----
+::
+  $ easy_install python_da
+
+History
+-------
+0.0.1 (2015-04-24)
+------------------
+* first release
+"""
+
+classifiers = [
+  "Development Status :: 3 - Alpha",
+  "License :: OSI Approved :: MIT License",
+  "Programming Language :: C++",
+  "Topic :: Software Development :: Libraries"
+]
+
+module = Extension('python_da', [
+  'ext/python_da.cpp'
+], include_dirs=["libda/include"])
+
+setup(name=name,
+      version=version,
+      description=short_description,
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords=['Data Structure'],
+      author="Masahiko Higashiyama",
+      author_email="masahiko.higashiyama@gmail.com",
+      url='https://github.com/shnya/python_da',
+      license='MIT',
+      ext_modules=[module],
+     )
