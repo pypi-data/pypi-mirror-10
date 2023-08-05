8dd05dfee59bdfafa0fa6630a9ca4be64beb0639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package lazy-paged-sequence/0.2/lazy_paged_sequence-0.2.tar.gz

---
+++ packages/lazy-paged-sequence/lazy_paged_sequence-0.2.tar.gz/lazy_paged_sequence-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="lazy_paged_sequence",
+    author="Misty De Meo",
+    author_email="mistydemeo@gmail.com",
+    license="AGPL",
+    version="0.2"
+)
