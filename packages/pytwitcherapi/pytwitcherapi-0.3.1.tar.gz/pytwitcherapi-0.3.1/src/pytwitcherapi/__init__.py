aea28841ae01890e70689b8c7b5051794779f57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package pytwitcherapi/0.3.1/pytwitcherapi-0.3.1.tar.gz

---
+++ packages/pytwitcherapi/pytwitcherapi-0.3.1.tar.gz/pytwitcherapi-0.3.1/src/pytwitcherapi/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from .models import *
+from .session import *
+from .exceptions import *
+
+__all__ = [models.__all__ +
+           session.__all__ +
+           exceptions.__all__]
+
+__author__ = 'David Zuber'
+__email__ = 'zuber.david@gmx.de'
+__version__ = '0.3.1'
