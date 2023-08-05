0557743ea9fe7f3f827ddff8b378a03549346c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package json-cfg/0.3.0/json-cfg-0.3.0.zip

---
+++ packages/json-cfg/json-cfg-0.3.0.zip/json-cfg-0.3.0/setup.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import codecs
+try:
+    from setuptools import setup
+    have_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    have_setuptools = False
+
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def find_version(*path):
+    with codecs.open(os.path.join(script_dir, *path), 'r', 'latin1') as f:
+        contents = f.read()
+
+    # The version line must have the form
+    # version_info = (X, Y, Z)
+    m = re.search(
+        r'^version_info\s*=\s*\(\s*(?P<v0>\d+)\s*,\s*(?P<v1>\d+)\s*,\s*(?P<v2>\d+)\s*\)\s*$',
+        contents,
+        re.MULTILINE,
+    )
+    if m:
+        return '%s.%s.%s' % (m.group('v0'), m.group('v1'), m.group('v2'))
+    raise RuntimeError('Unable to determine package version.')
+
+
+# Get the long description from the relevant file
+#with open('DESCRIPTION.rst', encoding='utf-8') as f:
+#    long_description = f.read()
+
+
+extra_setup_params = {}
+if have_setuptools:
+    extra_setup_params['test_suite'] = 'tests'
+
+
+setup(
+    name='json-cfg',
+    version=find_version('jsoncfg', '__init__.py'),
+    description='JSON config file parser with extended syntax (e.g.: comments), '
+                'line/column numbers and other extras.',
+    #long_description=long_description,
+
+    url='https://github.com/pasztorpisti/json-cfg',
+
+    author='István Pásztor',
+    author_email='pasztorpisti@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords='json configuration jsonconfiguration json-configuration comment',
+    packages=['jsoncfg', 'tests'],
+
+    **extra_setup_params
+)
---
+++ packages/json-cfg/json-cfg-0.3.0.zip/json-cfg-0.3.0/tests/test_functions.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+from jsoncfg import loads, loads_config, ParserException, JSONParserParams
+
+
+TEST_JSON_STRING = """
+{
+    int: 5,
+    float: 1.5,
+    str: "strval",
+    null: null,
+    true: true,
+    false: false,
+    array: [{a:0},{b:1}],
+    obj: {},
+}
+"""
+
+TEST_JSON_VALUE = {
+    'int': 5,
+    'float': 1.5,
+    'str': 'strval',
+    'null': None,
+    'true': True,
+    'false': False,
+    'array': [{'a': 0}, {'b': 1}],
+    'obj': {},
+}
+
+
+class TestLoads(TestCase):
+    def test_object_and_standard_json_datatype_loading(self):
+        obj = loads(TEST_JSON_STRING)
+        self.assertDictEqual(obj, TEST_JSON_VALUE)
+
+    def test_root_is_array(self):
+        lst = loads("[0, 1, 2]", JSONParserParams(root_is_array=True))
+        self.assertListEqual(lst, [0, 1, 2])
+
+    def test_invalid_json_scalar(self):
+        self.assertRaisesRegexp(ParserException, 'Invalid json scalar: "invalid_scalar"',
+                                loads, '{key:invalid_scalar}')
+
+    def test_duplicate_key(self):
+        self.assertRaisesRegexp(ParserException, 'Duplicate key: "my_duplicate_key"',
+                                loads, '{my_duplicate_key:0,my_duplicate_key:0}')
+
+
+class TestLoadsConfig(TestCase):
+    def test_object_and_standard_json_datatype_loading(self):
+        obj = loads_config(TEST_JSON_STRING)
+        self.assertDictEqual(obj(), TEST_JSON_VALUE)
+
+    def test_root_is_array(self):
+        lst = loads_config("[0, 1, 2]", JSONParserParams(root_is_array=True))
+        self.assertListEqual(lst(), [0, 1, 2])
+
+    def test_invalid_json_scalar(self):
+        self.assertRaisesRegexp(ParserException, 'Invalid json scalar: "invalid_scalar"',
+                                loads_config, '{key:invalid_scalar}')
+
+    def test_duplicate_key(self):
+        self.assertRaisesRegexp(ParserException, 'Duplicate key: "my_duplicate_key"',
+                                loads_config, '{my_duplicate_key:0,my_duplicate_key:0}')
