aa51a4a163744e5b18054cbc966e45deff2b08fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package uwsgi-tasks/0.3/uwsgi-tasks-0.3.tar.gz

---
+++ packages/uwsgi-tasks/uwsgi-tasks-0.3.tar.gz/uwsgi-tasks-0.3/setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from distutils.core import setup
+
+
+if sys.argv[-1] == 'test':
+    # python-mock is required to run unit-tests
+    import unittest
+    unittest.main('uwsgi_tasks.tests', argv=sys.argv[:-1])
+
+
+def get_long_description():
+    with open('./README.md') as f:
+        readme = f.read()
+
+    try:
+        import pandoc
+    except ImportError:
+        return readme
+
+    path = None
+    for p in ('/usr/local/bin/pandoc', '/usr/bin/pandoc'):
+        if os.path.exists(p):
+            path = p
+
+    if path is None:
+        print 'pandoc not found'
+        return readme
+
+    pandoc.core.PANDOC_PATH = path
+    doc = pandoc.Document()
+    doc.markdown = readme
+    return doc.rst
+
+
+setup(
+    name='uwsgi-tasks',
+    packages=['uwsgi_tasks'],
+    version='0.3',
+    description='Asynchronous tasks management with UWSGI server',
+    author='Oleg Churkin',
+    author_email='bahusoff@gmail.com',
+    url='https://github.com/Bahus/uwsgi_tasks',
+    keywords=['asynchronous', 'tasks', 'uwsgi'],
+    platforms='Platform Independent',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ],
+    long_description=get_long_description(),
+    requires=['uwsgi'],
+)
