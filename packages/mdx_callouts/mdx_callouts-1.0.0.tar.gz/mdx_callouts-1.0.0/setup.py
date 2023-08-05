96a3afeea33dbc46a62108c9ebf529b6b3b058a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package mdx_callouts/1.0.0/mdx_callouts-1.0.0.tar.gz

---
+++ packages/mdx_callouts/mdx_callouts-1.0.0.tar.gz/mdx_callouts-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mdx_callouts',
+    version='1.0.0',
+    description='Ability to model callouts in Markdown',
+    author='Marco Ceppi',
+    author_email='marco@ceppi.net',
+    url='https://github.com/marcoceppi/mdx_callouts',
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires = ['setuptools', 'markdown'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
