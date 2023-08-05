c55444d5d89fea2baa287bfe72544fbf26256c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:15 +0000

    Add package O365/0.5.2/O365-0.5.2.tar.gz

---
+++ packages/O365/O365-0.5.2.tar.gz/O365-0.5.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='O365',
+	version='0.5.2',
+	description='Python library for working with Microsoft Office 365',
+	author='Toben Archer',
+	author_email='sandslash+O365@gmail.com',
+	maintainer='Toben Archer',
+	maintainer_email='sandslash+O365@gmail.com',
+	url='https://github.com/Narcolapser/python-o365',
+	packages=['O365']
+	)
+
