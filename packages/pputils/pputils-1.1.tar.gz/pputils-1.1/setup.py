fac042ff4a4546689c91dce3eda8479924aa859e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package pputils/1.1/pputils-1.1.tar.gz

---
+++ packages/pputils/pputils-1.1.tar.gz/pputils-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "pputils",
+    version=1.1,
+    description="utils by python",
+    author='Peng Jun',
+    license='PJ',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={'console_scripts': [
+    'pputils = pputils.main:main']},
+        )
