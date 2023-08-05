c2f00a6f337b22758e6963503be28559325b5ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package twitchy-term/1.0.1/twitchy-term-1.0.1.tar.gz

---
+++ packages/twitchy-term/twitchy-term-1.0.1.tar.gz/twitchy-term-1.0.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+	name='twitchy-term',
+	version='1.0.1',
+	description='A terminal tool for browsing/streaming on Twitch.tv.',
+	long_description=open('README.rst').read(),
+	url='https://github.com/Andy-Au/twitchy-term',
+	author='Andy Au',
+	author_email='au.andy.ch@gmail.com',
+	license='MIT',
+	keywords='twitch livestreamer command line interface terminal stream',
+	classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console :: Curses',
+        'Operating System :: POSIX',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Games/Entertainment',
+        ],
+    packages=find_packages(),
+    install_requires=['livestreamer'],
+	entry_points={
+        'console_scripts': [
+            'twitchy-term=twitchyterm.__main__:main',
+        ],
+    },
+)
+
+
