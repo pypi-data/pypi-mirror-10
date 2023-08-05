81dbca7293316cb3651cea945508415b02107f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:55 +0000

    Add package ajenti.plugin.core/0.27/ajenti.plugin.core-0.27.tar.gz

---
+++ packages/ajenti.plugin.core/ajenti.plugin.core-0.27.tar.gz/ajenti.plugin.core-0.27/ajenti_plugin_core/main.py
@@ -0,0 +1,46 @@
+from aj.plugins.core.api.sidebar import *
+
+
+@component(SidebarItemProvider)
+class ItemProvider(SidebarItemProvider):
+    def __init__(self, context):
+        pass
+
+    def provide(self):
+        return [
+            {
+                'attach': None,
+                'id': 'category:general',
+                'name': 'General',
+                'children': [
+                ]
+            },
+            {
+                'attach': None,
+                'id': 'category:tools',
+                'name': 'Tools',
+                'children': [
+                ]
+            },
+            {
+                'attach': None,
+                'id': 'category:software',
+                'name': 'Software',
+                'children': [
+                ]
+            },
+            {
+                'attach': None,
+                'id': 'category:system',
+                'name': 'System',
+                'children': [
+                ]
+            },
+            {
+                'attach': None,
+                'id': 'category:other',
+                'name': 'Other',
+                'children': [
+                ]
+            },
+        ]
