9f6579334bd8ca8a9c83add495f6937bd56f29fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package pipreqs/0.2.1/pipreqs-0.2.1.tar.gz

---
+++ packages/pipreqs/pipreqs-0.2.1.tar.gz/pipreqs-0.2.1/tests/_data/test.py
@@ -0,0 +1,24 @@
+import atexit
+from __future__ import print_function
+from docopt import docopt
+import curses, logging, sqlite3
+import logging
+import os
+import sqlite3
+import time
+import sys
+import signal
+import requests
+import nonexistendmodule
+# import django
+import flask.ext.somext
+from sqlalchemy import model
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+import models
+
+def main():
+	pass
