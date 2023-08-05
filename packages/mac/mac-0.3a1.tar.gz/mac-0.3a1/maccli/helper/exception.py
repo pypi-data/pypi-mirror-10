cecc32236db2d3b0d47e6b48120891effd4e2f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package mac/0.3a1/mac-0.3a1.tar.gz

---
+++ packages/mac/mac-0.3a1.tar.gz/mac-0.3a1/maccli/helper/exception.py
@@ -0,0 +1,23 @@
+class MacApiError(Exception):
+    """An error status code was returned when querying the HTTP API"""
+    pass
+
+
+class MacAuthError(MacApiError):
+    """An 401 Unauthorized status code was returned when querying the API"""
+    pass
+
+class MacParseEnvException(Exception):
+    """Error while parsing variables in a macfile"""
+    pass
+
+class MacErrorCreatingTier(Exception):
+    """Error while parsing variables in a macfile"""
+    pass
+
+class FactError(Exception):
+    """ Error in the Fact  """
+    pass
+
+class InternalError(RuntimeError):
+    pass
---
+++ packages/mac/mac-0.3a1.tar.gz/mac-0.3a1/maccli/view/view_generic.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import sys
+
+
+def show(string=""):
+    print(string)
+
+
+def show_error(string):
+    print(string, file=sys.stderr)
+
+
+def general_help():
+    print("Welcome to the Pre-alpha version of Manageacloud.cli. This tool is under heavy development.")
+    print("For more information, please visit https://alpha.manageacloud.com")
+    print("")
+    print("To display the available options:")
+    print("")
+    print("    mac -h")
+    print("")
+
+
+def header(text, char=None):
+    if char is None:
+        char = "-"
+    print(text)
+    print(char * len(text))
+    print()
