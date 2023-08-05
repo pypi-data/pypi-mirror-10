c1d4108241c00b947ef4ad0c24e07ed26b0c6f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package KC_sanitize/1.0.0/KC_sanitize-1.0.0.tar.gz

---
+++ packages/KC_sanitize/KC_sanitize-1.0.0.tar.gz/KC_sanitize-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			=	'KC_sanitize',
+	version		    =	'1.0.0',
+	py_modules		=	['KC_sanitize'],
+	author			=	'Kevin Chen',
+    author_email    =   'hchen66@ucsc.edu',
+    url             =   'ucsc.edu',
+	description		=	'A simple divided for mins and secs',
+         )
