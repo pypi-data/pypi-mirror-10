9a2ce177be54ddfed83daef479c6cdb6a7d85eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package pysingleton/0.01.00/pysingleton-0.01.00.tar.gz

---
+++ packages/pysingleton/pysingleton-0.01.00.tar.gz/pysingleton-0.01.00/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pysingleton",
+    description="Use singletons with a decorator",
+    version="0.01.00",
+    author="Timo Furrer",
+    author_email="tuxtimo@gmail.com",
+    url="https://github.com/timofurrer/singleton",
+    packages=["singleton"]
+)
