195a8a74c1e5a7ba48ab7df1d6c41ebafa406efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package dharma/1.1/dharma-1.1.tar.gz

---
+++ packages/dharma/dharma-1.1.tar.gz/dharma-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+	name = 'dharma',
+	packages = ['dharma'],
+	version = '1.1',
+	description = 'A generation-based, context-free grammar fuzzer.',
+	author = 'Mozilla Security',
+	author_email = 'fuzzing@mozilla.com',
+	url = 'https://github.com/mozillasecurity/dharma',
+	download_url = 'https://github.com/mozillasecurity/dharma/tarball/1.1',
+	keywords = ['fuzzer', 'security', 'fuzzing', 'testing'],
+	classifiers = [ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+		'Intended Audience :: Developers',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+		'Topic :: Software Development :: Testing',
+	]
+)
