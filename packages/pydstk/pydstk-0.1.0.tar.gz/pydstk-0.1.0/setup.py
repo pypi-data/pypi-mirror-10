d60cf3008913f819522d4eb3d49e7769792eccaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package pydstk/0.1.0/pydstk-0.1.0.tar.gz

---
+++ packages/pydstk/pydstk-0.1.0.tar.gz/pydstk-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+CLASSIFIERS = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Developers',
+  'Topic :: Scientific/Engineering',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+  'Programming Language :: Python :: 3.2',
+  'Programming Language :: Python :: 3.3',
+  'Programming Language :: Python :: 3.4',
+]
+
+setup(
+  name='pydstk',
+  version='0.1.0',
+  description='Data Science Toolkit client library for Python',
+  py_modules=['pydstk'],
+  author='George Leslie-Waksman',
+  author_email='george@cloverhealth.com',
+  url='http://github.com/CloverHealth/pydstk',
+  license='MIT',
+  classifiers=CLASSIFIERS,
+  keywords='dstk',
+)
