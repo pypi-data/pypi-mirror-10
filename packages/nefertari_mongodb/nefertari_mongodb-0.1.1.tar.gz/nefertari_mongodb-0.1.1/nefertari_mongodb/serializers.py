aa745a27584c19865fa132421b6a768d0c0ce02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package nefertari_mongodb/0.1.1/nefertari_mongodb-0.1.1.tar.gz

---
+++ packages/nefertari_mongodb/nefertari_mongodb-0.1.1.tar.gz/nefertari_mongodb-0.1.1/nefertari_mongodb/serializers.py
@@ -0,0 +1,54 @@
+import logging
+import datetime
+import decimal
+
+import elasticsearch
+from bson import ObjectId, DBRef
+
+from nefertari.renderers import _JSONEncoder
+
+
+log = logging.getLogger(__name__)
+
+
+class JSONEncoder(_JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+
+        if isinstance(obj, DBRef):
+            return str(obj)
+
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
+
+        if (isinstance(obj, datetime.date) and
+                not isinstance(obj, datetime.datetime)):
+            return obj.strftime('%Y-%m-%d')
+
+        if hasattr(obj, 'to_dict'):
+            # If it got to this point, it means its a nested object.
+            # outter objects would have been handled with DataProxy.
+            return obj.to_dict(__nested=True)
+
+        return super(JSONEncoder, self).default(obj)
+
+
+class ESJSONSerializer(elasticsearch.serializer.JSONSerializer):
+    def default(self, data):
+        if isinstance(data, (ObjectId, DBRef)):
+            return str(data)
+        if (isinstance(data, datetime.date) and
+                not isinstance(data, datetime.datetime)):
+            return data.strftime('%Y-%m-%d')
+        if isinstance(data, datetime.time):
+            return data.strftime('%H:%M:%S')
+        if isinstance(data, datetime.timedelta):
+            return str(data)
+        if isinstance(data, decimal.Decimal):
+            return float(data)
+        try:
+            return super(ESJSONSerializer, self).default(data)
+        except:
+            import traceback
+            log.error(traceback.format_exc())
---
+++ packages/nefertari_mongodb/nefertari_mongodb-0.1.1.tar.gz/nefertari_mongodb-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'mongoengine==0.9',
+    'pymongo==2.8',
+    'zope.dottedname',
+    'elasticsearch',
+    'python-dateutil',
+    'pyramid_tm',
+    'nefertari==0.2.1'
+]
+
+setup(
+    name='nefertari_mongodb',
+    version="0.1.1",
+    description='mongodb engine for nefertari',
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+    ],
+    author='Brandicted',
+    author_email='hello@brandicted.com',
+    url='https://github.com/brandicted/nefertari-mongodb',
+    keywords='web wsgi bfg pylons pyramid rest mongodb mongoengine',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
