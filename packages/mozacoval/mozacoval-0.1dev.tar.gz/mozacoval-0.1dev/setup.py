987ba191809b4265d56a279740b405c2c4cbfc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:09 +0000

    Add package mozacoval/0.1dev/mozacoval-0.1dev.tar.gz

---
+++ packages/mozacoval/mozacoval-0.1dev.tar.gz/mozacoval-0.1dev/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env /home/tops/bin/python
+# -*- coding: utf-8 -*-
+# Author: fuchang.lz
+# Copyright (C) taobao.com 2015
+
+from distutils.core import setup
+
+setup(
+    name='mozacoval',
+    version='0.1dev',
+    packages=['mozacoval',],
+    license='Creative Commons',
+    long_description=open('README.txt').read(),
+)
