6e37508a95b991501a2e2c80a29915785bfa7200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:01 +0000

    Add package pyggplot/22/pyggplot-22.tar.gz

---
+++ packages/pyggplot/pyggplot-22.tar.gz/pyggplot-22/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+import os
+
+long_description = "Please visit https://github.com/TyberiusPrime/pyggplot for full description"
+if os.path.exists('README.md'):
+    with open('README.md') as op:
+        long_description = op.read()
+
+setup(
+    name='pyggplot',
+    version='22',
+    packages=['pyggplot',],
+    license='BSD',
+    #url='http://code.google.com/p/pydataframe/',
+    author='Florian Finkernagel',
+    description = "A pythonic wrapper around R's ggplot",
+    author_email='finkernagel@coonabibba.de',
+    long_description=long_description,
+    package_data={'pyggplot': ['LICENSE.txt', 'README.txt']},
+    include_package_data=True,    install_requires=[
+        'pandas>=0.15',
+        'rpy2',
+        'ordereddict',
+        ]
+)
