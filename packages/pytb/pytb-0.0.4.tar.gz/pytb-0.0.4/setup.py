8d596dc214274b60d91c5e6f91508f18298bf5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package pytb/0.0.4/pytb-0.0.4.tar.gz

---
+++ packages/pytb/pytb-0.0.4.tar.gz/pytb-0.0.4/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+try:
+   import pypandoc
+   description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   description = open('README.md').read()
+   raise
+
+packages = {
+    'pytb': 'py-modules/',
+    'pytb.scripts': 'py-modules/scripts'
+}
+
+setup(
+    name="pytb",
+    version="0.0.4",
+    author="mitmeedle",
+    author_email="mitmeedle@gmail.com",
+    entry_points={
+        'console_scripts': [
+            'pytb = pytb.scripts.pytb:main',
+        ]
+    },
+    packages=packages,
+    package_dir=packages,
+    long_description=description
+)
