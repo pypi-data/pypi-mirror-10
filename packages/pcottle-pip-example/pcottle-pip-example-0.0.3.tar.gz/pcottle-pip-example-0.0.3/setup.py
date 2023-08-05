e51c10a49ddb15c4652214891df0f2dc7c767380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package pcottle-pip-example/0.0.3/pcottle-pip-example-0.0.3.tar.gz

---
+++ packages/pcottle-pip-example/pcottle-pip-example-0.0.3.tar.gz/pcottle-pip-example-0.0.3/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='pcottle-pip-example',
+    packages=['pcottle-pip-example'],
+    version='0.0.3',
+    author=u'Peter Cottle',
+    author_email='petermcottle@gmail.com',
+    url='https://github.com/pcottle/example_for_pip',
+    license='BSD licence, see LICENCE.txt',
+    description='print out a simple statement',
+)
