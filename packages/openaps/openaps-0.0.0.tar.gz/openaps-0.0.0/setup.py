2e181b7a54d292dbd6fb6e260bedcd4682aa81fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package openaps/0.0.0/openaps-0.0.0.tar.gz

---
+++ packages/openaps/openaps-0.0.0.tar.gz/openaps-0.0.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+import openaps
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(name='openaps',
+    version=openaps.__version__, # http://semver.org/
+    description='DIY Open Source Artificial Pancreas System.',
+    long_description=readme(),
+    author="OpenAPS",
+    author_email="bewest+openaps@gmail.com",
+    # url="https://github.com/openaps/openaps",
+    url="https://openaps.org/",
+    packages=find_packages( ),
+    include_package_data = True,
+    install_requires = [
+      'pyserial', 'python-dateutil', 'argcomplete',
+      'gitpython',
+      'decocare', 'dexcom_reader'
+    ],
+    dependency_links = [
+      'http://github.com/compbrain/dexcom_reader/tarball/master#egg=dexcom_reader-0.0.1',
+      'http://github.com/bewest/decoding-carelink/tarball/master',
+    ],
+    scripts = [
+      'bin/openaps',
+      'bin/openaps-device',
+      'bin/openaps-enact',
+      'bin/openaps-suggest',
+      'bin/openaps-get',
+      'bin/openaps-use',
+      'bin/openaps-report',
+      'bin/openaps-vendor',
+      'bin/git-openaps-init',
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=False,
+)
+
+#####
+# EOF
