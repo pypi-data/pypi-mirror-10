24c7cad8b1e52cedb331866a00ab58ee3d79eb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package tangent-tokenauth/0.0.2/tangent-tokenauth-0.0.2.tar.gz

---
+++ packages/tangent-tokenauth/tangent-tokenauth-0.0.2.tar.gz/tangent-tokenauth-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tangent-tokenauth',
+    version='0.0.2',
+    author=u'Tangent Solutions',
+    author_email=u'admin@tangentsolutions.co.za',
+    packages=['tokenauth'],
+    include_package_data=True,
+    install_requires=required,
+    url='https://github.com/TangentMicroServices/PythonAuthenticationLib',
+    license='MIT licence, see LICENCE',
+    description='Authentication lib for Tangent Micro Services',
+    long_description=open('README.md').read(),
+)
