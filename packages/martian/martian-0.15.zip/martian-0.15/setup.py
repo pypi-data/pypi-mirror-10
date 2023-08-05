b4fd215e16c62d3b3932d38f6a3d8a2681e1debc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package martian/0.15/martian-0.15.zip

---
+++ packages/martian/martian-0.15.zip/martian-0.15/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('src', 'martian', 'README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+setup(
+    name='martian',
+    version='0.15',
+    author='Grok project',
+    author_email='grok-dev@zope.org',
+    description="""\
+Martian is a library that allows the embedding of configuration
+information in Python code. Martian can then grok the system and
+do the appropriate configuration registrations. One example of a system
+that uses Martian is the system where it originated: Grok
+(http://grok.zope.org)
+""",
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL',
+    test_suite='martian.tests.test_all.test_suite',
+    install_requires=[
+        'zope.interface',
+        'setuptools',
+    ],
+    extras_require=dict(test=['zope.testing']),
+)
