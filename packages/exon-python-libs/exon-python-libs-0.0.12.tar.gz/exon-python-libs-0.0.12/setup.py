34b490a7e4d4ba753f5c6648a6e78ceb37dd3696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:23 +0000

    Add package exon-python-libs/0.0.12/exon-python-libs-0.0.12.tar.gz

---
+++ packages/exon-python-libs/exon-python-libs-0.0.12.tar.gz/exon-python-libs-0.0.12/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "exon-python-libs",
+    version = "0.0.12",
+    packages = find_packages(),
+	# metadata for upload to PyPI
+    author = "Stephan Conrad",
+    author_email = "stephan@conrad.pics",
+    description = "Exon Python Library",
+    license = "Apache License (2.0)",
+    keywords = "exon-python-libs",
+    url = "http://exon.conrad.pics",
+)
