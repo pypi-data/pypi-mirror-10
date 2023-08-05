fcd3ce90f187f9f615f61ad62d6e24b0e654c097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:44 +0000

    Add package buildout.requirements/0.1.1/buildout.requirements-0.1.1.tar.gz

---
+++ packages/buildout.requirements/buildout.requirements-0.1.1.tar.gz/buildout.requirements-0.1.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1.1'
+
+long_description = (read('README.rst') + read('CONTRIBUTORS.rst'))
+entry_point = 'buildout.requirements:install'
+entry_points = {'zc.buildout.extension': ['ext = %s' % entry_point]}
+
+tests_require = ['zc.buildout', 'zope.testing', 'zc.recipe.egg']
+
+setup(name='buildout.requirements',
+      version=version,
+      description='Dump buildout picked versions '
+      'as a pip compatible requirements.txt file. Fork of buildout.dumprequirements.',
+      long_description=long_description,
+      classifiers=[
+          'Framework :: Buildout',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      license='GPL',
+      keywords='buildout extension dump picked versions requirements.txt',
+      author='Yves Müller',
+      author_email='yves.mueller@liqd.de',
+      url='https://github.com/liqd/adhocracy3.mercator/tree/master/src/buildout.requirements',
+      packages=find_packages(),
+      namespace_packages=['buildout'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      entry_points=entry_points)
