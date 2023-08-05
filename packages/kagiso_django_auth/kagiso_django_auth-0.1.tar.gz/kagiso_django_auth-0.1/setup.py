216077cccf51f15f57ea03bc92e78b155a8b6d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:54 +0000

    Add package kagiso_django_auth/0.1/kagiso_django_auth-0.1.tar.gz

---
+++ packages/kagiso_django_auth/kagiso_django_auth-0.1.tar.gz/kagiso_django_auth-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='kagiso_django_auth',
+    version='0.1',
+    author='Kagiso Media',
+    author_email='development@kagiso.io',
+    url='https://github.com/Kagiso-Future-Media/django_auth',
+    install_requires=[
+            'jsonfield==1.0.3',
+            'requests==2.6.0',
+    ],
+)
