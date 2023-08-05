385357d89a917c2eaad1fff70b239bb26bc7c909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:00 +0000

    Add package pyAverager/0.9.0/pyAverager-0.9.0.tar.gz

---
+++ packages/pyAverager/pyAverager-0.9.0.tar.gz/pyAverager-0.9.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyAverager',
+      version='0.9.0',
+      description='Parallel Python Averager for Climate Data',
+      author='Sheri Mickelson',
+      author_email='mickelso@ucar.edu',
+      url='https://github.com/sherimickelson/pyAverager',
+      packages=['pyaverager'],
+     )
