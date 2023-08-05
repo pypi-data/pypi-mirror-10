ab908e18e593478b1050175a515407a5c1ef7dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:08 +0000

    Add package pymldb/0.1.1/pymldb-0.1.1-py2-none-any.whl

---
+++ packages/pymldb/pymldb-0.1.1-py2-none-any.whl/mldb/resource.py
@@ -0,0 +1,56 @@
+import requests, json, mldb
+
+
+def decorate_response(fn):
+    def inner(*args, **kwargs):
+        return mldb.add_repr_html_to_response(fn(*args, **kwargs))
+    return inner
+
+class Resource(object):
+    def __init__(self, uri="http://localhost"):
+        if not uri.startswith("http"): 
+            raise Exception("URIs must start with 'http'")
+        self.uri = uri.strip("/")
+    
+    def __repr__(self):
+        return self.uri
+
+    def __str__(self):
+        return self.uri
+    
+    def __call__(self, frag):
+        return Resource(self.uri+"/"+str(frag).strip("/"))
+                                              
+    def __getattr__(self, frag): 
+        return Resource(self.uri+"/"+str(frag).strip("/"))
+    
+    @decorate_response
+    def get(self, *args, **kwargs): 
+        return requests.get(self.uri, **kwargs)
+    
+    @decorate_response
+    def get_query(self, *args, **kwargs): 
+        return requests.get(self.uri, 
+            params= {k: (json.dumps(v) if isinstance(v, dict) else v) 
+                 for k,v in dict(**kwargs).iteritems() })
+    
+    @decorate_response
+    def put(self, *args, **kwargs): 
+        return requests.put(self.uri, **kwargs)
+    
+    @decorate_response
+    def put_json(self, payload): 
+        return requests.put(self.uri, data=json.dumps(payload))
+    
+    @decorate_response
+    def post(self, *args, **kwargs): 
+        return requests.post(self.uri, **kwargs)
+    
+    @decorate_response
+    def post_json(self, payload): 
+        return requests.post(self.uri, data=json.dumps(payload))
+    
+    @decorate_response
+    def delete(self, *args, **kwargs): 
+        return requests.delete(self.uri, **kwargs)
+    
