ad906a0fc51808e592750a217b8881569fcda78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:35 +0000

    Add package deflake/0.0.3/deflake-0.0.3.tar.gz

---
+++ packages/deflake/deflake-0.0.3.tar.gz/deflake-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = "deflake",
+    version = "0.0.3",
+    author = "NCBI",
+    author_email = "",
+    description = ("A program to debug flakey programs"),
+    license = "Public Domain",
+    keywords = "testing flaky",
+    url = "http://packages.python.org/deflake",
+    scripts = ["deflake/deflake.py"],
+    packages = ["deflake"]
+)
