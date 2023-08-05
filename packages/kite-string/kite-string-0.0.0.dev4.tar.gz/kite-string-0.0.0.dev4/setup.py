483ebdb6221e3cdf677457eb1385886d5b014be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package kite-string/0.0.0.dev4/kite-string-0.0.0.dev4.tar.gz

---
+++ packages/kite-string/kite-string-0.0.0.dev4.tar.gz/kite-string-0.0.0.dev4/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+from kite.version import get_version
+
+setup(
+
+    name='kite-string',
+    description='command-line HTTP request wrapper for takosan .',
+    version=get_version(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        kite=kite.cli:main
+    ''',
+    url='https://github.com/laughk/kite-string',
+    author = 'Kei Iwasaki',
+    author_email = 'me@laughk.org',
+
+)
+
