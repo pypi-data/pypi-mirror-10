bafdfbcc98c821857dd04848d9b7a5c5793a0b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:50 +0000

    Add package aj/2.0.35/aj-2.0.35.tar.gz

---
+++ packages/aj/aj-2.0.35.tar.gz/aj-2.0.35/aj/api/__init__.py
@@ -0,0 +1,10 @@
+from aj.api.di import interface, component, service, Context, NoImplementationError
+
+
+__all__ = [
+    'interface',
+    'component',
+    'service',
+    'Context',
+    'NoImplementationError',
+]
