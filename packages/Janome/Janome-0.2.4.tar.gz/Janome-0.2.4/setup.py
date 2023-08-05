87a7b7a2e7e4bbc73ce13c80bc98f10d47e07c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package Janome/0.2.4/Janome-0.2.4.tar.gz

---
+++ packages/Janome/Janome-0.2.4.tar.gz/Janome-0.2.4/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+import os
+from zipfile import ZipFile
+from janome.dic import *
+
+dicdir = 'ipadic'
+
+if not os.path.exists('sysdic') and os.path.exists(os.path.join('ipadic', 'sysdic.zip')):
+    print('Unzip dictionary data...')
+    with ZipFile(os.path.join(dicdir, 'sysdic.zip')) as zf:
+        zf.extractall()
+
+version = '0.2.4'
+name = 'janome'
+short_description = '`janome` is a package for Japanese Morphological Analysis.'
+long_description = """\
+`janome` is a package for Japanese Morphological Analysis.
+
+Requirements
+------------
+* Python 2.7 and Python 3.4+
+
+Features and history
+--------------------
+See http://mocobeta.github.io/janome/ (for Japanese)
+
+"""
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: Apache License 2.0",
+    "Programming Language :: Python",
+    "Topic :: Natural Language :: Japanese",
+    ]
+
+setup(
+    name='Janome',
+    version='0.2.4',
+    description='Japanese morphological analysis engine.',
+    author='Tomoko Uchida',
+    author_email='tomoko.uchida.1111@gmail.com',
+    url='http://mocobeta.github.io/janome/',
+    packages=['janome','sysdic'],
+    package_data={'sysdic': ['fst.data', 'connections.data']},
+    py_modules=['janome.dic','janome.fst','janome.lattice','janome.tokenizer']
+)
+
