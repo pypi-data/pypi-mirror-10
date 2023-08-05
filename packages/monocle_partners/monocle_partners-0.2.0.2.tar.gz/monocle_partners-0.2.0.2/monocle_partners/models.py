cffcce980146b3cc13c0493b58923c9cb6a76454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:18 +0000

    Add package monocle_partners/0.2.0.2/monocle_partners-0.2.0.2.tar.gz

---
+++ packages/monocle_partners/monocle_partners-0.2.0.2.tar.gz/monocle_partners-0.2.0.2/monocle_partners/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from easy_thumbnails.fields import ThumbnailerImageField
+
+class Partner(models.Model):
+    name = models.CharField(max_length=255, blank=False, verbose_name='Название партнера')
+    url = models.CharField(max_length=255, blank=False, verbose_name='Ссылка на сайт')
+    image = models.ImageField(upload_to='partners/%Y/%m/%d', verbose_name='Логотип')
+    position = models.PositiveIntegerField(default=0, verbose_name='Позиция')
+    isShown = models.BooleanField(default=True, verbose_name='Показывать партнера')
+
+    def __str__(self):
+        return self.name
+
+    def image_admin(self):
+        return u'<img src="%s" height="80" />' % self.image.url
+
+    image_admin.short_description = u'Изображение'
+    image_admin.allow_tags = True
+
+    class Meta:
+        verbose_name_plural = u'Партнеры'
+        verbose_name = u'Партнер'
+        ordering = ['position']
