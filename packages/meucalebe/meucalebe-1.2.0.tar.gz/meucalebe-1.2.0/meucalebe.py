059af5404b5cf93c5b734282309bf333c6ad64f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package meucalebe/1.2.0/meucalebe-1.2.0.tar.gz

---
+++ packages/meucalebe/meucalebe-1.2.0.tar.gz/meucalebe-1.2.0/meucalebe.py
@@ -0,0 +1,11 @@
+"""Teste"""
+def print_lol(the_list, level = 0):
+    """Explicacao da funcao"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+        
---
+++ packages/meucalebe/meucalebe-1.2.0.tar.gz/meucalebe-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'meucalebe',
+    version = '1.2.0',
+    py_modules = ['meucalebe'],
+    author = 'calebe',
+    author_email = 'calebebastos21@gmail.com',
+    url = '',
+    description = 'Apenas teste'
+    )
