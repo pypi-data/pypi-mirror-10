d467941e51518cb9691adedc46fac542db043237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package pipeup/0.1.3/pipeup-0.1.3.tar.gz

---
+++ packages/pipeup/pipeup-0.1.3.tar.gz/pipeup-0.1.3/pipeup/cli.py
@@ -0,0 +1,86 @@
+
+import json
+import logging
+import sys
+import time
+from threading import Thread
+
+import click
+
+from websocket import WebSocketApp, WebSocketConnectionClosedException
+
+logging.basicConfig()
+
+
+def on_message(ws, msg):
+    message = json.loads(msg)
+
+    if message['action'] == 'connected':
+        click.echo(click.style('Piping to ' + message['msg'], fg='green'))
+
+
+def on_error(ws, error):
+    pass
+
+
+def on_close(ws):
+    click.echo(click.style('Lost connection.', fg='red'))
+
+
+def wrapper(key):
+    def on_open(ws):
+        click.echo(click.style('Connected.', fg='green'))
+
+        def run(key):
+            ws.send(json.dumps(dict(action='request', key=key)))
+
+            while True:
+                try:
+                    line = sys.stdin.readline()
+                    ws.send(json.dumps(dict(action='send', msg=line)))
+                except KeyboardInterrupt:
+                    break
+                except WebSocketConnectionClosedException:
+                    break
+
+                if not line.strip():
+                    break
+
+            ws.close()
+
+        Thread(target=run, args=(key,)).start()
+
+    return on_open
+
+
+def validate_key(ctx, param, value):
+    if value and (len(value) != 6 or not value.isalnum()):
+        raise click.BadParameter('key must be 6 character alphanumeric')
+    else:
+        return value
+
+
+@click.command()
+@click.option('--server', default='ws://pipeup.io/ws', help='Websocket URL to pipe to.')
+@click.option('--key', default=None, callback=validate_key, help='6 character alphanumeric key to pipe to.')
+def main(server, key):
+    for i in range(10):
+        click.echo('Connecting...')
+
+        try:
+            ws = WebSocketApp(server,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close,
+                              on_open=wrapper(key))
+
+            ws.run_forever(ping_interval=15)
+        except KeyboardInterrupt:
+            break
+        except:
+            pass
+
+        time.sleep(i * 1.5 + 1.5)
+    else:
+        click.echo(click.style('Failed to reconnect.', fg='red'))
+        sys.exit()
