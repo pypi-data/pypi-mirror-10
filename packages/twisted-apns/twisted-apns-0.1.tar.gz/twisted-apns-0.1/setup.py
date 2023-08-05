ff1196cbed65c563c3137d054c2b7d927cb745cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:17 +0000

    Add package twisted-apns/0.1/twisted-apns-0.1.tar.gz

---
+++ packages/twisted-apns/twisted-apns-0.1.tar.gz/twisted-apns-0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+  name = 'twisted-apns',
+  packages = ['apns'],
+  version = '0.1',
+  description = 'Twisted client for Apple Push Notification Service (APNs)',
+  author = 'Michał Łowicki',
+  author_email = 'mlowicki@opera.com',
+  url = 'https://github.com/operasoftware/twisted-apns',
+  download_url = 'https://github.com/operasoftware/twisted-apns/tarball/0.1',
+  keywords = ['twisted', 'apns'],
+  classifiers = [],
+  install_requires=['Twisted', 'pyOpenSSL']
+)
