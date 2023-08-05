e9b5d98f71efbaa4786b4ba817cb72e89b61db59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package mezzanine-slideshows/0.2.3/mezzanine-slideshows-0.2.3.tar.gz

---
+++ packages/mezzanine-slideshows/mezzanine-slideshows-0.2.3.tar.gz/mezzanine-slideshows-0.2.3/mezzanine_slideshows/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from mezzanine.pages.models import RichTextPage
+from mezzanine.galleries.models import Gallery
+
+
+class Slideshow(models.Model):
+    page = models.OneToOneField(RichTextPage)
+    galleries = models.ManyToManyField(Gallery)
+
+    class Meta:
+        verbose_name = "Slideshow"
+        verbose_name_plural = "Slideshows"
+
+    def __str__(self):
+        return self.page.title
