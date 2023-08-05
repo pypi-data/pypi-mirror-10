c2f464562a68a2d7f9bdd553fefe692a914d6793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package confined/0.1.6/confined-0.1.6.tar.gz

---
+++ packages/confined/confined-0.1.6.tar.gz/confined-0.1.6/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: "utf-8" -*-
+
+from distutils.core import setup
+
+import sys
+if "install" in sys.argv or "setup" in sys.argv or "sdist" in sys.argv:
+    from confined import test_valid
+    import unittest
+    loader= unittest.TestLoader()
+    suite=loader.loadTestsFromModule(test_valid)
+    runner=unittest.TextTestRunner(verbosity=2)
+    result=runner.run(suite)
+    if  not result.wasSuccessful():
+        raise Exception( "Test Failed")
+
+setup(
+        name='confined',
+        version='0.1.6',
+        author='Julien Tayon',
+        author_email='julien@tayon.net',
+        packages=['confined'],
+        url='http://github.org/jul/confined',
+        license="License :: OSI Approved :: BSD License",
+        description="Sage templating language with bounded resource",
+        long_description=open("README.rst").read(),
+        install_requires=[ open("requirements.txt").read(),  ],
+        requires=[ open("requirements.txt").read(),  ],
+        classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Python Software Foundation License',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ],
+)
+
+
