0ac0da5acb187b3d8eb3c9d0981650f1112acff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package upsilon/1.0/upsilon-1.0.tar.gz

---
+++ packages/upsilon/upsilon-1.0.tar.gz/upsilon-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='upsilon',
+    version='1.0',
+    description='Automated Classification of Periodic Variable Stars Using Machine Learning',
+    platforms=['any'],
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/dwkim78/upsilon',
+    license='MIT',
+    author='Dae-Won Kim',
+    author_email='dwkim78@gmail.com',
+    install_requires=['numpy>=1.9', 'scikit-learn>=0.16.1', 'scipy>=0.15',
+        #'pyfftw>=0.9.2',
+        ]
+)
---
+++ packages/upsilon/upsilon-1.0.tar.gz/upsilon-1.0/upsilon/test/predict.py
@@ -0,0 +1,58 @@
+__author__ = 'kim'
+
+import time
+from upsilon.extract_features.extract_features import ExtractFeatures
+from upsilon.datasets.base import load_EROS_lc
+from upsilon.datasets.base import load_rf_model
+from upsilon.predict.predict import predict
+
+from upsilon.utils.utils import sigma_clipping
+from upsilon.utils.logger import Logger
+
+
+def run():
+    """
+    Test UPSILoN package.
+
+    :return: None
+    """
+
+    logger = Logger().getLogger()
+
+    logger.info('Read a light curve')
+    date, mag, err = load_EROS_lc()
+
+    index = mag < 99.999
+    date = date[index]
+    mag = mag[index]
+    err = err[index]
+
+    logger.info('   Before sigma-clipping: %d data points' % len(date))
+    #date, mag, err = sigma_clipping_without_error(date, mag)
+    date, mag, err = sigma_clipping(date, mag, err)
+    logger.info('   After sigma-clipping: %d data points' % len(date))
+
+    for i in range(1):
+        start = time.time()
+        e_features = ExtractFeatures(date, mag, err)
+        e_features.run()
+        end = time.time()
+        logger.info('Feature extracting time: %.4f seconds' % (end - start))
+
+    logger.info('Extracted features (17 of these are used to predict a class):')
+    features = e_features.get_features_all()
+    for key, value in features.iteritems():
+        logger.info('   %s: %f' % (key, value))
+
+    logger.info('Load the UPSILoN classifier')
+    rf_model = load_rf_model()
+
+    label, prob = predict(rf_model, features)
+    logger.info('Classify the light curve')
+    logger.info('   Classified as %s with the class probability %.2f' %
+        (label, prob))
+
+    logger.handlers = []
+
+if __name__ == '__main__':
+    run()
