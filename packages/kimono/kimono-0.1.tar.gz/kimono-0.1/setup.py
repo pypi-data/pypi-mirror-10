16c891b086db82fecb3456a1ca89864419ecbcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:54 +0000

    Add package kimono/0.1/kimono-0.1.tar.gz

---
+++ packages/kimono/kimono-0.1.tar.gz/kimono-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kimono',
+      version='0.1',
+      description='The Kimono API Python Wrapper',
+      url='https://github.com/vu3jej/kimono',
+      author='Jithesh E J',
+      author_email='mail@jithesh.net',
+      license='MIT',
+      packages=['kimono'],
+      zip_safe=False)
