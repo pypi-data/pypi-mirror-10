560e5ac70b4e83804afd971b71725da932b16cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package xzqs_01/1.0.1/xzqs_01-1.0.1.tar.gz

---
+++ packages/xzqs_01/xzqs_01-1.0.1.tar.gz/xzqs_01-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='xzqs_01',
+	version='1.0.1',
+	py_modules=['nester'],
+	author='xzqs',
+	author_email='382208691@qq.com',
+	url='http://m1992.com',
+	description='My first python descrips',
+)
