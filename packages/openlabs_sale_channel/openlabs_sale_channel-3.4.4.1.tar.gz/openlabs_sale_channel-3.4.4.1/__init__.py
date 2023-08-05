1e5a2d2f63e51186b718f8022592dd7ba9fd1eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package openlabs_sale_channel/3.4.4.1/openlabs_sale_channel-3.4.4.1.tar.gz

---
+++ packages/openlabs_sale_channel/openlabs_sale_channel-3.4.4.1.tar.gz/openlabs_sale_channel-3.4.4.1/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+
+    :copyright: (c) 2015 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from trytond.pool import Pool
+from channel import (
+    SaleChannel, ReadUser, WriteUser, ChannelException
+)
+from wizard import OrderImportWizard, OrderImportWizardStart
+from product import ProductSaleChannelListing, Product
+from sale import Sale
+from user import User
+
+
+def register():
+    Pool.register(
+        SaleChannel,
+        ReadUser,
+        WriteUser,
+        ChannelException,
+        User,
+        Sale,
+        ProductSaleChannelListing,
+        Product,
+        OrderImportWizardStart,
+        module='sale_channel', type_='model'
+    )
+    Pool.register(
+        OrderImportWizard,
+        module='sale_channel', type_='wizard'
+    )
