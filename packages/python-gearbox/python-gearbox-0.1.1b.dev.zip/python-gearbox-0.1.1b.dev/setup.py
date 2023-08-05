8c79ab29102de9d38a8817add38a56b79c85df1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:52 +0000

    Add package python-gearbox/0.1.1b.dev/python-gearbox-0.1.1b.dev.zip

---
+++ packages/python-gearbox/python-gearbox-0.1.1b.dev.zip/python-gearbox-0.1.1b.dev/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '0.1.1b'
+
+setup(
+    name='python-gearbox',
+    version=version,
+    author='Eduardo M. Firvida Donestevez',
+    packages=find_packages(),
+    include_package_data=True,
+    author_email='efirvida@gmail.com',
+    description='Python library for gear transmission design',
+    requires=['numpy', 'scipy', 'jinja2'],
+    url='https://github.com/efirvida/python-gearbox',
+    download_url='https://github.com/efirvida/python-gearbox/archive/master.zip',
+    keywords=['gearbox', 'gear', 'agma', 'iso', 'gear transmission', 'engineering'],
+    platforms='any',
+    license='MIT',
+    zip_safe=False,
+    classifiers=['Intended Audience :: Developers',
+                 'Intended Audience :: Manufacturing',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+                 'Topic :: Software Development',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Software Development :: Libraries :: Application Frameworks'
+                 ]
+)
