ae4087a4389f6dc106e5051447faaec7ed5700d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:08 +0000

    Add package invocations/0.9.1/invocations-0.9.1-py2-none-any.whl

---
+++ packages/invocations/invocations-0.9.1-py2-none-any.whl/invocations/testing.py
@@ -0,0 +1,23 @@
+from invoke import ctask as task
+
+
+@task(help={
+    'module': "Just runs tests/STRING.py.",
+    'runner': "Use STRING to run tests instead of 'spec'.",
+    'opts': "Extra flags for the test runner",
+    'pty': "Whether to run tests under a pseudo-tty",
+})
+def test(c, module=None, runner=None, opts=None, pty=True):
+    """
+    Run a Spec or Nose-powered internal test suite.
+    """
+    runner = runner or 'spec'
+    # Allow selecting specific submodule
+    specific_module = " --tests=tests/%s.py" % module
+    args = (specific_module if module else "")
+    if opts:
+        args += " " + opts
+    # Always enable timing info by default. OPINIONATED
+    args += " --with-timing"
+    # Use pty by default so the spec/nose/Python process buffers "correctly"
+    c.run(runner + args, pty=pty)
