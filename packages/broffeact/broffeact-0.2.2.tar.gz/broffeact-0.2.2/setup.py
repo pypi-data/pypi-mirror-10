f8abee12a0a5dae4e019b10a89d06e69f15b6dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package broffeact/0.2.2/broffeact-0.2.2.tar.gz

---
+++ packages/broffeact/broffeact-0.2.2.tar.gz/broffeact-0.2.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+      name = 'broffeact',
+      packages = ['broffeact'], # this must be the same as the name above
+      version = '0.2.2',
+      description = 'A simple brunch/coffeescript/reactjs documentation generator',
+      author = 'Yves Lange',
+      author_email = 'kursion@gmail.com',
+      url = 'https://github.com/kursion/broffeact', # use the URL to the github repo
+      # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+      keywords = ['coffeescript', 'documentation', 'generator', 'reactjs', 'brunch'], # arbitrary keywords
+      classifiers = [],
+      zip_safe=False,
+      include_package_data=True,
+      entry_points={'console_scripts': [
+          'broffeact = broffeact.__init__:main'
+      ]}
+)
