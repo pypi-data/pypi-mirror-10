b8952292bc50e5170372a605d7e269bde07a96ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package remark/1.7.0/remark-1.7.0.zip

---
+++ packages/remark/remark-1.7.0.zip/remark-1.7.0/Remark/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Description: Remark package initialization
+# Documentation: implementation.txt
+
+# Remark 1.7.0
+# Copyright (c) 2009 - 2015
+# Kalle Rutanen
+# Distributed under the MIT license (see license.txt).
+
