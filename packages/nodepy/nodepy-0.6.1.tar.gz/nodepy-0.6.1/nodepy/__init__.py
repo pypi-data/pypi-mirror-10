64455934f8d7fe50c3da833b0adc6156c778e08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:48 +0000

    Add package nodepy/0.6.1/nodepy-0.6.1.tar.gz

---
+++ packages/nodepy/nodepy-0.6.1.tar.gz/nodepy-0.6.1/nodepy/__init__.py
@@ -0,0 +1,15 @@
+"""
+NodePy (Numerical ODE solvers in Python) is...
+"""
+
+__version__="0.6.1"
+
+import runge_kutta_method as rk
+import twostep_runge_kutta_method as tsrk
+import downwind_runge_kutta_method as dwrk
+import linear_multistep_method as lm
+import rooted_trees as rt
+import ivp
+import convergence as conv
+import low_storage_rk as lsrk
+import graph
