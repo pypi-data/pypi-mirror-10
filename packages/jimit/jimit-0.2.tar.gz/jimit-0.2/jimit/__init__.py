6ea504054ccb29b83b696bd523c3d33234b57f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package jimit/0.2/jimit-0.2.tar.gz

---
+++ packages/jimit/jimit-0.2.tar.gz/jimit-0.2/jimit/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/4/20'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+__version__ = "0.2"
