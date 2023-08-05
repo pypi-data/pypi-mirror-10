af0226d0f3415e31d78ba2d6a5eff82dd886eea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package MarkWiki/1.4.1/MarkWiki-1.4.1.tar.gz

---
+++ packages/MarkWiki/MarkWiki-1.4.1.tar.gz/MarkWiki-1.4.1/markwiki/storage/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2015, Matt Layman
+'''Storage services for persisted models'''
---
+++ packages/MarkWiki/MarkWiki-1.4.1.tar.gz/MarkWiki-1.4.1/markwiki/storage/fs/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2015, Matt Layman
+'''Storage services which use the file system to store data'''
