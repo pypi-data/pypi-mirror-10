63dac6c6ab0edad913259b125d408ab9e860d72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:29 +0000

    Add package solrbackups3/0.1/solrbackups3-0.1.tar.gz

---
+++ packages/solrbackups3/solrbackups3-0.1.tar.gz/solrbackups3-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='solrbackups3',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/yegortokmakov/solrbackups3',
+    license='MIT',
+    author='yegortokmakov',
+    author_email='yegor@tokmakov.biz',
+    dependency_links=['git+git://github.com/yegortokmakov/solrbackup.git#egg=solrbackup'],
+    install_requires=['solrbackup'],
+    description='S3 backup solution for Solr'
+)
