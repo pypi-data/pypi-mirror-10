08819ef774bf1d1e06597a421ec666701b097cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package templado/0.3/templado-0.3.tar.gz

---
+++ packages/templado/templado-0.3.tar.gz/templado-0.3/templado/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url
+from .views import ReportsListView, TemplatesListView, ReportFormView, DownloadReport, EditReportFormView, \
+    TemplateFormView, SearchView, HelpView, UploadStaticView
+
+urlpatterns = patterns('',
+    url(r'^uploadstatic/$', UploadStaticView.as_view(), name='upload-static'),
+    url(r'^create/$', TemplateFormView.as_view(), name='template-form'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
+    url(r'^edit/(?P<template>\d+)/$', TemplateFormView.as_view(), name='edit-template-form'),
+    url(r'^generate/(?P<template>\d+)/$', ReportFormView.as_view(), name='report-form'),
+    url(r'^regenerate/(?P<report>\d+)/$', EditReportFormView.as_view(), name='edit-report-form'),
+    url(r'^reports$', ReportsListView.as_view(), name='report-list'),
+    url(r'^download/(?P<report>\d+)/$', DownloadReport.as_view(), name='download-report'),
+    url(r'^help$', HelpView.as_view(), name='help'),
+    url(r'^$', TemplatesListView.as_view(), name='template-list'),
+)
