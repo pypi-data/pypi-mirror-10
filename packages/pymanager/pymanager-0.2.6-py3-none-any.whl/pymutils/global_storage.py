93f3e5a6e32a58ee6cf4711e6aeb4484d73f5b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package pymanager/0.2.6/pymanager-0.2.6-py3-none-any.whl

---
+++ packages/pymanager/pymanager-0.2.6-py3-none-any.whl/pymutils/global_storage.py
@@ -0,0 +1,12 @@
+class Globals:
+	shutdown = False
+	in_force_quit = False
+	may_terminate = False
+	keep_alive = False
+	terminate_time_allowed = 10
+	status = "booting"
+	default_shell = True
+	verbose = 0
+	outfile = None
+
+	messages = []
