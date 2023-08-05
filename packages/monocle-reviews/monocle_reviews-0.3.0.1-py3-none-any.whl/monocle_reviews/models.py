097555b917db6c878a5df79a11e3ffab78011a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:12 +0000

    Add package monocle-reviews/0.3.0.1/monocle_reviews-0.3.0.1-py3-none-any.whl

---
+++ packages/monocle-reviews/monocle_reviews-0.3.0.1-py3-none-any.whl/monocle_reviews/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+from filebrowser.fields import FileBrowseField
+
+class Review(models.Model):
+    reviewer = models.CharField(max_length=255, verbose_name='Имя клиента')
+    image = models.ImageField(upload_to='reviews/%Y/%m/%d', verbose_name='Фото клиента')
+    text = models.TextField(verbose_name='Текст отзыва')
+    isShown = models.BooleanField(default=True, verbose_name='Показывать отзыв')
+    position = models.PositiveIntegerField(default=0, verbose_name='Порядок')
+
+    def __str__(self):
+        return self.reviewer
+
+    class Meta:
+        verbose_name_plural = u'Отзывы'
+        verbose_name = u'Отзыв'
+        ordering = ['position']
+
+from django.apps import AppConfig
+class CustomAppConfig(AppConfig):
+        name = 'apps.monocle_reviews'
+        verbose_name = 'Отзывы клиентов'
