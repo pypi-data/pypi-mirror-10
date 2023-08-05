df3e1f3f34e53d581b7216b53da08c738d54fa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:10 +0000

    Add package reversefold.util/1.2.1/reversefold.util-1.2.1.tar.gz

---
+++ packages/reversefold.util/reversefold.util-1.2.1.tar.gz/reversefold.util-1.2.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='reversefold.util',
+    version='1.2.1',
+    description='SSH, Multiproc, tail.py, log.py, stream.py',
+    author='Justin Patrin',
+    author_email='papercrane@reversefold.com',
+    url='https://github.com/reversefold/util',
+    packages=find_packages(),
+    scripts=['tail.py', 'log.py', 'stream.py'],
+    license='MIT',
+    install_requires=[
+        'colorama',
+        'docopt',
+        'watchdog',
+    ],
+)
