572d1b0423a7598ee833399a08e54ed41221173f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package satispy/1.0a5/satispy-1.0a5.tar.gz

---
+++ packages/satispy/satispy-1.0a5.tar.gz/satispy-1.0a5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+setup(
+    name='satispy',
+    version='1.0a5',
+    description='An interface to SAT solver tools (like minisat)',
+    author='FÁBIÁN Tamás László',
+    author_email='giganetom@gmail.com',
+    url='https://github.com/netom/satispy/',
+    download_url='https://github.com/netom/satispy/tarball/1.0a5#egg=satispy-1.0a5',
+    license='BSD License',
+    platforms='OS Independent',
+    packages=['satispy', 'satispy.io', 'satispy.solver'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
