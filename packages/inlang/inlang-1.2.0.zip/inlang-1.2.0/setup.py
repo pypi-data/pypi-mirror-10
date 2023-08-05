8e394276e234d56ec33721ff4f0008fca161cd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package inlang/1.2.0/inlang-1.2.0.zip

---
+++ packages/inlang/inlang-1.2.0.zip/inlang-1.2.0/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+try:
+    unicode
+    def u8(s):
+        return s.decode('unicode-escape').encode('utf-8')
+except NameError:
+    def u8(s):
+        return s.encode('utf-8')
+
+setup(name='inlang',
+      version='1.2.0',
+      description=u8('A sample python package'),
+	  long_description="module to extract Indian states' names according to their respective language codes"
+      )
+
