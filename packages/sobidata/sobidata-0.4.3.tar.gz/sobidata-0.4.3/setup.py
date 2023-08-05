32b54ea80c09c1148088307cb2936afbf595d54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:07 +0000

    Add package sobidata/0.4.3/sobidata-0.4.3.tar.gz

---
+++ packages/sobidata/sobidata-0.4.3.tar.gz/sobidata-0.4.3/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+version = '0.4.3'
+
+with open('README.md') as readme:
+    # load long_description into memory
+    long_description = readme.read()
+    # save README (no extension) for pypi
+    with open('README', 'w') as myfile:
+        myfile.write(long_description)
+
+setup(
+    name = 'sobidata',
+    version = version,
+    description = 'Downloads your Social Bicycles route data.',
+    long_description = long_description,
+    author = 'Ryan McGreal',
+    author_email = 'ryan@quandyfactory.com',
+    license = 'LICENCE.txt',
+    url = 'https://github.com/quandyfactory/sobidata',
+    py_modules = ['sobidata'],
+    install_requires = [
+        'dicttoxml',
+        'openpyxl',
+        'requests'
+    ],
+    download_url = 'https://pypi.python.org/packages/source/s/sobidata/sobidata-%s.tar.gz?raw=true' % (version),
+    platforms='Cross-platform',
+    classifiers=[
+      'Programming Language :: Python',
+    ],
+)
+
