96b0921e2bd3740cc4537e0cc43c2dabbffd12c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:53 +0000

    Add package wafer-map/1.0.8/wafer_map-1.0.8.zip

---
+++ packages/wafer-map/wafer_map-1.0.8.zip/wafer_map-1.0.8/wafer_map/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+wafer_map
+
+This is the wafer_map package. It's designed to be a fast, easy-to-use
+plotting package for semiconductor wafer probe data.
+"""
+
+__version__ = "1.0.3"
+#__all__ = ['wm_app', 'wm_constants', 'wm_core', 'wm_frame', 'wm_info',
+#           'wm_legend', 'wm_utils']
