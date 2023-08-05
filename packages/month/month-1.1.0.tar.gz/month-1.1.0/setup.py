1f34e5b1977511ec08ce43af2d3adcfcf54079e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package month/1.1.0/month-1.1.0.tar.gz

---
+++ packages/month/month-1.1.0.tar.gz/month-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name           ='month',
+	version        ='1.1.0',
+	py_modules     =['month'],
+	author         ='ckddlf1774',
+	description    ='A simple printer of month lists add law list tab',
+)
