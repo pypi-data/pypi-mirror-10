9135d80427c2d1aa6e05a1ce2c94edaa7ee1bf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:39 +0000

    Add package list_lol/1.2.0/list_lol-1.2.0.zip

---
+++ packages/list_lol/list_lol-1.2.0.zip/list_lol-1.2.0/list_lol.py
@@ -0,0 +1,13 @@
+'list lo'
+
+def print_lol(the_list, level=0):
+	'this is the print_lol function'
+	for li in the_list:
+		if isinstance(li, list):
+			print_lol(li, level+1)
+		else:
+			for tab_stop in range(level):
+				print "\t",
+			print li
+
+
