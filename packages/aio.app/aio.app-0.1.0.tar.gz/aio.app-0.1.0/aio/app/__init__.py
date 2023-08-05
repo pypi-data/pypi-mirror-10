17f83f237bd006800033cb342f98820e2d090411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:21 +0000

    Add package aio.app/0.1.0/aio.app-0.1.0.tar.gz

---
+++ packages/aio.app/aio.app-0.1.0.tar.gz/aio.app-0.1.0/aio/app/__init__.py
@@ -0,0 +1,20 @@
+signals = None
+config = None
+modules = ()
+servers = {}
+
+
+def clear():
+    import aio.app
+    aio.app.signals = None
+    aio.app.config = None
+    aio.app.modules = ()
+
+    for server in aio.app.servers.values():
+        server.close()
+
+    import logging
+    del logging.root.handlers[:]
+    logging.basicConfig()
+
+    aio.app.servers = {}
