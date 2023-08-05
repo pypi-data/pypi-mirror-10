00c89da59fe132bf3834455de197373282d3448d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:57 +0000

    Add package JSONtoObject/0.0.3/JSONtoObject-0.0.3.tar.gz

---
+++ packages/JSONtoObject/JSONtoObject-0.0.3.tar.gz/JSONtoObject-0.0.3/JSONtoObject/wrapper.py
@@ -0,0 +1,37 @@
+__author__ = 'anass'
+
+
+class Wrapper(object):
+    """ py Object wrapping"""
+    def __init__(self, obj):
+        for key, val in obj.items():
+            self.__setattr__(key, wrap(val))
+
+    def get(self, prop):
+        if prop not in self.__dir__():
+            raise Exception("C\'ant find property" + prop)
+        return self.__dict__[prop]
+
+    def __dir__(self):
+        return self.__dict__.keys()
+
+    def to_json(self):
+        return {key: serialize(value) for key, value in self.__dict__.items()}
+
+
+def wrap(obj):
+    """Returns py object from json"""
+    if isinstance(obj, dict):
+        return Wrapper(obj)
+    elif isinstance(obj, list):
+        return [wrap(o) for o in obj]
+    else:
+        return obj
+
+
+def serialize(value):
+    if isinstance(value, Wrapper):
+        return value.to_json()
+    elif isinstance(value, list):
+        return [o.to_json() for o in value]
+    return value
