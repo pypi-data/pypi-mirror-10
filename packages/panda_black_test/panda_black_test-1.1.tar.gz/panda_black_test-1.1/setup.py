69a8e9289412f279d1a18f34d5372ab8780e6284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package panda_black_test/1.1/panda_black_test-1.1.tar.gz

---
+++ packages/panda_black_test/panda_black_test-1.1.tar.gz/panda_black_test-1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#****************************************************************#
+# ScriptName: setup.py
+# Author: $SHTERM_REAL_USER@alibaba-inc.com
+# Create Date: 2015-04-22 19:29
+# Modify Author: $SHTERM_REAL_USER@alibaba-inc.com
+# Modify Date: 2015-04-30 11:39
+# Function: 
+#***************************************************************#
+from setuptools import setup, find_packages
+setup(
+            name = "panda_black_test",
+            version = "1.1",
+            description='test for panda_black',
+            url='http://www.baidu.com/',
+            author='panda_black',
+            author_email='xxx',
+            platforms=['unix', 'linux'],
+            packages = find_packages(),
+            entry_points = {
+                'console_scripts': [
+                    'foo = demo:test',
+                    'bar = demo:test',
+                ]
+            },
+            classifiers=(
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.3',
+            ),
+            license="Apache License 2.0",
+            install_requires = [
+                'colorama>=0.2.5,<=0.3.3',
+                ]
+)
