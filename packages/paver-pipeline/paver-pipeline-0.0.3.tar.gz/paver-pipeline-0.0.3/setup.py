c8f85ca9bdc6319d130e031167cdb2a654ee0765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:01 +0000

    Add package paver-pipeline/0.0.3/paver-pipeline-0.0.3.tar.gz

---
+++ packages/paver-pipeline/paver-pipeline-0.0.3.tar.gz/paver-pipeline-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='paver-pipeline',
+    packages=['paver_pipeline'],
+    version='0.0.3',
+    author='kyle roux',
+    author_email='kyle@level2designs.com',
+    install_requires=[
+        'PyNg-Annotate',        
+        'jsmin',
+        'ugliPyJs',
+        'PyExecjs',
+    ]
+)
