f145321287f53c74daf28ab49ca2040a6c2709b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package jsonrpcclient/1.1.7/jsonrpcclient-1.1.7.tar.gz

---
+++ packages/jsonrpcclient/jsonrpcclient-1.1.7.tar.gz/jsonrpcclient-1.1.7/jsonrpcclient/exceptions.py
@@ -0,0 +1,54 @@
+"""exceptions.py"""
+
+
+class JsonRpcClientError(Exception):
+    """Base class for the other exceptions"""
+    pass
+
+
+class InvalidRequest(JsonRpcClientError):
+    """The request being sent is not valid JSON."""
+    def __init__(self):
+        super(InvalidRequest, self).__init__(
+            'The request you\'re sending is not valid json')
+
+
+class ConnectionError(JsonRpcClientError): # pylint: disable=redefined-builtin
+    """There was a network issue, invalid HTTP response or timeout."""
+    def __init__(self):
+        super(ConnectionError, self).__init__(
+            'Connection error')
+
+
+class ReceivedNoResponse(JsonRpcClientError):
+    """A response message was expected, but none was given."""
+    def __init__(self):
+        super(ReceivedNoResponse, self).__init__(
+            'No response was received')
+
+
+class UnwantedResponse(JsonRpcClientError):
+    """A response was not requested, but one was given anyway."""
+    def __init__(self):
+        super(UnwantedResponse, self).__init__(
+            'An unwanted response was given')
+
+
+class ParseResponseError(JsonRpcClientError):
+    """The response was not valid json."""
+    def __init__(self):
+        super(ParseResponseError, self).__init__(
+            'The response was not valid json')
+
+
+class InvalidResponse(JsonRpcClientError):
+    """The response was not a valid JSON-RPC response."""
+    def __init__(self):
+        super(InvalidResponse, self).__init__(
+            'The response was not a valid json-rpc 2.0 response')
+
+
+class ReceivedErrorResponse(JsonRpcClientError):
+    """The server gave a valid JSON-RPC *error* response."""
+    def __init__(self, code, message): #pylint:disable=unused-argument
+        super(ReceivedErrorResponse, self).__init__(message)
