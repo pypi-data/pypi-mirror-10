92e1d478866b2b75d597693d2151e5826a83f020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:39 +0000

    Add package noseapp_selenium/1.0.5/noseapp_selenium-1.0.5.tar.gz

---
+++ packages/noseapp_selenium/noseapp_selenium-1.0.5.tar.gz/noseapp_selenium-1.0.5/noseapp_selenium/forms/fields.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+
+from selenium.common.exceptions import NoSuchElementException
+
+from noseapp_selenium.tools import polling
+from noseapp_selenium.tools import make_object
+from noseapp_selenium.query import QueryObject
+from noseapp_selenium.query import QueryProcessor
+
+
+def selector(**kwargs):
+    """
+    proxy for tag attributes
+    """
+    return kwargs
+
+
+def fill_field_handler(f):
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        result = f(self, *args, **kwargs)
+
+        if self._settings.get('remenber', True):
+            self._observer.fill_field_handler(self)
+
+        return result
+    return wrapper
+
+
+def clear_field_handler(f):
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        result = f(self, *args, **kwargs)
+
+        self._observer.clear_field_handler(self)
+
+        return result
+    return wrapper
+
+
+class FieldError(BaseException):
+    pass
+
+
+class SimpleFieldInterface(object):
+
+    @property
+    def weight(self):
+        raise NotImplementedError
+
+    def fill(self, value=None):
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
+
+
+class FormField(object):
+    """
+    Base class for all fields
+    """
+
+    class Meta:
+        tag = None
+
+    def __init__(self,
+                 name,
+                 value=None,
+                 required=False,
+                 selector=None,
+                 error_mess=None,
+                 invalid_value=None,
+                 weight=None):
+
+        self.name = name
+
+        if not isinstance(selector, dict):
+            raise ValueError('incorrect selector')
+
+        self.__is_bind = False
+
+        self._query = None
+        self._settings = {}
+        self._observer = None
+        self._weight = weight
+        self._selector = selector
+
+        self.value = value
+        self.required = required
+        self.error_mess = error_mess
+        self.invalid_value = invalid_value
+
+    def bind(self, group):
+        try:
+            self._query = group._driver.query
+        except AttributeError:
+            self._query = QueryProcessor(group._driver)
+
+        self._observer = group._observer
+        self._settings = group._settings
+
+        if callable(self.value):
+            self.value = self.value()
+
+        if callable(self.invalid_value):
+            self.invalid_value = self.invalid_value()
+
+        self.__is_bind = True
+
+    @property
+    def weight(self):
+        return self._weight
+
+    def get_web_element(self):
+        if not self.__is_bind:
+            raise FieldError('Field is not binding to group')
+
+        wrapper = self._settings.get('wrapper', None)
+
+        if isinstance(wrapper, QueryObject):
+            query = self._query(
+                self._query.from_object(wrapper).first(),
+            )
+        else:
+            query = self._query
+
+        return query.from_object(
+            QueryObject(self.Meta.tag, **self._selector),
+        ).first()
+
+    @property
+    def obj(self):
+        return make_object(self.get_web_element())
+
+
+def field_on_base(*interfaces):
+    """
+    Create parent class on base interfaces
+    """
+    if not hasattr(field_on_base, '__classes__'):
+        field_on_base.__classes__ = {}
+
+    bases = tuple([FormField] + sorted(list(interfaces)))
+
+    try:
+        return field_on_base.__classes__[bases]
+    except KeyError:
+        field_on_base.__classes__[bases] = type('BaseField', bases, {})
+        return field_on_base.__classes__[bases]
+
+
+class Input(field_on_base(SimpleFieldInterface)):
+
+    class Meta:
+        tag = 'input'
+
+    @fill_field_handler
+    def fill(self, value=None):
+        if value is None:
+            value = self.value
+
+        self.get_web_element().send_keys(*value)
+
+    @clear_field_handler
+    def clear(self):
+        self.get_web_element().clear()
+
+
+class TextArea(Input):
+
+    class Meta:
+        tag = 'textarea'
+
+
+class Checkbox(field_on_base(SimpleFieldInterface)):
+
+    class Meta:
+        tag = 'input'
+
+    @fill_field_handler
+    def fill(self, value=None):
+        if value is None:
+            value = self.value
+
+        el = self.get_web_element()
+        current_value = el.is_selected()
+
+        if self._settings.get('allow_raises', True):
+
+            if current_value and value:
+                raise FieldError('Oops, checkbox did selected')
+            elif not current_value and not value:
+                raise FieldError('Oops, checkbox did unselected')
+
+        if (value and not current_value) or (not value and current_value):
+            el.click()
+
+    @clear_field_handler
+    def clear(self):
+        el = self.get_web_element()
+
+        if el.is_selected():
+            el.click()
+
+
+class RadioButton(Checkbox):
+
+    class Meta:
+        tag = 'input'
+
+    @fill_field_handler
+    def fill(self, value=None):
+        if value is None:
+            value = self.value
+
+        el = self.get_web_element()
+        current_value = el.is_selected()
+        changed = False
+
+        if (value and not current_value) or (not value and current_value):
+            el.click()
+            changed = True
+
+        return changed
+
+    @clear_field_handler
+    def clear(self):
+        pass
+
+
+class Select(field_on_base(SimpleFieldInterface)):
+
+    class Meta:
+        tag = 'select'
+
+    @fill_field_handler
+    def fill(self, value=None):
+        if value is None:
+            value = self.value
+
+        select = self.get_web_element()
+
+        options = filter(
+            lambda opt: opt.get_attribute('value') == value,
+            select.find_elements_by_tag_name('option'),
+        )
+
+        if options:
+            options[0].click()
+        else:
+            raise NoSuchElementException(
+                u'Cant do selected value for field "{}", selector "{}", option "{}"'.format(
+                    self.name, str(self._selector), value
+                ),
+            )
+
+    @clear_field_handler
+    def clear(self):
+        pass
---
+++ packages/noseapp_selenium/noseapp_selenium-1.0.5.tar.gz/noseapp_selenium-1.0.5/noseapp_selenium/query.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from noseapp.utils.common import waiting_for
+from noseapp.utils.common import TimeoutException
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.remote.webelement import WebElement
+
+from noseapp_selenium.tools import WebElementDecorator
+from noseapp_selenium.tools import get_driver_from_web_element
+
+
+logger = logging.getLogger(__name__)
+
+
+REPLACE_ATTRIBUTES = {
+    '_id': 'id',
+    '_class': 'class',
+    '_type': 'type',
+}
+
+REPLACE_TAGS = {
+    'link': 'a',
+}
+
+DEFAULT_SLEEP = 0.01
+DEFAULT_WAIT_TIMEOUT = 30
+
+
+class QueryError(BaseException):
+    pass
+
+
+def _error_handler(e, client, css):
+    """
+    Extend error message
+    """
+    prefix = u' ' if e.message else u''
+
+    if isinstance(client, WebElement):
+        e.message += u'{}QueryProcessor(From: {}, CSS: {})\n\n--\nSEARCH AREA: {}\n--\n'.format(
+            prefix,
+            repr(client),
+            css,
+            client.get_attribute('innerHTML'),
+        )
+    else:
+        e.message += u'{}QueryProcessor(From: {}, CSS: {})'.format(
+            prefix,
+            repr(client),
+            css,
+        )
+
+
+def _execute(client, css, get_all):
+    """
+    Execute css query
+    """
+    logger.debug(u'CSS: {} Get all: {}'.format(css, 'Yes' if get_all else 'No'))
+
+    css_executor = {
+        True: client.find_elements_by_css_selector,
+        False: client.find_element_by_css_selector,
+    }
+
+    try:
+        result = css_executor[get_all](css)
+
+        if isinstance(result, WebElement):
+            return WebElementDecorator(result)
+        elif isinstance(result, list):
+            return [WebElementDecorator(w) for w in result]
+
+        return result
+
+    except WebDriverException as e:
+        _error_handler(e, client, css)
+        raise
+    except KeyError:
+        raise QueryError('get_all param must be bool type only')
+
+
+def _apply_polling(result):
+    """
+    Apply polling by driver config
+    """
+    driver = get_driver_from_web_element(result._client)
+
+    if driver.config.POLLING_TIMEOUT:
+        result.wait(driver.config.POLLING_TIMEOUT)
+
+
+def _replace_attribute(atr_name):
+    """
+    Replacing attribute name for
+    excluding conflict with names of globals
+    """
+    return REPLACE_ATTRIBUTES.get(atr_name, atr_name).replace('_', '-')
+
+
+def _replace_tag(tag_name):
+    """
+    Replacing tag name for usability
+    """
+    return REPLACE_TAGS.get(tag_name, tag_name)
+
+
+def _handler(client, tag):
+    """
+    :type client: selenium.webdriver.remote.webdriver.WebDriver
+    :param tag: html tag name
+    """
+    def handle(**selector):
+        query = [_replace_tag(tag)]
+
+        def get_format(value):
+            if isinstance(value, _Contains):
+                return u'[{}*="{}"]'
+            return u'[{}="{}"]'
+
+        query.extend(
+            (
+                get_format(val).format(_replace_attribute(atr), val)
+                for atr, val in selector.items()
+            ),
+        )
+
+        return QueryResult(client, ''.join(query))
+
+    return handle
+
+
+class QueryObject(object):
+    """
+    Structure of css query.
+
+    Use instance of this class with
+    from_object method of QueryProcessor.
+    """
+
+    def __init__(self, tag, **selector):
+        self.tag = tag
+        self.selector = selector
+
+    def __unicode__(self):
+        return u'<{} {}>'.format(
+            self.tag,
+            u' '.join(
+                [u'{}="{}"'.format(k, v) for k, v in self.selector.items()]
+            ),
+        )
+
+
+class _Contains(object):
+    """
+    Marker for use contains inside css query
+    """
+
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return self.value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __unicode__(self):
+        return unicode(self.value)
+
+
+contains = _Contains
+
+
+class QueryResult(object):
+    """
+    Execute actions by css query and returning result
+    """
+
+    def __init__(self, client, css):
+        self._client = client
+        self._css = css
+
+    def __getattr__(self, item):
+        return getattr(QueryProcessor(self.first()), item)
+
+    @property
+    def exist(self):
+        """
+        Check element exist
+        """
+        if isinstance(self._client, (WebElement, WebElementDecorator)):
+            driver = get_driver_from_web_element(self._client)
+        else:
+            driver = self._client
+
+        driver.implicitly_wait(0)
+
+        try:
+            el = _execute(self._client, self._css, False)
+            if el:
+                driver.config.apply_implicitly_wait()
+                return True
+            driver.config.apply_implicitly_wait()
+            return False
+        except WebDriverException:
+            driver.config.apply_implicitly_wait()
+            return False
+        except BaseException:
+            driver.config.apply_implicitly_wait()
+            raise
+
+    @property
+    def with_wait(self):
+        self.wait()
+        return self
+
+    def wait(self, timeout=None, sleep=None):
+        """
+        Waiting for web element exist
+        """
+        try:
+            return waiting_for(
+                lambda: self.exist,
+                sleep=sleep or DEFAULT_SLEEP,
+                timeout=timeout or DEFAULT_WAIT_TIMEOUT,
+            )
+        except TimeoutException:
+            raise TimeoutException(
+                'Could not wait web element with css "{}"'.format(self._css),
+            )
+
+    def get(self, index):
+        """
+        Get web element by index
+        """
+        _apply_polling(self)
+
+        try:
+            return _execute(self._client, self._css, True)[index]
+        except IndexError as e:
+            e.message = 'Result does not have element with index "{}". Query: "{}".'.format(
+                index, self._css,
+            )
+            raise
+
+    def first(self):
+        """
+        Get first element on page
+        """
+        _apply_polling(self)
+
+        return _execute(self._client, self._css, False)
+
+    def all(self):
+        """
+        Get all elements of appropriate query
+        """
+        _apply_polling(self)
+
+        return _execute(self._client, self._css, True)
+
+
+class QueryProcessor(object):
+    """
+    Example:
+
+        driver = WebDriver()
+        query = QueryProcessor(driver)
+
+        search_wrapper = query.div(id='search').first()
+        search_field = query(search_wrapper).input(_class='search').first()
+        search_field.send_keys(*'Hello World!')
+    """
+
+    def __init__(self, client):
+        self._client = client
+
+    def __getattr__(self, item):
+        return _handler(self._client, item)
+
+    def __call__(self, client):
+        return self.__class__(client)
+
+    def from_object(self, obj):
+        """
+        Creating result from object
+
+        :type obj: QueryObject
+        """
+        if not isinstance(obj, QueryObject):
+            raise TypeError('"{}" is not QueryObject instance'.format(type(obj)))
+
+        return self.__getattr__(obj.tag)(**obj.selector)
+
+    def get_text(self):
+        """
+        Get text from page or web element
+        """
+        if isinstance(self._client, WebElement):
+            return self._client.text
+
+        return self._client.find_element_by_tag_name('body').text
