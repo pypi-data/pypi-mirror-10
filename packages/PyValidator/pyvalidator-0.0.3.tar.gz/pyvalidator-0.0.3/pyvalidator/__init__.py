f035c5326d8c837888b48462cbddeb1bd5be2cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package PyValidator/0.0.3/pyvalidator-0.0.3.tar.gz

---
+++ packages/PyValidator/pyvalidator-0.0.3.tar.gz/pyvalidator-0.0.3/pyvalidator/__init__.py
@@ -0,0 +1,9 @@
+from validator import Validator
+from validator import And, Or
+from validator import Optional, CustomError, CustomMissingkeyError
+
+from errors import NotEqual, WrongType
+from errors import SurplusKey, MissingKey
+from errors import FuncFail, FuncException
+from utils import OrderedList
+from errorbucket import ErrorBucket
