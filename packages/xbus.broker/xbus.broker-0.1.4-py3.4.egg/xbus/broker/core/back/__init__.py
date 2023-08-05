6bb472077d94b71d562963eb4ffc4fafc77bf6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:46 +0000

    Add package xbus.broker/0.1.4/xbus.broker-0.1.4-py3.4.egg

---
+++ packages/xbus.broker/xbus.broker-0.1.4-py3.4.egg/xbus/broker/core/back/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'jgavrel'
+
+from xbus.broker.core.back.event import Event
+
+from xbus.broker.core.back.rpc import XbusBrokerBack
+from xbus.broker.core.back.rpc import get_backserver
