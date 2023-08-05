36c5b5c9143f637b38a6fd5f194a25a22d98ffba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package kimono/0.2.1/kimono-0.2.1.tar.gz

---
+++ packages/kimono/kimono-0.2.1.tar.gz/kimono-0.2.1/README.rst
@@ -0,0 +1,23 @@
+kimono
+======
+
+Kimonolabs API Wrapper
+
+USAGE
+     
+
+.. code:: python
+
+    from kimono import Kimono
+    api = Kimono('APIKEY', 'APIID')
+
+IMPLEMENTED
+           
+
+`Retrieve an API <https://www.kimonolabs.com/apidocs#RetrieveApi>`__
+
+\`\ ``python api.retrieve_api()``
+
+`List all APIs <https://www.kimonolabs.com/apidocs#ListApis>`__
+
+\`\ ``python api.list_apis()``
---
+++ packages/kimono/kimono-0.2.1.tar.gz/kimono-0.2.1/kimono/kimono.py
@@ -0,0 +1,41 @@
+import requests
+
+__all__ = ['Kimono']
+
+BASE = 'https://www.kimonolabs.com'
+
+RETRIEVE_API = '{BASE_URL}/kimonoapis/{API_ID}?apikey={API_KEY}'
+LIST_APIS = '{BASE_URL}/kimonoapis?apikey={API_KEY}'
+
+
+class KimonoError(Exception):
+    '''Base class for kimono errors'''
+    def __init__(self, response):
+        self.response = response
+
+    def __str__(self):
+        return self.response.get('message', 'No error provided.')
+
+
+class Kimono(object):
+    def __init__(self, apikey=None, apiid=None):
+        self.apikey = apikey
+        self.apiid = apiid
+
+    def _get_request(self, url):
+        response = requests.get(url)
+        if response.status_code == requests.codes.ok:
+            return response.json()
+        else:
+            raise KimonoError(response.json())
+
+    def retrieve_api(self):
+        '''Returns an API object matching a specific API id'''
+        return self._get_request(RETRIEVE_API.format(BASE_URL=BASE,
+            API_ID=self.apiid,
+            API_KEY=self.apikey))
+
+    def list_apis(self):
+        '''Returns a list of all APIs for the specified user'''
+        return self._get_request(LIST_APIS.format(BASE_URL=BASE,
+            API_KEY=self.apikey))
