000d0762e00ed0f6bce9df4d200538c8ef18490d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package myria-python/1.2.3/myria-python-1.2.3.tar.gz

---
+++ packages/myria-python/myria-python-1.2.3.tar.gz/myria-python-1.2.3/myria/__init__.py
@@ -0,0 +1,14 @@
+from .connection import *
+from .errors import *
+from .relation import *
+from .query import *
+from .schema import *
+import cmd
+
+try:
+    # IPython is not required, so swallow exception if not installed
+    from .extension import *
+except ImportError:
+    pass
+
+version = "1.2-dev"
