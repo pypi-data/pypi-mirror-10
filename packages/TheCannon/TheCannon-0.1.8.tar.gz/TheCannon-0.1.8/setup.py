6d9327c0eb5a7cc455d39add46aa08a583f79dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:42 +0000

    Add package TheCannon/0.1.8/TheCannon-0.1.8.tar.gz

---
+++ packages/TheCannon/TheCannon-0.1.8.tar.gz/TheCannon-0.1.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='TheCannon',
+        version='0.1.8',
+        description='Data-driven stellar parameters and abundances from spectra',
+        url='http://github.com/annayqho/TheCannon',
+        author='Anna Y. Q. Ho',
+        author_email='annayqho@gmail.com',
+        license='MIT',
+        packages=['TheCannon'],
+        package_data={'TheCannon': 
+            ['TheCannon/helpers/*', 
+             'TheCannon/example_DR10/*']},
+        zip_safe=False)
