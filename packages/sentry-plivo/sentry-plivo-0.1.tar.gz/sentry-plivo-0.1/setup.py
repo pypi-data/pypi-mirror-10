699eead73bfb8b36a1f16dcde24dae88fc2e7883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:30 +0000

    Add package sentry-plivo/0.1/sentry-plivo-0.1.tar.gz

---
+++ packages/sentry-plivo/sentry-plivo-0.1.tar.gz/sentry-plivo-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='sentry-plivo',
+    version='0.1',
+    author='Ashish Dubey',
+    author_email='ashish.dubey91@gmail.com',
+    url='https://github.com/dash1291/sentry-plivo',
+    description='A Sentry plugin for sending SMS notifications via Plivo.',
+    long_description=open('README.rst', 'r').read(),
+    license='MIT',
+    zip_safe=False,
+    install_requires=[
+        'sentry',
+        'plivo'
+    ],
+    include_package_data=True,
+    entry_points={
+        'sentry.apps': [
+            'plivo = sentry_plivo',
+        ],
+        'sentry.plugins': [
+            'plivo = sentry_plivo.plugin:PlivoPlugin',
+        ]
+    }
+)
