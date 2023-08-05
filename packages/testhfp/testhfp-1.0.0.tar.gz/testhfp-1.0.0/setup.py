3b7bfa7b27eb6194e8d2df0422c7ca8b6ba30f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package testhfp/1.0.0/testhfp-1.0.0.tar.gz

---
+++ packages/testhfp/testhfp-1.0.0.tar.gz/testhfp-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'testhfp',
+version = '1.0.0',
+py_modules = ['testhfp'],
+author = 'BrianZz',
+author_email ='1242242@qq.com',
+url ='http://www.headsa.com',
+description ='a test project to upload a module to PyPI',
+)
---
+++ packages/testhfp/testhfp-1.0.0.tar.gz/testhfp-1.0.0/testhfp.py
@@ -0,0 +1,3 @@
+import sys
+def testhfp:
+print('testhfp')
