ef52ba4bb11191f39a81222c6cf5c43e33e479b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package python_hangman/1.4.0/python_hangman-1.4.0-py2.py3-none-any.whl

---
+++ packages/python_hangman/python_hangman-1.4.0-py2.py3-none-any.whl/hangman/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+==============
+Python Hangman
+==============
+
+A well tested, cli, python version-agnostic, multi-platform hangman game.
+It's built following TDD principles and each component services a
+sensibly distinct logical purpose.
+
+"""
+
+__author__ = 'Manu Phatak'
+__email__ = 'bionikspoon@gmail.com'
+__version__ = '1.4.0'
+
+
+from .dictionary import Dictionary
+from .hangman import Hangman, GameOver, GameWon
+from .presenter import Presenter
+from .commander import Commander
