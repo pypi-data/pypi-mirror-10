54004656174aa099e4ea0e463af9c65ce2639daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:11 +0000

    Add package django_weibo/0.1.0/django_weibo-0.1.0.tar.gz

---
+++ packages/django_weibo/django_weibo-0.1.0.tar.gz/django_weibo-0.1.0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django_weibo',
+    version='0.1.0',
+    packages=['django_weibo'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A django wrapper for Wechat API.',
+    long_description=README,
+    url='https://github.com/tolerious/django-weibo',
+    author='tolerious',
+    author_email='tolerious@qq.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
