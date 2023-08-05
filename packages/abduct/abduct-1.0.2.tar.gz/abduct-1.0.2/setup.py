345a9e9633e031809b176d1e7b053f8808137d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:33 +0000

    Add package abduct/1.0.2/abduct-1.0.2.tar.gz

---
+++ packages/abduct/abduct-1.0.2.tar.gz/abduct-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='abduct',
+    version='1.0.2',
+    packages=('abduct',),
+    url='https://github.com/themattrix/python-abduct',
+    license='MIT',
+    author='Matthew Tardiff',
+    author_email='mattrix@gmail.com',
+    install_requires=(),
+    tests_require=(),
+    description=(
+        'Capture stdout/stderr and optionally release when an exception '
+        'occurs.'),
+    classifiers=(
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'))
