da8d2bb2dfb993f05e8128d1b5327e3f4c24a01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:28 +0000

    Add package print_list_kangyu/1.2/print_list_kangyu-1.2.zip

---
+++ packages/print_list_kangyu/print_list_kangyu-1.2.zip/print_list_kangyu-1.2/print_list_kangyu.py
@@ -0,0 +1,10 @@
+#此模块打印传递进来的列表，其中如果含有嵌套列表，则递归打印
+def print_list(the_list, indent = False, level = 0):
+	for each in the_list:
+		if isinstance(each, list):
+			print_list(each, indent, level + 1)
+		else:
+			if indent:
+				for tab in range(level):
+					print('\t', end = '')
+			print(each)
