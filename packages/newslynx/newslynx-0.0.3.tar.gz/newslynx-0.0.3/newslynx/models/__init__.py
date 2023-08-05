0fd52ad735be9a2d80d11d08f1427589e7a72465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package newslynx/0.0.3/newslynx-0.0.3.tar.gz

---
+++ packages/newslynx/newslynx-0.0.3.tar.gz/newslynx-0.0.3/newslynx/models/__init__.py
@@ -0,0 +1,11 @@
+from .auth import Auth
+from .creator import Creator
+from .event import Event
+from .metric import Metric
+from .org import Org
+from .recipe import Recipe
+from .setting import Setting
+from .tag import Tag
+from .thing import Thing
+from .user import User
+from .sous_chef import SousChef, validate_sous_chef
---
+++ packages/newslynx/newslynx-0.0.3.tar.gz/newslynx-0.0.3/newslynx/models/tag.py
@@ -0,0 +1,74 @@
+from sqlalchemy.dialects.postgresql import ENUM
+from slugify import slugify
+
+from newslynx.core import db
+from newslynx.lib import dates
+from newslynx.constants import (
+    IMPACT_TAG_CATEGORIES, IMPACT_TAG_LEVELS, TAG_TYPES)
+
+
+class Tag(db.Model):
+
+    """
+    A tag is an arbitrary label (with arbitrary attributes)
+    which we can assign to a thing or an event.
+    """
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    org_id = db.Column(
+        db.Integer, db.ForeignKey('orgs.id'), index=True)
+    name = db.Column(db.Text)
+    slug = db.Column(db.Text, index=True)
+    created = db.Column(db.DateTime(timezone=True), index=True)
+    updated = db.Column(db.DateTime(timezone=True), index=True)
+    color = db.Column(db.Text)
+    type = db.Column(ENUM(*TAG_TYPES, name='tag_type_enum'), index=True)
+    category = db.Column(ENUM(*IMPACT_TAG_CATEGORIES, name='tag_categories_enum'))
+    level = db.Column(ENUM(*IMPACT_TAG_LEVELS, name='tag_levels_enum'))
+
+    __table_args__ = (
+        db.UniqueConstraint('org_id', 'name'),
+    )
+
+    def __init__(self, **kw):
+        self.org_id = kw.get('org_id')
+        self.name = kw.get('name')
+        self.slug = slugify(kw.get('slug', kw['name']))
+        self.created = kw.get('created', dates.now())
+        self.updated = kw.get('updated', dates.now())
+        self.type = kw.get('type')
+        self.color = kw.get('color')
+        self.category = kw.get('category')
+        self.level = kw.get('level')
+
+    def to_dict(self):
+        if self.type == 'impact':
+            return {
+                'id': self.id,
+                'org_id': self.org_id,
+                'name': self.name,
+                'slug': self.slug,
+                'type': self.type,
+                'color': self.color.lower(),
+                'category': self.category,
+                'level': self.level,
+                'created': self.created,
+                'updated': self.updated
+            }
+        else:
+            return {
+                'id': self.id,
+                'org_id': self.org_id,
+                'name': self.name,
+                'slug': self.slug,
+                'type': self.type,
+                'color': self.color.lower(),
+                'created': self.created,
+                'updated': self.updated
+            }
+
+    def __repr__(self):
+
+        return '<Tag %r / %r >' % (self.name, self.type)
