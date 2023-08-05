125736c06422e82cbc57a3f5121dcae993f6012b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:41 +0000

    Add package pyqt2waybinding/0.3/pyqt2waybinding-0.3.zip

---
+++ packages/pyqt2waybinding/pyqt2waybinding-0.3.zip/pyqt2waybinding-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pyqt2waybinding',
+      version='0.3',
+      description='Simple framework for easy 2-way binding with PyQt',
+      long_description=readme(),
+      url='http://github.com/jkokorian/pyqt2waybinding',
+      author='J. Kokorian',
+      author_email='J.Kokorian@TUDelft.nl',
+      license='MIT',
+      packages=[
+          'pyqt2waybinding'
+          ],
+      install_requires=[],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
