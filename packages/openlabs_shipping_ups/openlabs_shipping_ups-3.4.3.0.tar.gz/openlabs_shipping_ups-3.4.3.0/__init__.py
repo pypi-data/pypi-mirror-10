d197f431e3889c1f7124f20646fb0b03a920da78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:13 +0000

    Add package openlabs_shipping_ups/3.4.3.0/openlabs_shipping_ups-3.4.3.0.tar.gz

---
+++ packages/openlabs_shipping_ups/openlabs_shipping_ups-3.4.3.0.tar.gz/openlabs_shipping_ups-3.4.3.0/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+
+    :copyright: (c) 2014 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from trytond.pool import Pool
+from party import Address
+from carrier import Carrier, UPSService, CarrierConfig
+from sale import Configuration, Sale
+from stock import (
+    ShipmentOut, StockMove, ShippingUps, GenerateShippingLabel, Package
+)
+
+
+def register():
+    Pool.register(
+        Address,
+        Carrier,
+        CarrierConfig,
+        UPSService,
+        Configuration,
+        Sale,
+        StockMove,
+        ShipmentOut,
+        ShippingUps,
+        Package,
+        module='shipping_ups', type_='model'
+    )
+
+    Pool.register(
+        GenerateShippingLabel,
+        module='shipping_ups', type_='wizard'
+    )
