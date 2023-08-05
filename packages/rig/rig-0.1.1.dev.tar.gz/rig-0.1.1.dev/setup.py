b92f26bd72b3e92a9e56908ddcdf6225e9c0c300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package rig/0.1.1.dev/rig-0.1.1.dev.tar.gz

---
+++ packages/rig/rig-0.1.1.dev.tar.gz/rig-0.1.1.dev/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="rig",
+    version="0.1.1.dev",
+    packages=find_packages(),
+
+    # Metadata for PyPi
+    author="Jonathan Heathcote, Andrew Mundy",
+    description="A set of libraries for mapping problems to SpiNNaker",
+    license="GPLv2",
+
+    # Requirements
+    install_requires=["numpy>1.6", "six", "enum34", "sentinel"],
+    tests_require=["pytest>=2.6", "pytest-cov", "mock", "toposort"],
+
+    # Scripts
+    entry_points={
+        "console_scripts": [
+            "rig-boot = rig.scripts.rig_boot:main",
+            "rig-power = rig.scripts.rig_power:main",
+            "rig-info = rig.scripts.rig_info:main",
+        ],
+    }
+)
