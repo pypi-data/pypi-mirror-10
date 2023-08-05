b37445c871972ba4637a7cb4485956997f31e338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package not/1.1/not-1.1.tar.gz

---
+++ packages/not/not-1.1.tar.gz/not-1.1/notpy/config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+'''
+this constantsy file is for configuring stuff
+more than likely you won't edit it
+'''
+
+import os
+
+
+try:
+    token = open(os.path.join(os.environ['HOME'], '.not_token')).read()
+except Exception as e:
+    return 'No token! run not-setup! {0}'.format(e)
+
+
+SUFFIX = '.md'
+EDITOR = os.environ.get('EDITOR', 'vim')
+TOKEN = token
+CONSUMER_KEY = 'comradeloren'
+CONSUMER_SECRET = 'c219754d19198564'
+DEFAULT_LOGLEVEL = 'WARN'
