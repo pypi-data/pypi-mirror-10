6ffba0a87cb42b28ab11748b920e821c8cd7252e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package easysetup/0.0.12/easysetup-0.0.12.zip

---
+++ packages/easysetup/easysetup-0.0.12.zip/easysetup-0.0.12/easysetup/template/APPLICATION_NAME/APPLICATION_NAME.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Docstring."""
+
+# Python 3 compatibility
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# import builtins  # Python 3 compatibility
+# import future  # Python 3 compatibility
+# import io  # Python 3 compatibility
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod(verbose=True)
+    sys.exit(main())
