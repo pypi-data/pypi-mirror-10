3e7209fdf682b9299eebaeca8bde5068e7304fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package wikidata_suggest/0.0.3/wikidata_suggest-0.0.3.tar.gz

---
+++ packages/wikidata_suggest/wikidata_suggest-0.0.3.tar.gz/wikidata_suggest-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Command
+
+setup(
+    name = 'wikidata_suggest',
+    version = '0.0.3',
+    url = 'http://github.com/edsu/wikidata_suggest',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['wikidata_suggest'],
+    install_requires = ['requests', 'colorama'],
+    scripts = ['scripts/wd'],
+    description = 'Interactively look up Wikidata entities from the command line',
+    license='MIT License'
+)
