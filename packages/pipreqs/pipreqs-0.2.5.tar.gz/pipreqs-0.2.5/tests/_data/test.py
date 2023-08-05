b3c4c2941db18fe30bedad5ff9f2d8fbd5dbe19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package pipreqs/0.2.5/pipreqs-0.2.5.tar.gz

---
+++ packages/pipreqs/pipreqs-0.2.5.tar.gz/pipreqs-0.2.5/tests/_data/test.py
@@ -0,0 +1,39 @@
+"""unused import"""
+# pylint: disable=undefined-all-variable, import-error, no-absolute-import, too-few-public-methods, missing-docstring
+import xml.etree  # [unused-import]
+import xml.sax  # [unused-import]
+import os.path as test  # [unused-import]
+from sys import argv as test2  # [unused-import]
+from sys import flags  # [unused-import]
+# +1:[unused-import,unused-import]
+from collections import deque, OrderedDict, Counter
+import requests # [unused-import]
+# All imports above should be ignored 
+
+import atexit
+from __future__ import print_function
+from docopt import docopt
+import curses, logging, sqlite3
+import logging
+import os
+import sqlite3
+import time
+import sys
+import signal
+import bs4
+import nonexistendmodule
+import boto as b, import peewee as p,
+# import django
+import flask.ext.somext # # # 
+from sqlalchemy import model
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+import models
+
+def main():
+	pass
+
+import after_method_should_be_ignored
