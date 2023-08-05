0f664d2932223a569a6019a42a13ebfc0520f73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:38 +0000

    Add package ajenti.plugin.plugins/0.27/ajenti.plugin.plugins-0.27.tar.gz

---
+++ packages/ajenti.plugin.plugins/ajenti.plugin.plugins-0.27.tar.gz/ajenti.plugin.plugins-0.27/ajenti_plugin_plugins/tasks.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import shutil
+
+from aj.plugins.core.api.tasks import Task
+
+
+class InstallPlugin (Task):
+    name = 'Installing plugin'
+
+    def __init__(self, context, name=None, version=None):
+        Task.__init__(self, context)
+        self.spec = 'ajenti.plugin.%s==%s' % (name, version)
+
+    def run(self):
+        if os.path.exists('/root/.cache/pip'):
+            shutil.rmtree('/root/.cache/pip')
+        subprocess.check_output(['pip', 'install', self.spec])
