96857f872d28fc0bcfe362b11c19ac684d249732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:58 +0000

    Add package jenkins-jobs-slack/0.3.0/jenkins-jobs-slack-0.3.0.tar.gz

---
+++ packages/jenkins-jobs-slack/jenkins-jobs-slack-0.3.0.tar.gz/jenkins-jobs-slack-0.3.0/jenkins_jobs_slack/slack.py
@@ -0,0 +1,63 @@
+import xml.etree.ElementTree as XML
+
+
+def slack_properties(parser, xml_parent, data):
+    """yaml: slack
+
+    Example::
+
+      properties:
+        - slack:
+            notify-start: true
+            notify-success: true
+            notify-aborted: true
+            notify-notbuilt: true
+            notify-unstable: true
+            notify-failure: true
+            notify-backtonormal: true
+    """
+    if data is None:
+        data = dict()
+
+    notifier = XML.SubElement(
+        xml_parent, 'jenkins.plugins.slack.SlackNotifier_-SlackJobProperty')
+    notifier.set('plugin', 'slack@1.7')
+
+    for opt, attr in (('notify-start', 'startNotification'),
+                      ('notify-success', 'notifySuccess'),
+                      ('notify-aborted', 'notifyAborted'),
+                      ('notify-notbuilt', 'notifyNotBuilt'),
+                      ('notify-unstable', 'notifyUnstable'),
+                      ('notify-failure', 'notifyFailure'),
+                      ('notify-backtonormal', 'notifyBackToNormal'),
+                      ('notify-repeatedfailure', 'notifyRepeatedFailure'),
+                      ('include-test-summary', 'includeTestSummary'),
+                      ('show-commit-list', 'showCommitList')):
+        (XML.SubElement(notifier, attr)
+         .text) = 'true' if data.get(opt, True) else 'false'
+
+
+def slack_publisher(parser, xml_parent, data):
+    """yaml: slack
+
+    Example::
+
+      publishers:
+        - slack:
+            team-domain: example.com
+            auth-token: secret
+            build-server-url: https://jenkins.example.com
+            room: '#jenkins'
+    """
+    if data is None:
+        data = dict()
+
+    notifier = XML.SubElement(
+        xml_parent, 'jenkins.plugins.slack.SlackNotifier')
+    notifier.set('plugin', 'slack@1.7')
+
+    for (opt, attr) in (('team-domain', 'teamDomain'),
+                        ('auth-token', 'authToken'),
+                        ('build-server-url', 'buildServerUrl'),
+                        ('room', 'room')):
+        XML.SubElement(notifier, attr).text = data.get(opt, '')
