760b271d3850ef99d710b62be017e95a2b3106ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:57 +0000

    Add package cleo/0.3.0/cleo-0.3.0.tar.gz

---
+++ packages/cleo/cleo-0.3.0.tar.gz/cleo-0.3.0/cleo/_compat.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    long = long
+    unicode = unicode
+    basestring = basestring
+else:
+    long = int
+    unicode = str
+    basestring = str
+
+
+def decode_str(string, encodings=None):
+    if not PY2:
+        return string
+
+    if encodings is None:
+        encodings = ['utf-8', 'latin1', 'ascii']
+
+    for encoding in encodings:
+        try:
+            return string.decode(encoding)
+        except UnicodeDecodeError:
+            pass
+
+    return string.decode(encodings[0], errors='ignore')
