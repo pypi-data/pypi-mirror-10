2e41697a2d5e4b044d534d991992ef3aac47e2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:53 +0000

    Add package pytb/0.0.2/pytb-0.0.2.tar.gz

---
+++ packages/pytb/pytb-0.0.2.tar.gz/pytb-0.0.2/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+packages = {
+    'pytb': 'py-modules/',
+    'pytb.scripts': 'py-modules/scripts'
+}
+
+setup(
+    name="pytb",
+    version="0.0.2",
+    author="mitmeedle",
+    author_email="mitmeedle@gmail.com",
+    entry_points={
+        'console_scripts': [
+            'pytb = pytb.scripts.pytb:main',
+        ]
+    },
+    packages=packages,
+    package_dir=packages,
+)
