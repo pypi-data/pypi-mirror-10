6d1da587cb0189bd3c6001e2a2b6ee57f1127940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:46 +0000

    Add package django-netfields/0.3/django-netfields-0.3.tar.gz

---
+++ packages/django-netfields/django-netfields-0.3.tar.gz/django-netfields-0.3/testsettings.py
@@ -0,0 +1,18 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'netfields',
+    }
+}
+
+INSTALLED_APPS = (
+    'netfields',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+SECRET_KEY = "notimportant"
