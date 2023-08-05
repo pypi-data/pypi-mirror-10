9470208bb8f25e0dd26dd19b0da877efde3d3142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package func-prototypes/0.2/func_prototypes-0.2.tar.gz

---
+++ packages/func-prototypes/func_prototypes-0.2.tar.gz/func_prototypes-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='func_prototypes',
+      version='0.2',
+      description='library which helps people constrain python functions by type.',
+      url='https://github.com/andrewguy9/func_prototypes',
+      author='andrew thomson',
+      author_email='athomsonguy@gmail.com',
+      license='MIT',
+      packages=['func_prototypes'],
+      entry_points = {
+        'console_scripts': [
+          ],
+      },
+      zip_safe=False)
