b9f9576dac45df6a84e0ed4e0bf5e77c7a6be7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package read-device/3.1.2rc1/read-device-3.1.2rc1.tar.gz

---
+++ packages/read-device/read-device-3.1.2rc1.tar.gz/read-device-3.1.2rc1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='read-device',
+    version='3.1.2rc1',
+    description='Collect metrics from real-world devices',
+    #url=''
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+
+    ],
+    author='Lewis Eason',
+    author_email='me@lewiseason.co.uk',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'lxml',
+        'texttable',
+        'requests',
+        'peewee',
+    ],
+    entry_points='''
+        [console_scripts]
+        device=read_device.commands.device:main
+        meters=read_device.commands.meters:main
+    '''
+)
