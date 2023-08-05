ea2709ecfc9a354ccff33c5a0ca1fd8a97ada1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:35 +0000

    Add package django-rest-framework-nested/0.0.1/django-rest-framework-nested-0.0.1.tar.gz

---
+++ packages/django-rest-framework-nested/django-rest-framework-nested-0.0.1.tar.gz/django-rest-framework-nested-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-rest-framework-nested',
+    version='0.0.1',
+    packages=['rest_framework_nested'],
+    url='https://github.com/BakaBox/django-rest-framework-nested',
+    license='Apache',
+    author='James R Parks',
+    author_email='',
+    description=''
+)
