c0b06d782bc4a15e4c75c08778f943eb94517c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:54 +0000

    Add package aliyunoss/0.3.8b2/aliyunoss-0.3.8b2.tar.gz

---
+++ packages/aliyunoss/aliyunoss-0.3.8b2.tar.gz/aliyunoss-0.3.8b2/oss/pkg_info.py
@@ -0,0 +1,10 @@
+package = "aliyunoss"
+version = "0.3.8b2"
+url = "http://oss.aliyun.com"
+license = "MIT License"
+short_description = "Unofficial packaging for official aliyun oss python sdk"
+long_description = """
+Python SDK for Aliyun OSS(Open Storage Service).
+Hope we will get an official packaging fron Aliyun Lazy Team soon.
+"""
+
