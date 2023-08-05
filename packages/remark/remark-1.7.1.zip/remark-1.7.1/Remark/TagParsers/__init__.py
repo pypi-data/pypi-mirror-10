bdfb49cfca11774df6dd7dc04e8422534efe2ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:50 +0000

    Add package remark/1.7.1/remark-1.7.1.zip

---
+++ packages/remark/remark-1.7.1.zip/remark-1.7.1/Remark/TagParsers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Description: Remark.TagParsers package initialization
+# Documentation: tag_parsers.txt
+
+# Remark 1.7.1
+# Copyright (c) 2009 - 2015
+# Kalle Rutanen
+# Distributed under the MIT license (see license.txt).
+
---
+++ packages/remark/remark-1.7.1.zip/remark-1.7.1/Remark/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Description: Remark package initialization
+# Documentation: implementation.txt
+
+# Remark 1.7.1
+# Copyright (c) 2009 - 2015
+# Kalle Rutanen
+# Distributed under the MIT license (see license.txt).
+
