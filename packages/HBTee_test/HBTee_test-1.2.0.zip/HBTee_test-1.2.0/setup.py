34e27f004e70ebbbc13809199129cfa6b9db80ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:33 +0000

    Add package HBTee_test/1.2.0/HBTee_test-1.2.0.zip

---
+++ packages/HBTee_test/HBTee_test-1.2.0.zip/HBTee_test-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+    name            = 'HBTee_test',
+    version         = '1.2.0',
+    py_modules      = ['HBTee_test'],
+    author          = 'lsj',
+    author_email    = 'HBTee_ee@163.com',
+    url             = 'www.eetop.cn/blog/1394841',
+    description     = 'A simple example referring to Head first Python!',
+    
+    )
