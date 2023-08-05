338bd85b872e849a752879b5d0d81979d52c4f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package jimit/0.10/jimit-0.10.tar.gz

---
+++ packages/jimit/jimit-0.10.tar.gz/jimit-0.10/jimit/utils.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/4/20'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+import os
+import json
+import copy
+import decimal
+
+try:
+    file_path = os.path.join(os.path.dirname(__file__), 'state_code.json')
+    with open(file_path) as f:
+        index_state = json.load(f)
+except Exception, e:
+    print e.message
+    exit()
+
+
+class Commons():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def exchange_state(code):
+        if not isinstance(code, int):
+            result = Commons.exchange_state(50001)
+            return result
+
+        trunk_code = int(code / 100)
+        if str(trunk_code) not in index_state['trunk']:
+            result = Commons.exchange_state(50002)
+            return result
+
+        result = copy.copy(index_state['trunk'][(str(trunk_code))])
+
+        if str(code) in index_state['branch']:
+            result['sub'] = copy.copy(index_state["branch"][(str(code))])
+
+        return result
+
+
+class Check():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def previewing(member=list(), set_=dict()):
+        result = dict()
+        result['state'] = Commons.exchange_state(20000)
+
+        for item in member:
+            if item not in set_:
+                result['state'] = Commons.exchange_state(41201)
+                result['state']['sub']['zh-cn'] = ''.join([result['state']['sub']['zh-cn'], item])
+                return result
+
+        return result
+
+
+class Convert():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def sql2json(sql_obj):
+        j_sql = dict()
+        result = dict()
+        result['state'] = Commons.exchange_state(20000)
+
+        try:
+            for col in sql_obj._sa_class_manager.mapper.mapped_table.columns:
+                if isinstance(getattr(sql_obj, col.name), decimal.Decimal):
+                    j_sql[col.name] = str(getattr(sql_obj, col.name))
+                else:
+                    j_sql[col.name] = getattr(sql_obj, col.name)
+        except Exception, e:
+            result['state'] = Commons.exchange_state(50003)
+            result['state']['sub']['detail'] = e.message
+            return result
+
+        result['j_sql'] = j_sql
+        return result
