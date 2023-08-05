d8197b58666ed7971484e7a404f86c2702d16046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package locustauto/0.2.1/locustauto-0.2.1.tar.gz

---
+++ packages/locustauto/locustauto-0.2.1.tar.gz/locustauto-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='locustauto',
+    version='0.2.1',
+    author='Christopher Lee',
+    author_email='christopherlee2012@gmail.com',
+    py_modules=['locust_auto_test',],
+    url='https://github.com/BenjiLee/locustauto',
+    license='Apache2',
+    description='Automatically Test with Locust.io',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'pyyaml==3.11',
+        'locustio==0.7.2',
+    ],
+    entry_points = {
+        'console_scripts': ['locustauto=locust_auto_test:main'],
+    }
+)
