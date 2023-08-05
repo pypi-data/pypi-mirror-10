a539686d660bfe6c39d466e3108e07aede755d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:29 +0000

    Add package OpenWeatherForecast/0.1.6/OpenWeatherForecast-0.1.6-py34-none-any.whl

---
+++ packages/OpenWeatherForecast/OpenWeatherForecast-0.1.6-py34-none-any.whl/open_weather_forecast/get_temperature.py
@@ -0,0 +1,13 @@
+from open_weather_forecast.info_extractor.get_info import GetInfo
+
+
+class GetTemperature(GetInfo):
+
+    def __init__(self):
+        super(GetTemperature).__init__()
+        self.db = None
+
+    def store_data(self, data):
+        self.db = self.get_db_connection()
+        import ipdb
+        ipdb.set_trace()
