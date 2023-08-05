3cd8026227cb13755cb9db115f1af6d7fbf6238a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package ycyc/0.1.0.6/ycyc-0.1.0.6-py2.7.egg

---
+++ packages/ycyc/ycyc-0.1.0.6-py2.7.egg/ycyc/base/contextutils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from contextlib import contextmanager
+import six
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def catch(errors=Exception, reraise=None, callback=logger.warning):
+    """
+    A context manager to catch the exceptions raised from block.
+    :param errors: exception or exceptions tuple
+    :param reraise: reraise a new exception from catched exception(default: None)
+    :param callback: callback when catched a exception(default: logger.warning)
+    """
+    try:
+        yield
+    except errors as err:
+        if callback:
+            callback(err)
+        if reraise:
+            six.raise_from(reraise, err)
+
+
+@contextmanager
+def subprocessor(*args, **kwg):
+    """
+    With new subprocess call,terminate it when exit context
+    :param args: argument pass to subprocess.Popen
+    :param kwg: key word argument pass to subprocess.Popen
+    """
+    from subprocess import Popen
+    processor = None
+    try:
+        processor = Popen(*args, **kwg)
+        yield processor
+    finally:
+        if processor.poll() is None:
+            processor.terminate()
+            processor.wait()
