4eb041ea0643cfa82f9f7f75eb324f1911366261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:35 +0000

    Add package zipfile2/0.0.4/zipfile2-0.0.4.tar.gz

---
+++ packages/zipfile2/zipfile2-0.0.4.tar.gz/zipfile2-0.0.4/zipfile2/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ZIPFILE2 SETUP.PY
+version = '0.0.4'
+full_version = '0.0.4'
+git_revision = '0a918616dbbf6bead3f2fed6e8cd896a236b5648'
+is_released = True
+
+if not is_released:
+    version = full_version
