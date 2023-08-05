84adab07bfebc62d105b8ec2a4c5539d79cec59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package aio.config/0.0.11/aio.config-0.0.11.tar.gz

---
+++ packages/aio.config/aio.config-0.0.11.tar.gz/aio.config-0.0.11/aio/config/__init__.py
@@ -0,0 +1,78 @@
+import os
+import asyncio
+
+from configparser import ConfigParser, ExtendedInterpolation
+
+from aio.core.exceptions import MissingConfiguration
+
+from collections import OrderedDict
+
+
+def gather_config(modules, filename=None):
+    config_files = []
+    for module in modules:
+        path = module.__path__[0]
+        app_config = os.path.join(path, filename or "aio.conf")
+        if os.path.exists(app_config):
+            config_files.append(app_config)
+    return config_files
+
+
+def find_config(app_dir=None, filename=None):
+    if not app_dir:
+        app_dir = os.getcwd()
+    config = os.path.join(app_dir, filename or 'aio.conf')
+    if os.path.exists(config):
+        return config
+    config = os.path.join(app_dir, 'etc', filename or 'aio.conf')
+    if os.path.exists(config):
+        return config
+    config = os.path.join(app_dir, '/etc', filename or 'aio.conf')
+    if os.path.exists(config):
+        return config
+    raise MissingConfiguration('no configuration file found')
+
+
+@asyncio.coroutine
+def parse_config(config=None, config_files=None, app_dir=None,
+                 config_dict=None, config_string=None,
+                 modules=None, parser=None, interpolation=None,
+                 filename=None):
+    parser = parser or ConfigParser(
+        interpolation=interpolation or ExtendedInterpolation())
+    if config_dict:
+        parser.read_dict(config_dict)
+    elif config_string:
+        parser.read_string(config_string)
+    elif config_files:
+        for config_file in config_files:
+            parser.read_file(open(config))
+    elif modules:
+        for config_file in gather_config(modules, filename=filename):
+            parser.read_file(open(config_file))
+    else:
+        parser.read_file(
+            open(config or find_config(app_dir, filename=filename)))
+    return parser
+
+
+class ConfigSection(OrderedDict):
+
+    pass
+
+
+class Config(OrderedDict):
+
+    pass
+
+
+def replicate_config(config, test_section=None):
+    new_config = Config()
+    for section_name, section in config.items():
+        if test_section:
+            if not test_section(section_name):
+                continue
+        new_config[section_name] = ConfigSection()
+        for option_name, option in section.items():
+            new_config[section_name][option_name] = option
+    return new_config
---
+++ packages/aio.config/aio.config-0.0.11.tar.gz/aio.config-0.0.11/setup.py
@@ -0,0 +1,48 @@
+"""
+aio.config
+"""
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = "0.0.11"
+
+install_requires = ['distribute', 'aio.core']
+
+if sys.version_info < (3, 4):
+    install_requires += ['asyncio']
+
+tests_require = install_requires + ['aio.testing']
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = read("README.rst")
+
+setup(
+    name='aio.config',
+    version=version,
+    description="Aio configuration utilities",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='',
+    author='Ryan Northey',
+    author_email='ryan@3ca.org.uk',
+    url='http://github.com/phlax/aio.config',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['aio'],
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=tests_require,
+    test_suite="aio.config.tests",
+    install_requires=install_requires,
+    entry_points="""
+    # -*- Entry points: -*-
+    """)
