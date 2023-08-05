2b1e9d782c65b515a78415ef2e54f3fd8d88eb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:17 +0000

    Add package Flask-Upwork/1.0-pre1/Flask-Upwork-1.0-pre1.tar.gz

---
+++ packages/Flask-Upwork/Flask-Upwork-1.0-pre1.tar.gz/Flask-Upwork-1.0-pre1/setup.py
@@ -0,0 +1,51 @@
+"""
+Flask-Upwork
+-----------
+Adds Upwork API support to Flask.
+
+flask-upwork version 1.0
+(C) 2015 Upwork
+"""
+
+import os
+from setuptools import setup
+
+readme = open(os.path.join(os.path.dirname(__file__), 'README'))
+README = readme.read()
+readme.close()
+
+version = __import__('flaskext').get_version()
+
+setup(
+    name='Flask-Upwork',
+    version='1.0-pre1',
+    download_url='http://github.com/strogonoff/flask-upwork',
+    url='https://github.com/strogonoff/flask-upwork',
+    license='BSD',
+    author='Artem Gnilov',
+    author_email='boobsd@gmail.com',
+    maintainer='Volodymyr Hotsyk, Anton Strogonoff',
+    maintainer_email='gotsyk@gmail.com, astrogov@upwork.com',
+    description='Upwork API support to Flask',
+    long_description=README,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'oauth2',
+        'python-upwork>=1.0'
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
