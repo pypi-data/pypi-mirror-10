93296b299a15b636c7410153b6b412bcdc3b92ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package test_one/1.2.0/test_one-1.2.0.tar.gz

---
+++ packages/test_one/test_one-1.2.0.tar.gz/test_one-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name = "test_one",
+      version = "1.2.0",
+      py_modules = ["test_one"],
+      author =  "WilsonDu",
+      author_email = "jiahao_du@icloud.com",
+      url = "www.baidu.com",
+      description = "Change the print_lol API "
+      )
