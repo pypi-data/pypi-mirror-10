b8b75c4c121f99885cd5f92286d9aec4cc621a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:21 +0000

    Add package aldryn-bootstrap3/0.4.5/aldryn-bootstrap3-0.4.5.tar.gz

---
+++ packages/aldryn-bootstrap3/aldryn-bootstrap3-0.4.5.tar.gz/aldryn-bootstrap3-0.4.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from aldryn_bootstrap3 import __version__
+
+REQUIREMENTS = [
+    'django-durationfield>=0.5.1',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    name='aldryn-bootstrap3',
+    version=__version__,
+    description='cms plugins and helpers for bootstrap3 based sites',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/aldryn/aldryn-bootstrap3',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="test_settings.run",
+)
