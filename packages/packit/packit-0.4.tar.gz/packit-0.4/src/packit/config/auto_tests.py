736ae057fca3dfdd08b4499807157869321f5f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:05 +0000

    Add package packit/0.4/packit-0.4.tar.gz

---
+++ packages/packit/packit-0.4.tar.gz/packit-0.4/src/packit/config/auto_tests.py
@@ -0,0 +1,13 @@
+from pbr import packaging
+
+from .base import BaseConfig
+
+
+class AutoTestsConfig(BaseConfig):
+
+    def __call__(self, config, facility_section_name):
+        global_config = config.setdefault('global', {})
+        current_commands = global_config.get('commands', '')
+        global_config['commands'] = '\n'.join(['packit.commands.packit_test.PackitTest', current_commands])
+
+auto_tests_config = AutoTestsConfig()
