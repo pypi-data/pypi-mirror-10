980b130a091457f0fc67baf41cdd89cd65858836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package totesthowtoupload/1.0.0/totesthowtoupload-1.0.0.tar.gz

---
+++ packages/totesthowtoupload/totesthowtoupload-1.0.0.tar.gz/totesthowtoupload-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+name = 'totesthowtoupload',
+version = '1.0.0',
+py_modules = ['totesthowtoupload'],
+author = 'BrianZ',
+author_email ='1242242@qq.com',
+url ='http://www.headfirstlabs.com',
+description ='a test project to upload a module to PyPI',
+)
+
