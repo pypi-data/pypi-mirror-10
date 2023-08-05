9e6b1b7bd77f7492ccd608245e2d820203fcce87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:53 +0000

    Add package streambin/0.1/streambin-0.1.tar.gz

---
+++ packages/streambin/streambin-0.1.tar.gz/streambin-0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+#   Copyright 2015 Linux2Go
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+from setuptools import setup, find_packages
+
+setup(
+    name='streambin',
+    description='Client for Streambin.net',
+    author='Soren Hansen',
+    author_email='soren@linux2go.dk',
+    url='http://streambin.net/',
+    version='0.1',
+    packages=['streambin'],
+    zip_safe=True,
+    install_requires=['requests'],
+    entry_points={'console_scripts': ['streambin=streambin:main']}
+)
+
---
+++ packages/streambin/streambin-0.1.tar.gz/streambin-0.1/streambin/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#
+#   Copyright 2015 Linux2Go
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+import requests
+import argparse
+import sys
+
+def create_new(args):
+    resp = requests.post(args.url + '/new')
+    retval = resp.json()
+    print 'Read/Write URL:        ', retval['rw_url']
+    print 'Read-only URL:         ', retval['ro_url']
+    print 'Friendly read-only URL:', retval['html_ro_url']
+    return retval['rw_url']
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--url', default='http://streambin.net',
+                        help="Service URL")
+    parser.add_argument('uuid',
+                        help='Read/write ID (Use "new" to create new stream)')
+    parser.add_argument('file', help="File to upload (- denotes stdin)")
+
+    args = parser.parse_args()
+
+    if args.uuid == 'new':
+        write_url = create_new(args)
+    else:
+        write_url = args.url + '/' + args.uuid
+
+    if args.file == '-':
+        fp = sys.stdin
+    else:
+        fp = open(args.file, 'r')
+
+    while True:
+        l = fp.readline()
+        if l == '':
+            break
+        requests.post(write_url, l)
+        
+
+if __name__ == '__main__':
+    sys.exit(not main())
