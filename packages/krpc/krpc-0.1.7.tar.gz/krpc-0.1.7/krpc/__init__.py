53c5f2a1c52fbb9539c553a831bf91cfafa1049a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:57 +0000

    Add package krpc/0.1.7/krpc-0.1.7.tar.gz

---
+++ packages/krpc/krpc-0.1.7.tar.gz/krpc-0.1.7/krpc/__init__.py
@@ -0,0 +1,38 @@
+import socket
+from krpc.connection import Connection
+from krpc.client import Client
+from krpc.encoder import _Encoder
+from krpc.decoder import _Decoder
+
+DEFAULT_ADDRESS = '127.0.0.1'
+DEFAULT_RPC_PORT = 50000
+DEFAULT_STREAM_PORT = 50001
+
+def connect(address=DEFAULT_ADDRESS, rpc_port=DEFAULT_RPC_PORT, stream_port=DEFAULT_STREAM_PORT, name=None):
+    """
+    Connect to a kRPC server on the specified IP address and port numbers. If
+    stream_port is None, does not connect to the stream server.
+    Optionally give the kRPC server the supplied name to identify the client (up
+    to 32 bytes of UTF-8 encoded text).
+    """
+    assert rpc_port != stream_port
+
+    # Connect to RPC server
+    rpc_connection = Connection(address, rpc_port)
+    rpc_connection.connect()
+    rpc_connection.send(_Encoder.RPC_HELLO_MESSAGE)
+    rpc_connection.send(_Encoder.client_name(name))
+    client_identifier = rpc_connection.receive(_Decoder.GUID_LENGTH)
+
+    # Connect to Stream server
+    if stream_port is not None:
+        stream_connection = Connection(address, stream_port)
+        stream_connection.connect()
+        stream_connection.send(_Encoder.STREAM_HELLO_MESSAGE)
+        stream_connection.send(client_identifier)
+        ok_message = stream_connection.receive(_Decoder.OK_LENGTH)
+        assert ok_message == _Decoder.OK_MESSAGE
+    else:
+        stream_connection = None
+
+    return Client(rpc_connection, stream_connection)
