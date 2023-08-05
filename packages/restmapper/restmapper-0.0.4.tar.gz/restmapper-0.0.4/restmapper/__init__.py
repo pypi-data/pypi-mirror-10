af14ad2a98a5a7739dfba1cb71333212da463384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:02 +0000

    Add package restmapper/0.0.4/restmapper-0.0.4.tar.gz

---
+++ packages/restmapper/restmapper-0.0.4.tar.gz/restmapper-0.0.4/restmapper/__init__.py
@@ -0,0 +1,21 @@
+from restmapper import RestMapper
+
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+__all__ = [
+    '__author__',
+    '__copyright__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+    'RestMapper',
+]
+
