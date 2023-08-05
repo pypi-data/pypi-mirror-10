f782bd3e1f6e72ba19ef42233df1b2fef32d3a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package modulo1/1.0.0/modulo1-1.0.0.tar.gz

---
+++ packages/modulo1/modulo1-1.0.0.tar.gz/modulo1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'modulo1',
+        version = '1.0.0',
+        py_modules = ['modulo1'],
+        author = 'luiz',
+        author_email = 'luizsantos1992@gmail.com',
+        url = "http://agenciadev4.com.br",
+        description = "Aprendendo python",
+    )
