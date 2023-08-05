5ce66ef4ad65ac44d7ef0ce306307d3ca87f1196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package pyexperiment/0.2.8/pyexperiment-0.2.8.tar.gz

---
+++ packages/pyexperiment/pyexperiment-0.2.8.tar.gz/pyexperiment-0.2.8/pyexperiment/__init__.py
@@ -0,0 +1,17 @@
+"""The pyexperiment module - quick and clean experiments with Python.
+"""
+from pyexperiment.version import __version__
+
+from pyexperiment.utils.Singleton import SingletonIndirector
+from pyexperiment.utils.Singleton import InitializeableSingletonIndirector
+
+# For convenience, set up the basic tools here
+# pylint: disable=invalid-name
+from pyexperiment.Config import Config
+conf = SingletonIndirector(Config)
+
+from pyexperiment.Logger import TimingLogger
+log = InitializeableSingletonIndirector(TimingLogger)
+
+from pyexperiment.State import State
+state = SingletonIndirector(State)
