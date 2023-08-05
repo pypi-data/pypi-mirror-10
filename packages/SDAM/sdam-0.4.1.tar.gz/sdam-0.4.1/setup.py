346f1a217f77c699ac9e3818e1766e222d996a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:41 +0000

    Add package SDAM/0.4.1/sdam-0.4.1.tar.gz

---
+++ packages/SDAM/sdam-0.4.1.tar.gz/sdam-0.4.1/setup.py
@@ -0,0 +1,24 @@
+import sys
+if not sys.version_info.major < 3:
+    print("Sorry, Python 3 is not supported (yet)")
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "sdam",
+    version = "0.4.1",
+    packages = ['sdam'],
+    entry_points = {
+        'console_scripts': ['sdam = sdam:sdam.main'],
+    },
+    zip_safe = False,
+    install_requires = ['twisted>=15.0'],
+    author = "Oleksandr Kozachuk",
+    author_email = "ddeus.lp@mailnull.com",
+    description = "(s)tart programs (d)aemonized (a)nd (m)onitored.",
+    long_description = open('README.txt').read(),
+    license = "WTFPL",
+    url = "http://launchpad.net/sdam",
+    keywords = "daemon monitoring service",
+)
