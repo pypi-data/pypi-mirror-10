7e7fde2c4aae725433670a4f8d26b9640a72e014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package descriptive_id/0.1.1/descriptive_id-0.1.1.tar.gz

---
+++ packages/descriptive_id/descriptive_id-0.1.1.tar.gz/descriptive_id-0.1.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='descriptive_id',
+    packages=['descriptive_id'],
+    version='0.1.1',
+    description='django-descriptive-id is an abstract model that you can use as a mixin to give your models a human-readable, unique identifier.',
+    author='Bellhops',
+    author_email='tech@getbellhops.com',
+    url='https://github.com/bellhops/django-descriptive-id',
+    download_url='https://github.com/bellhops/django-descriptive-id/tarball/0.1',
+    keywords=['django', 'modelfield', 'uuid'],
+    classifiers=[],
+)
