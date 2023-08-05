de2c2d8c5317abe4618feba9773e488b6059f1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:18 +0000

    Add package pyxie/0.0.8-1/pyxie-0.0.8-1.tar.gz

---
+++ packages/pyxie/pyxie-0.0.8-1.tar.gz/pyxie-0.0.8-1/test-data/genprogs/basic_assignment.p/basic_assignment.c
@@ -0,0 +1,33 @@
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    double A;
+    int HELLO;
+    int Hello;
+    int HelloWorld;
+    string Hello_World;
+    string Hello_World_1;
+    int _A;
+    int a;
+    string a_2;
+    string a_3;
+    bool a_4;
+    bool a_5;
+    
+    a = 1;
+    A = 1.0;
+    _A = 16;
+    HELLO = 180150000;
+    Hello = 8;
+    HelloWorld = 5;
+    Hello_World = "Hello";
+    Hello_World_1 = "Hello' world";
+    a_2 = "Hello";
+    a_3 = "Hello\" world";
+    a_4 = true;
+    a_5 = false;
+    return 0;
+}
