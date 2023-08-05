efe8ab57f8626fb4950af42cd577afebe049cdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:41 +0000

    Add package django-secureadmin/1.2/django-secureadmin-1.2.tar.gz

---
+++ packages/django-secureadmin/django-secureadmin-1.2.tar.gz/django-secureadmin-1.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.2'
+
+setup(
+    name='django-secureadmin',
+    version=VERSION,
+    description="Django-SecureAdmin send verification mail when user last and new ip not equals.",
+    long_description=(
+        open("README.rst").read()),
+    keywords='secure django,django,secure django admin,django admin,secureadmin',
+    author='Mahdi Fakhrabadi',
+    author_email='mahdi.blackhat@gmail.com',
+    url='https://github.com/django-pci/django-axes',
+    license='MIT',
+    package_dir={'secureadmin': 'secureadmin'},
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Security',
+    ],
+    zip_safe=False,
+)
