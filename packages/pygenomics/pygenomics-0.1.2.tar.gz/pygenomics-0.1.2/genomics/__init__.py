12be45fd8d79c98f401eb133b2e3332f76aa14d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package pygenomics/0.1.2/pygenomics-0.1.2.tar.gz

---
+++ packages/pygenomics/pygenomics-0.1.2.tar.gz/pygenomics-0.1.2/genomics/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics
+   :synopsis: A library for modern population genomics analysis
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
+from . import config
+from .parallel import executor
+
+__version__ = '0.1.2'
+
+
+class GenomicsException(Exception):
+    '''A general exception for the library'''
+
+cfg = config.Config()
+cfg.load_config()
+if cfg.grid == 'Local':
+    lexec = executor.Local(cfg.grid_limit)
+else:
+    raise GenomicsException('Grid %s unknown' % cfg.grid)
