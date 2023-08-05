d02c6d9aac47c37856c4ee4ce649163f97f751bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package emigrate/0.4.1/emigrate-0.4.1.tar.gz

---
+++ packages/emigrate/emigrate-0.4.1.tar.gz/emigrate-0.4.1/emigrate/__init__.py
@@ -0,0 +1,9 @@
+"""Emigrate: An electrophoresis solver.
+
+Class Constructor constructs an electrophoresis system.
+
+Class Migrate solves the system.
+"""
+import ionize
+from Migrate import Migrate
+from Electrolyte import Electrolyte
