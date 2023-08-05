8933035e6145a37326753a0fee0f45939ac55482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:07 +0000

    Add package kimono/0.2.3/kimono-0.2.3.tar.gz

---
+++ packages/kimono/kimono-0.2.3.tar.gz/kimono-0.2.3/README.rst
@@ -0,0 +1,25 @@
+kimono
+======
+
+USAGE
+
+
+.. code:: python
+
+    from kimono import Kimono
+    api = Kimono('APIKEY', 'APIID')
+
+IMPLEMENTED
+
+
+`Retrieve an API <https://www.kimonolabs.com/apidocs#RetrieveApi>`__
+
+.. code:: python
+
+    api.retrieve_api()
+
+`List all APIs <https://www.kimonolabs.com/apidocs#ListApis>`__
+
+.. code:: python
+
+    api.list_apis()
---
+++ packages/kimono/kimono-0.2.3.tar.gz/kimono-0.2.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='kimono',
+    version='0.2.3',
+    description='Python wrapper for kimonolabs API',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'requests',
+    ],
+    url='https://github.com/vu3jej/kimono',
+    author='Jithesh E J',
+    author_email='mail@jithesh.net',
+    license='MIT',
+    packages=['kimono'],
+    zip_safe=False)
