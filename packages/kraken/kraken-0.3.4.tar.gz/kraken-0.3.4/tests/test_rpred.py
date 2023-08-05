e98752c7b019d5a563cb1aa0d27dcb99fb5169b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:33 +0000

    Add package kraken/0.3.4/kraken-0.3.4.tar.gz

---
+++ packages/kraken/kraken-0.3.4.tar.gz/kraken-0.3.4/tests/test_rpred.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import os
+import tempfile
+import pickle
+
+from nose.tools import raises 
+
+from kraken import rpred
+from kraken.lib.exceptions import KrakenInvalidModelException
+
+class TestRecognition(object):
+
+    """
+    Tests of the recognition facility and associated routines.
+    """
+
+    def setUp(self):
+        self.temp = tempfile.NamedTemporaryFile(delete=False)
+
+    def tearDown(self):
+        self.temp.close()
+        os.unlink(self.temp.name)
+
+    def test_load_rnn_invalid(self):
+        """
+        Test correct handling of non-pickle files.
+        """
+        pass
+
+    def test_load_rnn_no_seqrecognizer(self):
+        """
+        Test correct handling of non-SeqRecognizer pickles.
+        """
+        pass
+
+    def test_load_rnn_gz(self):
+        """
+        Test correct handling of gzipped models.
+        """
+        pass
+
+    def test_load_rnn_bz2(self):
+        """
+        Test correct handling of bzip2 compressed models.
+        """
+        pass
+
+    def test_load_rnn_uncompressed(self):
+        """
+        Test correct handling of uncompressed models.
+        """
+        pass
+
+    def test_load_rnn_aliasing(self):
+        """
+        Test correct aliasing of ocrolib classes.
+        """
+        pass
+
+    def test_load_rnn_aliasing_old(self):
+        """
+        Test correct aliasing of pre-ocrolib classes.
+        """
+        pass
