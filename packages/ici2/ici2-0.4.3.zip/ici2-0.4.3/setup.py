ab36e129248516febcb7f4bef0ed6c90cbf6915f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:47 +0000

    Add package ici2/0.4.3/ici2-0.4.3.zip

---
+++ packages/ici2/ici2-0.4.3.zip/ici2-0.4.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'ici2',
+    version = '0.4.3',
+    py_modules = ['ici2'],
+    author = 'realhu1989',
+    author_email = 'huyunyan@live.cn',
+    )
