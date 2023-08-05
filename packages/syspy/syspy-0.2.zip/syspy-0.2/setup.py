2f5b0b2c14cb8e2bfeeca29cbf99f6d5e0fd7d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package syspy/0.2/syspy-0.2.zip

---
+++ packages/syspy/syspy-0.2.zip/syspy-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'syspy',
+    version = '0.2',
+    url = 'https://github.com/aligoren/syspy',
+    download_url = 'https://github.com/aligoren/syspy/archive/master.zip',
+    author = 'Ali GOREN <goren.ali@yandex.com>',
+    author_email = 'goren.ali@yandex.com',
+    license = 'Apache v2.0 License',
+    packages = ['syspy'],
+    description = 'Windows System Informations',
+    keywords = ['sys', 'util', 'system', 'info', 'information', 'windows', 'os'],
+)
