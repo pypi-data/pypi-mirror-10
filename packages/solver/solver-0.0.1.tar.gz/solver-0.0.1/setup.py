801b75942d51c9b2871c8ec9ac82bbf8d0916359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package solver/0.0.1/solver-0.0.1.tar.gz

---
+++ packages/solver/solver-0.0.1.tar.gz/solver-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name = "solver",
+    packages = ["solver"],
+    version = "0.0.1",
+    author = "Bahrom Matyakubov",
+    author_email = "bahrom.matyakubov@gmail.com",
+    url = "https://pypi.python.org/pypi/solver",
+    description = "Math problem solver",
+    keywords = ["solver", "education", "mathematics"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Education",
+        "Topic :: Education :: Computer Aided Instruction (CAI)",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+)
---
+++ packages/solver/solver-0.0.1.tar.gz/solver-0.0.1/solver/main.py
@@ -0,0 +1,4 @@
+__author__ = 'bahrom'
+
+
+print("Hello, world!")
