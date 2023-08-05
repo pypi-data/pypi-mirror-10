7d1c9f8161024dc27814eda75fb0a50278a5adef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:16 +0000

    Add package mypypiproject/0.0.4/mypypiproject-0.0.4.tar.gz

---
+++ packages/mypypiproject/mypypiproject-0.0.4.tar.gz/mypypiproject-0.0.4/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "mypypiproject",
+    version = "0.0.4",
+    author = "David Daniels",
+    author_email = "david8381@gmail.com",
+    description = ("A demo"),
+    license = "BSD",
+    keywords = "example doc tutorial",
+    url = "http://packages.python.org/mypypiproject",
+    packages=['mypypiproject','tests'],
+    long_description="just an example",
+)
