359058d422d484a7326b83a29f1e5dba99d06dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package officedissector/1.0/officedissector-1.0.tar.gz

---
+++ packages/officedissector/officedissector-1.0.tar.gz/officedissector-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    author='Grier Forensics',
+    author_email='jdgrier@grierforensics.com',
+    description="""OfficeDissector is a parser library for static security analysis of OOXML documents.""",
+    name='officedissector',
+    version='1.0',
+    url='http://www.officedissector.com/',
+    packages=['officedissector'])
