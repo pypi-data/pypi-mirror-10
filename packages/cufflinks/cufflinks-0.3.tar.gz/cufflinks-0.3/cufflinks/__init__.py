b0c46c267b9714bf18842b1db1b16e139f18586c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:03 +0000

    Add package cufflinks/0.3/cufflinks-0.3.tar.gz

---
+++ packages/cufflinks/cufflinks-0.3.tar.gz/cufflinks-0.3/cufflinks/__init__.py
@@ -0,0 +1,18 @@
+"""
+
+A productivity tool that binds pandas and plotly.
+It also provides tools for color generation and transformation. 
+
+Author: @jorgesantos
+
+"""
+
+
+from plotlytools import *
+from pandastools import *
+from utils import pp
+from extract import to_df
+import colors
+import datetools
+import utils
+import datagen
