c93419dd19835a98d96628a0fc05f8b534e2712d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package asynctest/0.2.0/asynctest-0.2.0-py3.4.egg

---
+++ packages/asynctest/asynctest-0.2.0-py3.4.egg/asynctest/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# flake8: noqa
+
+import unittest
+from unittest import *
+
+# Shadows unittest with our enhanced classes
+from .case import *
+from .mock import *
+
+# And load or own tools
+from .helpers import *
+
+__all__ = unittest.__all__
