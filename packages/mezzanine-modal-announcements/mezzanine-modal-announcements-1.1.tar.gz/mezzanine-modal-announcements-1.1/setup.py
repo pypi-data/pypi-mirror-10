09e677896af7e1e31e5d7dfca67d4995dd544440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package mezzanine-modal-announcements/1.1/mezzanine-modal-announcements-1.1.tar.gz

---
+++ packages/mezzanine-modal-announcements/mezzanine-modal-announcements-1.1.tar.gz/mezzanine-modal-announcements-1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+    name = 'mezzanine-modal-announcements',
+    packages = find_packages(),
+    version = '1.1',
+    description = 'Popup announcements for Mezzanine websites utilizing Bootstrap modals.',
+    author = 'Joshua Cartmell',
+    author_email = 'chat@joshc.io',
+    url = 'https://github.com/joshcartme/mezzanine-modal-announcements',
+    download_url = 'https://github.com/joshcartme/mezzanine-modal-announcements/tarball/1.1',
+    keywords = ['mezzanine', 'django', 'announcements'],
+    classifiers = [],
+)
