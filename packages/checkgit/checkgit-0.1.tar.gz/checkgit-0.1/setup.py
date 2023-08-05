439b90e9782704597dfa2dec59fe2d74c1f82944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package checkgit/0.1/checkgit-0.1.tar.gz

---
+++ packages/checkgit/checkgit-0.1.tar.gz/checkgit-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import os
+
+setup(name='checkgit',
+      packages=['checkgit'],
+      version='0.1',
+      description='A quick visual check of several git repositories',
+      author='Greg Ashton',
+      author_email='gash789@gmail.com',
+      url='https://github.com/ga7g08/CheckGit',
+      download_url='https://github.com/ga7g08/CheckGit/tarball/0.1',
+      keywords=['git'],
+      classifiers=[],
+      scripts=['checkgit/checkgit'],
+      )
