196809548244f0576f5d62268bade0d25c339059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package gemini/0.15.1/gemini-0.15.1.tar.gz

---
+++ packages/gemini/gemini-0.15.1.tar.gz/gemini-0.15.1/gemini/tool_autosomal_recessive.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+from gemini_inheritance_model_utils import GeminiInheritanceModelFactory
+
+def run(parser, args):
+    if os.path.exists(args.db):
+        auto_recessive_factory = \
+            GeminiInheritanceModelFactory(args, model="auto_rec")
+        auto_recessive_factory.get_candidates()
