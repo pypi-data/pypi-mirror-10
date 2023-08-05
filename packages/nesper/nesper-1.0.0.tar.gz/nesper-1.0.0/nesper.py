98b931eb9dd9623bdd1d5c16033bb0f931fad165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:13 +0000

    Add package nesper/1.0.0/nesper-1.0.0.tar.gz

---
+++ packages/nesper/nesper-1.0.0.tar.gz/nesper-1.0.0/nesper.py
@@ -0,0 +1,9 @@
+"""Esse é um módulo "nester.py" e fornece uma função chamada print_lol() que imprime listas que podem ou não conter listas aninhadas."""
+def print_lol(the_list, level):
+	for i in the_list:
+		if isinstance(i, list):
+			print_lol(i, level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end=")
+			print(i)
