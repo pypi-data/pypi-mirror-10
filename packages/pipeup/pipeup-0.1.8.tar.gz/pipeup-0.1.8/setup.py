0285d31537e9a426e876c654033cfa20e820a419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:57 +0000

    Add package pipeup/0.1.8/pipeup-0.1.8.tar.gz

---
+++ packages/pipeup/pipeup-0.1.8.tar.gz/pipeup-0.1.8/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup
+
+setup(
+    name='pipeup',
+    version='0.1.8',
+    packages=['pipeup'],
+    install_requires=['click', 'websocket-client'],
+    extras_require={
+        'server': ['tornado'],
+        'server-pubnub': ['tornado', 'pubnub', 'redis']
+    },
+    entry_points={
+        'console_scripts': ['pipeup=pipeup.cli:main'],
+    },
+    download_url='https://github.com/nathancahill/pipeup/tarball/0.1.8',
+    author='Nathan Cahill',
+    author_email='nathan@nathancahill.com',
+    url='http://github.com/nathancahill/pipeup',
+    keywords=['pipeup', 'pipe', 'stream', 'websockets'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    license='MIT',
+    description='A command line tool for piping to a webpage',
+    long_description=open('README.rst').read(),
+)
