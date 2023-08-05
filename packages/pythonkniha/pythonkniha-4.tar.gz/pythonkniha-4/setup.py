1a7c34b40cd9db838108066f92f7a01fb4c5e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:54 +0000

    Add package pythonkniha/4/pythonkniha-4.tar.gz

---
+++ packages/pythonkniha/pythonkniha-4.tar.gz/pythonkniha-4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonkniha',
+    version='4',
+    author='Ondrej Sika',
+    author_email='ondrej@ondrejsika.com',
+    url='http://ondrejsika.com/books/python-kniha',
+    description='example package',
+    py_modules=['pythonkniha'],
+)
+
