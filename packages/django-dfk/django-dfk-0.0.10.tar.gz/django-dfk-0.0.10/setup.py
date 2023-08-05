ac06210d88012e6ebfebde80d7f3b3f9a575925f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package django-dfk/0.0.9/django-dfk-0.0.10.tar.gz

---
+++ packages/django-dfk/django-dfk-0.0.10.tar.gz/django-dfk-0.0.10/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.0.10'
+
+setup(
+    name='django-dfk',
+    description="Deferred foreign keys for Django",
+    version=version,
+    long_description=open("README.rst").read() + "\n" + open(
+        os.path.join("docs", "HISTORY.rst")).read(),
+    author='Dan Fairs',
+    author_email='dan@fezconsulting.com',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    zip_safe=False,
+    install_requires=[
+        'django>=1.3,<1.8'
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License"
+    ],
+)
