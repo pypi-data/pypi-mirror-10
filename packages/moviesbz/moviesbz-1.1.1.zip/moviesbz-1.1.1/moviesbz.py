c0eed3e0bf1be974a6a22c09040e70ef9d63e96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package moviesbz/1.1.1/moviesbz-1.1.1.zip

---
+++ packages/moviesbz/moviesbz-1.1.1.zip/moviesbz-1.1.1/moviesbz.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''这是moviesbz.py模块，提供了一个movies_item函数，这个函数的作用是递归的打印一个列表。其中有可能包含嵌套列表。'''
+def movies_item (The_list,indent=False,level=0):
+    '''这个函数取一个位置参数，名为The_list.The_list可以是任何一个python列表。indent参数可以控制是否打印制表符，默认为不打印列表中的每个数据项会递归的输出到屏幕上，每个数据项各占一行。'''
+    for each_item in The_list:
+        if isinstance(each_item,list):
+            movies_item(each_item,indent,level+1)
+        else:
+            if indent:
+                for number in range(level):
+                    print '\t',
+            print each_item
+
+
---
+++ packages/moviesbz/moviesbz-1.1.1.zip/moviesbz-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+       name         ='moviesbz',
+       version      ='1.1.1',
+       py_modules   =['moviesbz'],
+       author       ='zhengjunna',
+       author_email ='zhengjunna@wisdomin.com',
+       url          ='http://bingzha.index',
+       description  ='A simple printer of nester lists',
+    )
