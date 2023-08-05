9a2910b6cf1f2b525214f08c9e30efdc00781dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:08 +0000

    Add package diagnosis/2.0.2/diagnosis-2.0.2.zip

---
+++ packages/diagnosis/diagnosis-2.0.2.zip/diagnosis-2.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name	=	'diagnosis',
+    version	=	'2.0.2',
+    py_modules	=	['DiagnosisClass'],
+    author	=	'Daine Huang',
+    author_email	=	'daine199@gmail.com',
+    url	=	'http://hi.baidu.com/huangyif',
+    description	=	'This is a Class that provide people automation run windows program.',
+    )
