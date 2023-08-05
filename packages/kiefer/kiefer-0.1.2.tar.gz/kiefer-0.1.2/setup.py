a7b4831d0a70e419c46e6b675dd04df0d04948ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:32 +0000

    Add package kiefer/0.1.2/kiefer-0.1.2.tar.gz

---
+++ packages/kiefer/kiefer-0.1.2.tar.gz/kiefer-0.1.2/setup.py
@@ -0,0 +1,19 @@
+import pypandoc
+from setuptools import setup
+
+
+requirements = ['requests-oauthlib==0.5.0', ]
+
+long_description = open('README.rst').read()
+
+setup(
+    name='kiefer',
+    version='0.1.2',
+    description='A Python wrapper for the Jawbone UP API.',
+    long_description=long_description,
+    author='Andy Goldschmidt',
+    author_email='andy@abby.io',
+    license='MIT',
+    url='https://github.com/andygoldschmidt/kiefer',
+    install_requires=requirements
+)
