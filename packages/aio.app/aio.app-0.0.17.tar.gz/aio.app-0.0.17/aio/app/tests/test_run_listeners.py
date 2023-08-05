8d85427b37d6a9282f52c7b740be2107263be2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:59 +0000

    Add package aio.app/0.0.17/aio.app-0.0.17.tar.gz

---
+++ packages/aio.app/aio.app-0.0.17.tar.gz/aio.app-0.0.17/aio/app/tests/test_run_listeners.py
@@ -0,0 +1,45 @@
+import asyncio
+
+from aio.testing import aiotest
+from aio.app.testing import AioAppTestCase
+from aio.app.runner import runner
+
+LISTENER_CONFIG = """
+[aio:commands]
+run: aio.app.cmd.cmd_run
+
+[listen:foo]
+test-signal: aio.app.tests._test_listener
+"""
+
+
+class RunListenersTestCase(AioAppTestCase):
+
+    @aiotest
+    def test_run_listeners(self):
+        """
+        on run a listener for "test-signal" should be set up from config file
+        """
+        import aio.app
+
+        class Result:
+            message = None
+        res = Result()
+
+        @asyncio.coroutine
+        def test_listener(signal, resp):
+            res.message = "%s received: %s" % (
+                signal, resp)
+
+        aio.app.tests._test_listener = test_listener
+
+        yield from runner(
+            ['run'],
+            config_string=LISTENER_CONFIG)
+
+        yield from aio.app.signals.emit('test-signal', 'BOOM!')
+
+        self.assertEqual(
+            res.message,
+            'test-signal received: BOOM!')
+        del aio.app.tests._test_listener
