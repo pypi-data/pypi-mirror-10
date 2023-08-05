b67d935695b2a55f7c391977fefcae3e21ee7cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package concept-formation/0.1.1/concept_formation-0.1.1.tar.gz

---
+++ packages/concept-formation/concept_formation-0.1.1.tar.gz/concept_formation-0.1.1/concept_formation/trestle.py
@@ -0,0 +1,45 @@
+from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, division
+from concept_formation.cobweb3 import Cobweb3Tree
+from concept_formation.cobweb3 import Cobweb3Node
+from concept_formation.structure_mapper import structure_map
+
+class TrestleTree(Cobweb3Tree):
+
+    def __init__(self, alpha=0.001, scaling=True):
+        self.root = Cobweb3Node()
+        self.root.root = self.root
+        self.root.alpha = alpha
+        self.root.scaling = scaling
+
+    def ifit(self, instance):
+        """
+        A modification of ifit to call Trestle instead.
+        """
+        return self.trestle(instance)
+
+    def trestle_categorize(self, instance):
+        """
+        The Trestle categorize function, this Trestle categorizes all the
+        sub-components before categorizing itself.
+        """
+        temp_instance = structure_map(self.root, instance)
+        return self.cobweb_categorize(temp_instance)
+
+    def categorize(self, instance):
+        """
+        A categorize function that can be used polymorphicaly without 
+        having to worry about the type of the underlying object.
+
+        In Trestle's case this calls trestle_categorize()
+        """
+        return self.trestle_categorize(instance)
+
+    def trestle(self, instance):
+        """
+        Recursively calls Trestle on all of the components in a depth-first
+        traversal. Once all of the components have been classified then then it
+        classifies the current node.
+        """
+        temp_instance = structure_map(self.root, instance)
+        return self.cobweb(temp_instance)
---
+++ packages/concept-formation/concept_formation-0.1.1.tar.gz/concept_formation-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='concept_formation',
+    version='0.1.1',
+    author='Christopher J. MacLellan, Erik Harpstead',
+    author_email='maclellan.christopher@gmail.com, whitill29@gmail.com',
+    packages=['concept_formation', 
+              'concept_formation.test',
+              'concept_formation.examples'],
+    url='http://pypi.python.org/pypi/concept_formation/',
+    license='LICENSE.txt',
+    description='A library for doing incremental concept formation using algorithms in the COBWEB family',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+)
