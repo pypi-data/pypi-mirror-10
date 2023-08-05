2695aa2015a37138fcd4b85adfd90fc1fdeea3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:21 +0000

    Add package django-secretballot/0.3.0/django-secretballot-0.3.0.tar.gz

---
+++ packages/django-secretballot/django-secretballot-0.3.0.tar.gz/django-secretballot-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-secretballot',
+    version="0.3.0",
+    package_dir={'secretballot': 'secretballot'},
+    packages=['secretballot'],
+    description='Django anonymous voting application',
+    author='James Turk',
+    author_email='jturk@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-secretballot/',
+    long_description=long_description,
+    platforms=["any"],
+    install_requires=[
+        "django",
+        "south",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
