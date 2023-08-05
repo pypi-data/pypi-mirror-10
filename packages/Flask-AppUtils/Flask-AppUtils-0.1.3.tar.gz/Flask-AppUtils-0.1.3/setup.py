72cd9e0d772747fba27399815b250edf6272e1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:04 +0000

    Add package Flask-AppUtils/0.1.3/Flask-AppUtils-0.1.3.tar.gz

---
+++ packages/Flask-AppUtils/Flask-AppUtils-0.1.3.tar.gz/Flask-AppUtils-0.1.3/setup.py
@@ -0,0 +1,60 @@
+"""
+Flask-AppUtils
+--------------
+
+A collection of useful patterns and helpers for Flask applications
+
+install
+-------
+::
+
+    $ pip install flask-apputils
+
+
+modules
+-------
+- `decorators`
+- `filters`
+- `helpers`
+- `routing`
+- `middlewares`
+- `mimes`
+- `sessions.itdangerous_session`
+- `sessions.redis_session`
+- `validators`
+
+
+See: Readme_
+
+.. _Readme: https://github.com/kofrasa/flask-apputils/blob/master/README.md
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-AppUtils',
+    version='0.1.3',
+    license='BSD',
+    author='Francis Asante',
+    author_email='kofrasa@gmail.com',
+    url='http://github.com/kofrasa/flask-apputils',
+    description='A collection of useful patterns and helpers for Flask applications',
+    long_description=__doc__,
+    packages=['flask_apputils'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=['Flask>=0.8'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
