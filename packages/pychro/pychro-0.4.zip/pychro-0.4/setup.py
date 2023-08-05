d26b33fd98b3e94104525bf1ab0047362d9b6c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:51 +0000

    Add package pychro/0.4/pychro-0.4.zip

---
+++ packages/pychro/pychro-0.4.zip/pychro-0.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='pychro',
+      version='0.4',
+      packages=['pychro'],
+      package_data={'pychro':['libpychroc.so', 'PychroCLib.dll']},
+      author='Jon Turner',
+      description='Library to access Chronicle-Queue message journals',
+      url='https://github.com/jontuk/pychro',
+      license='Apache 2.0',
+      classifiers=[ 'Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: Apache Software License',
+                    'Programming Language :: Python :: 3.4' ]
+)
