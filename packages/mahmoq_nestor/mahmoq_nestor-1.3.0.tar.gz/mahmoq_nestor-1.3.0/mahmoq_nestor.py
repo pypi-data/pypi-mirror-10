e421a7df0a31320fbc44ae0d56f6daf001e09d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:19 +0000

    Add package mahmoq_nestor/1.3.0/mahmoq_nestor-1.3.0.tar.gz

---
+++ packages/mahmoq_nestor/mahmoq_nestor-1.3.0.tar.gz/mahmoq_nestor-1.3.0/mahmoq_nestor.py
@@ -0,0 +1,19 @@
+"""
+This is the mahmoq_nestor.py module and it provides one function called print_lol(), which prints lists that may or may not include nested lists
+This is from hf_python
+"""
+
+def print_lol(the_list, indent=False, level=0):
+  """
+  This function takes a positional argument called the_list, which is any python  list. Each data item is the list is (recursively) printed to the screen
+  A second arg is used to insert tab-stops
+  """
+  for each_item in the_list:
+    if isinstance(each_item, list):
+      print_lol(each_item, indent, level+1)
+    else:
+      if indent:
+        for tab_stop in range(level):
+          print("\t", end='')
+        print(each_item)
+
