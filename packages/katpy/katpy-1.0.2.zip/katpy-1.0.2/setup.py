15e4d1a20038c5af94ccacb00a5d9f6526634a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package katpy/1.0.2/katpy-1.0.2.zip

---
+++ packages/katpy/katpy-1.0.2.zip/katpy-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='katpy',
+      version='1.0.2',
+      description='An unofficial Python API for http://kickass.to/',
+      url='https://github.com/s4chin/Katpy',
+      author='Sachin Kelkar',
+      author_email='sachinkel19@gmail.com',
+      license='MIT',
+      py_modules=['kat'])
