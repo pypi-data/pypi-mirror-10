e1362459d6ec5c242b34a129cb4b6bd409088110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package Encry/1.0.1.Dev/Encry-1.0.1.Dev.tar.gz

---
+++ packages/Encry/Encry-1.0.1.Dev.tar.gz/Encry-1.0.1.Dev/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Encry',
+      version='1.0.1.Dev',
+      description='A simple encryption module',
+      url='encry.cpnerd.koding.io/Encry.py',
+      author='CPNerd',
+      author_email='shadow889566@gmail.com',
+      license='MIT',
+      packages=['Encry'],
+      zip_safe=False)
