588c63c48c1ed9c80049e22b6ce27e8de0c2add1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:46 +0000

    Add package uartis/1.0.0/uartis-1.0.0.zip

---
+++ packages/uartis/uartis-1.0.0.zip/uartis-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'uartis',
+    version = '1.0.0',
+    py_modules = ['uartis'],
+    author = 'uartis',
+    author_mail = 'freetime4y@gmail.com',
+    url = 'http://uartis.wordpress.com',
+    description = 'This is my first source',
+    )
