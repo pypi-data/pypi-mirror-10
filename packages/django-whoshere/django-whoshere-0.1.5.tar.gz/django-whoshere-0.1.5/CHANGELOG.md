6491d459fa6a1fee1e1dbf10993d9b3a61434dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:03 +0000

    Add package django-whoshere/0.1.5/django-whoshere-0.1.5.tar.gz

---
+++ packages/django-whoshere/django-whoshere-0.1.5.tar.gz/django-whoshere-0.1.5/CHANGELOG.md
@@ -0,0 +1,19 @@
+#Changelog
+
+##0.1.5
+- Added template tags
+
+###0.1.4
+- Replaced urllib with requests for better ssl support
+
+###0.1.3
+ - Added geolocation with Telize
+
+###0.1.2
+ - Added support for better ip lookups with django-ipware
+
+###0.1.1
+ - Added Tests
+ 
+##0.1.0
+ - Initial commit
---
+++ packages/django-whoshere/django-whoshere-0.1.5.tar.gz/django-whoshere-0.1.5/django_whoshere/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+
+from django_whoshere.apps import TELIZE, GeoIP
+from django_whoshere.models import UserSession
+
+
+class UserSessionAdmin(admin.ModelAdmin):
+    """
+    Admin module for displaying active users
+    """
+    list_display = [
+        'username',
+        'ip',
+        'user_agent',
+        'last_login'
+    ]
+
+    def get_queryset(self, request):
+        """Only show users that have tracking info"""
+        qs = super(UserSessionAdmin, self).get_queryset(request)
+        return qs.filter(pk__in=UserSession.active_user_ids())
+
+    def has_add_permission(self, request, obj=None):
+        """Don't allow adds"""
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        """Read only user link"""
+        return False
+
+    search_fields = ['username', 'first_name', 'last_name']
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields]
+
+    # Add geolocation columns if possible
+    if GeoIP or TELIZE:
+        list_display.insert(2, 'city')
+        list_display.insert(3, 'country')
+
+
+admin.site.register(UserSession, UserSessionAdmin)
