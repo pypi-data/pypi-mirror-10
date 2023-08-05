12ce994fa1fbf8ae8b8fda7506995245a7d18d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:42 +0000

    Add package lyricscreen/0.5.4/lyricscreen-0.5.4.tar.gz

---
+++ packages/lyricscreen/lyricscreen-0.5.4.tar.gz/lyricscreen-0.5.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re, ast
+
+from distutils.core import setup
+
+_version_re = re.compile(r'__version__\s*=\s*(.*)')
+with open('lyricscreen/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+        name="lyricscreen",
+        version=version,
+        description="A lyrics management and display web app and server.",
+        author="Daniel Flanagan",
+        author_email="daniel@lytedev.io",
+        url="https://github.com/lytedev/lyricscreen",
+        license="MIT",
+
+        packages=["lyricscreen"],
+        package_data={'lyricscreen': ['http/*', 'http/**/*']},
+        scripts=["scripts/lyricscreen"],
+        requires=[
+            "asyncio",
+            "websockets",
+            "jsonpickle"
+        ],
+    )
+
