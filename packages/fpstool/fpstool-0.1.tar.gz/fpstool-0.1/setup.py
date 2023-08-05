869968cb851e28360f3fb08d177e45e163cc1ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package fpstool/0.1/fpstool-0.1.tar.gz

---
+++ packages/fpstool/fpstool-0.1.tar.gz/fpstool-0.1/setup.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import fpstool
+from setuptools import setup, find_packages
+
+setup(
+    name='fpstool',
+    version=fpstool.__version__,
+    license="MIT",
+    description='get FPS for android',
+
+    author='codeskyblue',
+    author_email='codeskyblue@gmail.com',
+    url='http://github.com/netease/unknown',
+
+    packages = find_packages(),
+    include_package_data=True,
+    package_data={},
+    install_requires=[
+        'requests',
+        ],
+    # entry_points='''
+    #     [console_scripts]
+    #     fpstool = pyfpstool:main
+    # ''',
+)
