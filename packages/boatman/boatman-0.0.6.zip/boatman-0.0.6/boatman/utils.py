f05115b03c84536b5ec65ce85e00fdc0f9179aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:14 +0000

    Add package boatman/0.0.6/boatman-0.0.6.zip

---
+++ packages/boatman/boatman-0.0.6.zip/boatman-0.0.6/boatman/utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2009-2015 Joao Carlos Roseta Matos
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Utilities."""
+
+# Python 3 compatibility
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# import io  # Python 3 compatibility
+import os
+import sys
+
+# from builtins import input  # Python 3 compatibility
+
+
+def run(win_cmd='', x_cmd=''):
+    """Run OS command."""
+    if sys.platform == 'win32':
+        os.system(win_cmd)
+    else:
+        os.system(x_cmd)
+
+
+if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod(verbose=True)
+    pass
