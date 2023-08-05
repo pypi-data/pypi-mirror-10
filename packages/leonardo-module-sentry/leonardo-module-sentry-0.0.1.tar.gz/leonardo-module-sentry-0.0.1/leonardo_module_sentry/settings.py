90f335eaf8f0b2b0e0db3160534f290ed20ada33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package leonardo-module-sentry/0.0.1/leonardo-module-sentry-0.0.1.tar.gz

---
+++ packages/leonardo-module-sentry/leonardo-module-sentry-0.0.1.tar.gz/leonardo-module-sentry-0.0.1/leonardo_module_sentry/settings.py
@@ -0,0 +1,42 @@
+
+RAVEN_CONFIG = {
+    'dsn': 'http://public:secret@example.com/1',
+}
+
+ERROR_HANDLER_MODULE = "leonardo_module_sentry.urls"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file', ],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
---
+++ packages/leonardo-module-sentry/leonardo-module-sentry-0.0.1.tar.gz/leonardo-module-sentry-0.0.1/leonardo_module_sentry/urls.py
@@ -0,0 +1,15 @@
+
+def handler500(request):
+    """
+    500 error handler which includes ``request`` in the context.
+
+    Templates: `500.html`
+    Context: None
+    """
+    from django.template import Context, loader
+    from django.http import HttpResponseServerError
+
+    t = loader.get_template('500.html')
+    return HttpResponseServerError(t.render(Context({
+        'request': request,
+    })))
