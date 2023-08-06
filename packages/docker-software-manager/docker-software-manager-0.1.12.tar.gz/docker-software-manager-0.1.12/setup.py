8651929fdfb1dadf6d45a402a9a1ea87d7238ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:57 +0000

    Add package docker-software-manager/0.1.12/docker-software-manager-0.1.12.tar.gz

---
+++ packages/docker-software-manager/docker-software-manager-0.1.12.tar.gz/docker-software-manager-0.1.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name='docker-software-manager',
+      version='0.1.12',
+      description='Manage your server software with a combination of docker, systemd, docker-compose, docker-gen and nginx ',
+      url='https://github.com/robomod/docker-software-manager',
+      author='Andreas Ihrig (RoboMod)',
+      author_email='mod.andy@gmx.de',
+      license='GPLv2',
+      packages=['dsm'],
+      install_requires=['docker-compose'],
+      package_data={
+        'dsm': ['defaults.conf', 
+                'docker-gen/install.sh', 
+                'docker-gen/remove.sh', 
+                'docker-gen/update.sh',
+                'docker-gen/docker-gen-nginx.service',
+                'docker-gen/nginx.tmpl',
+                'software-template/docker-compose.yml',
+                'software-template/software.service',
+                'software-template/backups/.placeholder',
+                'software-template/data/.placeholder',]
+        },
+      entry_points = {
+        'console_scripts': ['dsm=dsm.cli:main']
+        }
+      )
