7910c51cef8754343724cd2826c4e3e7dfc6a43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:28 +0000

    Add package CleanerVersion/1.5.1/CleanerVersion-1.5.1.tar.gz

---
+++ packages/CleanerVersion/CleanerVersion-1.5.1.tar.gz/CleanerVersion-1.5.1/versions/static/js/admin_addon.js
@@ -0,0 +1,24 @@
+function update_document() {
+    // for the Source template:
+    (function ($) {
+        if ($('ul').hasClass('object-tools')) {
+            $('input[name="_addanother"]').before('<input formaction="will_not_clone/" type="submit" formmethod="POST" value="Save without cloning">');
+        }
+    })(django.jQuery)
+
+
+}
+
+function make_restore_button() {
+    (function ($) {
+        if ($('ul').hasClass('object-tools')) {
+            if ($('div.field-id p').text() != $('div.field-identity p').text()) {
+                $('input[name="_addanother"]').before('<input formaction="restore/" type="submit" formmethod="POST" value="Restore"');
+            }
+        }
+    })(django.jQuery)
+
+}
+
+// give time to jquery to load..
+setTimeout("update_document();make_restore_button();", 1000);
