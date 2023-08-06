88b896d1b64383f3d67024e582c8c90cd7399703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:00 +0000

    Add package py3o.renderserver/0.5/py3o.renderserver-0.5.zip

---
+++ packages/py3o.renderserver/py3o.renderserver-0.5.zip/py3o.renderserver-0.5/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+start-py3o-renderserver --java=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server/libjvm.so --ure=/usr/lib --office=/usr/lib/libreoffice --driver=juno --sofficeport=8997
