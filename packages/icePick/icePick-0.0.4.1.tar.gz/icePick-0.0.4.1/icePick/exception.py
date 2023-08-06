481d0bd267362af9907c10c0b34c99b925b7f759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:03 +0000

    Add package icePick/0.0.4.1/icePick-0.0.4.1.tar.gz

---
+++ packages/icePick/icePick-0.0.4.1.tar.gz/icePick-0.0.4.1/icePick/exception.py
@@ -0,0 +1,9 @@
+__all__ = ('RecorderException', 'StructureException')
+
+
+class RecorderException(Exception):
+    pass
+
+
+class StructureException(Exception):
+    pass
---
+++ packages/icePick/icePick-0.0.4.1.tar.gz/icePick-0.0.4.1/icePick/order.py
@@ -0,0 +1,61 @@
+import enum
+from .parser import Parser
+
+__all__ = ('Order')
+
+
+class Order:
+    recorder = None
+    parser = None
+
+    class Method(enum.Enum):
+        GET = "GET"
+        POST = "POST"
+        PUT = "POST"
+        DELETE = "POST"
+
+    def __init__(self, url, ua, method=None, charset='utf-8', content_type='text/html'):
+        self.url = url
+        self.user_agent = ua
+        self.method = method
+        self.charset = charset
+        self.content_type = content_type
+
+        self._validate()
+
+    def _validate(self):
+        self.charset = self.charset.lower()
+        self.content_type = self.content_type.lower()
+
+        if not isinstance(self.method, self.Method):
+            self.method = self.Method.GET
+
+    def get_headers(self):
+        headers = {
+            'User-agent': self.user_agent,
+            'Content-Type': "{0}; {1}".format(self.content_type, self.charset)
+        }
+        return headers
+
+    def parse(self, html):
+        if not self.parser:
+            return {}
+
+        parser = self.parser(html)
+        return parser.run()
+
+    def save(self, result):
+        if not self.recorder:
+            return False
+
+        if isinstance(result, list):
+            for value in result:
+                record = self.recorder.new(value)
+                record.save()
+            return True
+
+        elif isinstance(result, dict):
+            record = self.recorder.new(result)
+            return record.save()
+
+        return False
