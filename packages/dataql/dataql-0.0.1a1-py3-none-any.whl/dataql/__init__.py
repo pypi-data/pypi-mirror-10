52d6a9f94b477beb15e594b380f074fbb7a9c6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:28 +0000

    Add package dataql/0.0.1a1/dataql-0.0.1a1-py3-none-any.whl

---
+++ packages/dataql/dataql-0.0.1a1-py3-none-any.whl/dataql/__init__.py
@@ -0,0 +1,8 @@
+"""``dataql`` is for "Data Query Language". It allows to query data in a simple way."""
+
+__author__ = 'Stephane "Twidi" Angel'
+__contact__ = 's.angel@twidi.com'
+__homepage__ = 'https://github.com/twidi/py-dataql'
+
+__version__ = '0.0.1a1'
+VERSION = tuple(__version__.split('.'))
