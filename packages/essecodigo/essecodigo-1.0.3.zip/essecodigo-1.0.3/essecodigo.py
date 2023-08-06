47794df9b0d1d1d7ddd6a4fc59e1fb24e48b9871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:51 +0000

    Add package essecodigo/1.0.3/essecodigo-1.0.3.zip

---
+++ packages/essecodigo/essecodigo-1.0.3.zip/essecodigo-1.0.3/essecodigo.py
@@ -0,0 +1,16 @@
+''' esta função serve para imprimir uma lista que esta dentro de outra lista e
+precisa ser chamada uma só ver por lista mais externa'''
+
+def print_lista(insira_a_lista_aqui,tabulações = 0):
+
+    '''para cada objeto da lista adicionada como argumento da função sera analizado
+    para verificar se é uma lista interna ou não e imprimir o resultado, e o argumento
+    tabulações é a quantidade que você deseja para tabular as informações.'''
+    
+    for lista in insira_a_lista_aqui:
+        if isinstance(lista , list):
+           print_lista(lista, tabulações)
+        else:
+            for parar in range(tabulações):
+                print('\t')
+            print(lista)
---
+++ packages/essecodigo/essecodigo-1.0.3.zip/essecodigo-1.0.3/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+
+setup(
+    name        =   'essecodigo',
+    version     =   '1.0.3',
+    py_modules  =   ['essecodigo'],
+    author      =   'Rynnan',
+    author_email=   'moaberenato@hotmail.com',
+    url         =   '...',
+    description =   'Para imprimir listas internas de uma lista externa',
+    )
