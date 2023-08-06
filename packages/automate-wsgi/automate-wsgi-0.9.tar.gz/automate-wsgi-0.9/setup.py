459dab61d81fa7c8e9b00c63f96db919152dcd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:45 +0000

    Add package automate-wsgi/0.9/automate-wsgi-0.9.tar.gz

---
+++ packages/automate-wsgi/automate-wsgi-0.9.tar.gz/automate-wsgi-0.9/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+install_reqs = parse_requirements('requirements.pip')
+
+def get_version(filename):
+    import re
+    with open(filename) as fh:
+        metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
+        return metadata['version']
+
+setupopts = dict(
+    name="automate-wsgi",
+    version=get_version('automate_wsgi/__init__.py'),
+    packages=find_packages(),
+
+    install_requires=[str(ir.req) for ir in install_reqs],
+
+    author="Tuomas Airaksinen",
+    author_email="tuomas.airaksinen@gmail.com",
+    description="WSGI Support for Automate",
+    long_description="This extension provides Web server for WSGI-aware extensions, "
+                     "such as Remote Procedure Call Support for Automate, Web User Interface for Automate. "
+                     "It is of no use alone.",
+    license="GPL",
+    keywords="automation, GPIO, Raspberry Pi, RPIO, enaml, traits",
+    url="http://github.com/tuomas2/automate_webui",
+    entry_points={},
+
+    classifiers=["Development Status :: 4 - Beta",
+                 "Environment :: Console",
+                 "Environment :: Web Environment",
+                 "Intended Audience :: Education",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Information Technology",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
+                 "Operating System :: Microsoft :: Windows",
+                 "Operating System :: POSIX",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: Scientific/Engineering",
+                 "Topic :: Software Development",
+                 "Topic :: Software Development :: Libraries"]
+)
+
+if __name__ == "__main__":
+    setup(**setupopts)
