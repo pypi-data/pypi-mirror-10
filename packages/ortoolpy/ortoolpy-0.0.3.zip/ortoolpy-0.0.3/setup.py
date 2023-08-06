4227bffd34792be1d755c4b90f8fd22cbc8f942a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:21 +0000

    Add package ortoolpy/0.0.3/ortoolpy-0.0.3.zip

---
+++ packages/ortoolpy/ortoolpy-0.0.3.zip/ortoolpy-0.0.3/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+version = '0.0.3'
+name = 'ortoolpy'
+short_description = '`ortoolpy` is a package for ortoolpy.'
+long_description = """\
+`ortoolpy` is a package for ortoolpy.
+::
+
+   from ortoolpy import knapsack
+   kn = knapsack()
+
+Requirements
+------------
+* Python 2 or Python 3
+
+Features
+--------
+* nothing
+
+Setup
+-----
+::
+
+   $ pip install ortoolpy
+   or
+   $ easy_install ortoolpy
+
+History
+-------
+0.0.1 (2015-6-26)
+~~~~~~~~~~~~~~~~~~
+* first release
+
+"""
+
+classifiers = [
+   "Development Status :: 1 - Planning",
+   "License :: OSI Approved :: Python Software Foundation License",
+   "Programming Language :: Python",
+   "Topic :: Software Development",
+]
+
+setup(
+    name=name,
+    version=version,
+    description=short_description,
+    long_description=long_description,
+    classifiers=classifiers,
+    py_modules=['__init__', 'knapsack'],
+    keywords=['ortoolpy',],
+    author='Saito Tsutomu',
+    author_email='tsutomu@kke.co.jp',
+    url='https://pypi.python.org/pypi/ortoolpy',
+    license='PSFL',
+)
