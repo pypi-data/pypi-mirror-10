b3497f7b86eb1151ad4ae9754264a0399521ff55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:25 +0000

    Add package docker-software-manager/0.1.6/docker_software_manager-0.1.6-py27-none-any.whl

---
+++ packages/docker-software-manager/docker_software_manager-0.1.6-py27-none-any.whl/dsm/docker-gen/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# install docker-gen binary link
+LINK="/usr/local/bin/docker-gen"
+if [ ! -h "$LINK" ]
+then
+    ln -s $BASEDIR/docker-gen $LINK
+fi
+
+# install docker-gen nginx sites link
+LINK="/etc/nginx/sites-enabled/docker-sites"
+TARGET="/etc/nginx/sites-available/docker-sites"
+if [ ! -h "$LINK" ]
+then
+    ln -s $TARGET $LINK
+fi
+
+# install docker-gen nginx systemd service
+#LINK="/etc/systemd/system/docker-gen-nginx.service"
+LINK="/server/utils/docker-gen/docker-gen-nginx.service"
+#if [ ! -h "$LINK" ]
+#then
+    #ln -s $BASEDIR/docker-gen-nginx.service $LINK
+    systemctl enable $LINK
+#fi
---
+++ packages/docker-software-manager/docker_software_manager-0.1.6-py27-none-any.whl/dsm/docker-gen/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+URL="https://github.com/jwilder/docker-gen"
+
+LATEST_VERSION_URL=`curl -s -L -I $URL/releases/latest | grep Location | cut -d' ' -f2`
+LATEST_VERSION=`echo $LATEST_VERSION_URL | rev | cut -d'/' -f1 | rev | sed 's/^[ \t\r]*//g' | sed 's/[ \t\r]*$//g'`
+
+if [ ! -f "$BASEDIR/docker-gen-linux-amd64-$LATEST_VERSION.tar.gz" ]
+then
+    cd $BASEDIR
+    wget $URL/releases/download/$LATEST_VERSION/docker-gen-linux-amd64-$LATEST_VERSION.tar.gz -O 
+    tar xvzf docker-gen-linux-amd64-$LATEST_VERSION.tar.gz
+else
+    echo "Up to date"
+fi
