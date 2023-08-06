780ec4a7deef59edfc12748c5ebc47e4830296bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:42 +0000

    Add package beefapi/0.1/beefapi.tar.gz

---
+++ packages/beefapi/beefapi.tar.gz/beefapi-0.1a/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'beefapi',
+  version = '0.1a',
+  description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
+  author = 'byt3bl33d3r',
+  author_email = 'byt3bl33d3r@gmail.com',
+  url = 'https://github.com/byt3bl33d3r/BeEF-API',
+  keywords = ['api', 'beef', 'BeEF'],
+  py_modules=['beefapi'],
+)
