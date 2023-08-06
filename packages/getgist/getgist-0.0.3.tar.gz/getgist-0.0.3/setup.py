14a67178f61c5845b562b7804cff40098ba2fac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:53 +0000

    Add package getgist/0.0.3/getgist-0.0.3.tar.gz

---
+++ packages/getgist/getgist-0.0.3.tar.gz/getgist-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+url = 'http://github.com/cuducos/getgist'
+
+setup(author='Eduardo Cuducos',
+      author_email='cuducos@gmail.com',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Intended Audience :: Developers',
+                   'Topic :: Utilities',
+                   'License :: Public Domain'],
+      description='Command-line to update local files from GitHub gists',
+      include_package_data=True,
+      keywords='gist, command-line, github, dotfiles',
+      license='Public Domain',
+      long_description='Check `GetGist at GitHub <{}>`_.'.format(url),
+      name='getgist',
+      package_dir={'getgist': '.'},
+      packages=['getgist'],
+      scripts=['bin/getgist'],
+      url=url,
+      version='0.0.3',
+      zip_safe=False)
