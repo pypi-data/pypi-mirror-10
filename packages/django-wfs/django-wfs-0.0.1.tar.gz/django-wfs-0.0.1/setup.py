def1b6f027c4fc4c413e74b862379fd845180cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:10 +0000

    Add package django-wfs/0.0.1/django-wfs-0.0.1.tar.gz

---
+++ packages/django-wfs/django-wfs-0.0.1.tar.gz/django-wfs-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='django-wfs',
+    packages=['django-wfs'],
+    version='0.0.1',
+    description='A WFS (web feature service) implementation as a Django application.',
+    author='Vasco Pinho',
+    author_email='vascogpinho@gmail.com',
+    url='https://github.com/vascop/django-wfs',
+    download_url='https://github.com/vascop/django-wfs/tarball/master',
+    long_description=open('README.md', 'r').read(),
+    license='Apache 2.0',
+    keywords=['wfs', 'geo', 'django'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+    ],
+)
