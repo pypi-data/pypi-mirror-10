e58d25020cf538404509b6666beb6672e50f2586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:21 +0000

    Add package grafilter/0.3.1/grafilter-0.3.1-py3-none-any.whl

---
+++ packages/grafilter/grafilter-0.3.1-py3-none-any.whl/grafilter/influxdb.py
@@ -0,0 +1,106 @@
+from datetime import datetime, timedelta
+
+import requests
+
+from .utils import build_id, simplify_keys
+
+
+class InfluxDBBackend(object):
+    def __init__(self, config):
+        self._config = config
+
+    def metrics(self):
+        response = requests.get(
+            self._config['INFLUXDB_URL'] + "/query",
+            params={
+                'db': self._config['INFLUXDB_DB'],
+                'q': "SHOW SERIES",
+            },
+        )
+        result = []
+        response_json = response.json()['results'][0]
+        for series in response_json.get('series', []):
+            for tag_values in series['values']:
+                tag_dict = dict(zip(series['columns'], tag_values))
+                for ignored_tag in self._config['IGNORED_TAGS']:
+                    try:
+                        del tag_dict[ignored_tag]
+                    except KeyError:
+                        pass
+                result.append((
+                    build_id(series['name'], tag_dict),
+                    {
+                        'base_name': series['name'],
+                        'tags': tag_dict,
+                    },
+                ))
+        return sorted(result)
+
+    def metric(
+        self,
+        base_name,
+        tags,
+        period=None,
+        resolution=80,
+        start=None,
+        merge=None,
+        transform=None,
+    ):
+        if merge is not None:
+            resolution += 1
+        if period is None:
+            period = timedelta(3600)
+        if start is not None:
+            end = start + period
+            timespec = "time > '{start}' AND time < '{end}'".format(
+                end=end.strftime("%Y-%m-%d %H:%M:%S"),
+                start=start.strftime("%Y-%m-%d %H:%M:%S"),
+            )
+        else:
+            timespec = "time > now() - {}s".format(int(period.total_seconds()))
+
+        tag_filter = ""
+        for key, value in tags.items():
+            tag_filter += " and \"{}\" = '{}'".format(key, value)
+
+        tick = int(period.total_seconds() / resolution)
+
+        response = requests.get(
+            self._config['INFLUXDB_URL'] + "/query",
+            params={
+                'db': self._config['INFLUXDB_DB'],
+                'q': "SELECT mean(value) from \"{base_name}\" "
+                     "WHERE {timespec}{tag_filter} GROUP BY time({tick}s), *".format(
+                    base_name=base_name,
+                    tag_filter=tag_filter,
+                    timespec=timespec,
+                    tick=tick,
+                ),
+            },
+        )
+        series_list = response.json()['results'][0]['series']
+        data = {}
+
+        for series in series_list:
+            series_id = build_id(series['name'], series.get('tags', {}))
+            data[series_id] = []
+            prev_value = None
+            for time, value in series['values']:
+                if merge is not None and value is not None:
+                    if prev_value is None:
+                        prev_value = value
+                        continue
+                    prev_value, value = value, merge(prev_value, value, tick)
+                if transform is None:
+                    data[series_id].append(value)
+                else:
+                    data[series_id].append(transform(value))
+
+        simplify_keys(data)
+
+        data['x'] = [
+            int(datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ").timestamp() * 1000)
+            for time, value in series_list[0]['values']
+        ]
+
+        return data
---
+++ packages/grafilter/grafilter-0.3.1-py3-none-any.whl/grafilter/utils.py
@@ -0,0 +1,136 @@
+from copy import copy
+from datetime import datetime, timezone
+from urllib.parse import quote_plus, unquote_plus
+
+from parsedatetime import Calendar
+
+
+calendar = Calendar()
+
+
+def build_id(base_name, tag_dict):
+    """
+    Turns a metric name and a dict of tags into a canonical name for
+    that series.
+
+    base_name: "cpu_load"
+    tag_dict: {'host': "foo.example.com", 'region': "us-west"}
+
+    becomes
+
+    "cpu_load/host:foo.example.com/region:us-west"
+    """
+    result = quote(base_name)
+    for key in sorted(tag_dict.keys()):
+        result += "/{}:{}".format(quote(key), quote(str(tag_dict[key])))
+    return result
+
+
+def build_reduced_ids(base_name, tag_dict):
+    """
+    Returns a dict mapping tag keys to metric IDs without that tag.
+    """
+    result = {}
+    for key in tag_dict.keys():
+        reduced_tag_dict = copy(tag_dict)
+        del reduced_tag_dict[key]
+        result[key] = build_id(base_name, reduced_tag_dict)
+    return result
+
+
+def quote(s):
+    # https://github.com/mitsuhiko/flask/issues/900
+    # http://www.leakon.com/archives/865
+    return quote_plus(quote_plus(s))
+
+
+def unquote(s):
+    return unquote_plus(unquote_plus(s))
+
+
+def parse_datetime(s):
+    if not s:
+        return None
+    return calendar.parseDT(s, sourceTime=datetime.now(timezone.utc), tzinfo=timezone.utc)[0]
+
+
+def parse_id(metric_id):
+    """
+    This is the reverse of build_id().
+    """
+    tokens = metric_id.split("/")
+    base_name = unquote(tokens.pop(0))
+    tags = {}
+    for token in tokens:
+        key, value = token.split(":", 1)
+        tags[unquote(key)] = unquote(value)
+    return base_name, tags
+
+
+def parse_timedelta(s):
+    return calendar.parseDT(
+        s,
+        sourceTime=datetime.now(timezone.utc),
+        tzinfo=timezone.utc,
+    )[0] - datetime.now(timezone.utc)
+
+
+def search_string(query, candidate):
+    candidate = candidate.lower()
+    for token in query.split():
+        if token.lower() not in candidate:
+            return False
+    return True
+
+
+def simplify_keys(d):
+    """
+    Given a dictionary with metric IDs as keys, this will try to trim
+    down the keys as much as possible by removing information present
+    in all keys. Will modify the dictionary in place.
+
+    input: {
+        "foo/bar:baz/crackle:pop": None,
+        "foo/bar:wiggle/crackle:snap": None,
+    }
+
+    output: {
+        "baz pop": None,
+        "wiggle snap": None,
+    }
+    """
+    base_names = {}
+    normalized_keys = {}
+    for key in sorted(d.keys()):
+        base_name, tags = parse_id(key)
+        tags[""] = base_name
+        base_names[key] = base_name
+        normalized_keys[key] = tags
+
+    keys_to_remove = set()
+
+    for key, tags in normalized_keys.items():
+        for tag, value in tags.items():
+            found_mismatch = False
+            for _, candidate_tags in normalized_keys.items():
+                if candidate_tags[tag] != value:
+                    found_mismatch = True
+                    break
+            if not found_mismatch:
+                keys_to_remove.add(tag)
+
+    for tags in normalized_keys.values():
+        for key in keys_to_remove:
+            try:
+                del tags[key]
+            except KeyError:
+                pass
+
+    for original_key, tags in normalized_keys.items():
+        new_key = ""
+        for key in sorted(tags.keys()):
+            new_key += tags[key] + " "
+        new_key = new_key.strip()
+        if not new_key:
+            new_key = base_names[original_key]
+        d[new_key] = d.pop(original_key)
