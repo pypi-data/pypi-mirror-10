df62b4b0425721e8ad724689aa409d9405dca631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:33 +0000

    Add package turbosmsua/0.2/turbosmsua-0.2.tar.gz

---
+++ packages/turbosmsua/turbosmsua-0.2.tar.gz/turbosmsua-0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+  name = 'turbosmsua',
+  packages = ['turbosmsua'],
+  version = '0.2',
+  description = 'Client for https://turbosms.ua',
+  author = 'Serbokryl Oleg',
+  author_email = 'chezar1995@gmail.com',
+  url = 'https://github.com/Krokop/turbosmsua',
+  download_url = 'https://github.com/Krokop/turbosmsua/tarball/0.2',
+  keywords = ['turbosmsua', 'turbosms', 'sms ua'],
+  classifiers = [],
+  # install_requires = ["suds-jurko >= 0.6",],
+)
