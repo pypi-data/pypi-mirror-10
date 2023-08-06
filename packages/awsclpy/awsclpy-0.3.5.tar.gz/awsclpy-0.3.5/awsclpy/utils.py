c3cf3d28c5a3de7546444405ee956617823864a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:40 +0000

    Add package awsclpy/0.3.5/awsclpy-0.3.5.tar.gz

---
+++ packages/awsclpy/awsclpy-0.3.5.tar.gz/awsclpy-0.3.5/awsclpy/utils.py
@@ -0,0 +1,22 @@
+import os
+import collections
+from six import string_types
+
+def log(log, time, logdir):
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+
+    date = time.strftime('%Y%m%d')
+    time = time.strftime('%H%M%S')
+
+    with open('%s/%s' % (logdir, date), 'a') as logfile:
+        logfile.write("%s_%s: %s\n" % (date, time, log))
+
+# By Cristian http://stackoverflow.com/a/2158532/227992
+def flatten(l):
+    for el in l:
+        if isinstance(el, collections.Iterable) and not isinstance(el, string_types):
+            for sub in flatten(el):
+                yield sub
+        else:
+            yield str(el)
---
+++ packages/awsclpy/awsclpy-0.3.5.tar.gz/awsclpy-0.3.5/setup.py
@@ -0,0 +1,39 @@
+import pip
+from setuptools import setup
+
+setup(name='awsclpy',
+      version='0.3.5',
+      description='Chain AWSCLI commands in Python.',
+      long_description='Run AWSCLI commands and use their outputs in next ' +
+      'commands.',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Development Status :: 4 - Beta',
+      ],
+      keywords=[
+          'awsclpy',
+          'awscli',
+          'aws',
+          'amazon web services',
+          'command line interface',
+      ],
+      author='Hamid Nazari',
+      author_email='hn@linux.com',
+      maintainer='Hamid Nazari',
+      maintainer_email='hn@linux.com',
+      url='http://github.com/hamidnazari/awsclpy',
+      license='MIT',
+      packages=['awsclpy'],
+      install_requires=[
+        'awscli==1.7.36',
+        'six==1.9.0'
+      ],
+      zip_safe=False)
