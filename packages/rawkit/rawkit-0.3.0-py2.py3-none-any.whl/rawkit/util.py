358eb70edac2f74cc7b3f219a23a487730e590b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package rawkit/0.3.0/rawkit-0.3.0-py2.py3-none-any.whl

---
+++ packages/rawkit/rawkit-0.3.0-py2.py3-none-any.whl/rawkit/util.py
@@ -0,0 +1,43 @@
+import ctypes
+import os
+
+from libraw.bindings import LibRaw
+
+
+def discover(path):
+    """
+    Recursively search for raw files in a given directory.
+    :param path: the directory to recursively search
+    :type path: :class:`str`
+    """
+    file_list = []
+    libraw = LibRaw()
+    raw = libraw.libraw_init(0)
+
+    for root, _, files in os.walk(path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name).encode('ascii')
+            if libraw.libraw_open_file(raw, file_path) == 0:
+                file_list.append(file_path)
+            libraw.libraw_recycle(raw)
+
+    libraw.libraw_close(raw)
+    return file_list
+
+
+def camera_list():
+    """
+    Return a list of cameras which are supported by the currently linked
+    version of LibRaw.
+
+    :returns: A list of supported cameras
+    :rtype: :class:`str tuple`
+    """
+
+    libraw = LibRaw()
+    print(libraw)
+    libraw.libraw_cameraList.restype = ctypes.POINTER(
+        ctypes.c_char_p * libraw.libraw_cameraCount()
+    )
+    data_pointer = libraw.libraw_cameraList()
+    return [x.decode('ascii') for x in data_pointer.contents]
