d3b58e50a1912d77f566bc60dadacb7158c379b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:50 +0000

    Add package onegov.core/0.4.4/onegov.core-0.4.4-py2-none-any.whl

---
+++ packages/onegov.core/onegov.core-0.4.4-py2-none-any.whl/onegov/core/tests/test_layout.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from sedate import replace_timezone
+from onegov.core.layout import Layout
+
+
+def test_chunks():
+    layout = Layout(model=object(), request=object())
+    assert list(layout.chunks('ABCDEFG', 3, 'x')) == [
+        ('A', 'B', 'C'),
+        ('D', 'E', 'F'),
+        ('G', 'x', 'x')
+    ]
+
+
+def test_format_date():
+    layout = Layout(model=object(), request=object())
+
+    dt = replace_timezone(datetime(2015, 6, 17, 12, 0), 'Europe/Zurich')
+
+    assert layout.format_date(dt, 'datetime') == '17.06.2015 12:00'
+    assert layout.format_date(dt, 'date') == '17.06.2015'
+    assert layout.format_date(dt.date(), 'date') == '17.06.2015'
+    assert layout.format_date(dt, 'time') == '12:00'
+    assert layout.format_date(dt.time(), 'time') == '12:00'
+
+
+def test_format_number():
+
+    layout = Layout(model=object(), request=object())
+
+    assert layout.format_number(100) == "100"
+    assert layout.format_number(100, 2) == "100.00"
+    assert layout.format_number(1000) == "1'000"
+    assert layout.format_number(1000.00) == "1'000.00"
+    assert layout.format_number(1000.00, 0) == "1'000"
