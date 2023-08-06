b9bb21bd6169a3191aa4dfe041c01d8132d7567c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package django-idmap/0.3.3/django-idmap-0.3.3.zip

---
+++ packages/django-idmap/django-idmap-0.3.3.zip/django-idmap-0.3.3/idmap/manager.py
@@ -0,0 +1,14 @@
+from django.db.models.manager import Manager
+
+from .queryset import SharedMemoryQuerySet
+
+
+class SharedMemoryManager(Manager):
+
+    use_for_related_fields = True
+
+    def get_queryset(self):
+        return SharedMemoryQuerySet(self.model, using=self._db)
+
+    def get_query_set(self):
+        return self.get_queryset()
