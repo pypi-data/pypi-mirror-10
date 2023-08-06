eeebf4412038c66ace7a6b902b4a56fbce626569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:39 +0000

    Add package ohh/0.3.4/ohh-0.3.4.tar.gz

---
+++ packages/ohh/ohh-0.3.4.tar.gz/ohh-0.3.4/ohh/shell_completion.py
@@ -0,0 +1,34 @@
+INIT_FILE = """COMPLETION_DIR="$HOME/.ohh/zsh/completions"
+fpath+=$COMPLETION_DIR
+compinit
+"""
+
+COMPLETION_FILE = """#compdef ohh
+#autoload
+
+local -a _1st_arguments
+
+_1st_arguments=(
+  'login:Login to the server'
+  'start:Start a working session'
+  'stop:Stop a working session'
+  'status:Check the current status'
+)
+
+local expl
+
+_arguments \
+  '(-v --version)'{-v,--version}'[show version]' \
+  '(-h --help)'{-h,--help}'[show help]' \
+  '*:: :->subcmds' && return 0
+
+if (( CURRENT == 1 )); then
+  _describe -t commands "ohh subcommand" _1st_arguments
+  return
+fi
+"""
+
+INIT_CONTENT = """
+# added by ohh setup
+[[ -f $HOME/.ohh/zsh/ohh-init.sh ]] && . $HOME/.ohh/zsh/ohh-init.sh
+"""
