923f6d16911bd430a0dd3dd8865896fdec790a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:09 +0000

    Add package django-time-logger/0.0.5/django-time-logger-0.0.5.tar.gz

---
+++ packages/django-time-logger/django-time-logger-0.0.5.tar.gz/django-time-logger-0.0.5/setup.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# python setup.py sdist register upload
+from distutils.core import setup
+
+setup(
+    name='django-time-logger',
+    version='0.0.5',
+    description='Time logger for django',
+    author='Telminov Sergey',
+    url='https://github.com/telminov/django-time-logger',
+    packages=[
+        'time_logger',
+        'time_logger/middleware',
+    ],
+    include_package_data=True,
+    license='The MIT License',
+)
