210e50e0250caeef0655c7f562e54dd7aa926966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:16 +0000

    Add package striplog/0.5.2/striplog-0.5.2.tar.gz

---
+++ packages/striplog/striplog-0.5.2.tar.gz/striplog-0.5.2/striplog/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf 8 -*-
+"""
+==================
+striplog
+==================
+"""
+from .legend import Decor, Legend
+from .interval import Interval
+from .component import Component
+from .rock import Rock  # Deprecated v 1.5
+from .well import Well
+from .striplog import Striplog
+from .lexicon import Lexicon
+
+__all__ = ['Lexicon',
+           'Component',
+           'Rock',
+           'Decor',
+           'Legend',
+           'Interval',
+           'Well',
+           'Striplog']
