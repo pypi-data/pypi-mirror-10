b9e8727dc10e97ab823a3cd585e6bd3fb16f970f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:00 +0000

    Add package algopy/0.5.3/algopy-0.5.3.zip

---
+++ packages/algopy/algopy-0.5.3.zip/algopy-0.5.3/algopy/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM ALGOPY SETUP.PY
+short_version='0.5.3'
+version='0.5.3'
+release=True
+
+if not release:
+    version += '.dev2386d237891ea5761845029601b0fe35ff4c8fd6'
