1a30d50b69d93ba00f187a743681405005d54835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:28 +0000

    Add package finsky/0.1.0/finsky-0.1.0.tar.gz

---
+++ packages/finsky/finsky-0.1.0.tar.gz/finsky-0.1.0/README.rst
@@ -0,0 +1,19 @@
+finsky
+======
+
+Google Play API
+
+Why the name?
+-------------
+
+The word ``finsky`` appears a lot in the Google Play APK source code, so I
+guessed it has some internal significance.
+
+Acknowledgments
+---------------
+
+-  `googleplay-api <https://github.com/egirault/googleplay-api>`__
+-  `django-googleplay-api <https://github.com/gotlium/django-googleplay-api>`__
+-  `gpsoauth <https://github.com/simon-weber/gpsoauth>`__
+-  Dima Kovalenko's `reverse engineering of
+   EncryptedPasswd <http://codedigging.com/blog/2014-06-09-about-encryptedpasswd/>`__
---
+++ packages/finsky/finsky-0.1.0.tar.gz/finsky-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import re
+
+pkg_init_file = open('finsky/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_init_file))
+
+def readme():
+    return open('./README.rst').read()
+
+setup(name='finsky',
+      version=metadata['version'],
+      description='Google Play API',
+      long_description=readme(),
+      url='https://github.com/mmcloughlin/finsky',
+      author='Michael McLoughlin',
+      author_email='mmcloughlin@gmail.com',
+      license='MIT',
+      packages=['finsky'],
+      install_requires=[
+          'requests',
+          'pydash',
+          'protobuf',
+          ],
+      )
