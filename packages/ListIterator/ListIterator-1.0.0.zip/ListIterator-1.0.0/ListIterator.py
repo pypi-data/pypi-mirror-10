e4f950edf6e722af2d479e079a83cb568335ef15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:42 +0000

    Add package ListIterator/1.0.0/ListIterator-1.0.0.zip

---
+++ packages/ListIterator/ListIterator-1.0.0.zip/ListIterator-1.0.0/ListIterator.py
@@ -0,0 +1,12 @@
+"""Code to iterate through nested lists"""
+
+"""Takes the list as input and recursively checks if it contains
+nested list and prints its elements to console"""
+
+def processList(inputList):
+	for each_item in inputList:
+		if isinstance(each_item,list):
+			processList(each_item)
+		else:
+			print(each_item)
+ 
