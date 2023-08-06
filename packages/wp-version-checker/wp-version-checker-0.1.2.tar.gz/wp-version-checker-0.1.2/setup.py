cdb32112c907b7ab6ba8d19b8881d5cea5002191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package wp-version-checker/0.1.2/wp-version-checker-0.1.2.tar.gz

---
+++ packages/wp-version-checker/wp-version-checker-0.1.2.tar.gz/wp-version-checker-0.1.2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+setup(
+    name = "wp-version-checker",
+    version = "0.1.2",
+    packages = find_packages(),
+    scripts = ['wp_version_checker.py'],
+    author = "Dundee",
+    author_email = "daniel@milde.cz",
+    description = "Wordpress version checker",
+    license = "GPL",
+    keywords = "wordpress",
+    url = "https://github.com/Dundee/wp-version-checker",
+    long_description = read_md('README.md'),
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+    ]
+)
