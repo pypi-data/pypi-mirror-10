17c89fece6280cae4be44bb63d96eaa91627f1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:38 +0000

    Add package manila-ui/1.0.1/manila_ui-1.0.1-py2-none-any.whl

---
+++ packages/manila-ui/manila_ui-1.0.1-py2-none-any.whl/manila_ui/enabled/_90_manila_admin_shares.py
@@ -0,0 +1,7 @@
+PANEL_DASHBOARD = 'admin'
+PANEL_GROUP = 'admin'
+PANEL = 'shares'
+ADD_PANEL = 'manila_ui.dashboards.admin.shares.panel.Shares'
+UPDATE_HORIZON_CONFIG = {
+    'customization_module': 'manila_ui.overrides',
+}
