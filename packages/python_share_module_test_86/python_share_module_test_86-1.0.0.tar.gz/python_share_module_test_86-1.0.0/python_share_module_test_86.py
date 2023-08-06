986559194891cb6d27e55ca760833e57a06e10db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:15 +0000

    Add package python_share_module_test_86/1.0.0/python_share_module_test_86-1.0.0.tar.gz

---
+++ packages/python_share_module_test_86/python_share_module_test_86-1.0.0.tar.gz/python_share_module_test_86-1.0.0/python_share_module_test_86.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print('module share tool test!')
+print('idle editor test!')
---
+++ packages/python_share_module_test_86/python_share_module_test_86-1.0.0.tar.gz/python_share_module_test_86-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'python_share_module_test_86',
+	version 	= '1.0.0',
+	py_modules	= ['python_share_module_test_86'],
+	author		= 'joey',
+	author_email	= 'black_pho@qq.com',
+	url		= 'http://312300477@qq.com',
+	description	= 'A test for python\'s module share test',
+)	
