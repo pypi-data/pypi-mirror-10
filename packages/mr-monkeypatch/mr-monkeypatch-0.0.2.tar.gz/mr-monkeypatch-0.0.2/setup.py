f0c2c065fd67148a8a66030679414022249e1dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:15 +0000

    Add package mr-monkeypatch/0.0.2/mr-monkeypatch-0.0.2.tar.gz

---
+++ packages/mr-monkeypatch/mr-monkeypatch-0.0.2.tar.gz/mr-monkeypatch-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(
+    name="mr-monkeypatch",
+    version="0.0.2",
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    description="A monkey patching library for python",
+    author='phoeagon',
+    author_email='admin@phoeagon.info',
+    url='https://github.com/phoeagon/monkeypatch-python',
+    download_url='https://github.com/phoeagon/monkeypatch-python/tarball/0.0.2',
+    packages = ['monkeypatch'],
+    package_dir = {'monkeypatch': 'src'},
+    test_suite = 'monkeypatch.monkeypatch_test',
+    package_data={
+        'monkeypatch': ['README', 'LICENSE']
+    },
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    long_description="""Provide dynamic method name resolution and routing
+    simulates monkey patching in Ruby.""",
+)
---
+++ packages/mr-monkeypatch/mr-monkeypatch-0.0.2.tar.gz/mr-monkeypatch-0.0.2/src/monkeypatch.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# Filename: monkeypatch_test.py
+
+import re
+import logging
+from collections import defaultdict
+try:
+    import exceptions
+except:
+    exceptions = type('Exception', (), {'AttributeError': AttributeError})
+
+__resolve__ = defaultdict(dict)
+__pending_resolve__ = {}
+
+
+def _bypass(attr):
+    return attr in ['cls', 'path', 'obj'] or attr.startswith('__')
+
+
+class _ProxyClass(object):
+
+    def __init__(self, cls, obj, path=''):
+        self.__mp_obj = obj
+        self.__mp_cls = cls
+        self.__mp_path = path
+
+    def __call__(self, *args, **kwargs):
+        for cls in self.__mp_cls.mro():
+            for path, func in __resolve__[cls].items():
+                m = re.match(path, self.__mp_path)
+                if m:
+                    args = (self.__mp_obj,) + m.groups() + args
+                    return func(*args, **kwargs)
+        # Fail to resolve
+        # Method invocation is never a statement, so eval is save
+        parsed_path = self.__mp_path.split('.')
+        attr = object.__getattribute__(self.__mp_obj, parsed_path[1])
+        retval = eval('attr' + '.'.join(parsed_path[2:]) + '(*args, **kwargs)')
+        return retval
+
+    def __getattribute__(self, attr):
+        try:
+            return object.__getattribute__(self, attr)
+        except exceptions.AttributeError:
+            return _ProxyClass(self.__mp_cls, self.__mp_obj, self.__mp_path + '.' + attr)
+
+
+def monkeypatch(cls):
+    """Class decorator.
+    """
+    global __resolve__, __pending_resolve__
+    __resolve__[cls] = __pending_resolve__
+    __pending_resolve__ = {}
+
+    def getattribute(self, attr):
+        try:
+            return object.__getattribute__(self, attr)
+        except exceptions.AttributeError:
+            return _ProxyClass(cls, self, attr)
+
+    return type(cls.__name__, (cls,), {
+        '__getattribute__': getattribute,
+        '__original_getattribute__': cls.__getattribute__,
+    })
+
+
+def route(path):
+    """Return a decorator.
+    """
+    def decorator(func):
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
+        global __pending_resolve__
+        __pending_resolve__[path] = func
+        return inner
+    return decorator
