045a00d6501b5fce1ac62dfbc4ea4c8216c23497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:27 +0000

    Add package devilry/2.1.0-beta.015/devilry-2.1.0-beta.015.tar.gz

---
+++ packages/devilry/devilry-2.1.0-beta.015.tar.gz/devilry-2.1.0-beta.015/devilry/apps/core/templatetags/devilry_core_tags.py
@@ -0,0 +1,39 @@
+import bleach
+from django import template
+from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
+
+
+@register.filter
+def devilry_user_displayname(user):
+    if not user:
+        return ''
+    return user.devilryuserprofile.full_name or user.username
+
+
+@register.filter
+def format_is_passing_grade(is_passing_grade):
+    if is_passing_grade:
+        return _('passed')
+    else:
+        return _('failed')
+
+
+@register.filter
+def devilry_feedback_shortformat(staticfeedback):
+    if not staticfeedback:
+        return ''
+    if staticfeedback.grade in ('Passed', 'Failed'):
+        return staticfeedback.grade
+    else:
+        return u'{} ({})'.format(
+            staticfeedback.grade,
+            format_is_passing_grade(staticfeedback.is_passing_grade))
+
+@register.filter
+def devilry_escape_html(html):
+    """
+    Escape all html in the given ``html``.
+    """
+    return bleach.clean(html)
