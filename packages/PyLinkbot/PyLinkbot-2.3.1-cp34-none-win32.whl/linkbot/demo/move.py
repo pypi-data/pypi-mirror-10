d41b0bcd8fbfab0ca4c8d30b888e02aff1b784d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:07 +0000

    Add package PyLinkbot/2.3.1/PyLinkbot-2.3.1-cp34-none-win32.whl

---
+++ packages/PyLinkbot/PyLinkbot-2.3.1-cp34-none-win32.whl/linkbot/demo/move.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import linkbot
+import time
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print ("Usage: {0} <Linkbot Serial ID>".format(sys.argv[0]))
+        quit()
+    serialID = sys.argv[1]
+    myLinkbot = linkbot.Linkbot(serialID)
+    myLinkbot.connect()
+
+    myLinkbot.move(400, 90, 90)
+    myLinkbot.resetToZero()
+    myLinkbot.move(-90, -90, -90)
+    myLinkbot.move(90, 90, 90)
+    myLinkbot.move(-90, -90, -90)
+
