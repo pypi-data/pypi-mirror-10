a1a2717cc21e24c4ebc7d1c1c648140b1c555cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:31 +0000

    Add package LogGabor/0.2/LogGabor-0.2.tar.gz

---
+++ packages/LogGabor/LogGabor-0.2.tar.gz/LogGabor-0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+
+NAME = "LogGabor"
+version = "0.2"
+
+setup(
+    name = NAME,
+    version = version,
+    packages=find_packages('src', exclude='docs'),
+    package_dir = {'': 'src'},
+    author = "Laurent Perrinet INT - CNRS",
+    author_email = "Laurent.Perrinet@univ-amu.fr",
+    description = "A log-Gabor pyramid is an oriented multiresolution scheme for images inspired by biology.",
+    long_description=open("README.md").read(),
+    license = "GPLv2",
+    keywords = ('computational neuroscience', 'simulation', 'analysis', 'visualization', 'computer vision'),
+    url = 'https://github.com/meduz/' + NAME, # use the URL to the github repo
+    download_url = 'https://github.com/meduz/' + NAME + '/tarball/' + version,
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: POSIX',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Utilities',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                  ],
+     )
