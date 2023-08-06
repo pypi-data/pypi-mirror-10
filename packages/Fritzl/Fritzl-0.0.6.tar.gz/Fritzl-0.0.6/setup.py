ed9b4977e1e17f965bf02f196dbc144c2d5378a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:29 +0000

    Add package Fritzl/0.0.6/Fritzl-0.0.6.tar.gz

---
+++ packages/Fritzl/Fritzl-0.0.6.tar.gz/Fritzl-0.0.6/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Fritzl',
+      version='0.0.6',
+      description='Control Fritz AVM DECT200',
+      author='Mathias Perlet',
+      author_email='mathias@mperlet.de',
+      url='https://github.com/mperlet/fritzl',
+      packages=['Fritzl'],
+      keywords = ['avm', 'dect200', 'fritzbox', 'dect', 'switch', 'smart home'], 
+     )
