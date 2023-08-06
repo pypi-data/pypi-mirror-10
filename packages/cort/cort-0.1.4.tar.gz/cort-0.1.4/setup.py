54aff090195193801124b8e828da442d963118a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:57 +0000

    Add package cort/0.1.4/cort-0.1.4.tar.gz

---
+++ packages/cort/cort-0.1.4.tar.gz/cort-0.1.4/setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+
+
+setup(
+    name='cort',
+    version='0.1.4',
+    packages=['cort',
+              'cort.analysis',
+              'cort.core',
+              'cort.test',
+              'cort.coreference',
+              'cort.test.multigraph',
+              'cort.test.analysis',
+              'cort.test.core',
+              'cort.coreference.multigraph',
+              'cort.coreference.approaches',
+              'cort.util'],
+
+    url='http://github.com/smartschat/cort',
+    license='MIT',
+    author='Sebastian Martschat, Thierry Goeckel',
+    author_email='sebastian.martschat@gmail.com',
+    description='A coreference resolution research toolkit.',
+    keywords = ['NLP', 'CL', 'natural language processing',
+                'computational linguistics', 'coreference resolution',
+                'text analytics'],
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Text Processing',
+        ],
+    install_requires=['nltk', 'numpy', 'matplotlib', 'mmh3', 'future'],
+    package_data={
+        'cort': ['analysis/visualization/style.css',
+                 'analysis/visualization/lib/*',
+                 'resources/*']
+    },
+    scripts=['bin/cort-train', 'bin/cort-predict', 'bin/run-multigraph'],
+)
