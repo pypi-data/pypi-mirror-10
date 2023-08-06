eae90ba4426131a2b83f83cc7c10d3144faae8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:34 +0000

    Add package phjs-manager/0.0.27/phjs-manager-0.0.27.tar.gz

---
+++ packages/phjs-manager/phjs-manager-0.0.27.tar.gz/phjs-manager-0.0.27/app/docker_adapter.py
@@ -0,0 +1,21 @@
+from docker import Client
+from docker.utils import utils
+
+
+class DockerAdapter:
+    """Adapter for docker api"""
+    def __init__(self, base_url):
+        self.dockerClient = Client(base_url=base_url)
+
+    def run_container(self, image_path, run_command, port_bind):
+        container = self.dockerClient.create_container(
+            image=image_path,
+            command=run_command, detach=True,
+            host_config=utils.create_host_config(
+                network_mode="host"
+            )
+        )
+        self.dockerClient.start(container=container['Id'])
+
+    def get_nodes(self):
+        return self.dockerClient.containers()
