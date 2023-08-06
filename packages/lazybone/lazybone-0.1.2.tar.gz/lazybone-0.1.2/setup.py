31034fb1fa43e30b809c34ef39afab45faa23998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:32 +0000

    Add package lazybone/0.1.2/lazybone-0.1.2.tar.gz

---
+++ packages/lazybone/lazybone-0.1.2.tar.gz/lazybone-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(name='lazybone',
+      version='0.1.2',
+      py_modules=['lazybone'],
+      description='Python Library for connecting to a Lazybone Bluetooth Relay',
+      author='Daniel Karpinski',
+      author_email='dankarpinski@hotmail.com',
+      url='https://github.com/dankarpinski/lazybone',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Operating System :: POSIX :: Linux'
+          ],
+      install_requires=[
+          "pybluez"
+          ]
+      )
