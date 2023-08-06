7e587766bdc6c77a1e248390163dd73d95e351a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:53 +0000

    Add package enhance_djpro_tpl/0.0.2/enhance_djpro_tpl-0.0.2.tar.gz

---
+++ packages/enhance_djpro_tpl/enhance_djpro_tpl-0.0.2.tar.gz/enhance_djpro_tpl-0.0.2/enhance_djpro_tpl/project_template/project_name/settings_release.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+"""
+Django settings for {{ project_name }} project.
+This settings is for different to other env.
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
