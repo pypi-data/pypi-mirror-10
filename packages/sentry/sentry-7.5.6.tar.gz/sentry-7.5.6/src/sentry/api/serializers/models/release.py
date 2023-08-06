d11485aa177184f7ab185ba969a3cb5192f72f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:08 +0000

    Add package sentry/7.5.6/sentry-7.5.6.tar.gz

---
+++ packages/sentry/sentry-7.5.6.tar.gz/sentry-7.5.6/src/sentry/api/serializers/models/release.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+from sentry.api.serializers import Serializer, register
+from sentry.models import Release
+
+
+@register(Release)
+class ReleaseSerializer(Serializer):
+    def serialize(self, obj, attrs, user):
+        d = {
+            'version': obj.version,
+            'ref': obj.ref,
+            'url': obj.url,
+            'dateStarted': obj.date_started,
+            'dateReleased': obj.date_released,
+            'dateCreated': obj.date_added,
+            'data': obj.data,
+        }
+        return d
---
+++ packages/sentry/sentry-7.5.6.tar.gz/sentry-7.5.6/src/sentry/models/release.py
@@ -0,0 +1,38 @@
+"""
+sentry.models.release
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import, print_function
+
+from django.db import models
+from django.utils import timezone
+from jsonfield import JSONField
+
+from sentry.db.models import FlexibleForeignKey, Model, sane_repr
+
+
+class Release(Model):
+    """
+    A release is generally created when a new version is pushed into a
+    production state.
+    """
+    project = FlexibleForeignKey('sentry.Project')
+    version = models.CharField(max_length=64)
+    # ref might be the branch name being released
+    ref = models.CharField(max_length=64, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    date_added = models.DateTimeField(default=timezone.now)
+    date_started = models.DateTimeField(null=True, blank=True)
+    date_released = models.DateTimeField(null=True, blank=True)
+    # arbitrary data recorded with the release
+    data = JSONField(default={})
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_release'
+        unique_together = (('project', 'version'),)
+
+    __repr__ = sane_repr('project_id', 'version')
