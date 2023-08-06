b26275f1546afac4be033aec0569fd4496a73e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:27 +0000

    Add package duong_pypkg/1.1.0/duong_pypkg-1.1.0.zip

---
+++ packages/duong_pypkg/duong_pypkg-1.1.0.zip/duong_pypkg-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'duong_pypkg',
+    version = '1.1.0',
+    py_modules = ['duong_printlist'],
+    author = 'Duong',
+    author_email= 'jms.hoang@gmail.com',
+    url = 'http://www.123.com',
+    description = 'Print list with or without nested items.'
+)
