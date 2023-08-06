9ad24b25879e0cdf8d4c1640f79c4369f12e50bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:01 +0000

    Add package Flask-SQLAlchemy-Booster/0.1.2/Flask-SQLAlchemy-Booster-0.1.2.tar.gz

---
+++ packages/Flask-SQLAlchemy-Booster/Flask-SQLAlchemy-Booster-0.1.2.tar.gz/Flask-SQLAlchemy-Booster-0.1.2/flask_sqlalchemy_booster/__init__.py
@@ -0,0 +1,11 @@
+"""flask_sqlalchemy_booster
+
+A wrapper over Flask-SQLAlchemy
+
+"""
+
+from .core import FlaskSQLAlchemyBooster
+from .query_booster import QueryBooster
+import responses
+import utils
+from .json_encoder import json_encoder
