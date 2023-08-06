708ddb0bae5bc5892b20d9ce0c80fda87eccc387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:00 +0000

    Add package lky_sketch/1.0.0/lky_sketch-1.0.0.zip

---
+++ packages/lky_sketch/lky_sketch-1.0.0.zip/lky_sketch-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+            name             = 'lky_sketch',
+            version          = '1.0.0',
+            py_modules       = ['lky_sketch'],
+            author           = 'BruceLee',
+            author_email     = '531047675@qq.com',
+            url              = 'http://www.headfirstlabs.com',
+            description      = 'A simple demo of reading file with python and dealing exception.You can download sketch.txt at "http://python.itcarlow.ie/resources.html" '
+    
+    )
