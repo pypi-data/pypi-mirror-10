2690145af223cedca6353b582d3fd953a87e5352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:06 +0000

    Add package gnuplotlib/0.2/gnuplotlib-0.2.tar.gz

---
+++ packages/gnuplotlib/gnuplotlib-0.2.tar.gz/gnuplotlib-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name         = 'gnuplotlib',
+      version      = '0.2',
+      author       = 'Dima Kogan',
+      author_email = 'dima@secretsauce.net',
+      url          = 'http://github.com/dkogan/gnuplotlib',
+      description  = 'Gnuplot-based plotting for numpy',
+      license      = 'LGPL-3+',
+      py_modules   = ['gnuplotlib'] )
