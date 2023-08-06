18a22031ab08614a74747e1d80b296681735f2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:04 +0000

    Add package ctrfoobar2000/1.2.0.2/ctrfoobar2000-1.2.0.2.zip

---
+++ packages/ctrfoobar2000/ctrfoobar2000-1.2.0.2.zip/ctrfoobar2000-1.2.0.2/ctrfoobar2000/foobar.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import subprocess
+# if os.path.isdir(r'c:/pyx'):
+	# sys.path.insert(0, r'c:\\pyx')
+# if sys.platform == 'win32':
+	# import module002a
+# if not os.path.isdir(r'e:\PROJECT_EDIT\git'):
+	# raise SystemError('Please re-Set module ctrlfoobar2000 !')
+# sys.path.insert(0, 'e:\PROJECT_EDIT\git')
+# os.chdir('e:\PROJECT_EDIT\git\ctrfoobar2000')
+from ctrfoobar2000 import control
+foobar = control.control()
+
+class foobarx(control.control):
+	def __init__(self, verbosity=None):
+		self.args = ['-h', '-l', '-p', '-s', '-P', '-n', '-r', '-R', '-V', '-M', '-i', '-t', '-c', '-k', '-C']
+		self.ctype = foobar.ctype
+		self.foobar2000 = foobar.foobar2000
+		self.type_foobar = foobar.type_foobar
+		self.prog_path = foobar.prog_path
+		self.conf = foobar.conf
+		self.host = foobar.host
+		self.port = foobar.port
+		super(foobarx, self)
+
+	def start(self, args):
+		return subprocess.Popen(args)
+
+	def usagex(self):
+		if len(sys.argv) == 1:
+			foobar.usage()
+			# data = ['c:\\Program Files\\foobar2000\\foobar2000.exe']
+			# module002a.main(data)
+		elif len(sys.argv) == 2:
+			if sys.platform == 'win32':
+				if sys.argv[1] not in self.args:
+					if os.path.isdir(sys.argv[1]) or os.path.isfile(sys.argv[1]):
+						data1 = [r'c:\\PROGRA~1\\foobar2000\\foobar2000.exe']
+						data2 = [r'c:\\PROGRA~2\\foobar2000\\foobar2000.exe']
+						if os.path.isfile(data1[0]):
+							# module002a.main(data1)
+							self.start(data1)
+						else:
+							# module002a.main(data2)
+							self.start(data1)
+					elif sys.argv[1] == '-o' or sys.argv[1] == '-O':
+						# data = ['c:\\Program Files\\foobar2000\\foobar2000.exe']
+						# module002a.main(data)
+						# c.usage()
+						self.usage()
+					else:
+						self.usage()
+				else:
+					self.usage()
+			else:
+				# print "\t your are not windows os !"
+				self.usage()
+		else:
+			self.usage()
+
+def main():
+	c = foobarx()
+	c.usagex()
+
+if __name__ == "__main__":
+	c = foobarx()
+	c.usagex()
