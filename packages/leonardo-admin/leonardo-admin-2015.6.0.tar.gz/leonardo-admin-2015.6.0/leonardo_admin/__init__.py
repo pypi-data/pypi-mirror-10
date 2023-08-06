83fa30c4060143076184d51f9a7b2a2537f5f081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:55 +0000

    Add package leonardo-admin/2015.6.0/leonardo-admin-2015.6.0.tar.gz

---
+++ packages/leonardo-admin/leonardo-admin-2015.6.0.tar.gz/leonardo-admin-2015.6.0/leonardo_admin/__init__.py
@@ -0,0 +1,39 @@
+
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+default_app_config = 'leonardo_admin.Config'
+
+
+class Default(object):
+
+    optgroup = 'Admin'
+
+    # don't decorate admin urls
+    public = True
+
+    @property
+    def apps(self):
+        apps = [
+
+            'bootstrap_admin',  # theme
+            'bootstrap_admin_feincms',  # theme
+
+            'django.contrib.admin',
+            'django.contrib.admindocs',
+
+            'leonardo_admin',
+        ]
+        return apps
+
+    plugins = [
+        ('leonardo_admin.apps.admin', _('Admin Site')),
+    ]
+
+
+class Config(AppConfig, Default):
+    name = 'leonardo_admin'
+    verbose_name = _("Admin")
+
+default = Default()
