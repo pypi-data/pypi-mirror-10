72931fec88cebe947d6389b521e5eefb4650f2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package imgtohttps/0.4/imgtohttps-0.4.tar.gz

---
+++ packages/imgtohttps/imgtohttps-0.4.tar.gz/imgtohttps-0.4/imgtohttps/storage.py
@@ -0,0 +1,11 @@
+from flask.ext.redis import FlaskRedis
+
+from imgtohttps.lib import LinkSet, LinksMapping, LinkRegistry
+
+
+storage = FlaskRedis()
+
+secure_domains = LinkSet(storage, 'secure_domains')
+insecure_domains = LinkSet(storage, 'insecure_domains')
+already_uploaded_links = LinksMapping(storage, 'already_uploaded_links')
+image_registry = LinkRegistry(storage, 'image_')
