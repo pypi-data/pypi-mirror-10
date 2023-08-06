243515f12586273ca7df6daf2419e27e2c1d00e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:33 +0000

    Add package allmydata-tahoe/1.10.1/allmydata-tahoe-1.10.1.tar.gz

---
+++ packages/allmydata-tahoe/allmydata-tahoe-1.10.1.tar.gz/allmydata-tahoe-1.10.1/src/allmydata/_version.py
@@ -0,0 +1,9 @@
+
+# This _version.py is generated from git metadata by the tahoe setup.py.
+
+__pkgname__ = 'allmydata-tahoe'
+real_version = '1.10.1'
+full_version = '2875a9ffc415cd665026419001f2311b4efd44e1'
+branch = 'master'
+verstr = '1.10.1'
+__version__ = verstr
