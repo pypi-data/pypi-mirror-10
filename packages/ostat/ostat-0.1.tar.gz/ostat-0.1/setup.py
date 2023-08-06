1c073e650f25fad1ae568b98fe5c092a18e1fc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:37 +0000

    Add package ostat/0.1/ostat-0.1.tar.gz

---
+++ packages/ostat/ostat-0.1.tar.gz/ostat-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ostat',
+    version='0.1',
+    description=(
+        'efficient, accurate, and stable calculation of online '
+        'statistical quantities.'
+    ),
+    url='https://s.mcquay.me/sm/py-ostat/',
+    author='Stephen McQuay',
+    author_email='stephen@mcquay.me',
+    license='MIT',
+    packages=['ostat'],
+    zip_safe=False,
+)
