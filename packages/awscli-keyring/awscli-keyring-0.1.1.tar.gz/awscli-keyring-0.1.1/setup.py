84fa8cba02aafb0c1ea041c79ab7120760041551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:13 +0000

    Add package awscli-keyring/0.1.1/awscli-keyring-0.1.1.tar.gz

---
+++ packages/awscli-keyring/awscli-keyring-0.1.1.tar.gz/awscli-keyring-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name='awscli-keyring',
+    version='0.1.1',
+    description='AWS command line credentials from OS X Keychain and other keyrings.',
+    long_description='AWS command line credentials from OS X Keychain and other keyrings.',
+    author='Samuel Cochran',
+    author_email='sj26@sj26.com',
+    url='https://github.com/sj26/awscli-keyring',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'awscli',
+        'keyring',
+    ],
+    license="MIT",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+)
