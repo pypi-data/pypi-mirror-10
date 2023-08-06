8acf8208244974228be7bb4ec340f7e58b36d8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:21 +0000

    Add package ConfigMaster/2.1.0/ConfigMaster-2.1.0.tar.gz

---
+++ packages/ConfigMaster/ConfigMaster-2.1.0.tar.gz/ConfigMaster-2.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="ConfigMaster",
+    version='2.1.0',
+    description="Programmatic configuration library for Python 3.",
+    author=" ",
+    author_email="eyesismine@gmail.com",
+    url="https://bitbucket.org/SunDwarf/configmaster",
+    packages=["configmaster"],
+    license="MIT",
+)
