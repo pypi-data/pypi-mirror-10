91bff7a81723c1adb944f91a35a57ff3406b6bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:17 +0000

    Add package jimit/0.20/jimit-0.20.tar.gz

---
+++ packages/jimit/jimit-0.20.tar.gz/jimit-0.20/jimit/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/4/20'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+
+from common import (
+    Common
+)
+
+from check import (
+    Check
+)
+
+from convert import (
+    Convert
+)
+
+from ji_time import (
+    JITime
+)
+
+from router import (
+    Router
+)
+
+__version__ = "0.20"
+
+__all__ = [
+    'Common', 'Check', 'Convert', 'JITime', 'Router'
+]
