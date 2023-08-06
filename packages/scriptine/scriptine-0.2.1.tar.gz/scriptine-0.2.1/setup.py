335bfb45f17bd59b98de717b765d7e50a51cc093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:41 +0000

    Add package scriptine/0.2.1/scriptine-0.2.1.tar.gz

---
+++ packages/scriptine/scriptine-0.2.1.tar.gz/scriptine-0.2.1/setup.py
@@ -0,0 +1,37 @@
+import scriptine
+import os
+
+name = "scriptine"
+version = scriptine.__version__
+
+class zipdist(scriptine.misc.DistutilsCommand):
+    def run(self):
+        from scriptine.meta import zipdist_command
+        zipdist_command()
+
+options = scriptine.misc.Options(
+    name = name,
+    version = version,
+    author = "Oliver Tonnhofer",
+    author_email = "olt@omniscale.de",
+    description = 'python shell scripts made easy',
+    long_description=open('README.rst').read() +'\n' + open('CHANGELOG.txt').read(),
+    license = 'MIT License',
+    url = 'https://github.com/olt/scriptine',
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      ],
+    packages = ['scriptine'],
+    zip_safe = False,
+    cmdclass = {
+        'zipdist': zipdist,
+    }
+)
+
+if __name__ == '__main__':
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
+    setup(**options)
