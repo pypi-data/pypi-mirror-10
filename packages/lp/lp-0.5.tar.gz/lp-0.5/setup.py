ec323ae470af92259b0499f70cb0eab6614e78c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:20 +0000

    Add package lp/0.5/lp-0.5.tar.gz

---
+++ packages/lp/lp-0.5.tar.gz/lp-0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='lp',
+    description='An aid for choosing words in Letterpress',
+    url='https://github.com/colons/lp',
+    author='colons',
+    author_email='pypi@colons.co',
+    version='0.5',
+    license='LICENSE',
+    platforms=['any'],
+    packages=['lp'],
+    scripts=['scripts/lp'],
+    long_description=README,
+    classifiers=[
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'ansicolors',
+        'bottle',
+        'pillow',
+    ],
+    include_package_data=True,
+)
