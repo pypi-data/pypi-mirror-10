08d5a57fce19e3bd8c126178dee54e174dd32bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:38 +0000

    Add package hatcher/0.8.4/hatcher-0.8.4.tar.gz

---
+++ packages/hatcher/hatcher-0.8.4.tar.gz/hatcher-0.8.4/hatcher/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.8.4'
+full_version = '0.8.4'
+git_revision = '2e375076df4da4aef274e6dd777bae62ca4c9c0a'
+is_released = True
+
+if not is_released:
+    version = full_version
