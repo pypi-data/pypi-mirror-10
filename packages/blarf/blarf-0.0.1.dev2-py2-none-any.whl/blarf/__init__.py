98a124c7accb9f4ac8b443f94fd870ce3a8f5c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:41 +0000

    Add package blarf/0.0.1.dev2/blarf-0.0.1.dev2-py2-none-any.whl

---
+++ packages/blarf/blarf-0.0.1.dev2-py2-none-any.whl/blarf/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'rtuin'
+
+import blarf_console
+
+def console():
+    blarf_console.blarf_console()
---
+++ packages/blarf/blarf-0.0.1.dev2-py2-none-any.whl/blarf/blarf_console.py
@@ -0,0 +1,5 @@
+__author__ = 'rtuin'
+
+class blarf_console:
+    def __init__(self):
+        print("blarf!")
