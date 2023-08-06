2d467a6bff414d475d74ce230dabd68f53accfac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:50 +0000

    Add package django-sponsors/0.1.1/django_sponsors-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-sponsors/django_sponsors-0.1.1-py2.py3-none-any.whl/sponsors/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SponsorsConfig(AppConfig):
+    name = 'sponsors'
+    verbose_name = "Django Sponsors"
