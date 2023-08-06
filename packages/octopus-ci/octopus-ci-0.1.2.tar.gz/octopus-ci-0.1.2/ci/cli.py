d28bc6956b363dbdd404e355468e99956b95ba09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:03 +0000

    Add package octopus-ci/0.1.2/octopus-ci-0.1.2.tar.gz

---
+++ packages/octopus-ci/octopus-ci-0.1.2.tar.gz/octopus-ci-0.1.2/ci/cli.py
@@ -0,0 +1,19 @@
+__author__ = 'soroosh'
+
+import click
+
+from ci import probe
+
+@click.command("probe")
+@click.option('--host', required=True, help='Host name which you want to ping.')
+@click.option('--port', required=True, help='Port number which you want to ping.')
+@click.option('--retries', default=10, help='Number of retries. default: 10')
+@click.option('--interval', default=1, help='Time interval between probes (In second). default 1 second')
+def probe_cmd(host, port, retries, interval):
+    probe.try_connect(host, port, retries, interval)
+
+
+def main(): probe_cmd()
+
+if __name__ == '__main__':
+    main()
