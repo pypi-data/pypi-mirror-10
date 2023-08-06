dcf8f6eae77a62c412a6333d504e8597567f25c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:15 +0000

    Add package facebook-online-friend-tracker/1.0.2/facebook-online-friend-tracker-1.0.2.tar.gz

---
+++ packages/facebook-online-friend-tracker/facebook-online-friend-tracker-1.0.2.tar.gz/facebook-online-friend-tracker-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='facebook-online-friend-tracker',
+  version='1.0.2',
+  description='This tool tracks the number of online friends a user has on Facebook at a given time.',
+  author='Baraa Hamodi',
+  author_email='bhamodi@uwaterloo.ca',
+  url='https://github.com/bhamodi/facebook-online-friend-tracker',
+  license='MIT',
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+  ],
+  include_package_data=True,
+  packages=find_packages(),
+  install_requires=[
+    'selenium>=2.46.0',
+    'openpyxl>=2.2.5',
+  ],
+  entry_points={
+    'console_scripts': [
+      'facebook-online-friend-tracker=src:main',
+    ],
+  },
+)
