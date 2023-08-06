6240a6c8685a9084f32d2c2cadbb182515276fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:24 +0000

    Add package django-mailer/1.0/django-mailer-1.0.tar.gz

---
+++ packages/django-mailer/django-mailer-1.0.tar.gz/django-mailer-1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="django-mailer",
+    version=__import__("mailer").__version__,
+    description="A reusable Django app for queuing the sending of email",
+    long_description=open("docs/usage.rst").read() + open("CHANGES.rst").read(),
+    author="Pinax Team",
+    author_email="developers@pinaxproject.com",
+    url="http://github.com/pinax/django-mailer/",
+    packages=[
+        "mailer",
+        "mailer.management",
+        "mailer.management.commands",
+    ],
+    package_dir={"mailer": "mailer"},
+    package_data={'mailer': ['locale/*/LC_MESSAGES/*.*']},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Framework :: Django",
+    ],
+    install_requires=[
+        'Django >= 1.4',
+        'lockfile >= 0.8',
+        ],
+)
