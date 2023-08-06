12016aa4c26a5dfb226fff92635ab146ae589884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:14 +0000

    Add package django-extra-views/0.7.1/django-extra-views-0.7.1.tar.gz

---
+++ packages/django-extra-views/django-extra-views-0.7.1.tar.gz/django-extra-views-0.7.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='django-extra-views',
+    version='0.7.1',
+    url='https://github.com/AndrewIngram/django-extra-views',
+    install_requires=[
+        'Django >=1.4',
+        'six>=1.5.2',
+    ],
+    description="Extra class-based views for Django",
+    long_description=open('README.rst', 'r').read(),
+    license="MIT",
+    author="Andrew Ingram",
+    author_email="andy@andrewingram.net",
+    packages=['extra_views'],
+    package_dir={'extra_views': 'extra_views'},
+    include_package_data = True,    # include everything in source control
+    package_data={'extra_views': ['*.py','contrib/*.py','tests/*.py','tests/templates/*.html', 'tests/templates/extra_views/*.html']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3']
+)
