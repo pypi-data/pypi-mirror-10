9329471990676b61083a7e3776f512b478937038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:15 +0000

    Add package Mopidy-Mopify/1.5.3/Mopidy-Mopify-1.5.3.tar.gz

---
+++ packages/Mopidy-Mopify/Mopidy-Mopify-1.5.3.tar.gz/Mopidy-Mopify-1.5.3/mopidy_mopify/queuemanager/frontend.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from mopidy.core import CoreListener
+import pykka
+import logging
+
+from .. import mem
+
+class QueueManagerFrontend(pykka.ThreadingActor, CoreListener):
+
+    def __init__(self, config, core):
+        super(QueueManagerFrontend, self).__init__()
+        self.config = config
+        self.core = core
+
+    def track_playback_started(self, tl_track):
+        tlids = [tl_track.tlid]
+
+        mem.queuemanager.remove_from_queue(tlids)
+        mem.queuemanager.remove_from_playlist(tlids)
