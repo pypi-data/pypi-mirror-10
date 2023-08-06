a86ad29cdfe18841db2cfd5ef39037857422b520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package pyudorandom/0.0.2/pyudorandom-0.0.2.tar.gz

---
+++ packages/pyudorandom/pyudorandom-0.0.2.tar.gz/pyudorandom-0.0.2/pyudorandom/__init__.py
@@ -0,0 +1,7 @@
+from .pyudorandom import shuffle, items, bin_gcd, indices
+
+__title__ = 'pyudorandom'
+__version__ = '0.0.2'
+__author__ = 'Mats Julian Olsen'
+__license__ = 'MIT'
+__all__ = ['shuffle', 'items', 'bin_gcd', 'indices']
