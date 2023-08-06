0aa979c1776492b7d82d8a66adc70aa221fcaaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:15 +0000

    Add package 3d-wallet-generator/0.2.0/3d-wallet-generator-0.2.0.tar.gz

---
+++ packages/3d-wallet-generator/3d-wallet-generator-0.2.0.tar.gz/3d-wallet-generator-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name = '3d-wallet-generator',
+    packages = ['gen_3dwallet'],
+    version = '0.2.0',
+    description = 'A tool to help you design and export 3D-printable bitcoin/cryptocurrency wallets',
+    long_description=long_description,
+    author = 'BTC Spry',
+    author_email = 'btcspry@bitforwarder.com',
+    url = 'https://github.com/btcspry/3d-wallet-generator',
+    install_requires=["bitcoin>=1.1.29","PyQrCode>=1.1"],
+    keywords = ['bitcoin','litecoin','dogecoin','wallet','3d printer','cryptocurrency','altcoin','money'],
+    classifiers = ["Programming Language :: Python :: 3 :: Only","License :: OSI Approved :: MIT License","Operating System :: OS Independent","Intended Audience :: End Users/Desktop","Environment :: Console","Development Status :: 4 - Beta","Topic :: Utilities"],
+    entry_points={
+        'console_scripts': [
+            '3dwallet = gen_3dwallet.base:main',
+        ],
+    },
+)
