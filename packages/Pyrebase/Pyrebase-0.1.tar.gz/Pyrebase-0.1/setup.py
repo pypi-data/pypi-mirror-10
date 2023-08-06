cff09386557dd005a5fbbc9a60a5e85e45b67d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:03 +0000

    Add package Pyrebase/0.1/Pyrebase-0.1.tar.gz

---
+++ packages/Pyrebase/Pyrebase-0.1.tar.gz/Pyrebase-0.1/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Pyrebase',
+
+    version='0.1',
+
+    description='A simple interface for the Firebase REST API',
+
+    url='https://github.com/thisbejim/Pyrebase',
+
+    author='James Childs-Maidment',
+    author_email='jchildsmaidment@outlook.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='Firebase',
+
+    packages=find_packages(),
+
+    install_requires=['requests_futures','firebase_token_generator'],
+)
