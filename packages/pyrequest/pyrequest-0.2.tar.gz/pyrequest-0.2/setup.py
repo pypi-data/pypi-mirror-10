71633c7ea4b64e4ec5f59206f5f30af78436c662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:59 +0000

    Add package pyrequest/0.2/pyrequest-0.2.tar.gz

---
+++ packages/pyrequest/pyrequest-0.2.tar.gz/pyrequest-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+import os
+
+
+def Readme():
+    return open(os.path.join(os.path.dirname(__file__), 'README.md'), "r").read()
+
+
+setup(
+    name='pyrequest',
+    packages=['pyhttp'],
+    version='0.2',
+    description='Pythonic way of HTTP request',
+    long_description=Readme(),
+    author='plasmashadow',
+    author_email='plasmashadowx@gmail.com',
+    url='https://github.com/plasmashadow/pyhttp.git',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers'
+    ],
+    include_package_data=True,
+    license='MIT License'
+)
