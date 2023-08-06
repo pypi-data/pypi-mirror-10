211627eed7f641f65a3d2969965c164ef75410d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:00 +0000

    Add package clustercron/0.3.0.dev2/clustercron-0.3.0.dev2.tar.gz

---
+++ packages/clustercron/clustercron-0.3.0.dev2.tar.gz/clustercron-0.3.0.dev2/tests/test_elb.py
@@ -0,0 +1,87 @@
+"""
+Tests for `clustercron` module.
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+import mock
+import pytest
+from clustercron import elb
+
+
+def test_Elb_init():
+    elb_lb = elb.Elb('mylbname')
+    assert elb_lb.lb_name == 'mylbname'
+
+
+@pytest.mark.parametrize('instance_id,inst_health_states,is_master', [
+    (
+        u'i-1d564f5c',
+        [
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+        ],
+        True,
+    ),
+    (
+        u'i-1d564f5c',
+        [
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+        ],
+        True,
+    ),
+    (
+        u'i-1d564f5c',
+        [
+            {'instance_id': u'i-cba0ce84', 'state': u'Anything'},
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+        ],
+        True,
+    ),
+    (
+        u'i-cba0ce84',
+        [
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+        ],
+        False,
+    ),
+    (
+        u'i-cba0ce84',
+        [
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+        ],
+        False,
+    ),
+    (
+        u'i-cba0ce84',
+        [
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+            {'instance_id': u'i-1d564f5c', 'state': u'anything'},
+        ],
+        True,
+    ),
+    (
+        None,
+        [
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+        ],
+        False,
+    ),
+    (
+        None,
+        [
+            {'instance_id': u'i-1d564f5c', 'state': u'InService'},
+            {'instance_id': u'i-cba0ce84', 'state': u'InService'},
+        ],
+        False,
+    ),
+])
+def test_elb_is_master(instance_id, inst_health_states, is_master):
+    print(instance_id, inst_health_states, is_master)
+    elb_lb = elb.Elb('mylbname')
+    assert elb_lb._is_master(
+        instance_id, [mock.Mock(**x) for x in inst_health_states]) == is_master
