e8cf029d5db474af2cb1a28368ac0206ed5b1d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:40 +0000

    Add package openupgradelib/0.1.0/openupgradelib-0.1.0.tar.gz

---
+++ packages/openupgradelib/openupgradelib-0.1.0.tar.gz/openupgradelib-0.1.0/tests/test_openupgradelib.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_openupgradelib
+----------------------------------
+
+Tests for `openupgradelib` module.
+"""
+
+import unittest
+
+from openupgradelib import openupgrade
+
+
+class TestOpenupgradelib(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
