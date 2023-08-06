592589ddaaf3735ca595503526c4104fc8eb9746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:31 +0000

    Add package PasswordReader/0.1.1/PasswordReader-0.1.1.tar.gz

---
+++ packages/PasswordReader/PasswordReader-0.1.1.tar.gz/PasswordReader-0.1.1/passwordreader/Modules.py
@@ -0,0 +1,57 @@
+import os
+import getpass
+import PasswordReader
+
+__all__ = ["PasswordReaderModule",
+           "EnvironmentModule",
+           "FileModule",
+           "ConsoleModule"]
+
+
+class EnvironmentModule(PasswordReader.PasswordReaderModule):
+
+    def __init__(self, variable_name):
+        self._variable_name = variable_name
+
+    def readPassword(self):
+        if self._variable_name in os.environ:
+            return os.environ[self._variable_name]
+        else:
+            return None
+
+
+class FileModule(PasswordReader.PasswordReaderModule):
+
+    def __init__(self, file_name):
+        self._file_name = file_name
+
+    def readPassword(self):
+        file_name = self._file_name
+
+        if file_name.startswith('@'):
+            file_name = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), file_name[1:])
+
+        try:
+            with open(file_name) as f:
+                return f.readline()
+        except IOError:
+            return None
+
+
+class ConsoleModule(PasswordReader.PasswordReaderModule):
+
+    def __init__(self, prompt):
+        self._prompt = prompt
+
+    def readPassword(self):
+        return getpass.getpass(self._prompt)
+
+
+class ValueModule(PasswordReader.PasswordReaderModule):
+
+    def __init__(self, value):
+        self._value = value
+
+    def readPassword(self):
+        return self._value
---
+++ packages/PasswordReader/PasswordReader-0.1.1.tar.gz/PasswordReader-0.1.1/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PasswordReader',
+      version='0.1.1',
+      description='PasswordReader tool',
+      author='Michal Rysavy',
+      packages=['passwordreader'],
+      )
