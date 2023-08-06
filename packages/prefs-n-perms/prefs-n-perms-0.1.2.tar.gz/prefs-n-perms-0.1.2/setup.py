120aa979d81cd8d06e0370b495780ea766183fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:44 +0000

    Add package prefs-n-perms/0.1.2/prefs-n-perms-0.1.2.tar.gz

---
+++ packages/prefs-n-perms/prefs-n-perms-0.1.2.tar.gz/prefs-n-perms-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='prefs-n-perms',
+    version='0.1.2',
+    packages=['prefs_n_perms'],
+    include_package_data=True,
+    install_requires=[
+        'hiredis',
+        'redis',
+        'redish',
+    ],
+    zip_safe=True,
+    url='https://github.com/revpoint/prefs-n-perms',
+    author='RevPoint Media LLC',
+    author_email='jangl@revpointmedia.com',
+    description='Manage preferences and permissions for Jangl app',
+)
