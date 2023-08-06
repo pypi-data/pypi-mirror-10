8b06e0ea28ea92a88158f375d0adf18fd0d17f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:03 +0000

    Add package django-toolkit/0.2.0/django-toolkit-0.2.0.tar.gz

---
+++ packages/django-toolkit/django-toolkit-0.2.0.tar.gz/django-toolkit-0.2.0/django_toolkit/tests/__init__.py
@@ -0,0 +1,5 @@
+from django_toolkit.tests.date_util import *
+from django_toolkit.tests.datetime_util import *
+from django_toolkit.tests.markup import *
+from django_toolkit.tests.url import *
+from django_toolkit.tests.csv import *
