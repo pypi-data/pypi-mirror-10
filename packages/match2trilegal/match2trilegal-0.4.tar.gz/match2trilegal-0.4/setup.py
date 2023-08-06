47479cf155ca5eca36d1ac744357bdb03fa90af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package match2trilegal/0.4/match2trilegal-0.4.tar.gz

---
+++ packages/match2trilegal/match2trilegal-0.4.tar.gz/match2trilegal-0.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='match2trilegal',
+    author='Phil Rosenfield',
+    author_email='philip.rosenfield@unipd.it',
+    version='0.4',
+    py_modules=['match2trilegal'],
+    scripts=['match2trilegal'],
+    install_requires=['numpy'],
+    url='https://github.com/philrosenfield/match2trilegal'
+)
