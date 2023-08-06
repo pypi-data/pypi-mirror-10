7942e4f1f77c37bb852dad4516edf2ce58fdfd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:47 +0000

    Add package google-parser/0.0.21/google-parser-0.0.21.tar.gz

---
+++ packages/google-parser/google-parser-0.0.21.tar.gz/google-parser-0.0.21/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+VERSION = "0.0.21"
+
+setup(
+    name='google-parser',
+    description="Convert html to snippets",
+    version=VERSION,
+    url='https://github.com/KokocGroup/google-parser',
+    download_url='https://github.com/KokocGroup/google-parser/tarball/v{}'.format(VERSION),
+    packages=['google_parser', 'google_query'],
+    install_requires=[
+        'pyquery>=1.2.9',
+    ],
+)
