115feaa9146dfafbcb1acd028de361c54506ee53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:15 +0000

    Add package repositorytools/3.0.52/repositorytools-3.0.52.tar.gz

---
+++ packages/repositorytools/repositorytools-3.0.52.tar.gz/repositorytools-3.0.52/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import re
+
+version = ''
+with open('repositorytools/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+setup(name='repositorytools',
+      version=version,
+      description='Tools for working with artifact repositories',
+      author='Michel Samia',
+      author_email='stardust1985@gmail.com',
+      url='https://github.com/stardust85/repositorytools',
+      license='Apache 2.0',
+      platforms='any',
+
+      classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Console',
+      ),
+
+      packages=['repositorytools'],
+      scripts=['scripts/artifact', 'scripts/repo'],
+      download_url='https://github.com/stardust85/repositorytools/tarball/3.0.52'
+)
