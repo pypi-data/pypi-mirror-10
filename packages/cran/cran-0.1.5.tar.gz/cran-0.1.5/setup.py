71d48eafc78b7a0da337575c9e58bdfc6cd24dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:02 +0000

    Add package cran/0.1.5/cran-0.1.5.tar.gz

---
+++ packages/cran/cran-0.1.5.tar.gz/cran-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='cran',
+      version='0.1.5',
+      description='Cran via Native Python Library Wrapping',
+      url='http://github.com/mindey/cran',
+      author='Mindey I.',
+      author_email='mindey@qq.com',
+      license='MIT',
+      packages=['cran'],
+      install_requires=[
+          'GitPython',
+          'pandas',
+          'pylab',
+      ],
+      zip_safe=False)
