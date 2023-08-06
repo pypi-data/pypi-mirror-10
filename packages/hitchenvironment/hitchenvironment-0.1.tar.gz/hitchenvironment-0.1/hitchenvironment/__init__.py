fd583f5b9c6a34f660d18db1c32147e54cf299c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:37 +0000

    Add package hitchenvironment/0.1/hitchenvironment-0.1.tar.gz

---
+++ packages/hitchenvironment/hitchenvironment-0.1.tar.gz/hitchenvironment-0.1/hitchenvironment/__init__.py
@@ -0,0 +1,12 @@
+from environment import Environment
+from environment import class_definition
+from environment import systembits
+from environment import has_internet
+from environment import platform
+from environment import class_definition
+from environment import HitchEnvironmentException
+
+
+
+
+
