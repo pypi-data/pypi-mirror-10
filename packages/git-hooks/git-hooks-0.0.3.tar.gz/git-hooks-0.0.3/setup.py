0bbf108fb0477443326fa17b0b5431d3bb7673bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package git-hooks/0.0.3/git-hooks-0.0.3.tar.gz

---
+++ packages/git-hooks/git-hooks-0.0.3.tar.gz/git-hooks-0.0.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="git-hooks",
+    version="0.0.3",
+    description="Show useful git hooks",
+    license="MIT",
+    author="JuanPabloAJ",
+    author_email="jpabloaj@gmail.com",
+    url="https://github.com/juanpabloaj/git-hooks",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    package_data={
+        'githooks': ['hooks/*/*']
+    },
+    entry_points={
+        'console_scripts': [
+            'git-hooks=githooks:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
