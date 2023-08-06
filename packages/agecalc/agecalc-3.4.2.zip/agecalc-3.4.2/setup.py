23532f0a75ef92e18d619099e27b35aeb484263d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package agecalc/3.4.2/agecalc-3.4.2.zip

---
+++ packages/agecalc/agecalc-3.4.2.zip/agecalc-3.4.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'agecalc',
+  packages = ['agecalc'],
+  version = '3.4.2',
+  description = 'Calculates birth information based on a specific Date of Birth.',
+  author = 'Ali Raja <alir6716@gmail.com>',
+  author_email = 'alir6716@gmail.com',
+  url = 'http://alir6716.github.io/agecalc',
+  download_url = 'https://github.com/alir6716/agecalc/tarball/3.4.2',
+  keywords = ['agecalc', 'age', 'calculator'],
+  classifiers = [],
+)
