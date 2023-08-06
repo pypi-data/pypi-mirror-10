5ca8564529d6dfe7ac943eacdb05928d33e5c541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:28 +0000

    Add package lista_aninhada/1.2.0/lista_aninhada-1.2.0.zip

---
+++ packages/lista_aninhada/lista_aninhada-1.2.0.zip/lista_aninhada-1.2.0/Setup.py
@@ -0,0 +1,13 @@
+__author__ = 'Gugu'
+
+from distutils.core import setup
+
+setup(
+        name         ='lista_aninhada',
+        version      ='1.2.0',
+        py_modules   =['lista_aninhada'],
+        author       ="eu",
+        author_email ="julietwisk@hotmail.com",
+        url          ='sem url',
+        description  ="print listas aninhadas",
+)
---
+++ packages/lista_aninhada/lista_aninhada-1.2.0.zip/lista_aninhada-1.2.0/lista_aninhada.py
@@ -0,0 +1,13 @@
+__author__ = 'Gugu'
+
+# Esta função identa as listas aninhadas (listas dentro de outras listas)
+
+def print_lol(a_list, indent=False, level=0):
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
