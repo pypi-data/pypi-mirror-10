b16f747b635506aed6d06e306fb3a0b5ec813d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:26 +0000

    Add package ip2location-python/0.1.1/ip2location-python-0.1.1.tar.gz

---
+++ packages/ip2location-python/ip2location-python-0.1.1.tar.gz/ip2location-python-0.1.1/README.rst
@@ -0,0 +1,8 @@
+What is ip2location
+=====================
+
+ip2location is a ip's locale query tool
+
+Data Providers:
+  * `Taobao <http://ip.taobao.com/>`_
+
---
+++ packages/ip2location-python/ip2location-python-0.1.1.tar.gz/ip2location-python-0.1.1/ip2location/metadata.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+NAME = 'ip2location-python'
+DESCRIPTION = 'ipv4 to location'
+VERSION = '0.1.1'
+AUTHOR = 'pengyao'
+AUTHOR_EMAIL = 'pengyao@pengyao.org'
+COPYRIGHT = 'Copyright © 2014 ' + AUTHOR
+LICENCE = 'Apache License V2.0'
+URL = 'https://github.com/pengyao/ip2location'
