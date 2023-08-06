a18e5fe22b009bf8794d8dc2c6affc485f48bd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package wyn/1.0.0/wyn-1.0.0.zip

---
+++ packages/wyn/wyn-1.0.0.zip/wyn-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wyn',
+    version = '1.0.0',
+    py_modules = ['wyn'],
+    author = 'wm',
+    author_email = '554620413@qq.com',
+    url = '554620413@qq.com',
+    description = 'Hello World',
+    )
