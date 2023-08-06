c9b9dbb583694a675264b6a59ed11e87639bc228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:22 +0000

    Add package port/0.0.6/port-0.0.6.tar.gz

---
+++ packages/port/port-0.0.6.tar.gz/port-0.0.6/port/cli.py
@@ -0,0 +1,157 @@
+import os
+import json
+import click
+import shutil
+import subprocess
+from click import echo
+from port.server import create_app
+from port.compile import build_site
+from port.host import host_site, unhost_site
+
+base = os.path.expanduser('~/.port')
+
+
+def app_for_site(site_name, port):
+    # Load site-specific config
+    path = os.path.join(base, site_name + '.json')
+    conf = json.load(open(path, 'r'))
+
+    # Prep paths
+    site_dir = conf['SITE_DIR']
+    conf['BUILD_DIR'] = os.path.join(site_dir, '.build')
+    conf['ASSET_DIR'] = os.path.join(site_dir, 'assets')
+    theme = os.path.join(base, 'themes', conf['THEME'])
+
+    return create_app(static_folder=theme, template_folder=theme, **conf)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('site_name')
+@click.option('--port', default=5005)
+def serve(site_name, port):
+    """
+    Serve a site
+    """
+    app = app_for_site(site_name, port)
+    app.run(host='0.0.0.0', debug=True, port=port)
+
+
+@cli.command()
+@click.argument('site_name')
+@click.argument('host_name')
+@click.argument('user')
+@click.option('--port', default=5005)
+def host(site_name, host_name, user, port):
+    """
+    Host a site (experimental, only tested on Ubuntu 14.04)
+    """
+    host_site(site_name, host_name, user, port=port)
+
+
+@cli.command()
+@click.argument('site_name')
+@click.argument('host_name')
+def unhost(site_name, host_name):
+    """
+    Unhost a site
+    """
+    unhost_site(site_name, host_name)
+
+
+@cli.command()
+@click.argument('site_name')
+def create(site_name):
+    """
+    Create a new site
+    """
+    theme_dir = os.path.join(base, 'themes')
+    if not os.path.exists(base):
+        os.makedirs(base)
+
+        default_themes = os.path.join(os.path.dirname(__file__), 'themes')
+        shutil.copytree(default_themes, theme_dir)
+
+    path = os.path.join(base, site_name + '.json')
+    if os.path.exists(path):
+        echo('Site already exists! Remove the existing config at {} and retry.'.format(path))
+        return
+
+
+    themes = os.listdir(theme_dir)
+    site_dir = input('Where will your site\'s files be located? ')
+    site_name = input('What will you call your site? ')
+    site_url = input('What url will your site be at? ')
+    site_desc = input('How would you describe your site? ')
+    per_page = input('How many posts should be shown per page? ')
+    theme = input('What theme will you use? Select from {} '.format(themes))
+
+    if theme not in themes:
+        theme = themes[0]
+
+    site_dir = os.path.expanduser(site_dir)
+    conf = {
+        'SITE_DIR': site_dir,
+        'SITE_NAME': site_name,
+        'SITE_URL': site_url,
+        'SITE_DESC': site_desc,
+        'PER_PAGE': int(per_page),
+        'THEME': theme
+    }
+
+    json.dump(conf, open(path, 'w'), indent=4, sort_keys=True)
+
+    # Create the site directories
+    if not os.path.exists(site_dir):
+        os.makedirs(site_dir)
+
+    for dir in ['assets', 'default_category']:
+        dir_path = os.path.join(site_dir, dir)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+    echo('Site created.')
+    echo('You can change your config by editing {}, make sure to restart the server afterwards.'.format(path))
+
+
+@cli.command()
+@click.argument('site_name')
+def build(site_name):
+    """
+    Build a site
+    """
+    # TO DO cleanup
+    print('Building...')
+
+    # Load site-specific config
+    path = os.path.join(base, site_name + '.json')
+    conf = json.load(open(path, 'r'))
+    build_site(conf)
+
+    echo('Built!')
+
+
+@cli.command()
+@click.argument('site_name')
+@click.argument('remote')
+def sync(site_name, remote):
+    """
+    A convenience command to sync a site's files to a remote folder
+    """
+    # Load site-specific config
+    path = os.path.join(base, site_name + '.json')
+    conf = json.load(open(path, 'r'))
+
+    # Prep paths
+    site_dir = conf['SITE_DIR']
+
+    subprocess.call([
+        'rsync',
+        '-ravu',
+        site_dir + '/',
+        remote
+    ])
---
+++ packages/port/port-0.0.6.tar.gz/port-0.0.6/port/compile.py
@@ -0,0 +1,154 @@
+import os
+import re
+import json
+import shutil
+from datetime import datetime
+from PyRSS2Gen import RSS2, RSSItem
+from dateutil.parser import parse
+from distutils.util import strtobool
+from port.md2html import compile_markdown
+
+meta_re = re.compile(r'%~\n(.+)\n%~', re.DOTALL)
+title_re = re.compile(r'^#\s?([^#\n]+)')
+
+
+def build_site(conf):
+    """
+    Build a site based on the passed config
+    """
+    site_dir = conf['SITE_DIR']
+
+    # Get all files and compile
+    posts_by_category = {}
+    categories = [c for c in os.listdir(site_dir)
+                    if os.path.isdir(os.path.join(site_dir, c))
+                    and c != 'assets'
+                    and not c.startswith('.')]
+    for cat in categories:
+        path = os.path.join(site_dir, cat)
+        posts_by_category[cat] = [compile_file(os.path.join(path, f)) for f in os.listdir(path) if f.endswith('.md')]
+    posts = sum(posts_by_category.values(), [])
+
+    # Remove existing build
+    build_dir = os.path.join(site_dir, '.build')
+    if os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
+    os.makedirs(build_dir)
+
+    # Write files
+    for post, build_slug in posts:
+        post_path = os.path.join(build_dir, build_slug + '.json')
+        json.dump(post, open(post_path, 'w'))
+
+    # Write RSS files
+    rss_dir = os.path.join(build_dir, 'rss')
+    os.makedirs(rss_dir)
+    for cat in categories:
+        rss_path = os.path.join(rss_dir, '{}.xml'.format(cat))
+        new_posts = [p for p, bs in posts_by_category[cat] if not p['draft']][:20]
+        compile_rss(new_posts, conf, rss_path)
+
+    new_posts = [p for p, bs in posts if not p['draft']][:20]
+    rss_path = os.path.join(rss_dir, 'rss.xml')
+    compile_rss(new_posts, conf, rss_path)
+
+
+def compile_file(path):
+    """
+    Compile a markdown file.
+    """
+    with open(path, 'r') as f:
+        raw = f.read()
+
+    parts = path.split('/')
+    category = parts[-2]
+    slug = parts[-1].replace('.md', '')
+
+    raw, meta = extract_metadata(raw)
+    html = compile_markdown(raw)
+
+    data = {
+        'html': html,
+        'slug': slug,
+        'category': category,
+        'url': '/{}/{}'.format(category, slug)
+    }
+    data.update(meta)
+
+
+    for k, v in data.items():
+        if isinstance(v, datetime):
+            data[k] = v.isoformat()
+
+    # Lead with the published timestamp so the
+    # built files are in the right order
+    build_slug = '{0}{1}_{2}_{3}'.format('D' if meta['draft'] else '',
+                                         meta['published_ts'],
+                                         category,
+                                         slug)
+    return data, build_slug
+
+
+def extract_metadata(raw):
+    """
+    Extract metadata from raw markdown content.
+    Returns the content with metadata removed and the extracted metadata.
+    """
+    # Defaults
+    meta = {
+        'title': '',
+        'published_at': datetime.now(),
+        'draft': False
+    }
+
+    # Try to extract metadata, demarcated by '%~'
+    meta_match = meta_re.search(raw)
+    if meta_match is not None:
+        meta_raw = meta_match.group(1)
+        ext_meta = dict([[i.strip() for i in l.split(':', 1)] for l in meta_raw.split('\n')])
+        meta.update(ext_meta)
+
+        # Remove the metadata before we compile
+        raw = meta_re.sub('', raw).strip()
+
+    # Convert to bool if necessary
+    if isinstance(meta['draft'], str):
+        meta['draft'] = strtobool(meta['draft'])
+
+    # Convert to timestamp if necessary
+    if isinstance(meta['published_at'], str):
+        meta['published_at'] = parse(meta['published_at'])
+    meta['published_ts'] = int(meta['published_at'].timestamp())
+
+    # Try to extract title
+    title_match = title_re.search(raw)
+    if title_match is not None:
+        meta['title'] = title_match.group(1)
+        meta['title_html'] = compile_markdown(meta['title'])
+
+        # Remove the title before we compile
+        raw = title_re.sub('', raw).strip()
+
+    return raw, meta
+
+
+def compile_rss(posts, conf, outpath):
+    """
+    Compile a list of Posts to the specified outpath.
+    """
+    items = [RSSItem(
+        title=p['title'],
+        link=os.path.join(conf['SITE_URL'], p['category'], p['slug']),
+        description=p['html'],
+        pubDate=p['published_at']
+    ) for p in posts]
+
+    rss = RSS2(
+        title=conf['SITE_NAME'],
+        link=conf['SITE_URL'],
+        description=conf['SITE_DESC'],
+        lastBuildDate=datetime.now(),
+        items=items
+    )
+
+    rss.write_xml(open(outpath, 'w'))
