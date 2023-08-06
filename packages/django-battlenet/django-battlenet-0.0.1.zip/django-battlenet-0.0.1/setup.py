f2523928f9cf872a524553222b77d0e59c608051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:35 +0000

    Add package django-battlenet/0.0.1/django-battlenet-0.0.1.zip

---
+++ packages/django-battlenet/django-battlenet-0.0.1.zip/django-battlenet-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='django-battlenet',
+    license='MIT',
+    version='0.0.1',
+    packages=['battlenet'],
+    description='API wrapper for Blizzards Community REST APIs & OAuth2 login',
+    author='Chris Gibson',
+    author_email='chris@chrisgibson.io',
+    url='https://github.com/chrisgibson17/django-battlenet-oauth2',
+    download_url='https://github.com/chrisgibson17/django-battlenet-oauth2/archive/master.tar.gz',
+)
