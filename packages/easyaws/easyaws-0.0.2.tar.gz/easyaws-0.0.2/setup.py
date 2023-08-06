e03bff50ffd1c861d3653fff9d14e6d61ea12970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package easyaws/0.0.2/easyaws-0.0.2.tar.gz

---
+++ packages/easyaws/easyaws-0.0.2.tar.gz/easyaws-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='easyaws',
+      version='0.0.2',
+      description='Easy to use AWS commands for developers (currently supports EC2; tools for S3 coming soon)',
+      packages=['easyaws'],
+      install_requires = ['awscli>=1.7.30'],
+      url='http://github.com/amberj/easyaws',
+      scripts = ["easyaws/ec2ssh", "easyaws/ec2list", "easyaws/easyaws"],
+      author='Amber Jain',
+      author_email='i.amber.jain@gmail.com',
+      license='ISC',
+      keywords = ["amazon", "aws", "ec2", "ami", "ssh", "cloud", "boto", "easy", "tools", "tool", "cli", "command", "devs", "developers"],
+      classifiers = [
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Topic :: Utilities"
+        ],
+      zip_safe=False)
