ef798245183dad55e6fb925dbfbe61d3028af85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:04 +0000

    Add package redisboard/0.0/redisboard-0.0.tar.gz

---
+++ packages/redisboard/redisboard-0.0.tar.gz/redisboard-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Ionel Cristian Maries',
+    author_email='contact@ionelmc.ro',
+    description='Redis monitoring and inspection drop-in application using django admin.',
+    install_requires=['django-redisboard'],
+    long_description='''Use `django-redisboard <https://pypi.python.org/pypi/django-redisboard/>`_ instead.''',
+    maintainer='Ionel Cristian Maries',
+    maintainer_email='contact@ionelmc.ro',
+    name='redisboard',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/django-redisboard/',
+    version="0.0",
+    zip_safe=False,
+)
