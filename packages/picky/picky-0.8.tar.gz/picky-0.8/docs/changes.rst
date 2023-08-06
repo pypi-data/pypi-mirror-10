0df690ec9e17ef161912a56ebe644f9d19152433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:32 +0000

    Add package picky/0.8/picky-0.8.tar.gz

---
+++ packages/picky/picky-0.8.tar.gz/picky-0.8/docs/changes.rst
@@ -0,0 +1,15 @@
+Changes
+=======
+
+0.8 (22 June 2015)
+------------------
+
+- Initial Release
+
+
+Still to Come
+=============
+
+- Python 3 support
+
+- Automated code coverage metrics
---
+++ packages/picky/picky-0.8.tar.gz/picky-0.8/picky/handlers.py
@@ -0,0 +1,122 @@
+from distutils.spawn import find_executable
+from logging import getLogger
+import os
+from subprocess import Popen, PIPE
+
+from picky.requirements import Requirements
+
+
+logger = getLogger(__name__)
+
+
+class Handler(object):
+
+    args = ()
+    name = None
+
+    @staticmethod
+    def parse_line(line):
+        raise NotImplementedError
+
+    @staticmethod
+    def serialise_line(name, version):
+        raise NotImplementedError
+
+    def read_source(self, if_, callable_, param, source):
+        if if_:
+            logger.info('Using %r for %s', param, self.name)
+            text = callable_(param)
+        else:
+            logger.debug('%r not found', param)
+            text = ''
+        return self.requirements(text, source)
+
+    def run_command(self, command):
+        process = Popen((command, )+self.args, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        if stderr:
+            logger.error('%s gave errors: %s', self.name, stderr)
+        return stdout
+
+    def read_file(self, path):
+        with open(path) as source:
+            return source.read()
+
+    def find_executable(self, command):
+        executable = find_executable(command)
+        if executable:
+            return True, os.path.abspath(executable)
+        else:
+            return False, command
+
+    def __init__(self, command, path):
+        self.executable_found, executable = self.find_executable(command)
+        path_exists = os.path.exists(path)
+        self.path = path
+
+        self.used = self.read_source(
+            if_=self.executable_found,
+            callable_=self.run_command,
+            param=executable,
+            source=' '.join((self.name, )+self.args)
+        )
+
+        self.specified = self.read_source(
+            if_=path_exists,
+            callable_=self.read_file,
+            param=path,
+            source=os.path.split(path)[-1]
+        )
+
+        if path_exists and not self.executable_found:
+            logger.error('%r found but %s missing', path, self.name)
+
+    def requirements(self, text, source):
+        return Requirements(text,
+                            self.parse_line,
+                            self.serialise_line,
+                            source)
+
+    def update(self, diff, when):
+        if self.executable_found:
+            if diff:
+                logger.warning('Updating %r', self.path)
+                self.specified.apply(diff, when)
+                with open(self.path, 'w') as target:
+                    target.write(self.specified.serialise())
+            else:
+                logger.debug('No differences to apply to %r', self.path)
+
+
+
+class PipHandler(Handler):
+
+    name = 'pip'
+    args = ('--disable-pip-version-check', 'freeze')
+
+    @staticmethod
+    def parse_line(line):
+        line = line.split('#')[0]
+        if '==' in line and not line.startswith('-e '):
+            return (p.strip() for p in line.split('=='))
+
+    @staticmethod
+    def serialise_line(name, version):
+        return name + '==' + version
+
+
+class CondaHandler(Handler):
+
+    name = 'conda'
+    args = ('list', '-e')
+
+    @staticmethod
+    def parse_line(line):
+        line = line.split('#')[0]
+        parts = [p.strip() for p in line.split('=')]
+        if len(parts) > 1:
+            return parts[:2]
+
+    @staticmethod
+    def serialise_line(name, version):
+        return name + '=' + version
