26125a715c80ce667a3fbe84988a379754da5d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:05 +0000

    Add package ignore-from-github/0.3.1/ignore-from-github-0.3.1.tar.gz

---
+++ packages/ignore-from-github/ignore-from-github-0.3.1.tar.gz/ignore-from-github-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import os.path
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+
+version = '0.3.1'
+
+with open(README) as fp:
+    longdesc = fp.read()
+
+setup(name='ignore-from-github',
+    include_package_data=True,
+    version=version,
+    description='Add common sets of ignored file types to your .gitignore easily',
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Intended Audience :: Developers'
+    ],
+    author='Anson Rosenthal',
+    author_email='anson.rosenthal@gmail.com',
+    license='MIT License',
+    url='https://github.com/anrosent/ignore.git',
+    scripts=['ignore']
+)
