9caa5746c8fae3c1d5885c2ba32671c5a47f60e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:34 +0000

    Add package pyTelegramBotAPI/0.1.9/pyTelegramBotAPI-0.1.9.tar.gz

---
+++ packages/pyTelegramBotAPI/pyTelegramBotAPI-0.1.9.tar.gz/pyTelegramBotAPI-0.1.9/telebot/__init__.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import json
+import time
+import threading
+
+from telebot import apihelper, types
+
+"""
+Module : telebot
+"""
+
+API_URL = r"https://api.telegram.org/"
+
+
+class TeleBot:
+    """ This is TeleBot Class
+    Methods:
+        getMe
+        sendMessage
+        forwardMessage
+        sendPhoto
+        sendAudio
+        sendDocument
+        sendSticker
+        sendVideo
+        sendLocation
+        sendChatAction
+        getUserProfilePhotos
+        getUpdates
+        setWebhook
+    """
+
+    def __init__(self, token):
+        self.token = token
+        self.update_listener = []
+        self.polling_thread = None
+        self.__stop_polling = False
+        self.interval = 3
+
+        self.last_update_id = 0
+
+    def get_update(self):
+        result = apihelper.get_updates(self.token, offset=(self.last_update_id + 1))
+        updates = result['result']
+        new_messages = []
+        for update in updates:
+            if update['update_id'] > self.last_update_id:
+                self.last_update_id = update['update_id']
+            msg = types.Message.de_json(json.dumps(update['message']))
+            new_messages.append(msg)
+
+        if len(new_messages) > 0:
+            self.__notify_update(new_messages)
+
+    def __notify_update(self, new_messages):
+        for listener in self.update_listener:
+            t = threading.Thread(target=listener, args=new_messages)
+            t.start()
+
+    def polling(self, interval=3):
+        """
+        Always get updates.
+        :param interval: interval secs.
+        :return:
+        """
+        self.interval = interval
+        # clear thread.
+        self.__stop_polling = True
+        time.sleep(interval + 1)
+        self.__stop_polling = False
+        self.polling_thread = threading.Thread(target=self.__polling, args=())
+        self.polling_thread.daemon = True
+        self.polling_thread.start()
+
+    def __polling(self):
+        print('telegram bot start polling')
+        while not self.__stop_polling:
+            try:
+                self.get_update()
+            except Exception as e:
+                print(e)
+            time.sleep(self.interval)
+
+        print('telegram bot stop polling')
+
+    def stop_polling(self):
+        self.__stop_polling = True
+
+    def set_update_listener(self, listener):
+        self.update_listener.append(listener)
+
+    def get_me(self):
+        result = apihelper.get_me(self.token)
+        return types.User.de_json(json.dumps(result['result']))
+
+    def get_user_profile_photos(self, user_id, offset=None, limit=None):
+        """
+        Retrieves the user profile photos of the person with 'user_id'
+        See https://core.telegram.org/bots/api#getuserprofilephotos
+        :param user_id:
+        :param offset:
+        :param limit:
+        :return:
+        """
+        result = apihelper.get_user_profile_photos(self.token, user_id, offset, limit)
+        return types.UserProfilePhotos.de_json(json.dumps(result['result']))
+
+    def send_message(self, chat_id, text, disable_web_page_preview=None, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send text messages.
+        :param chat_id:
+        :param text:
+        :param disable_web_page_preview:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_message(self.token, chat_id, text, disable_web_page_preview, reply_to_message_id,
+                                      reply_markup)
+
+    def forward_message(self, chat_id, from_chat_id, message_id):
+        """
+        Use this method to forward messages of any kind.
+        :param chat_id: which chat to forward
+        :param from_chat_id: which chat message from
+        :param message_id: message id
+        :return:
+        """
+        return apihelper.forward_message(self.token, chat_id, from_chat_id, message_id)
+
+    def send_photo(self, chat_id, photo, caption=None, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send photos.
+        :param chat_id:
+        :param photo:
+        :param caption:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_photo(self.token, chat_id, photo, caption, reply_to_message_id, reply_markup)
+
+    def send_audio(self, chat_id, data, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send audio files, if you want Telegram clients to display the file as a playable
+        voice message. For this to work, your audio must be in an .ogg file encoded with OPUS
+        :param chat_id:
+        :param data:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_data(self.token, chat_id, data, 'audio', reply_to_message_id, reply_markup)
+
+    def send_document(self, chat_id, data, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send general files.
+        :param chat_id:
+        :param data:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_data(self.token, chat_id, data, 'document', reply_to_message_id, reply_markup)
+
+    def send_sticker(self, chat_id, data, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send .webp stickers.
+        :param chat_id:
+        :param data:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_data(self.token, chat_id, data, 'sticker', reply_to_message_id, reply_markup)
+
+    def send_video(self, chat_id, data, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send video files, Telegram clients support mp4 videos.
+        :param chat_id:
+        :param data:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_data(self.token, chat_id, data, 'video', reply_to_message_id, reply_markup)
+
+    def send_location(self, chat_id, latitude, longitude, reply_to_message_id=None, reply_markup=None):
+        """
+        Use this method to send point on the map.
+        :param chat_id:
+        :param latitude:
+        :param longitude:
+        :param reply_to_message_id:
+        :param reply_markup:
+        :return:
+        """
+        return apihelper.send_location(self.token, chat_id, latitude, longitude, reply_to_message_id, reply_markup)
+
+    def send_chat_action(self, chat_id, action):
+        """
+        Use this method when you need to tell the user that something is happening on the bot's side.
+        The status is set for 5 seconds or less (when a message arrives from your bot, Telegram clients clear
+        its typing status).
+        :param chat_id:
+        :param action: string . typing,upload_photo,record_video,upload_video,record_audio,upload_audio,upload_document,
+                                find_location.
+        :return:
+        """
+        return apihelper.send_chat_action(self.token, chat_id, action)
+
---
+++ packages/pyTelegramBotAPI/pyTelegramBotAPI-0.1.9.tar.gz/pyTelegramBotAPI-0.1.9/telebot/types.py
@@ -0,0 +1,380 @@
+# -*- coding: utf-8 -*-
+"""
+Available types
+
+User
+GroupChat
+Message
+PhotoSize
+Audio
+Document
+Sticker
+Video
+Contact
+Location
+Update
+InputFile
+UserProfilePhotos
+ReplyKeyboardMarkup
+ReplyKeyboardHide
+ForceReply
+"""
+
+import json
+
+
+class Jsonable:
+    """
+    Subclasses of this class are guaranteed to be able to be converted to JSON format.
+    All subclasses of this class must override to_json.
+    """
+
+    def to_json(self):
+        """
+        Returns a JSON string representation of this class.
+
+        This function must be overridden by subclasses.
+        :return: a JSON formatted string.
+        """
+        raise NotImplementedError
+
+
+class User:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        id = obj['id']
+        first_name = obj['first_name']
+        last_name = None
+        username = None
+        if 'last_name' in obj:
+            last_name = obj['last_name']
+        if 'username' in obj:
+            username = obj['username']
+        return User(id, first_name, last_name, username)
+
+    def __init__(self, id, first_name, last_name=None, username=None):
+        self.id = id
+        self.first_name = first_name
+        self.username = username
+        self.last_name = last_name
+
+
+class GroupChat:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        id = obj['id']
+        title = obj['title']
+        return GroupChat(id, title)
+
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+
+
+class Message:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        message_id = obj['message_id']
+        from_user = User.de_json(json.dumps(obj['from']))
+        chat = Message.parse_chat(obj['chat'])
+        date = obj['date']
+        content_type = None
+        opts = {}
+        if 'text' in obj:
+            opts['text'] = obj['text']
+            content_type = 'text'
+        if 'audio' in obj:
+            opts['audio'] = Audio.de_json(json.dumps(obj['audio']))
+            content_type = 'audio'
+        if 'document' in obj:
+            opts['document'] = Document.de_json(json.dumps(obj['document']))
+            content_type = 'document'
+        if 'photo' in obj:
+            opts['photo'] = Message.parse_photo(obj['photo'])
+            content_type = 'photo'
+        if 'sticker' in obj:
+            opts['sticker'] = Sticker.de_json(json.dumps(obj['sticker']))
+            content_type = 'sticker'
+        if 'video' in obj:
+            opts['video'] = Video.de_json(json.dumps(obj['video']))
+            content_type = 'video'
+        if 'location' in obj:
+            opts['location'] = Location.de_json(json.dumps(obj['location']))
+            content_type = 'location'
+        if 'contact' in obj:
+            opts['contact'] = Contact.de_json(json.dumps(obj['contact']))
+            content_type = 'contact'
+        return Message(message_id, from_user, date, chat, content_type, opts)
+
+    @classmethod
+    def parse_chat(cls, chat):
+        if 'first_name' not in chat:
+            return GroupChat.de_json(json.dumps(chat))
+        else:
+            return User.de_json(json.dumps(chat))
+
+    @classmethod
+    def parse_photo(cls, photo_size_array):
+        ret = []
+        for ps in photo_size_array:
+            ret.append(PhotoSize.de_json(json.dumps(ps)))
+        return ret
+
+    def __init__(self, message_id, from_user, date, chat, content_type, options):
+        self.chat = chat
+        self.date = date
+        self.fromUser = from_user
+        self.message_id = message_id
+        self.content_type = content_type
+        for key in options:
+            setattr(self, key, options[key])
+
+
+class PhotoSize:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        file_id = obj['file_id']
+        width = obj['width']
+        height = obj['height']
+        file_size = None
+        if 'file_size' in obj:
+            file_size = obj['file_size']
+        return PhotoSize(file_id, width, height, file_size)
+
+    def __init__(self, file_id, width, height, file_size=None):
+        self.file_size = file_size
+        self.height = height
+        self.width = width
+        self.file_id = file_id
+
+
+class Audio:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        file_id = obj['file_id']
+        duration = obj['duration']
+        mime_type = None
+        file_size = None
+        if 'mime_type' in obj:
+            mime_type = obj['mime_type']
+        if 'file_size' in obj:
+            file_size = obj['file_size']
+        return Audio(file_id, duration, mime_type, file_size)
+
+    def __init__(self, file_id, duration, mime_type=None, file_size=None):
+        self.file_id = file_id
+        self.duration = duration
+        self.mime_type = mime_type
+        self.file_size = file_size
+
+
+class Document:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        file_id = obj['file_id']
+        thumb = None
+        if 'file_id' in obj['thumb']:
+            thumb = PhotoSize.de_json(json.dumps(obj['thumb']))
+        file_name = None
+        mime_type = None
+        file_size = None
+        if 'file_name' in obj:
+            file_name = obj['file_name']
+        if 'mine_type' in obj:
+            mime_type = obj['mime_type']
+        if 'file_size' in obj:
+            file_size = obj['file_size']
+        return Document(file_id, thumb, file_name, mime_type, file_size)
+
+    def __init__(self, file_id, thumb, file_name=None, mime_type=None, file_size=None):
+        self.file_id = file_id
+        self.thumb = thumb
+        self.file_name = file_name
+        self.mime_type = mime_type
+        self.file_size = file_size
+
+
+class Sticker:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        file_id = obj['file_id']
+        width = obj['width']
+        height = obj['height']
+        thumb = PhotoSize.de_json(json.dumps(obj['thumb']))
+        file_size = None
+        if 'file_size' in obj:
+            file_size = obj['file_size']
+        return Sticker(file_id, width, height, thumb, file_size)
+
+    def __init__(self, file_id, width, height, thumb, file_size=None):
+        self.file_id = file_id
+        self.width = width
+        self.height = height
+        self.thumb = thumb
+        self.file_size = file_size
+
+
+class Video:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        file_id = obj['file_id']
+        width = obj['width']
+        height = obj['height']
+        duration = obj['duration']
+        if 'file_id' in obj['thumb']:
+            thumb = PhotoSize.de_json(json.dumps(obj['thumb']))
+        caption = None
+        mime_type = None
+        file_size = None
+        if 'caption' in obj:
+            caption = obj['caption']
+        if 'mine_type' in obj:
+            mime_type = obj['mime_type']
+        if 'file_size' in obj:
+            file_size = obj['file_size']
+        return Video(file_id, width, height, duration, thumb, mime_type, file_size, caption)
+
+    def __init__(self, file_id, width, height, duration, thumb, mime_type=None, file_size=None, caption=None):
+        self.file_id = file_id
+        self.width = width
+        self.height = height
+        self.duration = duration
+        self.thumb = thumb
+        self.mime_type = mime_type
+        self.file_size = file_size
+        self.caption = caption
+
+
+class Contact:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        phone_number = obj['phone_number']
+        first_name = obj['first_name']
+        last_name = None
+        if 'last_name' in obj:
+            last_name = obj['last_name']
+        if 'user_id' in obj:
+            user_id = obj['user_id']
+        return Contact(phone_number, first_name, last_name, user_id)
+
+    def __init__(self, phone_number, first_name, last_name=None, user_id=None):
+        self.phone_number = phone_number
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_id = user_id
+
+
+class Location:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        longitude = obj['longitude']
+        latitude = obj['latitude']
+        return Location(longitude, latitude)
+
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
+
+
+class UserProfilePhotos:
+    @classmethod
+    def de_json(cls, json_string):
+        obj = json.loads(json_string)
+        total_count = obj['total_count']
+        photos = [[PhotoSize.de_json(json.dumps(y)) for y in x] for x in obj['photos']]
+        return UserProfilePhotos(total_count, photos)
+
+    def __init__(self, total_count, photos):
+        self.total_count = total_count
+        self.photos = photos
+
+
+class ForceReply(Jsonable):
+    def __init__(self, selective=None):
+        self.selective = selective
+
+    def to_json(self):
+        json_dict = {'force_reply': True}
+        if self.selective:
+            json_dict['selective'] = True
+        return json.dumps(json_dict)
+
+
+class ReplyKeyboardHide(Jsonable):
+    def __init__(self, selective=None):
+        self.selective = selective
+
+    def to_json(self):
+        json_dict = {'hide_keyboard': True}
+        if self.selective:
+            json_dict['selective'] = True
+        return json.dumps(json_dict)
+
+
+class ReplyKeyboardMarkup(Jsonable):
+    def __init__(self, resize_keyboard=None, one_time_keyboard=None, selective=None, row_width=3):
+        self.resize_keyboard = resize_keyboard
+        self.one_time_keyboard = one_time_keyboard
+        self.selective = selective
+        self.row_width = row_width
+
+        self.keyboard = []
+
+    def add(self, *args):
+        """
+        This function adds strings to the keyboard, while not exceeding row_width.
+        E.g. ReplyKeyboardMarkup#add("A", "B", "C") yields the json result {keyboard: [["A"], ["B"], ["C"]]}
+        when row_width is set to 1.
+        When row_width is set to 2, the following is the result of this function: {keyboard: [["A", "B"], ["C"]]}
+        See https://core.telegram.org/bots/api#replykeyboardmarkup
+        :param args: strings to append to the keyboard
+        """
+        i = 1
+        row = []
+        for string in args:
+            row.append(string)
+            if i % self.row_width == 0:
+                self.keyboard.append(row)
+                row = []
+            i += 1
+        if len(row) > 0:
+            self.keyboard.append(row)
+
+    def row(self, *args):
+        """
+        Adds a list of strings to the keyboard. This function does not consider row_width.
+        ReplyKeyboardMarkup#row("A")#row("B", "C")#to_json() outputs '{keyboard: [["A"], ["B", "C"]]}'
+        See https://core.telegram.org/bots/api#replykeyboardmarkup
+        :param args: strings
+        :return: self, to allow function chaining.
+        """
+        self.keyboard.append(args)
+        return self
+
+    def to_json(self):
+        """
+        Converts this object to its json representation following the Telegram API guidelines described here:
+        https://core.telegram.org/bots/api#replykeyboardmarkup
+        :return:
+        """
+        json_dict = {'keyboard': self.keyboard}
+        if self.one_time_keyboard:
+            json_dict['one_time_keyboard'] = True
+
+        if self.resize_keyboard:
+            json_dict['resize_keyboard'] = True
+
+        if self.selective:
+            json_dict['selective'] = True
+
+        return json.dumps(json_dict)
