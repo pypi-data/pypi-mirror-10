57a25f1f94d40d236b74ac4996c6159e923dde84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:59 +0000

    Add package wtforms-webwidgets/0.0.3/wtforms-webwidgets-0.0.3.tar.gz

---
+++ packages/wtforms-webwidgets/wtforms-webwidgets-0.0.3.tar.gz/wtforms-webwidgets-0.0.3/wtforms_webwidgets/__init__.py
@@ -0,0 +1,14 @@
+"""
+WTForms Extended Widgets
+########################
+
+This package aims to one day eventually contain advanced widgets for all the
+common web UI frameworks.
+
+Currently this module contains widgets for:
+ 
+ - Boostrap
+
+"""
+
+from .common import *
