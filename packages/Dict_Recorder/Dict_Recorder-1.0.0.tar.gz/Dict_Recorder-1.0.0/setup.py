a1a8f8371a88cb66142929b9722eb49e6dc58614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:39 +0000

    Add package Dict_Recorder/1.0.0/Dict_Recorder-1.0.0.tar.gz

---
+++ packages/Dict_Recorder/Dict_Recorder-1.0.0.tar.gz/Dict_Recorder-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'Dict_Recorder',
+	version		= '1.0.0',
+	py_modules	= ['Dict_Recorder'],
+	author		= 'James Martiuk',
+	author_email	= 'jmartiuk5@gmail.com',
+	url		= 'learningpython.com',
+	description	= 'A simple printer of nested lists',
+)
