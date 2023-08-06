03f72739dcec55ca76f9c10f4ea95db08a81a0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package easysnmp/0.2.2/easysnmp-0.2.2.tar.gz

---
+++ packages/easysnmp/easysnmp-0.2.2.tar.gz/easysnmp-0.2.2/easysnmp/compat.py
@@ -0,0 +1,23 @@
+import sys
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    text_type = str
+
+    def ub(s):
+        return s
+
+    def urepr(s):
+        return repr(s)
+else:
+    text_type = unicode  # noqa
+
+    def ub(s):
+        return s.decode('latin-1')
+
+    def urepr(s):
+        if isinstance(s, unicode):  # noqa
+            return repr(s)[1:]
+        else:
+            return repr(s)
