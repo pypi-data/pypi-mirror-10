dc015cbe08c1709ceea7340de8e38effda8f5820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:33 +0000

    Add package django-whoshere/0.1.6.2/django-whoshere-0.1.6.2.tar.gz

---
+++ packages/django-whoshere/django-whoshere-0.1.6.2.tar.gz/django-whoshere-0.1.6.2/CHANGELOG.md
@@ -0,0 +1,28 @@
+#Changelog
+
+##0.1.6.2
+- Added 24 hour cache on IP lookups
+
+##0.1.6.1
+- Added timeout on Telize lookups to prevent hangups
+
+##0.1.6
+- Added tracked request path
+
+##0.1.5
+- Added template tags
+
+###0.1.4
+- Replaced urllib with requests for better ssl support
+
+###0.1.3
+ - Added geolocation with Telize
+
+###0.1.2
+ - Added support for better ip lookups with django-ipware
+
+###0.1.1
+ - Added Tests
+ 
+##0.1.0
+ - Initial commit
---
+++ packages/django-whoshere/django-whoshere-0.1.6.2.tar.gz/django-whoshere-0.1.6.2/django_whoshere/middleware.py
@@ -0,0 +1,20 @@
+from django.core.cache import cache
+
+from django_whoshere.apps import TIMEOUT, PREFIX, get_ip
+
+
+class TrackMiddleware(object):
+
+    @staticmethod
+    def process_request(request):
+        """
+        Checks for authenticated user and adds ip and user-agent to the cache
+        """
+        if not hasattr(request, 'user') or not request.user:
+            return
+        key = '{}:{}'.format(PREFIX, request.user.pk)
+        # Add ip and user agent to cache if it's not there
+        if not cache.get(key):
+            cache.set(key,
+                      {'ip': get_ip(request), 'agent': request.META.get('HTTP_USER_AGENT', ''), 'path': request.path},
+                      TIMEOUT)
