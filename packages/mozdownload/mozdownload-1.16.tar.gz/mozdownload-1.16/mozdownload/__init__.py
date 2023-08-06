ded23a78b01401fb571073d9d6e1ffb1c6a09f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:02 +0000

    Add package mozdownload/1.16/mozdownload-1.16.tar.gz

---
+++ packages/mozdownload/mozdownload-1.16.tar.gz/mozdownload-1.16/mozdownload/__init__.py
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from .scraper import (Scraper,
+                      DailyScraper,
+                      DirectScraper,
+                      ReleaseScraper,
+                      ReleaseCandidateScraper,
+                      TinderboxScraper,
+                      TryScraper,
+                      )
+
+__all__ = [Scraper,
+           DailyScraper,
+           DirectScraper,
+           ReleaseScraper,
+           ReleaseCandidateScraper,
+           TinderboxScraper,
+           TryScraper,
+           ]
