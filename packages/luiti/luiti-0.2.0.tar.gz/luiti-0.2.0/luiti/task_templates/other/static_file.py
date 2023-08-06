b3feb01575745ac95c0efb2b884aa7522886cf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:10 +0000

    Add package luiti/0.2.0/luiti-0.2.0.tar.gz

---
+++ packages/luiti/luiti-0.2.0.tar.gz/luiti-0.2.0/luiti/task_templates/other/static_file.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8-*-
+
+
+from ...luigi_decorators import luigi
+from ...utils import TargetUtils
+
+
+class StaticFile(luigi.Task):
+    filepath = None
+
+    # Mimic default luigi.ExternalTask
+    def run(self):
+        pass
+
+    def complete(self):
+        return True
+
+    def output(self):
+        assert self.filepath, u"Please assign `filepath` !"
+        return TargetUtils.hdfs(self.filepath)
