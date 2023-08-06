2ac08dead8ee4787ff3fd38afb41ad153e337249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:02 +0000

    Add package khipu/0.0.5/khipu-0.0.5.tar.gz

---
+++ packages/khipu/khipu-0.0.5.tar.gz/khipu-0.0.5/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+exec(open("khipu/version.py").read())
+
+setup(
+    name='khipu',
+    version=__version__,
+    description='A client for the khipu API.',
+    author='Jonatan Rodriguez',
+    author_email='jrperdomoz@gmail.com',
+    maintainer='Jonatan Rodriguez',
+    maintainer_email='jrperdomoz@gmail.com',
+    url='https://github.com/jrperdomoz/khipu',
+    license="BSD",
+    packages=['khipu'],
+    # long_description=open("README.md").read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Programming Language :: Python :: 2.7'
+        # 'License :: OSI Approved :: Apache Software License',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=[
+        'requests',
+    ],
+    keywords='khipu chile',
+)
