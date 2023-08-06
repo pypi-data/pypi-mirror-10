f16ca7b099c64ff0de87ac999265acf59b691a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:12 +0000

    Add package django-conditional-aggregates/0.3.2/django-conditional-aggregates-0.3.2.tar.gz

---
+++ packages/django-conditional-aggregates/django-conditional-aggregates-0.3.2.tar.gz/django-conditional-aggregates-0.3.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-conditional-aggregates',
+    version='0.3.2',
+    packages=['aggregates'],
+    license='MIT',
+    long_description=open('README.rst').read(),
+    description=(
+        'Django aggregate functions which operate conditionally (i.e. generate'
+        ' SQL `CASE` statements)'
+    ),
+    author='Anentropic',
+    author_email='ego@anentropic.com',
+    url='https://github.com/anentropic/django-conditional-aggregates',
+    install_requires=[],
+)
