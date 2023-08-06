3afe0b00fb52ba4ca2734957c644b4f3a83afb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:54 +0000

    Add package colibri2trilegal/1.4/colibri2trilegal-1.4.tar.gz

---
+++ packages/colibri2trilegal/colibri2trilegal-1.4.tar.gz/colibri2trilegal-1.4/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='colibri2trilegal',
+    author='Phil Rosenfield',
+    author_email='philip.rosenfield@unipd.it',
+    version='1.4',
+    py_modules=['colibri2trilegal'],
+    scripts=['colibri2trilegal'],
+    install_requires=['matplotlib', 'numpy']
+)
