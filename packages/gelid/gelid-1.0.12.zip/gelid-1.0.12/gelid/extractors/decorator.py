5ef576da9c6b355fc382b5a1d0b0d12f87c2c380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:50 +0000

    Add package gelid/1.0.12/gelid-1.0.12.zip

---
+++ packages/gelid/gelid-1.0.12.zip/gelid-1.0.12/gelid/extractors/decorator.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+# lvjiyong on 2015/4/25.
+
+from gelid.extractors import html
+
+
+def store(func):
+    def modify(*args, **kwargs):
+        page = args[0]
+        key = func.__name__
+        value = page.stores.get(key)
+        if not value:
+            value = func(*args, **kwargs)
+            if value:
+                page.stores[key] = value
+        return value
+    return modify
