c0f3f4db17441741cd5110f26d06ec941788c585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:28 +0000

    Add package pypi_test/1.0.0/pypi_test-1.0.0.tar.gz

---
+++ packages/pypi_test/pypi_test-1.0.0.tar.gz/pypi_test-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'pypi_test',
+    version      = '1.0.0',
+    py_modules   = ['pypi_test'],
+    author       = 'obuy',
+    author_email = 'yubo.mailbox@gmail.com',
+    url          = 'www.obuy.com',
+    description  = 'A simple printer of pypi test lists',
+    )
