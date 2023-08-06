22898eaff0a57e4ad8346dc466f6500e55b304d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:55 +0000

    Add package WebPages/1.0.0a1/WebPages-1.0.0a1-py2-none-any.whl

---
+++ packages/WebPages/WebPages-1.0.0a1-py2-none-any.whl/webpages/middleware/session.py
@@ -0,0 +1,10 @@
+
+
+def Session(request):
+    """
+    Restore session based on Cookie session_id.
+
+    Set `request.session` with LazySession to fetch data from
+    database when we access it.
+    """
+    pass
