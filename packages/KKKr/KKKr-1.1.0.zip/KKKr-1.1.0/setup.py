b4275b41b4fac89763a6e18e2db6b8751f2317f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:53 +0000

    Add package KKKr/1.1.0/KKKr-1.1.0.zip

---
+++ packages/KKKr/KKKr-1.1.0.zip/KKKr-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		 name = 'KKKr',
+		 version = '1.1.0',
+		 py_modules = ['nester'],
+		 author = 'hfpython',
+		 author_email = '@qq.com',
+		 url = 'http://',
+		 description = "A simple printer",
+	  )
