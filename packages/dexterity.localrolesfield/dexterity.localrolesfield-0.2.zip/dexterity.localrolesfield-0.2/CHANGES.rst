2c3707b75e348af2ad52e6539628427d435db6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package dexterity.localrolesfield/0.2/dexterity.localrolesfield-0.2.zip

---
+++ packages/dexterity.localrolesfield/dexterity.localrolesfield-0.2.zip/dexterity.localrolesfield-0.2/CHANGES.rst
@@ -0,0 +1,21 @@
+Changelog
+=========
+
+0.2 (2015-06-02)
+----------------
+
+- Avoid exception on site deletion.
+  [sgeulette]
+
+- Add single value local role field.
+  [cedricmessiant]
+
+- Also lookup behaviors when searching for local role fields.
+  [cedricmessiant]
+
+
+0.1 (2014-10-24)
+----------------
+
+- Initial release
+  [mpeeters]
---
+++ packages/dexterity.localrolesfield/dexterity.localrolesfield-0.2.zip/dexterity.localrolesfield-0.2/src/dexterity/localrolesfield/utils.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Utils."""
+from zope.component import getUtility
+
+from plone.behavior.interfaces import IBehavior
+from plone.dexterity.interfaces import IDexterityFTI
+
+from dexterity.localrolesfield.interfaces import IBaseLocalRoleField
+
+
+def get_localrole_fields(fti):
+    """Get all local role(s) fields for given fti.
+
+    Lookup local role(s) fields on content from its schema and its behaviors.
+    Return field name and field object for each found field.
+    """
+    fti_schema = fti.lookupSchema()
+    fields = [(n, f) for n, f in fti_schema.namesAndDescriptions(all=True)
+              if IBaseLocalRoleField.providedBy(f)]
+
+    # also lookup behaviors
+    for behavior_id in fti.behaviors:
+        behavior = getUtility(IBehavior, behavior_id).interface
+        fields.extend(
+            [(n, f) for n, f in behavior.namesAndDescriptions(all=True)
+             if IBaseLocalRoleField.providedBy(f)])
+
+    return fields
