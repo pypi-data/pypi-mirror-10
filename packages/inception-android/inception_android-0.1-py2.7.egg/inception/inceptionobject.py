3b6e8990a3b55b97ea43717933de9c60dc4c4062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:41 +0000

    Add package inception-android/0.1/inception_android-0.1-py2.7.egg

---
+++ packages/inception-android/inception_android-0.1-py2.7.egg/inception/inceptionobject.py
@@ -0,0 +1,80 @@
+import subprocess, os
+
+class InceptionExecCmdFailedException(Exception):
+    pass
+
+class NoWorkDirException(Exception):
+    pass
+
+class InceptionObject(object):
+
+    ADB_INSTANCE = None
+
+    def __init__(self):
+        self.workDir = None
+        self.outDir = None
+        self.logCmd = []
+
+    def d(self, *messages):
+        print("%s:\t%s" % (self.__class__.__name__, "\t".join(messages) ))
+
+    def getAdb(self, busybox = False):
+        if not InceptionObject.ADB_INSTANCE:
+            from tools.adbwrapper import Adb
+            InceptionObject.ADB_INSTANCE = Adb()
+        InceptionObject.ADB_INSTANCE.setBusyBoxCmds(busybox)
+        return InceptionObject.ADB_INSTANCE
+
+    def writeCmdLog(self, out):
+        f = open(out, "w")
+        f.write("\n".join(self.logCmd))
+        f.close()
+
+    def execCmd(self, *cmd, **kwargs):
+        #cmd = " ".join(cmd)
+        failMessage = None if "failMessage" not in kwargs else kwargs["failMessage"]
+        cwd = "." if "cwd" not in kwargs else kwargs["cwd"]
+        stdin = None if "stdin" not in kwargs else kwargs["stdin"]
+        stdout = None if "stdout" not in kwargs else kwargs["stdout"]
+        preview = False if "preview" not in kwargs else kwargs["preview"]
+
+        cmdStr = " ".join(cmd)
+        self.d(cmdStr)
+        self.logCmd.append(cmdStr)
+        if not preview:
+            try:
+                if stdout is not None:
+                    result = subprocess.call(cmd, cwd = cwd, stdin = stdin, stdout = stdout, shell = "|" in cmd)
+                else:
+                    result = subprocess.check_output(cmd, cwd = cwd, stdin = stdin)
+            except OSError as e:
+                raise InceptionExecCmdFailedException(e)
+            except subprocess.CalledProcessError as e:
+                raise InceptionExecCmdFailedException(failMessage or e)
+
+            #if result != 0 and failMessage is not None:
+            #    raise InceptionExecCmdFailedException(failMessage)
+            self.d(str(result))
+            return result
+        else:
+            return True
+
+    def createDir(self, d):
+        if not os.path.exists(d):
+            self.d("Creating:", d)
+            os.makedirs(d)
+        else:
+            self.d("Exists:", d)
+
+    def createPathString(self, *args):
+        return os.path.join(*args)
+
+
+    def setWorkDir(self, workDir):
+        self.workDir = workDir
+
+
+    def getWorkDir(self, *append):
+        if not self.workDir:
+            raise NoWorkDirException()
+        return os.path.join(self.workDir, *append)
