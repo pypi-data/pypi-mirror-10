43c975997ab87bf5ece6dcf6c98dd2e49b3b091d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:41 +0000

    Add package clif/0.2.0/clif-0.2.0.tar.gz

---
+++ packages/clif/clif-0.2.0.tar.gz/clif-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='clif',
+    version='0.2.0',
+    author='François Ménabé',
+    author_email='francois.menabe@gmail.com',
+    license='MIT License',
+    description='Framework for generating command-line',
+    long_description=open('README.rst').read(),
+    keywords=['command-line', 'argparse', 'wrapper', 'clg', 'framework'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    packages=['clif'],
+    install_requires=['clg', 'pyyaml', 'yamlordereddictloader'])
