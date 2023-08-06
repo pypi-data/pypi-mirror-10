7ea9c626583a7defa2469c884add95da4d502f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:03 +0000

    Add package funniest_pan/0.1/funniest_pan-0.1.tar.gz

---
+++ packages/funniest_pan/funniest_pan-0.1.tar.gz/funniest_pan-0.1/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'panc'
+
+from setuptools import setup
+
+setup(
+    name='funniest_pan',
+    version='0.1',
+    description='The funniest joke in the world adapted by Pan',
+    url='http://github.com/panc/funniest_pan',
+    author='panc',
+    author_email='panc@example.com',
+    license='Purdue',
+    packages=['funniest'],
+    zip_safe=False)
