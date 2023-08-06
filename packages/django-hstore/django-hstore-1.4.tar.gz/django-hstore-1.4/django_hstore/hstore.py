52d4311eb360727e85219166b53e32eb4c71a35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:13 +0000

    Add package django-hstore/1.4/django-hstore-1.4.tar.gz

---
+++ packages/django-hstore/django-hstore-1.4.tar.gz/django-hstore-1.4/django_hstore/hstore.py
@@ -0,0 +1,12 @@
+from django_hstore.fields import DictionaryField, ReferencesField, SerializedDictionaryField  # noqa
+from django_hstore.managers import HStoreManager  # noqa
+from django_hstore.apps import GEODJANGO_INSTALLED
+
+
+if GEODJANGO_INSTALLED:
+    from django_hstore.managers import HStoreGeoManager  # noqa
+
+
+import django
+if django.get_version() < '1.7':
+    from . import apps  # noqa
