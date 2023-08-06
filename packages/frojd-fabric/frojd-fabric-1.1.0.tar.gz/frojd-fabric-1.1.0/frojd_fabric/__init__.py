63ec8283251965dabe310a06da964390a0b3aa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:31 +0000

    Add package frojd-fabric/1.1.0/frojd-fabric-1.1.0.tar.gz

---
+++ packages/frojd-fabric/frojd-fabric-1.1.0.tar.gz/frojd-fabric-1.1.0/frojd_fabric/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+frojd_fabric
+------------
+
+:copyright: (c) 2014-2015 by Fröjd Interactive AB
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "frojd_fabric"
+__version__ = "1.1.0"
+__build__ = 110
+__license__ = "MIT"
+__copyright__ = "Copyright 2014-2015 Fröjd Interactive AB"
