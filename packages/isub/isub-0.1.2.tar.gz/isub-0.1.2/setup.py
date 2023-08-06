a389f1da59d3160905d1936716b7d14814b1559b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package isub/0.1.2/isub-0.1.2.tar.gz

---
+++ packages/isub/isub-0.1.2.tar.gz/isub-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+        name='isub',
+        version="0.1.2",
+        entry_points = {
+            'console_scripts':
+            ['isub=isub:main'],
+            },
+        description="A utility CLI for TORQUE, a portable batch system",
+        url="https://bitbucket.org/kmiya/isub",
+        author="KoheiMiyaguchi",
+        author_email="quote.curly@gmail.com",
+        classifiers=[
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Utilities'
+            ]
+        )
