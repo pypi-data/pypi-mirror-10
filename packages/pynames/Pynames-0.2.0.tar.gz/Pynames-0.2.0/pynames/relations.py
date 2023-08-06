df6b0c0f9d8450f7ee7a5d5dad7f8b7b12ea4a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:36 +0000

    Add package pynames/0.2.0/Pynames-0.2.0.tar.gz

---
+++ packages/pynames/Pynames-0.2.0.tar.gz/Pynames-0.2.0/pynames/relations.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+class GENDER:
+    MALE = 'm'
+    FEMALE = 'f'
+    MF = ['m', 'f']
+
+    ALL = ['m', 'f']
+
+
+class LANGUAGE:
+    RU = 'ru'
+    EN = 'en'
+    NATIVE = 'native'
+
+    ALL = ['ru', 'en', 'native']
+
+
+LANGUAGE_FORMS_LANGTH = {LANGUAGE.RU: 12}
