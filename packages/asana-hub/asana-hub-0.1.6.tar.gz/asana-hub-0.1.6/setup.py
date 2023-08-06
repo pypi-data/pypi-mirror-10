e3f96481ad0ada2041f21cc2e2d65692a5acdd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:35 +0000

    Add package asana-hub/0.1.6/asana-hub-0.1.6.tar.gz

---
+++ packages/asana-hub/asana-hub-0.1.6.tar.gz/asana-hub-0.1.6/setup.py
@@ -0,0 +1,28 @@
+import os
+
+__VERSION__ = "0.1.6"
+
+from distutils.core import setup
+setup(
+  name = 'asana-hub',
+  packages = ['asana_hub'],
+  version = __VERSION__,
+  description = 'A python lib & tool for creating issues and tasks simultaneously on github and asana, and keeping them in sync.',
+  license = 'MIT',
+  author = 'Josh Whelchel',
+  author_email = 'josh+asanahub@loudr.fm',
+  url = 'https://github.com/loudr/asana-hub',
+  download_url = 'https://github.com/loudr/asana-hub/tarball/0.1.6',
+  keywords = ['github', 'asana', 'connect'],
+  classifiers = [],
+  requires = [
+    'asana',
+    'PyGithub',
+    'certifi',
+    'requests',
+    'urllib3'
+  ],
+  scripts=[os.path.join(".", name) for name in [
+      "asana-hub",
+    ]]
+)
