5be4e7925060e668d6027aaeb19bc3e995d04113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:12 +0000

    Add package isitup/0.2.0/isitup-0.2.0.tar.gz

---
+++ packages/isitup/isitup-0.2.0.tar.gz/isitup-0.2.0/isitup/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+import requests
+
+
+def check(url):
+    response = requests.get(
+        "https://isitup.org/{0}.json".format(url),
+        headers={'User-Agent': 'https://github.com/lord63/isitup'}
+    ).json()
+    status_code = response["status_code"]
+    if status_code == 1:
+        time, code, ip = (response['response_time'], response['response_code'],
+                          response['response_ip'])
+        return ("Yay, {0} is up.\nIt took {1} ms for a {2} response code "
+                "with an ip of {3}".format(url, time, code, ip))
+    if status_code == 2:
+        return "{0} seems to be down!"
+    if status_code == 3:
+        return "We need a valid domain to check! Try again."
