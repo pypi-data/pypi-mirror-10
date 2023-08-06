84cf14e1fc2019ac983a2bf2fee840e375695061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:12 +0000

    Add package boxsdk/1.2.0/boxsdk-1.2.0.tar.gz

---
+++ packages/boxsdk/boxsdk-1.2.0.tar.gz/boxsdk-1.2.0/boxsdk/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+from .auth.jwt_auth import JWTAuth
+from .auth.oauth2 import OAuth2
+from .client import Client
+from .object import *  # pylint:disable=wildcard-import,redefined-builtin
