b80781fa4e4c976bb8c461e0bd94b4da8bdfc12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:05 +0000

    Add package tc_shortener/0.2.1/tc_shortener-0.2.1.tar.gz

---
+++ packages/tc_shortener/tc_shortener-0.2.1.tar.gz/tc_shortener-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tc_shortener',
+    version='0.2.1',
+    url='http://github.com/thumbor-community/shortener',
+    license='MIT',
+    author='Thumbor Community',
+    description='URL Shortener',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'thumbor',
+        'tc_core',
+        'shortuuid'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
