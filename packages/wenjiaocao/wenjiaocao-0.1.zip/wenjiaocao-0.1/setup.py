0fd96f9ae34b9eae8e61de246e2d65cbf26feb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:04 +0000

    Add package wenjiaocao/0.1/wenjiaocao-0.1.zip

---
+++ packages/wenjiaocao/wenjiaocao-0.1.zip/wenjiaocao-0.1/setup.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(name='wenjiaocao', version='0.1', description='who is she', url='http://www.caixin.com/', author='qianlong.yang',
+      author_email='626568549@qq.com', license='MIT', packages=['wenjiaocao'], zip_safe=False)
---
+++ packages/wenjiaocao/wenjiaocao-0.1.zip/wenjiaocao-0.1/wenjiaocao/who.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+def sheis():
+    return 'She is an editor in caixin.com'
+
