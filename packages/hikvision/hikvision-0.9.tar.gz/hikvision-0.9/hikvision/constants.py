291c75acf982f35a8bfb0c3ee49ffe6bdd0ab73f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:33 +0000

    Add package hikvision/0.9/hikvision-0.9.tar.gz

---
+++ packages/hikvision/hikvision-0.9.tar.gz/hikvision-0.9/hikvision/constants.py
@@ -0,0 +1,17 @@
+"""
+hikvision.constants
+~~~~~~~~~~~~~~~~~~~~
+
+List of constants
+
+Copyright (c) 2015 Finbarr Brady <https://github.com/fbradyirl>
+Licensed under the MIT license.
+"""
+
+DEFAULT_PORT = None
+XML_ENCODING = 'UTF-8'
+
+DEFAULT_HEADERS = {
+    'Content-Type': 'application/xml; charset=UTF-8',
+    'Accept': '*/*'
+}
