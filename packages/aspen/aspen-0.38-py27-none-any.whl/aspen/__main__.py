13d9b25b7d97d7d8e8be93df663c48075c5b8b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package aspen/0.38/aspen-0.38-py27-none-any.whl

---
+++ packages/aspen/aspen-0.38-py27-none-any.whl/aspen/__main__.py
@@ -0,0 +1,28 @@
+"""
+python -m aspen
+===============
+
+Aspen ships with a server (wsgiref.simple_server) that is
+suitable for development and testing.  It can be invoked via:
+
+    python -m aspen
+
+though even for development you'll likely want to specify a
+project root, so a more likely incantation is:
+
+    ASPEN_PROJECT_ROOT=/path/to/wherever python -m aspen
+
+For production deployment, you should probably deploy using
+a higher performance WSGI server like Gunicorn, uwsgi, Spawning,
+or the like.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from aspen import serve, Website
+
+if __name__ == '__main__':
+    website = Website()
+    serve(website)
