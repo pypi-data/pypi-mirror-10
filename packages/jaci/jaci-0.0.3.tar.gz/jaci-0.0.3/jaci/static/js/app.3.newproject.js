9c77d9cfe68af98bb3512dfda4fb10381db5b12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:15 +0000

    Add package jaci/0.0.3/jaci-0.0.3.tar.gz

---
+++ packages/jaci/jaci-0.0.3.tar.gz/jaci-0.0.3/jaci/static/js/app.3.newproject.js
@@ -0,0 +1,30 @@
+angular.module('JaciApp.NewBuilder', ['JaciApp.Common']).controller('NewBuilderController', function ($rootScope, $scope, $state, $http, $cookies, hotkeys, notify) {
+    // 'name': 'Device Management [unit tests]',
+    // 'git_uri': 'git@github.com:gabrielfalcao/lettuce.git',
+    // 'build_instructions': 'make test',
+
+    $scope.builder = {
+        'shell_script':[
+            '#!/bin/bash',
+            'set -e',
+            'pip install virtualenv',
+            'virtualenv .venv',
+            'source .venv/bin/activate',
+            'pip install -r requirements.txt || echo "skipped development.txt"',
+            'pip install -r development.txt || echo "skipped development.txt"',
+            'make'
+        ].join('\n')
+    };
+
+    $scope.createBuilder = function(builder) {
+        $http.post('/api/builder', builder).
+            success(function(data, status, headers, config) {
+                console.log("/api/builder OK");
+                $rootScope.go('/');
+            }).
+            error(function(data, status, headers, config) {
+                console.log("/api/builder FAILED");
+                $rootScope.go("/");
+            });
+    };
+});
---
+++ packages/jaci/jaci-0.0.3.tar.gz/jaci-0.0.3/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ast
+import os
+from setuptools import setup, find_packages
+
+
+local_file = lambda *f: \
+    open(os.path.join(os.path.dirname(__file__), *f)).read()
+
+
+class VersionFinder(ast.NodeVisitor):
+    VARIABLE_NAME = 'version'
+
+    def __init__(self):
+        self.version = None
+
+    def visit_Assign(self, node):
+        try:
+            if node.targets[0].id == self.VARIABLE_NAME:
+                self.version = node.value.s
+        except:
+            pass
+
+
+def read_version():
+    finder = VersionFinder()
+    finder.visit(ast.parse(local_file('jaci', 'version.py')))
+    return finder.version
+
+
+requirements = [
+    'ansi2html==1.1.0',
+    'ansiconv==1.0.0',
+    'bcrypt==1.1.1',
+    'blist==1.3.6',
+    'cffi==1.1.2',
+    'GitHub-Flask==2.0.1',
+    'lineup==0.1.4',
+    'milieu==0.1.5',
+    'plant==0.1.1',
+    'pyOpenSSL==0.15.1',
+    'python-dateutil==2.4.2',
+    'redis==2.8.0',
+    'requests==2.5.1',
+    'tumbler==0.0.19',
+]
+
+setup(
+    name='jaci',
+    version='0.0.3',
+    description='Jaci',
+    entry_points={
+        'console_scripts': ['jaci = jaci.cli:main'],
+    },
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://falcao.it/jaci',
+    packages=find_packages(exclude=['*tests*']),
+    # package_data={'jaci': [
+    #     'recursive-include jaci/templates *'
+    #     'recursive-include jaci/static *'
+    # ]},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+)
