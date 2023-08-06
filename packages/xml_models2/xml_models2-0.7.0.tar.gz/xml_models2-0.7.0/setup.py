95f9c68f21ebbd1ecff8611a446b130d3824d598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:50 +0000

    Add package xml_models2/0.7.0/xml_models2-0.7.0.tar.gz

---
+++ packages/xml_models2/xml_models2-0.7.0.tar.gz/xml_models2-0.7.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='xml_models2',
+      version='0.7.0',
+      description='XML backed models queried from external REST apis',
+      author='Geoff Ford and Chris Tarttelin and Cam McHugh',
+      author_email='g_ford@hotmail.ccom',
+      url='http://github.com/alephnullplex/xml_models',
+      packages=['xml_models'],
+      install_requires=['lxml', 'python-dateutil', 'pytz', 'future', 'requests'],
+      tests_require=['mock', 'nose', 'coverage'],
+      test_suite="nose.collector"
+      )
