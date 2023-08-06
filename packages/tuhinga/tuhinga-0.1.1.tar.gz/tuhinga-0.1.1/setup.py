4865dca0f4f9191bd6b6516da1505b05c7979ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:09 +0000

    Add package tuhinga/0.1.1/tuhinga-0.1.1.tar.gz

---
+++ packages/tuhinga/tuhinga-0.1.1.tar.gz/tuhinga-0.1.1/setup.py
@@ -0,0 +1,55 @@
+# vim: set et ts=4 sw=4 sts=4 ai:
+
+from setuptools import setup
+import tuhinga
+
+setup(
+    name='tuhinga',
+    version=tuhinga.__version__,
+    description='Minimalistic markup language that translates to XML/HTML',
+    author=tuhinga.__author__,
+    author_email='benjamin@babab.nl',
+    url='http://github.com/babab/tuhinga',
+    download_url='https://pypi.python.org/pypi/tuhinga',
+    py_modules=['tuhinga'],
+    license='ISC',
+    long_description='{}\n{}'.format(open('README.rst').read(),
+                                     open('CHANGELOG.rst').read()),
+    platforms='any',
+    scripts=['tuh'],
+    data_files=[
+        ('share/tuhinga/examples', ['examples/bootstrap-navbar.tuh',
+                                    'examples/dev-test.tuh',
+                                    'examples/pretty-minimal.tuh',
+                                    'examples/very-minimal.tuh']),
+        ('share/tuhinga', ['LICENSE', 'README.rst', 'tuh.vim',
+                           'tuhinga_webrepl.py'])
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Adaptive Technologies',
+        'Topic :: Documentation',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business',
+        'Topic :: Text Editors :: Documentation',
+        'Topic :: Text Editors :: Text Processing',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: XML',
+        'Topic :: Utilities',
+    ],
+)
---
+++ packages/tuhinga/tuhinga-0.1.1.tar.gz/tuhinga-0.1.1/tests.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2014-2015 Benjamin Althues <benjamin@babab.nl>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from nose.tools import eq_, timed
+
+import tuhinga
+
+
+@timed(.005)
+def test_shortcut_string():
+    html = tuhinga.string('html5\n  head\n')
+    eq_(html, '<!doctype html>\n<html>\n  <head></head>\n</html>\n')
+
+
+@timed(.005)
+def test_shortcut_string_args():
+    html = tuhinga.string('html5\n   head\n', input_indent=3, output_indent=8)
+    eq_(html, '<!doctype html>\n<html>\n        <head></head>\n</html>\n')
+
+
+@timed(.005)
+def test_mapping():
+    html = tuhinga.string('html5\n  body\n    input-checkbox test\n')
+    comp = ('<!doctype html>\n<html>\n  <body>\n'
+            '    <input type="checkbox" value="test">\n'
+            '  </body>\n</html>\n')
+    eq_(html, comp)
+
+
+@timed(.005)
+def test_multiline_content():
+    html = tuhinga.string('html5\n  body\n'
+                          '    :: some test content\n'
+                          '    :: some more test content')
+    comp = ('<!doctype html>\n<html>\n  <body>\n'
+            '    some test content\n'
+            '    some more test content\n'
+            '  </body>\n</html>\n')
+    eq_(html, comp)
