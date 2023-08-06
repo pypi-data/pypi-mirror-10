8fe40246295f7aabfc365e658e2114e66522eb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:17 +0000

    Add package suds_requests/0.2/suds_requests-0.2-1.tar.gz

---
+++ packages/suds_requests/suds_requests-0.2-1.tar.gz/suds_requests-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='suds_requests',
+    version='0.2',
+    description=open('README.rst').read(),
+    author='Jason Michalski',
+    author_email='armooo@armooo.net',
+    py_modules=['suds_requests'],
+    install_requires=['requests', 'suds'],
+    license='MIT',
+    url='https://github.com/armooo/suds_requests',
+)
