906e6f50bd66cb4d8236ba88709f9631e04895f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:48 +0000

    Add package rawdata/0.0.4c/rawdata-0.0.4c.zip

---
+++ packages/rawdata/rawdata-0.0.4c.zip/rawdata-0.0.4c/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='rawdata',
+    version='0.0.4c',
+    author='Duncan Murray',
+    author_email='djmurray@acutesoftware.com.au',
+    packages=['rawdata'],
+    include_package_data=True,
+    url='https://github.com/acutesoftware/rawdata',
+    license='GNU General Public License v3 (GPLv3)',
+    description='Generate realistic raw datasets with optional DQ issues',
+    long_description=open('README.rst').read(),
+    classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Web Environment',
+    'Programming Language :: Python :: 3.4',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Games/Entertainment :: Simulation',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+
+)
