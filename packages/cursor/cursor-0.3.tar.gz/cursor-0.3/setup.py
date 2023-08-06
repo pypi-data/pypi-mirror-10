f566846115d7fe2a5a49dac012ccb423a32c1053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:46 +0000

    Add package cursor/0.3/cursor-0.3.tar.gz

---
+++ packages/cursor/cursor-0.3.tar.gz/cursor-0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = "cursor",
+  packages = ["cursor"],
+  version = "0.3",
+  description = "A small Python package to hide or show the terminal cursor",
+  author = "Gijs Timmers",
+  author_email = "gijs.timmers@student.kuleuven.be",
+  url = "https://github.com/GijsTimmers/cursor",
+  keywords = ["cursor", "terminal", "hide", "show"],
+  classifiers = [],
+)
