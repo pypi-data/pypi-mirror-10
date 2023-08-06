1f3a7bf35869f981e210a8c26ba61cdbc6acf4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package django-vkontakte-wall/0.8.0/django-vkontakte-wall-0.8.0.tar.gz

---
+++ packages/django-vkontakte-wall/django-vkontakte-wall-0.8.0.tar.gz/django-vkontakte-wall-0.8.0/vkontakte_wall/factories.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import random
+
+from django.utils import timezone
+import factory
+from vkontakte_api.factories import DjangoModelNoCommitFactory
+from vkontakte_groups.factories import GroupFactory
+from vkontakte_users.factories import UserFactory
+
+from .models import Post
+
+
+class PostFactory(DjangoModelNoCommitFactory):
+
+    date = factory.LazyAttribute(lambda o: timezone.now())
+
+    owner = factory.SubFactory(UserFactory)
+    author = factory.SubFactory(UserFactory)
+    remote_id = factory.LazyAttributeSequence(lambda o, n: '%s_%s' % (o.owner.remote_id, n))
+
+    likes_count = factory.LazyAttribute(lambda o: random.randrange(100))
+    reposts_count = factory.LazyAttribute(lambda o: random.randrange(100))
+    comments_count = factory.LazyAttribute(lambda o: random.randrange(100))
+    # actions_count = factory.LazyAttribute(lambda o: random.randrange(100))
+
+    class Meta:
+        model = Post
+
+
+class GroupPostFactory(PostFactory):
+    owner = factory.SubFactory(GroupFactory)
+    remote_id = factory.LazyAttributeSequence(lambda o, n: '-%s_%s' % (o.owner.remote_id, n))
