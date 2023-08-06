4b74827333737a56f1192b6d6f838d882896debe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:31 +0000

    Add package frojd_fabric_cli/1.0.4/frojd_fabric_cli-1.0.4.tar.gz

---
+++ packages/frojd_fabric_cli/frojd_fabric_cli-1.0.4.tar.gz/frojd_fabric_cli-1.0.4/frojd_fabric_cli/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+frojd_fabric_cli
+------------
+:copyright: (c) 2015 by Fröjd Interactive AB
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "frojd_fabric_cli"
+__version__ = "1.0.4"
+__build__ = 104
+__license__ = "MIT"
+__copyright__ = "Copyright 2015 Fröjd Interactive AB"
