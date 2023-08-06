8a7ff47c9881cadc078fd187d285cd93cfae6069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package pntools/0.1/pntools-0.1.tar.gz

---
+++ packages/pntools/pntools-0.1.tar.gz/pntools-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name = 'pntools',
+      version = '0.1',
+      description = 'Petri net and labelled partial order tools',
+      url = 'https://github.com/irgangla/pntools',
+      author = 'Thomas Irgang',
+      author_email = 'pntools@irgang-la.com',
+      license = 'MIT',
+      packages = ['pntools'],
+      zip_safe = False)
+
+
