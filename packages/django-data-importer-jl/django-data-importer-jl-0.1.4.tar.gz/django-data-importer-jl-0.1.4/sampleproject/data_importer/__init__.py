381335b9159d725942b091275bb26cce7a542883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:04 +0000

    Add package django-data-importer-jl/0.1.4/django-data-importer-jl-0.1.4.tar.gz

---
+++ packages/django-data-importer-jl/django-data-importer-jl-0.1.4.tar.gz/django-data-importer-jl-0.1.4/sampleproject/data_importer/__init__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+from builtins import *
+VERSION = (0, 1, 4)
+
+from .base import BaseImporter, ValidationError
+from .readers import *
+
+
+def get_version():
+    "Returns the version as a human-format string."
+    return '.'.join([str(i) for i in VERSION])
+
+
+__version__ = get_version()
