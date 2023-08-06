df172c13e5c362c94d4c7dd4fd97fdd7b204cc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:05 +0000

    Add package easy_phi/0.2.5/easy_phi-0.2.5.tar.gz

---
+++ packages/easy_phi/easy_phi-0.2.5.tar.gz/easy_phi-0.2.5/easy_phi/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+""" Easy Phi project, software for measurement equipment platform
+Project website: http://easy-phi.ch
+
+"""
+from easy_phi.app import VERSION
+
+__version__ = VERSION
+__license__ = "GPL v3.0"
+__project__ = "easy_phi"
+
---
+++ packages/easy_phi/easy_phi-0.2.5.tar.gz/easy_phi-0.2.5/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+# options reference: https://docs.python.org/2/distutils/
+setup(
+    name="easy_phi",
+    packages=["easy_phi", 'static'],
+    version="0.2.5",
+    license="GPL v3.0",
+    description='Easy Phi project web application',
+    long_description="Software for test measurement equipment platform.",
+    author='Team Quantum',
+    author_email='shadeless@ya.ru',
+    url='https://github.com/QuantumCMU/easy_phi_API',
+    download_url='http://github.com/QuantumCMU/easy_phi_API/archive/master.zip',
+    keywords=['measurement equipment', 'SCPI', 'udev', 'TMC', 'VISA'],
+    classifiers=(  # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: "
+        "Interface Engine/Protocol Translator",
+    ),
+    entry_points = {
+        'console_scripts': ['easy_phi = easy_phi.app:main']
+    },
+    data_files=[
+        ('/etc', ['scripts/easy_phi.conf']),
+        ('/etc/udev/rules.d', ['scripts/99-easy_phi-modules.rules']),
+        ('/etc/easy_phi', ['scripts/modules_conf_patches.conf',
+                           'scripts/widgets.conf']),
+    ],
+    package_data={
+        'static': ['*.html', 'css/*', 'js/*'],
+    },
+    install_requires=['tornado', 'pyudev', 'pyserial', 'dicttoxml'],
+    # TODO: add unit tests
+)
