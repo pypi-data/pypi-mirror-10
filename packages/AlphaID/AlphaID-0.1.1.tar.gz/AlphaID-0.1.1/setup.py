835e3e30fee2a6e01275c5971050ce60f755f72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:56 +0000

    Add package AlphaID/0.1.1/AlphaID-0.1.1.tar.gz

---
+++ packages/AlphaID/AlphaID-0.1.1.tar.gz/AlphaID-0.1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='AlphaID',
+    version='0.1.1',
+    author='Thinh Tran',
+    author_email='duythinht@gmail.com',
+    url='https://github.com/duythinht/AlphaID',
+    packages=['alphaid',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='Python library to generate ID like youtube, twitter, tinyURL...',
+)
