1d1040de34a173aa8b62b8c4952c35bd00436d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:46 +0000

    Add package scrapyd/1.1.0/scrapyd-1.1.0.tar.gz

---
+++ packages/scrapyd/scrapyd-1.1.0.tar.gz/scrapyd-1.1.0/docs/news.rst
@@ -0,0 +1,9 @@
+.. _news:
+
+Release notes
+=============
+
+1.0
+---
+
+First standalone release (it was previously shipped with Scrapy until Scrapy 0.16).
---
+++ packages/scrapyd/scrapyd-1.1.0.tar.gz/scrapyd-1.1.0/setup.py
@@ -0,0 +1,43 @@
+from os.path import join, dirname
+
+with open(join(dirname(__file__), 'scrapyd/VERSION')) as f:
+    version = f.read().strip()
+
+setup_args = {
+    'name': 'scrapyd',
+    'version': version,
+    'url': 'https://github.com/scrapy/scrapyd',
+    'description': 'A service for running Scrapy spiders, with an HTTP API',
+    'long_description': open('README.rst').read(),
+    'author': 'Scrapy developers',
+    'maintainer': 'Scrapy developers',
+    'maintainer_email': 'info@scrapy.org',
+    'license': 'BSD',
+    'packages': ['scrapyd'],
+    'include_package_data': True,
+    'zip_safe': False,
+    'classifiers': [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    'entry_points': '''\
+    [console_scripts]
+    scrapyd = scrapyd.scripts.scrapyd_run:main
+    '''
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+else:
+    setup_args['install_requires'] = ['Twisted>=8.0', 'Scrapy>=0.17']
+
+setup(**setup_args)
