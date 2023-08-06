fd016142a3c87352ed2366254f0e15d3e2f4cb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package mdtraj/1.4.0/mdtraj-1.4.0.tar.gz

---
+++ packages/mdtraj/mdtraj-1.4.0.tar.gz/mdtraj-1.4.0/mdtraj/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM MDTRAJ SETUP.PY
+short_version = '1.4.0'
+version = '1.4.0'
+full_version = '1.4.0'
+git_revision = 'bf794a4a3d8291f0b4d27d3a5160ae231699a507'
+release = True
+
+if not release:
+    version = full_version
