0690e9628514c7281c3c90b18f656bf964a0a263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:04 +0000

    Add package ocflib/2015.07.03.16.06/ocflib-2015.07.03.16.06.tar.gz

---
+++ packages/ocflib/ocflib-2015.07.03.16.06.tar.gz/ocflib-2015.07.03.16.06/ocflib/misc/shell.py
@@ -0,0 +1,6 @@
+"""Misc utilities for helping with shell commands."""
+# shlex.quote is new in Python 3.3
+try:
+    from shlex import quote as escape_arg  # noqa
+except ImportError:
+    from pipes import quote as escape_arg  # noqa
