98faed5efe22ff663990686ffd4c46c43ae272da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:22 +0000

    Add package jaci/0.0.18/jaci-0.0.18.tar.gz

---
+++ packages/jaci/jaci-0.0.18.tar.gz/jaci-0.0.18/jaci/static/js/app.8.show.builder.js
@@ -0,0 +1,35 @@
+angular.module('JaciApp.ShowBuilder', ['JaciApp.Common']).controller('ShowBuilderController', function ($rootScope, $scope, $state, $http, $location, hotkeys, notify, $stateParams) {
+
+    var builderId = $stateParams.builder_id;
+    $rootScope.builder = $rootScope.builders[builderId];
+    if ($rootScope.buildCache[builderId] === undefined) {
+        $rootScope.buildCache[builderId] = {};
+    }
+
+    function refresh() {
+        $http
+            .get('/api/builder/' + $rootScope.builder.id + '/builds')
+
+            .success(function(data, status, headers, config) {
+                $rootScope.buildCache[builderId] = data;
+                $scope.builds = data;
+            })
+
+            .error(function(data, status, headers, config) {
+                notify('failed to retreve builder info');
+                console.log("failed to retreve builder info", data);
+                clearInterval(poller);
+            });
+    }
+    $scope.refresh = refresh;
+
+    var limit = 720;
+    var counter = 0;
+    var poller = setInterval(function(){
+        counter++;
+        if (counter > 720) {
+            clearInterval(poller);
+        }
+        refresh();
+    }, 500);
+});
