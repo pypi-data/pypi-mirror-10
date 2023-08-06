eeb2d4c22ef15fb49a6b65c91883fa042bac2d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:14 +0000

    Add package muffin-rest/0.0.11/muffin-rest-0.0.11.tar.gz

---
+++ packages/muffin-rest/muffin-rest-0.0.11.tar.gz/muffin-rest-0.0.11/muffin_rest/__init__.py
@@ -0,0 +1,13 @@
+""" REST helpers for Muffin Framework. """
+
+# Package information
+# ===================
+
+__version__ = "0.0.11"
+__project__ = "muffin-rest"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+from .api import *      # noqa
+from .forms import *    # noqa
+from .handlers import * # noqa
