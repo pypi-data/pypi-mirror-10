2f008a0a703b1edd7b7a49addf238084c06dcf39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:01 +0000

    Add package django-statsy/0.1.3/django-statsy-0.1.3.zip

---
+++ packages/django-statsy/django-statsy-0.1.3.zip/django-statsy-0.1.3/statsy/admin.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from django.contrib import admin
+
+from statsy.models import StatsyGroup, StatsyEvent, StatsyObject
+
+
+class StatsyGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('is_active',)
+
+
+class StatsyEventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('is_active',)
+
+
+class StatsyObjectAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'user')
+    search_fields = ('user__username', 'text_value', 'float_value', 'url')
+    list_filter = ('group', 'event', 'label')
+    # date_hierarchy = 'created_at'
+    list_select_related = ('group', 'event', 'user')
+
+    # def object_value(self, obj):
+    #     return obj.value
+
+    # object_value.short_description = 'Value'
+
+
+admin.site.register(StatsyGroup, StatsyGroupAdmin)
+admin.site.register(StatsyEvent, StatsyEventAdmin)
+admin.site.register(StatsyObject, StatsyObjectAdmin)
