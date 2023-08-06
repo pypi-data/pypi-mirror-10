2b987a5710b0453419ce907602fa1e52ff103222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:18 +0000

    Add package eventsim/0.3/eventsim-0.3.zip

---
+++ packages/eventsim/eventsim-0.3.zip/eventsim-0.3/setup.py
@@ -0,0 +1,32 @@
+# Probsim setup
+# (TSK, 2015-06-28)
+
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+    
+from distutils.core import setup
+
+setup(
+	name='eventsim',
+	version='0.3',
+	description='various useful tools in simulating discrete system events based on outcome and probabilities',
+	long_description = open('README').read(),
+	author='Taiwo Kareem',
+	author_email='taiwo.kareem36@gmail.com',
+	url='http://bitbucket.org/tushortz/eventsim',
+	packages=['eventsim'],
+	platforms='any',
+	license='MIT',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Intended Audience :: Education', 
+		'Intended Audience :: Financial and Insurance Industry',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.4',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+)
+
