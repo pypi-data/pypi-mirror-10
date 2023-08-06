166c44b5cdfb7879f44773afcb0ff460f2aca788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:02 +0000

    Add package hitchtest/0.5.2/hitchtest-0.5.2.tar.gz

---
+++ packages/hitchtest/hitchtest-0.5.2.tar.gz/hitchtest-0.5.2/hitchtest/executionengine.py
@@ -0,0 +1,30 @@
+import inspect
+import signal
+from hitchtest import utils
+from hitchtest.signal_manager import SignalManager
+
+class HitchAbortedException(Exception):
+    pass
+
+class ExecutionEngine(object):
+    def __init__(self, settings, preconditions):
+        self.settings = settings
+        self.preconditions = preconditions
+        self.signal_manager = SignalManager(self.abort)
+
+    def set_up(self):
+        pass
+
+    def on_success(self):
+        pass
+
+    def on_failure(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+    def abort(self, signal, frame):
+        self.aborted = True
+        utils.stop_ipython()
+        raise HitchAbortedException("Hitch aborted")
