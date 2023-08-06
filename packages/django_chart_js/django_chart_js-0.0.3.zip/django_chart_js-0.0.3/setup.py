1a949c8b2bc8d29533a6a0ee515a25fdab22403e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:53 +0000

    Add package django_chart_js/0.0.3/django_chart_js-0.0.3.zip

---
+++ packages/django_chart_js/django_chart_js-0.0.3.zip/django_chart_js-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from django_chart_js import VERSION
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='django_chart_js',
+    version='.'.join(map(str, VERSION)),
+    description='Django Chart.js wrapper',
+    author='deliro',
+    author_email='t4k.kitaetz@gmail.com',
+    long_description=readme,
+    license='Apache 2.0',
+    requires=['django'],
+    packages=find_packages(),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ),
+)
