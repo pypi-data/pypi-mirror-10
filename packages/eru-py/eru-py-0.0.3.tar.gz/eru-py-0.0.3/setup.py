ece0ed8e4526d675835c23066d34c99189d7d66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:33 +0000

    Add package eru-py/0.0.3/eru-py-0.0.3.tar.gz

---
+++ packages/eru-py/eru-py-0.0.3.tar.gz/eru-py-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eru-py',
+    version='0.0.3',
+    author='tonic',
+    zip_safe=False,
+    author_email='tonic@wolege.ca',
+    description='ERU client for python',
+    py_modules=['eruhttp'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.7.0',
+    ],
+)
