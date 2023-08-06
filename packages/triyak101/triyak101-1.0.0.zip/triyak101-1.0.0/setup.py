19bf554d29f2673fe583ef7f5df2494ad6076df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:07 +0000

    Add package triyak101/1.0.0/triyak101-1.0.0.zip

---
+++ packages/triyak101/triyak101-1.0.0.zip/triyak101-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'triyak101',
+	version		= '1.0.0',
+	py_modules	= ['triyak101'],
+	author		= 'TriyakGupta',
+	author_email	= 'triyakgupta13@gmail.com',
+	url 		= 'http://www.google.com',
+	description	= 'test',
+     )	
