249e912e395b13c234452830e72836f175672576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:41 +0000

    Add package algorithmia/0.9.0/algorithmia-0.9.0.linux-x86_64.tar.gz

---
+++ packages/algorithmia/algorithmia-0.9.0.linux-x86_64.tar.gz/usr/local/lib/python3.4/dist-packages/Algorithmia/data.py
@@ -0,0 +1,62 @@
+'Algorithmia Data API Client (python)'
+
+import re
+import json
+import tempfile
+
+class datafile(object):
+    def __init__(self, client, dataUrl):
+        self.client = client
+        # Parse dataUrl
+        self.path = re.sub(r'^data://|^/', '', dataUrl)
+        self.url = '/v1/data/' + self.path
+
+    # Deprecated:
+    def get(self):
+        return self.client.getHelper(self.url)
+
+    # Get file from the data api
+    def getFile(self):
+        # Make HTTP get request
+        response = self.client.getHelper(self.url)
+        with tempfile.NamedTemporaryFile(delete = False) as f:
+            for block in response.iter_content(1024):
+                if not block:
+                    break;
+                f.write(block)
+            f.flush()
+            return open(f.name)
+
+    def getBytes(self):
+        # Make HTTP get request
+        return self.client.getHelper(self.url).content
+
+    def getString(self):
+        # Make HTTP get request
+        return self.client.getHelper(self.url).text
+
+    def getJson(self):
+        # Make HTTP get request
+        return self.client.getHelper(self.url).json()
+
+    def put(self, data):
+        # Post to data api
+        self.client.putHelper(self.url, bytes(data))
+        return self
+
+    def putJson(self, data):
+        # Post to data api
+        json = json.dumps(data)
+        self.client.putHelper(self.url, json)
+        return self
+
+    def putFile(self, path):
+        # Post file to data api
+        with open(path, 'r') as f:
+            self.client.putHelper(self.url, f)
+            return self
+
+    def delete(self):
+        # Delete from data api
+        self.client.deleteHelper(self.url)
+        return True
