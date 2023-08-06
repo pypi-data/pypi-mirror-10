8681a0915868abf870d80b280d7801ff0a5ee42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:28 +0000

    Add package py-eve-chat-mon/0.4/py-eve-chat-mon-0.4.tar.gz

---
+++ packages/py-eve-chat-mon/py-eve-chat-mon-0.4.tar.gz/py-eve-chat-mon-0.4/py_eve_chat_mon/exceptions.py
@@ -0,0 +1,34 @@
+
+class EveChatMonException(Exception):
+    """Base class for all exceptions
+
+        Attributes:
+            message -- An explanation of the error
+    """
+    def __init__(self, message):
+        super(EveChatMonException, self).__init__(message)
+
+class InvalidChatDirectory(EveChatMonException):
+    """Exception raised when the supplied path to Eve's chat log directory is invalid.
+
+        Attributes:
+            path -- The invalid path provided
+            message -- An explanation of the error
+    """
+    def __init__(self, path, message):
+        super(InvalidChatDirectory, self).__init__(message)
+        self.path = path
+
+class InvalidCallable(EveChatMonException):
+    """Exception raised when a supplied argument does not support __call__"""
+    def __init__(self, message):
+        super(InvalidCallable, self).__init__(message)
+
+
+class InvalidMonitorState(EveChatMonException):
+    def __init__(self, message):
+        super(InvalidMonitorState, self).__init__(message)
+
+class ObserverAlreadyAdded(EveChatMonException):
+    def __init__(self):
+        super(ObserverAlreadyAdded, self).__init__("File monitor already initialized")
---
+++ packages/py-eve-chat-mon/py-eve-chat-mon-0.4.tar.gz/py-eve-chat-mon-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-eve-chat-mon",
+    version="0.4",
+    author="Andrew Martinez",
+    author_email="andrew.p.martinez@gmail.com",
+    install_requires=[
+        "watchdog==0.8.3"
+    ],
+    description=("A library that focuses on monitoring EVE Online chat logs for messages and doing nothing else. It is meant to be focused and lightweight."),
+    license="MIT",
+    keywords="EVE chat monitor",
+    url='https://github.com/andrewpmartinez/py-eve-chat-mon',
+    download_url='https://github.com/andrewpmartinez/py-eve-chat-mon/tarball/0.',
+    packages=find_packages(),
+    long_description="See github page for full details.",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
