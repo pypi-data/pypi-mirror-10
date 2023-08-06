ffded8248677f28ea115e70cf876294ddf165e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:05 +0000

    Add package jmbo-post/2.0.0/jmbo-post-2.0.0.tar.gz

---
+++ packages/jmbo-post/jmbo-post-2.0.0.tar.gz/jmbo-post-2.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-post',
+    version='2.0.0',
+    description='Jmbo post application. Post is a synonym for article. It is the most common content type.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-post',
+    packages = find_packages(),
+    install_requires = [
+        'django-ckeditor>=4.2.1',
+        'jmbo>=2.0.0',
+        'beautifulsoup4',
+    ],
+    tests_require=[
+        'django-setuptest>=0.1.4',
+        'psycopg2',
+    ],
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
