2f80672c991c9723ebb6ccb2c1f19987ce8e540d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:43 +0000

    Add package yunlib/1.0.0/yunlib-1.0.0.tar.gz

---
+++ packages/yunlib/yunlib-1.0.0.tar.gz/yunlib-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'yunlib',
+    version         = '1.0.0',
+    py_modules      = ['yunlib'],
+    author          = 'joung won yun',
+    author_email    = 'wizice100@gmail.com',
+    url             = 'http://www.wizice.com',
+    description     = 'A simple libaray for string ',
+    )
