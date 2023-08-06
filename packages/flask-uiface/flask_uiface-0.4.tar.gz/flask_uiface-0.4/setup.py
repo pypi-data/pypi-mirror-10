13e9fcf9edead0e7ab24a340d4d74c08672b8e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:09 +0000

    Add package flask-uiface/0.4/flask_uiface-0.4.tar.gz

---
+++ packages/flask-uiface/flask_uiface-0.4.tar.gz/flask_uiface-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'flask_uiface',
+  packages = ['flask_uiface'], # this must be the same as the name above
+  version = '0.4',
+  description = 'Random user avatars for the rest of us!',
+  author = 'Henri Kuiper',
+  author_email = 'henrikuiper@zdevops.com',
+  url = 'https://github.com/zdevops/flask-uiface', # use the URL to the github repo
+  download_url = 'https://github.com/zdevops/flask-uiface', # I'll explain this in a second
+  keywords = ['flask', 'avatar', 'uifaces'], # arbitrary keywords
+  classifiers = [],
+)
