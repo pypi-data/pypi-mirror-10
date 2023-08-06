630a09a0f0f866b8a274ec59045f9e4285f8a9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:16 +0000

    Add package pys3website/0.2.1/pys3website-0.2.1.tar.gz

---
+++ packages/pys3website/pys3website-0.2.1.tar.gz/pys3website-0.2.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pys3website',
+      version='0.2.1',
+      description='pys3website syncs a local folder to a static website bucket on Amazon S3',
+      author='Dongwook Yoon',
+      author_email='dy252@cornell.edu',
+      url='https://github.com/DongwookYoon/pys3website',
+      download_url= 'https://github.com/DongwookYoon/pys3website/tarball/0.2.1',
+      keywords = ['AWS', 'S3', 'WebSite'],
+      classifiers=[],
+      packages=['pys3website'],
+     )
