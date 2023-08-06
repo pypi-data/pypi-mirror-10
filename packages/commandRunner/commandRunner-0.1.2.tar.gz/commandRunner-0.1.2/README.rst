ffcc332b1c2c997493198c8e38a4023da16e8d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:08 +0000

    Add package commandRunner/0.1.2/commandRunner-0.1.2.tar.gz

---
+++ packages/commandRunner/commandRunner-0.1.2.tar.gz/commandRunner-0.1.2/README.rst
@@ -0,0 +1,62 @@
+commandRunner
+=============
+
+commandRunner is yet another package created to handle running commands,
+scripts or programs on the command line. The principle class lets you run
+anything locally on your machine. Later classes are targetted at Analytics
+and data processing platforms such as Grid Engine and HADOOP. The class
+attempts to  run commands in in a moderately thread safe way by requiring that
+you provide with sufficient information that it can build a uniquely labelled
+temp directory for all input and output files. This means that this can play
+nicely with things like celery workers.
+
+Release 0.1
+-----------
+
+This release supports running commands on localhost.  It also uses interpolation
+for the commands with the same syntax and python templates
+
+Future
+------
+
+In the future we'll provide classes to run commands over RServe, Grid Engine,
+Hadoop, Octave, and SAS Server.
+
+
+Usage
+-----
+
+    from commandRunner import *
+
+    r = commandRunner("ID_STRING", "/tmp/",
+                      ".in", ".out", "ls /tmp > $OUTPUT", "STRING OF DATA")
+    r.prepare()
+    exit_status = r.run_cmd()
+    r.tidy()
+    print(r.output_data)
+
+__init__ initalises all the class variables needed and performs the command
+string interpolation.
+
+r.prepare() builds a temporary directory and makes any input file which is
+needed. In this instance "ID_STRING", and a path where temporary files can be
+placed are used to create a tempdir called /tmp/ID_STRING/. Next it takes and
+string of data and makes and input file given the provided input file ending
+(.in) which would be /tmp/ID_STRING/ID_STRING.in and this file would contain
+"STRING OF DATA"
+
+r.run_cmd() runs the command string provided. First anything labelled $OUTPUT
+of $INPUT will be replaced with the path to the temporary files the process
+will generate.  In this instance "ls /tmp > $OUTPUT" will become
+"ls /tmp > /tmp/ID_STRING/ID_STRING.out". Any command will be run so this is
+potentially very dangerous. The exit status of the command is returned
+
+r.tidy() cleans up deleting any input and output files and the temporary
+working directory. Any data in the output file is read in to r.output_data
+
+Tests
+-----
+
+Run tests with:
+
+    python test_commandRunner.py
---
+++ packages/commandRunner/commandRunner-0.1.2.tar.gz/commandRunner-0.1.2/commandRunner/commandRunner.py
@@ -0,0 +1,87 @@
+import os
+import re
+from subprocess import call
+
+class commandRunner:
+    tmp_id = None
+    tmp_path = None
+    in_glob = None
+    out_glob = None
+    command = None
+    data = None
+    output_data = None
+    in_path = None
+    out_path = None
+    path = None
+
+    def __init__(self, tmp_id, tmp_path, in_glob, out_glob, cmd, data):
+        ''' Constructs a local job '''
+
+        if os.path.isdir(tmp_path):
+            self.tmp_path = tmp_path
+        else:
+            raise OSError
+
+        self.tmp_id = tmp_id
+        self.in_glob = in_glob
+        self.out_glob = out_glob
+        self.data = data
+        self.tmp_path = re.sub("/$", '', self.tmp_path)
+        self.in_glob = re.sub("^\.", '', self.in_glob)
+        self.out_glob = re.sub("^\.", '', self.out_glob)
+        self.path = self.tmp_path+"/"+self.tmp_id+"/"
+        self.out_path = self.path+self.tmp_id+"."+self.out_glob
+        self.in_path = self.path+self.tmp_id+"."+self.in_glob
+        self.command = self.__translate_command(cmd)
+
+    def __translate_command(self, command):
+        '''
+            takes the command string and substitutes the relevant files names
+        '''
+        # interpolate the file names if needed
+        command = command.replace("$OUTPUT", self.out_path)
+        command = command.replace("$INPUT", self.in_path)
+        return(command)
+
+    def prepare(self):
+        '''
+            Makes a directory and then moves the input data file there
+        '''
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+        if self.data is not None:
+            fh = open(self.in_path, 'w')
+            fh.write(self.data)
+            fh.close()
+
+    def run_cmd(self):
+        '''
+            run the command we constructed when the object was initialised.
+            If exit is 0 then pass back if not decide what to do next. (try
+            again?)
+        '''
+        exit_status = None
+        try:
+            exit_status = call(self.command, shell=True)
+        except Exception as e:
+            raise OSError
+
+        if exit_status == 0:
+            if os.path.exists(self.out_path):
+                with open(self.out_path, 'r') as content_file:
+                    self.output_data = content_file.read()
+        else:
+            raise OSError
+        return(exit_status)
+
+    def tidy(self):
+        '''
+            Delete everything in the tmp dir and then remove the temp dir
+        '''
+        if os.path.exists(self.in_path):
+            os.remove(self.in_path)
+        if os.path.exists(self.out_path):
+            os.remove(self.out_path)
+        if os.path.exists(self.path):
+            os.rmdir(self.path)
