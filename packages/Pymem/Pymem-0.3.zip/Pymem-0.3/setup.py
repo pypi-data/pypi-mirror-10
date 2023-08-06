9d511d6499860f31de136fd822a4e272b1c3ecf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:37 +0000

    Add package Pymem/0.3/Pymem-0.3.zip

---
+++ packages/Pymem/Pymem-0.3.zip/Pymem-0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+setuptools.setup(
+    name='Pymem',
+    version='0.3',
+    description='pymem: python memory access made easy',
+    author='Fabien Reboia',
+    author_email='srounet@gmail.com',
+    maintainer='Fabien Reboia',
+    maintainer_email='srounet@gmail.com',
+    url='http://pymem.readthedocs.org/en/latest/',
+    long_description="A python library for windows, providing the needed functions to start working on your own with memory editing",
+    license="postcard license",
+    packages = setuptools.find_packages(),
+    platforms=["windows"],
+    keywords='memory win32 windows process',
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Assembly',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+    install_requires=[
+        'pyfasm',
+        'sphinx_rtd_theme'
+    ],
+)
