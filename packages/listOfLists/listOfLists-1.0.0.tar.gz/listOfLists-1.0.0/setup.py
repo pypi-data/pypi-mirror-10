1f8c1e36a872ce51913c95379e2493ecac4c150b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:23 +0000

    Add package listOfLists/1.0.0/listOfLists-1.0.0.tar.gz

---
+++ packages/listOfLists/listOfLists-1.0.0.tar.gz/listOfLists-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup ( 
+	name         = 'listOfLists',
+	version      = '1.0.0',
+	py_modules   = ['listOfLists'],
+	author       = 'Vikctar',
+        author_email = 'vikc.steve@gmail.com',
+	description  = 'A simple printer of lists and nested lists'
+
+      )
