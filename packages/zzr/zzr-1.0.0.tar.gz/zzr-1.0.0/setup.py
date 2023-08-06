da3d6abe08f5ad4fec3da70eec2667fbeab30131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:11 +0000

    Add package zzr/1.0.0/zzr-1.0.0.tar.gz

---
+++ packages/zzr/zzr-1.0.0.tar.gz/zzr-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name                 =   'zzr',
+    version              =   '1.0.0',
+    py_modules      =    ['zzr'],
+    author               =   'zzr0427',
+    author_email    =   'zzr0427@toki.waseda.jp',
+    description       =   'This is a test program for python learning.'
+    )
