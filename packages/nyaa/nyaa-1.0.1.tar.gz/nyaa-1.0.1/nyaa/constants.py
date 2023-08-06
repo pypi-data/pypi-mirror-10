d39a7f4f8951e7513d27ef88ffd0a604e2597222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package nyaa/1.0.1/nyaa-1.0.1.tar.gz

---
+++ packages/nyaa/nyaa-1.0.1.tar.gz/nyaa-1.0.1/nyaa/constants.py
@@ -0,0 +1,27 @@
+import re
+import sys
+from collections import namedtuple
+
+PY3 = sys.version_info >= (3, 3)
+
+URL_NYAA = 'http://www.nyaa.se/'
+NyaaResult = namedtuple('NyaaResult',
+    ['title', 'category', 'link', 'guid', 'date',
+     'seeders', 'leechers', 'downloads', 'size', ])
+FORMAT_DATETIME = '%a, %d %b %Y %H:%M:%S{}'.format(' %z' if PY3 else '')
+RE_DESC = re.compile(
+    r'(?P<seeders>[\d]+) seeder\(s\), (?P<leechers>[\d]+) leecher\(s\), (?P<downloads>[\d]+) download\(s\) - (?P<size>[\d\.]+) (?P<format>[\w]+)',
+    re.IGNORECASE)
+
+class SortBy:
+    DATE = 1
+    SEEDERS = 2
+    LEECHERS = 3
+    DOWNLOADS = 4
+    SIZE = 5
+    NAME = 6
+
+    @classmethod
+    def vars(cls):
+        local = [var for var in dir(cls) if not callable(var) and not '_' in var]
+        return local
