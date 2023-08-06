e39bc2f49f8bcfb2de88869cea68a7245d0ddd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package jme/1.0.0/jme-1.0.0.tar.gz

---
+++ packages/jme/jme-1.0.0.tar.gz/jme-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'jme',
+        version = '1.0.0',
+        py_modules = ['jme'],
+        author = 'jme',
+        author_email = 'jmehsieh@gmail.com',
+        url = 'http://www.jmehsieh.com',
+        description = 'Print my name',
+    )
