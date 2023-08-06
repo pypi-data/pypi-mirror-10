6c0a99118544bcb574990a3f062e1254d79a8529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:25 +0000

    Add package colordiffs/0.1.5/colordiffs-0.1.5.tar.gz

---
+++ packages/colordiffs/colordiffs-0.1.5.tar.gz/colordiffs-0.1.5/colordiffs/colordiffs.py
@@ -0,0 +1,63 @@
+import os
+import re
+import subprocess
+
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexer import Lexer
+from pygments.lexers import guess_lexer, guess_lexer_for_filename
+from pygments.token import Text
+from pygments.util import ClassNotFound
+
+from .output import output
+from .diff import parse_diff_output
+from .utils import NoneLexer
+
+
+def colordiffs(diff):
+    """Diff is a list of lines from a diff output"""
+    old_commit, new_commit = diff.commits[0], diff.commits[1]
+    file1, file2 = diff.file_a, diff.file_b
+
+    old_code = colorize(
+        file_contents_at_commit(old_commit, file1), file1)
+    new_code = colorize(
+        file_contents_at_commit(new_commit, file2), file2)
+    return old_code, new_code
+
+
+def colorize(code, filename=None):
+    try:
+        if filename is not None:
+            lexer = guess_lexer_for_filename(filename, code)
+        else:
+            lexer = guess_lexer(code)
+    except ClassNotFound:
+        lexer = NoneLexer()
+
+    formatter = TerminalFormatter()
+    result = highlight(code, lexer, formatter)
+    result = result.split('\n')
+
+    if result[-1] == '':
+        return result[:-1]
+    return result
+
+
+def file_contents_at_commit(git_obj, filename):
+    f = open(os.devnull, 'w')
+    try:
+        # this is probably HEAD, just use cat to get file contents
+        output = subprocess.check_output(
+            ['git', 'show', git_obj], stderr=f)
+    except subprocess.CalledProcessError:
+        output = subprocess.check_output(
+            ['cat', filename])
+    f.close()
+    return output
+
+
+def run(diff):
+    for a_diff in parse_diff_output(diff):
+        a, b = colordiffs(a_diff)
+        output(a_diff, a, b)
