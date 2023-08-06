3f374147d580277e5d7aab75dbcb5d9b6ab14047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:39 +0000

    Add package FanfictionAPI/0.91/FanfictionAPI-0.91.tar.gz

---
+++ packages/FanfictionAPI/FanfictionAPI-0.91.tar.gz/FanfictionAPI-0.91/FanfictionAPI/author.py
@@ -0,0 +1,131 @@
+__author__ = 'akotian'
+
+'''
+Autor class
+'''
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+import json
+import urls
+import re
+from fanfic import Fanfic
+
+class Author(object):
+
+  def __init__(self, url=None, html=None):
+    """
+    Constructor
+    """
+    self._fanfics = None
+    self._fanfics_urls = [] 
+    self._favorite_fanfics = None
+    self._favorite_authors = None
+    self._beta_reader = None
+    self._join_date = None
+    self._profile_update_date = None
+    self._author_id = None
+    self._author_country = None
+    if url != None:
+      self._url = urls.normalize_url(url)
+    self._html = Fanfic(self._url, html)._get_html()
+
+
+  def get_fanfics(self):  
+    '''
+    Returns:
+      Authors fanfics
+    '''
+    if self._fanfics is None:
+      title = self._html.select("#st .mystories .stitle")
+      self._get_fanfic_urls(title)
+    return (Fanfic(url) for url in self._fanfics_urls)
+
+  def get_favorite_fanfics(self):  
+    '''
+    Returns:
+      Authors favorite fanfics
+    '''
+    if self._favorite_fanfics is None:
+      title = self._html.select("#fs .favstories .stitle")
+      self._get_fanfic_urls(title)
+    return (Fanfic(url) for url in self._fanfics_urls)
+
+  def get_favorite_authors(self):  
+    '''
+    Returns:
+      Authors favorite authors 
+    '''
+    if self._favorite_authors is None:
+      title = self._html.select("#fa dl a")
+      self._get_fanfic_urls(title)
+    return (Fanfic(url) for url in self._fanfics_urls)
+
+  def _get_fanfic_urls(self, title):
+    '''
+      Process titles and links
+
+      Returns:
+        JSON stream consisting of title and its corresponding link
+    '''
+    for i in title:
+        self._fanfics_urls.append(urls.fanfiction_base_url + i.attrs['href'])
+
+  def is_beta_reader(self):  
+    '''
+    Returns:
+      Is Beta reader
+    '''
+    if self._beta_reader is None:
+      table = self._html.select('#bio')[0].find_previous_sibling("table")
+      profile_type = table.select('tr:nth-of-type(2) td a')[0].get_text()
+      if (profile_type == 'Beta Profile'):
+        self._beta_reader = True
+      else:
+        self._beta_reader = False 
+    return self._beta_reader  
+
+  def get_join_date(self):  
+    '''
+    Returns:
+      Author join date 
+    '''
+    if self._join_date is None:
+      table = self._html.select('#bio')[0].find_previous_sibling("table")
+      self._join_date = table.select('tr:nth-of-type(3) td span')[0].get_text()
+    return self._join_date
+
+  def get_last_profile_update(self):  
+    '''
+    Returns:
+      Last profile update date 
+    '''
+    if self._profile_update_date is None:
+      table = self._html.select('#bio')[0].find_previous_sibling("table")
+      self._profile_update_date = table.select('tr:nth-of-type(3) td span:nth-of-type(2)')[0].get_text()
+    return self._profile_update_date
+
+  def get_id(self):  
+    '''
+    Returns:
+      Author id 
+    '''
+    if self._author_id is None:
+      table = self._html.select('#bio')[0].find_previous_sibling("table")
+      data = table.select('tr:nth-of-type(3) td')[0].get_text()
+      result = data.split(",") 
+      if 'id' in result[1]:
+        self._author_id = result[1].split(":")[1].strip()
+      else:
+        self._author_id = -1  
+    return self._author_id 
+
+  def get_country(self):  
+    '''
+    Returns:
+      Author's country 
+    '''
+    if self._author_country is None:
+      table = self._html.select('#bio')[0].find_previous_sibling("table")
+      self._author_country = table.select('tr:nth-of-type(3) td img')[0].attrs['title']
+    return self._author_country
+
---
+++ packages/FanfictionAPI/FanfictionAPI-0.91.tar.gz/FanfictionAPI-0.91/FanfictionAPI/category.py
@@ -0,0 +1,272 @@
+__author__ = 'jwsm'
+
+from FanfictionAPI import urls, listing
+
+
+class Category(listing.Listing):
+    """
+    Represents a category, as found at http://www.fanfiction.net/medium/category
+    Do not access member variables directly
+    """
+
+    def __init__(self, url, html=None):
+        """
+        Construct a category from its url. The html arg is intended for testing purposes.
+
+        Args:
+            url (str): The url of the category to construct
+        """
+        super().__init__(url, html)
+        self._param_keys = {
+            'censorid': 'r',
+            '_genreid1': 'g',
+            'timerange': 't',
+            'sortid': 'srt',
+            'languageid': 'lan',
+            'length': 'len',
+            'statusid': 's',
+            'verseid1': 'v1',
+            'characterid1': 'c',
+            'withpairing': 'pm',
+        }
+        self._page_key = 'p'
+        self._params = {'p': self.current_page_number()}
+
+    def sort_by_options(self):
+        """
+        Returns:
+            {str: int}: possible values for Sort By filter
+        """
+        return self._get_filter_options('sortid')
+
+    def time_range_options(self):
+        """
+        Returns:
+            {str: int}: possible values for Time Range filter
+        """
+        return self._get_filter_options('timerange')
+
+    def genre_options(self):
+        """
+        Returns:
+            {str: int}: possible values for Genre filter
+        """
+        return self._get_filter_options('_genreid1')
+
+    def ratings(self):
+        """
+        Returns:
+            {str: int}: possible values for Ratings filter
+        """
+        return self._get_filter_options('censorid')
+
+    def languages(self):
+        """
+        Returns:
+            {str: int}: possible values for Languages filter
+        """
+        return self._get_filter_options('languageid')
+
+    def length(self):
+        """
+        Returns:
+            {str: int}: possible values for Length filter
+        """
+        return self._get_filter_options('length')
+
+    def status(self):
+        """
+        Returns:
+            {str: int}: possible values for Status filter
+        """
+        return self._get_filter_options('statusid')
+
+    def worlds(self):
+        """
+        Returns:
+            {str: int}: possible values for Worlds filter
+        """
+        return self._get_filter_options('verseid1')
+
+    def characters(self):
+        """
+        Returns:
+            {str: int}: possible values for Characters filter
+        """
+        chars = self._get_filter_options('characterid1')
+        chars[0] = chars[0][:-4].strip()
+        return chars
+
+    '''
+    def _set_filter(self, filter_name, option, selection=None, with_filter=True):
+        """
+        Set a filter for the Category by modifying the query params
+
+        Args:
+            filter_name (str): The name of the filter to set, as used in fanfiction.net's source
+            option (str): The value that the filter should take. Possibly options may be found by calling
+                          the appropriate accessor method
+            selection (str): 'A', 'B', 'C', or 'D'. Which option to set
+            with_filter (boolean): if True, method will set the With filter. If not, method will set the
+                                   Without filter
+        """
+        selection_dict = {
+            'A': '1',
+            'B': '2',
+            'C': '3',
+            'D': '4',
+        }
+
+        if option not in self._get_filter_options(filter_name):
+            raise ValueError('%s is not recognized' % option)
+
+        key = self._param_keys[filter_name]
+
+        if not with_filter:
+            key = '_' + key
+
+        if selection:
+            key += selection_dict[selection]
+
+        self._params[key] = self._get_filter_options(filter_name)[option]
+    '''
+
+    def set_sorting_method(self, option):
+        """
+        Set the Sort By filter
+
+        Args:
+            option (str): the name of the sorting method to use
+        """
+        self._set_filter('sortid', option)
+
+    def set_time_range(self, option):
+        """
+        Set the Time Range filter
+
+        Args:
+            option (str): the name of the time range to use
+        """
+        self._set_filter('timerange', option)
+
+    def set_genre_filter(self, genre, selection, with_filter=True):
+        """
+        Set the Genre filter
+        If with_filter is False, selection must be None.
+
+        Args:
+            genre (str): the name of the genre to use
+            selection (str): 'A' or 'B', to select the appropriate genre filter
+            with_filter (boolean): if True, sets With filter. If False, sets Without filter
+        """
+        if selection != 'A' and selection != 'B':
+            raise ValueError('selection arg should be \'A\' or \'B\'')
+
+        if selection and not with_filter:
+            raise ValueError('Without Genre filter has only one option')
+
+        self._set_filter('_genreid1', genre, selection, with_filter)
+
+    def set_rating_filter(self, rating):
+        """
+        Set the Rating filter
+
+        Args:
+            rating (str): the ratings to include
+        """
+        self._set_filter('censorid', rating)
+
+    def set_language_filter(self, language):
+        """
+        Set the Language filter
+
+        Args:
+            language (str): the language to include
+        """
+        self._set_filter('languageid', language)
+
+    def set_length_filter(self, length):
+        """
+        Set the Length filter
+
+        Args:
+            length (str): the length to use
+        """
+        self._set_filter('length', length)
+
+    def set_status_filter(self, status):
+        """
+        Set the Status filter
+
+        Args:
+            status (str): the length to use
+        """
+        self._set_filter('statusid', status)
+
+    def set_world_filter(self, world, with_filter=True):
+        """
+        Set the World filter
+
+        Args:
+            world (str): the world to use
+            with_filter (boolean): if True, sets With filter. Else: sets Without filter
+        """
+        self._set_filter('verseid1', world, None, with_filter)
+
+    def set_character_filter(self, character, selection, with_filter=True):
+        """
+        Set the Character filter
+        if with_filter is False, selection must be 'A' or 'B'
+
+        Args:
+            character (str): the name of the character to use
+            selection (str): 'A', 'B', 'C', or 'D', to select the appropriate filter
+            with_filter (boolean): if True, sets With filter. Else, sets Without filter
+        """
+        if selection not in ['A', 'B', 'C', 'D']:
+            raise ValueError("Character selection must be 'A', 'B', 'C', or 'D'")
+        if not with_filter:
+            if selection not in ['A', 'B']:
+                raise ValueError('Character filter only has two options')
+
+        self._set_filter('characterid1', character, selection, with_filter)
+
+    def set_with_pairing_filter(self, setting, with_filter=True):
+        """
+        Set the With_pairing filter
+
+        Args:
+            setting (Boolean): if True, filter is set. If False, turned off
+            with_filter (Boolean): if True, With filter is used, If False, Without filter is used
+        """
+        key = 'pm'
+
+        if not with_filter:
+            key = '_' + key
+
+        if setting is True:
+            self._params[key] = '1'
+        else:
+            self._params.pop(key, None)
+
+    def build_filtered_url(self):
+        """
+        Get the new url, based on all the applied filters
+
+        Returns:
+            str: category url
+        """
+
+        url = urls.remove_params(self._url) + '?'
+
+        for param in self._params.keys():
+            url += '&' + param + '=' + self._params[param]
+
+        self._url = url
+        return url
+
+    def clear_filters(self):
+        """
+        Remove all filters, except for page number
+        """
+        self._params = {'p': str(self.current_page_number())}
