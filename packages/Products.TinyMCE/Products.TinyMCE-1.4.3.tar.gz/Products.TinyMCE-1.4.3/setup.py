1ed7994c76e1d81be0c7c8a14fc12d9d61bd6fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:55 +0000

    Add package Products.TinyMCE/1.4.3/Products.TinyMCE-1.4.3.tar.gz

---
+++ packages/Products.TinyMCE/Products.TinyMCE-1.4.3.tar.gz/Products.TinyMCE-1.4.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+setup(name='Products.TinyMCE',
+    version='1.4.3',
+    description=("Adds support for TinyMCE, a platform independent web based "
+                 "Javascript HTML WYSIWYG editor, to Plone."),
+    long_description=(open("README.rst").read() + '\n\n' +
+                      open("HISTORY.rst").read()),
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='editor editors tinymce wysiwyg',
+    author='Four Digits',
+    author_email='rob@fourdigits.nl',
+    url='http://plone.org/products/tinymce',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.imaging>=1.0.2',
+        'plone.outputfilters',
+        'plone.namedfile',
+        'plone.app.layout>=2.3.7',
+        'plone.caching',
+        'Products.ResourceRegistries',
+        'zope.app.content',
+        'zope.schema>=3.5.4',
+        # depends on *either* elementtree or lxml...
+        # we can expect one of those in all versions of Plone
+    ],
+    extras_require={
+        'test': ['plone.app.testing',
+                 'zope.testing',
+                 'unittest2',
+                 'Pillow',
+                 'plone.app.contenttypes'],
+        'docs': ['sphinx'],
+    },
+)
