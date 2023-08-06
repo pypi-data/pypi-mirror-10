1aa8bbad0d5efcbf2a8fe0e6226a4e25fc366060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:37 +0000

    Add package wuguannan/1.0.0/wuguannan-1.0.0.tar.gz

---
+++ packages/wuguannan/wuguannan-1.0.0.tar.gz/wuguannan-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+              name        ='wuguannan',
+              version     ='1.0.0',
+              py_modules  =['wuguan'],
+              author      ='hfpython',
+              author_email='hypython@headfirstlab.com',
+              url         ='http://www/python/com',
+              description ='a simple list',
+)
