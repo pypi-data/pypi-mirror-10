21073901e8d28f7315add7dcb4063be1173e0e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:08 +0000

    Add package ores/0.1.0/ores-0.1.0.zip

---
+++ packages/ores/ores-0.1.0.zip/ores-0.1.0/ores/wsgi/util.py
@@ -0,0 +1,18 @@
+
+class ParamError(Exception): pass
+
+
+def read_param(request, param, default=None, type=str):
+    try:
+        value = request.args.get(param, request.form.get(param, default))
+        return type(value.strip())
+    except (ValueError, TypeError) as e:
+        raise ParamError("Could not interpret {0}. {1}"
+                         .format(param, str(e)))
+
+
+def read_bar_split_param(request, param, default=None, type=str):
+    values = read_param(request, param, default=default)
+    if values is None:
+        return []
+    return [type(value) for value in values.split("|")]
