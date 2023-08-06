22bf52db78335e97b1fb2b67577ad53419bca8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:49 +0000

    Add package jsonplustypes/0.4/jsonplustypes-0.4.tar.gz

---
+++ packages/jsonplustypes/jsonplustypes-0.4.tar.gz/jsonplustypes-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name="jsonplustypes",
+        version="0.4",
+        packages=["jsonplustypes",],
+        license="MIT",
+        description="json with datetime and more, KISS",
+        long_description=open("README.txt").read(),
+        maintainer="sloev",
+        maintainer_email="johannesgj@gmail.com",
+        url="http://github.com/sloev/jsonplustypes"
+        )
