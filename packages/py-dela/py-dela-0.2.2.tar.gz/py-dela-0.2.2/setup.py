b9d1692e5e93459e2256d582ef5cc51000297eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:58 +0000

    Add package py-dela/0.2.2/py-dela-0.2.2.tar.gz

---
+++ packages/py-dela/py-dela-0.2.2.tar.gz/py-dela-0.2.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='py-dela',
+      version='0.2.2',
+      description='Python DELA Dictionary(Unitex) support library',
+      long_description=readme(),
+      author='Ulysses Rangel Ribeiro',
+      author_email='ulyssesrr@gmail.com',
+      url='https://github.com/ulyssesrr/py-dela',
+      download_url = 'https://github.com/ulyssesrr/py-dela/tarball/0.2.2',
+      license="LGPL",
+      keywords=['dela', 'unitex', 'lemmatization'],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   ("License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"),
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   ("Topic :: Software Development :: Libraries :: Python "
+                    "Modules"),
+                   "Topic :: Text Processing :: Linguistic"],
+      packages=['pydela'])
