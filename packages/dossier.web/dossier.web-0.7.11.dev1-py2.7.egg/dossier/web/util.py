b5f0952f60bb407d77fdb26cd3481050f1019de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:23 +0000

    Add package dossier.web/0.7.11.dev1/dossier.web-0.7.11.dev1-py2.7.egg

---
+++ packages/dossier.web/dossier.web-0.7.11.dev1-py2.7.egg/dossier/web/util.py
@@ -0,0 +1,22 @@
+'''Utility functions that don't belong elsewhere.
+
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2015 Diffeo, Inc.
+'''
+from __future__ import absolute_import, division, print_function
+
+from dossier.fc import FeatureCollection, FeatureTokens, StringCounter
+
+
+def fc_to_json(fc):
+    # If `fc` has already been converted to a dict elsewhere, then
+    # don't try to do it again.
+    if not isinstance(fc, FeatureCollection):
+        return fc
+    d = {}
+    for name, feat in fc.iteritems():
+        if isinstance(feat, (unicode, StringCounter, dict)):
+            d[name] = feat
+        elif isinstance(feat, FeatureTokens):
+            d[name] = feat.to_dict()
+    return d
