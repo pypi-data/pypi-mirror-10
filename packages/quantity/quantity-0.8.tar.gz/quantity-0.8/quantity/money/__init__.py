3a075eae66ae25a1c316d8b50970de9d4d8f22da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:22 +0000

    Add package quantity/0.8/quantity-0.8.tar.gz

---
+++ packages/quantity/quantity-0.8.tar.gz/quantity-0.8/quantity/money/__init__.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------------
+## Name:        money (package)
+## Purpose:     Currency-safe computations with money amounts.
+##
+## Author:      Michael Amrhein (mamrhein@users.sourceforge.net)
+##
+## Copyright:   (c) 2013 ff. Michael Amrhein
+## License:     This program is free software. You can redistribute it, use it
+##              and/or modify it under the terms of the 2-clause BSD license.
+##              For license details please read the file LICENSE.TXT provided
+##              together with the source code.
+##----------------------------------------------------------------------------
+## $Source: quantity/money/__init__.py $
+## $Revision: cad1851d666e 2015-05-11 16:36 +0200 mamrhein $
+
+
+"""Currency-safe computations with money amounts.
+
+Money is a special type of quantity. Its unit type is known as currency.
+
+Money differs from physical quantities mainly in two aspects:
+
+* Money amounts are discrete. For each currency there is a smallest fraction
+  that can not be split further.
+
+* The relation between different currencies is not fixed, instead, it varies
+  over time.
+
+The sub-package `quantity.money` provides classes and functions to deal with
+these specifics. Its main classes :class:`Money`, :class:`Currency`,
+:class:`ExchangeRate` and the function :func:`registerCurrency` can also be
+imported from `quantity`.
+
+Usage
+=====
+
+Registering a currency
+----------------------
+
+A currency must explicitly be registered as a unit for further use. The
+easiest way to do this is to call the function :func:`registerCurrency`:
+
+    >>> EUR = registerCurrency('EUR')
+    ... HKD = registerCurrency('HKD')
+    ... TND = registerCurrency('TND')
+    ... USD = registerCurrency('USD')
+    >>> EUR, HKD, TND, USD
+    (Currency(u'EUR'), Currency(u'HKD'), Currency(u'TND'), Currency(u'USD'))
+
+The function is backed by a database of currencies defined in ISO 4217. It
+takes the 3-character ISO 4217 code as parameter.
+
+:class:`Currency` derives from :class:`Unit`. Each instance has a symbol
+(which is the 3-character ISO 4217 code) and a name. In addition, it holds
+the smallest fraction defined for amounts in this currency:
+
+    >>> TND.symbol
+    u'TND'
+    >>> TND.name
+    u'Tunisian Dinar'
+    >>> TND.smallestFraction
+    Decimal('0.001')
+
+Instantiating a money amount
+----------------------------
+
+As :class:`Money` derives from :class:`Quantity`, an instance can simply be
+created by giving an amount and a unit:
+
+    >>> Money(30, EUR)
+    Money(Decimal(30, 2), Currency(u'EUR'))
+
+All amounts of money are rounded according to the smallest fraction defined
+for the currency:
+
+    >>> Money(3.128, EUR)
+    Money(Decimal('3.13'), Currency(u'EUR'))
+    >>> Money(41.1783, TND)
+    Money(Decimal('41.178'), Currency(u'TND'))
+
+As with other quantities, money amounts can also be derived from a string or
+build using the operator `^`:
+
+    >>> Money('3.18 USD')
+    Money(Decimal('3.18'), Currency(u'USD'))
+    >>> 3.18 ^ USD
+    Money(Decimal('3.18'), Currency(u'USD'))
+
+Computing with money amounts
+----------------------------
+
+:class:`Money` derives from :class:`Quantity`, so all operations on quantities
+can also be applied to instances of :class:`Money`. But because there is no
+fixed relation between currencies, there is no implicit conversion between
+money amounts of different currencies:
+
+    >>> Money(30, EUR) + Money(3.18, EUR)
+    Money(Decimal('33.18'), Currency(u'EUR'))
+    >>> Money(30, EUR) + Money(3.18, USD)
+    IncompatibleUnitsError: Can't convert 'US Dollar' to 'Euro'
+
+Resulting values are always quantized to the smallest fraction defined with
+the currency:
+
+    >>> Money('3.20 USD') / 3
+    Money(Decimal('1.07'), Currency(u'USD'))
+    >>> Money('3.20 TND') / 3
+    Money(Decimal('1.067'), Currency(u'TND'))
+
+Converting between different currencies
+---------------------------------------
+
+Exchange rates
+^^^^^^^^^^^^^^
+
+A conversion factor between two currencies can be defined by using the
+:class:`ExchangeRate`. It is given a unit currency (aka base currency), a unit
+multiple, a term currency (aka price currency) and a term amount, i.e. the
+amount in term currency equivalent to unit multiple in unit currency:
+
+    >>> fxEUR2HKD = ExchangeRate(EUR, 1, HKD, Decimal('8.395804'))
+    >>> fxEUR2HKD
+    ExchangeRate(Currency(u'EUR'), Decimal(1), Currency(u'HKD'),
+        Decimal('8.395804'))
+
+`unitMultiple` and `termAmount` will always be adjusted so that the resulting
+unit multiple is a power to 10 and the resulting term amounts magnitude is >=
+-1. The latter will always be rounded to 6 decimal digits.
+
+    >>> fxTND2EUR = ExchangeRate(TND, 5, EUR, Decimal('0.0082073'))
+    >>> fxTND2EUR
+    ExchangeRate(Currency(u'TND'), Decimal(100), Currency(u'EUR'),
+        Decimal('0.164146'))
+
+The resulting rate for an amount of 1 unit currency in term currency can be
+obtained via the property :attr:`ExchangeRate.rate`:
+
+    >>> fxTND2EUR.rate
+    Decimal('0.00164146')
+
+The property :attr:`ExchangeRate.quotation` gives a tuple of unit currency,
+term currency and rate:
+
+    >>> fxTND2EUR.quotation
+    (Currency(u'TND'), Currency(u'EUR'), Decimal('0.00164146'))
+
+The properties :attr:`ExchangeRate.inverseRate` and
+:attr:`ExchangeRate.inverseQuotation` give the rate and the quotation in the
+opposite direction (but do not round the rate!):
+
+    >>> fxTND2EUR.inverseRate
+    Fraction(50000000, 82073)
+    >>> fxTND2EUR.inverseQuotation
+    (Currency(u'EUR'), Currency(u'TND'), Fraction(50000000, 82073))
+
+The inverse ExchangeRate can be created by calling the method
+:meth:`ExchangeRate.inverted`:
+
+    >>> fxEUR2TND = fxTND2EUR.inverted()
+    >>> fxEUR2TND
+    ExchangeRate(Currency(u'EUR'), Decimal(1), Currency(u'TND'),
+        Decimal('609.213749'))
+
+An exchange rate can be derived from two other exchange rates, provided that
+they have one currency in common ("triangulation"). If the unit currency of
+one exchange rate is equal to the term currency of the other, the two exchange
+rates can be multiplied with each other. If either the unit currencies or the
+term currencies are equal, the two exchange rates can be divided.
+
+    >>> fxEUR2HKD * fxTND2EUR
+    ExchangeRate(Currency(u'TND'), Decimal(10), Currency(u'HKD'),
+        Decimal('0.137814'))
+    >>> fxEUR2HKD / fxEUR2TND
+    ExchangeRate(Currency(u'TND'), Decimal(10), Currency(u'HKD'),
+        Decimal('0.137814'))
+    >>> fxEUR2TND / fxEUR2HKD
+    ExchangeRate(Currency(u'HKD'), Decimal(1), Currency(u'TND'),
+        Decimal('72.561693'))
+    >>> fxHKD2EUR = fxEUR2HKD.inverted()
+    >>> fxTND2EUR / fxHKD2EUR
+    ExchangeRate(Currency(u'TND'), Decimal(10), Currency(u'HKD'),
+        Decimal('0.137814'))
+
+Converting money amounts using exchange rates
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Multiplying an amount in some currency with an exchange rate with the same
+currency as unit currency results in the equivalent amount in term currency:
+
+    >>> mEUR = 5.27 ^ EUR
+    >>> mEUR * fxEUR2HKD
+    Money(Decimal('44.25'), Currency(u'HKD'))
+    >>> mEUR * fxEUR2TND
+    Money(Decimal('3210.556'), Currency(u'TND'))
+
+Likewise, dividing an amount in some currency with an exchange rate with the
+same currency as term currency results in the equivalent amount in unit
+currency:
+
+>>> fxHKD2EUR = fxEUR2HKD.inverted()
+>>> mEUR / fxHKD2EUR
+Money(Decimal('44.25'), Currency(u'HKD'))
+
+Combining Money with other quantities
+-------------------------------------
+
+As :class:`Money` derives from :class:`Quantity`, it can be combined with
+other quantities in order to define a new quantity. This is, for example,
+useful for defining prices per quantum.
+
+    >>> class PricePerMass(Quantity):
+    ...     defineAs = Money / Mass
+
+Because :class:`Money` has no reference unit, there is no reference unit
+created for the derived quantity.
+
+    >>> list(PricePerMass.Unit.registeredUnits())
+    []
+
+Units must be explicitly defined.
+
+    >>> EURpKG = PricePerMass.Unit(defineAs=EUR/KILOGRAM)
+    >>> list(PricePerMass.Unit.registeredUnits())
+    [PricePerMass.Unit(u'EUR/kg')]
+
+As with other derived quantities, the function :func:`quantity.generateUnits`
+can be used to create all units from the cross-product of units of the base
+quantities.
+
+Instances of the derived quantity can be created and used just like those of
+other quantities.
+
+    >>> p = 17.45 ^ EURpKG
+    >>> p * Decimal('1.05')
+    PricePerMass(Decimal('18.354', 4), PricePerMass.Unit(u'EUR/kg'))
+    >>> m = 530 ^ GRAM
+    >>> m * p
+    Money(Decimal('9.26'), Currency(u'EUR'))
+
+Note that instances of the derived class are not automatically quantized to
+the quantum defined for the currency.
+
+    >>> PricePerMass.getQuantum(EURpKG) is None
+    True
+
+Instances of such a "money per quantum" class can also be converted using
+exchange rates, as long as the resulting unit is defined.
+
+    >>> p * fxEUR2HKD
+    QuantityError: Resulting unit not defined: HKD/kg.
+    >>> HKDpKG = PricePerMass.Unit(defineAs=HKD/KILOGRAM)
+    >>> p * fxEUR2HKD
+    PricePerMass(Decimal('146.75865392'), PricePerMass.Unit(u'HKD/kg'))
+"""
+
+
+from __future__ import absolute_import, unicode_literals
+from .moneybase import Currency, Money, ExchangeRate
+from .currencies import getCurrencyInfo, registerCurrency
+
+
+__metaclass__ = type
+
+
+__all__ = [
+    'Currency',
+    'Money',
+    'ExchangeRate',
+    'getCurrencyInfo',
+    'registerCurrency',
+]
---
+++ packages/quantity/quantity-0.8.tar.gz/quantity-0.8/quantity/money/tests/test_money.py
@@ -0,0 +1,247 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------------
+## Name:        test_money
+## Purpose:     Test driver for package quantity.money
+##
+## Author:      Michael Amrhein (mamrhein@users.sourceforge.net)
+##
+## Copyright:   (c) 2015 Michael Amrhein
+##----------------------------------------------------------------------------
+## $Source: quantity/money/tests/test_money.py $
+## $Revision: 6d7ca48b5760 2015-05-08 21:41 +0200 mamrhein $
+
+
+"""Test driver for package quantity.money"""
+
+
+from __future__ import absolute_import, unicode_literals
+import unittest
+import operator
+from fractions import Fraction
+from decimalfp import Decimal
+from quantity import (Quantity, QuantityError, IncompatibleUnitsError,
+                      UndefinedResultError)
+from quantity.money import (Currency, Money, ExchangeRate,
+                            getCurrencyInfo, registerCurrency)
+from quantity.predefined import Mass, GRAM, KILOGRAM, OUNCE
+
+
+__metaclass__ = type
+
+
+EUR = registerCurrency('EUR')
+HKD = registerCurrency('HKD')
+USD = registerCurrency('USD')
+
+
+class PricePerMass(Quantity):
+    defineAs = Money / Mass
+
+EURpKG = PricePerMass.Unit(defineAs=EUR / KILOGRAM)
+HKDpKG = PricePerMass.Unit(defineAs=HKD / KILOGRAM)
+
+
+class Test1_Currency(unittest.TestCase):
+
+    def testConstructor(self):
+        self.assertRaises(TypeError, Currency, 5)
+        self.assertRaises(ValueError, Currency, '')
+        self.assertRaises(TypeError, Currency, 'XXT', 'XTest', 3.4)
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', -1)
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', None, 'a')
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', None,
+                          Fraction(1, 3))
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', None, '-0.1')
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', None, 1)
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', None, '0.03')
+        self.assertRaises(ValueError, Currency, 'XXT', 'XTest', 3, '0.1')
+        XXT = Currency('XXT', 'XTest', 3)
+        self.assertEqual(XXT.isoCode, 'XXT')
+        self.assertEqual(XXT.isoCode, XXT.symbol)
+        self.assertEqual(XXT.name, 'XTest')
+        self.assertEqual(XXT.smallestFraction, Decimal('0.001'))
+        self.assertTrue(Currency.getUnitBySymbol('XXT') is XXT)
+        self.assertTrue(Currency('XXT') is XXT)
+
+    def testCurrencyDatabase(self):
+        self.assertEqual(getCurrencyInfo('EUR')[0], 'EUR')
+        self.assertRaises(ValueError, getCurrencyInfo, 'abc')
+        EUR = registerCurrency('EUR')
+        self.assertTrue(EUR is Currency.getUnitBySymbol('EUR'))
+
+
+class Test2_Money(unittest.TestCase):
+
+    def testConstructor(self):
+        EUR = registerCurrency('EUR')
+        HKD = registerCurrency('HKD')
+        self.assertRaises(QuantityError, Money, 'x', EUR)
+        self.assertRaises(QuantityError, Money, 5)
+        self.assertRaises(QuantityError, Money, 'x EUR')
+        self.assertRaises(QuantityError, Money, '3 fgh')
+        self.assertRaises(QuantityError, Money, '3 kg')
+        self.assertRaises(IncompatibleUnitsError, Money, '3 EUR', HKD)
+        self.assertEqual(Money.getQuantum(HKD), HKD.smallestFraction)
+        m3eur = Money('3 EUR')
+        self.assertTrue(m3eur.unit is EUR)
+        self.assertTrue(m3eur.currency is EUR)
+        self.assertEqual(m3eur.amount, Decimal(3))
+        m333c = Money(Fraction(10, 3), EUR)
+        self.assertEqual(m333c.amount, Decimal('3.33'))
+        p = Quantity('3.17 EUR/kg')
+        self.assertEqual(p.amount, Decimal('3.17'))
+        self.assertTrue(p.unit is EURpKG)
+
+    def testAlternateConstructor(self):
+        m30c = 0.30 ^ EUR
+        self.assertTrue(m30c.unit is EUR)
+        self.assertTrue(m30c.currency is EUR)
+        self.assertEqual(m30c.amount, Decimal('0.3'))
+        self.assertRaises(TypeError, operator.xor, EUR, 3)
+
+    def testComputations(self):
+        d = Decimal('0.3')
+        m30c = d ^ EUR
+        self.assertEqual(m30c + m30c, (d + d) ^ EUR)
+        self.assertEqual(m30c - m30c, 0 ^ EUR)
+        self.assertEqual(7 * m30c, (7 * d) ^ EUR)
+        self.assertEqual(m30c / 7, (d / 7) ^ EUR)
+        self.assertRaises(IncompatibleUnitsError, operator.add, m30c, 2 ^ HKD)
+        self.assertRaises(IncompatibleUnitsError, operator.sub, m30c, 2 ^ HKD)
+        self.assertRaises(UndefinedResultError, operator.mul, m30c, 2 ^ HKD)
+        self.assertRaises(IncompatibleUnitsError, operator.truediv, m30c,
+                          2 ^ HKD)
+        self.assertRaises(UndefinedResultError, operator.pow, m30c, 2)
+
+    def testMixedComputations(self):
+        d = Decimal('12.647')
+        p = d ^ EURpKG
+        self.assertEqual(2 * p, (2 * d) ^ EURpKG)
+        self.assertEqual(p * 5, (5 * d) ^ EURpKG)
+        self.assertEqual(p / 5, (d / 5) ^ EURpKG)
+        self.assertRaises(UndefinedResultError, operator.truediv, 5, p)
+        m = 1.5 ^ KILOGRAM
+        self.assertEqual(m * p, (1.5 * d) ^ EUR)
+        self.assertEqual(m * p, p * m)
+        m = 500 ^ GRAM
+        self.assertEqual(m * p, (d / 2) ^ EUR)
+        m = 24 ^ OUNCE
+        self.assertEqual(m * p, Decimal(24 * KILOGRAM(OUNCE) * d, 2) ^ EUR)
+        self.assertRaises(UndefinedResultError, operator.truediv, m, p)
+        self.assertRaises(UndefinedResultError, operator.truediv, p, m)
+
+
+class Test3_ExchangeRate(unittest.TestCase):
+
+    def testConstructor(self):
+        # unknown ISO 4217 code given for a currency:
+        self.assertRaises(ValueError, ExchangeRate, 'abc', 1, EUR, 1)
+        self.assertRaises(ValueError, ExchangeRate, EUR, 1, 'abc', 1)
+        # wrong type of value given as currency
+        self.assertRaises(TypeError, ExchangeRate, 3, 1, EUR, 1)
+        self.assertRaises(TypeError, ExchangeRate, EUR, 1, 3, 1)
+        # identical currencies
+        self.assertRaises(ValueError, ExchangeRate, EUR, 1, EUR, 1)
+        self.assertRaises(ValueError, ExchangeRate, EUR, 1, 'EUR', 1)
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', 1, EUR, 1)
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', 1, 'EUR', 1)
+        # unit multiple not an Integral or not >= 1
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', 'a', 'HKD', 1)
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', Decimal('0.1'),
+                          'HKD', 1)
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', 0.1, 'HKD', 1)
+        # term amount < 0.000001
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', 1, 'HKD',
+                          Decimal('0.0000009'))
+        # term amount not convertable to a Decimal
+        self.assertRaises(ValueError, ExchangeRate, 'EUR', 1, 'HKD', '0.x')
+        # test attributes
+        rate = Decimal('8.395804')
+        exch = ExchangeRate(EUR, 1, HKD, rate)
+        self.assertTrue(exch.unitCurrency is EUR)
+        self.assertTrue(exch.termCurrency is HKD)
+        self.assertEqual(exch.rate, rate)
+        self.assertEqual(exch.inverseRate, 1 / rate)
+        self.assertEqual(exch.quotation, (EUR, HKD, rate))
+        self.assertEqual(exch.inverseQuotation, (HKD, EUR, 1 / rate))
+        # test adjustment
+        rate = Decimal('0.00000927')
+        exch = ExchangeRate(EUR, 1, HKD, rate)
+        self.assertEqual(exch._unitMultiple, Decimal(100000))
+        self.assertEqual(exch._termAmount, Decimal('0.927', 6))
+        self.assertEqual(exch.rate, rate)
+        exch2 = ExchangeRate('EUR', 50, 'HKD', 50 * rate)
+        self.assertEqual(exch.rate, exch2.rate)
+
+    def testComputations(self):
+        rateEUR2HKD = Decimal('8.395804')
+        fxEUR2HKD = ExchangeRate(EUR, 1, HKD, rateEUR2HKD)
+        rateEUR2USD = Decimal('1.0457')
+        fxEUR2USD = ExchangeRate(EUR, 1, USD, rateEUR2USD)
+        # inversion
+        fxHKD2EUR = fxEUR2HKD.inverted()
+        self.assertEqual(fxHKD2EUR.rate, Decimal(1 / rateEUR2HKD, 6))
+        fxUSD2EUR = fxEUR2USD.inverted()
+        self.assertTrue(fxUSD2EUR.unitCurrency is USD)
+        self.assertTrue(fxUSD2EUR.termCurrency is EUR)
+        # ExchangeRate * Money
+        self.assertEqual(fxEUR2HKD * (1 ^ EUR),
+                         Money(rateEUR2HKD, HKD))
+        self.assertEqual(fxEUR2HKD * (1000 ^ EUR),
+                         Money(1000 * rateEUR2HKD, HKD))
+        self.assertRaises(ValueError, operator.mul, fxEUR2HKD, 1 ^ HKD)
+        # ExchangeRate * Money/Quantity
+        d = Decimal('12.647')
+        p = d ^ EURpKG
+        self.assertEqual(fxEUR2HKD * p, PricePerMass(d * rateEUR2HKD, HKDpKG))
+        self.assertEqual(p * fxEUR2HKD, PricePerMass(d * rateEUR2HKD, HKDpKG))
+        self.assertRaises(QuantityError, operator.mul, fxHKD2EUR, p)
+        self.assertRaises(QuantityError, operator.mul, p, fxHKD2EUR)
+        self.assertRaises(QuantityError, operator.mul, fxEUR2USD, p)
+        self.assertRaises(QuantityError, operator.mul, p, fxEUR2USD)
+        # ExchangeRate * ExchangeRate
+        fxHKD2USD = fxHKD2EUR * fxEUR2USD
+        self.assertEqual(fxHKD2USD.rate,
+                         Decimal(fxHKD2EUR.rate * fxEUR2USD.rate, 6))
+        self.assertTrue(fxHKD2USD.unitCurrency is HKD)
+        self.assertTrue(fxHKD2USD.termCurrency is USD)
+        fxUSD2HKD = fxEUR2HKD * fxUSD2EUR
+        self.assertEqual(fxUSD2HKD.rate,
+                         Decimal(fxEUR2HKD.rate * fxUSD2EUR.rate, 6))
+        self.assertTrue(fxUSD2HKD.unitCurrency is USD)
+        self.assertTrue(fxUSD2HKD.termCurrency is HKD)
+        self.assertRaises(ValueError, operator.mul, fxEUR2HKD, fxEUR2USD)
+        # unsupported multiplications
+        self.assertRaises(TypeError, operator.mul, fxEUR2HKD, EUR)
+        self.assertRaises(TypeError, operator.mul, fxEUR2HKD, 5)
+        self.assertRaises(TypeError, operator.mul, fxEUR2HKD, Mass(7))
+        # Money / ExchangeRate
+        self.assertEqual((100 ^ HKD) / fxEUR2HKD,
+                         Money(Decimal(100 / rateEUR2HKD, 6), EUR))
+        self.assertRaises(ValueError, operator.truediv, 1 ^ EUR, fxEUR2HKD)
+        # Money/Quantity / ExchangeRate
+        self.assertEqual(p / fxHKD2EUR,
+                         PricePerMass(d / fxHKD2EUR.rate, HKDpKG))
+        self.assertRaises(QuantityError, operator.truediv, p, fxEUR2HKD)
+        # ExchangeRate / ExchangeRate
+        fxUSD2HKD = fxEUR2HKD / fxEUR2USD
+        self.assertEqual(fxUSD2HKD.rate,
+                         Decimal(fxEUR2HKD.rate / fxEUR2USD.rate, 6))
+        self.assertTrue(fxUSD2HKD.unitCurrency is USD)
+        self.assertTrue(fxUSD2HKD.termCurrency is HKD)
+        fxHKD2USD = fxHKD2EUR / fxUSD2EUR
+        self.assertEqual(fxHKD2USD.rate,
+                         Decimal(fxHKD2EUR.rate / fxUSD2EUR.rate, 6))
+        self.assertTrue(fxHKD2USD.unitCurrency is HKD)
+        self.assertTrue(fxHKD2USD.termCurrency is USD)
+        self.assertEqual(fxHKD2EUR / fxUSD2EUR, fxEUR2USD / fxEUR2HKD)
+        self.assertRaises(ValueError, operator.truediv, fxHKD2EUR, fxEUR2USD)
+        # unsupported divisions
+        self.assertRaises(TypeError, operator.truediv, fxEUR2HKD, 1 ^ HKD)
+        self.assertRaises(TypeError, operator.truediv, EUR, fxEUR2HKD)
+        self.assertRaises(TypeError, operator.truediv, 5, fxEUR2HKD)
+        self.assertRaises(TypeError, operator.truediv, fxEUR2HKD, EUR)
+        self.assertRaises(TypeError, operator.truediv, fxEUR2HKD, 5)
+        self.assertRaises(TypeError, operator.truediv, fxEUR2HKD, Mass(7))
+        self.assertRaises(TypeError, operator.truediv, fxEUR2HKD, p)
