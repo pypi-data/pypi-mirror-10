15f5850fc71a60a7efa2a290d49eed1df6ec3c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:48 +0000

    Add package mete0r.recipe.whoami/0.1.0/mete0r.recipe.whoami-0.1.0.tar.gz

---
+++ packages/mete0r.recipe.whoami/mete0r.recipe.whoami-0.1.0.tar.gz/mete0r.recipe.whoami-0.1.0/mete0r/recipe/whoami/tests.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+#   mete0r.recipe.whoami : a zc.buildout recipe to know whoami
+#   Copyright (C) 2014 mete0r <mete0r@sarangbang.or.kr>
+#
+#   This file is part of mete0r.recipe.whoami.
+#
+#   mete0r.recipe.whoami is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public License as
+#   published by the Free Software Foundation, either version 3 of the License,
+#   or (at your option) any later version.
+#
+#   mete0r.recipe.whoami is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+#   License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from unittest import TestCase
+
+
+class RecipeTest(TestCase):
+
+    def test_nothing(self):
+        pass
