898cdbfc13717cb720f84207a73aafde4ffe9c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:56 +0000

    Add package kotoba/3.1.0/Kotoba-3.1.0.tar.gz

---
+++ packages/kotoba/Kotoba-3.1.0.tar.gz/Kotoba-3.1.0/setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name         = 'Kotoba',
+    version      = '3.1.0',
+    description  = 'XML/JSON Reading Library with Level-3 CSS Selector',
+    author       = 'Juti Noppornpitak',
+    author_email = 'juti_n@yahoo.co.jp',
+    url          = 'http://shiroyuki.com/work/projects-kotoba',
+    packages     = ['kotoba']
+)
