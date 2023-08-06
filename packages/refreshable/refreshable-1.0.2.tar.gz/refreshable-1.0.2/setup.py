d7d7bcf6460fcf1946bc3221af6aa60040ca7b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:32 +0000

    Add package refreshable/1.0.2/refreshable-1.0.2.tar.gz

---
+++ packages/refreshable/refreshable-1.0.2.tar.gz/refreshable-1.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import fnmatch
+import glob
+import os
+import sys
+
+from setuptools import setup
+
+VERSION = "1.0.2"
+
+setup(
+    name='refreshable',
+    version=VERSION,
+    description='Tools for livecoding performances',
+    author='Zach Banks',
+    author_email='zbanks@mit.edu',
+    url='https://github.com/zbanks/doitlive',
+    packages=[
+        'refreshable', 
+    ],
+    download_url="https://github.com/zbanks/doitlive/tarball/{}".format(VERSION),
+    zip_safe=True,
+    package_dir = {
+        'refreshable': 'src'
+    },
+    package_data={
+    },
+)
