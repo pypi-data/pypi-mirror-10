f15012a17413ffaee7ea0c720b715e8bfbb03ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:23 +0000

    Add package hikvision/1.1/hikvision-1.1.tar.gz

---
+++ packages/hikvision/hikvision-1.1.tar.gz/hikvision-1.1/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2015 Finbarr Brady <https://github.com/fbradyirl>
+# Licensed under the MIT license.
+
+# Used this guide to create module
+# http://peterdowns.com/posts/first-time-with-pypi.html
+
+# git tag 0.1 -m "0.1 release"
+# git push --tags origin master
+# 
+# Upload to PyPI Live
+# python setup.py register -r pypi ; python setup.py sdist upload -r pypi
+# 
+
+from distutils.core import setup
+
+setup(
+    name='hikvision',
+    version='1.1',
+    description='Provides a python interface to interact with a hikvision camera',
+    author='Finbarr Brady',
+    author_email='fbradyirl@users.noreply.github.com',
+    url='https://github.com/fbradyirl/hikvision',
+    download_url = 'https://github.com/fbradyirl/hikvision/tarball/1.1',
+    keywords='hikvision camera python cgi interface',
+    packages=['hikvision'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet'
+        ],
+    )
