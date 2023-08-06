daa2c073b8a190ab5a561461847171d6b4476de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:26 +0000

    Add package ip2location-python/0.1.2/ip2location-python-0.1.2.tar.gz

---
+++ packages/ip2location-python/ip2location-python-0.1.2.tar.gz/ip2location-python-0.1.2/ip2location/metadata.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+NAME = 'ip2location-python'
+DESCRIPTION = 'ipv4 to location'
+VERSION = '0.1.2'
+AUTHOR = 'pengyao'
+AUTHOR_EMAIL = 'pengyao@pengyao.org'
+COPYRIGHT = 'Copyright © 2015 ' + AUTHOR
+LICENCE = 'Apache License V2.0'
+URL = 'https://github.com/pengyao/ip2location-python'
