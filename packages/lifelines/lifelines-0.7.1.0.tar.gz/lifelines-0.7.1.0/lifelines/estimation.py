a0814023ac2eedaaae773dc658af23cb77b0a7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:19 +0000

    Add package lifelines/0.7.1.0/lifelines-0.7.1.0.tar.gz

---
+++ packages/lifelines/lifelines-0.7.1.0.tar.gz/lifelines-0.7.1.0/lifelines/estimation.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from lifelines._base_fitter import BaseFitter
+from lifelines.weibull_fitter import WeibullFitter
+from lifelines.exponential_fitter import ExponentialFitter
+from lifelines.nelson_aalen_fitter import NelsonAalenFitter
+from lifelines.kaplan_meier_fitter import KaplanMeierFitter
+from lifelines.breslow_fleming_harrington_fitter import BreslowFlemingHarringtonFitter
+from lifelines.coxph_fitter import CoxPHFitter
+from lifelines.aalen_additive_fitter import AalenAdditiveFitter
