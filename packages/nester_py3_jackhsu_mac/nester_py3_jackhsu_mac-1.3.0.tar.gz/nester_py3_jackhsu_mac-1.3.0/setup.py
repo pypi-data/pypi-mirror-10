5e45065d602129a0cefad435c76be64423fe6fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:29 +0000

    Add package nester_py3_jackhsu_mac/1.3.0/nester_py3_jackhsu_mac-1.3.0.tar.gz

---
+++ packages/nester_py3_jackhsu_mac/nester_py3_jackhsu_mac-1.3.0.tar.gz/nester_py3_jackhsu_mac-1.3.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name        = "nester_py3_jackhsu_mac",
+    version     = "1.3.0",
+    py_module   = ["nester_py3_jackhsu_mac"],
+    author      = "jackhsu",
+    )
