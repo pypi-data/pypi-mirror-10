fad2449e5947f92ecd9164a5d578f401dec352aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:21 +0000

    Add package urlopen/1.0.0/urlopen-1.0.0.zip

---
+++ packages/urlopen/urlopen-1.0.0.zip/urlopen-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+		name			='urlopen',
+		version			='1.0.0',
+		py_modules		=['urlopen'],
+		author			='kindian',
+		author_email	='patra.kailash06@gmail.com',
+		url				='http://www.headfirstlabs.com',
+		description		='A simple printer of automated lists',
+                package_data = { '':['*.txt']},
+		)
