dd96825293e912a50665ca08e902c266b08794c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:52 +0000

    Add package pydcard/0.0.5/pydcard-0.0.5.tar.gz

---
+++ packages/pydcard/pydcard-0.0.5.tar.gz/pydcard-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+__version__ = '0.0.5'
+__author__ = 'John Lin'
+__email__ = 'linton.tw@gmail.com'
+
+setup(name='pydcard',
+      version=__version__,
+      description='This is a Dcard API wrapper',
+      long_description=open('README.md').read(),
+      url='http://github.com/John-Lin/pydcard',
+      author=__author__,
+      author_email=__email__,
+      license='MIT',
+      packages=['pydcard'],
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      install_requires=['requests'],
+      zip_safe=False)
