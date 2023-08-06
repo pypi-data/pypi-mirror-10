47c87d8e4685acd360d8a347c9639deb884dac00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:11 +0000

    Add package django-constance-updated/1.0.1/django-constance-updated-1.0.1.tar.gz

---
+++ packages/django-constance-updated/django-constance-updated-1.0.1.tar.gz/django-constance-updated-1.0.1/constance/models.py
@@ -0,0 +1,30 @@
+from django.db.models import signals
+
+
+def create_perm(app, created_models, verbosity, db, **kwargs):
+    """
+    Creates a fake content type and permission
+    to be able to check for permissions
+    """
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
+    from django import VERSION
+
+    if ContentType._meta.installed and Permission._meta.installed:
+        if VERSION >= (1, 8):
+            content_type, created = ContentType.objects.get_or_create(
+                app_label='constance',
+                model='config')
+        else:
+            content_type, created = ContentType.objects.get_or_create(
+                name='config',
+                app_label='constance',
+                model='config')
+
+        permission, created = Permission.objects.get_or_create(
+            name='Can change config',
+            content_type=content_type,
+            codename='change_config')
+
+
+signals.post_syncdb.connect(create_perm, dispatch_uid="constance.create_perm")
