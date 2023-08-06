c7321eec1dee2042df8e958de4c5861a9cc8e6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:07 +0000

    Add package httpie-edgegrid/1.0.2/httpie-edgegrid-1.0.2.tar.gz

---
+++ packages/httpie-edgegrid/httpie-edgegrid-1.0.2.tar.gz/httpie-edgegrid-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+setup(
+    name='httpie-edgegrid',
+    description='Edgegrid plugin for HTTPie.',
+    long_description=open('README.rst').read().strip(),
+    version='1.0.2',
+    author='Kirsten Hunter',
+    author_email='khunter@akamai.com',
+    license='Apache 2.0',
+    url='https://github.com/akamai-open/httpie-edgegrid',
+    download_url='https://github.com/akamai-open/httpie-edgegrid',
+    py_modules=['httpie_edgegrid'],
+    zip_safe=False,
+    entry_points={
+        'httpie.plugins.auth.v1': [
+            'httpie_oauth1 = httpie_edgegrid:EdgeGridPlugin'
+        ]
+    },
+    install_requires=[
+        'httpie>=0.7.0',
+	'edgegrid-python>=1.0.5'
+    ],
+)
