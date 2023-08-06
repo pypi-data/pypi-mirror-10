d76282d825003a078022340c7001c5eb380100a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:23 +0000

    Add package pyARGO/0.0.1/pyargo-0.0.1.tar.gz

---
+++ packages/pyARGO/pyargo-0.0.1.tar.gz/pyargo-0.0.1/tests/test_argo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_argo
+----------------------------------
+
+Tests for `argo` module.
+"""
+
+pass
+#from argo import argo
+
+
+# import unittest
+
+
+# class TestArgo(unittest.TestCase):
+
+#    def setUp(self):
+#        pass
+
+#    def test_something(self):
+#        pass
+
+#    def tearDown(self):
+#        pass
+
+# if __name__ == '__main__':
+#    unittest.main()
