ad408c09b3250766c0b578bd65f02c2f1bd7f16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:15 +0000

    Add package NetStorageKit/1.0.4/NetStorageKit-1.0.4.tar.gz

---
+++ packages/NetStorageKit/NetStorageKit-1.0.4.tar.gz/NetStorageKit-1.0.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from os.path import expanduser
+from setuptools import setup
+
+setup(
+    name='NetStorageKit',
+    version='1.0.4',
+    description='Akamai\'s NetStorage API communication kit',
+    author='Ernesto Mendoza Blanco',
+    author_email='ernestom@mentanetwork.com',
+    install_requires=[
+        'requests',
+        'responses',
+        'pytest',
+        'ntplib',
+        'pytest-cov',
+        'requests[security]'
+    ],
+    url='https://github.com/MentaNetwork/NetStorageKit-Python',
+    download_url='https://github.com/MentaNetwork/NetStorageKit-Python',
+    package_data={'': ['*.json.dist']},
+    include_package_data=True,
+    data_files=[(expanduser('~'), ['netstoragekit_test_credentials.json.dist'])],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python']
+)
