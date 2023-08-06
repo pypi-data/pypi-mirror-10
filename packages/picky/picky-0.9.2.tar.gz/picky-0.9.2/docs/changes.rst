77127e967400d75a6abbab971d9d2b4cf94e05f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:04 +0000

    Add package picky/0.9.2/picky-0.9.2.tar.gz

---
+++ packages/picky/picky-0.9.2.tar.gz/picky-0.9.2/docs/changes.rst
@@ -0,0 +1,33 @@
+Changes
+=======
+
+0.9.2 (1 July 2015)
+-------------------
+
+- check to see if ``pip`` takes ``--disable-pip-version-check`` before using it.
+
+
+0.9.1 (23 June 2015)
+--------------------
+
+- correct the dependency specification of :mod:`argparse` so it only
+  occurs on Python 2.6
+
+0.9 (22 June 2015)
+------------------
+
+- Python 3 support
+
+- Fixed handling of package 'extras' in pip output and specifications.
+
+- Fixed handling of arbitrary equality clauses in pip output and specifications.
+
+0.8 (22 June 2015)
+------------------
+
+- Initial Release
+
+Still to Come
+=============
+
+- Automated code coverage metrics
---
+++ packages/picky/picky-0.9.2.tar.gz/picky-0.9.2/setup.py
@@ -0,0 +1,50 @@
+# See license.txt for license details.
+# Copyright (c) 2015 Simplistix Ltd
+
+import os, sys
+
+from setuptools import setup, find_packages
+
+base_dir = os.path.dirname(__file__)
+
+setup(
+    name='picky',
+    version='0.9.2',
+    author='Chris Withers',
+    author_email='chris@simplistix.co.uk',
+    license='MIT',
+    description=(
+        "A tool for checking versions of packages used by conda or pip "
+        "are as specified in their requirements files."
+    ),
+    long_description=open(os.path.join(base_dir,'docs','description.rst')).read(),
+    url='https://github.com/Simplistix/picky',
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    extras_require={
+        ':python_version=="2.6"': ['argparse'],
+        'test': [
+            'testfixtures',
+            'nose',
+            'nose_fixes',
+            'nose-cov',
+            ],
+        'build': ['sphinx', 'pkginfo', 'setuptools-git', 'twine', 'wheel']
+},
+    entry_points = {
+        'console_scripts': [
+            'picky = picky.main:main',
+    ]}
+)
