98853a7eb8b67e25276b834f43a381c4a7a6cad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:08 +0000

    Add package qlutils/0.1.0.9/qlutils-0.1.0.9.tar.gz

---
+++ packages/qlutils/qlutils-0.1.0.9.tar.gz/qlutils-0.1.0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='qlutils',
+      packages=find_packages(),
+      version='0.1.0.9',
+      author='Victor Polevoy',
+      author_email='contact@vpolevoy.com',
+      url='https://bitbucket.org/fx_/python3-qlutils',
+      requires=['qllauncher'],
+      description='QuakeLive Utilities.',
+      entry_points = {
+          'console_scripts': [
+              'qllserver = qlutils.qllserver:main',
+          ],              
+      },
+      )
