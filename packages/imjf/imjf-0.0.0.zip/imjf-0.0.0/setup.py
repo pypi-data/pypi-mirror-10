1f1e5fe20a63892f98d0f14cc723140a75c5477e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package imjf/0.0.0/imjf-0.0.0.zip

---
+++ packages/imjf/imjf-0.0.0.zip/imjf-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='imjf',
+    packages=['imjf'],
+    version='0.0.0',
+    description='The official Python client for www.isMyJsFucked.com',
+    author='Drew French',
+    author_email='rectangletangle@gmail.com',
+    url='https://github.com/rectangletangle/imjf-py',
+    download_url='https://github.com/rectangletangle/imjf-py/tarball/0.0.0',
+    install_requires=['requests']
+)
