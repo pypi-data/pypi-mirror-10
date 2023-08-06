4619abcd4b8be92629dbb491b22db8836595f585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:45 +0000

    Add package ConfigWrapper/0.4.1/ConfigWrapper-0.4.1.tar.gz

---
+++ packages/ConfigWrapper/ConfigWrapper-0.4.1.tar.gz/ConfigWrapper-0.4.1/src/configwrapper/options/relations.py
@@ -0,0 +1,94 @@
+from ConfigParser import NoSectionError
+from configwrapper import ValidationError
+from configwrapper.section import ConfigSection, ConfigOption
+
+__author__ = 'Lai Tash (lai.tash@yandex.ru)'
+
+
+class SectionOption(ConfigOption):
+    def __init__(self, section=None, *args, **kwargs):
+        self.section_cls = section
+        super(SectionOption, self).__init__(*args, **kwargs)
+
+    def validate(self, instance, serialized=None):
+        super(SectionOption, self).validate(instance, serialized)
+        section = self._instanciate(instance, serialized)
+        if not section.exists():
+            raise ValidationError('Section %s does not exist' %
+                                  section.config_name)
+
+
+    def _get_default(self, instance):
+        if self.default is not None:
+            return self.default
+        return self.deserialize('', instance)
+
+
+    def deserialize(self, string_, instance):
+        result= self._instanciate(instance, string_)
+        if result.prefix is None:
+            raise ValidationError('SectionOption section prefix should not be '
+                                  'None')
+        return result
+
+    def _instanciate(self, instance, string_):
+        suffix = string_
+        section_cls = self.section_cls or self.name
+        if isinstance(section_cls, basestring):
+            try:
+                section = getattr(instance.schema, section_cls)
+            except AttributeError:
+                raise NoSectionError(section_cls)
+        else:
+            section = section_cls
+        if not isinstance(section, ConfigSection):
+            if (isinstance(section, type) and
+                    issubclass(section, ConfigSection)):
+                section = section()
+            else:
+                raise ValidationError('Not a section"')
+        return section[suffix].bind(schema=instance.schema)
+
+    def serialize(self, value, instance):
+        return value.suffix
+
+    def _validate_serialized(self, string_):
+        pass
+
+    def _validate_value(self, value):
+        return isinstance(value, ConfigSection)
+
+
+class ObjectOption(ConfigOption):
+    def __init__(self, factories, *args, **kwargs):
+        self.factories = factories
+        super(ObjectOption, self).__init__(*args, **kwargs)
+
+    def _get_default(self, instance):
+        if self.default is not None:
+            return self.create(self.default, instance)
+        return self.deserialize('', instance)
+
+    def create(self, factory, instance):
+        return self.factories[factory](instance)
+
+    def deserialize(self, string_, instance):
+        return self.create(string_, instance)
+
+    def serialize(self, value, instance):
+        raise NotImplementedError()
+
+    def _validate_serialized(self, string_):
+        return string_ in self.factories
+
+    def _validate_value(self, value):
+        pass
+
+
+class CachedObjectOption(ObjectOption):
+    def create(self, factory, instance):
+        result = self.factories[factory](instance)
+        self.factories[factory] = lambda sec: result
+        return result
+
+
---
+++ packages/ConfigWrapper/ConfigWrapper-0.4.1.tar.gz/ConfigWrapper-0.4.1/src/configwrapper/schema.py
@@ -0,0 +1,63 @@
+from itertools import ifilter
+from configwrapper import ValidationError
+from configwrapper.section import ConfigSection
+
+__author__ = 'Lai Tash (lai.tash@yandex.ru)'
+
+
+#class _ConfigSchemaMeta(type):
+#    def __init__(cls, *args, **kwargs):
+#        for key, member in cls.__dict__.iteritems():
+#            if (isinstance(member, type) and
+#                    issubclass(member, ConfigSection)):
+#                obj = member()
+#                obj.schema = cls
+#                setattr(cls, key, )
+
+
+class ConfigSchema(object):
+
+    allow_undefined = False
+
+    def __init__(self, config, allow_undefined=None, **sections):
+        if allow_undefined is not None:
+            self.allow_undefined = allow_undefined
+        self.config = config
+        for name in dir(self):
+            member = getattr(self, name)
+            if isinstance(member, type) and issubclass(member, ConfigSection):
+                obj = member()
+                obj.bind(schema=self)
+                if obj.prefix is None:
+                    obj.bind(prefix=name)
+                setattr(self, name, obj)
+            elif isinstance(member, ConfigSection):
+                member.bind(schema=self)
+                if member.prefix is None:
+                    member.bind(prefix=name)
+        for key, section in sections.iteritems():
+            section.bind(schema=self)
+            if section.prefix is None:
+                section.bind(prefix=key)
+            setattr(self, key, section)
+
+    @property
+    def defined_sections(self):
+        for key in dir(self):
+            member = getattr(self, key)
+            if isinstance(member, ConfigSection):
+                yield member
+
+    def validate(self):
+        for section_name in self.config.sections():
+            section = (s.match_name(section_name)
+                       for s in self.defined_sections)
+            section = ifilter(None, section)
+            section = next(section, None)
+
+            if section is None:
+                if not self.allow_undefined:
+                    raise ValidationError("Undefined section: '%s'" %
+                                          section_name)
+            else:
+                section.validate()
