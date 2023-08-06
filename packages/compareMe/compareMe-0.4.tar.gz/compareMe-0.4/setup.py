1c893ec9a9bedb80b14a54fd3bf890c695fae67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package compareMe/0.4/compareMe-0.4.tar.gz

---
+++ packages/compareMe/compareMe-0.4.tar.gz/compareMe-0.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'compareMe',
+  packages = ['compareMe' , 'compareMe.bin'], 
+  version = '0.4',
+  description = 'A comparison library',
+  author = 'Punit Dhoot',
+  author_email = 'punitdhoot1@gmail.com',
+  url = 'https://github.com/pdhoot/Compare-Me', 
+  download_url = 'https://github.com/pdhoot/Compare-Me/tarball/0.4',
+  keywords = ['compare' , 'users' , 'spoj' ,'codechef' , 'github'], # arbitrary keywords
+  classifiers = [],
+  scripts = ['compareMe/bin/compareMe'],
+  install_requires = ['bs4'],
+  license = 'MIT'
+)
