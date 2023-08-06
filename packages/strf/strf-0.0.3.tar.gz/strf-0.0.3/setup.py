d8c9a53a5185c953629487f92bbdd2d36ceb1a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:36 +0000

    Add package strf/0.0.3/strf-0.0.3.tar.gz

---
+++ packages/strf/strf-0.0.3.tar.gz/strf-0.0.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='strf',
+    packages=['strf'],
+    package_dir={'strf':'strf'},
+    version='0.0.3',
+    description="string format locals",
+    long_description=readme,
+    author="Douglas La Rocca",
+    author_email='larocca@larocca.io',
+    url='https://github.com/douglas-larocca/strf',
+    license="BSD",
+    zip_safe=False,
+    keywords='string format locals inspect',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+)
