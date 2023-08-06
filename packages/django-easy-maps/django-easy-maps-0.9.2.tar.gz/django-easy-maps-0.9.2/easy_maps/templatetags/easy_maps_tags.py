1022657aec472cc51dd597ebd6125ab1a9b36ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:22 +0000

    Add package django-easy-maps/0.9.2/django-easy-maps-0.9.2.tar.gz

---
+++ packages/django-easy-maps/django-easy-maps-0.9.2.tar.gz/django-easy-maps-0.9.2/easy_maps/templatetags/easy_maps_tags.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+from classytags.core import Options
+from classytags.helpers import InclusionTag
+from classytags.arguments import Argument, IntegerArgument
+
+from ..conf import settings
+from ..models import Address
+
+register = template.Library()
+
+CENTER = settings.EASY_MAPS_CENTER
+
+
+class EasyMapTag(InclusionTag):
+    """
+    The syntax:
+
+    {% easy_map <address> [<width> <height>] [<zoom>] [using <template_name>] %}
+
+    The "address" parameter can be an ``easy_maps.Address`` instance or a string
+    describing it.  If an address is not found a new entry is created
+    in the database.
+
+    """
+    name = 'easy_map'
+    template = 'easy_maps/map.html'
+    options = Options(
+        Argument('address', resolve=True, required=True),
+        IntegerArgument('width', required=False, default=None),
+        IntegerArgument('height', required=False, default=None),
+        IntegerArgument('zoom', required=False, default=16),
+        'using',
+        Argument('template_name', default=None, required=False),
+    )
+
+    def render_tag(self, context, **kwargs):
+        params = dict((k,v) for k,v in kwargs.items() if v is not None)
+        if len(params.keys()) == 3 or len(params.keys()) > 5:
+            raise template.TemplateSyntaxError("easy_map tag has the following syntax: "
+                "{% easy_map <address> [<width> <height>] [zoom] [using <template_name>] %}")
+        return super(EasyMapTag, self).render_tag(context, **kwargs)
+    
+    def get_template(self, context, **kwargs):
+        return kwargs.get('template_name', None) or self.template
+
+    def parse_address(self, address=None):
+        if isinstance(address, Address):
+            return address
+
+        if not address:
+            return Address(latitude=CENTER[0], longitude=CENTER[1])
+        else:
+            return Address.objects.get_or_create(address=address)[0]
+
+        raise NotImplementedError
+                                               
+    def get_context(self, context, **kwargs):
+        kwargs.update({'map': self.parse_address(kwargs.pop('address'))})
+        return kwargs
+
+register.tag(EasyMapTag)
---
+++ packages/django-easy-maps/django-easy-maps-0.9.2.tar.gz/django-easy-maps-0.9.2/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+from easy_maps import __version__
+
+setup(
+    name='django-easy-maps',
+    version=__version__,
+    packages=[
+        'easy_maps',
+        'easy_maps.migrations',
+        'easy_maps.templatetags',
+        'easy_maps.south_migrations',
+    ],
+    include_package_data=True,
+    license='MIT License',
+    description="This app makes it easy to display a map for a given address",
+    long_description=README,
+    url='https://github.com/kmike/django-easy-maps',
+    author='Mikhail Korobov',
+    author_email='kmike84@gmail.com',
+    maintainer='Basil Shubin',
+    maintainer_email='basil.shubin@gmail.com',
+    install_requires=[
+        'django',
+        'django-appconf',
+        'django-classy-tags==0.6.2',
+        'geopy>=0.96',
+    ],    
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',        
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    zip_safe=False,
+)
