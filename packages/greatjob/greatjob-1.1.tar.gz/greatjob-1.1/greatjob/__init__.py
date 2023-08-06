2efde471ec99952931d44737c562060f7babe017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:03 +0000

    Add package greatjob/1.1/greatjob-1.1.tar.gz

---
+++ packages/greatjob/greatjob-1.1.tar.gz/greatjob-1.1/greatjob/__init__.py
@@ -0,0 +1,14 @@
+
+import os 
+
+main_image = os.path.dirname(os.path.abspath(__file__)) + "/thumbsup.png"
+sparkle_image = os.path.dirname(os.path.abspath(__file__)) + "/sparkle.png"
+
+def main():
+	cwd = os.getcwd()
+	os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/proj/")
+	# os.system('make clean')
+	# os.system('make install')
+
+	os.system("/usr/local/bin/imageleap -i %s -p %s" % (main_image, sparkle_image))
+
---
+++ packages/greatjob/greatjob-1.1.tar.gz/greatjob-1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages, Extension
+
+setup(name='greatjob',
+      version='1.1',
+      description='Congratulations',
+      author='Celeen Rusk',
+      author_email='celeenrusk@gmail.com',
+      liscense="MIT",
+      packages=find_packages(),
+      package_data={
+            '': ['imageleap', 'sparkle.png', 'thumbsup.png', 'proj/*']
+      },
+      entry_points={
+      	'console_scripts': [
+      		'greatjob=greatjob:main'
+      	]
+      }
+     )
