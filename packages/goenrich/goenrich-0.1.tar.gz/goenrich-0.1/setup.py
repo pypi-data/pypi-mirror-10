59abe719d911e8841bc8212b45634b4144a77eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package goenrich/0.1/goenrich-0.1.tar.gz

---
+++ packages/goenrich/goenrich-0.1.tar.gz/goenrich-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='goenrich',
+      version='0.1',
+      description='GO enrichment with python -- pandas meets networkx',
+      url='http://github.com/jdrudolph/enrich',
+      author='Jan Daniel Rudolph',
+      author_email='jan.daniel.rudolph@gmail.com',
+      license='MIT',
+      packages=['goenrich'],
+      zip_safe=False)
