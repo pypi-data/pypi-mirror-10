4c21fa6b529a51d2a75385ff7184807b3cefb51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:02 +0000

    Add package jaraco.modb/2.1.1/jaraco.modb-2.1.1.tar.gz

---
+++ packages/jaraco.modb/jaraco.modb-2.1.1.tar.gz/jaraco.modb-2.1.1/tests/test_handlers.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import importlib
+import collections
+import decimal
+
+import jsonpickle
+
+import jaraco.modb.handlers
+
+def setup_module():
+	importlib.import_module('jaraco.modb')
+
+def roundtrip(ob):
+	encoded = jsonpickle.encode(ob)
+	print('encoded is', encoded)
+	decoded = jsonpickle.decode(encoded)
+	assert decoded == ob
+
+def test_OrderedDict():
+	d = collections.OrderedDict(y=3)
+	roundtrip(d)
+
+def test_Decimal():
+	roundtrip(decimal.Decimal(1.0))
+	roundtrip(decimal.Decimal(1000))
+
+@jaraco.modb.handlers.SimpleReduceHandler.handles
+class MyUnicode(unicode):
+	def __reduce__(self):
+		return MyUnicode, (unicode(self),)
+
+def test_UnicodeSubclass():
+	roundtrip(MyUnicode('foo'))
+
+def test_custom_handler_references():
+	"""
+	https://github.com/jsonpickle/jsonpickle/issues/37
+	"""
+	ob = MyUnicode('test me')
+	subject = dict(a=ob, b=ob, c=ob)
+	roundtrip(subject)
