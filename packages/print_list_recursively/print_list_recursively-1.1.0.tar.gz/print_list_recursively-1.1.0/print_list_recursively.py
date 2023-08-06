1ebabeba4c4f2096eea8c30f5bdab51df5b119a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:14 +0000

    Add package print_list_recursively/1.1.0/print_list_recursively-1.1.0.tar.gz

---
+++ packages/print_list_recursively/print_list_recursively-1.1.0.tar.gz/print_list_recursively-1.1.0/print_list_recursively.py
@@ -0,0 +1,11 @@
+def print_list_recursively(lists, tab_indention_num):
+    for each_item in lists:
+        if isinstance(each_item, list):
+            print_list_recursively(each_item, tab_indention_num+1)
+        else:
+            tab_indention_num = int(tab_indention_num)
+            if(tab_indention_num < 0):
+                tab_indention_num = 0
+            for tab_indention in range(tab_indention_num):
+                print("\t", end="")
+            print(each_item)
