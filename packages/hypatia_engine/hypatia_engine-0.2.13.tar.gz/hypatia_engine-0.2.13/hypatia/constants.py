c7c26af48062ae528f2388b1b4589b0d320c39ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:41 +0000

    Add package hypatia_engine/0.2.13/hypatia_engine-0.2.13.tar.gz

---
+++ packages/hypatia_engine/hypatia_engine-0.2.13.tar.gz/hypatia_engine-0.2.13/hypatia/constants.py
@@ -0,0 +1,38 @@
+# This module is part of Hypatia and is released under the
+# MIT License: http://opensource.org/licenses/MIT
+
+"""Constants for Hypatia Engine
+
+Right now this just contains stuff pertaining to Walkabout, i.e.,
+direction and action specifications. These classes are to be passed as
+parameters.
+
+Examples:
+  >>> from hypatia import animations
+  >>> sprite = animations.Walkabout('debug')
+  >>> sprite.animations[Action.Walk][Direction.Right]
+  <pyganim.PygAnimation object at 0x...>
+
+"""
+
+from enum import Enum
+
+
+class Direction(Enum):
+    """Specific to movement of a sprite/surface.
+
+    """
+
+    Up = 1
+    Down = 2
+    Left = 3
+    Right = 4
+
+
+class Action(Enum):
+    """Specific to movement of a sprite/surface.
+
+    """
+
+    Walk = 1
+    Stand = 2
