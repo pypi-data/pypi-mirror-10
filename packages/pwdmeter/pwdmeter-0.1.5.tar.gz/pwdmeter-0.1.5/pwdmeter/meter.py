e5522d83ab57208359107fa6ce954fe14d04fd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:47 +0000

    Add package pwdmeter/0.1.5/pwdmeter-0.1.5.tar.gz

---
+++ packages/pwdmeter/pwdmeter-0.1.5.tar.gz/pwdmeter-0.1.5/pwdmeter/meter.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Meter(object):
+
+    def __init__(self, factors, threshold=0.75, language='en'):
+        self.factors = factors
+        self.threshold = float(threshold)
+
+    def test(self, value):
+        total_score = 1.0
+        feedbacks = {}
+        for factor in self.factors:
+            score, feedback = factor.test(value)
+            logger.debug("%s: %s, %s" % (factor.category, score, feedback))
+            if feedback is not None:
+                feedbacks[factor.category] = feedback
+            total_score *= score
+        return total_score, None if total_score >= self.threshold else feedbacks
+
+
+def test(value):
+    from pwdmeter import NonASCIIFactor, NonDictionaryFactor, LengthFactor, VarietyFactor, CasemixFactor, CharmixFactor
+    m = Meter([NonDictionaryFactor(), NonASCIIFactor(), LengthFactor(), VarietyFactor(), CasemixFactor(), CharmixFactor()])
+    return m.test(value)
