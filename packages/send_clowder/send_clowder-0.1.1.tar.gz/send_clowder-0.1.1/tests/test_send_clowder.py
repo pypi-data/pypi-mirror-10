ac972b7d51ff9a9f777534ef35155a53d2b7842c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:27 +0000

    Add package send_clowder/0.1.1/send_clowder-0.1.1.tar.gz

---
+++ packages/send_clowder/send_clowder-0.1.1.tar.gz/send_clowder-0.1.1/tests/test_send_clowder.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_send_clowder
+----------------------------------
+
+Tests for `send_clowder` module.
+"""
+
+import unittest
+
+from send_clowder import send_clowder
+
+
+class TestSend_clowder(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
