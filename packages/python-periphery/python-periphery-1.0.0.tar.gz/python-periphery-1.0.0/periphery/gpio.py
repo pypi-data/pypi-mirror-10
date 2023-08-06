33734f8ecccd6265786c11d05135d5c7801cad35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:24 +0000

    Add package python-periphery/1.0.0/python-periphery-1.0.0.tar.gz

---
+++ packages/python-periphery/python-periphery-1.0.0.tar.gz/python-periphery-1.0.0/periphery/gpio.py
@@ -0,0 +1,313 @@
+import os
+import select
+
+class GPIOError(IOError):
+    """Base class for GPIO errors."""
+    pass
+
+class GPIO(object):
+    def __init__(self, pin, direction):
+        """Instantiate a GPIO object and open the sysfs GPIO corresponding to
+        the specified pin, with the specified direction.
+
+        Direction "in" is input; "out" is output, initialized to low; "high" is
+        output, initialized to high; and "low" is output, initialized to low.
+
+        Args:
+            pin (int): Linux pin number.
+            direction (str): pin direction, can be "in", "out", "high", "low".
+
+        Returns:
+            GPIO: GPIO object.
+
+        Raises:
+            GPIOError: if an I/O or OS error occurs.
+            TypeError: if `pin` or `direction`  types are invalid.
+            ValueError: if `direction` value is invalid.
+
+        """
+        self._fd = None
+        self._pin = None
+        self._open(pin, direction)
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, t, value, traceback):
+        self.close()
+
+    def _open(self, pin, direction):
+        if not isinstance(pin, int):
+            raise TypeError("Invalid pin type, should be integer.")
+        if not isinstance(direction, str):
+            raise TypeError("Invalid direction type, should be string.")
+        if direction.lower() not in ["in", "out", "high", "low"]:
+            raise ValueError("Invalid direction, can be: \"in\", \"out\", \"high\", \"low\".")
+
+        gpio_path = "/sys/class/gpio/gpio%d" % pin
+
+        if not os.path.isdir(gpio_path):
+            # Export the pin
+            try:
+                f_export = open("/sys/class/gpio/export", "w")
+                f_export.write("%d\n" % pin)
+                f_export.close()
+            except IOError as e:
+                raise GPIOError(e.errno, "Exporting GPIO: " + e.strerror)
+
+        # Write direction
+        try:
+            direction = direction.lower()
+            f_direction = open("/sys/class/gpio/gpio%d/direction" % pin, "w")
+            f_direction.write(direction + "\n")
+            f_direction.close()
+        except IOError as e:
+            raise GPIOError(e.errno, "Setting GPIO direction: " + e.strerror)
+
+        # Open value
+        try:
+            self._fd = os.open("/sys/class/gpio/gpio%d/value" % pin, os.O_RDWR)
+        except OSError as e:
+            raise GPIOError(e.errno, "Opening GPIO: " + e.strerror)
+
+        self._pin = pin
+
+
+    # Methods
+
+    def read(self):
+        """Read the state of the GPIO.
+
+        Returns:
+            bool: ``True`` for high state, ``False`` for low state.
+
+        Raises:
+            GPIOError: if an I/O or OS error occurs.
+
+        """
+        # Read value
+        try:
+            buf = os.read(self._fd, 2)
+        except OSError as e:
+            raise GPIOError(e.errno, "Reading GPIO: " + e.strerror)
+
+        # Rewind
+        try:
+            os.lseek(self._fd, 0, os.SEEK_SET)
+        except OSError as e:
+            raise GPIOError(e.errno, "Rewinding GPIO: " + e.strerror)
+
+        if buf[0] == b"0"[0]:
+            return False
+        elif buf[0] == b"1"[0]:
+            return True
+
+        raise GPIOError(None, "Unknown GPIO value: \"%s\"" % buf[0])
+
+    def write(self, value):
+        """Set the state of the GPIO to `value`.
+
+        Args:
+            value (bool): ``True`` for high state, ``False`` for low state.
+
+        Raises:
+            GPIOError: if an I/O or OS error occurs.
+            TypeError: if `value` type is not bool.
+
+        """
+        if not isinstance(value, bool):
+            raise TypeError("Invalid value type, should be bool.")
+
+        # Write value
+        try:
+            if value:
+                os.write(self._fd, b"1\n")
+            else:
+                os.write(self._fd, b"0\n")
+        except OSError as e:
+            raise GPIOError(e.errno, "Writing GPIO: " + e.strerror)
+
+        # Rewind
+        try:
+            os.lseek(self._fd, 0, os.SEEK_SET)
+        except OSError as e:
+            raise GPIOError(e.errno, "Rewinding GPIO: " + e.strerror)
+
+    def poll(self, timeout):
+        """Poll a GPIO for the edge event configured with the .edge property.
+
+        `timeout` can be a positive number for a timeout in seconds, 0 for a
+        non-blocking poll, or negative or None for a blocking poll.
+
+        Args:
+            timeout (int, float, None): timeout duration in seconds.
+
+        Returns:
+            bool: ``True`` if an edge event occurred, ``False`` on timeout.
+
+        Raises:
+            GPIOError: if an I/O or OS error occurs.
+            TypeError: if `timeout` type is not None or int.
+
+        """
+        if timeout is not None and not isinstance(timeout, int) and not isinstance(timeout, float):
+            raise TypeError("Invalid timeout type, should be integer, float, or None.")
+
+        # Seek to the end
+        try:
+            os.lseek(self._fd, 0, os.SEEK_END)
+        except OSError as e:
+            raise GPIOError(e.errno, "Seeking to end of GPIO: " + e.strerror)
+
+        # Poll
+        p = select.poll()
+        p.register(self._fd, select.POLLPRI | select.POLLERR)
+        events = p.poll(int(timeout*1000))
+
+        # If GPIO edge interrupt occurred
+        if len(events) > 0:
+            # Rewind
+            try:
+                os.lseek(self._fd, 0, os.SEEK_SET)
+            except OSError as e:
+                raise GPIOError(e.errno, "Rewinding GPIO: " + e.strerror)
+
+            return True
+
+        return False
+
+    def close(self):
+        """Close the sysfs GPIO.
+
+        Raises:
+            GPIOError: if an I/O or OS error occurs.
+
+        """
+        if self._fd is None:
+            return
+
+        try:
+            os.close(self._fd)
+        except OSError as e:
+            raise GPIOError(e.errno, "Closing GPIO: " + e.strerror)
+
+        self._fd = None
+
+    # Immutable properties
+
+    @property
+    def fd(self):
+        """Get the file descriptor for the underlying sysfs GPIO "value" file
+        of the GPIO object.
+
+        :type: int
+        """
+        return self._fd
+
+    @property
+    def pin(self):
+        """Get the sysfs GPIO pin.
+
+        :type: int
+        """
+        return self._pin
+
+    @property
+    def supports_interrupts(self):
+        """Get whether or not this GPIO supports edge interrupts configurable
+        with the .edge property.
+
+        :type: bool
+        """
+        return os.path.isfile("/sys/class/gpio/gpio%d/edge" % self._pin)
+
+    # Mutable properties
+
+    def _get_direction(self):
+        # Read direction
+        try:
+            f_direction = open("/sys/class/gpio/gpio%d/direction" % self._pin, "r")
+            direction = f_direction.read()
+            f_direction.close()
+        except IOError as e:
+            raise GPIOError(e.errno, "Getting GPIO direction: " + e.strerror)
+
+        return direction.strip()
+
+    def _set_direction(self, direction):
+        if not isinstance(direction, str):
+            raise TypeError("Invalid direction type, should be string.")
+        if direction.lower() not in ["in", "out", "high", "low"]:
+            raise ValueError("Invalid direction, can be: \"in\", \"out\", \"high\", \"low\".")
+
+        # Write direction
+        try:
+            direction = direction.lower()
+            f_direction = open("/sys/class/gpio/gpio%d/direction" % self._pin, "w")
+            f_direction.write(direction + "\n")
+            f_direction.close()
+        except IOError as e:
+            raise GPIOError(e.errno, "Setting GPIO direction: " + e.strerror)
+
+    direction = property(_get_direction, _set_direction)
+    """Get or set the GPIO's direction. Can be "in", "out", "high", "low".
+
+    Direction "in" is input; "out" is output, initialized to low; "high" is
+    output, initialized to high; and "low" is output, initialized to low.
+
+    Raises:
+        GPIOError: if an I/O or OS error occurs.
+        TypeError: if `direction` type is not str.
+        ValueError: if `direction` value is invalid.
+
+    :type: str
+    """
+
+    def _get_edge(self):
+        # Read edge
+        try:
+            f_edge = open("/sys/class/gpio/gpio%d/edge" % self._pin, "r")
+            edge = f_edge.read()
+            f_edge.close()
+        except IOError as e:
+            raise GPIOError(e.errno, "Getting GPIO edge: " + e.strerror)
+
+        return edge.strip()
+
+    def _set_edge(self, edge):
+        if not isinstance(edge, str):
+            raise TypeError("Invalid edge type, should be string.")
+        if edge.lower() not in ["none", "rising", "falling", "both"]:
+            raise ValueError("Invalid edge, can be: \"none\", \"rising\", \"falling\", \"both\".")
+
+        # Write edge
+        try:
+            edge = edge.lower()
+            f_edge = open("/sys/class/gpio/gpio%d/edge" % self._pin, "w")
+            f_edge.write(edge + "\n")
+            f_edge.close()
+        except IOError as e:
+            raise GPIOError(e.errno, "Setting GPIO edge: " + e.strerror)
+
+    edge = property(_get_edge, _set_edge)
+    """Get or set the GPIO's interrupt edge. Can be "none", "rising", "falling", "both".
+
+    Raises:
+        GPIOError: if an I/O or OS error occurs.
+        TypeError: if `edge` type is not str.
+        ValueError: if `edge` value is invalid.
+
+    :type: str
+    """
+
+    # String representation
+
+    def __str__(self):
+        if self.supports_interrupts:
+            return "GPIO %d (fd=%d, direction=%s, supports interrupts, edge=%s)" % (self._pin, self._fd, self.direction, self.edge)
+
+        return "GPIO %d (fd=%d, direction=%s, no interrupts)" % (self._pin, self._fd, self.direction)
+
---
+++ packages/python-periphery/python-periphery-1.0.0.tar.gz/python-periphery-1.0.0/periphery/mmio.py
@@ -0,0 +1,318 @@
+import sys
+import os
+import mmap
+import ctypes
+
+# Alias long to int on Python 3
+if sys.version_info[0] >= 3:
+    long = int
+
+class MMIOError(IOError):
+    """Base class for MMIO errors."""
+    pass
+
+class MMIO(object):
+    def __init__(self, physaddr, size):
+        """Instantiate an MMIO object and map the region of physical memory
+        specified by the address base `physaddr` and size `size` in bytes.
+
+        Args:
+            physaddr (int, long): base physical address of memory region.
+            size (int, long): size of memory region.
+
+        Returns:
+            MMIO: MMIO object.
+
+        Raises:
+            MMIOError: if an I/O or OS error occurs.
+            TypeError: if `physaddr` or `size` types are invalid.
+
+        """
+        self.mapping = None
+        self._open(physaddr, size)
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, t, value, traceback):
+        self.close()
+
+    def _open(self, physaddr, size):
+        if not isinstance(physaddr, int) and not isinstance(physaddr, long):
+            raise TypeError("Invalid physaddr type, should be integer.")
+        if not isinstance(size, int) and not isinstance(size, long):
+            raise TypeError("Invalid size type, should be integer.")
+
+        pagesize = os.sysconf(os.sysconf_names['SC_PAGESIZE'])
+
+        self._physaddr = physaddr
+        self._size = size
+        self._aligned_physaddr = physaddr - (physaddr % pagesize)
+        self._aligned_size = size + (physaddr - self._aligned_physaddr)
+
+        try:
+            fd = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
+        except OSError as e:
+            raise MMIOError(e.errno, "Opening /dev/mem: " + e.strerror)
+
+        try:
+            self.mapping = mmap.mmap(fd, self._aligned_size, flags=mmap.MAP_SHARED, prot=(mmap.PROT_READ | mmap.PROT_WRITE), offset=self._aligned_physaddr)
+        except OSError as e:
+            raise MMIOError(e.errno, "Mapping /dev/mem: " + e.strerror)
+
+        try:
+            os.close(fd)
+        except OSError as e:
+            raise MMIOError(e.errno, "Closing /dev/mem: " + e.strerror)
+
+    # Methods
+
+    def _adjust_offset(self, offset):
+        return offset + (self._physaddr - self._aligned_physaddr)
+
+    def _validate_offset(self, offset, length):
+        if (offset+length) > self._aligned_size:
+            raise ValueError("Offset out of bounds.")
+
+    def read32(self, offset):
+        """Read 32-bits from mapped physical memory, starting at the specified
+        `offset`, in bytes, relative to the base physical address the MMIO
+        object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+
+        Returns:
+            int: 32-bit value read.
+
+        Raises:
+            TypeError: if `offset` type is invalid.
+            ValueError: if `offset` is out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, 4)
+        return ctypes.c_uint32.from_buffer(self.mapping, offset).value
+
+    def read16(self, offset):
+        """Read 16-bits from mapped physical memory, starting at the specified
+        `offset`, in bytes, relative to the base physical address the MMIO
+        object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+
+        Returns:
+            int: 16-bit value read.
+
+        Raises:
+            TypeError: if `offset` type is invalid.
+            ValueError: if `offset` is out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, 2)
+        return ctypes.c_uint16.from_buffer(self.mapping, offset).value
+
+    def read8(self, offset):
+        """Read 8-bits from mapped physical memory, starting at the specified
+        `offset`, in bytes, relative to the base physical address the MMIO
+        object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+
+        Returns:
+            int: 8-bit value read.
+
+        Raises:
+            TypeError: if `offset` type is invalid.
+            ValueError: if `offset` is out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, 1)
+        return ctypes.c_uint8.from_buffer(self.mapping, offset).value
+
+    def read(self, offset, length):
+        """Read an array of bytes from mapped physical memory, starting at the
+        specified `offset` in bytes, relative to the base physical address the
+        MMIO object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+            length (int): number of bytes to read
+
+        Returns:
+            bytes: bytes read.
+
+        Raises:
+            TypeError: if `offset` type is invalid.
+            ValueError: if `offset` is out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, length)
+
+        c_byte_array = (ctypes.c_uint8 * length).from_buffer(self.mapping, offset)
+        return bytes(bytearray(c_byte_array))
+
+    def write32(self, offset, value):
+        """Write 32-bits to mapped physical memory, starting at the specified
+        `offset`, in bytes, relative to the base physical address the MMIO
+        object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+            value (int, long): 32-bit value to write
+
+        Raises:
+            TypeError: if `offset` or `value` type are invalid.
+            ValueError: if `offset` or `value` are out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+        if not isinstance(value, int) and not isinstance(value, long):
+            raise TypeError("Invalid value type, should be integer.")
+        if value < 0 or value > 0xffffffff:
+            raise ValueError("Value out of bounds.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, 4)
+        ctypes.c_uint32.from_buffer(self.mapping, offset).value = value
+
+    def write16(self, offset, value):
+        """Write 16-bits to mapped physical memory, starting at the specified
+        `offset`, in bytes, relative to the base physical address the MMIO
+        object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+            value (int, long): 16-bit value to write
+
+        Raises:
+            TypeError: if `offset` or `value` type are invalid.
+            ValueError: if `offset` or `value` are out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+        if not isinstance(value, int) and not isinstance(value, long):
+            raise TypeError("Invalid value type, should be integer.")
+        if value < 0 or value > 0xffff:
+            raise ValueError("Value out of bounds.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, 2)
+        ctypes.c_uint16.from_buffer(self.mapping, offset).value = value
+
+    def write8(self, offset, value):
+        """Write 8-bits to mapped physical memory, starting at the specified
+        `offset`, in bytes, relative to the base physical address the MMIO
+        object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+            value (int, long): 8-bit value to write
+
+        Raises:
+            TypeError: if `offset` or `value` type are invalid.
+            ValueError: if `offset` or `value` are out of bounds.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+        if not isinstance(value, int) and not isinstance(value, long):
+            raise TypeError("Invalid value type, should be integer.")
+        if value < 0 or value > 0xff:
+            raise ValueError("Value out of bounds.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, 1)
+        ctypes.c_uint8.from_buffer(self.mapping, offset).value = value
+
+    def write(self, offset, data):
+        """Write an array of bytes to mapped physical memory, starting at the
+        specified `offset`, in bytes, relative to the base physical address the
+        MMIO object was opened with.
+
+        Args:
+            offset (int, long): offset from base physical address, in bytes.
+            data (bytes, bytearray, list): a byte array or list of 8-bit integers to write
+
+        Raises:
+            TypeError: if `offset` or `data` type are invalid.
+            ValueError: if `offset` is out of bounds, or if data is not valid bytes.
+
+        """
+        if not isinstance(offset, int) and not isinstance(offset, long):
+            raise TypeError("Invalid offset type, should be integer.")
+        if not isinstance(data, bytes) and not isinstance(data, bytearray) and not isinstance(data, list):
+            raise TypeError("Invalid data type, expected bytes, bytearray, or list.")
+
+        offset = self._adjust_offset(offset)
+        self._validate_offset(offset, len(data))
+
+        data = bytearray(data)
+
+        c_byte_array = (ctypes.c_uint8 * len(data)).from_buffer(self.mapping, offset)
+        for i in range(len(data)):
+            c_byte_array[i] = data[i]
+
+    def close(self):
+        """Unmap the MMIO object's mapped physical memory."""
+        if self.mapping is None:
+            return
+
+        self.mapping.close()
+        self.mapping = None
+
+        self._fd = None
+
+    # Immutable properties
+
+    @property
+    def base(self):
+        """Get the base physical address the MMIO object was opened with.
+
+        :type: int
+        """
+        return self._physaddr
+
+    @property
+    def size(self):
+        """Get the mapping size the MMIO object was opened with.
+
+        :type: int
+        """
+        return self._size
+
+    @property
+    def pointer(self):
+        """Get a ctypes void pointer to the memory mapped region.
+
+        :type: ctypes.c_void_p
+        """
+        return ctypes.cast(ctypes.pointer(ctypes.c_uint8.from_buffer(self.mapping, 0)), ctypes.c_void_p)
+
+    # String representation
+
+    def __str__(self):
+        return "MMIO 0x%08x (size=%d)" % (self.base, self.size)
+
