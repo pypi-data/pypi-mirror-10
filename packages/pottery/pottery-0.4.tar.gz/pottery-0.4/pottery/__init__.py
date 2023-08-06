036ab5b5818cfc43d47f3e21e273ec5f9354a809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:20 +0000

    Add package pottery/0.4/pottery-0.4.tar.gz

---
+++ packages/pottery/pottery-0.4.tar.gz/pottery-0.4/pottery/__init__.py
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------------#
+#   __init__.py                                                               #
+#                                                                             #
+#   Copyright (c) 2015, Rajiv Bakulesh Shah.                                  #
+#   All rights reserved.                                                      #
+#-----------------------------------------------------------------------------#
+
+__title__ = 'pottery'
+__version__ = '0.4'
+__author__ = 'Rajiv Bakulesh Shah'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (c) 2015, Rajiv Bakulesh Shah'
+
+from .containers import RedisDict
+from .containers import RedisList
+from .containers import RedisSet
+from .exceptions import KeyExistsError
