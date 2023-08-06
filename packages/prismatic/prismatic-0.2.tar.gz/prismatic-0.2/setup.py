716c4e45781cb574877414b174bbe6645cc5f54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:06 +0000

    Add package prismatic/0.2/prismatic-0.2.tar.gz

---
+++ packages/prismatic/prismatic-0.2.tar.gz/prismatic-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'prismatic',
+  packages = ['prismatic'],
+  version = '0.2',
+  description = 'Serialization between JSON dicts and data objects designed for REST APIs',
+  author = 'Adam Byrtek',
+  author_email = 'adambyrtek@gmail.com',
+  url = 'https://github.com/adambyrtek/prismatic',
+  keywords = ['json', 'orm', 'serialization', 'api', 'rest'],
+  classifiers = [],
+)
