e532b1908b199a20a59f1198bced2f6f0748f210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:57 +0000

    Add package hicplotlib/0.1.1/hicplotlib-0.1.1.tar.gz

---
+++ packages/hicplotlib/hicplotlib-0.1.1.tar.gz/hicplotlib-0.1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='hicplotlib',
+      version='0.1.1',
+      description='Hi-C data plotting and analysis tool',
+      author='Ilya Flyamer',
+      author_email='flyamer@gmail.com',
+      url='https://github.com/Phlya/hicplotlib',
+      packages=['hicplotlib'],
+     )
