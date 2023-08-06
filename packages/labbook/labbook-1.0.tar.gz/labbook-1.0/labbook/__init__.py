194e48626704affc135b08cdd83a558d8c05d16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:59 +0000

    Add package labbook/1.0/labbook-1.0.tar.gz

---
+++ packages/labbook/labbook-1.0.tar.gz/labbook-1.0/labbook/__init__.py
@@ -0,0 +1,16 @@
+"""
+This file defines the public API your package exposes.
+
+The reasoning behind this is it allows you to "import os" in mypackage.py
+and then when users "import mypackage" they won't be bothered by "os"
+appearing in "dir(mypackage)".
+
+If you don't want to explicitly expose everything you can use:
+"from mypackage import *"
+
+"""
+
+VERSION = '1.0'
+
+#from labbook import a_function_of_mine
+#from labbook import MyClass
