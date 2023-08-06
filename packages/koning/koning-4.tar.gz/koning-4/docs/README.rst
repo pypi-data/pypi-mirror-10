8f3f83b15eea800cdb15d01fa90a33dfc9ea0fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:38 +0000

    Add package koning/4/koning-4.tar.gz

---
+++ packages/koning/koning-4.tar.gz/koning-4/docs/README.rst
@@ -0,0 +1,13 @@
+#########
+DE KONING
+#########
+
+Antipsychotica kunnen doden. Zonder verpleging is behandeling moord.
+
+(F)ACT bied niet de noodzakelijke verpleging:
+
+1) verpleging naar gelang de situatie. meer zorg naarmate de situatie erger is werkt niet preventief.
+2) niet 7 x 24 uurs verpleging, in het weekend en avonduren niet aanwezig.
+3) psychiater is niet op de hoogte van de situatie van de patient.
+4) behandelovereenkomsten zijn niet volledig.
+5) zonder bedden geen snelle terugbrenging naar ziekenhuis mogelijk.
---
+++ packages/koning/koning-4.tar.gz/koning-4/koning/version.py
@@ -0,0 +1,32 @@
+# koning/version.py
+#
+#
+
+""" echo version. """
+
+## IMPORTS
+
+
+from wet.kernel import kernel
+
+from koning import __version__
+
+## CMNDS
+
+def version(event): event.reply("KONING #%s - %s" % (__version__, TXT.strip()))
+
+kernel.register("version", version)
+
+TXT = """
+
+Antipsychotica kunnen doden. Zonder verpleging is behandeling moord.
+
+(F)ACT bied niet de noodzakelijke verpleging:
+
+1) verpleging naar gelang de situatie - meer zorg naarmate de situatie erger is, is niet preventief.
+2) niet 7 x 24 uurs verpleging - in het weekend en avonduren niet aanwezig  
+3) psychiater is niet op de hoogte van de situatie van de patient
+4) behandelovereenkomsten zijn niet volledig
+5) zonder bedden geen snelle terugbrenging naar ziekenhuis mogelijk.
+
+"""
