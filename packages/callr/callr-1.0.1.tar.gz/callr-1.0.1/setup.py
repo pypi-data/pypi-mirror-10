4a4c294b939bf56875adf97c2968a65f75c3bf1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:58 +0000

    Add package callr/1.0.1/callr-1.0.1.tar.gz

---
+++ packages/callr/callr-1.0.1.tar.gz/callr-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='callr',
+    version='1.0.1',
+    author='Michael Jacquin',
+    author_email='mj@callr.com',
+    packages=['callr'],
+    scripts=[],
+    url='https://github.com/THECALLR/sdk-python',
+    license='MIT',
+    description='Python SDK for CALLR API',
+    long_description="",
+)
