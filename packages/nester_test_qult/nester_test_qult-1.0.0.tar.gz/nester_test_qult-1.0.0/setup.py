1351f9b93f5e9fb560cdf9cf3ccdf2c15c92c282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:38 +0000

    Add package nester_test_qult/1.0.0/nester_test_qult-1.0.0.tar.gz

---
+++ packages/nester_test_qult/nester_test_qult-1.0.0.tar.gz/nester_test_qult-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester_test_qult',
+    version     = '1.0.0',
+    py_module   = ['nester_test_qult'],
+    author      = 'qult',
+    author_email= 'xuepengsun@outlook.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'A sample printer if nested lists',
+    )
