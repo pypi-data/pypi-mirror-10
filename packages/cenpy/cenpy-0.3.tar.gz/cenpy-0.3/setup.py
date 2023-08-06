af76c33c285af87bf97a05be2513c257ad7f80d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:45 +0000

    Add package cenpy/0.3/cenpy-0.3.tar.gz

---
+++ packages/cenpy/cenpy-0.3.tar.gz/cenpy-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cenpy',
+      version='0.3',
+      description='Explore and download data from Census APIs',
+      url='https://github.com/ljwolf/cenpy',
+      author='Levi John Wolf',
+      author_email='levi.john.wolf@gmail.com',
+      license='3-Clause BSD',
+      packages=['cenpy'],
+      zip_safe=False)
