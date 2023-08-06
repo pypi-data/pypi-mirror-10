d993e16b1dd8edf8f7ac1b833f7858b76732e914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package aio.app/0.1.5/aio.app-0.1.5.tar.gz

---
+++ packages/aio.app/aio.app-0.1.5.tar.gz/aio.app-0.1.5/aio/app/tests/test_cmd_run.py
@@ -0,0 +1,36 @@
+import os
+import io
+from configparser import ConfigParser
+
+import aio.testing
+from aio.testing.contextmanagers import redirect_all
+from aio.app.testing import AioAppTestCase
+from aio.app.runner import runner
+from aio.signals import Signals
+
+test_dir = os.path.dirname(__file__)
+
+
+class RunCommandTestCase(AioAppTestCase):
+
+    @aio.testing.run_until_complete
+    def _test_run_command(self):
+        """
+        with no args, runner reads config, but does not setup app
+        help msg is printed to stdout
+        """
+        from aio import app
+        conf = os.path.join(
+            test_dir, "resources", "test-2.conf")
+
+        with io.StringIO() as out, redirect_all(out):
+            yield from runner(['run'], configfile=conf)
+            stdout = out.getvalue()
+
+        self.assertEqual(stdout, "")
+
+        # config has been loaded
+        self.assertIsInstance(app.config, ConfigParser)
+
+        # signals have been added
+        self.assertIsInstance(app.signals, Signals)
