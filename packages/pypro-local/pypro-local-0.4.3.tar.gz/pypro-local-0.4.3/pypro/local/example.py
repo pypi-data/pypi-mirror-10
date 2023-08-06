c890467bbe344105a789abcfb182e91628cb6b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:58 +0000

    Add package pypro-local/0.4.3/pypro-local-0.4.3.tar.gz

---
+++ packages/pypro-local/pypro-local-0.4.3.tar.gz/pypro-local-0.4.3/pypro/local/example.py
@@ -0,0 +1,27 @@
+__author__ = 'teemu kanstren'
+
+import pypro.local.main
+import pypro.local.config as config
+
+config.MYSQL_HOST = "192.168.2.79"
+config.MYSQL_ENABLED = False
+
+config.SESSION_NAME = "session2"
+config.ES_INDEX = "pypro-local"
+config.ES_NW_ENABLED = False
+config.ES_FILE_ENABLED = True
+config.CSV_ENABLED = True
+config.KAFKA_ENABLED = True
+config.KAFKA_TOPIC = "session2"
+config.KAFKA_SERVER = "192.168.2.153"
+config.PRINT_CONSOLE = True
+
+config.INTERVAL = 1
+
+config.PROCESS_LIST = ["iperf"]
+
+pypro.local.main.run_poller()
+
+
+
+
