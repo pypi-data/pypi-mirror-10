533016edbb6d46f673bf085ced381cf62891af69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:36 +0000

    Add package django-translation-manager/0.2.10/django-translation-manager-0.2.10.tar.gz

---
+++ packages/django-translation-manager/django-translation-manager-0.2.10.tar.gz/django-translation-manager-0.2.10/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+PACKAGE = "translation_manager"
+NAME = "django-translation-manager"
+DESCRIPTION = "Django app for managing translations from admin"
+AUTHOR = "Pavel Císař, Martin Kubát, Mikuláš Mrva, Jakub Ladra, Oldřich Kučina - COEX CZ s.r.o (http://www.coex.cz)"
+AUTHOR_EMAIL = "pavel.cisar@coex.cz"
+URL = "https://github.com/COEXCZ/django-translation-manager"
+VERSION = '0.2.10'
+LICENSE = "CC BY-NC 3.0"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    url=URL,
+    packages=["translation_manager"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Internationalization"
+    ],
+    install_requires=[
+        "polib",
+        "django>=1.4, <1.8"
+    ],
+)
