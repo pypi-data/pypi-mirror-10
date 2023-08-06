880f75241f8bd57d74885465d9626085b7942e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:02 +0000

    Add package scal/0.2.1/Scal-0.2.1.tar.gz

---
+++ packages/scal/Scal-0.2.1.tar.gz/Scal-0.2.1/bin/generate_examples.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Compile examples located in the /example directory (where / is the root of
+# the repository).
+
+ROOTDIR="$(dirname $0)/.."
+EXAMPLEDIR=doc/examples
+cd "$ROOTDIR/$EXAMPLEDIR"
+make -B
+
+  ../../bin/scal $i > $basename.tex
+  xelatex $basename
