35c8d5003f34c927be3f9fe9fd6859aaab11f531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package phjs-manager/0.0.8/phjs-manager-0.0.8.tar.gz

---
+++ packages/phjs-manager/phjs-manager-0.0.8.tar.gz/phjs-manager-0.0.8/app/config_adapter.py
@@ -0,0 +1,36 @@
+import ConfigParser
+import logging
+
+class ConfigAdapter:
+    logging.basicConfig(level=logging.DEBUG)
+    log = logging.getLogger('config_parser')
+    config = ConfigParser.ConfigParser()
+    sections = []
+    data = {}
+    """Adapter for config file"""
+    def __init__(self, path):
+        self.config.read(path)
+        self.sections = self.config.sections()
+        options = self.config.options(self.sections[0])
+        for option in options:
+            try:
+                self.data[option] = self.config.get(self.sections[0], option)
+                if self.data[option] == -1:
+                    self.log.debug("skip: %s" % option)
+            except:
+                print("exception on %s!" % option)
+                self.data[option] = None
+
+
+    def get_socket_path(self):
+        return self.data['socket_path']
+
+
+    def get_container(self):
+        return self.data['container']
+
+    def get_run_command(self):
+        return self.data['run_command']
+
+    def get_start_port(self):
+        return self.data['start_port']
---
+++ packages/phjs-manager/phjs-manager-0.0.8.tar.gz/phjs-manager-0.0.8/app/docker_adapter.py
@@ -0,0 +1,20 @@
+from docker import Client
+from docker.utils import utils
+
+
+class DockerAdapter:
+    """Adapter for docker api"""
+    def __init__(self, base_url):
+        self.dockerClient = Client(base_url=base_url)
+
+    def run_container(self, image_path, run_command, port_bind):
+        self.dockerClient.create_container(
+            image=image_path,
+            command=run_command, detach=True,
+            host_config=utils.create_host_config(
+                port_bindings={port_bind: ('127.0.0.1',)}
+            )
+        )
+
+    def get_nodes(self):
+        return self.dockerClient.containers()
