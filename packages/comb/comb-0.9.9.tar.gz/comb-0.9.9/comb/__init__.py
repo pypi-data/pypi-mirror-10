5c12b6e0816d4c35d126fedeb56199883aa86aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:56 +0000

    Add package comb/0.9.9/comb-0.9.9.tar.gz

---
+++ packages/comb/comb-0.9.9.tar.gz/comb-0.9.9/comb/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+version = "0.9.9"
+version_info = (0, 9, 9, 0)
---
+++ packages/comb/comb-0.9.9.tar.gz/comb-0.9.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+import comb
+
+setup(
+    name='comb',
+    version=comb.version,
+    packages=['comb', 'comb.mq', 'comb.demo'],
+    install_requires=['cliez'],
+    url='http://comb.readthdocs.org',
+    license='http://opensource.org/licenses/MIT',
+    author='Wang WenPei',
+    author_email='wangwenpei@nextoa.com',
+    description='A simple and high-performance framework for create concurrent program',
+    keywords='comb,slot,thread,framework,hook',
+    entry_points={
+        'console_scripts': [
+            'comb = comb.main:main'
+        ]
+    },
+
+
+)
