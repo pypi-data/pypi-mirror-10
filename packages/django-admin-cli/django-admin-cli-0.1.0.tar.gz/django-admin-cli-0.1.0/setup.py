cbb7bd6003839fface28eb9560a17969462def99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:33 +0000

    Add package django-admin-cli/0.1.0/django-admin-cli-0.1.0.tar.gz

---
+++ packages/django-admin-cli/django-admin-cli-0.1.0.tar.gz/django-admin-cli-0.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import admin_cli
+
+setup(
+    name='django-admin-cli',
+    version=admin_cli.__version__,
+    url=admin_cli.__url__,
+    description=admin_cli.__doc__,
+    long_description=open('README.rst').read(),
+    author='ZuluPro (Anthony MONTHE)',
+    author_email='anthony.monthe@gmail.com',
+    license=admin_cli.__license__,
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+    ],
+    packages=find_packages(exclude=['tests.runtests.main']),
+    include_package_data=True,
+    test_suite='tests.runtests.main',
+    install_requires=[],
+)
