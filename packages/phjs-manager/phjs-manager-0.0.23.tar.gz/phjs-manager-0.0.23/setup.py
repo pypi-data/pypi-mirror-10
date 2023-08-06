a646c1fa6c6fbeafa568e9f7970443909f58f6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:33 +0000

    Add package phjs-manager/0.0.23/phjs-manager-0.0.23.tar.gz

---
+++ packages/phjs-manager/phjs-manager-0.0.23.tar.gz/phjs-manager-0.0.23/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='phjs-manager',
+    version='0.0.23',
+    author="michey",
+    author_email='mixeyy@gmail.com',
+    packages=["app"],
+    include_package_data=True,
+    url="http://pypi.python.org/pypi/MyApplication_v010/",
+    description="phjs-manager",
+    install_requires=['docker-py', 'flask'],
+    entry_points={
+    'console_scripts': [
+        'phjs-manager = app:run',
+    ],
+    },
+)
