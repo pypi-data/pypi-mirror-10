f2f4c4a58a093e9d205df9b2f9477af4ed2e8591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:34 +0000

    Add package gale_web/0.2/gale_web-0.2.tar.gz

---
+++ packages/gale_web/gale_web-0.2.tar.gz/gale_web-0.2/gale/wsgi/stream.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python
+#coding:utf-8
+# Author        : tuxpy
+# Email         : q8886888@qq.com.com
+# Last modified : 2015-06-12 12:29:59
+# Filename      : stream.py
+# Description   : 
+from wsgiref import simple_server
+from functools import partial
+
+def wsgi_callback(cls, env, start_response):
+    from .web import WSGIApplication
+    wsgi_app = WSGIApplication()
+    return wsgi_app.__call__(env, start_response, cls)
+
+class StreamServer(object):
+    """为了兼容py3，py3的server用得是wsgi server"""
+    def __init__(self, listen_add, callback, max_client = 1000, reuse_add = True, timeout = 15):
+        host, port = listen_add
+        _server = simple_server.make_server(host, port, partial(wsgi_callback, callback))
+        _server.timeout = timeout
+        _server.request_queue_size = max_client
+        _server.allow_reuse_address = reuse_add
+
+        self._server = _server
+
+    def serve_forever(self):
+        self._server.serve_forever()
+
