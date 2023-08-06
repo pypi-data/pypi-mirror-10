4d570d67df3ed942545b90237eaf655b4f8a4387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package sprockets.handlers.status/0.1.2/sprockets.handlers.status-0.1.2.tar.gz

---
+++ packages/sprockets.handlers.status/sprockets.handlers.status-0.1.2.tar.gz/sprockets.handlers.status-0.1.2/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+setuptools.setup(
+    name='sprockets.handlers.status',
+    version='0.1.2',
+    description='A small handler for reporting application status',
+    long_description=open('test-requirements.txt', 'r').read(),
+    url='https://github.com/sprockets/sprockets.handlers.status',
+    author='AWeber Communications',
+    author_email='api@aweber.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=['sprockets',
+              'sprockets.handlers',
+              'sprockets.handlers.status'],
+    package_data={'': ['LICENSE', 'README.md']},
+    include_package_data=True,
+    install_requires=['tornado'],
+    namespace_packages=['sprockets',
+                        'sprockets.handlers'],
+    zip_safe=False)
