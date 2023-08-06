e50c69750af28d1bdcb682aa1f647da8b1099918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:31 +0000

    Add package gslrandom/0.1/gslrandom-0.1.tar.gz

---
+++ packages/gslrandom/gslrandom-0.1.tar.gz/gslrandom-0.1/gslrandom/cgslrandom.cpp
@@ -0,0 +1,41 @@
+#include "cgslrandom.h"
+
+
+BasicRNG::BasicRNG()
+{
+    r = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set (r, time(NULL));
+}
+
+BasicRNG::BasicRNG(unsigned long seed)
+{
+    r = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set (r, seed);
+}
+
+void BasicRNG::multinomial(int K,
+                           unsigned int N,
+                           const double p[],
+                           unsigned int n[])
+{
+    gsl_ran_multinomial(r, K, N, p, n);
+}
+
+void sample_multinomial(BasicRNG* brng,
+                        int K,
+                        unsigned int N,
+                        const double p[],
+                        unsigned int n[])
+{
+    brng->multinomial(K, N, p, n);
+}
+
+void dumb_sample_multinomial(int K,
+                             unsigned int N,
+                             const double p[],
+                             unsigned int n[])
+{
+    BasicRNG* brng = new BasicRNG();
+    brng->multinomial(K, N, p, n);
+    delete brng;
+}
