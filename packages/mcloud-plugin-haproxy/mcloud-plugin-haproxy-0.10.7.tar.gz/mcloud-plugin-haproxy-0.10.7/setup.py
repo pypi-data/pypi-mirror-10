12f5f78507a2db45a08a641599fc84e823e6e76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:54 +0000

    Add package mcloud-plugin-haproxy/0.10.7/mcloud-plugin-haproxy-0.10.7.tar.gz

---
+++ packages/mcloud-plugin-haproxy/mcloud-plugin-haproxy-0.10.7.tar.gz/mcloud-plugin-haproxy-0.10.7/setup.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+
+import os
+import sys
+import imp
+
+metadata = imp.load_source(
+    'metadata', os.path.join('../../mcloud', 'metadata.py'))
+
+from setuptools import setup, find_packages
+
+# See here for more options:
+# <http://pythonhosted.org/setuptools/setuptools.html>
+setup(
+    name='mcloud-plugin-haproxy',
+    version=metadata.version,
+    author='Alex Rudakov',
+    author_email='ribozz@gmail.com',
+    maintainer='Alex Rudakov',
+    maintainer_email='ribozz@gmail.com',
+    url='mcloud.io',
+    description='Haproxy integration for mcloud',
+    long_description=open('README.rst').read(),
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Software Distribution',
+    ],
+    py_modules=['mcloud_haproxy'],
+    install_requires=[
+        'mcloud==%s' % metadata.version,
+        'PyYAML'
+    ],
+
+    entry_points={
+        'mcloud_plugins': [
+            'haproxy = mcloud_haproxy:HaproxyPlugin'
+        ]
+    }
+)
