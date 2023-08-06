7d5ba9897565c3ff613a88755d4483e0de520dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:31 +0000

    Add package plone.jsonapi.core/0.5/plone.jsonapi.core-0.5.tar.gz

---
+++ packages/plone.jsonapi.core/plone.jsonapi.core-0.5.tar.gz/plone.jsonapi.core-0.5/src/plone/jsonapi/core/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# convenience imports
+import version
+from browser import router
+from browser import decorators
---
+++ packages/plone.jsonapi.core/plone.jsonapi.core-0.5.tar.gz/plone.jsonapi.core-0.5/src/plone/jsonapi/core/browser/decorators.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import time
+import traceback
+import simplejson as json
+from helpers import error
+
+__author__ = 'Ramon Bartl <ramon.bartl@googlemail.com>'
+__docformat__ = 'plaintext'
+
+
+def handle_errors(f):
+    """ simple JSON error handler
+    """
+
+    def decorator(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        # XXX we should create a custom Exception class
+        except Exception, e:
+            return error(str(e), error=str(traceback.format_exc()))
+    return decorator
+
+
+def runtime(func):
+    """ simple runtime measurement of the called function
+    """
+
+    def decorator(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        result.update(dict(_runtime=end-start))
+        return result
+
+    return decorator
+
+
+def returns_json(func):
+    """ returns json output
+    """
+
+    def decorator(*args, **kwargs):
+        instance = args[0]
+        request = getattr(instance, 'request', None)
+        request.response.setHeader("Content-Type", "application/json")
+        result = func(*args, **kwargs)
+        return json.dumps(result)
+
+    return decorator
+
+
+def supports_jsonp(func):
+    """ suports jsonp output
+    """
+
+    def decorator(*args, **kwargs):
+        instance = args[0]
+        request = getattr(instance, 'request', None)
+
+        c = request.form.get("c", None)
+        if c is not None:
+            return "%s(%s);" % (str(c), func(*args, **kwargs))
+        return func(*args, **kwargs)
+
+    return decorator
