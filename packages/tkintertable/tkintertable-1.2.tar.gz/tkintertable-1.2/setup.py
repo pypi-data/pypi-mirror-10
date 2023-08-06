a6620eae775388a0683891151f2077868c31adad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:12 +0000

    Add package tkintertable/1.2/tkintertable-1.2.tar.gz

---
+++ packages/tkintertable/tkintertable-1.2.tar.gz/tkintertable-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import sys,os
+home=os.path.expanduser('~')
+
+setup(
+    name = 'tkintertable',
+    version = '1.2',
+    description = 'Extendable table class for Tkinter',
+    url='https://github.com/dmnfarrell/tkintertable',
+    license='GPL v3',
+    author = 'Damien Farrell',
+    author_email = 'farrell.damien[at]gmail.com',
+    packages = ['tkintertable'],
+    install_requires=['Pmw'],
+    dependency_links = [
+          "http://download.sourceforge.net/pmw/Pmw.1.3.tar.gz"],
+    entry_points = { 'gui_scripts': [
+                     'tablesapp = tkintertable.TablesApp:main']},
+    classifiers = ["Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development :: User Interfaces",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research"],
+    keywords = ['tkinter', 'spreadsheet', 'table'],
+)
