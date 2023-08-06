74cce3296a3ef184a45f769c97688aa226dc2590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:52 +0000

    Add package okonomiyaki/0.8.0/okonomiyaki-0.8.0.tar.gz

---
+++ packages/okonomiyaki/okonomiyaki-0.8.0.tar.gz/okonomiyaki-0.8.0/okonomiyaki/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+#  Copyright (c) 2013 by Enthought, Inc.
+#  All rights reserved.
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
