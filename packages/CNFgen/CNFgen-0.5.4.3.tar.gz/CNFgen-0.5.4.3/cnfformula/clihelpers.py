50071c7321edf3af854843a3226f0d5e74e025f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:57 +0000

    Add package CNFgen/0.5.4.3/CNFgen-0.5.4.3.tar.gz

---
+++ packages/CNFgen/CNFgen-0.5.4.3.tar.gz/CNFgen-0.5.4.3/cnfformula/clihelpers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from __future__ import print_function
+
+
+__docstring__ =\
+"""Helpers functions, to build command line utilities
+
+Copyright (C) 2015  Massimo Lauria <lauria@kth.se>
+https://github.com/MassimoLauria/cnfgen.git
+
+"""
+
