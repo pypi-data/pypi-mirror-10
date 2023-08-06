3b39b2718f157cb333767a3d61adfb3c88022403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:12 +0000

    Add package mpa/0.1.1b5/mpa-0.1.1b5.tar.gz

---
+++ packages/mpa/mpa-0.1.1b5.tar.gz/mpa-0.1.1b5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='mpa',
+	version = "0.1.1b5",
+	description="Matplotlib Plotting Assistant",
+    long_description=long_description,
+	url="https://github.com/yuhangwang/MPA",
+	author="Yuhang Wang",
+	license="BSD 3-Clause License",
+	packages = find_packages(),
+	package_data = {
+		"mpa" : ["README.md"]
+	},
+
+    )
