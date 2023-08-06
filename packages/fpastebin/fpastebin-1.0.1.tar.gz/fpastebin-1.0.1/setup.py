cc72452030730665f98b2f82a22448a697222e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:22 +0000

    Add package fpastebin/1.0.1/fpastebin-1.0.1.tar.gz

---
+++ packages/fpastebin/fpastebin-1.0.1.tar.gz/fpastebin-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name = 'fpastebin',
+    version = '1.0.1',
+    description='A tool to upload text to http://paste.fedoraproject.org/',
+    url = 'http://github.com/surajssd/fpastebin',
+    author = 'Suraj Deshmukh',
+    author_email = 'surajssd009005@gmail.com',
+
+    license = 'MIT',
+
+    packages = ['fpastebin'],
+    install_requires = ['requests'],
+    scripts=['fpastebin/fpastebin.py']
+)
