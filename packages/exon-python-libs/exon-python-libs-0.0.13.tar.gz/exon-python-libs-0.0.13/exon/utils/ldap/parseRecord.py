e42048d1397bd821339beeebd5150407efe6d80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package exon-python-libs/0.0.13/exon-python-libs-0.0.13.tar.gz

---
+++ packages/exon-python-libs/exon-python-libs-0.0.13.tar.gz/exon-python-libs-0.0.13/exon/utils/ldap/parseRecord.py
@@ -0,0 +1,44 @@
+__author__ = 'Stephan Conrad <stephan@conrad.pics>'
+
+def parseRecord(lines, accountType = "805306368"):
+    if type(lines) == list:
+        parse = False
+        record = 0
+        recs = []
+        tmp = {}
+        for line in lines:
+            line = line.strip()
+            if line.startswith("# record"):
+                num = line.replace('# record', '').strip()
+                parse = True
+            if line == '':
+                parse = False
+            if parse:
+                a = line.split(":")
+                if len(a) >= 2:
+                    key = a[0]
+                    value = ("".join(a[1:])).strip()
+                    if key in tmp:
+                        if type(tmp[key]) == list:
+                            tmp[key].append(value)
+                        else:
+                            tmp[key] = [tmp[key]]
+                    else:
+                        tmp[key] = value
+            else:
+                if tmp != {}:
+                    if 'sAMAccountType' in tmp:
+                        if tmp['sAMAccountType'] == accountType:
+                            user=False
+                            if 'isCriticalSystemObject' in tmp:
+                                user = tmp['isCriticalSystemObject'].lower() in 'false'
+                            else:
+                                user = True
+                            if user:
+                                tmp["systemUser"] = False
+                            else:
+                                tmp["systemUser"] = True
+                            recs.append(tmp)
+                tmp = {}
+        return recs
+
