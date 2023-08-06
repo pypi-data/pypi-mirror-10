1cae1ebddaba74f4a7c8b41b35efddbfd3f3d1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:54 +0000

    Add package bika.health/3.1.7/bika.health-3.1.7.tar.gz

---
+++ packages/bika.health/bika.health-3.1.7.tar.gz/bika.health-3.1.7/bika/health/interfaces.py
@@ -0,0 +1,72 @@
+from zope.interface import Interface
+
+class IPatient(Interface):
+    """Patient"""
+
+class IPatients(Interface):
+    """Patient folder"""
+
+class IDoctor(Interface):
+    """Doctor"""
+
+class IDoctors(Interface):
+    """Doctor folder"""
+
+class IDrugs(Interface):
+    ""
+
+class IDrugProhibitions(Interface):
+    ""
+
+class IImmunizations(Interface):
+    ""
+
+class ISymptoms(Interface):
+    ""
+
+class IDiseases(Interface):
+    ""
+
+class IAetiologicAgents(Interface):
+    ""
+
+class ITreatments(Interface):
+    ""
+
+class IVaccinationCenter(Interface):
+    ""
+
+class IVaccinationCenters(Interface):
+    ""
+
+class ICaseStatuses(Interface):
+    ""
+
+class ICaseOutcomes(Interface):
+    ""
+
+class ICaseSyndromicClassifications(Interface):
+    ""
+
+class IEpidemiologicalYears(Interface):
+    ""
+
+class IIdentifierTypes(Interface):
+    ""
+
+class IInsuranceCompany(Interface):
+    ""
+
+class IInsuranceCompanies(Interface):
+    ""
+
+class IEthnicity(Interface):
+    """
+    Ethnicity content type marker
+    """
+
+
+class IEthnicities(Interface):
+    """
+    Ethnicities content folder marker
+    """
