ac248f494bd0db39bb2356ec8d362ea2aef96766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:55 +0000

    Add package jsh/0.3.2/jsh-0.3.2.tar.gz

---
+++ packages/jsh/jsh-0.3.2.tar.gz/jsh-0.3.2/setup.py
@@ -0,0 +1,25 @@
+"""Setup for JSH"""
+
+from setuptools import setup
+from platform import system
+from jsh.version import __VERSION__
+
+setup(
+    name='jsh',
+    version=__VERSION__,
+    description='Junos-like shell library for Python',
+    author='InfDev',
+    author_email='infdev@ocado.com',
+    packages=['jsh'],
+    install_requires=['readline'] if system() == 'Darwin' else [],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
