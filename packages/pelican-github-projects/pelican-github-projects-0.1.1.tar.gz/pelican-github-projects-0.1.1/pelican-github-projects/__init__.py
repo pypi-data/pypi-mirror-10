8a1a966aa1ad0dd2b8f4777c423bc4ef7c5334cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:54 +0000

    Add package pelican-github-projects/0.1.1/pelican-github-projects-0.1.1.tar.gz

---
+++ packages/pelican-github-projects/pelican-github-projects-0.1.1.tar.gz/pelican-github-projects-0.1.1/pelican-github-projects/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'pelican-github-projects'
+__version__ = '0.1.1'
+__author__ = 'Kyah'
+__credits__ = ["Kyah", ]
+__maintainer__ = "Kyah"
+__email__ = "contact@octetsetquartdepouces.net"
+__status__ = "Stable"
+__license__ = 'GPLv2'
+__copyright__ = 'Copyright 2015'
+
+from .github import *
---
+++ packages/pelican-github-projects/pelican-github-projects-0.1.1.tar.gz/pelican-github-projects-0.1.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+version = __import__('pelican-github-projects').__version__
+download_url = 'https://github.com/octetsetquartdepouces/pelican-github-projects/archive/{}.zip'.format(version)
+
+setup(name='pelican-github-projects',
+      version=version,
+      url='https://github.com/octetsetquartdepouces/pelican-github-projects',
+      download_url=download_url,
+      author="Kyah",
+      author_email="contact@octetsetquartdepouces.net",
+      maintainer="Kyah",
+      maintainer_email="contact@octetsetquartdepouces.net",
+      description="Embed a list of your public GitHub projects in your pages",
+      long_description=open("README.rst").read(),
+      license='GPLv2',
+      platforms=['linux'],
+      packages=find_packages(exclude=["*.tests"]),
+      install_requires=['pelican'],
+      package_data={'': ['LICENSE', ]},
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Text Processing',
+      ],
+      zip_safe=True,
+      )
