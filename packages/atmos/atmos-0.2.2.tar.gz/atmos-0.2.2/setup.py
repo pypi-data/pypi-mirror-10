19f647f06564271f87133025c34a2465825cfc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:45 +0000

    Add package atmos/0.2.2/atmos-0.2.2.tar.gz

---
+++ packages/atmos/atmos-0.2.2.tar.gz/atmos-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='atmos',
+    packages=['atmos'],
+    version='0.2.2',
+    description='Atmospheric sciences utility library',
+    author='Jeremy McGibbon',
+    author_email='mcgibbon@uw.edu',
+    url='https://github.com/mcgibbon/atmos',
+    keywords=['atmos', 'atmospheric', 'equations', 'geoscience', 'science'],
+    classifiers=[],
+)
