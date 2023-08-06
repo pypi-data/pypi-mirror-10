b75c0c97d3b74ea618f8e25464f4b82b9b1c93ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:45 +0000

    Add package domestic-reader/0.4.3.8/domestic-reader-0.4.3.8.tar.gz

---
+++ packages/domestic-reader/domestic-reader-0.4.3.8.tar.gz/domestic-reader-0.4.3.8/domestic/core/feedtools.py
@@ -0,0 +1,14 @@
+import feedparser
+
+def isFeed(link):
+    feed = feedparser.parse(link)
+    if feed.bozo:
+        if type(feed.bozo_exception) == feedparser.CharacterEncodingOverride:
+            return True
+        else:
+            return False
+    else: return True
+
+def feedInfo(link):
+    rss = feedparser.parse(link)
+    return {"sitelink" : rss.feed.link, "feedlink" : rss.href, "title" : rss.feed.title, "description" : rss.feed.get("subtitle","")}
---
+++ packages/domestic-reader/domestic-reader-0.4.3.8.tar.gz/domestic-reader-0.4.3.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "domestic-reader",
+    packages = find_packages(),
+    package_data = {"domestic" : ["languages/*", "images/icons/*" ,"images/*.png", "media/*"]},
+    scripts = ["domestic-reader"],
+    version = "0.4.3.8",
+    license = "GPL v3",
+    description = "Free RSS/Atom Reader.",
+    author = "Metehan Özbek",
+    author_email = "metehan@metehan.us",
+    url = "https://github.com/mthnzbk/domestic",
+    download_url = "https://pypi.python.org/pypi/domestic-reader",
+    keywords = ["PyQt5","feedparser", "sqlite", "bs4", "reader", "rss reader", "BeautifulSoup"],
+    classifiers = [
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+
+)
