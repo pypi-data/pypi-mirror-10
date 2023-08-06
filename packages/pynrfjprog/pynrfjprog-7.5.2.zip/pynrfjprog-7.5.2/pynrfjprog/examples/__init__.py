54bb575334f2cf7a457220de2d6e6f92be88a8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:25 +0000

    Add package pynrfjprog/7.5.2/pynrfjprog-7.5.2.zip

---
+++ packages/pynrfjprog/pynrfjprog-7.5.2.zip/pynrfjprog-7.5.2/pynrfjprog/examples/__init__.py
@@ -0,0 +1,8 @@
+""" Examples for pynrfjprog. 
+    Each individual example can be used by importing examples as:
+    from pynrfjprog import examples;  then calling:
+    examples.python_help.run() """
+
+import program_hex
+import memory_read_write
+import python_help
