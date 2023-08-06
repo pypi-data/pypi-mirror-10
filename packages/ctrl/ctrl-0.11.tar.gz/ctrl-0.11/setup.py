7870309e85c0ad3bdcef96167a2a30bf9a79ee3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:49 +0000

    Add package ctrl/0.11/ctrl-0.11.tar.gz

---
+++ packages/ctrl/ctrl-0.11.tar.gz/ctrl-0.11/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ctrl',
+  packages = ['ctrl'],
+  version = '0.11',
+  description = 'Code, demos and utilities for learning to program with the Control Everything series.',
+  author = 'Chris Stones',
+  author_email = 'sirhcsenots@gmail.com',
+  url = 'https://github.com/sirhcsenots/ctrl',
+  download_url = 'https://github.com/sirhcsenots/ctrl/tarball/0.1',
+  keywords = ['ctrl', 'control', 'everything'],
+  classifiers = [],
+)
