540b1a76c5f613abe01c2be6f511881af69d4fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package soloprocess/0.0.3/soloprocess-0.0.3.tar.gz

---
+++ packages/soloprocess/soloprocess-0.0.3.tar.gz/soloprocess-0.0.3/setup.py
@@ -0,0 +1,10 @@
+# vim: set expandtab ts=4 sw=4 filetype=python fileencoding=utf8:
+
+from setuptools import find_packages, setup
+
+setup(
+
+    name="soloprocess",
+    version="0.0.3",
+    py_modules=["soloprocess"],
+)
