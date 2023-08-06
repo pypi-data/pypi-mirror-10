4018cc2634803b54d2cff2c19f59368f6a2c2d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:26 +0000

    Add package pypro-local/0.4.0/pypro-local-0.4.0.tar.gz

---
+++ packages/pypro-local/pypro-local-0.4.0.tar.gz/pypro-local-0.4.0/pypro/local/example.py
@@ -0,0 +1,20 @@
+__author__ = 'teemu kanstren'
+
+import pypro.local.main
+import pypro.local.config as config
+
+config.CSV_ENABLED = False
+config.MYSQL_HOST = "192.168.2.79"
+config.MYSQL_ENABLED = False
+
+config.ES_NW_ENABLED = False
+config.ES_FILE_ENABLED = True
+config.INTERVAL = 1
+
+config.PROCESS_LIST = [1132]
+
+pypro.local.main.run_poller()
+
+
+
+
