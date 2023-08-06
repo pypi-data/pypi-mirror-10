f61f978110b689099f24ca034aa294e0f9a65efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:45 +0000

    Add package planarradpy/0.0.1-2/planarradpy-0.0.1-2.tar.gz

---
+++ packages/planarradpy/planarradpy-0.0.1-2.tar.gz/planarradpy-0.0.1-2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='planarradpy',
+    version='0.0.1-2',
+    packages=['planarradpy.gui', 'planarradpy.log', 'planarradpy.docs', 'planarradpy.tests', 'planarradpy.inputs',
+              'planarradpy.inputs.iop_files', 'planarradpy.inputs.sky_files', 'planarradpy.inputs.phase_files',
+              'planarradpy.inputs.bottom_files', 'planarradpy.inputs.surface_files', 'planarradpy.outputs',
+              'planarradpy.scripts', 'planarradpy.libplanarradpy'],
+    url='https://marrabld.github.io/planarradpy/',
+    license='GPL',
+    scripts=['planarradpy/planarrad.run'],
+    include_package_data=True,
+    author='Dan Marrable',
+    author_email='marrabld+planarradpy@gmail.com',
+    description='Tool for batch running PlanarRad across multiple CPUs', requires=['PyQt4'],
+)
