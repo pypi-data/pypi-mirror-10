341992b974bd50a4d07ccee46975319e1665e64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:07 +0000

    Add package comiccrawler/2015.6.11/comiccrawler-2015.6.11-py3-none-any.whl

---
+++ packages/comiccrawler/comiccrawler-2015.6.11-py3-none-any.whl/comiccrawler/config.py
@@ -0,0 +1,66 @@
+#!python3
+
+"""
+This module provide a global setting object, which will save on exit.
+
+Usage:
+  # Basic
+  import config
+  config.load("some-file")
+  config.setting["key"] = "value"
+  config.save()
+  
+  # Use other section
+  sec1 = config.section("label1")
+  sec1["key"] = "value"
+  
+  # Set default value
+  default = {
+    "key1": "value1",
+	"key2": "value2"
+  }
+  sec2 = config.section("label2", default)
+  print(sec2["key1"])
+  # -> "value1"
+"""
+
+import configparser, atexit
+
+from .io import prepare_file
+
+def section(name, default = None):
+	if name not in config:
+		config[name] = {}
+
+	if default:
+		for key in default:
+			if key not in config[name]:
+				config[name][key] = default[key]
+				
+	return config[name]
+	
+def get(key, default = None):
+	if key not in setting and default is not None:
+		setting[key] = default
+	return setting[key]
+	
+def load(new_path=None):
+	global path
+	if new_path:
+		path = new_path
+	path = prepare_file(path)
+	config.read(path, "utf-8-sig")
+	
+def save(new_path=None):
+	global path
+	if new_path:
+		path = new_path
+	path = prepare_file(path)
+	with open(path, "w", encoding="utf-8") as file:
+		config.write(file)
+
+path = "~/comiccrawler/setting.ini"
+config = configparser.ConfigParser(interpolation=None)
+load()
+setting = section("DEFAULT")
+atexit.register(save)
