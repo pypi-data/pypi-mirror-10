ca9fca8b74a51c5506927f14cbac69a3cfdec931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:32 +0000

    Add package windrose/1.5/windrose-1.5-py2.py3-none-any.whl

---
+++ packages/windrose/windrose-1.5-py2.py3-none-any.whl/windrose/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from .windrose import WindAxesFactory, WindroseAxes, WindAxes, \
+    wrcontour, wrcontourf, wrbox, wrbar, wrpdf, wrscatter, \
+    clean, clean_df, plot_windrose, \
+    FIGSIZE_DEFAULT, DPI_DEFAULT, D_KIND_PLOT
