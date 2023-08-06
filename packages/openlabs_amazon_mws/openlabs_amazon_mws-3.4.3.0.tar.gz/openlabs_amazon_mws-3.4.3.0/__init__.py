54d5a04feee123327553744965a2964d6576ea5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package openlabs_amazon_mws/3.4.3.0/openlabs_amazon_mws-3.4.3.0.tar.gz

---
+++ packages/openlabs_amazon_mws/openlabs_amazon_mws-3.4.3.0.tar.gz/openlabs_amazon_mws-3.4.3.0/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Initialize module
+
+    :copyright: (c) 2013-2015 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from trytond.pool import Pool
+from channel import (
+    SaleChannel, CheckAmazonServiceStatus, CheckAmazonServiceStatusView,
+    CheckAmazonSettingsView, CheckAmazonSettings
+)
+from product import (
+    Product, ExportAmazonCatalogStart, ExportAmazonCatalog,
+    ExportAmazonCatalogDone, ExportAmazonPricingStart, ExportAmazonPricing,
+    ExportAmazonPricingDone, ExportAmazonInventoryStart,
+    ExportAmazonInventory, ExportAmazonInventoryDone, ProductCode, Template
+)
+from sale import Sale
+from party import Party, Address
+from country import Subdivision
+
+
+def register():
+    """
+    Register classes with pool
+    """
+    Pool.register(
+        SaleChannel,
+        Product,
+        ProductCode,
+        Template,
+        ExportAmazonCatalogStart,
+        ExportAmazonCatalogDone,
+        ExportAmazonPricingStart,
+        ExportAmazonPricingDone,
+        ExportAmazonInventoryStart,
+        ExportAmazonInventoryDone,
+        CheckAmazonServiceStatusView,
+        CheckAmazonSettingsView,
+        Sale,
+        Party,
+        Address,
+        Subdivision,
+        module='amazon_mws', type_='model'
+    )
+    Pool.register(
+        CheckAmazonServiceStatus,
+        CheckAmazonSettings,
+        ExportAmazonCatalog,
+        ExportAmazonPricing,
+        ExportAmazonInventory,
+        module='amazon_mws', type_='wizard'
+    )
