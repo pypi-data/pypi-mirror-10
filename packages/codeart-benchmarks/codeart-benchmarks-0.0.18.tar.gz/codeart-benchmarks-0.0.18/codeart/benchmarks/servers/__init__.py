15ba20f3b22768bd213cd3ddef35a7046ed0c353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:29 +0000

    Add package codeart-benchmarks/0.0.18/codeart-benchmarks-0.0.18.tar.gz

---
+++ packages/codeart-benchmarks/codeart-benchmarks-0.0.18.tar.gz/codeart-benchmarks-0.0.18/codeart/benchmarks/servers/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+
+def disable_logs():
+    logging.disable(logging.CRITICAL)
+    logger = logging.getLogger()
+    logger.setLevel(logging.CRITICAL)
+    logger.disabled = True
+    logger.propagate = False
+
+
+SERVER = os.getenv('SERVER', None)
+PORT = int(os.environ.get('PORT', 8000))
