a5eeb403cb54f1ac44af5481919b347b7e091fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:30 +0000

    Add package pysentosa/0.0.9/pysentosa-0.0.9.tar.gz

---
+++ packages/pysentosa/pysentosa-0.0.9.tar.gz/pysentosa-0.0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pysentosa",
+    version="0.0.9",
+    packages = find_packages(),
+    description = "Sentosa - An automatic algorithmic trading system",
+    long_description="Python API for sentosa trading system",
+    author = "Wu Fuheng",
+    author_email = "wufuheng@gmail.com",
+
+    license = "GPL",
+    keywords = ("sentosa", "python"),
+    platforms = "Independant",
+    url = "http://www.quant365.com",
+)
