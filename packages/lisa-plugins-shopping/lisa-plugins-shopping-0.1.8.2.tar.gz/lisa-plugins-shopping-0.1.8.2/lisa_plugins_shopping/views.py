4076c4745041674751b2683d7d671da0ea636373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:21 +0000

    Add package lisa-plugins-shopping/0.1.8.2/lisa-plugins-shopping-0.1.8.2.tar.gz

---
+++ packages/lisa-plugins-shopping/lisa-plugins-shopping-0.1.8.2.tar.gz/lisa-plugins-shopping-0.1.8.2/lisa_plugins_shopping/views.py
@@ -0,0 +1,11 @@
+from models import ShoppingList
+from rest_framework import viewsets
+from serializers import ShoppingListSerializer
+
+
+class ShoppingListViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to add/edit/delete shopping lists.
+    """
+    queryset = ShoppingList.objects.all()
+    serializer_class = ShoppingListSerializer
