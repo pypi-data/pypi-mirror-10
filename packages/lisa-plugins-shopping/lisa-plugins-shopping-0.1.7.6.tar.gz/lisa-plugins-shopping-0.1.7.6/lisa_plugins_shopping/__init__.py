24c14a26f18ecf6f6d42f8b9f32ca4c4fc17d862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:14 +0000

    Add package lisa-plugins-shopping/0.1.7.6/lisa-plugins-shopping-0.1.7.6.tar.gz

---
+++ packages/lisa-plugins-shopping/lisa-plugins-shopping-0.1.7.6.tar.gz/lisa-plugins-shopping-0.1.7.6/lisa_plugins_shopping/__init__.py
@@ -0,0 +1,25 @@
+from views import ShoppingListViewSet
+#from lisa_api.lisa.routers import DynamicRouter
+
+
+class ShoppingPlugin():
+    def __init__(self):
+        pass
+        #DynamicRouter().register_endpoint(r'shopping', ShoppingListViewSet)
+
+
+__title__ = 'Lisa Plugins Shopping'
+__version__ = '0.1.7.6'
+__author__ = 'Julien Syx'
+__license__ = 'Apache'
+__copyright__ = 'Copyright 2015 Julien Syx'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
+
+# Default datetime input and output formats
+ISO_8601 = 'iso-8601'
+
