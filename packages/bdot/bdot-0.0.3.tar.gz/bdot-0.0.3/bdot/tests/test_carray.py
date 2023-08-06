ee202319a5f015b35de23f01ae902589ae0f671b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:29 +0000

    Add package bdot/0.0.3/bdot-0.0.3.tar.gz

---
+++ packages/bdot/bdot-0.0.3.tar.gz/bdot-0.0.3/bdot/tests/test_carray.py
@@ -0,0 +1,59 @@
+import nose
+
+import bdot
+import bcolz
+import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+def test_dot_int64():
+
+
+	matrix = np.random.random_integers(0, 12000, size=(10000, 100))
+	bcarray = bdot.carray(matrix, chunklen=2**13, cparams=bcolz.cparams(clevel=2))
+
+	v = bcarray[0]
+
+	result = bcarray.dot(v)
+	expected = matrix.dot(v)
+
+	assert_array_equal(expected, result)
+
+def test_dot_int32():
+
+
+	matrix = np.random.random_integers(0, 12000, size=(10000, 100)).astype('int32')
+	bcarray = bdot.carray(matrix, chunklen=2**13, cparams=bcolz.cparams(clevel=2))
+
+	v = bcarray[0]
+
+	result = bcarray.dot(v)
+	expected = matrix.dot(v)
+
+	assert_array_equal(expected, result)
+
+def test_dot_float64():
+
+
+	matrix = np.random.random_sample(size=(10000, 100))
+	bcarray = bdot.carray(matrix, chunklen=2**13, cparams=bcolz.cparams(clevel=2))
+
+	v = bcarray[0]
+
+	result = bcarray.dot(v)
+	expected = matrix.dot(v)
+
+	assert_array_almost_equal(expected, result, decimal=5)
+
+
+def test_dot_float32():
+
+
+	matrix = np.random.random_sample(size=(10000, 100)).astype('float32')
+	bcarray = bdot.carray(matrix, chunklen=2**13, cparams=bcolz.cparams(clevel=2))
+
+	v = bcarray[0]
+
+	result = bcarray.dot(v)
+	expected = matrix.dot(v)
+
+	assert_array_almost_equal(expected, result, decimal=5)
