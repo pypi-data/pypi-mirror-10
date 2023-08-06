7a44fcd40775a6ee6ac2453a0884ba718f4fbffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:55 +0000

    Add package mondb/0.0.5.2/mondb-0.0.5.2.tar.gz

---
+++ packages/mondb/mondb-0.0.5.2.tar.gz/mondb-0.0.5.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    return open("README.md", "r").read()
+
+setup(
+    name = 'mondb',
+    packages = ['mondb', 'mondb/urltools', 'mondb/Query'],
+    version = '0.0.5.2',
+    long_description= readme(),
+    description = "MODELS for MONGODB",
+    author='plasmashadow',
+    author_email='plasmashadowx@gmail.com',
+    url='https://github.com/RevelutionWind/MDB.git',
+    license="BSD 2-Claus",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires=['pymongo >= 2.8', 'six >= 1.9.0', 'mock'],
+    test_suite="test"
+)
---
+++ packages/mondb/mondb-0.0.5.2.tar.gz/mondb-0.0.5.2/test/test_connection.py
@@ -0,0 +1,42 @@
+__author__ = 'plasmashadow'
+
+import unittest
+import mondb
+from mondb.Connection import create_engine
+import random, json
+
+
+class CrudTest(unittest.TestCase):
+    def setUp(self):
+        create_engine(host="localhost", port=27017, database="test")
+
+        class User(mondb.Document):
+            name = mondb.StringProperty()
+            age = mondb.IntegerProperty()
+            time = mondb.DateTimeProperty(auto_add=True)
+            lst = mondb.ListProperty()
+            dct = mondb.DictProperty()
+            jsn = mondb.JsonProperty()
+
+        self.User = User
+
+
+    def test_save(self):
+        user = self.User()
+        user.name = "sathya"
+        user.age = 23
+        user.lst = [1, 2, 5]
+        user.dct = {'tara': 'adhi'}
+        user.jsn = json.dumps({"hey": "hello"})
+        user.save()
+        self.assertEqual(user.name, "sathya")
+
+    def test_search(self):
+        self.age_number = random.choice(range(100))
+        self.User(name="adhi", age= self.age_number).save()
+        results = self.User.search(age=self.age_number)
+        self.assertEqual(len(results), 1)
+
+
+
+
