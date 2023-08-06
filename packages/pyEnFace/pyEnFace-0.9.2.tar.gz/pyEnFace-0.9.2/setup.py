d630791ff74923a9dac15ec3d3d8df1a8e3496fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:58 +0000

    Add package pyEnFace/0.9.2/pyEnFace-0.9.2.tar.gz

---
+++ packages/pyEnFace/pyEnFace-0.9.2.tar.gz/pyEnFace-0.9.2/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_packages
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description=f.read()
+
+setup(
+    name='pyEnFace',
+    version='0.9.2',
+    description='A python interface to the Enphase Developer API',
+    long_description=long_description,
+    url='https://github.com/e2thenegpii/EnphaseInterface',
+    author='Eldon Allred',
+    author_email='e2thenegpii@gmail.com',
+    license='Gnu Public License version 3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='enphase development',
+    packages=find_packages('pyEnFace'),
+    package_data={'':['DESCRIPTION.rst']},
+    include_package_data=True,
+    install_requires=['pandas']
+)
