61fc1f47cca491ec365c875ad6b7d1bdfed81da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:46 +0000

    Add package smartchoices/1.0/smartchoices-1.0.tar.gz

---
+++ packages/smartchoices/smartchoices-1.0.tar.gz/smartchoices-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='smartchoices',
+    version='1.0',
+    description='A smart choices interface for Django apps',
+    author='Storybird Developers',
+    author_email='dev@storybird.com',
+    url='https://github.com/storybird/smartchoices',
+    license='MIT',
+    modules=('smartchoices',),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
