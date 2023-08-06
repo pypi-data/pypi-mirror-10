c30a73fc43cb807c0a296a26e4829c53e46972a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:42 +0000

    Add package yagmail/0.3.76/yagmail-0.3.76.tar.gz

---
+++ packages/yagmail/yagmail-0.3.76.tar.gz/yagmail-0.3.76/yagmail/__init__.py
@@ -0,0 +1,10 @@
+from pkg_resources import get_distribution
+
+__project__ = 'yagmail'
+__version__ = get_distribution(__project__).version
+
+from .error import YagConnectionClosed
+from .error import YagAddressError
+from .yagmail import Connect
+from .yagmail import register
+from .yagmail import logging
