a8fac6c704b81a1a45b78f2db787919a8eb0e30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:54 +0000

    Add package django-writeback/0.1.9/django-writeback-0.1.9.zip

---
+++ packages/django-writeback/django-writeback-0.1.9.zip/django-writeback-0.1.9/writeback/urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import *
+
+from .views import MessageCreateView
+
+
+urlpatterns = patterns('',
+
+    url(r'^$', MessageCreateView.as_view(), name='writeback_message_create'),
+
+)
+
+# https://github.com/jaredly/django-feedback
+# TODO dependencies
+# TODO model customization
+# TODO testing
