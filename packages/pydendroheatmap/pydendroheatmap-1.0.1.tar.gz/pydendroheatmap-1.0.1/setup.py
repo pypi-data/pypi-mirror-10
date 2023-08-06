96b152ddd2121cfc0f9eb45a66ce8be0c6333d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:17 +0000

    Add package pydendroheatmap/1.0.1/pydendroheatmap-1.0.1.tar.gz

---
+++ packages/pydendroheatmap/pydendroheatmap-1.0.1.tar.gz/pydendroheatmap-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    f = open('README.md','r')
+    text = f.read()
+    f.close()
+    return text
+
+setup(name='pydendroheatmap',
+      version='1.0.1',
+      description='Tool for creating heatmaps, where rows and columns are organized by hierarchical clusters'
+                  'as seen in http://code.activestate.com/recipes/578175-hierarchical-clustering-heatmap-python/',
+      url='https://github.com/themantalope/pydendroheatmap',
+      author='Matt Antalek',
+      author_email='matthew.antalek@northwestern.edu',
+      license='MIT',
+      packages=['pydendroheatmap'],
+      setup_requires=['numpy'],
+      install_requires = [
+          'numpy',
+          'scipy',
+          'matplotlib',
+
+      ],
+      long_description=readme(),
+      classifiers=['Topic :: Scientific/Engineering :: Visualization'],
+      include_package_data=True)
