8b923ffedca948c8d0d7408e9077e3e9c3f61b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:44 +0000

    Add package pytest-bdd/2.13.0/pytest-bdd-2.13.0.tar.gz

---
+++ packages/pytest-bdd/pytest-bdd-2.13.0.tar.gz/pytest-bdd-2.13.0/pytest_bdd/exceptions.py
@@ -0,0 +1,61 @@
+"""pytest-bdd Exceptions."""
+
+
+class StepError(Exception):
+
+    """Step declaration error."""
+
+
+class ScenarioIsDecoratorOnly(Exception):
+
+    """Scenario can be only used as decorator."""
+
+
+class ScenarioValidationError(Exception):
+
+    """Base class for scenario validation."""
+
+
+class ScenarioNotFound(ScenarioValidationError):
+
+    """Scenario Not Found."""
+
+
+class ExamplesNotValidError(ScenarioValidationError):
+
+    """Example table is not valid."""
+
+
+class ScenarioExamplesNotValidError(ScenarioValidationError):
+
+    """Scenario steps parameters do not match declared scenario examples."""
+
+
+class FeatureExamplesNotValidError(ScenarioValidationError):
+
+    """Feature example table is not valid."""
+
+
+class StepTypeError(ScenarioValidationError):
+
+    """Step definition is not of the type expected in the scenario."""
+
+
+class GivenAlreadyUsed(ScenarioValidationError):
+
+    """Fixture that implements the Given has been already used."""
+
+
+class StepDefinitionNotFoundError(Exception):
+
+    """Step definition not found."""
+
+
+class InvalidStepParserError(Exception):
+
+    """Invalid step parser."""
+
+
+class NoScenariosFound(Exception):
+
+    """No scenarios found."""
