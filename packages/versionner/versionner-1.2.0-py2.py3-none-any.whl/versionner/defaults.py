8c9ff3df4384a74b50d8778c43792ff7ecae1875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:02 +0000

    Add package versionner/1.2.0/versionner-1.2.0-py2.py3-none-any.whl

---
+++ packages/versionner/versionner-1.2.0-py2.py3-none-any.whl/versionner/defaults.py
@@ -0,0 +1,10 @@
+"""
+    Default values for some versionner options
+"""
+
+RC_FILENAME = '.versionner.rc'
+DEFAULT_VERSION_FILE = './VERSION'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+DEFAULT_UP_PART = 'minor'
+DEFAULT_TAG_TIMEOUT = 5
+DEFAULT_INIT_VERSION = '0.1.0'
