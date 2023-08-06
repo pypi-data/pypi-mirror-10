57ed1261acd2511b147a74992291066aa958b601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:55 +0000

    Add package automate/0.9.2/automate-0.9.2.tar.gz

---
+++ packages/automate/automate-0.9.2.tar.gz/automate-0.9.2/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+def get_version(filename):
+    import re
+    with open(filename) as fh:
+        metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
+        return metadata['version']
+
+setupopts = dict(
+    name="automate",
+    version=get_version('src/automate/__init__.py'),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'traits==4.5.0',
+        'croniter==0.3.8',
+        'pyinotify==0.9.6',
+        'ipython==3.2',
+        'ansiconv==1.0',
+        'colorlog==2.6'],
+    test_suite='py.test',
+    tests_require=['pytest', 'pytest-capturelog'],
+    download_url='https://pypi.python.org/pypi/automate',
+    platforms = ['any'],
+    author="Tuomas Airaksinen",
+    author_email="tuomas.airaksinen@gmail.com",
+    description="Automate is a general purpose Python automatization library.",
+    long_description=open('README.rst').read(),
+    license="GPL",
+    keywords="automation, GPIO, Raspberry Pi, RPIO, traits",
+    url="http://github.com/tuomas2/automate",
+
+    classifiers=["Development Status :: 4 - Beta",
+                 "Environment :: Console",
+                 "Environment :: Web Environment",
+                 "Intended Audience :: Education",
+                 "Intended Audience :: End Users/Desktop",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Information Technology",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
+                 "Operating System :: Microsoft :: Windows",
+                 "Operating System :: POSIX",
+                 "Operating System :: POSIX :: Linux",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: Scientific/Engineering",
+                 "Topic :: Software Development",
+                 "Topic :: Software Development :: Libraries",
+                 "Topic :: Software Development :: Libraries :: Application Frameworks",
+                 ]
+)
+
+if __name__ == "__main__":
+    setup(**setupopts)
