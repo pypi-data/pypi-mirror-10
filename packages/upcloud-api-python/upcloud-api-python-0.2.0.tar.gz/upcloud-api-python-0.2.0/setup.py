d1f8bc47e6c05dec05f7798d59a76d56ba1d3628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:06 +0000

    Add package upcloud-api-python/0.2.0/upcloud-api-python-0.2.0.tar.gz

---
+++ packages/upcloud-api-python/upcloud-api-python-0.2.0.tar.gz/upcloud-api-python-0.2.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='upcloud-api-python',
+    version='0.2.0',
+    description='UpCloud API Client',
+    author='Elias Nygren',
+    author_email='elias.nygren@upcloud.com',
+    maintainer='Elias Nygren',
+    maintainer_email='elias.nygren@upcloud.com',
+    url='https://github.com/UpCloudLtd/upcloud-python-api',
+    packages=['upcloud', 'upcloud.cloud_manager'],
+    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.2.0',
+    license='MIT',
+    install_requires=[
+        'mock==1.0.1',
+        'py==1.4.26',
+        'pytest==2.6.4',
+        'requests==2.6.0',
+        'responses==0.3.0',
+        'six==1.9.0'
+    ]
+)
