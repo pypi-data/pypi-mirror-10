db829dd4faaf290ed656cde22b96e835347766e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:09 +0000

    Add package MendotaBuoy/0.0.0/MendotaBuoy-0.0.0.tar.gz

---
+++ packages/MendotaBuoy/MendotaBuoy-0.0.0.tar.gz/MendotaBuoy-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MendotaBuoy',
+    version='0.0.0',
+    author='Kenny Gao',
+    author_email='kgao9@wisc.edu',
+    packages=['mendotabuoy'],
+    scripts=[],
+    license='LICENSE.txt',
+    description='Mendota buoy l00 to a1 conversion.'
+)
