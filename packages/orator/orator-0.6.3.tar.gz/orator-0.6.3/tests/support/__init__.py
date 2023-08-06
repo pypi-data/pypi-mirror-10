0a369f7a4261a7bee4053e9fd59c696953b1f348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:31 +0000

    Add package orator/0.6.3/orator-0.6.3.tar.gz

---
+++ packages/orator/orator-0.6.3.tar.gz/orator-0.6.3/tests/support/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .. import OratorTestCase
+from .. import mock
+
+from ..utils import MockConnection, MockProcessor
+
+from orator.support.collection import Collection
+
+
+class SupportCollectionTestCase(OratorTestCase):
+
+    pass
