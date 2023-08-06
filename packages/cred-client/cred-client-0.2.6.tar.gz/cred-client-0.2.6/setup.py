c264186363815840fd395e6cc08d4a67b7eeacc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:43 +0000

    Add package cred-client/0.2.6/cred-client-0.2.6.tar.gz

---
+++ packages/cred-client/cred-client-0.2.6.tar.gz/cred-client-0.2.6/setup.py
@@ -0,0 +1,38 @@
+""" Setup file for the cred-client package. """
+from setuptools import setup
+import sys
+
+# The package only works with python >=3.0
+if sys.version_info < (3,):
+    print("I'm only for 3, please upgrade")
+    sys.exit(1)
+
+version = '0.2.6'
+
+setup(
+    name='cred-client',
+    version=version,
+    author='Tehnix',
+    author_email='ckl@codetalk.io',
+    packages=['cred'],
+    include_package_data=True,
+    scripts=[],
+    url='https://github.com/Tehnix/cred-client',
+    download_url='https://github.com/Tehnix/cred-client/tarball/v{0}'.format(version),
+    license='BSD',
+    description='Client Library for cred-server.',
+    # long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Home Automation',
+    ],
+    install_requires=[],
+    #test_suite='nose.collector',
+)
