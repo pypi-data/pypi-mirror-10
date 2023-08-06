9c0cc44d2ba472cd3c8121017cfc7ab6e94714f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:27 +0000

    Add package link_validator/0.2.0/link_validator-0.2.0.tar.gz

---
+++ packages/link_validator/link_validator-0.2.0.tar.gz/link_validator-0.2.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+  with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+except:
+  long_descr = "Will recursively search and validate the HTTP response code of each link on a specified web page - \
+                and those of its children up - to the maximum specified recursion depth."
+  
+setup(name='link_validator',
+      version='0.2.0',
+      description='Recursive link validator',
+      long_description=long_descr,
+      url='http://github.com/at1as/link-validator',
+      author='Jason Willems',
+      author_email='hello@jasonwillems.com',
+      license='MIT',
+      packages=['link_validator'],
+      entry_points = {
+        "console_scripts": ['link_validator = link_validator.link_validator:main']
+      },
+      zip_safe=False)
+
