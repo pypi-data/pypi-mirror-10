b6a29367c88fd09dfd76ed079fff8c4f222b55b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:54 +0000

    Add package enhance_djpro_tpl/0.0.8/enhance_djpro_tpl-0.0.8.tar.gz

---
+++ packages/enhance_djpro_tpl/enhance_djpro_tpl-0.0.8.tar.gz/enhance_djpro_tpl-0.0.8/setup.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+#
+from setuptools import setup, find_packages
+
+setup(
+    name = 'enhance_djpro_tpl',
+    version = '0.0.8',
+    keywords = ('enhance', 'project', 'template', 'app', 'django'),
+    description = 'An enhance django project template.',
+    license = 'GNU LESSER GENERAL PUBLIC LICENSE',
+    install_requires = [],
+
+    author = 'Maple.Liu',
+    author_email = 'fjliufeng@163.com',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
