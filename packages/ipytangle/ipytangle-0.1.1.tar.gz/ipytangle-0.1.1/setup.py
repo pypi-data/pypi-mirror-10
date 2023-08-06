633356a9cdd60f569858fd07f6ae9a9c314ad866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:47 +0000

    Add package ipytangle/0.1.1/ipytangle-0.1.1.tar.gz

---
+++ packages/ipytangle/ipytangle-0.1.1.tar.gz/ipytangle-0.1.1/setup.py
@@ -0,0 +1,32 @@
+import json
+
+from setuptools import setup
+
+
+try:
+    from jupyterpip import cmdclass
+except:
+    import pip
+    import importlib
+
+    pip.main(["install", "jupyter-pip"])
+    cmdclass = importlib.import_module("jupyterpip").cmdclass
+
+with open("setup.json") as f:
+    setup_data = json.load(f)
+
+
+with open("README.rst") as f:
+    setup_data.update(
+        long_description=f.read()
+    )
+
+setup_data.update(
+    cmdclass=cmdclass(
+        path="{0}/static/{0}".format(
+            setup_data["packages"][0]
+        )
+    )
+)
+
+setup(**setup_data)
