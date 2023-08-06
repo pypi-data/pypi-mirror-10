2422c8e36c391f5df850c79ad7ba869aec82c6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:30 +0000

    Add package odoo-ook/5.0.2/odoo-ook-5.0.2.tar.gz

---
+++ packages/odoo-ook/odoo-ook-5.0.2.tar.gz/odoo-ook-5.0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+        name             = "odoo-ook",
+        version          = "5.0.2",
+        author           = "Frederic van der Essen",
+        author_email     = "fvdessen+x@gmail.com",
+        packages         = ["ook"],
+        url              = "https://github.com/fvdsn/odoo-ook",
+        license          = "MIT",
+        install_requires = [
+            "appdirs",
+            "flake"
+        ],
+        scripts          = [
+            "scripts/ook"
+        ]
+    )
+
