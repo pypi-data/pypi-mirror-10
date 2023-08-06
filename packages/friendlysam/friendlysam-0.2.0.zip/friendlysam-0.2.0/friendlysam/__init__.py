554f57cfa35d3e2fd532b7f00cab5066cf46b2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:55 +0000

    Add package friendlysam/0.2.0/friendlysam-0.2.0.zip

---
+++ packages/friendlysam/friendlysam-0.2.0.zip/friendlysam-0.2.0/friendlysam/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import logging
+logger = logging.getLogger(__name__)
+
+from friendlysam.common import *
+from friendlysam.parts import *
+import friendlysam.opt
+from friendlysam.opt import *
+import friendlysam.models
---
+++ packages/friendlysam/friendlysam-0.2.0.zip/friendlysam-0.2.0/friendlysam/common.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+logger = logging.getLogger(__name__)
+
+import friendlysam as fs
+from friendlysam.compat import ignored
+
+try:
+    import pandas
+except ImportError:
+    pandas = None
+
+def short_default_repr(obj, desc=None):
+    if desc is None:
+        return '<{}.{} at {}>'.format(obj.__module__, obj.__class__.__name__, hex(id(obj)))
+    return '<{}.{} at {}: {}>'.format(obj.__module__, obj.__class__.__name__, hex(id(obj)), desc)
+
+def get_list(func, indices):
+    return [float(func(index)) for index in indices]
+
+def get_series(func, indices, **kwargs):
+    if not pandas:
+        raise RuntimeError('pandas is needed for this function').with_traceback(sys.exc_info()[2])
+    return pandas.Series(index=indices, data=get_list(func, indices), **kwargs)
