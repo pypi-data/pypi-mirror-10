383c5af2066ef2fd92a4f9563b3198b329fdb9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:09 +0000

    Add package ma.mongo/1.0.0/ma.mongo-1.0.0.tar.gz

---
+++ packages/ma.mongo/ma.mongo-1.0.0.tar.gz/ma.mongo-1.0.0/setup.mongo.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ma.mongo',
+    version = '1.0.0',
+    description='MongoDB utilities',
+    author='Fredrik Haard',
+    author_email='fredrik@metallapan.se',
+    packages=['ma.mongo']
+)
