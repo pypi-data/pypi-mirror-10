cd86116966f9b19664ec26cecbe7a2a25100f8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:31 +0000

    Add package SMBL/0.1.1.post42/SMBL-0.1.1.post42.tar.gz

---
+++ packages/SMBL/SMBL-0.1.1.post42.tar.gz/SMBL-0.1.1.post42/smbl/__init__.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import snakemake
+import platform
+import pkg_resources
+
+import smbl.messages
+
+
+# mapping wrappers
+#  - commands in practise:
+#       - https://github.com/lh3/mem-paper/blob/master/eval/time.txt
+#       - http://lh3lh3.users.sourceforge.net/alnROC.shtml
+
+version=pkg_resources.require("smbl")[0].version
+
+smbl.messages.message("",program="SMBL")
+smbl.messages.message("SnakeMake Bioinformatics Library",program="SMBL")
+smbl.messages.message("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",program="SMBL")
+smbl.messages.message("Version: {}".format(version),program="SMBL")
+smbl.messages.message("Web:     http://github.com/karel-brinda/smbl",program="SMBL")
+smbl.messages.message("Contact: Karel Brinda, karel.brinda@univ-mlv.fr",program="SMBL")
+smbl.messages.message("",program="SMBL")
+smbl.messages.message("Platform: {}".format(sys.platform),program="SMBL")
+smbl.messages.message("System: {}".format(platform.system()),program="SMBL")
+smbl.messages.message("Machine: {}".format(platform.machine()),program="SMBL")
+smbl.messages.message("Processor: {}".format(platform.processor()),program="SMBL")
+smbl.messages.message("Python version: {}".format(platform.python_version()),program="SMBL")
+smbl.messages.message("Python build: {}".format(", ".join(platform.python_build())),program="SMBL")
+smbl.messages.message("",program="SMBL")
+
+smbl_dir = os.path.join(os.path.expanduser("~"),".smbl")
+
+bin_dir  = os.path.join(smbl_dir,"bin")
+fa_dir   = os.path.join(smbl_dir,"fa")
+src_dir  = os.path.join(smbl_dir,"src")
+
+import smbl.utils
+import smbl.prog
+import smbl.fasta
+
+def include():
+	return os.path.join(
+			os.path.dirname(__file__),
+			"include_all.snake"
+		)
+
+def all_fastas():
+	return [
+			fastafile.get() for fastafile in smbl.fasta.get_registered_fastas()
+		]
+
+def all_programs():
+	return [
+			plugin.get_installation_files() for plugin in smbl.prog.plugins.get_registered_plugins()
+		]
+
+def all_compatible_programs():
+	return [
+			plugin.get_installation_files() for plugin in smbl.prog.plugins.get_registered_plugins()
+				if plugin.is_platform_supported()
+		]
+
+snakemake.shell(
+		"""
+			mkdir -p "{}" "{}" "{}"
+		""".format(bin_dir,fa_dir,src_dir)
+	)
