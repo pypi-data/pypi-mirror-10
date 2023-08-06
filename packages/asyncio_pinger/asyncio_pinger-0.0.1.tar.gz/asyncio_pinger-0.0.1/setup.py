70b2f263eaef5de06ac65db55b57b3b3ce80f5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:31 +0000

    Add package asyncio_pinger/0.0.1/asyncio_pinger-0.0.1.tar.gz

---
+++ packages/asyncio_pinger/asyncio_pinger-0.0.1.tar.gz/asyncio_pinger-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='asyncio_pinger',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/lmasikl/asyncio_pinger',
+    license='MIT',
+    author='Maxim Krivodaev',
+    author_email='maxim.krivodaev@gmail.com',
+    description='Find servers ping and sort it.'
+)
