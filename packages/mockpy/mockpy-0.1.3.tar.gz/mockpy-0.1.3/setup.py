d193930bac652e7399495d35c77a560b84c119b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:29 +0000

    Add package mockpy/0.1.3/mockpy-0.1.3.tar.gz

---
+++ packages/mockpy/mockpy-0.1.3.tar.gz/mockpy-0.1.3/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('Readme.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    "mock",
+    "cherrypy",
+    "termcolor",
+    "watchdog"
+]
+
+test_requirements = [
+    "mock",
+    "termcolor"
+]
+
+setup(
+    name='mockpy',
+    version='0.1.3',
+    description="Mockpy is an work in progress open source tool to quickly create mock servers. Mockpy is inspired from wiremock and uses libmproxy for the proxy functionality",
+    long_description=readme + '\n\n' + history,
+    author="Omar Abdelhafith",
+    author_email='o.arrabi@me.com',
+    url='https://github.com/oarrabi/mockpy',
+    packages=[
+        'mockpy',
+        "mockpy.core",
+        "mockpy.models",
+        "mockpy.utils",
+        "mockpy.status",
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='mockpy',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    entry_points={
+        'console_scripts': [
+            'mockpy=mockpy.mockpy:start',
+        ],
+    },
+)
