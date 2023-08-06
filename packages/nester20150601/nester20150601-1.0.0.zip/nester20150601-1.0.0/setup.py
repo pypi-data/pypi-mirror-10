71df2f19b71b717ea230128b48b203bc0678ff16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package nester20150601/1.0.0/nester20150601-1.0.0.zip

---
+++ packages/nester20150601/nester20150601-1.0.0.zip/nester20150601-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name  = 'nester20150601',
+		version = '1.0.0',
+		py_modules = ['nester20150601'],
+		author = 'hfpython',
+		author_email = '123@qq.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple from Head First Python',
+	)
