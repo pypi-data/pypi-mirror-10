e35f0347d53af56afc49aa2f99f844cfda7d7946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:32 +0000

    Add package ticketbis/0.1/ticketbis-0.1.tar.gz

---
+++ packages/ticketbis/ticketbis-0.1.tar.gz/ticketbis-0.1/ticketbis/tests/_creds.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# (c) 2015 Ticketbis
+
+#API_ENDPOINT = u'http://ec2-54-74-140-5.eu-west-1.compute.amazonaws.com/'
+#CLIENT_ID = u'test'
+#CLIENT_SECRET = u'test'
+#ACCESS_TOKEN = u'testTOKEN'
+
+API_ENDPOINT = u'http://api.ticketbis.com/'
+CLIENT_ID = u'TgmOMjbTqySP56RBiom3wM4xiPbq7J8eCgW99g4B'
+CLIENT_SECRET = u'WdbFgy7GnZrF32RX6ohBC3rh4HW2Ta4QOTZNc5cfKdSNGOniVb5ezrDtwMRlNnH40Mq0emfYqf0sVuFq5pm2m1EWC1Ziaz4vGnyq6PqSAE2zjNJw6qIISqdBLSYkZCMz'
+ACCESS_TOKEN = u'tBqaWOVvLxVEDAquHm0CsqU40LBv0I'
