78d8a675cf36c983e29ce503a4dbc67be3b8a510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:27 +0000

    Add package python-hosts/0.2.9/python-hosts-0.2.9.tar.gz

---
+++ packages/python-hosts/python-hosts-0.2.9.tar.gz/python-hosts-0.2.9/setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as testcommand
+
+version = "0.2.9"
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.md').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = []
+
+long_description = readme + '\n\n' + history
+
+if sys.argv[-1] == 'readme':
+    print(long_description)
+    sys.exit()
+
+
+class PyTest(testcommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def __init__(self):
+        super(testcommand, self).__init__()
+        self.pytest_args = None
+        self.test_args = None
+        self.test_suite = None
+
+    def initialize_options(self):
+        testcommand.initialize_options(self)
+
+    def finalize_options(self):
+        testcommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='python-hosts',
+    version=version,
+    author='Jon Hadfield',
+    author_email='jon.hadfield@lessknown.co.uk',
+    url = 'https://github.com/jonhadfield/python-hosts',
+    download_url = 'https://github.com/jonhadfield/python-hosts/tarball/0.2.9',
+    install_requires=[],
+    description='A hosts file manager library written in python',
+    long_description=long_description,
+    packages=['python_hosts'],
+    platforms='any',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Operating System',
+        'Topic :: System :: Networking',
+    ],
+    keywords=(
+        'hosts, python, network'
+    ),
+    cmdclass = {'test': PyTest},
+    test_suite='tests',
+    tests_require=['pytest']
+)
