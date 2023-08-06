1dc8ef9f4f7c018a897025390131fdba8c916c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:58 +0000

    Add package rasa/0.0.4/rasa-0.0.4.tar.gz

---
+++ packages/rasa/rasa-0.0.4.tar.gz/rasa-0.0.4/HISTORY.rst
@@ -0,0 +1,26 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.0.4 (2015-06-02)
+++++++++++++++++++
+
+* Fixed packaging bug
+
+0.0.3 (2015-05-30)
+++++++++++++++++++
+
+* Renamed replace functions to update
+* Added constants for new services (rasa.constants)
+
+0.0.2 (2015-05-24)
+++++++++++++++++++
+
+* Changed function structure
+* Added functions for network/service objects
+
+0.0.1 (2015-04-28)
+++++++++++++++++++
+
+* Initial testing
---
+++ packages/rasa/rasa-0.0.4.tar.gz/rasa-0.0.4/setup.py
@@ -0,0 +1,36 @@
+import re
+
+from codecs import open
+from setuptools import setup
+
+version = ''
+with open('rasa/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+
+
+long_description = history
+
+config = {
+    'name': 'rasa',
+    'packages': ['rasa'],
+    'version': version,
+    'description': 'A wrapper Cisco ASA REST API',
+    'long_description': long_description,
+    'author': 'Patrick Ogenstad',
+    'author_email': 'patrick@ogenstad.com',
+    'license': 'Apache',
+    'url': 'http://networklore.com/rasa/',
+    'install_requires': ['requests'],
+    'classifiers': ['Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: System Administrators']
+}
+
+setup(**config)
