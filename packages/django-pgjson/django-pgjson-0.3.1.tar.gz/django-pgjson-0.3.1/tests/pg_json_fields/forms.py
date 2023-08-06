8280521533a446c124d29101bf8931add96e3ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:36 +0000

    Add package django-pgjson/0.3.1/django-pgjson-0.3.1.tar.gz

---
+++ packages/django-pgjson/django-pgjson-0.3.1.tar.gz/django-pgjson-0.3.1/tests/pg_json_fields/forms.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+from django.forms.models import ModelForm
+
+from .models import IntModel
+
+
+class IntArrayForm(ModelForm):
+    class Meta:
+        model = IntModel
