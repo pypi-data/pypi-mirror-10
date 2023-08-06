fa265ecb2edd2911046ccf016a4915983a7d53d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:52 +0000

    Add package django-newsletters/0.0.2/django-newsletters-0.0.2.tar.gz

---
+++ packages/django-newsletters/django-newsletters-0.0.2.tar.gz/django-newsletters-0.0.2/newsletters/models.py
@@ -0,0 +1,15 @@
+from django.utils.translation import ugettext as _
+from django.db import models
+
+
+class Subscription(models.Model):
+    email = models.EmailField(unique=True)
+    ip = models.IPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('Subscription')
+        verbose_name_plural = _('Subscriptions')
+
+    def __str__(self):
+        return self.email
