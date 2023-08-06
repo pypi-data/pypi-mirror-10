8ed7ad281618a388a1ff8eec70364da100478831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:49 +0000

    Add package shapeshift/0.3.0/shapeshift-0.3.0-py2.py3-none-any.whl

---
+++ packages/shapeshift/shapeshift-0.3.0-py2.py3-none-any.whl/shapeshift/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Transforming python logs
+"""
+from __future__ import absolute_import
+from collections import namedtuple
+
+from .formatters import JSONFormatter, KeyValueFormatter
+
+version_info = namedtuple("version_info", ["major", "minor", "patch"])
+VERSION = version_info(0, 3, 0)
+__version__ = "{0.major}.{0.minor}.{0.patch}".format(VERSION)
+
+__all__ = ["JSONFormatter", "KeyValueFormatter", "MessagePackFormatter"]
