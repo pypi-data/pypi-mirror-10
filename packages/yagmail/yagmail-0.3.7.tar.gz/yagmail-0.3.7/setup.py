2ecaeb69d01d9434b4fe58979c609d0fe34d10b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:17 +0000

    Add package yagmail/0.3.7/yagmail-0.3.7.tar.gz

---
+++ packages/yagmail/yagmail-0.3.7.tar.gz/yagmail-0.3.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# run updater like: python setup.py sdist bdist_wininst upload
+
+setup(name = 'yagmail',
+      version = "0.3.7",
+      description = 'Yet Another GMAIL client',
+      url = 'https://github.com/kootenpv/yagmail',
+      author = 'Pascal van Kooten',
+      author_email = 'kootenpv@gmail.com',
+      license = 'GPL',
+      packages = ['yagmail'],
+      install_requires = [ 
+          'keyring',
+      ],
+      entry_points = { 
+          'console_scripts': ['yagmail = yagmail.yagmail:main'] 
+          },
+      zip_safe = False)
