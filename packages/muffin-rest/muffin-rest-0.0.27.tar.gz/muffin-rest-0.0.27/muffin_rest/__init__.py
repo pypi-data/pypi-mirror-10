66dfc1df20d79b2456f473d216679286f5a965f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:43 +0000

    Add package muffin-rest/0.0.27/muffin-rest-0.0.27.tar.gz

---
+++ packages/muffin-rest/muffin-rest-0.0.27.tar.gz/muffin-rest-0.0.27/muffin_rest/__init__.py
@@ -0,0 +1,43 @@
+""" REST helpers for Muffin Framework. """
+
+from muffin import HTTPNotFound, HTTPBadRequest, HTTPForbidden
+
+# Package information
+# ===================
+
+__version__ = "0.0.27"
+__project__ = "muffin-rest"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+
+class RESTNotFound(HTTPNotFound):
+
+    """ Custom excption class for stopping default application error handlers. """
+
+    pass
+
+
+class RESTBadRequest(HTTPBadRequest):
+
+    """ Custom excption class for stopping default application error handlers. """
+
+    pass
+
+
+class RESTForbidden(HTTPForbidden):
+
+    """ Custom exception class for stopping default application error handlers. """
+
+    pass
+
+from .api import *      # noqa
+from .filters import *  # noqa
+from .forms import *    # noqa
+from .handlers import * # noqa
+
+try:
+    from .peewee import PWRESTHandler           # noqa
+    from .peewee import PWFilter, PWLikeFilter  # noqa
+except ImportError:
+    pass
