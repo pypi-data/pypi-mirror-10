d20d512d3c57455f953983603d8c06f2f4d44a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:03 +0000

    Add package diffpy.srmise/0.5.1/diffpy.srmise-0.5.1.tar.gz

---
+++ packages/diffpy.srmise/diffpy.srmise-0.5.1.tar.gz/diffpy.srmise-0.5.1/diffpy/srmise/applications/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# SrMise            by Luke Granlund
+#                   (c) 2014 trustees of the Michigan State University.
+#                   All rights reserved.
+#
+# File coded by:    Luke Granlund
+#
+# See LICENSE.txt for license information.
+#
+##############################################################################
