a6f4103504e00ff148fcb5289a5a122e627768c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:47 +0000

    Add package sjt94/1.0.0/sjt94-1.0.0.zip

---
+++ packages/sjt94/sjt94-1.0.0.zip/sjt94-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='sjt94',
+	version='1.0.0',
+	py_modules=['sjt94'],
+	author='abc',
+	author_email='yjt@1.com',
+	url='www.aaa.com',
+	description='A simple',
+	)
