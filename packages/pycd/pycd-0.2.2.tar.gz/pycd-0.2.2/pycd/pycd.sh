e272af1d77075ffa6fc7215aea9b7e34098214d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:17 +0000

    Add package pycd/0.2.2/pycd-0.2.2.tar.gz

---
+++ packages/pycd/pycd-0.2.2.tar.gz/pycd-0.2.2/pycd/pycd.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env sh
+#
+
+function pycd ()
+{
+  local module_path
+  module_path=`pypkg find $1 --no-warning`
+  if [ "$module_path" != "" ]; then
+    cd $module_path
+  else
+    if [ "$1" = "" ]; then
+      echo "Usage: pycd <module_name>"
+    else
+      echo "$1 not found"
+    fi
+  fi
+}
+
+
+# ------------------------------
+# Completion
+# ------------------------------
+
+# ZSH support
+if [[ -n ${ZSH_VERSION-} ]]; then
+    autoload -U +X bashcompinit && bashcompinit
+fi
+
+_pycd()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=""
+
+    if [[ ${opts} = "" ]] ; then
+        opts=$(pypkg list)
+    fi
+
+    if [[ ${cur} = * ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+        return 0
+    fi
+}
+complete -F _pycd pycd
+
+# Setup antigen's autocompletion
+_pypkg () {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="find list install_pycd help"
+
+    COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+    return 0
+}
+complete -F _pypkg pypkg
