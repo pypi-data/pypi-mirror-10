b945466f0ff2a1c911927a859dbb35effa171e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:31 +0000

    Add package astronomicon/0.0.6/astronomicon-0.0.6.zip

---
+++ packages/astronomicon/astronomicon-0.0.6.zip/astronomicon-0.0.6/astronomicon.py
@@ -0,0 +1,10 @@
+""" Astronomicon.py  This contains a set of astronomy and astrology calculations as an exercise in learning python.
+Absolutely not ready yet"""
+
+""" get int returns the integer component of a division"""    
+def get_int(one, two):
+    return int(one / two);
+
+""" get modulus returns the modulus component of a division"""
+def get_mod(one, two):
+    return (one % two);
