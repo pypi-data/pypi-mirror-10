6bb8f3295861f246f822b5ce7a4f6594dc0f5d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package carpentry-ci/0.1.36/carpentry-ci-0.1.36.tar.gz

---
+++ packages/carpentry-ci/carpentry-ci-0.1.36.tar.gz/carpentry-ci-0.1.36/carpentry/util.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from __future__ import unicode_literals
+
+import warnings
+from docker import Client
+from docker.utils import kwargs_from_env
+
+
+warnings.simplefilter("ignore")
+
+
+def render_string(template, context):
+    return template.format(**context)
+
+
+def calculate_redis_key(instructions):
+    out_key = render_string('carpentry:stdout:{id}', instructions)
+    err_key = render_string('carpentry:stderr:{id}', instructions)
+    return out_key, err_key
+
+
+def get_docker_client():
+    kwargs = kwargs_from_env()
+    if 'tls' in kwargs:
+        kwargs['tls'].verify = False
+
+    kwargs['timeout'] = 60 * 5
+
+    docker = Client(**kwargs)
+    return docker
