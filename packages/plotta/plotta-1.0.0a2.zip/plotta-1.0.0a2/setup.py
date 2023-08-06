9e267e7d0dc13b22dfda5282655182d4516c1ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package plotta/1.0.0a2/plotta-1.0.0a2.zip

---
+++ packages/plotta/plotta-1.0.0a2.zip/plotta-1.0.0a2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='plotta',
+    version='1.0.0a2',
+    install_requires=['unirest'],
+
+    author='Guido Zuidhof',
+    author_email='me@guido.io',
+
+    description='Python wrapper for Plotta API',
+    url='https://github.com/gzuidhof/plotta-python',
+    license='MIT',
+    keywords='plot plotting plotta',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
