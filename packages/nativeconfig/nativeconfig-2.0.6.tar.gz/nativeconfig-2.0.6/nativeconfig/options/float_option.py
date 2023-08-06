846e74d18fba622f8b5a7606e39b32609a9a042a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:11 +0000

    Add package nativeconfig/2.0.6/nativeconfig-2.0.6.tar.gz

---
+++ packages/nativeconfig/nativeconfig-2.0.6.tar.gz/nativeconfig-2.0.6/nativeconfig/options/float_option.py
@@ -0,0 +1,42 @@
+import json
+from nativeconfig.exceptions import DeserializationError, ValidationError
+from nativeconfig.options.base_option import BaseOption
+
+
+class FloatOption(BaseOption):
+    """
+    FloatOption represents Python float in config.
+
+    """
+
+    def __init__(self, name, **kwargs):
+        """
+        Accepts all the arguments of BaseConfig except choices.
+        """
+        super().__init__(name, **kwargs)
+
+    def serialize(self, python_value):
+        return str(python_value)
+
+    def deserialize(self, raw_value):
+        try:
+            value = float(raw_value)
+        except ValueError:
+            raise DeserializationError("Unable to deserialize \"{}\" into float value!".format(raw_value), raw_value)
+        else:
+            return value
+
+    def deserialize_json(self, json_value):
+        try:
+            value = json.loads(json_value)
+        except ValueError:
+            raise DeserializationError("Invalid json: \"{}\"".format(json_value), json_value)
+        else:
+            return value
+
+    def validate(self, python_value):
+        super().validate(python_value)
+        try:
+            valid_val = float(python_value)
+        except ValueError:
+            raise ValidationError("Invalid float value \"{}\"!".format(python_value), python_value)
