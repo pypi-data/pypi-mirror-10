7dcdca34823d62f84b0075e361b2b63d9450db73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:50 +0000

    Add package MDAnalysis/0.10.0/MDAnalysis-0.10.0.tar.gz

---
+++ packages/MDAnalysis/MDAnalysis-0.10.0.tar.gz/MDAnalysis-0.10.0/MDAnalysis/coordinates/DMS.py
@@ -0,0 +1,117 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# MDAnalysis --- http://www.MDAnalysis.org
+# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver Beckstein
+# and contributors (see AUTHORS for the full list)
+#
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+# N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and O. Beckstein.
+# MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
+# J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
+#
+
+"""
+DESRES file format --- :mod:`MDAnalysis.coordinates.DMS`
+========================================================
+
+Classes to read DESRES_ Molecular Structure file format (DMS_)
+coordinate files (as used by the Desmond_ MD package).
+
+.. _DESRES: http://www.deshawresearch.com
+.. _Desmond: http://www.deshawresearch.com/resources_desmond.html
+.. _DMS: http://www.deshawresearch.com/Desmond_Users_Guide-0.7.pdf
+"""
+
+import numpy
+import sqlite3
+
+from . import base
+from MDAnalysis.coordinates.core import triclinic_box, triclinic_vectors
+
+
+class Timestep(base.Timestep):
+    def _init_unitcell(self):
+        return {'x': numpy.zeros(3),
+                'y': numpy.zeros(3),
+                'z': numpy.zeros(3)}
+
+    @property
+    def dimensions(self):
+        """unitcell dimensions (A, B, C, alpha, beta, gamma)"""
+        x = self._unitcell['x']
+        y = self._unitcell['y']
+        z = self._unitcell['z']
+        return triclinic_box(x, y, z)
+
+    @dimensions.setter
+    def dimensions(self, box):
+        x, y, z = triclinic_vectors(box)
+        cell = {'x': x, 'y': y, 'z': z}
+        self._unitcell = cell
+
+
+class DMSReader(base.SingleFrameReader):
+    """
+    Reads both coordinates and velocities.
+    """
+    format = 'DMS'
+    units = {'time': None, 'length': 'A', 'velocity': 'A/ps'}
+    _Timestep = Timestep
+
+    def get_coordinates(self, cur):
+        cur.execute('SELECT * FROM particle')
+        particles = cur.fetchall()
+        return [(p['x'], p['y'], p['z']) for p in particles]
+
+    def get_particle_by_columns(self, cur, columns=['x', 'y', 'z']):
+        cur.execute('SELECT * FROM particle')
+        particles = cur.fetchall()
+        return [tuple([p[c] for c in columns]) for p in particles]
+
+    def get_global_cell(self, cur):
+        cur.execute('SELECT * FROM global_cell')
+        rows = cur.fetchall()
+        assert len(rows) == 3
+        x = [row["x"] for row in rows]
+        y = [row["y"] for row in rows]
+        z = [row["z"] for row in rows]
+        return {'x': x, 'y': y, 'z': z}
+
+    def _read_first_frame(self):
+        coords_list = None
+        velocities_list = None
+        con = sqlite3.connect(self.filename)
+
+        def dict_factory(cursor, row):
+            d = {}
+            for idx, col in enumerate(cursor.description):
+                d[col[0]] = row[idx]
+            return d
+
+        with con:
+            # This will return dictionaries instead of tuples, when calling cur.fetch() or fetchall()
+            con.row_factory = dict_factory
+            cur = con.cursor()
+            coords_list = self.get_coordinates(cur)
+            velocities_list = self.get_particle_by_columns(cur, columns=['vx', 'vy', 'vz'])
+            unitcell = self.get_global_cell(cur)
+        assert coords_list
+        self.numatoms = len(coords_list)
+        coords_list = numpy.array(coords_list)
+        self.ts = self._Timestep(coords_list)
+        self.ts.frame = 1  # 1-based frame number
+        if velocities_list:
+            # TODO: use a Timestep that knows about velocities such as TRR.Timestep or better, TRJ.Timestep
+            velocities_arr = numpy.array(velocities_list, dtype=numpy.float32)
+            if numpy.any(velocities_arr):
+                self.ts._velocities = velocities_arr
+                self.convert_velocities_from_native(self.ts._velocities)  # converts nm/ps to A/ps units
+        # ts._unitcell layout is format dependent; Timestep.dimensions does the conversion
+        self.ts._unitcell = unitcell
+        if self.convert_units:
+            self.convert_pos_from_native(self.ts._pos)  # in-place !
+            self.convert_pos_from_native(self.ts._unitcell)  # in-place ! (all are lengths)
---
+++ packages/MDAnalysis/MDAnalysis-0.10.0.tar.gz/MDAnalysis-0.10.0/MDAnalysis/coordinates/xdrfile/XTC.py
@@ -0,0 +1,111 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# MDAnalysis --- http://www.MDAnalysis.org
+# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver Beckstein
+# and contributors (see AUTHORS for the full list)
+#
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+# N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and O. Beckstein.
+# MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
+# J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
+#
+
+"""
+Gromacs XTC trajectory I/O --- :mod:`MDAnalysis.coordinates.xdrfile.XTC`
+========================================================================
+
+Classes for reading and writing of `Gromacs XTC trajectories`_
+together with supporting code.
+
+.. note:: Users should access classes from :mod:`MDAnalysis.coordinates.XTC`.
+
+.. _Gromacs XTC trajectories: http://www.gromacs.org/Documentation/File_Formats/.xtc_File
+.. _Gromacs: http://www.gromacs.org
+
+
+.. SeeAlso:: :mod:`MDAnalysis.coordinates.xdrfile.libxdrfile2` for low-level
+   bindings to the Gromacs trajectory file formats
+
+Classes
+-------
+
+.. autoclass:: Timestep
+   :members:
+   :inherited-members:
+.. autoclass:: XTCReader
+   :members:
+   :inherited-members:
+.. autoclass:: XTCWriter
+   :members:
+   :inherited-members:
+"""
+
+import numpy
+import errno
+
+from . import statno
+from . import core
+from . import libxdrfile2
+from .core import Timestep
+
+
+class XTCWriter(core.TrjWriter):
+    """Write a Gromacs_ XTC trajectory.
+
+    .. _Gromacs: http://www.gromacs.org
+    """
+    format = "XTC"
+
+
+class XTCReader(core.TrjReader):
+    """Read Gromacs_ XTC trajectory.
+
+    .. _Gromacs: http://www.gromacs.org
+    """
+    format = "XTC"
+    _Timestep = Timestep
+    _Writer = XTCWriter
+
+    def _allocate_sub(self, DIM):
+        self._pos_buf = numpy.zeros((self._trr_numatoms, DIM), dtype=numpy.float32, order='C')
+        self._velocities_buf = None
+        self._forces_buf = None
+
+    def _read_trj_natoms(self, filename):
+        return libxdrfile2.read_xtc_natoms(filename)
+
+    def _read_trj_numframes(self, filename):
+        self._numframes, self._offsets = libxdrfile2.read_xtc_numframes(filename)
+        self._store_offsets()
+
+    def _read_next_timestep(self, ts=None):
+        if ts is None:
+            ts = self.ts
+
+        if self.xdrfile is None:
+            self.open_trajectory()
+
+        if self._sub is None:
+            ts.status, ts.step, ts.time, ts.prec = libxdrfile2.read_xtc(self.xdrfile, ts._unitcell, ts._pos)
+        else:
+            ts.status, ts.step, ts.time, ts.prec = libxdrfile2.read_xtc(self.xdrfile, ts._unitcell, self._pos_buf)
+            ts._pos[:] = self._pos_buf[self._sub]
+
+        if ts.status == libxdrfile2.exdrENDOFFILE:
+            raise IOError(errno.EIO, "End of file reached for %s file" % self.format,
+                          self.filename)
+        elif not ts.status == libxdrfile2.exdrOK:
+            raise IOError(errno.EBADF, "Problem with %s file, status %s" %
+                                       (self.format, statno.ERRORCODE[ts.status]), self.filename)
+
+        if self.convert_units:
+            self.convert_pos_from_native(ts._pos)  # in-place !
+            self.convert_pos_from_native(ts._unitcell)  # in-place ! (note: xtc contain unit vecs!)
+            ts.time = self.convert_time_from_native(ts.time)  # in-place does not work with scalars
+
+        ts.frame += 1
+        return ts
