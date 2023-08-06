433d9feda1419afecab553827cee3537500cbf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:49 +0000

    Add package readlike/0.1.0/readlike-0.1.0.tar.gz

---
+++ packages/readlike/readlike-0.1.0.tar.gz/readlike-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='readlike',
+    version='0.1.0',
+    description='GNU Readline-like line editing module',
+    long_description=long_description,
+    url='http://jangler.info/code/readlike',
+    author='Brandon Mulcahy',
+    author_email='brandon@jangler.info',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+    ],
+    keywords=['line editing', 'readlike', 'readline', 'urwid'],
+)
