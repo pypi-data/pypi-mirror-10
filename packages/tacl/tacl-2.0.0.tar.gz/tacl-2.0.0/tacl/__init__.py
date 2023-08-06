f6c8db4943c0fb46252f90703f90c3a02b5cac4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:52 +0000

    Add package tacl/2.0.0/tacl-2.0.0.tar.gz

---
+++ packages/tacl/tacl-2.0.0.tar.gz/tacl-2.0.0/tacl/__init__.py
@@ -0,0 +1,12 @@
+from . import constants
+from .catalogue import Catalogue
+from .corpus import Corpus
+from .data_store import DataStore
+from .highlighter import Highlighter
+from .report import Report
+from .sequence import Sequencer
+from .statistics_report import StatisticsReport
+from .stripper import Stripper
+from .tei_corpus import TEICorpus
+from .text import BaseText, Text
+from .tokenizer import Tokenizer
