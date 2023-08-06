72daaa8981288f03f9539aa2a158565be9f77860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package PageCalc/0.2.3/PageCalc-0.2.3.tar.gz

---
+++ packages/PageCalc/PageCalc-0.2.3.tar.gz/PageCalc-0.2.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+@author: saaj
+'''
+
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+
+setup(
+  name             = 'PageCalc',
+  version          = '0.2.3',
+  author           = 'saaj',
+  author_email     = 'mail@saaj.me',
+  packages         = ['pagecalc'],
+  package_data     = {'pagecalc' : ['example/*']},
+  test_suite       = 'pagecalc.test',
+  url              = 'https://bitbucket.org/saaj/pagecalc',
+  license          = 'LGPL-2.1+',
+  description      = 'Python pagination calculator',
+  long_description = open('README.txt', 'rb').read().decode('utf-8'),
+  platforms        = ['Any'],
+  keywords         = 'python pagination',
+  classifiers      = [
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Intended Audience :: Developers'
+  ]
+)
+
