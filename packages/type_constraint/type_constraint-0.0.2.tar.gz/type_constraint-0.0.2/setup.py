221499cd3f67a735c66a8be981fbd9a8910d7235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:59 +0000

    Add package type_constraint/0.0.2/type_constraint-0.0.2.tar.gz

---
+++ packages/type_constraint/type_constraint-0.0.2.tar.gz/type_constraint-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='type_constraint',
+    version='0.0.2',
+    py_modules=['type_constraint'],
+    author='timchow', 
+    author_email='jingjiang@staff.sina.com.cn',
+    license='LGPL',
+    description='a type constraint tool for python function',
+    keywords='type constraint',
+    url='http://tieba.baidu.com/f?ie=utf-8&kw=%E5%91%A8%E4%BA%95%E6%B1%9F',
+    long_description=open('README.txt').read(),
+)
