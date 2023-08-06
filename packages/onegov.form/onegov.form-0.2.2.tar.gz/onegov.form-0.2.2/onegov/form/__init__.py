aecfa333d509b1345e5261a33d81d1b9f87b5573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package onegov.form/0.2.2/onegov.form-0.2.2.tar.gz

---
+++ packages/onegov.form/onegov.form-0.2.2.tar.gz/onegov.form-0.2.2/onegov/form/__init__.py
@@ -0,0 +1,11 @@
+from onegov.form.collection import FormCollection
+from onegov.form.core import Form, with_options
+from onegov.form.models import FormDefinition, FormSubmission
+
+__all__ = [
+    'Form',
+    'FormCollection',
+    'FormDefinition',
+    'FormSubmission',
+    'with_options'
+]
