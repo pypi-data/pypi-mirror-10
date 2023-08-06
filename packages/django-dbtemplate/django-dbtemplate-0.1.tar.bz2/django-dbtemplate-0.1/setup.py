7ac6df0620568e134fc84e4d883e5429f88bf2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:56 +0000

    Add package django-dbtemplate/0.1/django-dbtemplate-0.1.tar.bz2

---
+++ packages/django-dbtemplate/django-dbtemplate-0.1.tar.bz2/django-dbtemplate-0.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+requires = [
+    'django',
+    'pyyaml',
+]
+
+develop_requires = [
+    'mixer',
+]
+
+
+def long_description():
+    curdir = os.path.dirname(__file__)
+    readme = os.path.join(curdir, "README.rst")
+    with open(readme) as f:
+        return f.read()
+
+
+setup(
+    name='django-dbtemplate',
+    author='Matvey Kruglov',
+    author_email='kubuzzzz@gmail.com',
+    version='0.1',
+    description='Allow to override templates from admin interface',
+    long_description=long_description(),
+    packages=find_packages(exclude=[
+        'tests',
+        'dbtemplate.tests',
+        'dbtemplate.tests.*',
+    ]),
+    include_package_data=True,
+    install_requires=requires,
+    extras_require={
+        'develop': develop_requires,
+    },
+    zip_safe=False,
+    test_suite='tests.main',
+)
