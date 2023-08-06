49ade405b29e6b91678d4d11b53d9491361bc985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:53 +0000

    Add package findspark/0.0.3/findspark-0.0.3-py2.py3-none-any.whl

---
+++ packages/findspark/findspark-0.0.3-py2.py3-none-any.whl/findspark.py
@@ -0,0 +1,60 @@
+"""Find spark home, and initialize by adding pyspark to sys.path.
+
+If SPARK_HOME is defined, it will be used to put pyspark on sys.path.
+Otherwise, common locations for spark (currently only Homebrew's default) will be searched.
+"""
+
+from glob import glob
+import os
+import sys
+
+__version__ = '0.0.3'
+
+
+def find():
+    """Find a local spark installation.
+
+    Will first check the SPARK_HOME env variable, and otherwise
+    search common installation locations, e.g. from homebrew
+    """
+    spark_home = os.environ.get('SPARK_HOME', None)
+
+    if not spark_home:
+        for path in [
+            '/usr/local/opt/apache-spark/libexec', # OS X Homebrew
+            # Any other common places to look?
+        ]:
+            if os.path.exists(path):
+                spark_home = path
+                break
+
+    if not spark_home:
+        raise ValueError("Couldn't find Spark, make sure SPARK_HOME env is set"
+                         " or Spark is in an expected location (e.g. from homebrew installation).")
+
+    return spark_home
+
+
+def init(spark_home=None):
+    """Make pyspark importable.
+
+    Sets environmental variables and adds dependencies to sys.path.
+    If no Spark location is provided, will try to find an installation.
+
+    Parameters
+    ----------
+    spark_home : str, optional, default = None
+        Path to Spark installation, will try to find automatically
+        if not provided
+    """
+
+    if not spark_home:
+        spark_home = find()
+
+    # ensure SPARK_HOME is defined
+    os.environ['SPARK_HOME'] = spark_home
+
+    # add pyspark to sys.path
+    spark_python = os.path.join(spark_home, 'python')
+    py4j = glob(os.path.join(spark_python, 'lib', 'py4j-*.zip'))[0]
+    sys.path[:0] = [spark_python, py4j]
