ad99dda2e231bc2a6977630fc4675488d3de6374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:24 +0000

    Add package horse-nonsense/0.1.1/horse-nonsense-0.1.1.tar.gz

---
+++ packages/horse-nonsense/horse-nonsense-0.1.1.tar.gz/horse-nonsense-0.1.1/setup.py
@@ -0,0 +1,57 @@
+"""A setuptools based setup module for horse-nonsense.
+
+https://github.com/grieve/horse-nonsense
+"""
+
+from os import path
+from setuptools import setup
+from setuptools import find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='horse-nonsense',
+    version='0.1.1',
+
+    description='A chat bot framework for Slack',
+    long_description="""
+""",
+
+    url='https://github.com/grieve/horse-nonsense',
+
+    author='Ryan Grieve',
+    author_email='me@ryangrieve.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Chat',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='chatbot slack chat bot communication nonsense',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        'pillow'
+    ],
+
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+
+    package_data={},
+    data_files=[],
+    entry_points={},
+)
