c2a3287b1be4a4dd845fa3605cb403dec25ac45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:45 +0000

    Add package neuronunit/0.1.3.1/neuronunit-0.1.3.1.tar.gz

---
+++ packages/neuronunit/neuronunit-0.1.3.1.tar.gz/neuronunit-0.1.3.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+	name='neuronunit',
+	version='0.1.3.1',
+	author='Rick Gerkin',
+	author_email='rgerkin@asu.edu',
+        packages=[
+            'neuronunit',
+            'neuronunit.capabilities',
+            'neuronunit.neuroconstruct',
+            'neuronunit.models',
+            'neuronunit.tests'],
+	url='http://github.com/scidash/neuronunit',
+	license='MIT',
+	description='A SciUnit library for data-driven testing of single-neuron physiology models.',
+	long_description="",
+	install_requires=['sciunit>=0.1.3.1','numpy','scipy','neo']
+)
