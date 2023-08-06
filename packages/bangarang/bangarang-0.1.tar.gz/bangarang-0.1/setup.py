9f68fdd25f9d13f6b5f3de467510ed96714c431d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:48 +0000

    Add package bangarang/0.1/bangarang-0.1.tar.gz

---
+++ packages/bangarang/bangarang-0.1.tar.gz/bangarang-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "bangarang",
+    packages = ["bangarang"],
+    version = "0.1",
+    description = "Offical client library for the bangarang monitoring platform",
+    author = "Eliot Hedeman",
+    author_email = "eliot.d.hedeman@gmail.com",
+    url = "https://github.com/eliothedeman/python-bangarang",
+    download_url = "https://github.com/eliothedeman/python-bangarang/tarball/0.1",
+    keywords = ["bangarang", "monitoring", "analytics"],
+    classifiers = [],
+)
