86f9db1b1ca70ed82a04853ca94cf3b12b7642ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package cloudmesh/2.3.2/cloudmesh-2.3.2.tar.bz2

---
+++ packages/cloudmesh/cloudmesh-2.3.2.tar.bz2/cloudmesh-2.3.2/fabfile/__init__.py
@@ -0,0 +1,35 @@
+print("CLEAN")
+import clean
+print("SERVER")
+import server
+print("BUILD")
+import build
+import git
+import pep8
+import pypi
+import doc
+import mq
+import test
+import nose
+import mongo
+import iptable
+import tunnel
+import user
+import mooc
+import hpc
+import qc
+import manage
+import india
+import ipython
+import security
+
+try:
+    import queue
+except Exception, e:
+    import sys
+    import traceback
+    print "ERROR: failed to load queue fabfile"
+    print e
+    print '-' * 60
+    traceback.print_exc(file=sys.stdout)
+    print '-' * 60
