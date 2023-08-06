30406d136266050f02e4980a5add4473a5941375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:52 +0000

    Add package clusterpoint-api/0.3.0/clusterpoint-api-0.3.0.tar.gz

---
+++ packages/clusterpoint-api/clusterpoint-api-0.3.0.tar.gz/clusterpoint-api-0.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='clusterpoint-api',
+    version='0.3.0',
+    author='Peteris Krisjanis',
+    author_email='peteris.krisjanis@clusterpoint.com',
+    packages=['clusterpoint-api'],
+    url='https://github.com/clusterpoint/python-client-api',
+    license='GNU Affero General Public License',
+    description='Python API for Clusterpoint Database',
+    long_description=open('README').read(),
+)
