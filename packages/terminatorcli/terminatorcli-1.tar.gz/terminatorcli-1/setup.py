08c88d560fdb30c191fd6cc35cac3c44303aa19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package terminatorcli/1/terminatorcli-1.tar.gz

---
+++ packages/terminatorcli/terminatorcli-1.tar.gz/terminatorcli-1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='terminatorcli',
+      version='1',
+      description='command line autocomplete',
+      url='https://github.com/FranklinInnovationLab/terminator',
+      author='jason xie',
+      author_email='jxieeducation@berkeley.edu',
+      license='MIT',
+      py_modules=['terminatorcli'],
+      scripts = ['terminator'])
+
