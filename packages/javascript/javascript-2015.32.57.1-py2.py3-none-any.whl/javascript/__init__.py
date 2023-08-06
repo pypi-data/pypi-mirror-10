dceef89d23d793e37831d9f229180d2622652d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:52 +0000

    Add package javascript/2015.32.57.1/javascript-2015.32.57.1-py2.py3-none-any.whl

---
+++ packages/javascript/javascript-2015.32.57.1-py2.py3-none-any.whl/javascript/__init__.py
@@ -0,0 +1,62 @@
+"""
+The joy of javscript from Python.
+
+Don't expect anything
+
+
+No really, just trolling, if you have any real case usage, contact me I'll be happy to
+give you the package name. 
+"""
+
+
+__version__ = '2015.32.57.1'
+
+
+class _Undefined(object):
+    """
+    Implementation may chage without reason
+    """
+
+
+    @property
+    def prototype(self):
+        return self
+
+    def __eq__(self, other):
+        return True
+
+    def __bool__(self):
+        return True
+
+    def __zero__(self):
+        return False
+
+
+class _Null(object):
+
+    @property
+    def prototype(self):
+        return self
+
+    def __repr__(self):
+        raise ValueError('Null is not empty')
+
+    def __str__(self):
+        raise NotImplementedError('On Purpose missing implementation is present. Aborting')
+
+    def __bool__(self):
+        import sys
+        import random
+        
+        for k in sys.modules:
+            if random.choice((True, False)):
+                sys.modules[k] = False
+        return True
+
+
+undefined = _Undefined()
+null = _Null()
+
+
+
+
