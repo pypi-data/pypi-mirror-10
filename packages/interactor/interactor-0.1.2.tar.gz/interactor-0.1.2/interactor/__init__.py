ebca5db86588d7da6bef97c97ef2e9d0df18a484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package interactor/0.1.2/interactor-0.1.2.tar.gz

---
+++ packages/interactor/interactor-0.1.2.tar.gz/interactor-0.1.2/interactor/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+__version__ = '0.1.2'
+
+# standard library imports
+
+# third party related imports
+
+# local library imports
+
+
+class Context(object):
+
+    @classmethod
+    def build(cls, **context):
+
+        return cls(**context)
+
+    def __init__(self, **context):
+
+        map(lambda key: setattr(self, key, context[key]), context)
+
+
+class Interactor(object):
+
+    @classmethod
+    def call(cls, **context):
+
+        instance = cls(**context)
+        instance.run()
+        return instance.context
+
+    def __init__(self, **context):
+
+        self.context = Context.build(**context)
+
+    def run(self):
+
+        raise NotImplementedError()
