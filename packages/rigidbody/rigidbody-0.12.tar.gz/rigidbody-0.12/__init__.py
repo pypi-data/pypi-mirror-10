50be6aa48bfb4f15a046ec76a7dc91afda6d3091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:52 +0000

    Add package rigidbody/0.12/rigidbody-0.12.tar.gz

---
+++ packages/rigidbody/rigidbody-0.12.tar.gz/rigidbody-0.12/__init__.py
@@ -0,0 +1,5 @@
+from .arithmetic import \
+	pr, unpr, normalized, unreduce, unreduce_2d, sumsq, skew, \
+	unit, orthonormalize, minmedmax, cis, dots
+
+from .transform import SO3, SE3
---
+++ packages/rigidbody/rigidbody-0.12.tar.gz/rigidbody-0.12/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='rigidbody',
+      description='Rigid body transforms in 3D',
+      version='0.12',
+      author='Alex Flint',
+      author_email='alex.flint@gmail.com',
+      url='https://github.com/alexflint/rigidbody',
+      packages=['rigidbody'],
+      package_dir={'rigidbody': '.'},
+      )
