7968fc72af6e8b2fd11634a938b70398fd2990b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:53 +0000

    Add package flask-spawn/0.1.4/flask-spawn-0.1.4.tar.gz

---
+++ packages/flask-spawn/flask-spawn-0.1.4.tar.gz/flask-spawn-0.1.4/flaskspawn/spawn.py
@@ -0,0 +1,33 @@
+from cookiecutter.main import cookiecutter
+from .snippets.snippets import add_database_files
+import click
+import os
+
+@click.group()
+def spawn():
+    pass
+
+@spawn.command()
+@click.option('-t', '--type', type=click.Choice(['simple-frontend', 'simple-api']), default='simple-frontend')
+@click.option('-d', '--database', is_flag=True, help="use if you want to add code for database migrations")
+@click.option('-b', '--blueprint', multiple=True)
+@click.option('-v', '--view', multiple=True)
+@click.argument('appname', required=True)
+def new(appname, type, database, blueprint, view):
+    base_directory = os.path.dirname(os.path.abspath(__file__))
+
+    if type == 'simple-frontend':
+        cookiecutter_path = '{0}/cookiecutters/simple-frontend/'.format(base_directory)
+
+    cookiecutter(cookiecutter_path,
+                 no_input=True,
+                 extra_context={'repo_name': '{0}'.format(appname)})
+
+    if database:
+        add_database_files(appname, base_directory)
+
+    if blueprint:
+        click.echo('\n'.join(blueprint))
+
+    if view:
+        click.echo('\n'.join(view))
