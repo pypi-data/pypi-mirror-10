e8a27e4ef923874b769bfc203177eef7dbd6fe8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:26 +0000

    Add package PyHighcharts/2.0/PyHighcharts-2.0.tar.gz

---
+++ packages/PyHighcharts/PyHighcharts-2.0.tar.gz/PyHighcharts-2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name="PyHighcharts",
+    version="2.0",
+    package_dir="",
+    packages=['pyhighcharts'],
+
+    install_requires=[],
+    package_data={},
+
+    # metadata for upload to PyPI
+    author="fin.yeates",
+    author_email="findlay.yeates@deitek.co.uk",
+    description="A python wrapper for the highcharts charting library",
+    license="PSF",
+    keywords=["PyHighcharts", "highcharts"],
+    url="https://github.com/fidyeates/PyHighcharts/tree/dev",
+    download_url="https://github.com/fidyeates/PyHighcharts/tarball/2.0"
+)
