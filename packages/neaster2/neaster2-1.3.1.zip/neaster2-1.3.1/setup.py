70702095a2344b01fdc8a4344e4cf1cfea54e9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:18 +0000

    Add package neaster2/1.3.1/neaster2-1.3.1.zip

---
+++ packages/neaster2/neaster2-1.3.1.zip/neaster2-1.3.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='neaster2',
+    version='1.3.1',
+    author='yt',
+    author_email='951815484@qq.com',
+    url='http://www.heafirstlabs.com',
+    )
