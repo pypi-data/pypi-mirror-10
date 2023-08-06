349e5d0996358741e5ca120c1d4a86acdb3f2293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:59 +0000

    Add package pyagentx/0.2/pyagentx-0.2.tar.gz

---
+++ packages/pyagentx/pyagentx-0.2.tar.gz/pyagentx-0.2/pyagentx/agent.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+import Queue
+
+import pyagentx
+from pyagentx.updater import Updater
+from pyagentx.network import Network
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+logger = logging.getLogger('pyagentx.agent')
+logger.addHandler(NullHandler())
+
+class AgentError(Exception):
+    pass
+
+class Agent(object):
+
+    def __init__(self):
+        self._updater_list = []
+        self._threads = []
+
+    def register(self, oid, class_, freq=10):
+        if Updater not in class_.__bases__:
+            raise AgentError('Class given isn\'t an updater')
+        self._updater_list.append({'oid':oid, 'class':class_, 'freq':freq})
+
+    def setup(self):
+        # Override this
+        pass
+
+    def start(self):
+        queue = Queue.Queue(maxsize=20)
+        self.setup()
+        # Start Updaters
+        for u in self._updater_list:
+            logger.debug('Starting updater [%s]' % u['oid'])
+            t = u['class']()
+            t.agent_setup(queue, u['oid'], u['freq'])
+            t.start()
+            self._threads.append(t)
+        # Start Network
+        oid_list = [u['oid'] for u in self._updater_list]
+        t = Network(queue, oid_list)
+        t.start()
+        self._threads.append(t)
+        # Do nothing ... just wait for someone to stop you
+        while True:
+            #logger.debug('Agent Sleeping ...')
+            time.sleep(1)
+
+    def stop(self):
+        logger.debug('Stop threads')
+        for t in self._threads:
+            t.stop.set()
+        logger.debug('Wait for updater')
+        for t in self._threads:
+            t.join()
+
