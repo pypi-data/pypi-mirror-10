02d06016a8208312cf7b32829fbd0e431e52bc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:27 +0000

    Add package secret/0.4.2/secret-0.4.2.tar.gz

---
+++ packages/secret/secret-0.4.2.tar.gz/secret-0.4.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='secret',
+    version='0.4.2',
+    description='Manage secrets',
+    keywords = 'secret secrets project vault',
+    author = 'Jussi Vaihia',
+    author_email = 'jussi.vaihia@futurice.com',
+    license='Apache2',
+    url='https://github.com/futurice/secret',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    install_requires=['boto3>=0.0.18', 'pycryptodome>=3.1'],
+    packages = ["secret"],
+    include_package_data = True,
+    entry_points={
+        'console_scripts': [
+            'secret = secret.secret:runner'
+            ]
+        }
+)
