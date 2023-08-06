b8761849a6b8bce6507d8b1f12573e571a77cd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package asyncio_pinger/0.0.4/asyncio-pinger-0.0.4.tar.gz

---
+++ packages/asyncio_pinger/asyncio-pinger-0.0.4.tar.gz/asyncio-pinger-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='asyncio-pinger',
+    version='0.0.4',
+    author='Maxim Krivodaev',
+    author_email='maxim.krivodaev@gmail.com',
+    packages=['asyncio_pinger', ],
+    scripts=[],
+    url='https://github.com/lmasikl/asyncio_pinger',
+    license='LICENSE.txt',
+    description='Ping based on asyncio.',
+    long_description='',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
