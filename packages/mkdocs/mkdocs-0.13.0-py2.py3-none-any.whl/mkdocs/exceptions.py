d9bb2e3c63835ec67892567a4562697437744aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package mkdocs/0.13.0/mkdocs-0.13.0-py2.py3-none-any.whl

---
+++ packages/mkdocs/mkdocs-0.13.0-py2.py3-none-any.whl/mkdocs/exceptions.py
@@ -0,0 +1,13 @@
+from click import ClickException
+
+
+class MkDocsException(ClickException):
+    """Base exceptions for all MkDocs Exceptions"""
+
+
+class ConfigurationError(MkDocsException):
+    """Error in configuration"""
+
+
+class MarkdownNotFound(MkDocsException):
+    """A linked local Markdown file isn't found in the table of contents."""
