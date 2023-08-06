0e5e36fb9c5335e8359763630c8eda9f5a26043e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:06 +0000

    Add package tlogger/0.2.0/tlogger-0.2.0.tar.gz

---
+++ packages/tlogger/tlogger-0.2.0.tar.gz/tlogger-0.2.0/tlogger/__init__.py
@@ -0,0 +1,3 @@
+# -*- mode: python; coding: utf-8; -*-
+
+from .logger import get_logger  # nopep8
---
+++ packages/tlogger/tlogger-0.2.0.tar.gz/tlogger-0.2.0/tlogger/utils.py
@@ -0,0 +1,34 @@
+# -*- mode: python; coding: utf-8; -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from datetime import datetime
+from logging import getLogger, basicConfig
+from uuid import uuid4
+
+
+def create_ts():
+    return datetime.utcnow()
+
+
+def create_logger(name):
+    basicConfig(level=0)
+    logger = getLogger(name)
+    return logger
+
+
+def create_guid():
+    return str(uuid4())
+
+
+def is_descriptor(obj):
+    if (
+        hasattr(obj, '__get__') or
+        hasattr(obj, '__set__') or
+        hasattr(obj, '__delete__')
+    ):
+            return True
+    return False
