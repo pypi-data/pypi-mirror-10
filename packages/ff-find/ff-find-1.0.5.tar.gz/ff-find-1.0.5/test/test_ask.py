bd29ee934a0a4dbb41274a9578946af446cf27a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:29 +0000

    Add package ff-find/1.0.5/ff-find-1.0.5.tar.gz

---
+++ packages/ff-find/ff-find-1.0.5.tar.gz/ff-find-1.0.5/test/test_ask.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, division
+
+try:
+    from io import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from test_manager import *
+
+from ff import utils
+
+
+_set_input__prompt = None
+def set_input(*a):
+    a = list(a)
+    def _(prompt):
+        global _set_input__prompt
+        _set_input__prompt = prompt
+        return a.pop(0)
+    try:
+        raw_input
+    except NameError:
+        utils.input = _
+    else:
+        utils.raw_input = _
+
+
+class TestAsk(unittest.TestCase):
+    def test_ask_simple(self):
+        question = 'My question'
+
+        set_input('n')
+        ret = utils.ask(question, 'yn')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'n')
+
+        set_input('y')
+        ret = utils.ask(question, 'yn')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'y')
+
+    def test_ask_with_one_wrong_answer(self):
+        question = 'My question'
+
+        set_input('w', 'y')
+        ret = utils.ask(question, 'yn')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'y')
+
+    def test_ask_with_uppercase_replies(self):
+        question = 'My question'
+
+        set_input('n')
+        ret = utils.ask(question, 'YN')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'n')
+
+        set_input('y')
+        ret = utils.ask(question, 'YN')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'y')
+
+    def test_ask_with_uppercase_answer(self):
+        question = 'My question'
+
+        set_input('N')
+        ret = utils.ask(question, 'yn')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'n')
+
+        set_input('Y')
+        ret = utils.ask(question, 'yn')
+        self.assertEqual(_set_input__prompt, question + ' (n,y) ')
+        self.assertEqual(ret, 'y')
+
+    def test_ask_with_default_answer_replied(self):
+        question = 'My question'
+
+        set_input('y')
+        ret = utils.ask(question, 'yn', 'y')
+        self.assertEqual(_set_input__prompt, question + ' (n,Y) ')
+        self.assertEqual(ret, 'y')
+
+        set_input('N')
+        ret = utils.ask(question, 'YN', 'n')
+        self.assertEqual(_set_input__prompt, question + ' (N,y) ')
+        self.assertEqual(ret, 'n')
+
+    def test_ask_with_default_answer_no_replied(self):
+        question = 'My question'
+
+        set_input('')
+        ret = utils.ask(question, 'yn', 'y')
+        self.assertEqual(_set_input__prompt, question + ' (n,Y) ')
+        self.assertEqual(ret, 'y')
+
+    def test_ask_multiplied_single_answer(self):
+        question = 'My question'
+
+        set_input('')
+        ret = utils.ask(question, 'yny', 'y')
+        self.assertEqual(_set_input__prompt, question + ' (n,Y) ')
+        self.assertEqual(ret, 'y')
+
+    def test_ask_with_default_value_not_in_answers(self):
+        question = 'My question'
+
+        set_input('')
+        ret = utils.ask(question, 'yn', 'w')
+        self.assertEqual(_set_input__prompt, question + ' (n,W,y) ')
+        self.assertEqual(ret, 'w')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/ff-find/ff-find-1.0.5.tar.gz/ff-find-1.0.5/test/test_prepare_pattern_fnmatch.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, division
+
+import re
+
+from test_manager import *
+
+from ff import pattern
+
+
+RE_TYPE = type(re.compile(''))
+
+
+class TestPatternCompileFnmatch(unittest.TestCase):
+    def test_empty(self):
+        pat = pattern.Pattern()
+        pat.pattern = ''
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE)
+        self.assertEqual(parsed_pat.pattern, r'()')
+
+    def test_empty_pattern_fnmatch_begin(self):
+        pat = pattern.Pattern()
+        pat.pattern = ''
+        pat.fnmatch_begin = True
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE)
+        self.assertEqual(parsed_pat.pattern, r'(\A)')
+
+    def test_empty_pattern_fnmatch_end(self):
+        pat = pattern.Pattern()
+        pat.pattern = ''
+        pat.fnmatch_end = True
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE | re.DOTALL | re.MULTILINE)
+        self.assertTrue(re.match(r'\( \\Z (?: \( \? [a-z]+ \) )? \)$', parsed_pat.pattern, re.VERBOSE), 'Pattern doesn\'t match: %s' % parsed_pat.pattern)
+
+    def test_empty_pattern_ignorecase(self):
+        pat = pattern.Pattern()
+        pat.pattern = ''
+        pat.ignorecase = True
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE | re.IGNORECASE)
+        self.assertEqual(parsed_pat.pattern, r'()')
+
+    def test_simple_pattern(self):
+        pat = pattern.Pattern()
+        pat.pattern = 'asd'
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE)
+        self.assertEqual(parsed_pat.pattern, r'(asd)')
+
+    def test_pattern_with_regexp(self):
+        pat = pattern.Pattern()
+        pat.pattern = r'^asd$'
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE)
+        self.assertEqual(parsed_pat.pattern, r'(\^asd\$)')
+
+    def test_pattern_with_wildcard_single(self):
+        pat = pattern.Pattern()
+        pat.pattern = r'asd?asd'
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE)
+        self.assertEqual(parsed_pat.pattern, r'(asd.asd)')
+
+    def test_pattern_with_wildcard_multi(self):
+        pat = pattern.Pattern()
+        pat.pattern = r'asd*asd'
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE)
+        self.assertEqual(parsed_pat.pattern, r'(asd.*asd)')
+
+    def test_pattern_with_wildcard_multi_and_fnmatch_end(self):
+        pat = pattern.Pattern()
+        pat.pattern = r'asd*asd'
+        pat.fnmatch_end = True
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE | re.MULTILINE | re.DOTALL)
+        self.assertTrue(re.match(r'^ \( asd \. \* asd \\Z ( \( \?[a-z]+ \) )? \) $', parsed_pat.pattern, re.VERBOSE), 'Pattern doesn\'t match: %s' % parsed_pat.pattern)
+
+    def test_full_options(self):
+        pat = pattern.Pattern()
+        pat.pattern = r'asd*asd'
+        pat.fnmatch_begin = True
+        pat.fnmatch_end = True
+        pat.ignorecase = True
+        parsed_pat = pat._prepare_pattern__compile_fnmatch()
+
+        self.assertIsInstance(parsed_pat, RE_TYPE)
+
+        self.assertEqual(parsed_pat.flags, re.UNICODE | re.MULTILINE | re.DOTALL | re.IGNORECASE)
+        self.assertTrue(re.match(r'^ \( \\A asd \. \* asd \\Z ( \( \?[a-z]+ \) )? \) $', parsed_pat.pattern, re.VERBOSE), 'Pattern doesn\'t match: %s' % parsed_pat.pattern)
+
+
+if __name__ == '__main__':
+    unittest.main()
