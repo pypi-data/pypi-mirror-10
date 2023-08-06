df62d1926a7cc9b401fa04602335af6b0b3ba3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:13 +0000

    Add package dju-intranet/0.1.3/dju-intranet-0.1.3.tar.gz

---
+++ packages/dju-intranet/dju-intranet-0.1.3.tar.gz/dju-intranet-0.1.3/setup.py
@@ -0,0 +1,36 @@
+import os.path
+
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import find_packages, setup
+
+
+def readfile(filename):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+            return f.read()
+    except (IOError, OSError):
+        return ''
+
+
+install_reqs = [
+    'lxml>=3.4.0',
+    'requests>2.4.3',
+]
+
+
+setup(
+    name='dju-intranet',
+    version='0.1.3',
+    description='Daejeon university intranet API',
+    long_description=readfile('README.rst'),
+    url='https://github.com/Kjwon15/dju-intranet',
+    download_url='https://github.com/Kjwon15/dju-intranet/releases',
+    author='Kjwon15',
+    author_email='kjwonmail@gmail.com',
+    packages=find_packages(),
+    install_requires=install_reqs,
+)
