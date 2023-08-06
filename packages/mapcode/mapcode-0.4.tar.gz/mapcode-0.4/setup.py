54e0a6c8d96297911c0d3fd48aeb729debd23815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:39 +0000

    Add package mapcode/0.4/mapcode-0.4.tar.gz

---
+++ packages/mapcode/mapcode-0.4.tar.gz/mapcode-0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup, Extension
+
+
+setup(
+    name='mapcode',
+    ext_modules=[Extension('mapcode',
+                           sources=['mapcodemodule.c', 'mapcodelib/mapcoder.c'],
+                           include_dirs=['mapcodelib']
+                           )],
+    # version number format is clibrary - python
+    version='0.4',
+    description='A Python module to do mapcode encoding and decoding.  See http://www.mapcode.com for more information.',
+    author='Erik Bos',
+    author_email='erik@xs4all.nl',
+    url='https://github.com/mapcode-foundation/mapcode-python',
+    download_url='https://github.com/mapcode-foundation/mapcode-python/tarball/v0.4',
+    license='Apache License 2.0',
+    classifiers=[
+       'Development Status :: 4 - Beta',
+       'License :: OSI Approved :: Apache Software License'
+       ],
+)
