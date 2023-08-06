b50ed7c271d35c045ba520c6f64eb199954b8b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:47 +0000

    Add package pyMorfologik/0.1.4/pyMorfologik-0.1.4.zip

---
+++ packages/pyMorfologik/pyMorfologik-0.1.4.zip/pyMorfologik-0.1.4/setup.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='pyMorfologik',
+    packages=['pyMorfologik'],
+    version='0.1.4',
+    install_requires=[],
+    author='Damian Mirecki',
+    author_email='dmirecki.dm@gmail.com',
+    description='Binding for Morfologik tool',
+    url='https://github.com/dmirecki/pyMorfologik',
+    keywords=['morfologik']
+)
