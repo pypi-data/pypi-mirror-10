6b03c307e6b257ab2a73487621e4e2d898b0194a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:32 +0000

    Add package leonardo-import-export/2015.7.1/leonardo-import-export-2015.7.1.tar.gz

---
+++ packages/leonardo-import-export/leonardo-import-export-2015.7.1.tar.gz/leonardo-import-export-2015.7.1/leonardo_import_export/__init__.py
@@ -0,0 +1,19 @@
+
+from django.apps import AppConfig
+
+default_app_config = 'leonardo_import_export.Config'
+
+
+class Default(object):
+
+    apps = [
+        'import_export',
+        'leonardo_import_export'
+    ]
+
+
+class Config(AppConfig, Default):
+    name = 'leonardo_import_export'
+    verbose_name = "Import & Export"
+
+default = Default()
