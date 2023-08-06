79d98395a7df4cd9f0fc3912bef7ae617f5a9302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:51 +0000

    Add package redis-gadgets/0.1.4/redis_gadgets-0.1.7.tar.gz

---
+++ packages/redis-gadgets/redis_gadgets-0.1.7.tar.gz/redis_gadgets-0.1.7/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+# http://bugs.python.org/issue8876
+try:
+    import os
+    del os.link
+except:
+    pass
+
+setup(
+    name='redis_gadgets',
+    author='Keep.com development team',
+    author_email='opensource@keep.com',
+    version='0.1.7',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    url='http://keep.com',
+    license='MIT',
+    description='Light-weight tools to implement high-level features in Redis',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    install_requires=open('requirements.txt').readlines()
+)
