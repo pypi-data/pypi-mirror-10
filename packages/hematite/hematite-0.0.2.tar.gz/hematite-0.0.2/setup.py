61368ca0cb10f43cfab2d1c89002e1f0433796c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:56 +0000

    Add package hematite/0.0.2/hematite-0.0.2.tar.gz

---
+++ packages/hematite/hematite-0.0.2.tar.gz/hematite-0.0.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(name='hematite',
+          version='0.0.2',
+          author='Mahmoud Hashemi and Mark Williams',
+          url='https://github.com/mahmoud/hematite',
+          install_requires=[],
+          packages=find_packages(),
+          license='BSD',
+          classifiers=['Intended Audience :: Developers',
+                       'Topic :: Internet :: WWW/HTTP',
+                       'Topic :: Internet :: WWW/HTTP :: HTTP Servers'])
