5162f455a6470a1e451fb56023b061fbc929768a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package dexterity.localrolesfield/0.2.1/dexterity.localrolesfield-0.2.1.zip

---
+++ packages/dexterity.localrolesfield/dexterity.localrolesfield-0.2.1.zip/dexterity.localrolesfield-0.2.1/CHANGES.rst
@@ -0,0 +1,28 @@
+Changelog
+=========
+
+0.2.1 (2015-06-02)
+------------------
+
+- Update Readme
+  [sgeulette]
+
+
+0.2 (2015-06-02)
+----------------
+
+- Avoid exception on site deletion.
+  [sgeulette]
+
+- Add single value local role field.
+  [cedricmessiant]
+
+- Also lookup behaviors when searching for local role fields.
+  [cedricmessiant]
+
+
+0.1 (2014-10-24)
+----------------
+
+- Initial release
+  [mpeeters]
---
+++ packages/dexterity.localrolesfield/dexterity.localrolesfield-0.2.1.zip/dexterity.localrolesfield-0.2.1/src/dexterity/localrolesfield/browser/settings.py
@@ -0,0 +1,54 @@
+# encoding: utf-8
+from collective.z3cform.datagridfield import DictRow
+from z3c.form import field
+from zope.interface import Interface
+from zope.schema import Choice
+from zope.schema import TextLine
+from dexterity.localroles import _ as LRMF
+
+from dexterity.localroles.browser.settings import LocalRoleConfigurationForm
+from dexterity.localroles.browser.settings import LocalRoleConfigurationPage
+from dexterity.localroles.browser.settings import LocalRoleList
+from dexterity.localroles.browser.settings import Role
+from dexterity.localroles.browser.settings import WorkflowState
+from dexterity.localroles.vocabulary import plone_role_generator
+
+from dexterity.localrolesfield.field import LocalRolesField
+from dexterity.localrolesfield.utils import get_localrole_fields
+from dexterity.localrolesfield import _
+
+
+class ILocalRoleConfig(Interface):
+    state = WorkflowState(title=LRMF(u'state'), required=True)
+
+    value = TextLine(title=_(u'suffix'), required=False, default=u'')
+
+    roles = Role(title=LRMF(u'roles'),
+                 value_type=Choice(source=plone_role_generator),
+                 required=True)
+
+
+class LocalRoleFieldConfigurationForm(LocalRoleConfigurationForm):
+
+    @property
+    def fields(self):
+        fields = super(LocalRoleFieldConfigurationForm, self).fields
+        fields = fields.values()
+        schema_fields = []
+        for name, fti_field in get_localrole_fields(self.context.fti):
+            f = LocalRoleList(
+                __name__=str(name),
+                title=fti_field.title,
+                description=fti_field.description,
+                value_type=DictRow(title=u"fieldconfig",
+                                   schema=ILocalRoleConfig)
+            )
+            schema_fields.append(f)
+
+        schema_fields = sorted(schema_fields, key=lambda x: x.title)
+        fields.extend(schema_fields)
+        return field.Fields(*fields)
+
+
+class LocalRoleFieldConfigurationPage(LocalRoleConfigurationPage):
+    form = LocalRoleFieldConfigurationForm
