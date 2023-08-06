daca2391e1709106069725971bf3153a7eace97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:09 +0000

    Add package koning/5/koning-5.tar.gz

---
+++ packages/koning/koning-5.tar.gz/koning-5/koning/campagne.py
@@ -0,0 +1,119 @@
+# koning/campagne.py
+#
+#
+
+""" the thing to resolve. """
+
+## IMPORTS
+
+from wet.utils.time import a_time, nr_days, elapsed_days, today
+from wet.clock import Repeater
+from wet.kernel import kernel
+from wet.thing import Thing
+
+## basic imports
+
+import datetime
+import random
+import time
+
+## SuicideWarnLoop
+
+names = ["crisis", "verwijs", "uitstroom", "politie", "geslaagd"]
+
+crisisjaar = 150000
+politiejaar = 0.15 * crisisjaar 
+hapjaar = 0.40 * crisisjaar
+ketenjaar = 0.45 * crisisjaar
+verwijsjaar = crisisjaar * 0.85
+uitstroomjaar = crisisjaar * 0.05
+opnamejaar = crisisjaar * 0.15
+suicidejaar = 1854
+politiedag = politiejaar / 365.0
+politieuur = politiedag / 24.0
+politiemin = politieuur / 60.0
+hapdag = hapjaar / 365.0
+hapuur = hapdag / 24.0
+hapmin = hapuur / 60.0
+ketendag = ketenjaar / 365.0
+ketenuur = ketendag / 24.0
+ketenmin = ketenuur / 60.0
+crisisdag = crisisjaar / 365.0
+crisisuur = crisisdag / 24.0
+crisismin = crisisuur / 60.0
+verwijsdag = verwijsjaar / 365.0
+verwijsuur = verwijsdag / 24.0
+verwijsmin = verwijsuur / 60.0
+uitstroomdag = uitstroomjaar / 365.0
+uitstroomuur = uitstroomdag / 24.0
+uitstroommin = uitstroomuur / 60.0
+opnamedag = opnamejaar / 365.0
+opnameuur = opnamedag / 24.0
+opnamemin = opnameuur / 60.0
+suicidedag = suicidejaar / 365.0
+suicideuur = suicidedag / 24.0
+suicidemin = suicideuur / 60.0
+counter = 0
+startdate = "2015-01-01 00:00:00"
+starttime = a_time(startdate)
+
+bron = "https://www.113online.nl/informatie/113online%2B/cijfers-over-zelfmoord"
+url = "http://pypi.python.org/pypi/koning"
+source = "http://pikacode.com/bthate/wet"
+fact = "http://tuchtrecht.overheid.nl/zoeken/resultaat/uitspraak/2014/ECLI_NL_TGZRAMS_2014_94?zaaknummer=2013%2F221&Pagina=1&ItemIndex=1"
+tiny = "http://tinyurl.com/tuchtfact"
+
+prevcrisis = 0
+prevverwijs = 0
+prevuitstroom = 0
+prevopname = 0
+prevgeslaagd = 0
+
+lastcrisis = 0
+lastverwijs = 0
+lastuitstroom = 0
+lastopname = 0
+lastsuicide = 0
+
+def ltag(*args, **kwargs):
+    l = []
+    while 1:
+        c = random.choice(tags)
+        if c not in l: l.append(c)
+        if len(l) == args[0]: break
+    return " ".join(l)
+
+def genocide(*args, **kwargs):
+    time_diff = time.time() - today()
+    aantalmin = time_diff/ 60
+    crisis = int(aantalmin * crisismin)
+    politie = int(aantalmin * politiemin)
+    hap = int(aantalmin * hapmin)
+    keten = int(aantalmin * ketenmin)
+    verwijs = int(aantalmin * verwijsmin)
+    uitstroom = int(aantalmin * uitstroommin)
+    opname = int(aantalmin * opnamemin)
+    suicide = int(aantalmin * suicidemin)
+    elapsed = elapsed_days(time_diff)
+    lastcrisis = 0
+    if crisis > lastcrisis:
+        txt = "CRISIS %s POLITIE %s HAP %s KETEN %s VERWIJS %s UITSTROOM %s OPNAME %s SUICIDE %s %s %s" % (crisis, politie, hap, keten, verwijs, uitstroom, opname, suicide, url, elapsed)
+        lastcrisis = crisis
+        kernel.announce(txt)
+
+kernel.register("campagne.genocide", genocide)
+
+## Campagne class
+
+class CRISIS(Repeater): pass
+class VERWIJS(Repeater): pass
+class UITSTROOM(Repeater): pass
+class OPNAME(Repeater): pass
+class SUICIDE(Repeater): pass
+class CRISIS(Repeater): pass
+
+## campagne init
+
+def init(*args, **kwargs):
+    todo = CRISIS(genocide, int(60 * 60 * 24 / suicidedag))
+    kernel.put(todo.start)
---
+++ packages/koning/koning-5.tar.gz/koning-5/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+#
+
+import os
+import sys
+
+if sys.version_info.major < 3: print("you need to run KONING with python3") ; os._exit(1)
+
+try: use_setuptools()
+except: pass
+
+try:
+    from setuptools import setup
+except Exception as ex: print(str(ex)) ; os._exit(1)
+
+setup(
+    name='koning',
+    version='5',
+    url='https://pikacode.com/bthate/koning',
+    author='Bart Thate',
+    author_email='bthate@dds.nl',
+    description=""" Antipsychotica doden. Zonder verpleging is behandeling moord. """,
+    license='MIT',
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=["wet", ],
+    scripts=["bin/koning", ],
+    packages=['koning',
+             ],
+    long_description=""" 
+
+(F)ACT bied niet de noodzakelijke verpleging:
+
+1) verpleging naar gelang de situatie - meer zorg naarmate de situatie erger is, is niet preventief.
+2) niet 7 x 24 uurs verpleging - in het weekend en avonduren niet aanwezig  
+3) psychiater is niet op de hoogte van de situatie van de patient
+4) behandelovereenkomsten zijn niet volledig
+5) zonder bedden geen snelle terugbrenging naar ziekenhuis mogelijk.
+
+""",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+)
