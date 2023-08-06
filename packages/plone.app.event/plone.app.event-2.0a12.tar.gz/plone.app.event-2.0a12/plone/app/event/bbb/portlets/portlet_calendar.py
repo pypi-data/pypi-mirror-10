f40127a8b467254a38b97a3c0441655a95e2e85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:57 +0000

    Add package plone.app.event/2.0a12/plone.app.event-2.0a12.tar.gz

---
+++ packages/plone.app.event/plone.app.event-2.0a12.tar.gz/plone.app.event-2.0a12/plone/app/event/bbb/portlets/portlet_calendar.py
@@ -0,0 +1,47 @@
+from distutils.version import StrictVersion
+import pkg_resources
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.portlets import PloneMessageFactory as _
+
+from plone.app.event.portlets.portlet_calendar import Renderer as RendererBase
+from plone.app.event.portlets.portlet_calendar import ICalendarPortlet
+from plone.app.event.portlets.portlet_calendar import Assignment
+
+
+version = StrictVersion(pkg_resources.require("plone.app.portlets")[0].version)
+wanted = StrictVersion('3.0')
+
+if version >= wanted:
+    P_A_PORTLETS_PRE_3 = False
+    from plone.app.portlets.portlets import base
+else:
+    P_A_PORTLETS_PRE_3 = True
+    from z3c.form import field
+    from plone.app.portlets.browser import z3cformhelper as base
+
+
+class Renderer(RendererBase):
+    render = ViewPageTemplateFile('portlet_calendar.pt')
+
+
+class AddForm(base.AddForm):
+    if P_A_PORTLETS_PRE_3:
+        fields = field.Fields(ICalendarPortlet)
+    else:
+        schema = ICalendarPortlet
+    label = _(u"Add Calendar Portlet")
+    description = _(u"This portlet displays events in a calendar.")
+
+    def create(self, data):
+        return Assignment(state=data.get('state', None),
+                          search_base_uid=data.get('search_base_uid', None))
+
+
+class EditForm(base.EditForm):
+    if P_A_PORTLETS_PRE_3:
+        fields = field.Fields(ICalendarPortlet)
+    else:
+        schema = ICalendarPortlet
+    label = _(u"Edit Calendar Portlet")
+    description = _(u"This portlet displays events in a calendar.")
---
+++ packages/plone.app.event/plone.app.event-2.0a12.tar.gz/plone.app.event-2.0a12/plone/app/event/bbb/portlets/portlet_events.py
@@ -0,0 +1,48 @@
+from distutils.version import StrictVersion
+import pkg_resources
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.portlets import PloneMessageFactory as _
+
+from plone.app.event.portlets.portlet_events import Renderer as RendererBase
+from plone.app.event.portlets.portlet_events import Assignment
+from plone.app.event.portlets.portlet_events import IEventsPortlet
+
+
+version = StrictVersion(pkg_resources.require("plone.app.portlets")[0].version)
+wanted = StrictVersion('3.0')
+
+if version >= wanted:
+    P_A_PORTLETS_PRE_3 = False
+    from plone.app.portlets.portlets import base
+else:
+    P_A_PORTLETS_PRE_3 = True
+    from z3c.form import field
+    from plone.app.portlets.browser import z3cformhelper as base
+
+
+class Renderer(RendererBase):
+    render = ViewPageTemplateFile('portlet_events.pt')
+
+
+class AddForm(base.AddForm):
+    if P_A_PORTLETS_PRE_3:
+        fields = field.Fields(IEventsPortlet)
+    else:
+        schema = IEventsPortlet
+    label = _(u"Add Events Portlet")
+    description = _(u"This portlet lists upcoming Events.")
+
+    def create(self, data):
+        return Assignment(count=data.get('count', 5),
+                          state=data.get('state', None),
+                          search_base_uid=data.get('search_base_uid', 5))
+
+
+class EditForm(base.EditForm):
+    if P_A_PORTLETS_PRE_3:
+        fields = field.Fields(IEventsPortlet)
+    else:
+        schema = IEventsPortlet
+    label = _(u"Edit Events Portlet")
+    description = _(u"This portlet lists upcoming Events.")
