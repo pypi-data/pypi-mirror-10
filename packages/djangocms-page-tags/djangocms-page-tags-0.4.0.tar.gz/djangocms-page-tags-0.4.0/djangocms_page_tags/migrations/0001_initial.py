19b67bcd2344685694f42747542f8e7709984bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:03 +0000

    Add package djangocms-page-tags/0.4.0/djangocms-page-tags-0.4.0.tar.gz

---
+++ packages/djangocms-page-tags/djangocms-page-tags-0.4.0.tar.gz/djangocms-page-tags-0.4.0/djangocms_page_tags/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import taggit_autosuggest.managers
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taggit', '0001_initial'),
+        ('cms', '0003_auto_20140926_2347'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PageTags',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('extended_object', models.OneToOneField(editable=False, to='cms.Page')),
+                ('public_extension', models.OneToOneField(related_name='draft_extension', null=True, editable=False, to='djangocms_page_tags.PageTags')),
+                ('tags', taggit_autosuggest.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
+            ],
+            options={
+                'verbose_name': 'Page tags (all languages)',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TitleTags',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('extended_object', models.OneToOneField(editable=False, to='cms.Title')),
+                ('public_extension', models.OneToOneField(related_name='draft_extension', null=True, editable=False, to='djangocms_page_tags.TitleTags')),
+                ('tags', taggit_autosuggest.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
+            ],
+            options={
+                'verbose_name': 'Page tags (language-dependent)',
+            },
+            bases=(models.Model,),
+        ),
+    ]
