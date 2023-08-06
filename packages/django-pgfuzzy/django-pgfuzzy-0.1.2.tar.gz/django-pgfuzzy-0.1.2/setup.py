9784336842ee4276b8e54f26431dbb0521c46b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:24 +0000

    Add package django-pgfuzzy/0.1.2/django-pgfuzzy-0.1.2.tar.gz

---
+++ packages/django-pgfuzzy/django-pgfuzzy-0.1.2.tar.gz/django-pgfuzzy-0.1.2/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_dir, 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-pgfuzzy',
+    version='0.1.2',
+    packages=['pgfuzzy'],
+    include_package_data=True,
+    license='GPL v3 License',  # example license
+    description='A set of lookups for Django to allow usage of PostgreSQL fuzzystrmatch extension',
+    long_description=README,
+    url='http://github.com/codedbyjay/django-pgfuzzy',
+    author='Jean-Mark Wright',
+    author_email='jeanmark.wright@gmail.com',
+    package_data = {
+        'pgfuzzy' : ['migrations/*']
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[ req for req in open("%s/requirements.txt" % current_dir,"r").read().splitlines() if req.strip() and not req.startswith("#")]
+)
