f76572bb59eebb41da976c452eb0a69160369181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:33 +0000

    Add package amazon-mws/0.2.1/amazon-mws-0.2.1.tar.gz

---
+++ packages/amazon-mws/amazon-mws-0.2.1.tar.gz/amazon-mws-0.2.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+version = '0.2.1'
+
+setup(
+    name="amazon-mws",
+    version=version,
+    description="A python interface for Amazon MWS",
+    long_description=open('README.md', 'rt').read(),
+    author="Juozas Kaziukenas",
+    author_email="juozas@juokaz.com",
+    url="https://github.com/juokaz/python-amazon-mws",
+    packages=['mws'],
+    platforms=['OS Independent'],
+    license='LICENSE.txt'
+)
