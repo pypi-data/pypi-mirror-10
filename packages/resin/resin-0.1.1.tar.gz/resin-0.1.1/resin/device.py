c19acc785f4ac2d7e8068ba9efa34466371d0ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:46 +0000

    Add package resin/0.1.1/resin-0.1.1.tar.gz

---
+++ packages/resin/resin-0.1.1.tar.gz/resin-0.1.1/resin/device.py
@@ -0,0 +1,42 @@
+from .baseapi import BaseAPI
+
+
+class Device(BaseAPI):
+
+    def get(self, uuid):
+        params = {
+            'filter': 'uuid',
+            'eq': uuid
+        }
+        return self.request('device', 'GET', params=params)[0]
+
+    def get_all(self):
+        return self.request('device', 'GET')
+
+    def get_all_by_application(self, name):
+        # notworkingyet
+        params = {
+            'expand': 'application',
+            'eq': name,
+            'filter': "app_name"
+        }
+        return self.request('device', 'GET', params=params)
+
+    def get_by_name(self, name):
+        params = {
+            'filter': 'name',
+            'eq': name
+        }
+        return self.request('device', 'GET', params=params)[0]
+
+    def identify(self, uuid):
+        data = {
+            'uuid': uuid
+        }
+        return self.request('/blink', 'POST', data=data)
+
+    def get_name(self, uuid):
+        return self.get(uuid)['name']
+
+    def is_online(self, uuid):
+        return self.get(uuid)['is_online']
---
+++ packages/resin/resin-0.1.1.tar.gz/resin-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='resin',
+      version='0.1.1',
+      description='Thin wrapper for resin api[work in progress]',
+      url='http://github.com/craig-mulligan/resin-python-api-wrapper',
+      author='craig-mulligan',
+      author_email='craig@resin.io',
+      license='MIT',
+      packages=['resin'],
+      zip_safe=False)
