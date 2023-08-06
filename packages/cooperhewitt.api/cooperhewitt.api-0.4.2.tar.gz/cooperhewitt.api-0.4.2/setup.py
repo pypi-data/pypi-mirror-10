47e3724f4c63a0aac5b64af66c1a57c78abbfdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:50 +0000

    Add package cooperhewitt.api/0.4.2/cooperhewitt.api-0.4.2.tar.gz

---
+++ packages/cooperhewitt.api/cooperhewitt.api-0.4.2.tar.gz/cooperhewitt.api-0.4.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+setup(
+    name='cooperhewitt.api',
+    namespace_packages=['cooperhewitt'],
+    version='0.4.2',
+    description='Simple Python wrapper for Cooper-Hewitt API',
+    author='Smithsonian Cooper-Hewitt National Design Museum',
+    url='https://github.com/cooperhewitt/py-cooperhewitt-api',
+    install_requires=[
+        'requests'
+        ],
+    packages=packages,
+    scripts=[],
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-api/releases/tag/v0.4.2',
+    license='BSD')
