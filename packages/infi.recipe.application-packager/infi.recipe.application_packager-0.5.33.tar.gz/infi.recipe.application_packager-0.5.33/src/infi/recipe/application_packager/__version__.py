bb3dd29f19d17c96492a68a78aa9db2f032cf3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:25 +0000

    Add package infi.recipe.application-packager/0.5.33/infi.recipe.application_packager-0.5.33.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.33.tar.gz/infi.recipe.application_packager-0.5.33/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.5.33"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.5.33)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ac80bdb38d43d458bbcc16a6bc9123388db988de'
+__git_head_subject__ = 'Finished release v0.5.33.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2015-07-08 11:51:48'
