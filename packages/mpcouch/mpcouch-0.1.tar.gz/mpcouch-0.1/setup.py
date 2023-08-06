af8c7d9f927dfcae473cab55ae7f5e78d65c59b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:42 +0000

    Add package mpcouch/0.1/mpcouch-0.1.tar.gz

---
+++ packages/mpcouch/mpcouch-0.1.tar.gz/mpcouch-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mpcouch',
+      version='0.1',
+      description='A multiprocess bulk-uploading helper for CouchDB',
+      url='https://github.com/scubbx/couch-bulk-multiprocess',
+      author='Markus Mayr',
+      author_email='markusmayr@gmx.net',
+      license='MIT',
+      packages=['mpcouch'],
+      zip_safe=False)
