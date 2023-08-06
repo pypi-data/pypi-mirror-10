c90812d61844d5b6278901272aafe80d5bd668f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:06 +0000

    Add package bioscraping/0.0.3/bioscraping-0.0.3.tar.gz

---
+++ packages/bioscraping/bioscraping-0.0.3.tar.gz/bioscraping-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'bioscraping',
+    version = '0.0.3',
+    packages=find_packages(),
+    url = 'https://github.com/mlovci/bioscraping',
+    license = 'MIT',
+    author = 'Michael T Lovci',
+    keywords=["scraping", "biological data"],
+    author_email = 'Michael.T.Lovci@gmail.com',
+    description = 'Scrape biological data from websites'
+)
