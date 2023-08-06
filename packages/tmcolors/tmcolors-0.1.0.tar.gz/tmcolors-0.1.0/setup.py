9c4e3fb1968e0353090fea192421f1a50f8beb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:48 +0000

    Add package tmcolors/0.1.0/tmcolors-0.1.0.tar.gz

---
+++ packages/tmcolors/tmcolors-0.1.0.tar.gz/tmcolors-0.1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tmcolors',
+    version='0.1.0',
+    description='ANSI colors for Python',
+    long_description=open('README.rst').read(),
+    author='Giorgos Verigakis, Marcin Sztolcman',
+    author_email='marcin@urzenia.net',
+    url='https://github.com/mysz/tmcolors',
+    license='ISC',
+    py_modules=['colors'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
+)
