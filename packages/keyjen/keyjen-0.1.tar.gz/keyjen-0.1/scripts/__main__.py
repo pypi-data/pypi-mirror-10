6526ddef57f3da5ae2a4047754381a8d3454ec07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:52 +0000

    Add package keyjen/0.1/keyjen-0.1.tar.gz

---
+++ packages/keyjen/keyjen-0.1.tar.gz/keyjen-0.1/scripts/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+from argument import Argument
+from generator import Generator
+
+
+def main():
+    args = Argument().args
+    generator = Generator(args.length, args.types.split(','), args.count, prefix=args.prefix, suffix=args.suffix)
+    generator.print_keys()
+
+if __name__ == "__main__":
+    main()
