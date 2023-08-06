62894640791a89c28f59cff7e9c0c6eb6347cc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:44 +0000

    Add package source/1.2.0/source-1.2.0.zip

---
+++ packages/source/source-1.2.0.zip/source-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name         ="source",
+      version      ="1.2.0",
+      py_modules   =['source'],
+      author       ="yang",
+      author_email ="1290657017@qq.com",
+      url          ="http://www.cctv.com",
+      description  =" A simple printer of source list",
+      )
