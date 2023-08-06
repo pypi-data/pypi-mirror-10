41b28e3f6448f2bcf3bab2973191679cc672902e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:43 +0000

    Add package atf/0.0.0.1/atf-0.0.0.1.tar.gz

---
+++ packages/atf/atf-0.0.0.1.tar.gz/atf-0.0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+if __name__ == '__main__':
+    setup(name='atf',
+          version='0.0.0.1',
+          description='ATF (ATF is a Test Framework)',
+          author='ATF Developers',
+          author_email='atf-devel@redhat.com',
+          url='http://atf-project.github.io/',
+          keywords=['testing'],
+          packages=['atf'],
+         )
