53c8e887ba714a615360ed23dfd2a02a90084221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:12 +0000

    Add package ebzl/0.1/ebzl-0.1-py2-none-any.whl

---
+++ packages/ebzl/ebzl-0.1-py2-none-any.whl/ebzl/lib/s3.py
@@ -0,0 +1,13 @@
+import boto
+
+from . import config
+
+
+def get_connection(profile_name):
+    return boto.connect_s3(*config.get_credentials(profile_name))
+
+
+def map_region_to_location(region):
+    if region.startswith("eu-"):
+        return "EU"
+    return region
