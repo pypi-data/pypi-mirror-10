3d37daafaa07b44360ee78deecb32fca23538ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:47 +0000

    Add package firstpkg/0.1/firstpkg-0.1.tar.gz

---
+++ packages/firstpkg/firstpkg-0.1.tar.gz/firstpkg-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='firstpkg',
+      version='0.1',
+      description='This is my first package',
+      url='http://github.com/hoangdangninh',
+      author='hoang dang',
+      author_email='hoangdangninh@gmail.com',
+      license='MIT',
+      packages=['firstpkg'],
+      zip_safe=False)
