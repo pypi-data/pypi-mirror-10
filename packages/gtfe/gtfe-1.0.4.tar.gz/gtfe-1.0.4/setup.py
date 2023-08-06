4ec684b8948d8f922a5caaefbe275504cb65f64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:50 +0000

    Add package gtfe/1.0.4/gtfe-1.0.4.tar.gz

---
+++ packages/gtfe/gtfe-1.0.4.tar.gz/gtfe-1.0.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='gtfe',
+    packages=find_packages(),
+    version='1.0.4',
+    description='Get the number of seconds since the Unix epoch for a given datetime.',
+    author='Matthew de Verteuil, Jason Peddle, Liav Koren',
+    author_email='onceuponajooks@gmail.com',
+    url='https://github.com/mverteuil/gtfe',
+    keywords='epoch datetime time timezone',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet'
+    ],
+)
