edda40e492ec3fbba1685decb2edd4af7a2ab386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:26 +0000

    Add package hitchserve/0.3.4/hitchserve-0.3.4.tar.gz

---
+++ packages/hitchserve/hitchserve-0.3.4.tar.gz/hitchserve-0.3.4/hitchserve/hitch_exception.py
@@ -0,0 +1,17 @@
+class HitchServeException(Exception):
+    pass
+
+class ServiceStartupTimeoutException(HitchServeException):
+    pass
+
+class ServiceSuddenStopException(HitchServeException):
+    pass
+
+class ServiceMisconfiguration(HitchServeException):
+    pass
+
+class BundleMisconfiguration(HitchServeException):
+    pass
+
+class WaitingForLogMessageTimeout(HitchServeException):
+    pass
