bb0e575cca90d52a9f47a16b91aa529805414adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:17 +0000

    Add package korobi_docs_theme/1.0.0/korobi_docs_theme-1.0.0.tar.gz

---
+++ packages/korobi_docs_theme/korobi_docs_theme-1.0.0.tar.gz/korobi_docs_theme-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='korobi_docs_theme',
+    version='1.0.0',
+    packages=['korobi_docs_theme'],
+    url='https://github.com/korobi/korobi_docs_theme',
+    author='korobi.io team',
+    description='The Korobi documentation theme.'
+)
