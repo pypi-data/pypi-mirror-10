e051536742b63cfda7057f38447382b750fa9272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:36 +0000

    Add package decisionTable/0.0.3/decisionTable-0.0.3-py2.py3-none-any.whl

---
+++ packages/decisionTable/decisionTable-0.0.3-py2.py3-none-any.whl/decisionTable/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+decisionTable
+------------
+Main package for decisionTable.
+"""
+
+from __future__ import absolute_import
+
+__author__ = u'Uroš Jarc'
+__email__ = u'jar.fmf@gmail.com'
+__version__ = u'0.0.3'
+__description__ = u'Transform decision table string to code logic.'
+
+from .DecisionTable import DecisionTable
