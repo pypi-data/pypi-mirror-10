cce3f19d198a58b38d12b0194cb2a4c807ac99cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:29 +0000

    Add package mockpy/0.1.2/mockpy-0.1.2.tar.gz

---
+++ packages/mockpy/mockpy-0.1.2.tar.gz/mockpy-0.1.2/mockpy/core/proxy_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import signal
+import sys
+
+from libmproxy import controller, proxy
+from libmproxy.proxy.server import ProxyServer
+
+from .proxy_mapper import *
+
+
+class MITMProxy(controller.Master):
+
+    def __init__(self, server, proxy_mapper):
+        controller.Master.__init__(self, server)
+        self.proxy_mapper = proxy_mapper
+
+    def handle_request(self, flow):
+        self.proxy_mapper.handle_request(flow)
+
+
+def start_proxy_server(port, inout_path, res_path, http_proxy):
+    config = proxy.ProxyConfig(port=port)
+    server = ProxyServer(config)
+
+    mapping_handler = MappingItemsManager(inout_path, res_path)
+
+    proxy_mapper = ProxyMapper(mapping_handler, http_proxy)
+    m = MITMProxy(server, proxy_mapper)
+
+    def signal_handler(signal, frame):
+        info("\nShutting down proxy server")
+        m.shutdown()
+        success("Proxy server stopped")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    m.run()
---
+++ packages/mockpy/mockpy-0.1.2.tar.gz/mockpy-0.1.2/mockpy/models/mapping_respone.py
@@ -0,0 +1,95 @@
+import json
+
+
+class MappingResponse(object):
+
+    def __init__(self, response_dic, res_path):
+        self.res_path = res_path
+        self.response_dic = response_dic
+
+        self.setup_status()
+        self.setup_body()
+        self.setup_headers()
+
+    def title(self):
+        return self.res_path + "/" + self.body.file_name
+
+    def setup_status(self):
+        self.status = self.response_dic["status"] if "status" in self.response_dic else 200
+
+    def setup_body(self):
+        self.body = BodyResponse(self.response_dic, self.res_path)
+
+    def setup_headers(self):
+        self.headers = self.response_dic.get("headers", {})
+        self.adjust_header_for_body_type()
+
+    def adjust_header_for_body_type(self):
+        if self.body.body_type == BodyResponse.IMAGE:
+            self.headers["Content-Type"] = "image/png"
+            self.headers["Accept-Ranges"] = "bytes"
+
+    def body_response(self):
+        return self.body.read_value()
+
+    def headers(self):
+        return self.headers
+
+
+class BodyResponse(object):
+
+    @property
+    def body_type(self):
+        return self._body_type
+
+    def __init__(self, dic, res_path):
+        self.res_path = res_path
+        self.value = ""
+        self._body_type = BodyResponse.NONE
+
+        if "body_file" in dic:
+            self._body_type = BodyResponse.FILE
+            self.file_name = dic["body_file"]
+            self.value_from_file(self.file_name)
+
+        elif "body" in dic:
+            self._body_type = BodyResponse.RAW
+            self.file_name = ""
+            self.set_from_object(dic["body"])
+
+        elif "body_image" in dic:
+            self._body_type = BodyResponse.IMAGE
+            self.file_name = dic["body_image"]
+            self.value_from_image(self.file_name)
+
+    def read_value(self):
+        return self.value()
+
+    """
+        Reading file
+    """
+    def value_from_file(self, file):
+        self.value = lambda: self.read_file(file, decode_utf8=True)
+
+    def set_from_object(self, object):
+        if isinstance(object, str):
+            self.value = lambda: json.dumps(object).encode('utf8')
+
+        elif isinstance(object, dict) or isinstance(object, list):
+            self.value = lambda: json.dumps(object).encode('utf8')
+
+    def value_from_image(self, image):
+        self.value = lambda: self.read_file(image, decode_utf8=False)
+
+    def read_file(self, file, decode_utf8=True):
+        with open(self.res_path + "/" + file, "r") as the_file:
+            content = the_file.read()
+            if decode_utf8:
+                return content.encode('utf8')
+            else:
+                return content
+
+    RAW = "raw"
+    IMAGE = "image"
+    FILE = "file"
+    NONE = "none"
