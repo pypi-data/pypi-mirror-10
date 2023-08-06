e6eac1907e763c96c43c5c9df099cbcccb8ed2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:32 +0000

    Add package django-healthchecks/0.1.2/django-healthchecks-0.1.2.tar.gz

---
+++ packages/django-healthchecks/django-healthchecks-0.1.2.tar.gz/django-healthchecks-0.1.2/src/django_healthchecks/views.py
@@ -0,0 +1,23 @@
+from django.http import JsonResponse, HttpResponse
+from django.http.response import Http404
+from django.views.generic import View
+
+from django_healthchecks.checker import create_report, create_service_result
+
+
+class HealthCheckView(View):
+    def get(self, request, *args, **kwargs):
+        report = create_report()
+        return JsonResponse(report)
+
+
+class HealthCheckServiceView(View):
+    def get(self, request, service, *args, **kwargs):
+        result = create_service_result(service)
+        if result is None:
+            raise Http404()
+
+        if result in (True, False):
+            return HttpResponse(str(result).lower())
+
+        return HttpResponse(result)
