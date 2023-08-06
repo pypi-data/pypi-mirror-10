13ff6c9fcbb0eac0becffb594436299fe2c7e1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:49 +0000

    Add package POAP/0.1.3/POAP-0.1.3-py2.py3-none-any.whl

---
+++ packages/POAP/POAP-0.1.3-py2.py3-none-any.whl/poap/test/test_process_worker.py
@@ -0,0 +1,56 @@
+"""
+Test shell-out to another process.
+"""
+
+import time
+import random
+import subprocess
+from poap.strategy import FixedSampleStrategy
+from poap.strategy import CheckWorkerStrategy
+from poap.controller import ThreadController
+from poap.controller import ChaosMonkey
+from poap.controller import ProcessWorkerThread
+
+
+class DummySim(ProcessWorkerThread):
+
+    def handle_eval(self, record):
+        args = ['./dummy_sim', str(*record.params)]
+        self.process = subprocess.Popen(args, stdout=subprocess.PIPE)
+        data = self.process.communicate()[0]
+        try:
+            self.finish_success(record, float(data))
+            self.lprint("Success: {0}".format(record.params))
+        except ValueError:
+            self.finish_failure(record)
+            self.lprint("Failure: {0}".format(record.params))
+
+
+def main():
+    "Testing routine."
+    samples = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    controller = ThreadController()
+    strategy = FixedSampleStrategy(samples)
+    strategy = CheckWorkerStrategy(controller, strategy)
+    controller.strategy = strategy
+    
+    def monitor():
+        "Report progress of the optimization, roughly once a second."
+        record = controller.best_point()
+        if record:
+            controller.lprint(record.value, record.params)
+        else:
+            controller.lprint('No points yet')
+        controller.add_timer(1, monitor)
+
+    for _ in range(5):
+        controller.launch_worker(DummySim(controller))
+
+    controller.add_timer(1, monitor)
+    monkey = ChaosMonkey(controller, mtbf=3)
+    result = controller.run()
+    print('Final', result.value, result.params)
+
+
+if __name__ == '__main__':
+    main()
