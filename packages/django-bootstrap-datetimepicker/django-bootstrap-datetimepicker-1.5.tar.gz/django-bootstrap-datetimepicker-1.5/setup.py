dcde73499f79e4845fe8275bb455de1972a04ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:39 +0000

    Add package django-bootstrap-datetimepicker/1.5/django-bootstrap-datetimepicker-1.5.tar.gz

---
+++ packages/django-bootstrap-datetimepicker/django-bootstrap-datetimepicker-1.5.tar.gz/django-bootstrap-datetimepicker-1.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'Natural Language :: Portuguese (Brazilian)',
+    'Programming Language :: JavaScript',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Utilities',
+]
+
+setup(
+    author="Marcelo Fonseca Tambalo",
+    author_email="marcelo.zokis@gmail.com",
+    version='1.5',
+    name='django-bootstrap-datetimepicker',
+    description='DateTime Picker to Django using Bootstrap Twitter and bootstrap-datetimepicker',
+    url='https://github.com/zokis/django-bootstrap-datetimepicker',
+    license='MIT',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.4',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
