e5278ba38a0d015820e8cb8115401eb1b22a81fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:16 +0000

    Add package bosi/0.1.3/bosi-0.1.3.tar.gz

---
+++ packages/bosi/bosi-0.1.3.tar.gz/bosi-0.1.3/etc/t5/puppet_template/redhat_compute.pp
@@ -0,0 +1,61 @@
+
+$binpath = "/usr/local/bin/:/bin/:/usr/bin:/usr/sbin:/usr/local/sbin:/sbin"
+
+# install and enable ntp
+package { "ntp":
+    ensure  => installed,
+}
+service { "ntp":
+    ensure  => running,
+    enable  => true,
+    require => Package['ntp'],
+}
+
+# dhcp configuration
+if %(deploy_dhcp_agent)s {
+    ini_setting { "dhcp agent interface driver":
+        ensure            => present,
+        path              => '/etc/neutron/dhcp_agent.ini',
+        section           => 'DEFAULT',
+        key_val_separator => '=',
+        setting           => 'interface_driver',
+        value             => 'neutron.agent.linux.interface.OVSInterfaceDriver',
+    }
+    ini_setting { "dhcp agent dhcp driver":
+        ensure            => present,
+        path              => '/etc/neutron/dhcp_agent.ini',
+        section           => 'DEFAULT',
+        key_val_separator => '=',
+        setting           => 'dhcp_driver',
+        value             => 'bsnstacklib.plugins.bigswitch.dhcp_driver.DnsmasqWithMetaData',
+    }
+    ini_setting { "dhcp agent enable isolated metadata":
+        ensure            => present,
+        path              => '/etc/neutron/dhcp_agent.ini',
+        section           => 'DEFAULT',
+        key_val_separator => '=',
+        setting           => 'enable_isolated_metadata',
+        value             => 'True',
+    }
+    ini_setting { "dhcp agent disable metadata network":
+        ensure            => present,
+        path              => '/etc/neutron/dhcp_agent.ini',
+        section           => 'DEFAULT',
+        key_val_separator => '=',
+        setting           => 'enable_metadata_network',
+        value             => 'False',
+    }
+    ini_setting { "dhcp agent disable dhcp_delete_namespaces":
+        ensure            => present,
+        path              => '/etc/neutron/dhcp_agent.ini',
+        section           => 'DEFAULT',
+        key_val_separator => '=',
+        setting           => 'dhcp_delete_namespaces',
+        value             => 'False',
+    }
+}
+
+# l3 agent configuration
+if %(deploy_l3_agent)s {
+    
+}
