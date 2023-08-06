9e6cb9cbc6877ee297d273a2d4ea2841864d01a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:51 +0000

    Add package django-time-logger/0.0.2/django-time-logger-0.0.2.tar.gz

---
+++ packages/django-time-logger/django-time-logger-0.0.2.tar.gz/django-time-logger-0.0.2/time_logger/mongo_models.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import datetime
+import mongoengine
+
+class ViewTimeLog(mongoengine.Document):
+    duration = mongoengine.IntField(help_text='View process duration in seconds')
+    view_func_path = mongoengine.StringField()
+    view_args = mongoengine.ListField()
+    view_kwargs = mongoengine.DictField()
+    username = mongoengine.StringField()
+    request_get = mongoengine.DictField()
+    request_post = mongoengine.DictField()
+    dc = mongoengine.DateTimeField()
+
+    meta = {
+        'indexes': ['-duration', 'dc'],
+    }
+
+    def save(self, *args, **kwargs):
+        if not self.dc:
+            self.dc = datetime.datetime.now()
+        return super(ViewTimeLog, self).save(*args, **kwargs)
