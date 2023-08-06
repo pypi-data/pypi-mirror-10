1a812240eba26bae56fb2134747eac71cec907ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:56 +0000

    Add package docker-debug/1.2/docker-debug-1.2.tar.gz

---
+++ packages/docker-debug/docker-debug-1.2.tar.gz/docker-debug-1.2/dockerdebug/main.py
@@ -0,0 +1,43 @@
+"""
+Debug a docker container.
+"""
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from docker.client import Client
+
+from dockerdebug.run import RunCommand
+from dockerdebug.tail import TailCommand
+
+
+def main():
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    subparsers = parser.add_subparsers()
+
+    run_parser = subparsers.add_parser("run",
+                                       formatter_class=ArgumentDefaultsHelpFormatter,
+                                       help="run a new docker container based on an existing one")
+    run_parser.add_argument("container",
+                            help="Container name to debug")
+    run_parser.add_argument("--command",
+                            default="/bin/bash",
+                            help="command to execute at runtime")
+    run_parser.set_defaults(func=RunCommand.build)
+
+    tail_parser = subparsers.add_parser("tail",
+                                        formatter_class=ArgumentDefaultsHelpFormatter,
+                                        help="tail docker container stdout/stderr log")
+    tail_parser.add_argument("container",
+                             help="Container name to tail")
+    tail_parser.set_defaults(func=TailCommand.build)
+
+    args, extra = parser.parse_known_args()
+
+    # connect to Docker
+    client = Client()
+
+    # build the command for the container
+    command = parser.func(client, args, extra)
+
+    # execute the command
+    command.execute()
