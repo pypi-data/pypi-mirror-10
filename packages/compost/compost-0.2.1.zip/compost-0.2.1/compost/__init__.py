cc039f4051c39205b5d3133430df4a0e5257ab85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:42 +0000

    Add package compost/0.2.1/compost-0.2.1.zip

---
+++ packages/compost/compost-0.2.1.zip/compost-0.2.1/compost/__init__.py
@@ -0,0 +1,17 @@
+
+from .dataset import (
+    Dataset,
+    ShortDatasetError,
+    SubMinuteTimestepError
+)
+
+from .saving_calculation import (
+    SavingCalculation,
+    ShortBaselineError,
+)
+
+from .model import (
+    DailyAverageModel,
+    WeekdayAverageModel,
+    MonthlyAverageModel
+)
