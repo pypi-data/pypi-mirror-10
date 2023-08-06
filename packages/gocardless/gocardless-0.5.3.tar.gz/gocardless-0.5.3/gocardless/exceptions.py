4274359958aac17b5abf03c95a36a750825adf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:21 +0000

    Add package gocardless/0.5.3/gocardless-0.5.3.tar.gz

---
+++ packages/gocardless/gocardless-0.5.3.tar.gz/gocardless-0.5.3/gocardless/exceptions.py
@@ -0,0 +1,27 @@
+class GoCardlessError(Exception):
+    pass
+
+
+class ClientError(GoCardlessError):
+    """Thrown when there was an error processing the request"""
+    def __init__(self, message, errors=None):
+        self.message = message
+        if errors is not None:
+            self.message += self._stringify_errors(errors)
+        super(ClientError, self).__init__(self.message)
+
+    def _stringify_errors(self, errors):
+        msgs = []
+        if isinstance(errors, list):
+            msgs += errors
+        elif isinstance(errors, dict):
+            for field in errors:
+                msgs += ['{} {}'.format(field, msg) for msg in errors[field]]
+        else:
+            msgs = [str(errors)]
+        return ", ".join(msgs)
+
+
+class SignatureError(GoCardlessError):
+    pass
+
