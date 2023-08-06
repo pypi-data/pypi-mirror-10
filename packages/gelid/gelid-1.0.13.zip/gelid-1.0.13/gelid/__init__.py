94b11d0730a0425072276a082072adc6b03eab29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:12 +0000

    Add package gelid/1.0.13/gelid-1.0.13.zip

---
+++ packages/gelid/gelid-1.0.13.zip/gelid-1.0.13/gelid/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+# lvjiyong on 2015/4/19.
+from gelid.extractors import html, score, tm, scan, regex, content, http, page, title, xpath
+from gelid.items import article
+from gelid.extractors.log import logger
+
+html = html
+score = score
+tm = tm
+scan = scan
+regex = regex
+content = content
+http = http
+page = page
+title = title
+xpath = xpath
+Article = article.Article
+logger = logger
