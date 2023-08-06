fd48209ac13732e80861ed5c1fd70f162a6b3f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:55 +0000

    Add package pyMTRX/1.8.0/pyMTRX-1.8.0.zip

---
+++ packages/pyMTRX/pyMTRX-1.8.0.zip/pyMTRX-1.8.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name='pyMTRX',
+       version='1.8.0',
+       description="Omicron NanoTechnology MATRIX SPM Support",
+       author='Alex Pronschinske',
+       url='https://www.github.com/ampron',
+       packages=['pyMTRX',],
+       #scripts=[ 'scripts/convert_spec.py',
+       #          'scripts/notebook_sheet.py',
+       #          'scripts/notebook_slides.py',
+       #        ],
+     )
