f025416a3cd4971129f508c62aa8870cb1d3d2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package onegov.form/0.3.1/onegov.form-0.3.1.tar.gz

---
+++ packages/onegov.form/onegov.form-0.3.1.tar.gz/onegov.form-0.3.1/onegov/form/__init__.py
@@ -0,0 +1,24 @@
+import logging
+log = logging.getLogger('onegov.form')  # noqa
+log.addHandler(logging.NullHandler())  # noqa
+
+from onegov.form.collection import FormCollection
+from onegov.form.core import Form, with_options
+from onegov.form.display import render_field
+from onegov.form.models import (
+    FormDefinition,
+    FormSubmission,
+    PendingFormSubmission,
+    CompleteFormSubmission
+)
+
+__all__ = [
+    'Form',
+    'FormCollection',
+    'FormDefinition',
+    'FormSubmission',
+    'PendingFormSubmission',
+    'CompleteFormSubmission',
+    'render_field',
+    'with_options'
+]
---
+++ packages/onegov.form/onegov.form-0.3.1.tar.gz/onegov.form-0.3.1/onegov/form/models.py
@@ -0,0 +1,120 @@
+from onegov.core.orm import Base
+from onegov.core.orm.mixins import TimestampMixin
+from onegov.core.orm.types import JSON, UUID
+from onegov.form.errors import UnableToComplete
+from onegov.form.parser import parse_form
+from sqlalchemy import Column, Enum, ForeignKey, Text
+from sqlalchemy.orm import (
+    deferred,
+    relationship,
+)
+from uuid import uuid4
+
+
+class FormDefinition(Base, TimestampMixin):
+    """ Defines a form stored in the database. """
+
+    __tablename__ = 'forms'
+
+    #: the name of the form (key, part of the url)
+    name = Column(Text, nullable=False, primary_key=True)
+
+    #: the title of the form
+    title = Column(Text, nullable=False)
+
+    #: metadata associated with the form, for storing small amounts of data
+    meta = Column(JSON, nullable=False, default=dict)
+
+    #: content associated with the form, for storing things like long texts
+    content = deferred(Column(JSON, nullable=False, default=dict))
+
+    #: the form as parsable string
+    definition = Column(Text, nullable=False)
+
+    #: the type of the form, this can be used to create custom polymorphic
+    #: subclasses. See `<http://docs.sqlalchemy.org/en/improve_toc/\
+    #: orm/extensions/declarative/inheritance.html>`_.
+    type = Column(Text, nullable=True)
+
+    #: link between forms and submissions
+    submissions = relationship('FormSubmission', backref='form')
+
+    __mapper_args__ = {
+        "polymorphic_on": 'type'
+    }
+
+    @property
+    def form_class(self):
+        """ Parses the form definition and returns a form class. """
+
+        return parse_form(self.definition)
+
+
+class FormSubmission(Base, TimestampMixin):
+    """ Defines a submitted form in the database. """
+
+    __tablename__ = 'submissions'
+
+    #: id of the form submission
+    id = Column(UUID, primary_key=True, default=uuid4)
+
+    #: name of the form this submission belongs to
+    name = Column(Text, ForeignKey(FormDefinition.name), nullable=False)
+
+    #: the source code of the form at the moment of submission. This is stored
+    #: alongside the submission as the original form may change later. We
+    #: want to keep the old form around just in case.
+    definition = Column(Text, nullable=False)
+
+    #: the submission data
+    data = Column(JSON, nullable=False)
+
+    #: the state of the submission
+    state = Column(
+        Enum('pending', 'complete', name='submission_state'),
+        nullable=False
+    )
+
+    __mapper_args__ = {
+        "polymorphic_on": 'state'
+    }
+
+    @property
+    def form_class(self):
+        """ Parses the form definition and returns a form class. """
+
+        return parse_form(self.definition)
+
+    def complete(self):
+        """ Changes the state to 'complete', if the data is valid. """
+
+        if not self.form_class(data=self.data).validate():
+            raise UnableToComplete()
+
+        self.state = 'complete'
+
+    def prune(self, form=None):
+        """ Pending submissions are not necessarily valid. This function
+        removes the invalid bits from the data if called.
+
+        On 'completed' submissions it has no effect.
+
+        """
+        if self.state == 'pending':
+
+            if not form:
+                form = self.form_class(data=self.data)
+                form.validate()
+
+            for field_id in form.errors:
+                if field_id in self.data:
+                    del self.data[field_id]
+                    form._fields[field_id].data = ''
+
+
+class PendingFormSubmission(FormSubmission):
+    __mapper_args__ = {'polymorphic_identity': 'pending'}
+
+
+class CompleteFormSubmission(FormSubmission):
+    __mapper_args__ = {'polymorphic_identity': 'complete'}
