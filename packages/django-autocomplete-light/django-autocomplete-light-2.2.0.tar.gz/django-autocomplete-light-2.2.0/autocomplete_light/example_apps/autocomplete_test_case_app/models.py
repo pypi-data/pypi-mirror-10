9d360d486b2f51c15b1acfae29903adc149e6b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:45 +0000

    Add package django-autocomplete-light/2.2.0/django-autocomplete-light-2.2.0.tar.gz

---
+++ packages/django-autocomplete-light/django-autocomplete-light-2.2.0.tar.gz/django-autocomplete-light-2.2.0/autocomplete_light/example_apps/autocomplete_test_case_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    groups = models.ManyToManyField('Group')
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class NonIntegerPk(models.Model):
+    name = models.CharField(primary_key=True, max_length=10)
+    relation = models.ForeignKey('self', null=True, blank=True)
+    noise = models.ForeignKey('basic.FkModel', null=True, blank=True)
+
+    for_inline = models.ForeignKey('self', null=True, blank=True,
+                                   related_name='inline')
