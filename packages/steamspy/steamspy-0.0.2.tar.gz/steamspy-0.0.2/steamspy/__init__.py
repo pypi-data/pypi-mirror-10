9c253d082eb7a81da663b780192afa89debd7fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:07 +0000

    Add package steamspy/0.0.2/steamspy-0.0.2.tar.gz

---
+++ packages/steamspy/steamspy-0.0.2.tar.gz/steamspy-0.0.2/steamspy/__init__.py
@@ -0,0 +1,2 @@
+from .methods import AppDetails, Genre, Top100In2Weeks, Top100Forever, \
+    Top100Owned, All
---
+++ packages/steamspy/steamspy-0.0.2.tar.gz/steamspy-0.0.2/steamspy/methods.py
@@ -0,0 +1,63 @@
+import inspect
+import requests
+from .objects import Game
+
+
+class APIHelper(object):
+    _API_URL = 'http://steamspy.com/api.php'
+    _attribute = None
+
+    def __init__(self):
+        _games = (Game(**game) for game in self._get().values())
+        if self._attribute:
+            self.games = sorted(
+                _games, reverse=True,
+                key=lambda game: getattr(game, self._attribute))
+        else:
+            self.games = list(_games)
+
+    def _get(self, params={}):
+        """
+        :param params: Parameters to pass to the steamspy query.
+        :type  params: ``dict``
+
+        :return: Steamspy API response object.
+        :rtype:  ``dict``
+        """
+        params['request'] = self.__class__.__name__.lower()
+        resp = requests.get(self._API_URL, params=params)
+        return resp.json()
+
+
+class AppDetails(APIHelper):
+    def __init__(self, appid):
+        """
+        :param appid: Steam AppID.
+        :type  appid: ``int``
+        """
+        self.game = Game(**self._get(params={'appid': appid}))
+
+
+class Genre(APIHelper):
+    def __init__(self, genre):
+        """
+        :param genre: Game genre.
+        :type  genre: ``str``
+        """
+        self.game = Game(**self._get(params={'genre': genre}))
+
+
+class Top100In2Weeks(APIHelper):
+    _attribute = 'players_2weeks'
+
+
+class Top100Forever(APIHelper):
+    _attribute = 'players_forever'
+
+
+class Top100Owned(APIHelper):
+    pass
+
+
+class All(APIHelper):
+    pass
