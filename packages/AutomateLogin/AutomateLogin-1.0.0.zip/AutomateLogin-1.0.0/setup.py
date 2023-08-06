37d3bd6c900a01adb22230b858b6e26a1cdb74fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:21 +0000

    Add package AutomateLogin/1.0.0/AutomateLogin-1.0.0.zip

---
+++ packages/AutomateLogin/AutomateLogin-1.0.0.zip/AutomateLogin-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+		name			='AutomateLogin',
+		version			='1.0.0',
+		py_modules		=['AutomateLogin'],
+		author			='kindian',
+		author_email	='patra.kailash06@gmail.com',
+		url				='http://www.headfirstlabs.com',
+		description		='A simple printer of automated lists',
+                package_data = { '':['*.txt']},
+		)
