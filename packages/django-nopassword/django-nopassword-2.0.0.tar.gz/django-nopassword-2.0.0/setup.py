ecbba5657d7a9fde9329a371c899ffe94c8d40ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package django-nopassword/2.0.0/django-nopassword-2.0.0.tar.gz

---
+++ packages/django-nopassword/django-nopassword-2.0.0.tar.gz/django-nopassword-2.0.0/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+
+def _read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except ImportError:
+        return None
+
+
+setup(
+    name="django-nopassword",
+    version='2.0.0',
+    url='http://github.com/relekang/django-nopassword',
+    author='Rolf Erik Lekang',
+    author_email='me@rolflekang.com',
+    description='Authentication backend for django that uses a one time code instead of passwords',
+    long_description=_read_long_description(),
+    packages=find_packages(exclude='tests'),
+    install_require=[
+        'django>=1.4',
+    ],
+    tests_require=[
+        'django>=1.4',
+        'twilio==4.4.0',
+        'mock>=1.0'
+    ],
+    license='MIT',
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ]
+)
