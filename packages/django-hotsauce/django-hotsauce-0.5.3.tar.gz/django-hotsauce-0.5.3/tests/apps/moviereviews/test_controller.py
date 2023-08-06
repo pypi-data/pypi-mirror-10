b1a952ce227929f8f3d69d86ce0e01c68aaa9334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:10 +0000

    Add package django-hotsauce/0.5.3/django-hotsauce-0.5.3.tar.gz

---
+++ packages/django-hotsauce/django-hotsauce-0.5.3.tar.gz/django-hotsauce-0.5.3/tests/apps/moviereviews/test_controller.py
@@ -0,0 +1,28 @@
+import sys
+#import mainapp
+from notmm.utils.wsgilib import HTTPRequest
+from test_support import (
+    WSGIControllerTestCase, 
+    TestClient, 
+    unittest,
+    make_app,
+    settings,
+    ResponseClass
+    )
+
+class WSGIAppTestCase(WSGIControllerTestCase):
+
+    def test_render_index(self):
+        response = self.client.get('/') # 
+        self.assertEqual(response.status_code, '200 OK')
+
+    def test_post_required_with_schevo_db(self):
+        req = HTTPRequest()
+        req.environ['REQUEST_METHOD'] = 'POST'
+
+        response = self.client.post('/test_post_required_with_schevo_db',
+            request=req, data={})
+        #print response.__name__
+        self.assertEqual(isinstance(response, ResponseClass), True, type(response))
+        self.assertEqual(response.status_code, '200 OK')
+
