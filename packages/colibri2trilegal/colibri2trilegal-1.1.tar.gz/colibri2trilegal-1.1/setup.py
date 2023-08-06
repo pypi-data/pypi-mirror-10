99c739369d8763d8e421277784603c9cc37c856e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:58 +0000

    Add package colibri2trilegal/1.1/colibri2trilegal-1.1.tar.gz

---
+++ packages/colibri2trilegal/colibri2trilegal-1.1.tar.gz/colibri2trilegal-1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='colibri2trilegal',
+    author='Phil Rosenfield',
+    author_email='philip.rosenfield@unipd.it',
+    version='1.1',
+    py_modules=['colibri2trilegal'],
+    scripts=['colibri2trilegal']
+)
