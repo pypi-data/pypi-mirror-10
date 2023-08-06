2a7ff4196c06bc97ba5495e925d3033fab35b424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:55 +0000

    Add package terminalcloud/0.2.3/terminalcloud-0.2.3.tar.gz

---
+++ packages/terminalcloud/terminalcloud-0.2.3.tar.gz/terminalcloud-0.2.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name = 'terminalcloud',
+      packages = ['terminalcloud'],
+      version = '0.2.3', description = 'Terminal.com API wrapper for python',
+      author = 'Enrique Conci',
+      author_email = 'e@cloudlabs.io',
+      url = 'https://github.com/terminalcloud/terminal-py',
+      keywords = ['terminal', 'terminal.com', 'API'],
+      classifiers = [])
