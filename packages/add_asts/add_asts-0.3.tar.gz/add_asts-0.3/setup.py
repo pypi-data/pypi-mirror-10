d023041ca378e375871b4c9a23697372235ac97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:55 +0000

    Add package add_asts/0.3/add_asts-0.3.tar.gz

---
+++ packages/add_asts/add_asts-0.3.tar.gz/add_asts-0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='add_asts',
+    author='Phil Rosenfield',
+    author_email='philip.rosenfield@unipd.it',
+    version='0.3',
+    py_modules=['asts'],
+    scripts=['asts'],
+    install_requires=['astropy', 'matplotlib', 'numpy', 'scipy']
+)
