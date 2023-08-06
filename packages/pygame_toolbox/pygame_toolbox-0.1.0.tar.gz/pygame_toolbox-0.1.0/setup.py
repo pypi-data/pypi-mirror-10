9d3b959c49ea75074eae282a3fe7c9502b7675b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:16 +0000

    Add package pygame_toolbox/0.1.0/pygame_toolbox-0.1.0.tar.gz

---
+++ packages/pygame_toolbox/pygame_toolbox-0.1.0.tar.gz/pygame_toolbox-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='pygame_toolbox',
+      version='0.1.0',
+      license='MIT',
+      description='Tools to help with game development using pygame\nFor' +
+                  ' complete details please reference the github page',
+      author='James Milam',
+      author_email='jmilam343@gmail.com',
+      url='https://github.com/jbm950/pygame_toolbox',
+      packages=find_packages(),
+      keywords='pygame'
+      )
