149abdb8c4715b2f3c5d22c44ac2db1ff273afbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package collective.emailconfirmationregistration/1.0.0a3/collective.emailconfirmationregistration-1.0.0a3.zip

---
+++ packages/collective.emailconfirmationregistration/collective.emailconfirmationregistration-1.0.0a3.zip/collective.emailconfirmationregistration-1.0.0a3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0a3'
+
+setup(name='collective.emailconfirmationregistration',
+      version=version,
+      description="",
+      long_description="%s\n%s" % (
+          open("README.rst").read(),
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      ),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
+      keywords='plone customizations',
+      author='Nathan Van Gheem',
+      author_email='vangheem@gmail.com',
+      url='https://github.com/collective/collective.emailconfirmationregistration',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.users>=2',
+          'validate_email',
+          'pyDNS'
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
