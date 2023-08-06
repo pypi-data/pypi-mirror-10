5f545c72fba4ef2ab8e09c0527fd4238101e2dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:31 +0000

    Add package militia/0.2/militia-0.2.tar.gz

---
+++ packages/militia/militia-0.2.tar.gz/militia-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='militia',
+      version='0.2',
+      description='Intelligent and intuitive applications for analysts',
+      url='http://github.com/vlall/militia',
+      author='V Lall, M Bartoli',
+      author_email='vishal.h.lall@gmail.com',
+      license='BSD',
+    packages=find_packages(exclude=['apps','tests']),
+      zip_safe=False)
