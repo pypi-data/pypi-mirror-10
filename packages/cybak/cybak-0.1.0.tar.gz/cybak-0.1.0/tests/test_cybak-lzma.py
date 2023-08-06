803017af1ac77cf9454d42358422335424289c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:36 +0000

    Add package cybak/0.1.0/cybak-0.1.0.tar.gz

---
+++ packages/cybak/cybak-0.1.0.tar.gz/cybak-0.1.0/tests/test_cybak-lzma.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_cybak-lzma
+----------------------------------
+
+Tests for `cybak-lzma` module.
+"""
+
+import unittest
+
+from cybak-lzma import cybak-lzma
+
+
+class TestCybak-lzma(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
