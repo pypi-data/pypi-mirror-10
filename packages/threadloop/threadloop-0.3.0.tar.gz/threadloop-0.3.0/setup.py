311c487b8f1563e88acb0cac7cbb64ef1ce4bf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:40 +0000

    Add package threadloop/0.3.0/threadloop-0.3.0.tar.gz

---
+++ packages/threadloop/threadloop-0.3.0.tar.gz/threadloop-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+install_requires = ['tornado']
+
+# if python < 3, we need futures backport
+try:
+    import concurrent.futures  # noqa
+except ImportError:
+    install_requires.append('futures')
+
+setup(
+    name='threadloop',
+    version='0.3.0',
+    author='Grayson Koonce',
+    author_email='breerly@gmail.com',
+    description='Tornado IOLoop Backed Concurrent Futures',
+    license='MIT',
+    url='https://github.com/breerly/threadloop',
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_requires
+)
