082900b9fa4b56ca589f5f9a19216fd9d4e85efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:12 +0000

    Add package minchin.text/5.0.1/minchin.text-5.0.1.zip

---
+++ packages/minchin.text/minchin.text-5.0.1.zip/minchin.text-5.0.1/CHANGELOG.md
@@ -0,0 +1,26 @@
+v 5.0 [2015-06-10]
+==================
+ - move package to `minchin.text`
+ - add tests for `minchin.text.centered()`
+ - add end character to progress bar
+ - add documentation to README.rst
+
+v 4.4 [2014-10-26]
+==================
+ - typographic changes to allow wmtext to work in Python 3
+
+v 4.1 [2014-03-10]
+==================
+ - allow access as a direct object.   
+     i.e. use `import wmtext` rather than `from wmtext import wmtext`
+
+v 4.0 [2014-02-06]
+==================
+ - packaged as a separate module
+ - added progress bar
+ - first public release
+ 
+v 3.2 [2014-02-02]
+==================
+ - add `length_no_ansi()` allowing you to determine the length of a string
+     after stripping out ANSI codes.
---
+++ packages/minchin.text/minchin.text-5.0.1.zip/minchin.text-5.0.1/setup.py
@@ -0,0 +1,48 @@
+
+import os
+import re
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+def get_version(filename="minchin/text.py"):
+    with open(os.path.join(base_dir, filename), encoding="utf-8") as initfile:
+        for line in initfile.readlines():
+            m = re.match("__version__ *= *['\"](.*)['\"]", line)
+            if m:
+                return m.group(1)
+
+setup(
+    name = "minchin.text",
+    version = get_version(),
+    description = "Python library for text formatting on the command line.",
+    long_description = "\n\n".join([open(os.path.join(base_dir, "README.rst")).read(), open(os.path.join(base_dir,"CHANGELOG.md")).read()]),
+    author = "W. Minchin",
+    author_email = "w_minchin@hotmail.com",
+    url = "https://github.com/MinchinWeb/minchin.text",
+	packages = find_packages(),
+    namespace_packages = ['minchin'],
+	include_package_data = True,
+    install_requires = [
+		"colorama >= 0.2.5"
+        ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: IronPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities'
+        ],
+)
