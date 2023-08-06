301c28261d2b1528b2c307481bb7b8a1bc633cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:53 +0000

    Add package kinto/0.2.2/kinto-0.2.2.tar.gz

---
+++ packages/kinto/kinto-0.2.2.tar.gz/kinto-0.2.2/kinto/tests/support.py
@@ -0,0 +1,36 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # NOQA
+
+import webtest
+from cliquet import utils
+from cliquet.tests import support as cliquet_support
+
+
+class BaseWebTest(object):
+
+    app = webtest.TestApp("config:config/kinto.ini",
+                          relative_to='.')
+
+    def __init__(self, *args, **kwargs):
+        super(BaseWebTest, self).__init__(*args, **kwargs)
+        self.app.RequestClass = cliquet_support.get_request_class(prefix="v0")
+        self.storage = self.app.app.registry.storage
+        self.storage.initialize_schema()
+        self.headers = {
+            'Content-Type': 'application/json',
+        }
+        self.headers.update(get_user_headers('mat'))
+
+    def tearDown(self):
+        super(BaseWebTest, self).tearDown()
+        self.storage.flush()
+
+
+def get_user_headers(user):
+    credentials = "%s:secret" % user
+    authorization = 'Basic {0}'.format(utils.encode64(credentials))
+    return {
+        'Authorization': authorization
+    }
