b5be4293925e24347501d471ba6281499295696a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:38 +0000

    Add package superParser/1.1.0/superParser-1.1.0.tar.gz

---
+++ packages/superParser/superParser-1.1.0.tar.gz/superParser-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	
+		name			= 'superParser',
+		version			= '1.1.0',
+		py_modules		= ['superParser'],
+		author			= 'Landpack',
+		author_email 	= 'landpack@sina.com',
+		url				= 'http://www.cnblogs.com/landpack/',
+		description		= 'A simple parser,you just tell me what item you want,and set a list for the parser,it is will return a dict.For more,please visit my blog !',
+	)
