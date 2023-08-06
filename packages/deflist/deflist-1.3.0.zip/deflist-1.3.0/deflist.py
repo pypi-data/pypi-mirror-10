dae570fc266f4897278117c5e310bd6ff002b744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:06 +0000

    Add package deflist/1.3.0/deflist-1.3.0.zip

---
+++ packages/deflist/deflist-1.3.0.zip/deflist-1.3.0/deflist.py
@@ -0,0 +1,14 @@
+"""
+在原有功能基础上，增加打印时没出现一层嵌套就缩进显示一个TAB制表符(增加了一个新的参数level(0，1，2))
+为了使模块功能变的更为灵活，把模块中必备的两个参数中的一个改为可选参数为它添加缺省值
+the last modify,为函数增加第三个参数(indent)，开始时这个参数值设置为False，也就是说默认情况下不打开缩进特性，当参数值为True时，显示缩进特性
+"""
+def print_list(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_list(each_item,indent,level+1) #每打印一次参数+1,表示下次打印列表时多增加一个TAB制表符
+        else:
+             if indent:
+                for tab_stop in range(level):
+                      print("\t",end=" ")
+            print(each_item)
