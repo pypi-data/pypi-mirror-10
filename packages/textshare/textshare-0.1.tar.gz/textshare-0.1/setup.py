56bfee0edd7fc5786e7de10e606935c97656a8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:55 +0000

    Add package textshare/0.1/textshare-0.1.tar.gz

---
+++ packages/textshare/textshare-0.1.tar.gz/textshare-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='textshare',
+      version='0.1',
+      description='share your text files easily',
+      url='http://github.com/bindingofisaac',
+      author='Vivek',
+      author_email='bindingofisaacs@gmail.com',
+      license='MIT',
+      packages=['textshare'],
+      zip_safe=False)
---
+++ packages/textshare/textshare-0.1.tar.gz/textshare-0.1/textshare/__init__.py
@@ -0,0 +1,2 @@
+def check_install():
+    return "Install Success!"
