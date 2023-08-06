cf2c108da0bf65b307fc1a831abe572ce927801b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package discovery/0.0.3/discovery-0.0.3.tar.gz

---
+++ packages/discovery/discovery-0.0.3.tar.gz/discovery-0.0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+import os
+
+from distutils.core import setup
+
+SCRIPT_DIR = os.path.dirname(__file__)
+if not SCRIPT_DIR:
+        SCRIPT_DIR = os.getcwd()
+
+
+# put together list of requirements to install
+install_requires = []
+with open(os.path.join(SCRIPT_DIR, 'requirements.txt')) as fh:
+        for line in fh.readlines():
+                    if line.startswith('-'):
+                                    continue
+
+        install_requires.append(line.strip())
+
+
+setup(name='discovery',
+      version='0.0.3',
+      description='Python library for getting values from ETCD',
+      author='Roberto Aguilar',
+      author_email='roberto@zymbit.com',
+      packages=['discovery'],
+      long_description=open('README.md').read(),
+      url='http://github.com/zymbit/discovery',
+      license='LICENSE.txt',
+      install_requires=install_requires
+)
