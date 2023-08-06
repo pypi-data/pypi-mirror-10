563a034dadd7fe9bec34659ed6097dcf636cdd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:09 +0000

    Add package argskwargs/0.0.2/argskwargs-0.0.2.tar.gz

---
+++ packages/argskwargs/argskwargs-0.0.2.tar.gz/argskwargs-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='argskwargs',
+    version='0.0.2',
+    author='Wouter Bolsterlee',
+    author_email='Wouter Bolsterlee',
+    py_modules=['argskwargs'],
+)
