26547611f80076645d84db4b35e0d22e42689387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:52 +0000

    Add package addhundred/1.0.0/addhundred-1.0.0.zip

---
+++ packages/addhundred/addhundred-1.0.0.zip/addhundred-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+ name = 'addhundred',
+ version = '1.0.0',
+ py_modules = ['addhundred'],
+ author = 'fpineda',
+ author_email = 'flyykidd3785@gmail.com',
+ description = 'A simple function that prints your number plus one hundred.',
+ )
