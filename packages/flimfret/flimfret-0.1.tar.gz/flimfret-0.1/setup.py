0cc4de1188d7f86e905dea1d8675482f27714ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:30 +0000

    Add package flimfret/0.1/flimfret-0.1.tar.gz

---
+++ packages/flimfret/flimfret-0.1.tar.gz/flimfret-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='flimfret',
+      version='0.1',
+      description='Pipeline for FLIM FRET analysis',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='alexmcasella@gmail.com',
+      license='MIT',
+      packages=['flimfret'],
+      classifiers=['Development Status :: 3 - Alpha','Programming Language :: Python :: 2.7'],
+      zip_safe=False)
