b69ee89dd68bcdd274dc74f89ba4879e4ee05b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:40 +0000

    Add package relate/0.21/relate-0.21.tar.gz

---
+++ packages/relate/relate-0.21.tar.gz/relate-0.21/src/__init__.py
@@ -0,0 +1,9 @@
+"""Implements a discrete mathematical relation in python"""
+
+__version__ = '0.21'
+from .relate import Relation
+from .relate import Isomorphism
+from .relate import Function
+from .relate import Partition
+from .relate import Relation_Tests
+
