76829159604b4e2d540bddc196ed1915b84bda32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:39 +0000

    Add package xlseries/0.1.3/xlseries-0.1.3.tar.gz

---
+++ packages/xlseries/xlseries-0.1.3.tar.gz/xlseries-0.1.3/xlseries/evaluation/evaluation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+evaluation
+
+TODO: This module has to be implemented. It will be responsible of evaluate
+data frames built by the existent strategies and asign a score based on some
+metaheruistic criteria.
+"""
