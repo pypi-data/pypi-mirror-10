879c90c47ff7277fac66f95b34eceed8a6759b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:04 +0000

    Add package urbandefinition/0.3/urbandefinition-0.3.tar.gz

---
+++ packages/urbandefinition/urbandefinition-0.3.tar.gz/urbandefinition-0.3/urbandefinition.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import argparse
+
+
+def get_definition(term):
+    r = requests.get('http://www.urbandictionary.com/define.php?term={}'.format(term))
+    soup = BeautifulSoup(r.content)
+    definition = soup.find(class_='def-panel')
+    meaning = definition.find(class_='meaning').text
+    print "Term: {}\n".format(term)
+    print "Meaning: {}".format(meaning)
+    example = definition.find(class_='example')
+    if example.text.strip() != '':
+        breaks = example.find_all('br')
+        for br in breaks:
+            br.replace_with('\n')
+        print "Example: {}".format(example.text)
+    else:
+        pass
+
+
+
+def command_line_runner():
+    parser = argparse.ArgumentParser(description="Get Urban Dictionary definitions from the terminal")
+    parser.add_argument('term', type=str, nargs='*', help='The term you want the urban definition for')
+    args = parser.parse_args()
+    if not args.term:
+        parser.print_help()
+    else:
+        get_definition(' '.join(args.term))
+
+
+if __name__ == '__main__':
+    command_line_runner()
