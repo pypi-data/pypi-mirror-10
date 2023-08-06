f75bb848d0aa7dad4d324ab91a031b79e0845a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:30 +0000

    Add package aws-portal/0.0/aws-portal-0.0.tar.gz

---
+++ packages/aws-portal/aws-portal-0.0.tar.gz/aws-portal-0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='aws-portal',
+  version='0.0',
+  author='Shubham Bhartiya',
+  author_email='bhartiya.shubham08@gmail.com',
+  scripts=['aws-portal'],
+  url='https://github.com/aasaanjobs/aws-portal.git',
+  install_requires=['boto', 'simplejson']
+)
