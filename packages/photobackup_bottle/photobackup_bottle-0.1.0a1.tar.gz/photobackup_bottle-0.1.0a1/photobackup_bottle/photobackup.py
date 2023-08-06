7c817a1e3f8428cd43347c8bd3f5ccd498bcf3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:59 +0000

    Add package photobackup_bottle/0.1.0a1/photobackup_bottle-0.1.0a1.tar.gz

---
+++ packages/photobackup_bottle/photobackup_bottle-0.1.0a1.tar.gz/photobackup_bottle-0.1.0a1/photobackup_bottle/photobackup.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# Copyright (C) 2013-2015 Stéphane Péchard.
+#
+# This file is part of PhotoBackup.
+#
+# PhotoBackup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PhotoBackup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""PhotoBackup Python server.
+
+Usage:
+  photobackup init
+  photobackup run
+  photobackup (-h | --help)
+  photobackup --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+
+# stlib
+import os
+import sys
+# pipped
+from bottle import abort, request, route, run, template
+import bottle
+from docopt import docopt
+from logbook import info, warn, error
+# local
+from . import __version__
+
+# run initiation routine
+arguments = docopt(__doc__, version='PhotoBackup ' + __version__)
+if (arguments['init']):
+    from . import install
+    install.install()
+    sys.exit("\nCreated, now launch PhotoBackup server with 'photobackup run'")
+
+# import potentially inexistant file
+try:
+    from .photobackup_settings import MEDIA_ROOT, PASSWORD, PORT
+except ImportError:
+    sys.exit("Settings file no created, please run 'photobackup init'")
+
+
+app = bottle.default_app()
+
+
+def end(code, message):
+    error(message)
+    abort(code, message)
+
+
+@route('/')
+def index():
+    here = os.path.abspath(os.path.dirname(__file__))
+    return template(os.path.join(here, 'index.html'))
+
+
+@route('/', method='POST')
+def save_image():
+    password = request.forms.get('password')
+    if password != PASSWORD:
+        end(403, "wrong password!")
+
+    upfile = request.files.get('upfile')
+    if not upfile:
+        end(401, "no file in the request!")
+
+    path = os.path.join(MEDIA_ROOT, upfile.raw_filename)
+    if not os.path.exists(path):
+        filesize = -1
+        try:
+            filesize = int(request.forms.get('filesize'))
+        except TypeError:
+            end(400, "missing file size in the request!")
+
+        # save file
+        info("upfile path: " + path)
+        upfile.save(path)
+
+        # check file size in request against written file size
+        if filesize != os.stat(path).st_size:
+            end(411, "file sizes do not match!")
+
+    else:
+        warn("file " + path + " already exists")
+
+
+@route('/test', method='POST')
+def test():
+    password = request.forms.get('password')
+    if password != PASSWORD:
+        end(403, "wrong password!")
+
+    if not os.path.exists(MEDIA_ROOT):
+        end(500, "MEDIA_ROOT does not exist!")
+
+    testfile = os.path.join(MEDIA_ROOT, '.test_file_to_write')
+    try:
+        with open(testfile, 'w') as tf:
+            tf.write('')
+    except:
+        end(500, "Can't write to MEDIA_ROOT!")
+    finally:
+        os.remove(testfile)
+        info("Test succeeded \o/")
+
+
+def main():
+    run(host="0.0.0.0", port=PORT, reloader=True)
+
+
+if __name__ == '__main__':
+    main()
