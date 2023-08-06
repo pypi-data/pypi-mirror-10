bb19ba779eec594b0d60c4fe4fafde3d28f126bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:25 +0000

    Add package mutablerecord/0.2/mutablerecord-0.2.tar.gz

---
+++ packages/mutablerecord/mutablerecord-0.2.tar.gz/mutablerecord-0.2/__init__.py
@@ -0,0 +1,14 @@
+from .record import \
+    Required, \
+    DataError, \
+    Validator, \
+    MustSatisfy, \
+    InstanceOf, \
+    InstanceOrNone, \
+    OneOf, \
+    Length, \
+    MutableRecord, \
+    MutableRecordSet, \
+    FieldView, \
+    FieldViewDescriptor, \
+    MutableRecordType
---
+++ packages/mutablerecord/mutablerecord-0.2.tar.gz/mutablerecord-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='mutablerecord',
+      description='recordset framework for small python structures',
+      version='0.2',
+      author='Alex Flint',
+      author_email='alex.flint@gmail.com',
+      url='https://github.com/alexflint/mutablerecord',
+      packages=['mutablerecord'],
+      package_dir={'mutablerecord': '.'},
+      )
