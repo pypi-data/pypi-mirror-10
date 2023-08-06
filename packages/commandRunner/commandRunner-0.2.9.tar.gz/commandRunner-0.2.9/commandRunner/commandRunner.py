eb22a3497c011fdd09296967572a66f9a19716c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:26 +0000

    Add package commandRunner/0.2.9/commandRunner-0.2.9.tar.gz

---
+++ packages/commandRunner/commandRunner-0.2.9.tar.gz/commandRunner-0.2.9/commandRunner/commandRunner.py
@@ -0,0 +1,122 @@
+import os
+import re
+import types
+from subprocess import call
+
+
+class commandRunner():
+
+    tmp_id = None
+    tmp_path = None
+    in_glob = None
+    out_glob = None
+    command = None
+    data = None
+    output_data = None
+    in_path = None
+    out_path = None
+    path = None
+    flags = None
+    options = None
+
+    def __init__(self, **kwargs):
+        '''
+            Constructs a local job
+            takes
+            tmp_id=, tmp_path=,
+            in_glob=, out_glob=,
+            command=, input_data=
+        '''
+
+        # if anything is passed it must be a string
+        for key, value in kwargs.items():
+            if key is 'flags' or key is 'options':
+                continue
+            if not isinstance(value, str) and value is not None:
+                raise TypeError('Argument {} not a string: {}'.format(key,
+                                                                      value))
+
+        if os.path.isdir(kwargs['tmp_path']):
+            self.tmp_path = kwargs.pop('tmp_path', '')
+        else:
+            raise OSError('tmp_path provided does not exist')
+
+        self.tmp_id = kwargs.pop('tmp_id', '')
+        self.in_glob = kwargs.pop('in_glob', '')
+        self.out_glob = kwargs.pop('out_glob', '')
+        self.data = kwargs.pop('input_data', '')
+        self.flags = kwargs.pop('flags', None)
+        self.options = kwargs.pop('options', None)
+        self.tmp_path = re.sub("/$", '', self.tmp_path)
+        self.path = self.tmp_path+"/"+self.tmp_id+"/"
+        if self.in_glob is not None:
+            self.in_glob = re.sub("^\.", '', self.in_glob)
+            self.in_path = self.path+self.tmp_id+"."+self.in_glob
+        self.out_glob = re.sub("^\.", '', self.out_glob)
+        self.out_path = self.path+self.tmp_id+"."+self.out_glob
+        self.command = self.__translate_command(kwargs.pop('command', ''))
+
+        # ensure we have an in_glob if we have been passed data
+        if self.data is not None and self.in_glob is None:
+            raise ValueError('in_glob missing but data provided')
+
+        if self.tmp_path is None:
+            raise ValueError('tmp_path missing')
+
+        if "$INPUT" in self.command and self.in_glob is None:
+            raise ValueError("$INPUT present in command "
+                             "but no in_glob provided")
+        if "$FLAGS" in self.command and self.flags is None:
+            raise ValueError("$FLAGS present in command "
+                             "but no flags () provided")
+
+        if "$OPTIONS" in self.command and self.options is None:
+            raise ValueError("$OPTIONS present in command "
+                             "but no options {{ }} provided")
+
+        if self.command is None:
+            raise ValueError('command is required')
+
+    def __translate_command(self, command):
+        '''
+            takes the command string and substitutes the relevant files names
+        '''
+        # interpolate the file names if needed
+        command = command.replace("$OUTPUT", self.out_path)
+        if self.in_path is not None:
+            command = command.replace("$INPUT", self.in_path)
+
+        flags_str = ""
+        if self.flags is not None:
+            for flag in self.flags:
+                flags_str += flag+" "
+        flags_str = flags_str[:-1]
+        command = command.replace("$FLAGS", flags_str)
+
+        options_str = ""
+        if self.options is not None:
+            for key, value in sorted(self.options.items()):
+                options_str += key+" "+value+" "
+        options_str = options_str[:-1]
+        command = command.replace("$OPTIONS", options_str)
+        return(command)
+
+    def prepare(self):
+        '''
+            Makes a directory and then moves the input data file there
+        '''
+        raise NotImplementedError
+
+    def run_cmd(self):
+        '''
+            run the command we constructed when the object was initialised.
+            If exit is 0 then pass back if not decide what to do next. (try
+            again?)
+        '''
+        raise NotImplementedError
+
+    def tidy(self):
+        '''
+            Delete everything in the tmp dir and then remove the temp dir
+        '''
+        raise NotImplementedError
