d5333c23ee421fce4e96cec760704692c925ab77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:52 +0000

    Add package stis_cti/0.4-beta4/stis_cti-0.4-beta4.tar.gz

---
+++ packages/stis_cti/stis_cti-0.4-beta4.tar.gz/stis_cti-0.4-beta4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+import numpy
+
+StisPixCteCorr_module = Extension('stis_cti.StisPixCte_FixY',
+    sources=['src/StisFixYCte.c', 'src/StisPixCteCorr_funcs.c', 'src/StisPixCte_FixY.c'],
+    include_dirs=[numpy.get_include(), 'src/'])
+
+setup(
+    name = 'stis_cti',
+    url = 'https://grit.stsci.edu/lockwood/cti_wrapper/repository/archive',
+    version = '0.4_beta4',
+    description = 'Apply pixel-based CTI-correction to HST/STIS CCD data',
+    author = 'Sean Lockwood, Phil Hodge, Pey Lian Lim and ' + \
+             'W.J. Hack (Python), J. Anderson (Fortran), Matt Davis',
+    author_email = 'help@stsci.edu',
+    maintainer = 'Sean Lockwood',
+    maintainer_email = 'lockwood@stsci.edu',
+    keywords = ['astronomy'],
+    classifiers = ['Programming Language :: Python',
+                   'Development Status :: 4 - Beta',
+                   'Operating System :: OS Independent',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: BSD License',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+    packages = ['stis_cti'],
+    install_requires = ['setuptools', 'numpy', 'astropy>= 1.0.1', 'stistools>= 1.0.2', 
+        'refstis>= 0.5.0', 'crds>= 1.3.0', 'stistools>= 1.0.2', 'stsci.tools>= 3.2.2'],
+    scripts = ['scripts/stis_cti', 'scripts/archive_dark_query'],
+    package_data = {'stis_cti': ['data/*.fits']},
+    ext_modules = [StisPixCteCorr_module],
+    )
