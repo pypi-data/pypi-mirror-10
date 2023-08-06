c4c1398e3cef057f629c18d1e7d9ccd1dffcd0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:40 +0000

    Add package port/0.1.0/port-0.1.0.tar.gz

---
+++ packages/port/port-0.1.0.tar.gz/port-0.1.0/port/fs.py
@@ -0,0 +1,94 @@
+import os
+
+
+class FileManager():
+    """
+    For interfacing with the file system,
+    used to keep paths consistent
+    """
+
+    def __init__(self, site_dir):
+        self.site_dir = os.path.expanduser(site_dir)
+        self.build_dir = os.path.join(self.site_dir, '.build')
+        self.rss_dir = os.path.join(self.build_dir, '.rss')
+        self.asset_dir = os.path.join(self.site_dir, 'assets')
+        self.index_dir = os.path.join(self.build_dir, '.searchindex')
+
+    def rss_path(self, category):
+        """
+        Path to compiled RSS files for category or 'all'
+        """
+        return os.path.join(self.rss_dir, '{}.xml'.format(category))
+
+    def post_path(self, build_slug):
+        """
+        Path to a compiled post
+        """
+        return os.path.join(self.build_dir, build_slug + '.json')
+
+    def category_dir(self, category):
+        """
+        Path to a compiled category
+        """
+        return os.path.join(self.build_dir, category)
+
+    def category_meta_path(self, category):
+        """
+        Path to compiled category meta
+        """
+        return os.path.join(self.build_dir, category, 'meta.json')
+
+    def raw_category_meta_path(self, category):
+        """
+        Path to uncompiled category meta
+        Returns None if no meta is specified
+        """
+        meta_path = os.path.join(self.site_dir, category, 'meta.yaml')
+        if not os.path.exists(meta_path):
+            return None
+        return meta_path
+
+    def raw_categories(self):
+        """
+        Uncompiled category names
+        """
+        return [c for c in os.listdir(self.site_dir)
+                if os.path.isdir(os.path.join(self.site_dir, c))
+                and c != 'assets'
+                and not c.startswith('.')]
+
+    def raw_for_category(self, category):
+        """
+        Paths for uncompiled posts for a category
+        """
+        path = os.path.join(self.site_dir, category)
+        return [os.path.join(path, f) for f in os.listdir(path)
+                                      if f.endswith('.md')]
+
+    def posts_for_category(self, category):
+        """
+        Paths for compiled posts for a category
+        """
+        cat_dir = self.category_dir(category)
+        return [os.path.join(cat_dir, f) for f in os.listdir(cat_dir)
+                                         if f.endswith('.json')
+                                         and f != 'meta.json'
+                                         and not f.startswith('D')]
+
+    def find_post(self, category, slug):
+        """
+        Find compiled post matching the category and slug
+        """
+        cat_dir = self.category_dir(category)
+
+        # meh
+        for f in os.listdir(cat_dir):
+            if slug in f:
+                return os.path.join(cat_dir, f)
+
+    def categories(self):
+        """
+        Compiled category names
+        """
+        return [c for c in os.listdir(self.build_dir)
+                  if not c.startswith('.')]
