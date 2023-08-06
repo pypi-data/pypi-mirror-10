7ca9631e0ab35fe815c2c0155c8d9764f69ad181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:27 +0000

    Add package carpentry-ci/0.0.32/carpentry-ci-0.0.32.tar.gz

---
+++ packages/carpentry-ci/carpentry-ci-0.0.32.tar.gz/carpentry-ci-0.0.32/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ast
+import os
+from setuptools import setup, find_packages
+
+
+local_file = lambda *f: \
+    open(os.path.join(os.path.dirname(__file__), *f)).read()
+
+
+class VersionFinder(ast.NodeVisitor):
+    VARIABLE_NAME = 'version'
+
+    def __init__(self):
+        self.version = None
+
+    def visit_Assign(self, node):
+        try:
+            if node.targets[0].id == self.VARIABLE_NAME:
+                self.version = node.value.s
+        except:
+            pass
+
+
+def read_version():
+    finder = VersionFinder()
+    finder.visit(ast.parse(local_file('carpentry', 'version.py')))
+    return finder.version
+
+
+requirements = [
+    'ansi2html==1.1.0',
+    'ansiconv==1.0.0',
+    'bcrypt==1.1.1',
+    'blist==1.3.6',
+    'cffi==1.1.2',
+    'pycrypto==2.6',
+    'GitHub-Flask==2.0.1',
+    'lineup==0.1.4',
+    'milieu==0.1.5',
+    'plant==0.1.1',
+    'pyOpenSSL==0.15.1',
+    'python-dateutil==2.4.2',
+    'redis==2.8.0',
+    'requests==2.5.1',
+    'tumbler==0.0.19',
+    'gittle==0.5.0',
+    'coloredlogs==1.0.1',
+    'cqlengine==0.21.0'
+]
+
+setup(
+    name='carpentry-ci',
+    version='0.0.32',
+    description='continuous integration for the people',
+    entry_points={
+        'console_scripts': ['carpentry = carpentry.cli:main'],
+    },
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://falcao.it/carpentry',
+    packages=find_packages(exclude=['*tests*']),
+    # package_data={'carpentry': [
+    #     'recursive-include carpentry/templates *'
+    #     'recursive-include carpentry/static *'
+    # ]},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+)
