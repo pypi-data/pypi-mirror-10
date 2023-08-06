f9df014fd5178369f6da6c630c1a4450f79a26b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:47 +0000

    Add package openstacksdk-plugin/0.2.0/openstacksdk-plugin-0.2.0.tar.gz

---
+++ packages/openstacksdk-plugin/openstacksdk-plugin-0.2.0.tar.gz/openstacksdk-plugin-0.2.0/doc/source/installation.rst
@@ -0,0 +1,12 @@
+============
+Installation
+============
+
+At the command line::
+
+    $ pip install openstacksdk-plugin
+
+Or, if you have virtualenvwrapper installed::
+
+    $ mkvirtualenv openstacksdk-plugin
+    $ pip install openstacksdk-plugin
---
+++ packages/openstacksdk-plugin/openstacksdk-plugin-0.2.0.tar.gz/openstacksdk-plugin-0.2.0/openstacksdk_plugin/tests/unit/example/test_example_service.py
@@ -0,0 +1,28 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import testtools
+
+from openstacksdk_plugin.example import example_service
+
+
+class TestExampleService(testtools.TestCase):
+
+    def test_service(self):
+        sut = example_service.ExampleService()
+        self.assertEqual('vendor:service', sut.service_type)
+        self.assertEqual('public', sut.visibility)
+        self.assertIsNone(sut.region)
+        self.assertIsNone(sut.service_name)
+        self.assertEqual(1, len(sut.valid_versions))
+        self.assertEqual('v1', sut.valid_versions[0].module)
+        self.assertEqual('v1', sut.valid_versions[0].path)
