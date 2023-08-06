922e31a4bb8ac32d67493fbf24de5d356f5b741d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:49 +0000

    Add package asynqp/0.4/asynqp-0.4-py3-none-any.whl

---
+++ packages/asynqp/asynqp-0.4-py3-none-any.whl/asynqp/exceptions.py
@@ -0,0 +1,44 @@
+from ._exceptions import AMQPError
+from .spec import EXCEPTIONS, CONSTANTS_INVERSE
+
+
+__all__ = [
+    "AMQPError",
+    "ConnectionClosedError",
+    "ConnectionLostError",
+    "UndeliverableMessage",
+    "Deleted"
+]
+__all__.extend(EXCEPTIONS.keys())
+
+
+class ConnectionClosedError(ConnectionError):
+    '''
+    Connection was closed normally by either the amqp server
+    or the client.
+    '''
+    pass
+
+
+class ConnectionLostError(ConnectionClosedError):
+    '''
+    Connection was closed unexpectedly
+    '''
+    pass
+
+
+class UndeliverableMessage(ValueError):
+    pass
+
+
+class Deleted(ValueError):
+    pass
+
+
+globals().update(EXCEPTIONS)
+
+
+def _get_exception_type(reply_code):
+    name = CONSTANTS_INVERSE[reply_code]
+    classname = ''.join([x.capitalize() for x in name.split('_')])
+    return EXCEPTIONS[classname]
