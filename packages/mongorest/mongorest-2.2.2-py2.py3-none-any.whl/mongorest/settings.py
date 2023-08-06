653174d258b8a5409c704fc6564925ef359e3be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:55 +0000

    Add package mongorest/2.2.2/mongorest-2.2.2-py2.py3-none-any.whl

---
+++ packages/mongorest/mongorest-2.2.2-py2.py3-none-any.whl/mongorest/settings.py
@@ -0,0 +1,76 @@
+# -*- encoding: UTF-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from inspect import getmembers
+from os import environ, path
+
+__all__ = [
+    'settings',
+]
+
+
+DEFAULT = {
+    'AUTH_COLLECTION': None,
+    'CORS': {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Accept,Accept-Encoding,Authorization,'
+                                        'Content-Length,Content-Type,Origin,'
+                                        'User-Agent,X-CSRFToken,'
+                                        'X-Requested-With',
+        'Access-Control-Allow-Credentials': 'false',
+    },
+    'MIDDLEWARES': [],
+    'MONGODB': {
+        'URI': '',
+        'USERNAME': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'HOSTS': [],
+        'PORT': 27017,
+        'PORTS': [],
+        'DATABASE': 'mongorest',
+        'OPTIONS': [],
+    },
+    'SESSION_STORE': None,
+}
+
+
+class Settings(object):
+    """
+    Settings Class
+    Will be responsible for loading the DEFAULT settings and USER settings
+    Uses the environment variable MONGOREST_SETTINGS_MODULE to find where the
+    Settings are store
+    """
+
+    def __getattr__(self, attr):
+        self._settings = DEFAULT
+
+        settings_module = environ.get('MONGOREST_SETTINGS_MODULE')
+        if settings_module:
+            try:
+                from importlib import import_module
+                loaded_settings = import_module(settings_module)
+            except ImportError:
+                source = '{0}.py'.format(settings_module.replace('.', '/'))
+
+                from imp import load_source
+                loaded_settings = load_source('settings', path.abspath(source))
+
+            self._settings = dict(
+                self._settings,
+                **dict(
+                    (name, setting)
+                    for (name, setting) in getmembers(loaded_settings)
+                    if name.isupper()
+                )
+            )
+
+        if attr not in self._settings:
+            raise AttributeError('Invalid setting: \'{0}\''.format(attr))
+
+        return self._settings[attr]
+
+
+settings = Settings()
