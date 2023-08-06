465c449c18e31f99cefb0c94745f853a32e66759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package terminalplot/0.2.5/terminalplot-0.2.5.tar.gz

---
+++ packages/terminalplot/terminalplot-0.2.5.tar.gz/terminalplot-0.2.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+exec(open('terminalplot/version.py').read())
+
+setup(
+    name='terminalplot',
+    version=__version__,
+    description='Plot points in terminal',
+    url='http://github.com/kressi/terminalplot',
+    author='Michael Kressibucher',
+    author_email='michael.kressibucher@gmail.com',
+    license='GPL',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
+    keywords='plot terminal graph console',
+    packages=['terminalplot'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': ['plot = terminalplot.command:main']
+    },
+    test_suite='nose.collector',
+    tests_require=['nose']
+)
