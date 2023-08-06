e3444e2d899b4da52a57fa3f3c1cf79ea8729ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package thorpy/1.4.2/thorpy-1.4.2.zip

---
+++ packages/thorpy/thorpy-1.4.2.zip/thorpy-1.4.2/thorpy/elements/image.py
@@ -0,0 +1,23 @@
+from thorpy.elements.element import Element
+from thorpy.painting.painters.imageframe import ImageFrame
+from thorpy.painting.painters.basicframe import BasicFrame
+from thorpy.miscgui import style, functions
+
+
+class Image(Element):
+
+    def __init__(self, path=None, color=None, elements=None, normal_params=None):
+        super(Image, self).__init__("", elements, normal_params)
+        if path:
+            painter = ImageFrame(path, mode=None)
+        else:
+            if color:
+                painter = BasicFrame(style.SIZE, color)
+            else:
+                raise Exception("You must specify either a path or a color")
+        self.set_painter(painter)
+
+    def set_alpha(self, alpha):
+        img = self.get_image()
+        img.set_alpha(alpha)
+        self.set_image(img)
