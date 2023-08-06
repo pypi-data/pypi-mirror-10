a730409c9bda142a70aef2fcc2f4dbbe62482aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:29 +0000

    Add package cHaversine/0.1.4/cHaversine-0.1.4.tar.gz

---
+++ packages/cHaversine/cHaversine-0.1.4.tar.gz/cHaversine-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name = 'cHaversine',
+    packages = ['cHaversine'],
+    version = '0.1.4',
+    description = 'Fast haversine calculation',
+    author = 'Eric Jiang',
+    author_email = 'eric@doublemap.com',
+    install_requires = ['Cython'],
+    url = 'https://github.com/doublemap/cHaversine',
+    keywords = ['math', 'geo', 'cython'],
+    classifiers = ['License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'],
+    ext_modules = cythonize("cHaversine/cHaversine.pyx")
+)
