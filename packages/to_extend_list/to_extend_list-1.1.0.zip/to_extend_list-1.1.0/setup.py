2618e321c0d6cb1eb03428c1db1d19423e1a8d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:06 +0000

    Add package to_extend_list/1.1.0/to_extend_list-1.1.0.zip

---
+++ packages/to_extend_list/to_extend_list-1.1.0.zip/to_extend_list-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+            name                = 'to_extend_list',
+            version             = '1.1.0',
+            py_modules          = ['to_extend_list'],
+            author              = 'BruceLee',
+            author_email        = '531047675@qq.com',
+            url                 = 'http://www.headfirstlabs.com',
+            description         = 'a sample to display how to extend a build in type',
+
+
+    )
---
+++ packages/to_extend_list/to_extend_list-1.1.0.zip/to_extend_list-1.1.0/to_extend_list.py
@@ -0,0 +1,35 @@
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+    (minute,second) =  time_string.split(splitter)
+
+    return (minute  + '.' + second)
+
+
+class AthleteList(list):
+
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self]))[0:3])
+        
+
+  
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline()
+        templ = data.strip().split(',')
+        return(AthleteList(templ.pop(0), templ.pop(0) ,templ))
+    except IOError as ioerr:
+        print('File error: ' + str(ioerr))
+        return(None)
+
