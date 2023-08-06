df8e24eae8c9f4940a80a3cad63b3e7de187fdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package astronomicon/0.0.1/astronomicon-0.0.1.zip

---
+++ packages/astronomicon/astronomicon-0.0.1.zip/astronomicon-0.0.1/astronomicon.py
@@ -0,0 +1,25 @@
+def hello (one, two):
+    print ("one");
+    print (one);
+    print ("two");   
+    print (two);
+    
+def get_int(one, two):
+    return int(one / two);
+
+def get_mod(one, two):
+    return (one % two);
+
+    
+   
+print ("Hello Python");
+
+hello(1,2);
+
+varr = get_int(100,3)
+
+print (varr);
+
+modd = get_mod(10,7)
+
+print (modd);
