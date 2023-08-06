15eb6ba0097920f099e965d03e24fcb97ccbe692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:00 +0000

    Add package PyMailMan/0.1/PyMailMan-0.1.tar.gz

---
+++ packages/PyMailMan/PyMailMan-0.1.tar.gz/PyMailMan-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'PyMailMan',
+    version = '0.1',
+    keywords = ('mail'),
+    description = 'A simple tool for sending mail by Python',
+    license = 'MIT License',
+    install_requires = [],
+
+    author = 'kongkongyzt',
+    author_email = 'kongkongyzt@gmail.com',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
