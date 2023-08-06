d1274b8e4934b2337efd7e39a8a7675c98eaf919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:00 +0000

    Add package Products.ContentWellPortlets/4.3.0/Products.ContentWellPortlets-4.3.0.tar.gz

---
+++ packages/Products.ContentWellPortlets/Products.ContentWellPortlets-4.3.0.tar.gz/Products.ContentWellPortlets-4.3.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+version = '4.3.0'
+
+
+setup(
+    name='Products.ContentWellPortlets',
+    version=version,
+    description="A Plone product that enables you to add portlets to the "
+                "central column in a page",
+    long_description="%s\n%s" % (
+        open("README.rst").read(), open("CHANGES.rst").read()),
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='plone portletmanager',
+    author='WebLion',
+    author_email='support@weblion.psu.edu',
+    url='http://weblion.psu.edu',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlone>=4.0b1'
+    ],
+    extras_require=dict(
+        test=[
+            'Products.PloneTestCase',
+        ]
+    ),
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
