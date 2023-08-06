acfaf983c28d2df99f0acb28f0e786c399801a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:27 +0000

    Add package bdot/0.1.0/bdot-0.1.0.tar.gz

---
+++ packages/bdot/bdot-0.1.0.tar.gz/bdot-0.1.0/bdot/carray.py
@@ -0,0 +1,24 @@
+# internal imports
+from bdot import carray_ext
+
+# external imports
+import numpy as np
+import bcolz
+
+class carray(bcolz.carray):
+
+	def dot(self, matrix):
+
+		# check dtype compatibility
+		if self.dtype.type != matrix.dtype.type:
+			raise ValueError("inputs must have the same dtype. Found {0} and {1}".format(self.dtype, matrix.dtype))
+
+		# check shape
+		if( (self.shape[1] != matrix.shape[0]) and 
+			(type(matrix) == carray) and (self.shape[1] != matrix.shape[1])):
+			raise ValueError("inputs must have compatible shapes. Found {0} and {1}".format(self.shape, matrix.shape))
+
+		if type(matrix) == np.ndarray:
+			return carray_ext._dot(self, matrix)
+		else:
+			return carray_ext._dot_mat_carray(self, matrix, matrix[0])
