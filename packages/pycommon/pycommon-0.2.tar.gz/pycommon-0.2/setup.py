f3b218d1fb6bd476d091a7e1984111ee7a05096a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:49 +0000

    Add package pycommon/0.2/pycommon-0.2.tar.gz

---
+++ packages/pycommon/pycommon-0.2.tar.gz/pycommon-0.2/setup.py
@@ -0,0 +1,16 @@
+"""
+Setup script
+"""
+from distutils.core import setup
+setup(
+    name='pycommon',
+    packages=['pycommon'],
+    version='0.2',
+    description='Miscellaneous utilties',
+    author='David Grant',
+    author_email='davidgrant@gmail.com',
+    url='https://github.com/dgrant/pycommon',
+    download_url='https://github.com/dgrant/pycommon/tarball/0.1',
+    keywords=['utility', 'miscellaneous', 'library'],
+    classifiers=[],
+)
