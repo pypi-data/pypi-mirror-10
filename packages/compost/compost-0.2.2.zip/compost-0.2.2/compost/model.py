49c1e791b6ab9ed5ea467bacb706835e9b3d7133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:24 +0000

    Add package compost/0.2.2/compost-0.2.2.zip

---
+++ packages/compost/compost-0.2.2.zip/compost-0.2.2/compost/model.py
@@ -0,0 +1,32 @@
+from pandas import Series, DataFrame
+from compost import Dataset
+
+class DailyAverageModel(object):
+    def __init__(self, source_data, cumulative=False):
+        data = Dataset(source_data, 60*60*24, cumulative).interpolate()
+        source = data.measurements.diff().value[1:]
+        self.result = source.mean()
+
+    def prediction(self, index):
+        return Series(self.result, index=index)
+
+class WeekdayAverageModel(object):
+    def __init__(self, source_data, cumulative=False):
+        data = Dataset(source_data, 60*60*24, cumulative).interpolate()
+        source = data.measurements.diff().value[1:].groupby(data.measurements[1:].index.weekday)
+        self.parameters = source.mean()
+        self.parameters.name = ['value']
+
+    def prediction(self, index):
+        return DataFrame(index=index).join(self.parameters, on=index.weekday).value
+
+
+class MonthlyAverageModel(object):
+    def __init__(self, source_data, cumulative=False):
+        data = Dataset(source_data, 60*60*24, cumulative).interpolate()
+        source = data.measurements.diff().value[1:].groupby(data.measurements[1:].index.month)
+        self.parameters = source.mean()
+        self.parameters.name = ['value']
+
+    def prediction(self, index):
+        return DataFrame(index=index).join(self.parameters, on=index.month).value
