a5567d81a674200cdf07af790f80e61201da9591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:12 +0000

    Add package iamine/0.3.0/iamine-0.3.0.tar.gz

---
+++ packages/iamine/iamine-0.3.0.tar.gz/iamine-0.3.0/iamine/__init__.py
@@ -0,0 +1,21 @@
+"""
+IA Mine
+~~~~~~~
+
+Internet Archive data mining tools.
+
+:copyright: (c) 2015 by Internet Archive.
+:license: AGPL 3, see LICENSE for more details.
+
+"""
+__title__ = 'iamine'
+__version__ = '0.3.0'
+__author__ = 'Jacob M. Johnson'
+__license__ = 'AGPL 3'
+__copyright__ = 'Copyright 2015 Internet Archive'
+
+
+from .api import mine_items, search, mine_urls, configure
+
+
+__all__ = ['mine_items', 'search', 'mine_urls', '__version__', 'configure']
