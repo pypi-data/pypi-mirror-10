7f415df8ae84bea46ae7da447c910f41f94d518c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:25 +0000

    Add package nester_jackhsu_mac/1.0.0/nester_jackhsu_mac-1.0.0.tar.gz

---
+++ packages/nester_jackhsu_mac/nester_jackhsu_mac-1.0.0.tar.gz/nester_jackhsu_mac-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name        = "nester_jackhsu_mac",
+    version     = "1.0.0",
+    py_module   = ["nester_jackhsu_mac"],
+    author      = "jackhsu",
+      )
