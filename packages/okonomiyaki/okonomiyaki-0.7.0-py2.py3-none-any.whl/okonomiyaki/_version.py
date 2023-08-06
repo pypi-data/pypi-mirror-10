46ea4ec7df8c965b18e279758f4eca2be4a1d0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:44 +0000

    Add package okonomiyaki/0.7.0/okonomiyaki-0.7.0-py2.py3-none-any.whl

---
+++ packages/okonomiyaki/okonomiyaki-0.7.0-py2.py3-none-any.whl/okonomiyaki/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM OKONOMIYAKI SETUP.PY
+version = '0.7.0'
+full_version = '0.7.0'
+git_revision = '55f658db1aa1315cd904b3f6d693900e06f38332'
+is_released = True
+
+if not is_released:
+    version = full_version
