6e5204dc8efda18b88c34bf92304f7cece292014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package django-pgfuzzy/0.1/django-pgfuzzy-0.1.tar.gz

---
+++ packages/django-pgfuzzy/django-pgfuzzy-0.1.tar.gz/django-pgfuzzy-0.1/pgfuzzy/__init__.py
@@ -0,0 +1,34 @@
+from django.db import connection
+
+def soundex(text):
+    cursor = connection.cursor()
+    cursor.execute("SELECT soundex(%s)", [text])
+    row = cursor.fetchone()
+    return row[0]
+
+def sounds_like(txt1, txt2):
+    return soundex(txt1) == soundex(txt2)
+
+def difference(text, text1):
+    cursor = connection.cursor()
+    cursor.execute("SELECT difference(%s, %s)", [text, text1])
+    row = cursor.fetchone()
+    return row[0]
+
+def levenshtein(source, target):
+    cursor = connection.cursor()
+    cursor.execute("SELECT levenshtein(%s, %s)", [source, target])
+    row = cursor.fetchone()
+    return row[0]
+
+def metaphone(text, max_output_length):
+    cursor = connection.cursor()
+    cursor.execute("SELECT metaphone(%s, %s)", [text, max_output_length])
+    row = cursor.fetchone()
+    return row[0]
+
+def dmetaphone(text):
+    cursor = connection.cursor()
+    cursor.execute("SELECT dmetaphone(%s)", [text])
+    row = cursor.fetchone()
+    return row[0]
