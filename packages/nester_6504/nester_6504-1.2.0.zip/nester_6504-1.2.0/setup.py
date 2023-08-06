59fbed747adde9219cb0728cccac76544074f8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:59 +0000

    Add package nester_6504/1.2.0/nester_6504-1.2.0.zip

---
+++ packages/nester_6504/nester_6504-1.2.0.zip/nester_6504-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_6504',
+    version = '1.2.0',
+    py_modules = ['nester_6504'],
+    author = 'hfpython',
+    author_email = 'hfython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    desciption = 'A simple printer of nested lists',
+    )
