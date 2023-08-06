895a337ddacd68571ee51baf3fb309fb302c33fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package collective.easyform/1.0a3/collective.easyform-1.0a3.zip

---
+++ packages/collective.easyform/collective.easyform-1.0a3.zip/collective.easyform-1.0a3/CHANGES.rst
@@ -0,0 +1,46 @@
+Changelog
+=========
+
+1.0a3 (2015-06-02)
+------------------
+
+- Fixed templates
+  [kroman0]
+  
+- Fixed field order #17
+  [kroman0]
+
+- Fixed Plone 5 support
+  [kroman0]
+
+- Fixed dependencies
+  [kroman0]
+
+- Set script type correctly
+  [fredvd]
+  
+1.0a2 (2014-06-13)
+------------------
+
+- Fixed conflict with plone.app.multilingual[dexterity]
+  [kroman0]
+  
+- Fixed rendering saved data form
+  [kroman0]
+  
+- Fixed and updated tests
+  [kroman0]
+  
+- Fix unicode encoding in download csv
+  [gborelli]
+
+- Added Italian translation
+  [gborelli]
+
+- Added fields parameter in Mailer override expressions
+  [gborelli]
+
+1.0a1 (2014-03-18)
+------------------
+
+- Initial release
---
+++ packages/collective.easyform/collective.easyform-1.0a3.zip/collective.easyform-1.0a3/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a3'
+
+setup(name='collective.easyform',
+      version=version,
+      description="Forms for Plone",
+      long_description=open("README.rst").read() +
+      "\n" + open("CHANGES.rst").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
+      keywords='form plone ttw',
+      author='Roman Kozlovskyi',
+      author_email='krzroman@gmail.com',
+      url='https://github.com/quintagroup/collective.easyform',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.app.textfield',
+          'plone.autoform',
+          'plone.dexterity',
+          'plone.namedfile',
+          'plone.schemaeditor',
+          'plone.supermodel',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'recaptcha': [
+              'plone.formwidget.recaptcha'
+          ],
+          'test': [
+              'plone.app.testing[robot]',
+              'plone.app.robotframework',
+          ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
