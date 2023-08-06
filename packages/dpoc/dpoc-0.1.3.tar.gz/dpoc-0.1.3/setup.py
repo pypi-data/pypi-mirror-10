7525982337d68cbce7bf65649c4589b12d9de73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:58 +0000

    Add package dpoc/0.1.3/dpoc-0.1.3.tar.gz

---
+++ packages/dpoc/dpoc-0.1.3.tar.gz/dpoc-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dpoc',
+      version='0.1.3',
+      description='Dumb Python OpenTSDB Client',
+      url='http://github.com/lexh/dpoc',
+      author='Lex Herbert',
+      author_email='lex.herbert@gmail.com',
+      license='MIT',
+      packages=['dpoc'],
+      zip_safe=False)
