a3da6834c347d438a461ca291847fe0ad7b34a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:18 +0000

    Add package gopress/0.3/gopress-0.3.zip

---
+++ packages/gopress/gopress-0.3.zip/gopress-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gopress',
+      version='0.3',
+      description='Module to scrape gopress.be website',
+      url='http://github.com/exergos/gopress-scrape',
+      author='Exergos',
+      author_email='info@exergos.be',
+      license='MIT',
+      packages=['gopress'],
+      zip_safe=False)
