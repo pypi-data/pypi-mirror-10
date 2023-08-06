a1b93064d942d7b40897494f0335dd8b67365077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:03 +0000

    Add package txrudp/0.2.0/txrudp-0.2.0-py2.py3-none-any.whl

---
+++ packages/txrudp/txrudp-0.2.0-py2.py3-none-any.whl/tests/test_packet.py
@@ -0,0 +1,238 @@
+import unittest
+
+import jsonschema
+
+from txrudp import packet
+
+
+class TestPacketAPI(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.dest_addr = ('123.45.67.89', 12345)
+        cls.source_addr = ('132.45.67.89', 54321)
+        cls.json_dict = {
+            'sequence_number': 1,
+            'dest_ip': cls.dest_addr[0],
+            'dest_port': cls.dest_addr[1],
+            'source_ip': cls.source_addr[0],
+            'source_port': cls.source_addr[1],
+            'payload': 'Yellow submarine',
+            'more_fragments': 4,
+            'ack': 28,
+            'fin': True,
+            'syn': True
+        }
+
+    def test_init_with_minimal_parametres(self):
+        p = packet.Packet(1, self.dest_addr, self.source_addr)
+        self.assertEqual(p.sequence_number, 1)
+        self.assertEqual(p.dest_addr, self.dest_addr)
+        self.assertEqual(p.source_addr, self.source_addr)
+        self.assertEqual(p.payload, '')
+        self.assertEqual(p.more_fragments, 0)
+        self.assertEqual(p.ack, 0)
+        self.assertFalse(p.fin)
+        self.assertFalse(p.syn)
+
+    def test_init_with_all_parametres(self):
+        p = packet.Packet(
+            sequence_number=1,
+            dest_addr=self.dest_addr,
+            source_addr=self.source_addr,
+            payload='Yellow submarine',
+            more_fragments=4,
+            ack=28,
+            fin=True,
+            syn=True
+        )
+        self.assertEqual(p.sequence_number, 1)
+        self.assertEqual(p.dest_addr, self.dest_addr)
+        self.assertEqual(p.source_addr, self.source_addr)
+        self.assertEqual(p.payload, 'Yellow submarine')
+        self.assertEqual(p.more_fragments, 4)
+        self.assertEqual(p.ack, 28)
+        self.assertTrue(p.fin)
+        self.assertTrue(p.syn)
+
+    def _make_packet_with_seqnum(self, seqnum):
+        return packet.Packet(seqnum, self.dest_addr, self.source_addr)
+
+    def test_ordering(self):
+        p1 = self._make_packet_with_seqnum(1)
+        p2 = self._make_packet_with_seqnum(2)
+        p3 = self._make_packet_with_seqnum(1)
+
+        self.assertEqual(p1, p1)
+        self.assertNotEqual(p1, p2)
+        self.assertEqual(p1, p3)
+
+        self.assertGreater(p2, p1)
+        self.assertLess(p1, p2)
+
+        self.assertGreaterEqual(p1, p1)
+        self.assertLessEqual(p1, p1)
+
+    def _assert_packet_equals_json(self, p, json_obj):
+        self.assertEqual(p.sequence_number, json_obj['sequence_number'])
+        self.assertEqual(p.dest_addr[0], json_obj['dest_ip'])
+        self.assertEqual(p.dest_addr[1], json_obj['dest_port'])
+        self.assertEqual(p.source_addr[0], json_obj['source_ip'])
+        self.assertEqual(p.source_addr[1], json_obj['source_port'])
+        self.assertEqual(p.payload, json_obj['payload'])
+        self.assertEqual(p.more_fragments, json_obj['more_fragments'])
+        self.assertEqual(p.ack, json_obj['ack'])
+        self.assertEqual(p.fin, json_obj['fin'])
+        self.assertEqual(p.syn, json_obj['syn'])
+
+    def test_to_json(self):
+        p = packet.Packet(
+            sequence_number=1,
+            dest_addr=self.dest_addr,
+            source_addr=self.source_addr,
+            payload='Yellow submarine',
+            more_fragments=4,
+            ack=28,
+            fin=True,
+            syn=True
+        )
+        self.assertEqual(p.to_json(), self.json_dict)
+
+    def test_from_validated_json(self):
+        p = packet.Packet.from_validated_json(self.json_dict)
+        self._assert_packet_equals_json(p, self.json_dict)
+
+    def test_from_unvalidated_good_json(self):
+        try:
+            p = packet.Packet.from_validated_json(self.json_dict)
+        except Exception:
+            self.fail('Unpacking valid JSON failed.')
+        else:
+            self._assert_packet_equals_json(p, self.json_dict)
+
+    def _assert_json_fails_validation(self, json_obj):
+        with self.assertRaises(jsonschema.ValidationError):
+            packet.Packet.from_unvalidated_json(json_obj)
+
+    def test_from_validated_bad_json_with_bad_sequence_number(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['sequence_number'] = -1
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['sequence_number'] = 3.4
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['sequence_number']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_dest_ip(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['dest_ip'] = 42
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['dest_ip'] = '127.0'
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['dest_ip'] = 'FE80:0000:0000::z:B3FF:FE1E:8329'
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['dest_ip']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_dest_port(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['dest_port'] = 3.4
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['dest_port'] = 0
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['dest_port'] = 65536
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['dest_port']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_source_ip(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['source_ip'] = 42
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['source_ip'] = '127.0'
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['source_ip'] = 'FE80:0000:0000:z::B3FF:FE1E:8329'
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['source_ip']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_source_port(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['source_port'] = 3.4
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['source_port'] = 0
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['source_port'] = 65536
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['source_port']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_payload(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['payload'] = 42
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['payload']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_more_fragments(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['more_fragments'] = 3.4
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['more_fragments'] = -1
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['more_fragments']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_ack(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['ack'] = 3.4
+        self._assert_json_fails_validation(bad_json)
+
+        bad_json['ack'] = -1
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['ack']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_fin(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['fin'] = 42
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['fin']
+        self._assert_json_fails_validation(bad_json)
+
+    def test_from_validated_bad_json_with_bad_syn(self):
+        bad_json = dict(self.json_dict)
+
+        bad_json['syn'] = 42
+        self._assert_json_fails_validation(bad_json)
+
+        del bad_json['syn']
+        self._assert_json_fails_validation(bad_json)
---
+++ packages/txrudp/txrudp-0.2.0-py2.py3-none-any.whl/txrudp/packet.py
@@ -0,0 +1,228 @@
+"""
+Specification of RUDP packet structure.
+
+Classes:
+    Packet: An RUDP packet implementing a total ordering and
+        serializing to/from JSON.
+"""
+
+import functools
+
+import jsonschema
+
+# NOTE: jsonschema's `format` specifier was tested and found
+# lacking. Hence the use of regexes from Regular Expressions Cookbook.
+# For now, only standard (non-compressed) IPv6 addresses are
+# supported. This might change in the future.
+_IPV4_REGEX = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+_IPV6_REGEX = r'^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$'
+
+RUDP_PACKET_JSON_SCHEMA = {
+    '$schema': 'http://json-schema.org/schema#',
+    'id': 'RUDP_PACKET_JSON_SCHEMA',
+    'type': 'object',
+    'properties': {
+        'sequence_number': {
+            'type': 'integer',
+            'minimum': 0
+        },
+        'dest_ip': {
+            'anyOf': [
+                {'type': 'string', 'pattern': _IPV4_REGEX},
+                {'type': 'string', 'pattern': _IPV6_REGEX}
+            ]
+        },
+        'dest_port': {
+            'type': 'integer',
+            'minimum': 1,
+            'maximum': 65535
+        },
+        'source_ip': {
+            'anyOf': [
+                {'type': 'string', 'pattern': _IPV4_REGEX},
+                {'type': 'string', 'pattern': _IPV6_REGEX}
+            ]
+        },
+        'source_port': {
+            'type': 'integer',
+            'minimum': 1,
+            'maximum': 65535
+        },
+        'payload': {
+            'type': 'string',
+            'default': ''
+        },
+        'more_fragments': {
+            'type': 'integer',
+            'minimum': 0,
+            'default': 0
+        },
+        'ack': {
+            'type': 'integer',
+            'minimum': 0,
+            'default': 0
+        },
+        'fin': {
+            'type': 'boolean',
+            'default': False
+        },
+        'syn': {
+            'type': 'boolean',
+            'default': False
+        },
+    },
+    'additionalProperties': False,
+    'required': [
+        'sequence_number',
+        'dest_ip',
+        'dest_port',
+        'source_ip',
+        'source_port',
+        'payload',
+        'ack',
+        'fin',
+        'syn',
+        'more_fragments'
+    ],
+}
+
+
+@functools.total_ordering
+class Packet(object):
+
+    """An RUDP packet."""
+
+    def __init__(
+        self,
+        sequence_number,
+        dest_addr,
+        source_addr,
+        payload='',
+        more_fragments=0,
+        ack=0,
+        fin=False,
+        syn=False,
+    ):
+        """
+        Create a new packet with the given fields.
+
+        Args:
+            seqnum: The packet's sequence number, as an int.
+            dest_addr: Tuple of destination addres (ip, port).
+            source_addr: Tuple of local host addres (ip, port).
+            payload: The packet's payload, as a string.
+            more_fragments: The number of segments that follow this
+                packet and are delivering remaining parts of the same
+                payload.
+            ack: If positive, it is the acknowledgement number of
+                the next packet the receiver is expecting. Otherwise,
+                ignore.
+            fin: When True, signals that this packet ends the connection.
+            syn: When True, signals the start of a new conenction.
+
+        NOTE: The destination address may not be the address of the
+        first recipient of this packet in case where the recipient is
+        a mediator node relaying packet (e.g. for NAT punching).
+        """
+        self.sequence_number = sequence_number
+        self.dest_addr = dest_addr
+        self.source_addr = source_addr
+        self.payload = payload
+        self.more_fragments = more_fragments
+        self.ack = ack
+        self.fin = fin
+        self.syn = syn
+
+    def __eq__(self, other):
+        if isinstance(other, Packet):
+            return self.sequence_number == other.sequence_number
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Packet):
+            return self.sequence_number < other.sequence_number
+        else:
+            return NotImplemented
+
+    def to_json(self):
+        """
+        Create a JSON object representing this packet.
+
+        Returns:
+            A JSON object that should be valid against
+            RUDP_PACKET_JSON_SCHEMA.
+        """
+        return {
+            'sequence_number': self.sequence_number,
+            'dest_ip': self.dest_addr[0],
+            'dest_port': self.dest_addr[1],
+            'source_ip': self.source_addr[0],
+            'source_port': self.source_addr[1],
+            'payload': self.payload,
+            'more_fragments': self.more_fragments,
+            'ack': self.ack,
+            'fin': self.fin,
+            'syn': self.syn
+        }
+
+    @classmethod
+    def from_unvalidated_json(cls, json_obj):
+        """
+        Create a Packet from an unvalidated json_obj.
+
+        Args:
+            json_obj: An RUDP packet in JSON format.
+
+        Returns:
+            An RUDPMesssage instance representing the same packet.
+
+        Raises:
+            jsonschema.ValidationError: The packet was invalid.
+        """
+        cls.validate(json_obj)
+        return cls.from_validated_json(json_obj)
+
+    @classmethod
+    def from_validated_json(cls, json_obj):
+        """
+        Create a Packet from a validated json_obj.
+
+        Args:
+            json_obj: An RUDP packet in JSON format.
+
+        Returns:
+            An RUDPMesssage instance representing the same packet.
+        """
+        sequence_number = int(json_obj['sequence_number'])
+        dest_addr = (json_obj['dest_ip'], int(json_obj['dest_port']))
+        source_addr = (json_obj['source_ip'], int(json_obj['source_port']))
+        payload = json_obj['payload']
+        more_fragments = json_obj['more_fragments']
+        ack = int(json_obj['ack'])
+        fin = bool(json_obj['fin'])
+        syn = bool(json_obj['syn'])
+
+        return cls(
+            sequence_number,
+            dest_addr,
+            source_addr,
+            payload,
+            more_fragments,
+            ack,
+            fin,
+            syn
+        )
+
+    @staticmethod
+    def validate(packet):
+        """
+        Test the packet against RUDP_PACKET_JSON_SCHEMA.
+
+        Args:
+            packet: The packet to validate, as a JSON object.
+
+        Raises:
+            jsonschema.ValidationError: The packet was invalid.
+        """
+        jsonschema.validate(packet, RUDP_PACKET_JSON_SCHEMA)
