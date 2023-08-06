5b225f95e5f1e38dd6f5e7d08d70c7ce4bc6f23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:21 +0000

    Add package bypy/1.0.20/bypy-1.0.20.tar.gz

---
+++ packages/bypy/bypy-1.0.20.tar.gz/bypy-1.0.20/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup,find_packages
+
+import bypy
+
+setup(
+	name='bypy',
+	version=bypy.__version__,
+	description='Python client for Baidu Yun (Personal Cloud Storage) 百度云/百度网盘 Python 客户端',
+	author='Hou Tianze',
+	author_email='houtianze@gmail.com',
+	url='https://github.com/houtianze/bypy',
+	download_url='https://github.com/houtianze/bypy/tarball/1.0.20',
+	packages=find_packages(),
+	scripts=['bypy.py', 'bypygui.pyw'],
+	keywords = ['bypy', 'baidu', 'baidu pcs', 'client']
+	)
+
+# vim: set fileencoding=utf-8
