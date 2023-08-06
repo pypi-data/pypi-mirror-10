f42e248852e129519aa99681ce43a710f8cd8d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:10 +0000

    Add package django-wfs/0.0.3/django-wfs-0.0.3.tar.gz

---
+++ packages/django-wfs/django-wfs-0.0.3.tar.gz/django-wfs-0.0.3/django-wfs/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'django-wfs.apps.WFSConfig'
---
+++ packages/django-wfs/django-wfs-0.0.3.tar.gz/django-wfs-0.0.3/django-wfs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WFSConfig(AppConfig):
+    name = "django-wfs"
+    verbose_name = "WFS"
