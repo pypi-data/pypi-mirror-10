5d82f42458a2f92b20b6c87ef7cf167312ebf44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:39 +0000

    Add package AtomicPuppy/0.1.2/AtomicPuppy-0.1.2.tar.gz

---
+++ packages/AtomicPuppy/AtomicPuppy-0.1.2.tar.gz/AtomicPuppy-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name="AtomicPuppy",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=0.15.3",
+        "asyncio==3.4.3",
+        "chardet==2.3.0",
+        "gevent==1.0.1",
+        "greenlet==0.4.5",
+        "pybreaker==0.2.3",
+        "PyYAML==3.11",
+        "redis==2.10.3",
+        "retrying==1.3.3",
+        "requests==2.7.0"
+    ],
+    url = 'https://github.com/madedotcom/atomicpuppy',
+    download_url = 'https://github.com/madedotcom/atomicpuppy/tarball/0.1.2',
+    description = 'A service-activator component for eventstore',
+    author = 'Bob Gregory',
+    keywords = ['eventstore'],
+)
