a29d9cb1358f350321357c56cc6f7e30306b5970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:01 +0000

    Add package yagmail/0.4.82/yagmail-0.4.82.tar.gz

---
+++ packages/yagmail/yagmail-0.4.82.tar.gz/yagmail-0.4.82/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+# run updater like: python setup.py sdist bdist_wininst upload
+
+MAJOR_VERSION = '0'
+MINOR_VERSION = '4'
+MICRO_VERSION = '82'
+VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+
+setup(name = 'yagmail',
+      version = VERSION,
+      description = 'Yet Another GMAIL client',
+      url = 'https://github.com/kootenpv/yagmail',
+      author = 'Pascal van Kooten',
+      author_email = 'kootenpv@gmail.com',
+      license = 'GPL',
+      packages = ['yagmail'],
+      install_requires = [ 
+          'keyring',
+      ],
+      entry_points = { 
+          'console_scripts': ['yagmail = yagmail.yagmail:main'] 
+          },
+      zip_safe = False)
